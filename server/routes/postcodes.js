b5499d6f0cb3f9ac06cf357c721175c3ffde1a3e
Steven Hamilton <sutebeno@gmail.com>
2018-11-22 11:07:33 +0000

    Tidying and commenting postcode.js

--- server/routes/postcodes.js
+++ server/routes/postcodes.js
@@ -2,32 +2,28 @@ var express = require('express');
 var router = express.Router();
 const models = require('../models/index');
 
-/* GET all locations and POST new locations */
-// CRUD Location API by postalCode
+/* GET with Postcode parameter to find matching addresses */
 router.route('/:postcode')
 
   .get(async function (req, res) {
 
-    //console.log(res);
-
-
-    // models.testpostcode.create({
-    //   postalcode: 'SUCC ESS'
-    // });
     let postcodeData =[];
 
+    //Find matching postcode data
     let results = await models.pcodedata.findAll({
       where: {
         postcode: req.params.postcode
       }
     });
 
+    //Go through any results found from DB and map to JSON
     for (let i=0, len=results.length; i<len; i++){
 
       let data=results[i].dataValues;
 
       let myObject = {
         locationName: data.rm_organisation_name,
+        //Use Building Name if it exists, otherwise use building number
         addressLine1: (data.building_name !== "") ? data.building_name : data.building_number,
         addressLine2: data.street_description,
         townCity: data.post_town,
