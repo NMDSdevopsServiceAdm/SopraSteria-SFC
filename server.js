85150d172fc7f7a8e3261156d8033435baaa7164
Nasir Muhammad <nasir.m@hotmail.co.uk>
2018-11-26 11:24:36 +0000

    registration files

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./server/routes/index');
 var locations = require('./server/routes/locations');
 var postcodes = require('./server/routes/postcodes');
+var registration = require('./server/routes/registration');
+
 var errors = require('./server/routes/errors');
 
 var app = express();
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // app.use('/', routes);
 app.use('/api/locations', locations);
 app.use('/api/postcodes', postcodes);
+app.use('/api/registration', registration);
 app.use('/api/errors', errors);
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
