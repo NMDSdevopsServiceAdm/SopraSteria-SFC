f38df1ddd785036176575cbae68f37eb242b0b74
denny13 <dfeighan13@gmail.com>
2018-12-03 20:28:23 +0000

    Update the validation

--- src/app/core/model/registration.model.ts
+++ src/app/core/model/registration.model.ts
@@ -1,15 +1,14 @@
 
 export interface RegistrationModel {
 
-  locationId: string,
-  locationName: string,
-  addressLine1: string,
-  addressLine2: string,
-  townCity: string,
-  county: string,
-  postalCode: string,
-  mainService: string,
-  isRegulated: boolean,
+  locationName: string;
+  addressLine1: string;
+  addressLine2: string;
+  townCity: string;
+  county: string;
+  postalCode: string;
+  mainService: string;
+  isRegulated: boolean;
   user: {
     fullname: string,
     jobTitle: string,
@@ -18,9 +17,9 @@ export interface RegistrationModel {
     username: string,
     password: string,
     securityQuestion: string,
-    securityAnswer: string,
-  },
-  detailsChanged: boolean
+    securityAnswer: string
+  };
+  detailsChanged: boolean;
   //  fullname: string;
   //  jobTitle: string;
   //  emailAddress: string;
