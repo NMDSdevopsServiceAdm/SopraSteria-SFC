d8ae0e6b635b1492751ba2f65706c73bfdcd1a9a
Adam Faryna <Adam Faryna adam.faryna@ext.soprasteria.com>
2018-12-27 19:19:46 +0000

    Vacancies & vacancies-confirmation: added submit, fixed some UI errors.

--- src/app/core/services/job.service.ts
+++ src/app/core/services/job.service.ts
@@ -1,22 +1,16 @@
 import { Injectable } from "@angular/core"
-import { HttpClient, HttpErrorResponse } from "@angular/common/http"
-import { Router } from "@angular/router"
-import { Observable, BehaviorSubject } from "rxjs"
+import { HttpClient } from "@angular/common/http"
+import { BehaviorSubject } from "rxjs"
 import { catchError, debounceTime, map } from "rxjs/operators"
-import { ErrorObservable } from "rxjs-compat/observable/ErrorObservable"
 import { FormBuilder, FormGroup } from "@angular/forms"
 
 import { HttpErrorHandler } from "./http-error-handler.service"
-import { Message } from "../model/message.model"
-import { Job } from "../model/job.model"
 
 @Injectable({
   providedIn: "root"
 })
 export class JobService {
   constructor(private http: HttpClient, private httpErrorHandler: HttpErrorHandler, private formBuilder: FormBuilder) {}
-
-  public currentVacanciesForm: BehaviorSubject<FormGroup> = new BehaviorSubject<FormGroup>(this.formBuilder.group({}))
 
   /*
    * GET /api/jobs
@@ -28,7 +22,7 @@ export class JobService {
       }
     }
 
-    return this.http.get<any>(`/api/jobs`, options)
+    return this.http.get<any>("/api/jobs", options)
       .pipe(
         debounceTime(500),
         map(res => res.jobs),
