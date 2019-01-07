5e13f6d6dc002c4da0f356e6e9d42e90f19c5fbb
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-01-07 12:35:29 +0000

    Handling DB SSL as a environment variable override

--- server/models/index.js
+++ server/models/index.js
@@ -16,6 +16,14 @@ config.port = process.env.DB_PORT ?  process.env.DB_PORT : config.port;
 config.database = process.env.DB_NAME ?  process.env.DB_NAME : config.database;
 config.username = process.env.DB_USER ?  process.env.DB_USER : config.username;
 config.password = process.env.DB_PASS ?  process.env.DB_PASS : config.password;
+config.password = process.env.DB_PASS ?  process.env.DB_PASS : config.password;
+if (config.dialectOptions) {
+  config.dialectOptions.ssl = process.env.DB_SSL && parseInt(process.env.DB_SSL) === 1 ?  true : false;
+} else {
+  config.dialectOptions = {
+    ssl: process.env.DB_SSL && parseInt(process.env.DB_SSL) === 1 ?  true : false
+  }
+}
 
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
