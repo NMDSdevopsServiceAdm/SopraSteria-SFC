726b3ab3648d263dc5cfed0b48ab93f88c213afd
Nasir Muhammad <nasir.m@hotmail.co.uk>
2019-01-09 22:21:38 +0000

    fix build error

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const fs = require('fs');xxxxxxxxxxxxxxxxxxxx
+const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
