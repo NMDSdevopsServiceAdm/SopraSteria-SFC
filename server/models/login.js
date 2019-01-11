f29d5f9e612d3b4188084f7654a7e4aa074110fd
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-01-11 09:21:02 +0000

    Relaxing the constraint on login model to allow nullable passwords

--- server/models/login.js
+++ server/models/login.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     password: {
      type: DataTypes.TEXT,
-     allowNull: false,
+     allowNull: true,
      field: "Password"
     },
     securityQuestion: {
