cc3168d7f52f78857d416244884ecf58181edaec
Adam Faryna <Adam Faryna adam.faryna@ext.soprasteria.com>
2019-01-28 16:19:16 +0000

    message-service: support for multiple messages of the same type at once.

--- src/app/core/services/message.service.ts
+++ src/app/core/services/message.service.ts
@@ -6,40 +6,70 @@ import { BehaviorSubject, Observable } from 'rxjs'
   providedIn: "root"
 })
 export class MessageService {
-  private _success$ = new BehaviorSubject<string>("")
-  private _info$ = new BehaviorSubject<string>("")
-  private _warning$ = new BehaviorSubject<string>("")
-  private _error$ = new BehaviorSubject<string>("")
+  private _success$ = new BehaviorSubject<string[]>([])
+  private _info$ = new BehaviorSubject<string[]>([])
+  private _warning$ = new BehaviorSubject<string[]>([])
+  private _error$ = new BehaviorSubject<string[]>([])
 
-  success$: Observable<string> = this._success$.asObservable()
-  info$: Observable<string> = this._info$.asObservable()
-  warning$: Observable<string> = this._warning$.asObservable()
-  error$: Observable<string> = this._error$.asObservable()
+  private success: string[] = []
+  private info: string[] = []
+  private warning: string[] = []
+  private error: string[] = []
 
+  success$: Observable<string[]> = this._success$.asObservable()
+  info$: Observable<string[]> = this._info$.asObservable()
+  warning$: Observable<string[]> = this._warning$.asObservable()
+  error$: Observable<string[]> = this._error$.asObservable()
+
+  // DEPRECATED - use dedicated methods
   show(status, message) {
     switch(status) {
-      case "success": this._success$.next(message); break;
-      case "info": this._info$.next(message); break;
-      case "warning": this._warning$.next(message); break;
-      case "error": this._error$.next(message); break;
+      case "success": this.showSuccess(message); break;
+      case "info": this.showInfo(message); break;
+      case "warning": this.showWarning(message); break;
+      case "error": this.showError(message); break;
       default: throw new TypeError(`Unknown message type ${JSON.stringify(message)}!`)
     }
   }
 
+  showSuccess(message) {
+    this.success.push(message)
+    this._success$.next(this.success)
+  }
+
+  showInfo(message) {
+    this.info.push(message)
+    this._info$.next(this.info)
+  }
+
+  showWarning(message) {
+    this.warning.push(message)
+    this._warning$.next(this.warning)
+  }
+
+  showError(message) {
+    this.error.push(message)
+    this._error$.next(this.error)
+  }
+
   clearSuccess() {
-    this._success$.next("")
+    this.success = []
+    this._success$.next(this.success)
   }
 
   clearInfo() {
-    this._info$.next("")
+    this.info = []
+    this._info$.next(this.info)
   }
 
   clearWarning() {
-    this._warning$.next("")
+    this.warning = []
+    this._warning$.next(this.warning)
   }
 
   clearError() {
-    this._error$.next("")
+    this.error = []
+    this._error$.next(this.error)
   }
 
   clearAll() {
