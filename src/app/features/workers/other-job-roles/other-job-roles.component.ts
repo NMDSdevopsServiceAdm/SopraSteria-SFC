4cb4699bd37f28a1b96859e435768c968d8769d1
Adam Faryna <Adam Faryna adam.faryna@ext.soprasteria.com>
2019-01-29 16:56:23 +0000

    other-job-role-front: done

--- src/app/features/workers/other-job-roles/other-job-roles.component.ts
+++ src/app/features/workers/other-job-roles/other-job-roles.component.ts
@@ -1,14 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormArray, FormGroup, FormBuilder, Validators } from "@angular/forms"
+import { ActivatedRoute, Router } from "@angular/router"
+import { Subscription } from "rxjs"
+
+import { MessageService } from "../../../core/services/message.service"
+import { WorkerService } from "../../../core/services/worker.service"
+import { JobService } from "../../../core/services/job.service"
+import { Worker } from "../../../core/model/worker.model"
+import { Job } from "../../../core/model/job.model"
+
 
 @Component({
   selector: 'app-other-job-roles',
   templateUrl: './other-job-roles.component.html'
 })
-export class OtherJobRolesComponent implements OnInit {
+export class OtherJobRolesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private workerService: WorkerService,
+    private messageService: MessageService,
+    private jobService: JobService
+  ) {
+    this.saveHandler = this.saveHandler.bind(this)
   }
 
+  form: FormGroup
+  availableJobRoles: Job[]
+
+  private subscriptions: Subscription[] = []
+  private worker: Worker
+  private workerId: string
+
+  async submitHandler() {
+    try {
+      await this.saveHandler()
+      this.router.navigate([`/worker/national-insurance-number/${this.workerId}`])
+
+    } catch (err) {
+      // keep typescript transpiler silent
+    }
+  }
+
+  saveHandler() {
+    return new Promise((resolve, reject) => {
+      this.messageService.clearError()
+
+      if (this.form.valid) {
+        this.worker.otherJobs = this.form.value.selectedJobRoles.filter(j => j.checked)
+        this.subscriptions.push(
+          this.workerService.updateWorker(this.workerId, this.worker).subscribe(resolve, reject)
+        )
+
+      } else {
+        reject()
+      }
+    })
+  }
+
+  onChange(control) {
+    control.value.checked = !control.value.checked
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      selectedJobRoles: this.formBuilder.array([])
+    })
+
+    const params = this.route.snapshot.paramMap
+    this.workerId = params.has("id") ? params.get("id") : null
+
+    this.subscriptions.push(
+      this.jobService.getJobs().subscribe(availableJobRoles => {
+        if (this.workerId) {
+          this.subscriptions.push(
+            this.workerService.getWorker(this.workerId).subscribe(worker => {
+              this.worker = worker
+              const selectedJobRoles = (FormArray) this.form.controls.selectedJobRoles
+
+              const jobs = worker.otherJobs ?
+                availableJobRoles.map(j => this.formBuilder.control({jobId: j.id, title: j.title, checked: worker.otherJobs.some(o => o.jobId === j.id)})) :
+                availableJobRoles.map(j => this.formBuilder.control({jobId: j.id, title: j.title, checked: false}))
+              jobs.forEach(j => (this.form.controls.selectedJobRoles as FormArray).push(j))
+            })
+          )
+        }
+      })
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe())
+    this.messageService.clearAll()
+  }
 }
