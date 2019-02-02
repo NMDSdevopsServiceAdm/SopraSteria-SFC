dd120806fb2b5df82445a219c5c25b45f436083e
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-02-02 13:11:28 +0000

    Removing unnecessary logging.

--- server/routes/testOnly/location.js
+++ server/routes/testOnly/location.js
@@ -5,12 +5,8 @@ const models = require('../../models');
 // returns a random set of addresses from the location dataset
 //  if passing the limit parameter can vary the set size (defaults to 100)
 router.route('/random').get(async (req, res) => {
-  const setSize = req.query.limit ? req.query.limit : 10;
-  const sanitizeSize = req.sanitize(req.query.limit);
-
-  console.log("WA DEBUG : set size: ", setSize);
-  console.log("WA DEBUG : sanitized size: ", sanitizeSize);
-
+  const setSize = req.sanitize(req.query.limit) ? req.sanitize(req.query.limit) : 10;
+  
   try {
     let results = await models.location.findAll({
       order: [
