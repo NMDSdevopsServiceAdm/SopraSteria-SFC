a3cce76b569159ddde097ce524e41c72157bfdca
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-02-02 10:09:16 +0000

    Revising the scope of "test" api to include sfcdev too, to allow remote running of regression tests. Adding a timestamp test API endpoint, that will make it easier to allow for differences in test host and server times.

--- server/routes/testOnly/index.js
+++ server/routes/testOnly/index.js
@@ -2,16 +2,18 @@
 const express = require('express');
 const router = express.Router();
 
-const Authorization = require('../../utils/security/isLocalhost');
+const Authorization = require('../../utils/security/isLocalTest');
 
 const Postcode = require('./postcode');
 const Location = require('./location');
 const Truncate = require('./cleanStart');
+const Timestamp = require('./timestamp');
 
-// ensure all test only routes are authorised - restricted by localhost
+// ensure all test only routes are authorised - restricted by local test environments (localhost and dev only)
 router.use('/', Authorization.isAuthorised);
 router.use('/postcodes', Postcode);
 router.use('/locations', Location);
 router.use('/clean', Truncate);
+router.use('/timestamp', Timestamp);
 
 module.exports = router;
