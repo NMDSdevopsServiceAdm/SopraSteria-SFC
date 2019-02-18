223236887eaf8dc9413a8cef9adcf935ca4e6ea0
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-02-18 06:17:36 +0000

    Deleting unused

--- proxy.js
+++ proxy.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./server/routes/index');
-var errors = require('./server/routes/errors');
 
 var proxy = require('express-http-proxy');
 var app = express();
