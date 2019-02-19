925cf076f86c4156eac87542d4b6f7c68ae6141a
Charlie Francis <charliefrancis@gmail.com>
2019-02-19 13:08:53 +0000

    Moving Shared Directives into dedicated folder

--- src/app/shared/components/date-picker/date-picker.component.ts
+++ src/app/shared/components/date-picker/date-picker.component.ts
@@ -1,45 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, Validators } from "@angular/forms"
-import { DateValidator } from "../../core/validators/date.validator"
+import { FormGroup, Validators } from '@angular/forms';
+import { DateValidator } from 'src/app/core/validators/date.validator';
 
 @Component({
   selector: 'app-date-picker',
-  templateUrl: './date-picker.component.html'
+  templateUrl: './date-picker.component.html',
 })
 export class DatePickerComponent implements OnInit {
-
-  @Input() formControlPrefix: string = ""
-  @Input() formGroup: FormGroup
-  @Input() label: string = ""
+  @Input() formControlPrefix = '';
+  @Input() formGroup: FormGroup;
+  @Input() label = '';
 
   ngOnInit() {
-    this.formGroup.setValidators(
-      Validators.compose([
-        this.formGroup.validator,
-        DateValidator.dateValid()
-      ])
-    )
+    this.formGroup.setValidators(Validators.compose([this.formGroup.validator, DateValidator.dateValid()]));
 
     this.formGroup.controls.day.setValidators(
-      Validators.compose([
-        this.formGroup.controls.day.validator,
-        Validators.min(1)
-      ])
-    )
+      Validators.compose([this.formGroup.controls.day.validator, Validators.min(1)])
+    );
 
     this.formGroup.controls.month.setValidators(
-      Validators.compose([
-        this.formGroup.controls.month.validator,
-        Validators.min(1)
-      ])
-    )
+      Validators.compose([this.formGroup.controls.month.validator, Validators.min(1)])
+    );
 
     this.formGroup.controls.year.setValidators(
-      Validators.compose([
-        this.formGroup.controls.year.validator,
-        Validators.minLength(4),
-        Validators.maxLength(4)
-      ])
-    )
+      Validators.compose([this.formGroup.controls.year.validator, Validators.minLength(4), Validators.maxLength(4)])
+    );
   }
 }
