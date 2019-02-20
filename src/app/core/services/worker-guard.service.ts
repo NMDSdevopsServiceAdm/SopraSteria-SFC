07290772db0c4dea97f45cc7eacf644906822057
Charlie Francis <charliefrancis@gmail.com>
2019-02-20 11:45:04 +0000

    Fixing Worker Guard by removing subscription

--- src/app/core/services/worker-guard.service.ts
+++ src/app/core/services/worker-guard.service.ts
@@ -3,32 +3,26 @@ import { CanActivateChild, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError, map, take } from 'rxjs/operators';
 import { WorkerService } from './worker.service';
-import { Worker } from '../../core/model/worker.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WorkerGuard implements CanActivateChild {
-  worker: Worker;
-
-  constructor(private router: Router, private _workerService: WorkerService) {}
+  constructor(private router: Router, private workerService: WorkerService) {}
 
   canActivateChild(): Observable<boolean> {
-    this._workerService.worker$.subscribe(worker => this.worker = worker);
-
-    //const workerId = this.workerService.workerId;
-    const workerId = this._workerService.worker.uid;
+    const workerId = this.workerService.workerId;
 
     if (!workerId) {
       return this.denyAccess();
     }
 
-    return this._workerService.getWorker(workerId).pipe(
+    return this.workerService.getWorker(workerId).pipe(
       take(1),
       map(worker => true),
       catchError(error => {
         return this.denyAccess();
-      }),
+      })
     );
   }
 
