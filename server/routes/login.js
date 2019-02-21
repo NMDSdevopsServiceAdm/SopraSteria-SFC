2a92f73325de68288a7e602b392fc9c048b2b1e4
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-02-21 13:24:00 +0000

    Tracking invalid login attempts with auditing plus also incmrementing InvalidAttempt counter on the record. Sends (would do) emai lon after 10 failed attempts.

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,6 +7,7 @@
 const Login = require('../models').login;
 
 const generateJWT = require('../utils/security/generateJWT');
+const uuid = require('uuid');
 
 router.post('/',async function(req, res) {
    Login
@@ -16,10 +17,10 @@ router.post('/',async function(req, res) {
           isActive:true
         }
         ,
-        attributes: ['id', 'isActive', 'registrationId', 'firstLogin', 'Hash'],
+        attributes: ['id', 'username', 'isActive', 'invalidAttempt', 'registrationId', 'firstLogin', 'Hash'],
         include: [ {
           model: models.user,
-          attributes: ['fullname', 'isAdmin','establishmentId'],
+          attributes: ['id', 'fullname', 'email', 'isAdmin','establishmentId'],
           include: [{
             model: models.establishment,
             attributes: ['id', 'name', 'isRegulated', 'nmdsId'],
@@ -39,9 +40,8 @@ router.post('/',async function(req, res) {
           });
         }
 
-        login.comparePassword(req.body.password, (err, isMatch) => {
+        login.comparePassword(req.body.password, async (err, isMatch) => {
           if (isMatch && !err) {
-            console.log("WA DEBUG - inside login endpoint - user: ", login.user.isAdmin)
             const token = generateJWT.loginJWT(12, login.user.establishmentId, req.body.username, login.user.isAdmin);
             var date = new Date().getTime();
             date += (12 * 60 * 60 * 1000);          
@@ -54,17 +54,74 @@ router.post('/',async function(req, res) {
               new Date(date).toISOString()
             );
 
-            // check if this is the first time logged in and if so, update the "FirstLogin" timestamp
-            if (!login.firstLogin) {
-                login.update({
-                  firstLogin: new Date()
-                });
-            }
+            await models.sequelize.transaction(async t => {
+              // check if this is the first time logged in and if so, update the "FirstLogin" timestamp
+              // reset the number of failed attempts on any successful login
+              const loginUpdate = {
+                invalidAttempt: 0
+              };
+              if (!login.firstLogin) {
+                loginUpdate.firstLogin = new Date();
+              }
+              login.update(loginUpdate);
+
+              // add an audit record
+              const auditEvent = {
+                userFk: login.user.id,
+                username: login.username,
+                type: 'loginSuccess',
+                property: 'password',
+                event: {}
+              };
+              await models.userAudit.create(auditEvent);
+            });
 
             return res.set({'Authorization': 'Bearer ' + token}).status(200).json(response);
 
           } else {
-              res.status(401).send({success: false, msg: 'Authentication failed.'});
+            await models.sequelize.transaction(async t => {
+              const maxNumberOfFailedAttempts = 10;
+
+              // increment the number of failed attempts by one
+              const loginUpdate = {
+                invalidAttempt: login.invalidAttempt + 1
+              };
+              login.update(loginUpdate);
+
+              if (login.invalidAttempt === (maxNumberOfFailedAttempts+1)) {
+                // send reset password email
+                const expiresTTLms = 60*24*1000; // 24 hours
+
+                const requestUuid = uuid.v4();
+                const now = new Date();
+                const expiresIn = new Date(now.getTime() + expiresTTLms);
+          
+                await models.passwordTracking.create({
+                  userFk: login.user.id,
+                  created: now.toISOString(),
+                  expires: expiresIn.toISOString(),
+                  uuid: requestUuid
+                });
+          
+                const resetLink = `${req.protocol}://${req.get('host')}/api/registration/validateResetPassword?reset=${requestUuid}`;
+                console.log("WA TODO - send the email link by email: ", resetLink);
+
+                // TODO - send the email! - https://trello.com/c/ONiKc7Ck
+          
+              }
+
+              // add an audit record
+              const auditEvent = {
+                userFk: login.user.id,
+                username: login.username,
+                type: login.invalidAttempt >= (maxNumberOfFailedAttempts+1) ? 'loginWhileLocked' : 'loginFailed',
+                property: 'password',
+                event: {}
+              };
+              await models.userAudit.create(auditEvent);
+            });
+
+            res.status(401).send({success: false, msg: 'Authentication failed.'});
           }
         })
       })
