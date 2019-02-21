dc3ea03bac22d157eb887180544135f91304c780
Charlie Francis <charliefrancis@gmail.com>
2019-02-21 17:39:19 +0000

    143: Updating Services Capacity Component to give max value

--- src/app/features/services-capacity/services-capacity.component.ts
+++ src/app/features/services-capacity/services-capacity.component.ts
@@ -1,65 +1,66 @@
-import { Component, Input, OnInit, OnDestroy } from "@angular/core"
-import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms"
-import { Router } from "@angular/router"
-
-import { MessageService } from "../../core/services/message.service"
-import { EstablishmentService } from "../../core/services/establishment.service"
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { MessageService } from '@core/services/message.service';
 
 @Component({
   selector: 'app-services-capacity',
   templateUrl: './services-capacity.component.html',
-  styleUrls: ['./services-capacity.component.scss']
+  styleUrls: ['./services-capacity.component.scss'],
 })
 export class ServicesCapacityComponent implements OnInit, OnDestroy {
+  public form: FormGroup;
+  public capacitiesAvailable: object;
+  private subscriptions = [];
+
   constructor(
-    private fb: FormBuilder,
+    private formBuilder: FormBuilder,
     private router: Router,
     private establishmentService: EstablishmentService,
-    private messageService: MessageService) {}
+    private messageService: MessageService
+  ) {}
 
-  form: FormGroup
+  ngOnInit() {
+    this.form = this.formBuilder.group({});
 
-  capacitiesAvailable: object;
+    this.subscriptions.push(
+      this.establishmentService.getCapacity(true).subscribe(data => {
+        this.capacitiesAvailable = data.allServiceCapacities;
 
-  private subscriptions = []
+        data.allServiceCapacities.forEach(service => {
+          service.questions.forEach(question => {
+            let answer = data.capacities.find(cc => question.questionId === cc.questionId);
+            answer = answer ? answer.answer : '';
+            this.form.registerControl(
+              question.questionId.toString(),
+              this.formBuilder.control(parseFloat(answer), [Validators.required])
+            );
+          });
+        });
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
+    this.messageService.clearAll();
+  }
 
   submitHandler(): void {
     if (this.form.valid) {
       this.subscriptions.push(
-        this.establishmentService.postCapacity(this.formToApi())
-          .subscribe(() => this.router.navigate(["/share-options"])))
-
+        this.establishmentService
+          .postCapacity(this.formToApi())
+          .subscribe(() => this.router.navigate(['/share-options']))
+      );
     } else {
-      this.messageService.clearError()
-      this.messageService.show("error", "Please fill the required fields.")
+      this.messageService.clearError();
+      this.messageService.show('error', 'Please fill the required fields.');
     }
   }
 
   private formToApi() {
-    return Object.entries(this.form.value).map(([key, value]) => ({ questionId: parseFloat(key), answer: value }))
-  }
-
-  ngOnInit() {
-    this.form = this.fb.group({})
-
-    this.subscriptions.push(
-      this.establishmentService.getCapacity(true)
-        .subscribe(data => {
-          this.capacitiesAvailable = data.allServiceCapacities
-
-          data.allServiceCapacities.forEach(service => {
-            service.questions.forEach(question => {
-              let answer = data.capacities.find(cc => question.questionId === cc.questionId)
-              answer = answer ? answer.answer : ""
-              this.form.registerControl(question.questionId.toString(), this.fb.control(parseFloat(answer), [Validators.required]))
-            })
-          })
-        })
-    )
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.forEach(s => s.unsubscribe())
-    this.messageService.clearAll()
+    return Object.entries(this.form.value).map(([key, value]) => ({ questionId: parseFloat(key), answer: value }));
   }
 }
