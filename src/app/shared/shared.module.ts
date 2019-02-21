043836e2f4407a8bd32186c71cc136ef985b58d2
Charlie Francis <charliefrancis@gmail.com>
2019-02-21 18:21:38 +0000

    343: Updating Start Button to be a reusable component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AutoSuggestComponent } from './components/auto-suggest/auto-suggest.component';
 import { DatePickerComponent } from './components/date-picker/date-picker.component';
 import { DetailsComponent } from './components/details/details.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { StartButtonComponent } from './components/start-button/start-button.component';
 import { SubmitButtonComponent } from './components/submit-button/submit-button.component';
 import { NoPaste } from './directives/no-paste.directive';
 import { NumberDigitsMax } from './directives/number-digits-max.directive';
@@ -14,35 +16,34 @@ import { NumberPositiveOnly } from './directives/number-positive-only.directive'
 import { Number } from './directives/number.directive';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   declarations: [
     AutoSuggestComponent,
     DatePickerComponent,
     DetailsComponent,
     MessagesComponent,
+    StartButtonComponent,
     SubmitButtonComponent,
     NoPaste,
     NumberDigitsMax,
     NumberIntOnly,
     NumberMax,
     NumberPositiveOnly,
-    Number
+    Number,
   ],
   exports: [
     AutoSuggestComponent,
     DatePickerComponent,
     DetailsComponent,
     MessagesComponent,
+    StartButtonComponent,
     SubmitButtonComponent,
     NoPaste,
     NumberDigitsMax,
     NumberIntOnly,
     NumberMax,
     NumberPositiveOnly,
-    Number
+    Number,
   ],
 })
 export class SharedModule {}
