20eea14b0eb73745eb9d0976139f8217bbaadace
Charlie Francis <charliefrancis@gmail.com>
2019-02-25 14:41:41 +0000

    Resolver now redirects to welcome if not found

--- src/app/features/workers/worker.resolver.ts
+++ src/app/features/workers/worker.resolver.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { WorkerService } from '@core/services/worker.service';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class WorkerResolver implements Resolve<any> {
-  constructor(private workerService: WorkerService) {}
+  constructor(private router: Router, private workerService: WorkerService) {}
 
   resolve(route: ActivatedRouteSnapshot) {
-    return this.workerService.getWorker(route.paramMap.get('id'));
+    return this.workerService.getWorker(route.paramMap.get('id')).pipe(
+      catchError(() => {
+        this.router.navigate(['/welcome']);
+        return of(null);
+      })
+    );
   }
 }
