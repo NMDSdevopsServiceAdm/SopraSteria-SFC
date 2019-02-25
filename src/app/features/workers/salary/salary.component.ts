5664fea65293d70376c5098dfb1f7f14aa04fd35
Charlie Francis <charliefrancis@gmail.com>
2019-02-25 13:36:16 +0000

    Updating Components to use new Worker Resolver

--- src/app/features/workers/salary/salary.component.ts
+++ src/app/features/workers/salary/salary.component.ts
@@ -1,31 +1,42 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Contracts } from '@core/constants/contracts.enum';
 import { Worker } from '@core/model/worker.model';
 import { MessageService } from '@core/services/message.service';
 import { WorkerEditResponse, WorkerService } from '@core/services/worker.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-salary',
   templateUrl: './salary.component.html',
 })
 export class SalaryComponent implements OnInit, OnDestroy {
+  public backLink: string;
   public form: FormGroup;
   private worker: Worker;
   private workerId: string;
-  private subscriptions = [];
+  private subscriptions: Subscription = new Subscription();
 
   constructor(
     private workerService: WorkerService,
     private messageService: MessageService,
     private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
     private router: Router
   ) {
     this.saveHandler = this.saveHandler.bind(this);
   }
 
   ngOnInit() {
+    this.worker = this.route.parent.snapshot.data.worker;
+
+    this.backLink =
+      this.worker.zeroHoursContract === 'Yes' ||
+      [Contracts.Agency, Contracts.Pool_Bank, Contracts.Other].includes(this.worker.contract)
+        ? 'average-weekly-hours'
+        : 'weekly-contracted-hours';
+
     this.form = this.formBuilder.group({
       terms: null,
       rate: null,
@@ -45,45 +56,24 @@ export class SalaryComponent implements OnInit, OnDestroy {
       rate.updateValueAndValidity();
     });
 
-    this.workerId = this.workerService.workerId;
-
-    this.subscriptions.push(
-      this.workerService.getWorker(this.workerId).subscribe(worker => {
-        this.worker = worker;
-
-        if (worker.annualHourlyPay) {
-          this.form.patchValue({
-            terms: worker.annualHourlyPay.value,
-            rate: worker.annualHourlyPay.rate,
-          });
-        }
-      })
-    );
+    if (this.worker.annualHourlyPay) {
+      this.form.patchValue({
+        terms: this.worker.annualHourlyPay.value,
+        rate: this.worker.annualHourlyPay.rate,
+      });
+    }
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach(s => s.unsubscribe());
+    this.subscriptions.unsubscribe();
     this.messageService.clearAll();
-  }
-
-  goBack(event: MouseEvent) {
-    event.preventDefault();
-
-    if (
-      this.worker.zeroHoursContract === 'Yes' ||
-      [Contracts.Agency, Contracts.Pool_Bank, Contracts.Other].includes(this.worker.contract)
-    ) {
-      this.router.navigate(['/worker/average-weekly-hours']);
-    } else {
-      this.router.navigate(['/worker/weekly-contracted-hours']);
-    }
   }
 
   async submitHandler() {
     try {
       await this.saveHandler();
 
-      this.router.navigate(['/worker/care-certificate']);
+      this.router.navigate(['/worker', this.worker.uid, 'care-certificate']);
     } catch (err) {
       // keep typescript transpiler silent
     }
@@ -98,7 +88,7 @@ export class SalaryComponent implements OnInit, OnDestroy {
         const worker = this.worker || ({} as Worker);
         worker.annualHourlyPay = terms.value ? { value: terms.value, rate: rate.value } : null;
 
-        this.subscriptions.push(this.workerService.setWorker(worker).subscribe(resolve, reject));
+        this.subscriptions.add(this.workerService.setWorker(worker).subscribe(resolve, reject));
       } else {
         // TODO - ideally, the min and max values should be constants
         // and shared equally between onNgInit when creating the form control
