538723ba4a4ec16f6032863694b0e1f2be8d6398
Charlie Francis <charliefrancis@gmail.com>
2019-02-25 10:09:16 +0000

    Adding Staff Records Table and styling

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Worker } from '@core/model/worker.model';
+import { WorkerService } from '@core/services/worker.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  public workers: Worker[];
+  private subscriptions = [];
 
-  constructor() { }
+  constructor(private workerService: WorkerService) {}
 
   ngOnInit() {
+    this.subscriptions.push(
+      this.workerService.getAllWorkers().subscribe(data => {
+        console.log(data);
+        this.workers = data;
+      })
+    );
   }
-
 }
