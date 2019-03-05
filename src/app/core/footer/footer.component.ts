1e31f341622c6aedb21f9af0942166eb3ae8fcf2
Charlie Francis <charliefrancis@gmail.com>
2019-03-05 22:49:52 +0000

    .

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,19 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
-  constructor() { }
-
-  preventDefault(event) {
-    event.preventDefault()
-  }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
