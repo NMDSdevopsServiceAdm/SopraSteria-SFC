d65e415f781d937afee9ae36607b8c0d8f21767e
Charlie Francis <charliefrancis@gmail.com>
2019-03-05 11:23:39 +0000

    292: Removing Staff Question from Establishment flow

--- src/app/features/confirm-leavers/confirm-leavers.component.ts
+++ src/app/features/confirm-leavers/confirm-leavers.component.ts
@@ -1,37 +1,39 @@
-import { Component, OnInit, OnDestroy } from '@angular/core'
-import { Router } from "@angular/router"
-import { map } from 'rxjs/operators'
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
-import { EstablishmentService } from "../../core/services/establishment.service"
+import { EstablishmentService } from '../../core/services/establishment.service';
 
 @Component({
   selector: 'app-confirm-leavers',
   templateUrl: './confirm-leavers.component.html',
-  styleUrls: ['./confirm-leavers.component.scss']
+  styleUrls: ['./confirm-leavers.component.scss'],
 })
 export class ConfirmLeaversComponent implements OnInit, OnDestroy {
-  private subscriptions = []
+  private subscriptions = [];
 
-  total: number
+  total: number;
 
-  constructor(private router: Router, private establishmentService: EstablishmentService) { }
-
-  makeChangeHandler() {
-    this.router.navigate(["/leavers"])
-  }
-
-  submitHandler() {
-    this.router.navigate(["/staff"])
-  }
+  constructor(private router: Router, private establishmentService: EstablishmentService) {}
 
   ngOnInit() {
     this.subscriptions.push(
-      this.establishmentService.getJobs()
+      this.establishmentService
+        .getJobs()
         .pipe(map(jobs => jobs.TotalLeavers))
-        .subscribe(total => this.total = total))
+        .subscribe(total => (this.total = total))
+    );
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach(s => s.unsubscribe())
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
+
+  makeChangeHandler() {
+    this.router.navigate(['/leavers']);
+  }
+
+  submitHandler() {
+    this.router.navigate(['/dashboard']);
   }
 }
