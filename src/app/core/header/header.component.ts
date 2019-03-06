fc647c61325abed19e89d336596c57885b7fc3ca
Charlie Francis <charliefrancis@gmail.com>
2019-03-06 11:05:35 +0000

    Updating Header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../services/auth-service';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@core/services/auth-service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public fullname: string;
 
   constructor(private authService: AuthService) {}
 
-  isLoggedIn() {
-    return this.authService.isLoggedIn;
+  ngOnInit() {
+    this.fullname = this.authService.fullname;
   }
 
-  hasFullname() {
-    return this.fullname = this.authService.fullname;
+  isLoggedIn() {
+    return this.authService.isLoggedIn;
   }
 
   signOut(event) {
