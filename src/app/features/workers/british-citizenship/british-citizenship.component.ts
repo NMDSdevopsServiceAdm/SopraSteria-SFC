4f956a285817890b48b8c6de3f6f423f0e775609
Charlie Francis <charliefrancis@gmail.com>
2019-03-13 13:39:34 +0000

    195: British Citizenship only sending relevant properties on update

--- src/app/features/workers/british-citizenship/british-citizenship.component.ts
+++ src/app/features/workers/british-citizenship/british-citizenship.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Worker } from '@core/model/worker.model';
 import { MessageService } from '@core/services/message.service';
 import { WorkerEditResponse, WorkerService } from '@core/services/worker.service';
 import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-british-citizenship',
@@ -18,7 +19,6 @@ export class BritishCitizenshipComponent implements OnInit, OnDestroy {
 
   constructor(
     private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
     private router: Router,
     private workerService: WorkerService,
     private messageService: MessageService
@@ -27,21 +27,23 @@ export class BritishCitizenshipComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.worker = this.route.parent.snapshot.data.worker;
-
-    if (this.worker.nationality && this.worker.nationality.value === 'British') {
-      this.router.navigate(['/worker', this.worker.uid, 'nationality'], { replaceUrl: true });
-    }
-
     this.form = this.formBuilder.group({
       citizenship: null,
     });
 
-    if (this.worker.britishCitizenship) {
-      this.form.patchValue({
-        citizenship: this.worker.britishCitizenship,
-      });
-    }
+    this.workerService.worker$.pipe(take(1)).subscribe(worker => {
+      this.worker = worker;
+
+      if (this.worker.nationality && this.worker.nationality.value === 'British') {
+        this.router.navigate(['/worker', this.worker.uid, 'nationality'], { replaceUrl: true });
+      }
+
+      if (this.worker.britishCitizenship) {
+        this.form.patchValue({
+          citizenship: this.worker.britishCitizenship,
+        });
+      }
+    });
   }
 
   ngOnDestroy() {
@@ -64,8 +66,16 @@ export class BritishCitizenshipComponent implements OnInit, OnDestroy {
       this.messageService.clearError();
 
       if (this.form.valid) {
-        this.worker.britishCitizenship = citizenship;
-        this.subscriptions.add(this.workerService.setWorker(this.worker).subscribe(resolve, reject));
+        const props = {
+          britishCitizenship: citizenship,
+        };
+
+        this.subscriptions.add(
+          this.workerService.updateWorker(this.worker.uid, props).subscribe(data => {
+            this.workerService.setState({ ...this.worker, ...data });
+            resolve();
+          }, reject)
+        );
       } else {
         this.messageService.show('error', 'Please fill the required fields.');
         reject();
