46a52c0b456c91a1a8d1c399a08fee6d1d4a2e9a
Charlie Francis <charliefrancis@gmail.com>
2019-03-14 10:49:34 +0000

    195: Average Weekly Hours only sending relevant properties on update

--- src/app/features/workers/average-weekly-hours/average-weekly-hours.component.ts
+++ src/app/features/workers/average-weekly-hours/average-weekly-hours.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Contracts } from '@core/constants/contracts.enum';
 import { Worker } from '@core/model/worker.model';
 import { MessageService } from '@core/services/message.service';
 import { WorkerEditResponse, WorkerService } from '@core/services/worker.service';
 import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { isNull } from 'util';
 
 @Component({
   selector: 'app-average-weekly-hours',
@@ -20,44 +22,51 @@ export class AverageWeeklyHoursComponent implements OnInit, OnDestroy {
     private workerService: WorkerService,
     private messageService: MessageService,
     private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
     private router: Router
   ) {
     this.saveHandler = this.saveHandler.bind(this);
+    this.hoursRequiredValidator = this.hoursRequiredValidator.bind(this);
   }
 
   ngOnInit() {
-    this.worker = this.route.parent.snapshot.data.worker;
-
-    if (
-      !(
-        this.worker.zeroHoursContract === 'Yes' ||
-        [Contracts.Agency, Contracts.Pool_Bank, Contracts.Other].includes(this.worker.contract)
-      )
-    ) {
-      this.router.navigate(['/worker', this.worker.uid, 'weekly-contracted-hours'], { replaceUrl: true });
-    }
-
     this.form = this.formBuilder.group({
       weeklyHoursAverageKnown: null,
-      weeklyHoursAverage: [null, [Validators.min(0), Validators.max(65)]],
+      weeklyHoursAverage: [null, [Validators.min(0), Validators.max(65), this.hoursRequiredValidator]],
     });
 
-    if (this.worker.weeklyHoursAverage) {
-      this.form.patchValue({
-        weeklyHoursAverageKnown: this.worker.weeklyHoursAverage.value,
-        weeklyHoursAverage: this.worker.weeklyHoursAverage.hours,
-      });
-    }
+    this.workerService.worker$.pipe(take(1)).subscribe(worker => {
+      this.worker = worker;
+
+      if (
+        !(
+          this.worker.zeroHoursContract === 'Yes' ||
+          [Contracts.Agency, Contracts.Pool_Bank, Contracts.Other].includes(this.worker.contract)
+        )
+      ) {
+        this.router.navigate(['/worker', this.worker.uid, 'weekly-contracted-hours'], { replaceUrl: true });
+      }
+
+      if (this.worker.weeklyHoursAverage) {
+        this.form.patchValue({
+          weeklyHoursAverageKnown: this.worker.weeklyHoursAverage.value,
+          weeklyHoursAverage: !isNull(this.worker.weeklyHoursAverage.hours)
+            ? this.worker.weeklyHoursAverage.hours
+            : null,
+        });
+      }
+
+      this.subscriptions.add(
+        this.form.controls.weeklyHoursAverageKnown.valueChanges.subscribe(() => {
+          this.form.controls.weeklyHoursAverage.reset();
+          this.form.controls.weeklyHoursAverage.updateValueAndValidity();
+        })
+      );
+    });
   }
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
     this.messageService.clearAll();
-  }
-
-  weeklyHoursAverageKnownChangeHandler() {
-    this.form.controls['weeklyHoursAverage'].reset();
   }
 
   async submitHandler() {
@@ -76,13 +85,25 @@ export class AverageWeeklyHoursComponent implements OnInit, OnDestroy {
       this.messageService.clearError();
 
       if (this.form.valid) {
-        const worker = this.worker || ({} as Worker);
-        worker.weeklyHoursAverage = weeklyHoursAverageKnown.value
-          ? { value: weeklyHoursAverageKnown.value, hours: weeklyHoursAverage.value }
-          : null;
+        const props = {
+          weeklyHoursAverage: {
+            value: weeklyHoursAverageKnown.value,
+            ...(weeklyHoursAverage.value && {
+              hours: weeklyHoursAverage.value,
+            }),
+          },
+        };
 
-        this.subscriptions.add(this.workerService.setWorker(worker).subscribe(resolve, reject));
+        this.subscriptions.add(
+          this.workerService.updateWorker(this.worker.uid, props).subscribe(data => {
+            this.workerService.setState({ ...this.worker, ...data });
+            resolve();
+          }, reject)
+        );
       } else {
+        if (weeklyHoursAverage.errors.required) {
+          this.messageService.show('error', 'Average weekly hours is required');
+        }
         if (weeklyHoursAverage.errors.min || weeklyHoursAverage.errors.max) {
           this.messageService.show('error', 'Average weekly hours must be between 0 and 65.');
         }
@@ -90,5 +111,17 @@ export class AverageWeeklyHoursComponent implements OnInit, OnDestroy {
         reject();
       }
     });
+  }
+
+  hoursRequiredValidator() {
+    if (this.form) {
+      const { weeklyHoursAverageKnown, weeklyHoursAverage } = this.form.value;
+
+      if (weeklyHoursAverageKnown === 'Yes' && isNull(weeklyHoursAverage)) {
+        return { required: true };
+      }
+    }
+
+    return null;
   }
 }
