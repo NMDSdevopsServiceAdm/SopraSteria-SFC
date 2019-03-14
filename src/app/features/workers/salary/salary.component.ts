afc3b85544faf6e225fd0ad9366772ca870d412a
Charlie Francis <charliefrancis@gmail.com>
2019-03-14 11:31:54 +0000

    195: Salary only sending relevant properties on update Using constants to set min/max values in Salary component

--- src/app/features/workers/salary/salary.component.ts
+++ src/app/features/workers/salary/salary.component.ts
@@ -1,19 +1,24 @@
+import { DecimalPipe } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Contracts } from '@core/constants/contracts.enum';
 import { Worker } from '@core/model/worker.model';
 import { MessageService } from '@core/services/message.service';
 import { WorkerEditResponse, WorkerService } from '@core/services/worker.service';
 import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-salary',
   templateUrl: './salary.component.html',
+  providers: [DecimalPipe],
 })
 export class SalaryComponent implements OnInit, OnDestroy {
   public backLink: string;
   public form: FormGroup;
+  public hourly = { min: 2.5, max: 200 };
+  public annually = { min: 500, max: 200000 };
   private worker: Worker;
   private subscriptions: Subscription = new Subscription();
 
@@ -21,56 +26,56 @@ export class SalaryComponent implements OnInit, OnDestroy {
     private workerService: WorkerService,
     private messageService: MessageService,
     private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private decimalPipe: DecimalPipe
   ) {
     this.saveHandler = this.saveHandler.bind(this);
   }
 
   ngOnInit() {
-    this.worker = this.route.parent.snapshot.data.worker;
-
-    this.backLink =
-      this.worker.zeroHoursContract === 'Yes' ||
-      [Contracts.Agency, Contracts.Pool_Bank, Contracts.Other].includes(this.worker.contract)
-        ? 'average-weekly-hours'
-        : 'weekly-contracted-hours';
-
     this.form = this.formBuilder.group({
       terms: null,
       rate: null,
     });
 
-    this.form.get('terms').valueChanges.subscribe(val => {
-      const rate = this.form.get('rate');
-      rate.clearValidators();
-      rate.reset();
+    this.workerService.worker$.pipe(take(1)).subscribe(worker => {
+      this.worker = worker;
+      this.backLink =
+        this.worker.zeroHoursContract === 'Yes' ||
+        [Contracts.Agency, Contracts.Pool_Bank, Contracts.Other].includes(this.worker.contract)
+          ? 'average-weekly-hours'
+          : 'weekly-contracted-hours';
 
-      if (val === 'Hourly') {
-        rate.setValidators([Validators.min(2.5), Validators.max(200), Validators.required]);
-      } else if (val === 'Annually') {
-        rate.setValidators([Validators.min(500), Validators.max(200000), Validators.required]);
+      if (this.worker.annualHourlyPay) {
+        this.form.patchValue({
+          terms: this.worker.annualHourlyPay.value,
+          rate:
+            this.worker.annualHourlyPay.value === 'Annually'
+              ? this.worker.annualHourlyPay.rate.toFixed(0)
+              : this.worker.annualHourlyPay.rate.toFixed(2),
+        });
       }
-
-      rate.updateValueAndValidity();
     });
 
-    if (this.worker.annualHourlyPay) {
-      let rate = '';
-      switch (this.worker.annualHourlyPay.value) {
-        case 'Annually':
-          rate = this.worker.annualHourlyPay.rate.toFixed(0);
-          break;
-        case 'Hourly':
-          rate = this.worker.annualHourlyPay.rate.toFixed(2);
-          break;
-      }
+    this.subscriptions.add(
+      this.form.controls.terms.valueChanges.subscribe(val => {
+        const rate = this.form.controls.rate;
+        rate.clearValidators();
+        rate.reset();
 
-      this.form.patchValue({
-        terms: this.worker.annualHourlyPay.value,
-        rate: rate,
-      });
-    }
+        if (val === 'Hourly') {
+          rate.setValidators([Validators.min(this.hourly.min), Validators.max(this.hourly.max), Validators.required]);
+        } else if (val === 'Annually') {
+          rate.setValidators([
+            Validators.min(this.annually.min),
+            Validators.max(this.annually.max),
+            Validators.required,
+          ]);
+        }
+
+        rate.updateValueAndValidity();
+      })
+    );
   }
 
   ngOnDestroy() {
@@ -97,18 +102,40 @@ export class SalaryComponent implements OnInit, OnDestroy {
         const worker = this.worker || ({} as Worker);
         worker.annualHourlyPay = terms.value ? { value: terms.value, rate: rate.value } : null;
 
-        this.subscriptions.add(this.workerService.setWorker(worker).subscribe(resolve, reject));
+        const props = {
+          annualHourlyPay: terms.value
+            ? {
+                value: terms.value,
+                rate: rate.value,
+              }
+            : null,
+        };
+
+        this.subscriptions.add(
+          this.workerService.updateWorker(this.worker.uid, props).subscribe(data => {
+            this.workerService.setState({ ...this.worker, ...data });
+            resolve();
+          }, reject)
+        );
       } else {
-        // TODO - ideally, the min and max values should be constants
-        // and shared equally between onNgInit when creating the form control
-        // and here within onSubmit.
-        // Look to improving when implementing cross-validation checks.
         if (terms.value === 'Hourly' && rate.errors) {
-          this.messageService.show('error', 'Hourly rate must be between £2.50 and £200.');
+          this.messageService.show(
+            'error',
+            `Hourly rate must be between £${this.decimalPipe.transform(
+              this.hourly.min,
+              '1.2-2'
+            )} and £${this.decimalPipe.transform(this.hourly.max, '1.2-2')}.`
+          );
         }
 
         if (terms.value === 'Annually' && rate.errors) {
-          this.messageService.show('error', 'Annual salary must be between £500 and £200,000');
+          this.messageService.show(
+            'error',
+            `Annual salary must be between £${this.decimalPipe.transform(
+              this.annually.min,
+              '1.0-0'
+            )} and £${this.decimalPipe.transform(this.annually.max, '1.0-0')}.`
+          );
         }
 
         reject();
