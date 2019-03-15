acd9a0c29d04c12e33ba60bcfda9ce97a918f0cd
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-03-15 15:49:42 +0000

    Controlling sequelize output; default is quiet.

--- server/models/index.js
+++ server/models/index.js
@@ -19,6 +19,7 @@ config.dialect = appConfig.get('db.dialect');
 config.dialectOptions = {
   ssl: appConfig.get('db.ssl')
 };
+config.logging = appConfig.get('log.sequelize');
 
 // setup connection pool
 config.pool = {
