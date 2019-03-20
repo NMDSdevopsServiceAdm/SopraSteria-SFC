342a35237a05b4efaed2b9910d0dcdbbb40413e5
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-03-20 10:39:13 +0000

    Adding Worker Leave Reason reference (lookup) endpoint

---
+++ server/models/workerReasons.js
@@ -0,0 +1,30 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  const WorkerLeaveReasons =  sequelize.define('workerLeaveReasons', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: false,
+      field: '"ID"'
+    },
+    seq: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'Seq'
+    },
+    reason: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      field: '"Reason"'
+    }
+  }, {
+    tableName: 'WorkerLeaveReasons',
+    schema: 'cqc',
+    createdAt: false,
+    updatedAt: false
+  });
+
+  return WorkerLeaveReasons;
+};
---
+++ server/routes/workerReason.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+const models = require('../models/index');
+
+/* GET ALL  qualifications*/
+router.route('/').get(async function (req, res) {
+  try {
+    let results = await models.workerLeaveReasons.findAll({
+        order: [
+          ["seq", "ASC"]
+        ]
+      });
+
+    res.send({
+      reasons: reasonsJSON(results)
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(503).send();
+  }    
+});
+
+function reasonsJSON(givenReasons){
+  let reasons=[];
+
+  givenReasons.forEach(thisReason => {
+    reasons.push({
+      id: thisReason.id,
+      reason: thisReason.reason
+    });
+  });
+
+  return reasons;
+};
+
+module.exports = router;
