9d759715a553f3d5fdf094fd05b38d1f111f3513
Charlie Francis <charliefrancis@gmail.com>
2019-03-26 15:20:44 +0000

    Removing HttpErrorHandler from Services Removing debouceTime from Services

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,75 +1,40 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, empty, Observable } from 'rxjs';
-import { catchError, debounceTime, map, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+
 import { EstablishmentService } from './establishment.service';
-import { HttpErrorHandler } from './http-error-handler.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   private _userDetails$: BehaviorSubject<string> = new BehaviorSubject<string>(null);
   public userDetails$: Observable<string> = this._userDetails$.asObservable();
 
-  constructor(
-    private http: HttpClient,
-    private httpErrorHandler: HttpErrorHandler,
-    private establishmentService: EstablishmentService
-  ) { }
+  constructor(private http: HttpClient, private establishmentService: EstablishmentService) {}
 
   /*
    * GET /api/user/establishment/:establishmentId
    */
   getUsernameFromEstbId() {
-
-    return this.http
-      .get<any>(
-        `/api/user/establishment/${this.establishmentService.establishmentId}`,
-        EstablishmentService.getOptions()
-      )
-      .pipe(
-        debounceTime(500),
-        catchError(this.httpErrorHandler.handleHttpError)
-      );
+    return this.http.get<any>(`/api/user/establishment/${this.establishmentService.establishmentId}`);
   }
 
   /*
    * GET /api/user/establishment/:establishmentId/:username
    */
   getUserDetails(username) {
-
-    return this.http
-      .get<any>(
-        `/api/user/establishment/${this.establishmentService.establishmentId}/${username}`,
-        EstablishmentService.getOptions()
-      )
-      .pipe(
-        debounceTime(500),
-        catchError(this.httpErrorHandler.handleHttpError)
-      );
+    return this.http.get<any>(`/api/user/establishment/${this.establishmentService.establishmentId}/${username}`);
   }
 
   /*
    * PUT /api/user/establishment/:establishmentId/:username
    */
   updateUserDetails(username, data) {
-
-    return this.http
-      .put<any>(
-        `/api/user/establishment/${this.establishmentService.establishmentId}/${username}`,
-        data,
-        EstablishmentService.getOptions()
-      )
-      .pipe(
-        debounceTime(500),
-        catchError(this.httpErrorHandler.handleHttpError)
-      );
+    return this.http.put<any>(`/api/user/establishment/${this.establishmentService.establishmentId}/${username}`, data);
   }
 
   updateState(data) {
     this._userDetails$.next(data);
   }
-
 }
