1493db251b7dbfdc2a81bd6bf3f5626d436c325a
Charlie Francis <charliefrancis@gmail.com>
2019-03-26 12:10:40 +0000

    Character Count can now count words

--- src/app/shared/components/character-count/character-count.component.ts
+++ src/app/shared/components/character-count/character-count.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-character-count',
@@ -12,15 +13,18 @@ export class CharacterCountComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
   @Input() control: FormControl;
   @Input() max: number;
-
-  constructor() {}
+  @Input() words: boolean = false;
 
   ngOnInit() {
     this.remaining = this.max;
 
     this.subscriptions.add(
-      this.control.valueChanges.subscribe(val => {
-        this.remaining = this.max - val.length;
+      this.control.valueChanges.pipe(debounceTime(200)).subscribe((value: string) => {
+        if (this.words) {
+          this.remaining = this.max - value.split(' ').filter((v: string) => !!v).length;
+        } else {
+          this.remaining = this.max - value.length;
+        }
       })
     );
   }
