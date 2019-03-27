7cfa30ac18b0e57e18047a7293cfa7eec4d6f2bd
Charlie Francis <charliefrancis@gmail.com>
2019-03-27 18:11:04 +0000

    Displaying Alerts to update workplace and staff records

--- src/app/features/dashboard/home-tab/home-tab.component.ts
+++ src/app/features/dashboard/home-tab/home-tab.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { WorkerService } from '@core/services/worker.service';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-tab',
@@ -6,7 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-tab.component.scss'],
 })
 export class HomeTabComponent implements OnInit {
-  constructor() {}
+  public updateWorkplace: boolean;
+  public updateStaffRecords: boolean;
+  private subscriptions: Subscription = new Subscription();
 
-  ngOnInit() {}
+  constructor(private establishmentService: EstablishmentService, private workerService: WorkerService) {}
+
+  ngOnInit() {
+    this.subscriptions.add(
+      this.workerService
+        .getAllWorkers()
+        .pipe(take(1))
+        .subscribe(workers => {
+          this.updateStaffRecords = workers.length < 0;
+        })
+    );
+
+    this.subscriptions.add(
+      this.establishmentService
+        .getEmployerType()
+        .pipe(take(1))
+        .subscribe(d => {
+          this.updateWorkplace = !d.employerType;
+        })
+    );
+  }
 }
