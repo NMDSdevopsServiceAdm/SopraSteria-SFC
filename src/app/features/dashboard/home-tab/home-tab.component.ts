1353d8d677be4436605202a944182b191e65c312
Charlie Francis <charliefrancis@gmail.com>
2019-03-27 14:25:47 +0000

    Adding Home Tab Component for Dashboard

---
+++ src/app/features/dashboard/home-tab/home-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-tab',
+  templateUrl: './home-tab.component.html',
+  styleUrls: ['./home-tab.component.scss'],
+})
+export class HomeTabComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
