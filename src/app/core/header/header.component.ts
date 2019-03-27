d948ade61d7c14e5f15bcefc5fbd9ad955384281
Charlie Francis <charliefrancis@gmail.com>
2019-03-27 15:06:55 +0000

    Adding Alerts for actions on Home Tab

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { WorkerService } from '@core/services/worker.service';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { AuthService } from '../services/auth-service';
 
@@ -6,10 +10,32 @@ import { AuthService } from '../services/auth-service';
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   public fullname: string;
+  public updateWorkplace: boolean;
+  public updateStaffRecords: boolean;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private establishmentService: EstablishmentService,
+    private workerService: WorkerService
+  ) {}
+
+  ngOnInit() {
+    this.subscriptions.add(
+      this.workerService
+        .getAllWorkers()
+        .pipe(take(1))
+        .subscribe(workers => {
+          this.updateStaffRecords = workers.length > 0;
+        })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 
   isLoggedIn() {
     return this.authService.isLoggedIn;
