fb63768b227b3d2634e3adafaf9438722c5a8b50
Charlie Francis <charliefrancis@gmail.com>
2019-04-03 09:19:22 +0100

    Updating Button CSS

--- src/app/shared/components/start-button/start-button.component.ts
+++ src/app/shared/components/start-button/start-button.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-start-button',
   templateUrl: './start-button.component.html',
-  styleUrls: ['./start-button.component.scss'],
 })
 export class StartButtonComponent {
   @Input() link: string;
