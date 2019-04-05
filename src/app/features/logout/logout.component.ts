24e0aa80cd2fca2acdca80846702db8c872c65fe
Charlie Francis <charliefrancis@gmail.com>
2019-04-05 13:28:16 +0100

    Logging out on HTTP 403

--- src/app/features/logout/logout.component.ts
+++ src/app/features/logout/logout.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '@core/services/auth-service';
+import { IdleService } from '@core/services/idle.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
 })
 export class LogoutComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private idleService: IdleService, private authService: AuthService) {
+    this.authService.logoutWithoutRouting();
+    this.idleService.clear();
+  }
 
   login() {
     this.router.navigate(['/login']);
