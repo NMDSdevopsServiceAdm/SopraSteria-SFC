60f465090090a21b4d72f4a36167d9a3949ce0c3
Omar Mirza <omar.creative.dev@gmail.com>
2019-04-09 11:32:10 +0100

    updates to forgot-~your-password, login, reset-password, logout screens & fixed logout link

--- src/app/features/forgot-your-password/edit/edit.component.ts
+++ src/app/features/forgot-your-password/edit/edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RequestPasswordResetResponse } from '@core/services/password-reset.service';
+import { ErrorDetails } from '@core/model/errorSummary.model';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
 
 @Component({
   selector: 'app-fp-edit',
@@ -9,15 +11,45 @@ import { RequestPasswordResetResponse } from '@core/services/password-reset.serv
 export class ForgotYourPasswordEditComponent implements OnInit {
   public form: FormGroup;
   public submitted = false;
+  public errorDetails: Array<ErrorDetails>;
 
   @Output() formDataOutput = new EventEmitter<RequestPasswordResetResponse>();
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private errorSummaryService: ErrorSummaryService
+  ) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       usernameOrEmail: ['', [Validators.required, Validators.maxLength(120)]],
     });
+
+    this.setupErrorDetails();
+  }
+
+  public setupErrorDetails(): void {
+    this.errorDetails = [
+      {
+        item: 'usernameOrEmail',
+        type: [
+          {
+            name: 'required',
+            message: 'Please enter your username or email address.',
+          }
+        ]
+      }
+    ];
+  }
+
+  /**
+   * Pass in formGroup or formControl name and errorType
+   * Then return error message
+   * @param item
+   * @param errorType
+   */
+  public getErrorMessage(item: string, errorType: string): string {
+    return this.errorSummaryService.getErrorMessage(item, errorType, this.errorDetails);
   }
 
   onSubmit() {
