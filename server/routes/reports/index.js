f7d6456695d8807bee0e8d415a675df900cc37f8
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-04-10 11:41:07 +0100

    Introducing the daily snapshot report API endpoint.

--- server/routes/reports/index.js
+++ server/routes/reports/index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const WDF = require('./wdf');
+const DailySnapshot = require('./dailySnapshot');
 
-// ensure all establishment routes are authorised
 router.use('/wdf', WDF);
+router.use('/dailySnapshot', DailySnapshot);
 
 router.route('/').get(async (req, res) => {
     return res.status(501).send();
