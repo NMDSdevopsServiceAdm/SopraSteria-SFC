9b15ceb223b0438fd35f4e8aeb19a9f971717a6a
Charlie Francis <charliefrancis@gmail.com>
2019-04-12 14:10:19 +0100

    Updating Google Analytics to track pageviews

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
+import 'core-js';
+
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import 'core-js';
-import { filter } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
+
+declare let gtag: Function;
+
+function getWindow(): any {
+  return window;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,13 +17,27 @@ import { filter } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
   title = 'ng-sfc-v2';
+  gaid: string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    if (getWindow().gaid) {
+      this.gaid = getWindow().gaid;
+    }
+  }
 
   ngOnInit() {
-    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
-      window.scrollTo(0, 0);
-    });
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(event => event as NavigationEnd)
+      )
+      .subscribe(event => {
+        console.log(this.gaid, { page_path: event.urlAfterRedirects });
+        gtag('config', this.gaid, {
+          page_path: event.urlAfterRedirects,
+        });
+        window.scrollTo(0, 0);
+      });
 
     if (localStorage.getItem('auth-token')) {
       localStorage.clear();
