506c56ea10fe866a548000509614908f3c5502db
Omar Mirza <omar.creative.dev@gmail.com>
2019-04-16 09:39:33 +0100

    commit WIP

--- src/app/features/select-workplace/select-workplace.component.ts
+++ src/app/features/select-workplace/select-workplace.component.ts
@@ -1,10 +1,7 @@
-
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-
 import { RegistrationService } from '../../core/services/registration.service';
 import { RegistrationModel } from '../../core/model/registration.model';
 import { RegistrationTrackerError } from './../../core/model/registrationTrackerError.model';
@@ -12,53 +9,36 @@ import { RegistrationTrackerError } from './../../core/model/registrationTracker
 @Component({
   selector: 'app-select-workplace',
   templateUrl: './select-workplace.component.html',
-  styleUrls: ['./select-workplace.component.scss']
 })
 export class SelectWorkplaceComponent implements OnInit, OnDestroy {
-  selectWorkplaceForm: FormGroup;
-  registration: RegistrationModel;
+  public form: FormGroup;
+  private registration: RegistrationModel;
   selectedAddressId: string;
   addressPostcode: string;
   mainService: string;
-
-  cqcPostcodeApiError: string;
-  cqclocationApiError: string;
-  nonCqcPostcodeApiError: string;
-
-  currentSection: number;
-  lastSection: number;
-  backLink: string;
-  secondItem: number;
-
-  isSubmitted = false;
+  submitted = false;
 
   private subscriptions: Subscription = new Subscription();
 
-  constructor(
-    private _registrationService: RegistrationService,
-    private router: Router,
-    private fb: FormBuilder
-  ) { }
+  constructor(private registrationService: RegistrationService, private router: Router, private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.selectWorkplaceForm = this.fb.group({
-      selectWorkplaceSelected: ['', Validators.required]
+    this.form = this.fb.group({
+      workplace: ['', Validators.required],
     });
 
-    // Watch selectWorkplaceSelected
+    // Watch workplace
     this.subscriptions.add(
-      this.selectWorkplaceForm.get('selectWorkplaceSelected').valueChanges.subscribe(
-        value => this.selectWorkplaceChanged(value)
-      )
+      this.form
+        .get('workplace')
+        .valueChanges.subscribe(value => this.selectWorkplaceChanged(value))
     );
 
     this.subscriptions.add(
-      this._registrationService.registration$.subscribe(registration => this.registration = registration)
+      this.registrationService.registration$.subscribe(registration => (this.registration = registration))
     );
 
-    this.setSectionNumbers();
-    this.resetPostcodeApi();
-
+    // this.resetPostcodeApi();
   }
 
   resetPostcodeApi() {
@@ -69,108 +49,44 @@ export class SelectWorkplaceComponent implements OnInit, OnDestroy {
     if (count === 1) {
       if (routeArray.length >= 2) {
         this.subscriptions.add(
-          this._registrationService.getLocationByPostCode(postcode).subscribe(res => {
-            this._registrationService.updateState(res);
+          this.registrationService.getLocationByPostCode(postcode).subscribe(res => {
+            this.registrationService.updateState(res);
           })
         );
       }
     }
   }
 
-  clickBack() {
-    if (this.registration.userRoute) {
-
-      this.currentSection = this.registration.userRoute.currentPage;
-      this.currentSection = this.currentSection - 1;
-      this.registration.userRoute.route.splice(-1);
-
-      //this.updateSectionNumbers(this.registration);
-      this.registration['userRoute'] = this.registration.userRoute;
-      this.registration.userRoute['currentPage'] = this.currentSection;
-      //this.registration.userRoute['route'] = this.registration.userRoute['route'];
-      this._registrationService.updateState(this.registration);
-
-      this.router.navigate([this.backLink]);
-    }
-    else {
-      this.updateSectionNumbers(this.registration);
-      this.clickBack();
-    }
-
-  }
-
-  setSectionNumbers() {
-    this.currentSection = this.registration.userRoute.currentPage;
-    this.backLink = this.registration.userRoute.route[this.currentSection - 1];
-    this.secondItem = 1;
-
-    this.currentSection = this.currentSection + 1;
-    this.lastSection = 8;
-  }
-
-  // && (this.currentSection === 2)
-
   selectWorkplaceChanged(value: string): void {
     this.selectedAddressId = this.registration.locationdata[value].locationId;
     this.mainService = this.registration.locationdata[value].mainService;
-
   }
 
   onSubmit() {
-    this.isSubmitted = true;
-
+    this.submitted = true;
 
     if (this.selectedAddressId) {
-
       this.save(this.selectedAddressId);
     }
   }
 
   save(selectedAddressId) {
-
     this.subscriptions.add(
-      this._registrationService.getLocationByLocationId(selectedAddressId).subscribe(
+      this.registrationService.getLocationByLocationId(selectedAddressId).subscribe(
         (data: RegistrationModel) => {
           if (data.success === 1) {
-
-            this.updateSectionNumbers(data);
-
-            this._registrationService.updateState(data);
-
+            this.registrationService.updateState(data);
             this.router.navigate(['/select-main-service']);
           }
         },
         (err: RegistrationTrackerError) => {
-          this.cqcPostcodeApiError = err.message;
+
         },
         () => {
           console.log('Get location by postcode complete');
         }
       )
     );
-  }
-
-  updateSectionNumbers(data) {
-    if (this.registration.userRoute) {
-      data['userRoute'] = this.registration.userRoute;
-      data.userRoute['currentPage'] = this.currentSection;
-      data.userRoute['route'] = this.registration.userRoute['route'];
-      data.userRoute['route'].push('/select-workplace');
-    }
-    else {
-
-      data['userRoute'] = {
-        currentPage: this.currentSection,
-        route: [
-          '/registered-question',
-          '/select-workplace'
-        ]
-      };
-
-      this._registrationService.updateState(data);
-
-    }
-
   }
 
   setRegulatedCheckFalse(data) {
@@ -183,18 +99,13 @@ export class SelectWorkplaceComponent implements OnInit, OnDestroy {
     this.addressPostcode = this.registration.locationdata[0].postalCode;
 
     this.subscriptions.add(
-      this._registrationService.getAddressByPostCode(this.addressPostcode).subscribe(
-        (data: RegistrationModel) => {
-          if (data.success === 1) {
-            this.updateSectionNumbers(data);
-            this.setRegulatedCheckFalse(data);
-
-            this._registrationService.updateState(data);
-
-            this.router.navigate(['/select-workplace-address']);
-          }
+      this.registrationService.getAddressByPostCode(this.addressPostcode).subscribe((data: RegistrationModel) => {
+        if (data.success === 1) {
+          this.setRegulatedCheckFalse(data);
+          this.registrationService.updateState(data);
+          this.router.navigate(['/select-workplace-address']);
         }
-      )
+      })
     );
   }
 
