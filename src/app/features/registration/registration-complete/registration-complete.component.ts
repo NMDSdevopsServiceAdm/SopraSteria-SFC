fd789559313a694660826cc2c31d64d0ee713ca4
Omar Mirza <omar.creative.dev@gmail.com>
2019-04-24 10:14:54 +0100

    update registration complete copy and remove redundant code

--- src/app/features/registration/registration-complete/registration-complete.component.ts
+++ src/app/features/registration/registration-complete/registration-complete.component.ts
@@ -1,28 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { RegistrationService } from '@core/services/registration.service';
-import { LocationAddress } from '@core/model/location-address.model';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registration-complete',
   templateUrl: './registration-complete.component.html'
 })
-export class RegistrationCompleteComponent implements OnInit, OnDestroy {
-  private isRegulated: boolean;
-  private subscriptions: Subscription = new Subscription();
-
-  constructor(private registrationService: RegistrationService) {}
-
-  ngOnInit() {
-    this.subscriptions.add(
-      this.registrationService.selectedLocationAddress$.subscribe(
-        (locationAddress: LocationAddress) => this.isRegulated = locationAddress.isRegulated || false
-      )
-    );
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe();
-  }
-
-}
+export class RegistrationCompleteComponent {}
