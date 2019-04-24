dfc95ed784d9033891bd83993ce7fea7fedb5d7c
Charlie Francis <charliefrancis@gmail.com>
2019-04-24 16:19:21 +0100

    .

--- src/app/shared/components/total-staff-panel/total-staff-panel.component.ts
+++ src/app/shared/components/total-staff-panel/total-staff-panel.component.ts
@@ -1,22 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-total-staff-panel',
   templateUrl: './total-staff-panel.component.html',
   styleUrls: ['./total-staff-panel.component.scss'],
 })
-export class TotalStaffPanelComponent implements OnInit {
+export class TotalStaffPanelComponent {
   @Input() totalStaff = 0;
   @Input() totalWorkers = 0;
 
   constructor() {}
-
-  ngOnInit() {
-    // const combined$ = combineLatest(this.establishmentService.getStaff(), this.workerService.getAllWorkers());
-    // this.subscriptions.add(
-    //   combined$.pipe(map(results => ({ staff: results[0], workers: results[1].length }))).subscribe(total => {
-    //     this.total = total;
-    //   })
-    // );
-  }
 }
