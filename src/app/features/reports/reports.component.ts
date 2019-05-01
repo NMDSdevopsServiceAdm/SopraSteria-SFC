bbad0acdb5bddca84eef864c1b7f1f251c8a5900
Omar Mirza <omar.creative.dev@gmail.com>
2019-05-01 18:54:11 +0100

    remove unused code, scss files, cleanup reports markup

--- src/app/features/reports/reports.component.ts
+++ src/app/features/reports/reports.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { DATE_DISPLAY_DEFAULT } from '@core/constants/constants';
 import { AuthService } from '@core/services/auth.service';
 import { ReportsService } from '@core/services/reports.service';
 import * as moment from 'moment';
@@ -8,15 +7,12 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-reports',
-  templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss'],
+  templateUrl: './reports.component.html'
 })
 export class ReportsComponent implements OnInit, OnDestroy {
   public updateEligibilityForm: FormGroup;
   public establishment: any;
   public reportDetails: {};
-  public lastLoggedIn = null;
-  public dateFormat = DATE_DISPLAY_DEFAULT;
   public displayWDFReport: {};
   public eligibility: {};
   public newDate: string;
