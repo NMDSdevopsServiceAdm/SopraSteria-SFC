b32435f1bf1ecd42e3c04ce807209115a579a0bc
Charlie Francis <charliefrancis@gmail.com>
2019-05-01 17:07:14 +0100

    Updated Page styles

--- src/app/features/workers/create-basic-records/create-basic-records.component.ts
+++ src/app/features/workers/create-basic-records/create-basic-records.component.ts
@@ -7,18 +7,17 @@ import { Worker } from '@core/model/worker.model';
 import { BackService } from '@core/services/back.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { JobService } from '@core/services/job.service';
-import { WorkerEditResponse, WorkerService } from '@core/services/worker.service';
+import { WorkerService } from '@core/services/worker.service';
 import { Subscription } from 'rxjs';
 import { isNull } from 'util';
 
 @Component({
   selector: 'app-create-basic-records',
   templateUrl: './create-basic-records.component.html',
-  styleUrls: ['./create-basic-records.component.scss'],
 })
 export class CreateBasicRecordsComponent implements OnInit, OnDestroy {
-  public contractsAvailable: Array<string> = [];
-  public jobsAvailable: Job[] = [];
+  public contracts: Array<string> = [];
+  public jobs: Job[] = [];
   public totalStaff: number;
   public totalWorkers = 0;
   public form: FormGroup;
@@ -70,53 +69,34 @@ export class CreateBasicRecordsComponent implements OnInit, OnDestroy {
       })
     );
 
-    this.subscriptions.add(this.jobService.getJobs().subscribe(jobs => (this.jobsAvailable = jobs)));
-    this.contractsAvailable = Object.values(Contracts);
+    this.subscriptions.add(this.jobService.getJobs().subscribe(jobs => (this.jobs = jobs)));
+    this.contracts = Object.values(Contracts);
 
     this.backService.setBackLink({ url: ['/worker/basic-records-start-screen'] });
   }
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
-    this.messageService.clearAll();
   }
 
-  createStaffRecordsItem(): FormGroup {
-    return this.formBuilder.group({
-      nameOrId: [null, Validators.required],
-      mainJob: [null, Validators.required],
-      contract: [null, Validators.required],
-      uid: null,
-      active: true,
-    });
+  public openStaffRecord(event: Event, index: number) {
+    event.preventDefault();
+    this._openStaffRecord(index);
   }
 
-  openStaffRecord(index: number) {
-    this.closeStaffRecords();
-    this.staffRecordsControl.controls[index].patchValue({ active: true });
-    setTimeout(() => {
-      this.elementRef.nativeElement.querySelector(`.staff-record:nth-of-type(${index + 1})`).scrollIntoView(true);
-    });
-  }
-
-  closeStaffRecords() {
-    this.staffRecordsControl.controls.forEach(control => {
-      control.patchValue({ active: false });
-    });
-  }
-
-  addStaffRecord() {
+  public addStaffRecord() {
     this.closeStaffRecords();
     this.staffRecordsControl.push(this.createStaffRecordsItem());
   }
 
-  deleteStaffRecord(index: number) {
+  public deleteStaffRecord(event: Event, index: number) {
+    event.preventDefault();
     const staffRecord = <FormGroup>this.staffRecordsControl.controls[index];
-    const uid = staffRecord.controls.uid.value;
+    const { uid } = staffRecord.value;
 
     if (uid) {
       this.subscriptions.add(
-        this.workerService.deleteWorker(staffRecord.controls.uid.value).subscribe(() => {
+        this.workerService.deleteWorker(uid).subscribe(() => {
           this.staffRecordsControl.controls.splice(index, 1);
         })
       );
@@ -125,8 +105,37 @@ export class CreateBasicRecordsComponent implements OnInit, OnDestroy {
     }
   }
 
+  private createStaffRecordsItem(): FormGroup {
+    return this.formBuilder.group({
+      nameOrId: [null, Validators.required],
+      mainJobRole: [null, Validators.required],
+      contract: [null, Validators.required],
+      uid: null,
+      active: true,
+    });
+  }
+
+  private _openStaffRecord(index: number) {
+    this.closeStaffRecords();
+    this.staffRecordsControl.controls[index].patchValue({ active: true });
+    setTimeout(() => {
+      this.elementRef.nativeElement.querySelector(`.staff-record:nth-of-type(${index + 1})`).scrollIntoView(true);
+    });
+  }
+
+  private closeStaffRecords() {
+    this.staffRecordsControl.controls.forEach(control => {
+      control.patchValue({ active: false });
+    });
+  }
+
   submitHandler() {
+    // TODO: Better validation and accessibility when submitting the form.
     this.submitted = true;
+
+    const active = this.staffRecordsControl.controls.findIndex(group => {
+      return group.get('active').value;
+    });
 
     if (this.form.valid || this.staffRecordsControl.length === 0) {
       this.workerService.setCreateStaffResponse(this.staffRecordsControl.length);
@@ -137,7 +146,7 @@ export class CreateBasicRecordsComponent implements OnInit, OnDestroy {
       });
 
       if (unsavedIndex >= 0) {
-        this.openStaffRecord(unsavedIndex);
+        this._openStaffRecord(unsavedIndex);
         this.saveStaffRecord(unsavedIndex);
       } else {
         this.elementRef.nativeElement.querySelector('form').scrollIntoView(true);
@@ -145,43 +154,46 @@ export class CreateBasicRecordsComponent implements OnInit, OnDestroy {
     }
   }
 
-  async saveStaffRecord(index: number) {
+  public saveStaffRecord(index: number) {
     const staffRecord = <FormGroup>this.staffRecordsControl.controls[index];
 
     Object.keys(staffRecord.controls).forEach(key => {
       staffRecord.get(key).markAsTouched();
     });
 
-    try {
-      if (staffRecord.valid) {
-        await this.saveHandler(staffRecord);
-        this.closeStaffRecords();
-      }
-    } catch (err) {}
-  }
-
-  saveHandler(staffRecord): Promise<WorkerEditResponse> {
-    return new Promise((resolve, reject) => {
-      const { nameOrId, contract, mainJob, uid } = staffRecord.controls;
+    if (staffRecord.valid) {
+      const { nameOrId, contract, mainJobRole, uid } = staffRecord.controls;
 
       const props = {
         nameOrId: nameOrId.value,
         contract: contract.value,
         mainJob: {
-          jobId: parseInt(mainJob.value, 10),
+          jobId: parseInt(mainJobRole.value, 10),
         },
       };
 
       if (!isNull(uid.value)) {
-        this.subscriptions.add(this.workerService.updateWorker(uid.value, props).subscribe(resolve, reject));
+        this.subscriptions.add(
+          this.workerService.updateWorker(uid.value, props).subscribe(
+            () => this.closeStaffRecords(),
+            error => {
+              console.log(error);
+            }
+          )
+        );
       } else {
         this.subscriptions.add(
-          this.workerService.createWorker(props as Worker).subscribe(data => {
-            staffRecord.patchValue({ uid: data.uid });
-            resolve();
-          }, reject)
+          this.workerService.createWorker(props as Worker).subscribe(
+            data => {
+              staffRecord.patchValue({ uid: data.uid });
+              this.closeStaffRecords();
+            },
+            error => {
+              console.log(error);
+            }
+          )
         );
       }
-    });
+    }
   }
 }
