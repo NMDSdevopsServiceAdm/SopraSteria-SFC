883ceb6c3ce558a69df8e8821c32b9bb6faa0dfe
Charlie Francis <charliefrancis@gmail.com>
2019-05-09 11:05:24 +0100

    Remove Close button from Contact Us Page

--- src/app/features/contact-us/contact-us.component.ts
+++ src/app/features/contact-us/contact-us.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { WindowRef } from '@core/services/window.ref';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
 })
 export class ContactUsComponent {
-  constructor(private windowRef: WindowRef) {}
-
-  closeWindow() {
-    this.windowRef.nativeWindow.close();
-  }
+  constructor() {}
 }
