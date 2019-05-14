6734c883a0ca30a1e0c21558992ffd55bd369d91
Charlie Francis <charliefrancis@gmail.com>
2019-05-14 14:41:47 +0100

    Back Link now persists when navigating fragments

--- src/app/core/services/back.service.ts
+++ src/app/core/services/back.service.ts
@@ -1,8 +1,10 @@
+import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { URLStructure } from '@core/model/url.model';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { distinctUntilChanged, filter, map } from 'rxjs/operators';
+import * as parse from 'url-parse';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +13,12 @@ export class BackService {
   private readonly _back$: BehaviorSubject<URLStructure> = new BehaviorSubject<URLStructure>(null);
   public readonly back$: Observable<URLStructure> = this._back$.asObservable();
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private location: Location) {
     this.router.events
       .pipe(
         filter(event => event instanceof NavigationEnd),
+        map(() => parse(this.router.url).pathname),
+        distinctUntilChanged(),
         map(() => this._back$.value),
         filter(val => !!val)
       )
