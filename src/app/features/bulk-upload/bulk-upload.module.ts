098e833087fde75ee3e6b0990114e4dc4f5026d3
Omar Mirza <omar.creative.dev@gmail.com>
2019-05-14 22:44:10 +0100

    commit WIP

--- src/app/features/bulk-upload/bulk-upload.module.ts
+++ src/app/features/bulk-upload/bulk-upload.module.ts
@@ -3,13 +3,14 @@ import { BulkUploadRoutingModule } from '@features/bulk-upload/bulk-upload-routi
 import { CheckWorkplaceReferencesComponent } from './check-workplace-references/check-workplace-references.component';
 import { CommonModule } from '@angular/common';
 import { DownloadDataFilesComponent } from './download-data-files/download-data-files.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '@shared/shared.module';
 import { UploadDataFilesComponent } from './upload-data-files/upload-data-files.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, BulkUploadRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule, BulkUploadRoutingModule],
   declarations: [
     BulkUploadPageComponent,
     CheckWorkplaceReferencesComponent,
--- src/app/features/bulk-upload/file-upload/file-upload.component.ts
+++ src/app/features/bulk-upload/file-upload/file-upload.component.ts
@@ -1,17 +1,47 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-file-upload',
-  templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.scss']
+  templateUrl: './file-upload.component.html'
 })
 export class FileUploadComponent implements OnInit {
-
+  private submitted = false;
+  private form: FormGroup;
   @Input() public multiple?: boolean;
 
-  constructor() {}
+  constructor(
+    private errorSummaryService: ErrorSummaryService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.setupForm();
+  }
+
+  private setupForm(): void {
+    this.form = this.formBuilder.group({
+      fileUpload: [null, Validators.required],
+    });
+  }
+
+  private onFilesSelection($event: Event): void {
+    const target = $event.target || $event.srcElement;
+    const files = target['files'];
+    console.log('files', files);
+  }
+
+  public onSubmit(): void {
+    console.log('onSubmit fired');
+    this.submitted = true;
+    this.errorSummaryService.syncFormErrorsEvent.next(true);
+
+    if (this.form.valid) {
+
+    } else {
+      this.errorSummaryService.scrollToErrorSummary();
+    }
   }
 
 }
