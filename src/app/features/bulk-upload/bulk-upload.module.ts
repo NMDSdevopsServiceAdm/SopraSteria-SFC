9e16a770ea2a6de3287f501362e9eee135e052d6
Omar Mirza <omar.creative.dev@gmail.com>
2019-05-14 14:59:08 +0100

    updted markup and module

--- src/app/features/bulk-upload/bulk-upload.module.ts
+++ src/app/features/bulk-upload/bulk-upload.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BulkUploadPageComponent } from './bulk-upload-page/bulk-upload-page.component';
 import { BulkUploadRoutingModule } from '@features/bulk-upload/bulk-upload-routing.module';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     BulkUploadRoutingModule
   ],
   declarations: [BulkUploadPageComponent]
