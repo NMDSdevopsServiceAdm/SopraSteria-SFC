bedc5d97f34dbc87c0710cda0966abc9b6fe3fd2
Omar Mirza <omar.creative.dev@gmail.com>
2019-05-14 16:21:36 +0100

    updated markup on child components

--- src/app/features/bulk-upload/bulk-upload.module.ts
+++ src/app/features/bulk-upload/bulk-upload.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BulkUploadPageComponent } from './bulk-upload-page/bulk-upload-page.component';
 import { BulkUploadRoutingModule } from '@features/bulk-upload/bulk-upload-routing.module';
-import { SharedModule } from '@shared/shared.module';
 import { CheckWorkplaceReferencesComponent } from './check-workplace-references/check-workplace-references.component';
+import { CommonModule } from '@angular/common';
 import { DownloadDataFilesComponent } from './download-data-files/download-data-files.component';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared/shared.module';
 import { UploadDataFilesComponent } from './upload-data-files/upload-data-files.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SharedModule,
-    BulkUploadRoutingModule
+  imports: [CommonModule, SharedModule, BulkUploadRoutingModule],
+  declarations: [
+    BulkUploadPageComponent,
+    CheckWorkplaceReferencesComponent,
+    DownloadDataFilesComponent,
+    UploadDataFilesComponent,
   ],
-  declarations: [BulkUploadPageComponent, CheckWorkplaceReferencesComponent, DownloadDataFilesComponent, UploadDataFilesComponent]
 })
 export class BulkUploadModule {}
