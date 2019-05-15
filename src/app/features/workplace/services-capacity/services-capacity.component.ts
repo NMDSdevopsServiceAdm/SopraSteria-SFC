cc0b5623d40081b874ee25645a9d73f23b65f781
Charlie Francis <charliefrancis@gmail.com>
2019-05-15 15:12:49 +0100

    Updating Errors for Service Capacities

--- src/app/features/workplace/services-capacity/services-capacity.component.ts
+++ src/app/features/workplace/services-capacity/services-capacity.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BackService } from '@core/services/back.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { EstablishmentService } from '@core/services/establishment.service';
+import { sortBy } from 'lodash';
 
 import { Question } from '../question/question.component';
 
 @Component({
   selector: 'app-services-capacity',
   templateUrl: './services-capacity.component.html',
-  styleUrls: ['./services-capacity.component.scss'],
 })
 export class ServicesCapacityComponent extends Question {
-  capacities: object;
+  public capacities: [];
+  public ready = false;
 
   constructor(
     protected formBuilder: FormBuilder,
@@ -27,26 +28,51 @@ export class ServicesCapacityComponent extends Question {
     this.form = this.formBuilder.group({});
   }
 
+  public generateId(str: string): string {
+    return str.replace(/^[^a-z]+|[^\w:.-]+/gi, '');
+  }
+
   protected init(): void {
     this.subscriptions.add(
       this.establishmentService.getCapacity(this.establishment.id, true).subscribe(capacities => {
-        this.capacities = capacities;
+        this.capacities = capacities.allServiceCapacities;
 
-        console.log(capacities);
+        capacities.allServiceCapacities.forEach((service, i) => {
+          const group = this.formBuilder.group({});
+          const questions = sortBy(service.questions, question => question.seq);
 
-        capacities.allServiceCapacities.forEach(service => {
-          service.questions.forEach(question => {
-            this.form.addControl(question.questionId.toString(), new FormControl(false));
+          const id = this.generateId(service.service);
+
+          questions.forEach(question => {
+            group.addControl(question.questionId.toString(), new FormControl(question.answer, [Validators.min(0)]));
+            this.formErrorsMap.push({
+              item: `${id}.${question.questionId}`,
+              type: [
+                {
+                  name: 'min',
+                  message: 'Answer must be 0 or more',
+                },
+              ],
+            });
           });
+
+          if (Object.keys(group.controls).length > 1) {
+            group.setValidators(this.capacityUtilisationValidator);
+            this.formErrorsMap.push({
+              item: id,
+              type: [
+                {
+                  name: 'overcapacity',
+                  message: 'Utilisation must be lower than Capacity provided',
+                },
+              ],
+            });
+          }
+
+          this.form.addControl(id, group);
         });
 
-        // data.allServiceCapacities.forEach(service => {
-        //   service.questions.forEach(question => {
-        //     let answer = data.capacities ? data.capacities.find(cc => question.questionId === cc.questionId) : null;
-        //     answer = answer ? parseInt(answer.answer, 10) : null;
-        //     this.form.addControl(question.questionId.toString(), new FormControl(answer));
-        //   });
-        // });
+        this.ready = true;
       })
     );
 
@@ -64,14 +90,19 @@ export class ServicesCapacityComponent extends Question {
   }
 
   protected generateUpdateProps() {
-    // const props = Object.entries(this.form.value).reduce((res, [key, value]) => {
-    //   if (value) {
-    //     res.push({ questionId: parseFloat(key), answer: value });
-    //   }
-    //   return res;
-    // }, []);
+    const capacities = [];
+    Object.keys(this.form.controls).map(groupKey => {
+      Object.entries(this.form.get(groupKey).value).reduce((res, [key, value]) => {
+        if (value) {
+          capacities.push({ questionId: parseInt(key, 10), answer: value });
+        }
+        return res;
+      }, []);
+    });
 
-    return null;
+    return {
+      capacities,
+    };
   }
 
   protected updateEstablishment(props): void {
@@ -80,5 +111,18 @@ export class ServicesCapacityComponent extends Question {
         .updateCapacity(this.establishment.id, props)
         .subscribe(data => this._onSuccess(data), error => this.onError(error))
     );
+  }
+
+  protected capacityUtilisationValidator(group: FormGroup): ValidationErrors {
+    const controls = [];
+    Object.keys(group.controls).forEach(key => {
+      controls.push(group.get(key));
+    });
+
+    if (controls[1] && controls[1].value > controls[0].value) {
+      return { overcapacity: { max: controls[0].value, actual: controls[1].value } };
+    }
+
+    return null;
   }
 }
