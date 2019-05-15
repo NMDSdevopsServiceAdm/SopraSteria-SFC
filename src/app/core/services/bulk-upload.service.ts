aebfd23508cd4dfc02e71e414263d9db30061ae2
Omar Mirza <omar.creative.dev@gmail.com>
2019-05-15 16:52:54 +0100

    commit wip

--- src/app/core/services/bulk-upload.service.ts
+++ src/app/core/services/bulk-upload.service.ts
@@ -1,9 +1,23 @@
+import { BehaviorSubject, Observable } from 'rxjs';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BulkUploadService {
+  constructor(private http: HttpClient, private establishmentService: EstablishmentService) {}
+
   public selectedFiles$: BehaviorSubject<Array<File>> = new BehaviorSubject(null);
+
+  public getPresignedUrl(filename: string): Observable<any> {
+    const params = new HttpParams().set('filename', filename);
+    const headers = new HttpHeaders().set('Content-Type', 'binary/octet-stream');
+
+    return this.http.get<any>(
+      `/api/establishment/${this.establishmentService.establishmentId}/bulkupload/signedUrl`,
+      { headers, params }
+    );
+  }
 }
--- src/app/features/bulk-upload/files-upload/files-upload.component.ts
+++ src/app/features/bulk-upload/files-upload/files-upload.component.ts
@@ -1,21 +1,21 @@
 import { BulkUploadService } from '@core/services/bulk-upload.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-files-upload',
-  templateUrl: './files-upload.component.html'
+  templateUrl: './files-upload.component.html',
 })
 export class FilesUploadComponent implements OnInit {
+  private form: FormGroup;
   private selectedFiles: Array<File>;
   private submitted = false;
-  private form: FormGroup;
 
   constructor(
     private bulkUploadService: BulkUploadService,
     private errorSummaryService: ErrorSummaryService,
-    private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -39,10 +39,15 @@ export class FilesUploadComponent implements OnInit {
     this.errorSummaryService.syncFormErrorsEvent.next(true);
 
     if (this.form.valid) {
-      // TODO in seperate pr
+      this.getPresignedUrls();
     } else {
       this.errorSummaryService.scrollToErrorSummary();
     }
   }
 
+  private getPresignedUrls(): void {
+    this.bulkUploadService.getPresignedUrl(this.selectedFiles[0].name).subscribe((data: any) => {
+      console.log('data', data);
+    });
+  }
 }
