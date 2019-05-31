161a861ce768b23a0df6998d19cb5918317f905f
Olu Alli <olu.alli@hmcts.net>
2019-05-31 12:40:27 +0100

    Feature | change request | Job Roles 'Other' Input field (#77) - WIP - Update Job model with `other` property - Add JobRole model for save job role text - update template to dynamically show other job text

--- src/app/core/model/job.model.ts
+++ src/app/core/model/job.model.ts
@@ -2,4 +2,11 @@ export interface Job {
   jobId: number;
   id?: number;
   title?: string;
+  other?: boolean;
+}
+
+export interface JobRole {
+  jobId: number;
+  title?: string;
+  other?: string;
 }
