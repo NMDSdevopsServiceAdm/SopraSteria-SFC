17dc4bf39cb052bba8e8a6ad9fa5533657312ab3
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-04 13:37:05 +0100

    added validatedFilesResponse and validatedFile interfaces

--- src/app/core/model/bulk-upload.model.ts
+++ src/app/core/model/bulk-upload.model.ts
@@ -5,3 +5,16 @@ export interface PresignedUrlResponse {
 export interface UploadFile extends File {
   extension: string;
 }
+
+export interface ValidatedFilesResponse {
+  establishments: ValidatedFile;
+  training: ValidatedFile;
+  workers: ValidatedFile;
+}
+
+export interface ValidatedFile {
+  errors: number;
+  filename: string;
+  records: number;
+  warnings: number;
+}
