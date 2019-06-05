e476f8b72761c7fb8c021ac17e030b23610b6886
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-05 16:55:44 +0100

    seperate updating form and prefilling logic out to seperate methods

--- src/app/features/workplace/other-services/other-services.component.ts
+++ src/app/features/workplace/other-services/other-services.component.ts
@@ -5,9 +5,11 @@ import { EstablishmentService } from '@core/services/establishment.service';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Question } from '../question/question.component';
 import { Router } from '@angular/router';
-import { ServiceGroup, Service } from '@core/model/services.model';
+import { Service } from '@core/model/services.model';
 import { HttpErrorResponse } from '@angular/common/http';
 import { filter } from 'lodash';
+import { WorkplaceService } from '@core/services/workplace.service';
+import { Workplace, WorkplaceCategory } from '@core/model/workplace.model';
 
 @Component({
   selector: 'app-other-services',
@@ -15,16 +17,18 @@ import { filter } from 'lodash';
 })
 export class OtherServicesComponent extends Question {
   private additionalOtherServiceMaxLength = 120;
-  private allServices: Array<Service> = [];
+  private allServices: Array<Workplace> = [];
+  private allOtherServices: Array<Workplace> = [];
   public renderForm = false;
-  public serviceGroups: ServiceGroup[];
+  public workplaceCategories: Array<WorkplaceCategory>;
 
   constructor(
     protected formBuilder: FormBuilder,
     protected router: Router,
     protected backService: BackService,
     protected errorSummaryService: ErrorSummaryService,
-    protected establishmentService: EstablishmentService
+    protected establishmentService: EstablishmentService,
+    protected workplaceService: WorkplaceService
   ) {
     super(formBuilder, router, backService, errorSummaryService, establishmentService);
 
@@ -35,23 +39,17 @@ export class OtherServicesComponent extends Question {
 
   protected init() {
     this.subscriptions.add(
-      this.establishmentService.getAllServices(this.establishment.id).subscribe(
-        serviceGroups => {
-          this.serviceGroups = serviceGroups;
-          this.serviceGroups.map(group => {
-            group.services.map(service => {
-              if (service.isMyService) {
-                this.form.get('otherServices').value.push(service.id);
-              }
-            });
-          });
-          this.serviceGroups.forEach((data: ServiceGroup) => this.allServices.push(...data.services));
+      this.workplaceService.getServicesByCategory(this.establishment.isRegulated).subscribe(
+        (workplaceCategories: Array<WorkplaceCategory>) => {
+          this.workplaceCategories = workplaceCategories;
+          this.workplaceCategories.forEach((data: WorkplaceCategory) => this.allServices.push(...data.services));
+          this.updateForm();
         },
         (error: HttpErrorResponse) => {
           this.serverError = this.errorSummaryService.getServerErrorMessage(error.status, this.serverErrorsMap);
           this.errorSummaryService.scrollToErrorSummary();
         },
-        () => this.updateForm()
+        () => this.preFillForm()
       )
     );
 
@@ -59,19 +57,15 @@ export class OtherServicesComponent extends Question {
   }
 
   private updateForm(): void {
-    this.allServices.forEach((service: Service) => {
-      if (service.other) {
+    this.allServices.forEach((workplace: Workplace) => {
+      if (workplace.other) {
         this.form.addControl(
-          `additionalOtherService${service.id}`,
+          `additionalOtherService${workplace.id}`,
           new FormControl(null, [Validators.maxLength(this.additionalOtherServiceMaxLength)])
         );
 
-        if (service.otherName) {
-          this.form.get(`additionalOtherService${service.id}`).setValue(service.otherName);
-        }
-
         this.formErrorsMap.push({
-          item: `additionalOtherService${service.id}`,
+          item: `additionalOtherService${workplace.id}`,
           type: [
             {
               name: 'maxlength',
@@ -80,9 +74,20 @@ export class OtherServicesComponent extends Question {
           ],
         });
       }
-
-      this.renderForm = true;
     });
+  }
+
+  private preFillForm(): void {
+    const allOtherServices = this.establishmentService.establishment.otherServices;
+    allOtherServices.forEach((data: WorkplaceCategory) => this.allOtherServices.push(...data.services));
+
+    this.allOtherServices.forEach((workplace: Workplace) => {
+      if (workplace.other) {
+        this.form.get('otherServices').value.push(workplace.id);
+        this.form.get(`additionalOtherService${workplace.id}`).setValue(workplace.other);
+      }
+    });
+    this.renderForm = true;
   }
 
   public toggle(target: HTMLInputElement) {
