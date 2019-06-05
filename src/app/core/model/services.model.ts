115b188704a692b3cc905058f4705cf1c4a1b177
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-05 16:25:23 +0100

    remove unused models

--- src/app/core/model/services.model.ts
+++ src/app/core/model/services.model.ts
@@ -1,3 +1,4 @@
+// TODO remove below and use Workplace instead
 export interface Service {
   id: number;
   isMyService?: boolean;
@@ -6,52 +7,15 @@ export interface Service {
   otherName?: string;
 }
 
+// TODO remove below and use WorkplaceCategory instead
 export interface ServiceGroup {
   category: string;
   services: Service[];
 }
 
+// TODO is this used??
 export interface Services {
   value: Service;
   index: number;
   array: Service[];
-}
-
-export interface AllServicesResponse {
-  allOtherServices: ServiceGroup[];
-}
-
-export interface ServicesModel {
-  id: number;
-  name: string;
-  mainService: [
-    {
-      id: number;
-      name: string;
-    }
-  ];
-  allOtherServices: [
-    {
-      category: string;
-      services: [
-        {
-          id: number;
-          name: string;
-          isMyService: boolean;
-        }
-      ];
-    }
-  ];
-  otherServices: [
-    {
-      category: string;
-      services: [
-        {
-          id: number;
-          name: string;
-          isMyService: boolean;
-        }
-      ];
-    }
-  ];
 }
