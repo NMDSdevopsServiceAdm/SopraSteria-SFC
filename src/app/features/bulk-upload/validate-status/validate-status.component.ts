7aa6ecf97214f7ea8abb7b9aa2c30a038e192dc1
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-08 09:49:57 +0100

    updated validate status component

--- src/app/features/bulk-upload/validate-status/validate-status.component.ts
+++ src/app/features/bulk-upload/validate-status/validate-status.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ValidateStatus } from '@core/model/bulk-upload.model';
 
 @Component({
   selector: 'app-validate-status',
   templateUrl: './validate-status.component.html',
   styleUrls: ['./validate-status.component.scss']
 })
-export class ValidateStatusComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ValidateStatusComponent {
+  public statusEnums = ValidateStatus;
+  @Input() public status: ValidateStatus;
 }
