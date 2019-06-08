7a6ffb9f29a1c49120fb28c6a4c8df5eaeb6159c
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-08 09:05:08 +0100

    added validate status enum for bulk upload validation step

--- src/app/core/model/bulk-upload.model.ts
+++ src/app/core/model/bulk-upload.model.ts
@@ -1,9 +1,17 @@
+export enum ValidateStatus {
+  Passed = 'Passed',
+  Failed = 'Failed',
+  In_Progress = 'In progress',
+}
+
+
 export interface PresignedUrlResponse {
   urls: string;
 }
 
 export interface UploadFile extends File {
   extension: string;
+  status?: ValidateStatus;
 }
 
 export interface ValidatedFilesResponse {
