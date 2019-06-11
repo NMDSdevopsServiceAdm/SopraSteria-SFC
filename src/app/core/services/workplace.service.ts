e552dbf3db3d8610f0803add86baa4fe5e83874a
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-11 15:52:04 +0100

    bugfix | correct other services api call

--- src/app/core/services/workplace.service.ts
+++ src/app/core/services/workplace.service.ts
@@ -1,14 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LocationAddress } from '@core/model/location.model';
 import { Observable } from 'rxjs';
-import { ServiceGroup } from '@core/model/services.model';
+import { AllServicesResponse, ServiceGroup } from '@core/model/services.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WorkplaceService {
-
   constructor(private http: HttpClient) {}
 
   public isRegulated(location: LocationAddress): boolean {
@@ -17,5 +17,12 @@ export class WorkplaceService {
 
   public getServicesByCategory(isRegulated: boolean): Observable<Array<ServiceGroup>> {
     return this.http.get<Array<ServiceGroup>>(`/api/services/byCategory?cqc=${isRegulated}`);
+  }
+
+  public getAllServices(establishmentId): Observable<ServiceGroup[]> {
+    const params = new HttpParams().set('all', 'true');
+    return this.http
+      .get<AllServicesResponse>(`/api/establishment/${establishmentId}/services`, { params })
+      .pipe(map(res => res.allOtherServices));
   }
 }
