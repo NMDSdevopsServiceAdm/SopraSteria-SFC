a97f45726c119ec2c2316bc3f26ed0a68846e877
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-14 19:00:03 +0100

    feature/new-custom-validator

--- src/app/shared/validators/custom-form-validators.ts
+++ src/app/shared/validators/custom-form-validators.ts
@@ -24,6 +24,27 @@ export class CustomValidators extends Validators {
     }
   }
 
+  static bothControlsHaveValues(group: AbstractControl): { [key: string]: boolean } | null {
+    const errors: ValidationErrors = {};
+    const control1Name: string = Object.keys(group['controls'])[0];
+    const control2Name: string = Object.keys(group['controls'])[1];
+
+    const control1: AbstractControl = group.get(control1Name);
+    const control2: AbstractControl = group.get(control2Name);
+
+    if (!control1.value && !control2.value) {
+      return { bothAreEmpty: true };
+    } else {
+      if (!control1.value && control2.value) {
+        errors[ `${control1Name}Empty` ] = true;
+        return errors;
+      } else if (control1.value && !control2.value) {
+        errors[ `${control2Name}Empty` ] = true;
+        return errors;
+      }
+    }
+  }
+
   static matchInputValues(c: AbstractControl): null | void {
     const passwordControl = c.get('createPasswordInput');
     const confirmPasswordControl = c.get('confirmPasswordInput');
