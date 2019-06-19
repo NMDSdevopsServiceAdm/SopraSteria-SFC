c82b7155468f17e470514998e38b1735e2bceb06
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-06-19 17:05:21 +0100

    removing unecessary logging

--- server/routes/registration.js
+++ server/routes/registration.js
@@ -302,6 +302,7 @@ router.route('/')
     // Username validation check
     if (req.body[0] && req.body[0].user && req.body[0].user.username) {
       if (!usernameCheck(req.body[0].user.username)) {
+        console.error(`Registration:  ${responseErrors.invalidUsername.message} - ${req.body[0].user.username}`);
         return res.status(400).json(responseErrors.invalidUsername);
       }
     }
