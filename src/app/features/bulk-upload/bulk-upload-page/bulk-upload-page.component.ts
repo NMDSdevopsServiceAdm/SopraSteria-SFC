d296cc77d31bad4adabf098534708b0421179572
tgreyuk <tgreyuk@gmail.com>
2019-06-19 23:48:55 +0100

    Added upload validation errors to error summary component

--- src/app/features/bulk-upload/bulk-upload-page/bulk-upload-page.component.ts
+++ src/app/features/bulk-upload/bulk-upload-page/bulk-upload-page.component.ts
@@ -1,37 +1,59 @@
+import { I18nPluralPipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ErrorDefinition, ErrorDetails } from '@core/model/errorSummary.model';
+import { LoggedInEstablishment } from '@core/model/logged-in.model';
 import { AuthService } from '@core/services/auth.service';
 import { BulkUploadService } from '@core/services/bulk-upload.service';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ErrorDetails } from '@core/model/errorSummary.model';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
-import { FormGroup } from '@angular/forms';
-import { LoggedInEstablishment } from '@core/model/logged-in.model';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bulk-upload-page',
   templateUrl: './bulk-upload-page.component.html',
+  providers: [I18nPluralPipe],
 })
 export class BulkUploadPageComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
   public establishment: LoggedInEstablishment | null;
   public form: FormGroup;
   public formErrorsMap: Array<ErrorDetails>;
+  public uploadValidationErrors: Array<ErrorDefinition>;
   public showErrorSummary: boolean;
 
   constructor(
     private authService: AuthService,
     private bulkUploadService: BulkUploadService,
-    private errorSummaryService: ErrorSummaryService
+    private errorSummaryService: ErrorSummaryService,
+    private i18nPluralPipe: I18nPluralPipe
   ) {}
 
   ngOnInit() {
     this.establishment = this.authService.establishment;
     this.setupFormErrorsMap();
+    this.setupUploadValidationErrors();
     this.setupSubscription();
   }
 
   public setupFormErrorsMap(): void {
     this.formErrorsMap = this.bulkUploadService.formErrorsMap();
+  }
+
+  public setupUploadValidationErrors(): void {
+    this.subscriptions.add(
+      this.bulkUploadService.totalErrors$.subscribe(totalErrors => {
+        this.uploadValidationErrors = [
+          {
+            name: 'bulkUploadValidation',
+            message: this.i18nPluralPipe.transform(totalErrors, {
+              '=1': 'There was # error in the files',
+              other: 'There were # errors in the files',
+            }),
+          },
+        ];
+        this.showErrorSummary = totalErrors > 0;
+      })
+    );
   }
 
   public setupSubscription(): void {
