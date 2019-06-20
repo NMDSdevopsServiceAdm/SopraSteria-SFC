5e9a254b0a4fea639c7a2f2797c37a182d7da1e8
Charlie Francis <charliefrancis@gmail.com>
2019-06-20 16:01:47 +0100

    Fixing Navigation, forgot establishmentId

--- src/app/features/workplace/start/start.component.ts
+++ src/app/features/workplace/start/start.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Establishment } from '@core/model/establishment.model';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
 })
-export class StartComponent {
-  constructor() {}
+export class StartComponent implements OnInit, OnDestroy {
+  public establishment: Establishment;
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(private establishmentService: EstablishmentService) {}
+
+  ngOnInit() {
+    this.subscriptions.add(
+      this.establishmentService.establishment$.pipe(take(1)).subscribe(establishment => {
+        this.establishment = establishment;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
