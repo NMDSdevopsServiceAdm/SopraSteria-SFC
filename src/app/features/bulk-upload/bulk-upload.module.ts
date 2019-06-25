baef5d5f19c427f8d9c7eb6efee232097274bc7f
tgreyuk <tgreyuk@gmail.com>
2019-06-25 14:41:10 +0100

    Provided DialogService and associated component to module

--- src/app/features/bulk-upload/bulk-upload.module.ts
+++ src/app/features/bulk-upload/bulk-upload.module.ts
@@ -1,7 +1,12 @@
+import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DialogService } from '@core/services/dialog.service';
 import { BulkUploadRoutingModule } from '@features/bulk-upload/bulk-upload-routing.module';
+import {
+  CompleteUploadWarningDialogComponent,
+} from '@features/bulk-upload/complete-upload-warning-dialog/complete-upload-warning-dialog';
 import { SharedModule } from '@shared/shared.module';
 
 import { BulkUploadPageComponent } from './bulk-upload-page/bulk-upload-page.component';
@@ -10,12 +15,13 @@ import { DownloadDataFilesComponent } from './download-data-files/download-data-
 import { FileValidateStatusComponent } from './file-validate-status/file-validate-status.component';
 import { FilesUploadProgressComponent } from './files-upload-progress/files-upload-progress.component';
 import { FilesUploadComponent } from './files-upload/files-upload.component';
+import { PostValidationActionsComponent } from '@features/bulk-upload/post-validation-actions/post-validation-actions.component';
 import { SelectedFilesListComponent } from './selected-files-list/selected-files-list.component';
 import { UploadDataFilesComponent } from './upload-data-files/upload-data-files.component';
 import { UploadedFilesListComponent } from './uploaded-files-list/uploaded-files-list.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, SharedModule, BulkUploadRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule, BulkUploadRoutingModule, OverlayModule],
   declarations: [
     BulkUploadPageComponent,
     CheckWorkplaceReferencesComponent,
@@ -26,6 +32,10 @@ import { UploadedFilesListComponent } from './uploaded-files-list/uploaded-files
     SelectedFilesListComponent,
     UploadDataFilesComponent,
     UploadedFilesListComponent,
+    CompleteUploadWarningDialogComponent,
+    PostValidationActionsComponent,
   ],
+  providers: [DialogService],
+  entryComponents: [CompleteUploadWarningDialogComponent],
 })
 export class BulkUploadModule {}
