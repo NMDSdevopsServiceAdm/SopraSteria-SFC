e172471c7b47fd472bed768aaf762a5dbe045fa8
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-26 13:59:51 +0100

    handle unsubscribe on component destroy

--- src/app/features/workplace/view-my-workplaces/view-my-workplaces.component.ts
+++ src/app/features/workplace/view-my-workplaces/view-my-workplaces.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoggedInEstablishment } from '@core/model/logged-in.model';
+import { MyWorkplace, MyWorkplacesResponse } from '@core/model/my-workplaces.model';
 import { AuthService } from '@core/services/auth.service';
 import { UserService } from '@core/services/user.service';
-import { MyWorkplace, MyWorkplacesResponse } from '@core/model/my-workplaces.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-view-my-workplaces',
   templateUrl: './view-my-workplaces.component.html',
 })
-export class ViewMyWorkplacesComponent implements OnInit {
+export class ViewMyWorkplacesComponent implements OnInit, OnDestroy {
   public establishment: LoggedInEstablishment | null;
   public myWorkplaces: Array<MyWorkplace> = [];
   public myWorkplacesCount: number;
+  private subscriptions: Subscription = new Subscription();
 
   constructor(private authService: AuthService, private userService: UserService) {}
 
@@ -21,14 +23,20 @@ export class ViewMyWorkplacesComponent implements OnInit {
   }
 
   private getMyEstablishments(): void {
-    this.userService.getMyEstablishments().subscribe((myWorkplaces: MyWorkplacesResponse) => {
-      if (myWorkplaces.subsidaries) {
-        this.myWorkplaces = myWorkplaces.subsidaries.establishments;
-        this.myWorkplacesCount = myWorkplaces.subsidaries.count;
-      } else {
-        this.myWorkplaces = [];
-        this.myWorkplacesCount = 0;
-      }
-    });
+    this.subscriptions.add(
+      this.userService.getMyEstablishments().subscribe((myWorkplaces: MyWorkplacesResponse) => {
+        if (myWorkplaces.subsidaries) {
+          this.myWorkplaces = myWorkplaces.subsidaries.establishments;
+          this.myWorkplacesCount = myWorkplaces.subsidaries.count;
+        } else {
+          this.myWorkplaces = [];
+          this.myWorkplacesCount = 0;
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }
