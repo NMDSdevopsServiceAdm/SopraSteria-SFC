251c3711c293ee1aaf27dd9374016065084e7d02
Olu Alli <olu.alli@hmcts.net>
2019-06-28 12:04:11 +0100

    Updates from PR comments

--- src/app/features/workplace/view-workplace/view-workplace.component.ts
+++ src/app/features/workplace/view-workplace/view-workplace.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '@core/services/auth.service';
 import { Establishment } from '@core/model/establishment.model';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { LoggedInEstablishment } from '@core/model/logged-in.model';
+import { URLStructure } from '@core/model/url.model';
 import { ParentPermissions } from '@core/model/my-workplaces.model';
 
 @Component({
@@ -12,19 +13,25 @@ import { ParentPermissions } from '@core/model/my-workplaces.model';
   templateUrl: './view-workplace.component.html',
 })
 export class ViewWorkplaceComponent implements OnInit {
-  public parentEstablishment: LoggedInEstablishment | null;
-  public subWorkPlace: Establishment;
+  public parentEstablishment: LoggedInEstablishment;
+  public workplace: Establishment;
+  public summaryReturnUrl: URLStructure;
 
   constructor(private establishmentService: EstablishmentService, private authService: AuthService) {}
 
   ngOnInit() {
     this.parentEstablishment = this.authService.establishment;
     this.establishmentService.establishment$.pipe(take(1)).subscribe(establishment => {
-      this.subWorkPlace = establishment;
+      this.workplace = establishment;
     });
+
+    this.summaryReturnUrl = {
+      url: ['/workplace', this.workplace.uid],
+      fragment: 'workplace',
+    };
   }
 
   public checkPermission() {
-    return this.subWorkPlace.parentPermissions === ParentPermissions.WorkplaceAndStaff;
+    return this.workplace.parentPermissions === ParentPermissions.WorkplaceAndStaff;
   }
 }
