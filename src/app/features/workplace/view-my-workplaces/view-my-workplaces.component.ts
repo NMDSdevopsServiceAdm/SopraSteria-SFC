ed2bd268918481cb4a079e4406f72b539412ba62
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-28 17:22:03 +0100

    feature | bulk upload | references server validation

--- src/app/features/workplace/view-my-workplaces/view-my-workplaces.component.ts
+++ src/app/features/workplace/view-my-workplaces/view-my-workplaces.component.ts
@@ -1,7 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ErrorDefinition } from '@core/model/errorSummary.model';
 import { LoggedInEstablishment } from '@core/model/logged-in.model';
 import { Workplace, GetWorkplacesResponse } from '@core/model/my-workplaces.model';
 import { AuthService } from '@core/services/auth.service';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { UserService } from '@core/services/user.service';
 import { Subscription } from 'rxjs';
 
@@ -10,29 +13,51 @@ import { Subscription } from 'rxjs';
   templateUrl: './view-my-workplaces.component.html',
 })
 export class ViewMyWorkplacesComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription = new Subscription();
   public establishment: LoggedInEstablishment | null;
+  public serverError: string;
+  public serverErrorsMap: ErrorDefinition[] = [];
   public workplaces: Workplace[] = [];
   public workplacesCount: number;
-  private subscriptions: Subscription = new Subscription();
 
-  constructor(private authService: AuthService, private userService: UserService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private errorSummaryService: ErrorSummaryService
+  ) {}
 
   ngOnInit() {
     this.establishment = this.authService.establishment;
     this.getEstablishments();
+    this.setupServerErrorsMap();
+  }
+
+  public setupServerErrorsMap(): void {
+    this.serverErrorsMap = [
+      {
+        name: 503,
+        message: 'Service unavailable.',
+      },
+    ];
   }
 
   private getEstablishments(): void {
     this.subscriptions.add(
-      this.userService.getEstablishments().subscribe((workplaces: GetWorkplacesResponse) => {
-        if (workplaces.subsidaries) {
-          this.workplaces = workplaces.subsidaries.establishments;
-          this.workplacesCount = workplaces.subsidaries.count;
-        } else {
-          this.workplaces = [];
-          this.workplacesCount = 0;
+      this.userService.getEstablishments().subscribe(
+        (workplaces: GetWorkplacesResponse) => {
+          if (workplaces.subsidaries) {
+            this.workplaces = workplaces.subsidaries.establishments;
+            this.workplacesCount = workplaces.subsidaries.count;
+          } else {
+            this.workplaces = [];
+            this.workplacesCount = 0;
+          }
+        },
+        (error: HttpErrorResponse) => {
+          this.serverError = this.errorSummaryService.getServerErrorMessage(error.status, this.serverErrorsMap);
+          this.errorSummaryService.scrollToErrorSummary();
         }
-      })
+      )
     );
   }
 
