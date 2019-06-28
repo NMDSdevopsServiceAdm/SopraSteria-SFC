19ca0d12ff40ef42fb7e91819fcb9f705c36cf2d
Omar Mirza <omar.creative.dev@gmail.com>
2019-06-28 15:12:29 +0100

    feature | bulk upload | references display grid (#838)
    * feature | bulk upload | references display grid
    
    * added TODO for updated references type, moved getReferences() data fetch to child class, set primary establishment name on base class
    
    * moved grid css to its own scss file, extended form-group--error on grid layouts, removed nested grid in markup
    
    * relocated common code to grid file

--- src/app/features/bulk-upload/workplace-references-page/workplace-references-page.component.ts
+++ src/app/features/bulk-upload/workplace-references-page/workplace-references-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BulkUploadFileType } from '@core/model/bulk-upload.model';
+import { GetWorkplacesResponse } from '@core/model/my-workplaces.model';
 import { AuthService } from '@core/services/auth.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { UserService } from '@core/services/user.service';
@@ -10,6 +11,7 @@ import { BulkUploadReferences } from '@features/bulk-upload/bulk-upload-referenc
 @Component({
   selector: 'app-workplace-references-page',
   templateUrl: '../bulk-upload-references/bulk-upload-references.html',
+  styleUrls: ['../bulk-upload-references/bulk-upload-references.scss'],
 })
 export class WorkplaceReferencesPageComponent extends BulkUploadReferences {
   // TODO check if needed
@@ -35,4 +37,17 @@ export class WorkplaceReferencesPageComponent extends BulkUploadReferences {
     this.authService.isFirstBulkUpload = false;
   }
    **/
+
+  protected getReferences(): void {
+    this.subscriptions.add(
+      this.userService.getEstablishments().subscribe((workplaces: GetWorkplacesResponse) => {
+        if (workplaces) {
+          this.references = workplaces.subsidaries ? workplaces.subsidaries.establishments : [];
+          if (this.references.length) {
+            this.updateForm();
+          }
+        }
+      })
+    );
+  }
 }
