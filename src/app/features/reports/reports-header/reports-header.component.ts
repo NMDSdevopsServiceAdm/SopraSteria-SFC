a08c0abc8bf285ca77327989d94e0e6aea8a53c5
tgreyuk <tgreyuk@gmail.com>
2019-07-02 17:58:04 +0100

    Updates to wdf UI

--- src/app/features/reports/reports-header/reports-header.component.ts
+++ src/app/features/reports/reports-header/reports-header.component.ts
@@ -1,9 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Establishment } from '@core/model/establishment.model';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-reports-header',
   templateUrl: './reports-header.component.html',
 })
-export class ReportsHeaderComponent {
+export class ReportsHeaderComponent implements OnInit {
   @Input() title = 'Reports';
+
+  public establishment: Establishment;
+
+  constructor(private establishmentService: EstablishmentService) {}
+
+  ngOnInit() {
+    this.establishmentService
+      .getEstablishment(this.establishmentService.establishmentId)
+      .pipe(take(1))
+      .subscribe(establishment => {
+        this.establishment = establishment;
+      });
+  }
 }
