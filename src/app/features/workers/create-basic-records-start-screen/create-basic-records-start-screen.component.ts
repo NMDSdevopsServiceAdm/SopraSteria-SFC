b155ca95bc187759b1e0fd23a98f425273a94690
Charlie Francis <charliefrancis@gmail.com>
2019-07-02 10:05:00 +0100

    Running Prettier on files

--- src/app/features/workers/create-basic-records-start-screen/create-basic-records-start-screen.component.ts
+++ src/app/features/workers/create-basic-records-start-screen/create-basic-records-start-screen.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BackService } from '@core/services/back.service';
 
 @Component({
-	selector: 'app-create-basic-records-start-screen',
-	templateUrl: './create-basic-records-start-screen.component.html',
+  selector: 'app-create-basic-records-start-screen',
+  templateUrl: './create-basic-records-start-screen.component.html',
 })
-export class CreateBasicRecordsStartScreenComponent {
-	constructor(
-		private backService: BackService
-	) {
-	}
+export class CreateBasicRecordsStartScreenComponent implements OnInit {
+  constructor(private backService: BackService) {}
 
-	ngOnInit() {
-		this.backService.setBackLink({ url: ['/dashboard'], fragment: 'staff-records' });
-	}
+  ngOnInit() {
+    this.backService.setBackLink({ url: ['/dashboard'], fragment: 'staff-records' });
+  }
 }
--- src/app/features/workers/create-staff-record-start-screen/create-staff-record-start-screen.component.ts
+++ src/app/features/workers/create-staff-record-start-screen/create-staff-record-start-screen.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BackService } from '@core/services/back.service';
 
 @Component({
-	selector: 'app-create-staff-record-start-screen',
-	templateUrl: './create-staff-record-start-screen.component.html',
+  selector: 'app-create-staff-record-start-screen',
+  templateUrl: './create-staff-record-start-screen.component.html',
 })
-export class CreateStaffRecordStartScreenComponent {
-	constructor(
-		private backService: BackService
-	) {
-	}
+export class CreateStaffRecordStartScreenComponent implements OnInit {
+  constructor(private backService: BackService) {}
 
-	ngOnInit() {
-		this.backService.setBackLink({ url: ['/dashboard'], fragment: 'staff-records' });
-	}
+  ngOnInit() {
+    this.backService.setBackLink({ url: ['/dashboard'], fragment: 'staff-records' });
+  }
 }
