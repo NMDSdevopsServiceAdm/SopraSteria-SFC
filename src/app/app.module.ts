d7407736221949d3a9166368c10298ef324edb1c
tgreyuk <tgreyuk@gmail.com>
2019-07-02 15:18:28 +0100

    Move shared tabs to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,17 +33,11 @@ import { WindowRef } from '@core/services/window.ref';
 import { WorkerService } from '@core/services/worker.service';
 import { DashboardComponent } from '@features/dashboard/dashboard.component';
 import { HomeTabComponent } from '@features/dashboard/home-tab/home-tab.component';
-import { StaffRecordsTabComponent } from '@features/dashboard/staff-records-tab/staff-records-tab.component';
-import { WorkplaceTabComponent } from '@features/dashboard/workplace-tab/workplace-tab.component';
 import { ForgotYourPasswordConfirmationComponent } from '@features/forgot-your-password/confirmation/confirmation.component';
 import { ForgotYourPasswordEditComponent } from '@features/forgot-your-password/edit/edit.component';
 import { ForgotYourPasswordComponent } from '@features/forgot-your-password/forgot-your-password.component';
 import { LoginComponent } from '@features/login/login.component';
 import { LogoutComponent } from '@features/logout/logout.component';
-import {
-  EligibilityDisplayOverviewComponent,
-} from '@features/reports/eligibility-display-overview/eligibility-display-overview.component';
-import { ReportsComponent } from '@features/reports/reports.component';
 import { ResetPasswordConfirmationComponent } from '@features/reset-password/confirmation/confirmation.component';
 import { ResetPasswordEditComponent } from '@features/reset-password/edit/edit.component';
 import { ResetPasswordComponent } from '@features/reset-password/reset-password.component';
@@ -58,7 +52,6 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent,
     DashboardComponent,
-    EligibilityDisplayOverviewComponent,
     FooterComponent,
     ForgotYourPasswordComponent,
     ForgotYourPasswordConfirmationComponent,
@@ -69,13 +62,10 @@ import { AppComponent } from './app.component';
     LogoutComponent,
     PageNotFoundComponent,
     ProblemWithTheServiceComponent,
-    ReportsComponent,
     ResetPasswordComponent,
     ResetPasswordConfirmationComponent,
     ResetPasswordEditComponent,
     ServiceUnavailableComponent,
-    StaffRecordsTabComponent,
-    WorkplaceTabComponent,
   ],
   imports: [
     Angulartics2Module.forRoot({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -16,6 +16,7 @@ import { MessagesComponent } from './components/messages/messages.component';
 import { PanelComponent } from './components/panel/panel.component';
 import { PhaseBannerComponent } from './components/phase-banner/phase-banner.component';
 import { ProgressComponent } from './components/progress/progress.component';
+import { StaffRecordsTabComponent } from './components/staff-records-tab/staff-records-tab.component';
 import { StartButtonComponent } from './components/start-button/start-button.component';
 import { StatusComponent } from './components/status/status.component';
 import { SubmitButtonComponent } from './components/submit-button/submit-button.component';
@@ -23,6 +24,7 @@ import { TabComponent } from './components/tabs/tab.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { TotalStaffPanelComponent } from './components/total-staff-panel/total-staff-panel.component';
 import { WorkplaceSummaryComponent } from './components/workplace-summary/workplace-summary.component';
+import { WorkplaceTabComponent } from './components/workplace-tab/workplace-tab.component';
 import { FileValueAccessorDirective } from './form-controls/file-control-value-accessor';
 import { AbsoluteNumberPipe } from './pipes/absolute-number.pipe';
 import { ClosedEndedAnswerPipe } from './pipes/closed-ended-answer.pipe';
@@ -55,6 +57,8 @@ import { OpenEndedAnswerPipe } from './pipes/open-ended-answer.pipe';
     WorkplaceSummaryComponent,
     OpenEndedAnswerPipe,
     ClosedEndedAnswerPipe,
+    WorkplaceTabComponent,
+    StaffRecordsTabComponent,
   ],
   exports: [
     AlertComponent,
@@ -80,6 +84,8 @@ import { OpenEndedAnswerPipe } from './pipes/open-ended-answer.pipe';
     WorkplaceSummaryComponent,
     OpenEndedAnswerPipe,
     ClosedEndedAnswerPipe,
+    WorkplaceTabComponent,
+    StaffRecordsTabComponent,
   ],
 })
 export class SharedModule {}
