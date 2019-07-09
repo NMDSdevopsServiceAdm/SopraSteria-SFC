77721c5540e2114140e13d4e9ffa6e465e5b2042
Charlie Francis <charliefrancis@gmail.com>
2019-07-09 16:02:28 +0100

    Adding Breadcrumb service so they can be shown/hidden

--- src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { NestedRoutesService } from '@core/services/nested-routes.service';
 import { Subscription } from 'rxjs';
 
@@ -10,12 +11,19 @@ export class BreadcrumbsComponent implements OnInit, OnDestroy {
   public breadcrumbs: any[] = [];
   public display = false;
   private subscriptions: Subscription = new Subscription();
-  constructor(private nestedRoutesService: NestedRoutesService) {}
+
+  constructor(private breadcrumbService: BreadcrumbService, private nestedRoutesService: NestedRoutesService) {}
 
   ngOnInit() {
     this.subscriptions.add(
       this.nestedRoutesService.routes$.subscribe(routes => {
         this.breadcrumbs = routes;
+      })
+    );
+
+    this.subscriptions.add(
+      this.breadcrumbService.display$.subscribe(display => {
+        this.display = display;
       })
     );
   }
