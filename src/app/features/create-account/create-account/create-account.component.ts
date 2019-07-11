befc0362fe0077a23f7ca05bcccd1cb2a286a582
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-11 14:52:23 +0100

    add form controls for permissions and optional primary user

--- src/app/features/create-account/create-account/create-account.component.ts
+++ src/app/features/create-account/create-account/create-account.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BackService } from '@core/services/back.service';
 import { CreateAccountService } from '@core/services/create-account/create-account.service';
@@ -21,6 +21,25 @@ export class CreateAccountComponent extends AccountDetails {
     private createAccountService: CreateAccountService
   ) {
     super(backService, errorSummaryService, fb, router);
+  }
+
+  protected init(): void {
+    this.addFormControls();
+  }
+
+  private addFormControls(): void {
+    this.form.addControl('permissions', new FormControl(null, Validators.required));
+    this.form.addControl('primaryUser', new FormControl(null));
+
+    this.formErrorsMap.push({
+      item: 'permissions',
+      type: [
+        {
+          name: 'required',
+          message: 'Please specify permissions for the new account.',
+        }
+      ],
+    });
   }
 
   protected save() {
