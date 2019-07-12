7034157ee5d47f1996191d99a447600a9e5c4172
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-12 12:09:26 +0100

    feature | extend create username component | so its reused on account… (#945)
    * feature | extend create username component | so its reused on account creation flow
    
    * remove console log

--- src/app/features/create-account/create-account-routing.module.ts
+++ src/app/features/create-account/create-account-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateAccountComponent } from '@features/create-account/create-account/create-account.component';
+import { CreateUsernameComponent } from '@features/create-account/create-username/create-username.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CreateAccountComponent,
+  },
+  {
+    path: 'create-username',
+    component: CreateUsernameComponent,
+    data: { title: 'Create Username' },
   },
 ];
 
