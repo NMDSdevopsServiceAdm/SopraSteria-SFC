5265ca3513226bf0f403f23bae0e00e69ff7070e
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-12 16:17:17 +0100

    add security-question route and update create-account module

--- src/app/features/create-account/create-account-routing.module.ts
+++ src/app/features/create-account/create-account-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateAccountComponent } from '@features/create-account/create-account/create-account.component';
 import { CreateUsernameComponent } from '@features/create-account/create-username/create-username.component';
+import { SecurityQuestionComponent } from '@features/create-account/security-question/security-question.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,11 @@ const routes: Routes = [
     path: 'create-username',
     component: CreateUsernameComponent,
     data: { title: 'Create Username' },
+  },
+  {
+    path: 'security-question',
+    component: SecurityQuestionComponent,
+    data: { title: 'Security Question' },
   },
 ];
 
--- src/app/features/create-account/create-account.module.ts
+++ src/app/features/create-account/create-account.module.ts
@@ -5,11 +5,10 @@ import { CreateAccountRoutingModule } from '@features/create-account/create-acco
 import { SharedModule } from '@shared/shared.module';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { CreateUsernameComponent } from './create-username/create-username.component';
+import { SecurityQuestionComponent } from './security-question/security-question.component';
 
 @NgModule({
-  imports: [
-    CommonModule, ReactiveFormsModule, SharedModule, CreateAccountRoutingModule
-  ],
-  declarations: [CreateAccountComponent, CreateUsernameComponent]
+  imports: [CommonModule, ReactiveFormsModule, SharedModule, CreateAccountRoutingModule],
+  declarations: [CreateAccountComponent, CreateUsernameComponent, SecurityQuestionComponent],
 })
-export class CreateAccountModule { }
+export class CreateAccountModule {}
