77ce18258311638bfda79a6a16ee079947093145
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-15 14:06:19 +0100

    create confirm account details skeleton component, route and update module

--- src/app/features/create-account/create-account-routing.module.ts
+++ src/app/features/create-account/create-account-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ConfirmAccountDetailsComponent } from '@features/create-account/confirm-account-details/confirm-account-details.component';
 import { CreateAccountComponent } from '@features/create-account/create-account/create-account.component';
 import { CreateUsernameComponent } from '@features/create-account/create-username/create-username.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { SecurityQuestionComponent } from '@features/create-account/security-question/security-question.component';
 
 const routes: Routes = [
@@ -18,6 +19,11 @@ const routes: Routes = [
     path: 'security-question',
     component: SecurityQuestionComponent,
     data: { title: 'Security Question' },
+  },
+  {
+    path: 'confirm-account-details',
+    component: ConfirmAccountDetailsComponent,
+    data: { title: 'Confirm Account Details' },
   },
 ];
 
