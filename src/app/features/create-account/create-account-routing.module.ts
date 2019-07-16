02283a3634742b742d23036a73f853b31bfc8c4a
tgreyuk <tgreyuk@gmail.com>
2019-07-16 14:51:41 +0100

    Added establishmentUid param to create-user path

--- src/app/features/create-account/create-account-routing.module.ts
+++ src/app/features/create-account/create-account-routing.module.ts
@@ -1,18 +1,24 @@
-import { AccountSavedComponent } from '@features/create-account/account-saved/account-saved.component';
-import { ConfirmAccountDetailsComponent } from '@features/create-account/confirm-account-details/confirm-account-details.component';
-import { CreateAccountComponent } from '@features/create-account/create-account/create-account.component';
-import { CreateUsernameComponent } from '@features/create-account/create-username/create-username.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountSavedComponent } from '@features/create-account/account-saved/account-saved.component';
+import {
+  ConfirmAccountDetailsComponent,
+} from '@features/create-account/confirm-account-details/confirm-account-details.component';
+import { CreateAccountComponent } from '@features/create-account/create-account/create-account.component';
+import { CreateUsernameComponent } from '@features/create-account/create-username/create-username.component';
 import { SecurityQuestionComponent } from '@features/create-account/security-question/security-question.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CreateAccountComponent,
   },
   {
-    path: 'create-username/:establishmentUid',
+    path: 'create-user/:establishmentUid',
+    component: CreateAccountComponent,
+    data: { title: 'Create User' },
+  },
+  {
+    path: 'create-username',
     component: CreateUsernameComponent,
     data: { title: 'Create Username' },
   },
