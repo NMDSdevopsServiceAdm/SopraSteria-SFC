b6bb65043c6409fe28326b9e9237c9d3bc6b5b50
tgreyuk <tgreyuk@gmail.com>
2019-07-19 23:00:58 +0100

    Updated ordering and fixed allowed read only limit

--- src/app/shared/components/user-accounts-summary/user-accounts-summary.component.ts
+++ src/app/shared/components/user-accounts-summary/user-accounts-summary.component.ts
@@ -1,32 +1,44 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Establishment } from '@core/model/establishment.model';
 import { Roles } from '@core/model/roles.enum';
 import { UserDetails, UserStatus } from '@core/model/userDetails.model';
 import { AuthService } from '@core/services/auth.service';
 import { UserService } from '@core/services/user.service';
 import { orderBy } from 'lodash';
-import { combineLatest } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-accounts-summary',
   templateUrl: './user-accounts-summary.component.html',
 })
-export class UserAccountsSummaryComponent implements OnInit {
+export class UserAccountsSummaryComponent implements OnInit, OnDestroy {
   @Input() workplace: Establishment;
 
+  private subscriptions: Subscription = new Subscription();
   public users: Array<UserDetails> = [];
   public canAddUser: boolean;
 
   constructor(private authService: AuthService, private userService: UserService) {}
 
   ngOnInit() {
-    combineLatest(this.authService.auth$, this.userService.getAllUsersForEstablishment(this.workplace.uid))
-      .pipe(take(1))
-      .subscribe(([auth, users]) => {
-        this.users = orderBy(users, ['status', 'role', 'username'], ['desc', 'asc', 'asc']);
-        this.canAddUser = auth && auth.role === Roles.Edit && this.userSlotsAvailable(users);
-      });
+    this.subscriptions.add(
+      this.userService
+        .getAllUsersForEstablishment(this.workplace.uid)
+        .pipe(withLatestFrom(this.authService.auth$))
+        .subscribe(([users, auth]) => {
+          this.users = orderBy(
+            users,
+            ['status', 'isPrimary', 'role', (user: UserDetails) => user.fullname.toLowerCase()],
+            ['desc', 'desc', 'asc', 'asc']
+          );
+          this.canAddUser = auth && auth.role === Roles.Edit && this.userSlotsAvailable(users);
+        })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
   public getUserType(user: UserDetails) {
@@ -40,6 +52,6 @@ export class UserAccountsSummaryComponent implements OnInit {
   private userSlotsAvailable(users: Array<UserDetails>) {
     const editUsers = users.filter(user => user.role === Roles.Edit);
     const readOnlyUsers = users.filter(user => user.role === Roles.Read);
-    return editUsers.length < 3 || readOnlyUsers.length < 3;
+    return editUsers.length < 3 || readOnlyUsers.length < 20;
   }
 }
