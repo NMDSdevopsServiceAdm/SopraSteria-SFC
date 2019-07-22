efc1b1ace141d52e53006ed2df535365b5e4d7ec
tgreyuk <tgreyuk@gmail.com>
2019-07-22 16:26:40 +0100

    Fixed parent readonly limits

--- src/app/shared/components/user-accounts-summary/user-accounts-summary.component.ts
+++ src/app/shared/components/user-accounts-summary/user-accounts-summary.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Establishment } from '@core/model/establishment.model';
 import { Roles } from '@core/model/roles.enum';
 import { UserDetails, UserStatus } from '@core/model/userDetails.model';
-import { AuthService } from '@core/services/auth.service';
 import { UserService } from '@core/services/user.service';
 import { orderBy } from 'lodash';
 import { Subscription } from 'rxjs';
@@ -19,20 +18,20 @@ export class UserAccountsSummaryComponent implements OnInit, OnDestroy {
   public users: Array<UserDetails> = [];
   public canAddUser: boolean;
 
-  constructor(private authService: AuthService, private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
     this.subscriptions.add(
       this.userService
         .getAllUsersForEstablishment(this.workplace.uid)
-        .pipe(withLatestFrom(this.authService.auth$))
-        .subscribe(([users, auth]) => {
+        .pipe(withLatestFrom(this.userService.loggedInUser$))
+        .subscribe(([users, loggedInUser]) => {
           this.users = orderBy(
             users,
             ['status', 'isPrimary', 'role', (user: UserDetails) => user.fullname.toLowerCase()],
             ['desc', 'desc', 'asc', 'asc']
           );
-          this.canAddUser = auth && auth.role === Roles.Edit && this.userSlotsAvailable(users);
+          this.canAddUser = loggedInUser.role === Roles.Edit && this.userSlotsAvailable(users);
         })
     );
   }
@@ -50,8 +49,9 @@ export class UserAccountsSummaryComponent implements OnInit, OnDestroy {
   }
 
   private userSlotsAvailable(users: Array<UserDetails>) {
+    const readOnlyLimit = this.workplace.isParent ? 20 : 3;
     const editUsers = users.filter(user => user.role === Roles.Edit);
     const readOnlyUsers = users.filter(user => user.role === Roles.Read);
-    return editUsers.length < 3 || readOnlyUsers.length < 20;
+    return editUsers.length < 3 || readOnlyUsers.length < readOnlyLimit;
   }
 }
