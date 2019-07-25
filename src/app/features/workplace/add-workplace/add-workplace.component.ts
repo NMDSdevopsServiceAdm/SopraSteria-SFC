8ebe1bf18400d6b0934316bae652a125038dc867
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-25 15:44:54 +0100

    update markup and finish component for add a workplace screen

--- src/app/features/workplace/add-workplace/add-workplace.component.ts
+++ src/app/features/workplace/add-workplace/add-workplace.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { BackService } from '@core/services/back.service';
 
 @Component({
   selector: 'app-add-workplace',
   templateUrl: './add-workplace.component.html',
 })
 export class AddWorkplaceComponent implements OnInit {
-  constructor() {}
+  constructor(private backService: BackService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setBackLink();
+  }
+
+  private setBackLink(): void {
+    this.backService.setBackLink({ url: ['/workplace/view-my-workplaces'] });
+  }
 }
