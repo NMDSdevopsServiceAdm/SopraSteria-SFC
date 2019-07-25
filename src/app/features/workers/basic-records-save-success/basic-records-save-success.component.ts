83cb0b12bcdc2693014c90179da1a475cfb9cca3
tgreyuk <tgreyuk@gmail.com>
2019-07-25 23:37:11 +0100

    Implemented WDF total staff warning

--- src/app/features/workers/basic-records-save-success/basic-records-save-success.component.ts
+++ src/app/features/workers/basic-records-save-success/basic-records-save-success.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { WorkerService } from '@core/services/worker.service';
 
 @Component({
   selector: 'app-basic-records-save-success',
   templateUrl: './basic-records-save-success.component.html',
-  styleUrls: ['./basic-records-save-success.component.scss'],
 })
-export class BasicRecordsSaveSuccessComponent implements OnInit {
+export class BasicRecordsSaveSuccessComponent implements OnInit, OnDestroy {
   public total: number;
+  public returnToWDF = false;
 
   constructor(private router: Router, private workerService: WorkerService) {}
 
@@ -17,5 +17,25 @@ export class BasicRecordsSaveSuccessComponent implements OnInit {
     if (this.total === 0) {
       this.router.navigate(['/dashboard'], { fragment: 'staff-records', replaceUrl: true });
     }
+    this.returnToWDF = this.workerService.returnTo && [...this.workerService.returnTo.url].pop() === 'wdf';
+  }
+
+  ngOnDestroy() {
+    this.workerService.setReturnTo(null);
+  }
+
+  public returnTo() {
+    if (this.returnToWDF) {
+      this.router.navigate(this.workerService.returnTo.url, { fragment: this.workerService.returnTo.fragment });
+      return;
+    }
+    this.router.navigate(['/dashboard'], { fragment: 'staff-records' });
+  }
+
+  get returnToLabel() {
+    if (this.returnToWDF) {
+      return 'Return to the WDF Report';
+    }
+    return 'Go to staff records';
   }
 }
