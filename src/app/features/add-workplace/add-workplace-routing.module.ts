6ff47d63c9104ca8c1fb639fcef1a9a3f543ef4e
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-28 08:02:06 +0100

    extend cqc question screen and use on add-workplace flow

--- src/app/features/add-workplace/add-workplace-routing.module.ts
+++ src/app/features/add-workplace/add-workplace-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ParentGuard } from '@core/guards/parent/parent.guard';
+import { RegulatedByCqcComponent } from '@features/add-workplace/regulated-by-cqc/regulated-by-cqc.component';
 import { StartComponent } from './start/start.component';
 
 const routes: Routes = [
   {
     path: 'start',
     component: StartComponent,
-    canActivate: [ParentGuard],
     data: { title: 'Add Workplace' },
+  },
+  {
+    path: 'regulated-by-cqc',
+    component: RegulatedByCqcComponent,
+    data: { title: 'Regulated by CQC' },
   },
 ];
 
--- src/app/features/add-workplace/add-workplace.module.ts
+++ src/app/features/add-workplace/add-workplace.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AddWorkplaceRoutingModule } from '@features/add-workplace/add-workplace-routing.module';
+import { RegulatedByCqcComponent } from '@features/add-workplace/regulated-by-cqc/regulated-by-cqc.component';
 import { StartComponent } from '@features/add-workplace/start/start.component';
 import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, SharedModule, AddWorkplaceRoutingModule],
-  declarations: [StartComponent],
+  declarations: [StartComponent, RegulatedByCqcComponent],
 })
 export class AddWorkplaceModule {}
