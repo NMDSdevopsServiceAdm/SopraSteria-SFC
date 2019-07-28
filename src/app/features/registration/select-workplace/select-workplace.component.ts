10cbbb906b454127530d50141d7df244f6b1bd02
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-28 09:36:41 +0100

    create base class for select workplace screen and extend and use it on registration flow

--- src/app/features/registration/select-workplace/select-workplace.component.ts
+++ src/app/features/registration/select-workplace/select-workplace.component.ts
@@ -1,38 +1,33 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ErrorDetails } from '@core/model/errorSummary.model';
 import { LocationAddress } from '@core/model/location.model';
 import { BackService } from '@core/services/back.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { RegistrationService } from '@core/services/registration.service';
-import { filter } from 'lodash';
-import { Subscription } from 'rxjs';
+import { SelectWorkplace } from '@features/workplace-find-and-select/select-workplace/select-workplace';
 
 @Component({
   selector: 'app-select-workplace',
   templateUrl: './select-workplace.component.html',
 })
-export class SelectWorkplaceComponent implements OnInit, OnDestroy {
-  public locationAddresses: Array<LocationAddress>;
-  public form: FormGroup;
-  public formErrorsMap: Array<ErrorDetails>;
-  public submitted = false;
-  private subscriptions: Subscription = new Subscription();
-
+export class SelectWorkplaceComponent extends SelectWorkplace {
   constructor(
-    private backService: BackService,
-    private errorSummaryService: ErrorSummaryService,
-    private fb: FormBuilder,
     private registrationService: RegistrationService,
-    private router: Router
-  ) {}
+    protected backService: BackService,
+    protected errorSummaryService: ErrorSummaryService,
+    protected formBuilder: FormBuilder,
+    protected router: Router
+  ) {
+    super(backService, errorSummaryService, formBuilder, router);
+  }
 
-  ngOnInit() {
-    this.setupForm();
-    this.setupFormErrorsMap();
+  protected init(): void {
     this.setBackLink();
+    this.setupSubscription();
+  }
 
+  protected setupSubscription(): void {
     this.subscriptions.add(
       this.registrationService.locationAddresses$.subscribe(
         (locationAddresses: Array<LocationAddress>) => (this.locationAddresses = locationAddresses)
@@ -40,62 +35,12 @@ export class SelectWorkplaceComponent implements OnInit, OnDestroy {
     );
   }
 
-  private setupForm(): void {
-    this.form = this.fb.group({
-      workplace: ['', Validators.required],
-    });
-  }
-
-  public setupFormErrorsMap(): void {
-    this.formErrorsMap = [
-      {
-        item: 'workplace',
-        type: [
-          {
-            name: 'required',
-            message: 'Please select an address.',
-          },
-        ],
-      },
-    ];
-  }
-
-  private getSelectedLocation(): LocationAddress {
-    const selectedLocationId: string = this.form.get('workplace').value;
-    return filter(this.locationAddresses, ['locationId', selectedLocationId])[0];
-  }
-
-  public onSubmit(): void {
-    this.submitted = true;
-    this.errorSummaryService.syncFormErrorsEvent.next(true);
-
-    if (this.form.valid) {
-      this.save();
-    } else {
-      this.errorSummaryService.scrollToErrorSummary();
-    }
-  }
-
-  /**
-   * Pass in formGroup or formControl name and errorType
-   * Then return error message
-   * @param item
-   * @param errorType
-   */
-  public getFormErrorMessage(item: string, errorType: string): string {
-    return this.errorSummaryService.getFormErrorMessage(item, errorType, this.formErrorsMap);
-  }
-
-  private save(): void {
+  protected save(): void {
     this.registrationService.selectedLocationAddress$.next(this.getSelectedLocation());
     this.router.navigate(['/registration/select-main-service']);
   }
 
-  private setBackLink(): void {
+  protected setBackLink(): void {
     this.backService.setBackLink({ url: ['/registration/regulated-by-cqc'] });
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe();
   }
 }
