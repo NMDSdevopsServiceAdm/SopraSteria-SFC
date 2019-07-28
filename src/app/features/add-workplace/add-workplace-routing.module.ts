27f16fc5922dd6f87a009c7488ddf4af4684ba92
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-28 10:09:14 +0100

    fix routing

--- src/app/features/add-workplace/add-workplace-routing.module.ts
+++ src/app/features/add-workplace/add-workplace-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegulatedByCqcComponent } from '@features/add-workplace/regulated-by-cqc/regulated-by-cqc.component';
+import { SelectWorkplaceComponent } from '@features/add-workplace/select-workplace/select-workplace.component';
 import { StartComponent } from './start/start.component';
 
 const routes: Routes = [
@@ -13,6 +14,11 @@ const routes: Routes = [
     path: 'regulated-by-cqc',
     component: RegulatedByCqcComponent,
     data: { title: 'Regulated by CQC' },
+  },
+  {
+    path: 'select-workplace',
+    component: SelectWorkplaceComponent,
+    data: { title: 'Select Workplace' },
   },
 ];
 
