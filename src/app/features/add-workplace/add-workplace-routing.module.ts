922efb0ba6e952f9c83c65d0efe0077de303fb1d
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-29 14:35:46 +0100

    Feature/create base class for find workplace address (#1120)
    * convert to getFirstErrorMessage for form errors for consistency
    
    * remove 503 server error code as its globally handled and redirected by interceptor
    
    * create WIP base class for find workplace address
    
    * update base class, extend base class and use on registration flow
    
    * extend base class for add-workplace flow

--- src/app/features/add-workplace/add-workplace-routing.module.ts
+++ src/app/features/add-workplace/add-workplace-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EnterWorkplaceAddressComponent } from '@features/add-workplace/enter-workplace-address/enter-workplace-address.component';
+import { FindWorkplaceAddressComponent } from '@features/add-workplace/find-workplace-address/find-workplace-address.component';
 import { RegulatedByCqcComponent } from '@features/add-workplace/regulated-by-cqc/regulated-by-cqc.component';
 import { SelectWorkplaceAddressComponent } from '@features/add-workplace/select-workplace-address/select-workplace-address.component';
 import { SelectWorkplaceComponent } from '@features/add-workplace/select-workplace/select-workplace.component';
@@ -31,6 +32,11 @@ const routes: Routes = [
     path: 'enter-workplace-address',
     component: EnterWorkplaceAddressComponent,
     data: { title: 'Enter Workplace Address' },
+  },
+  {
+    path: 'find-workplace-address',
+    component: FindWorkplaceAddressComponent,
+    data: { title: 'Find Workplace Address' },
   },
 ];
 
