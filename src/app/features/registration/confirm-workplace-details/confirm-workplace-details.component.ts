12af08366758ee4c7803fec53c85cdcd62552198
Omar Mirza <omar.creative.dev@gmail.com>
2019-07-29 15:53:50 +0100

    update base class and extend and reuse on registration flow

--- src/app/features/registration/confirm-workplace-details/confirm-workplace-details.component.ts
+++ src/app/features/registration/confirm-workplace-details/confirm-workplace-details.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { LocationAddress } from '@core/model/location.model';
 import { Service } from '@core/model/services.model';
 import { BackService } from '@core/services/back.service';
 import { RegistrationService } from '@core/services/registration.service';
-import { Subscription } from 'rxjs';
+import { ConfirmWorkplaceDetails } from '@features/workplace-find-and-select/confirm-workplace-details/confirm-workplace-details';
 
 @Component({
   selector: 'app-confirm-workplace-details',
   templateUrl: './confirm-workplace-details.component.html',
 })
-export class ConfirmWorkplaceDetailsComponent implements OnInit, OnDestroy {
-  public locationAddress: LocationAddress;
-  public workplaceService: Service;
-  private subscriptions: Subscription = new Subscription();
-
-  constructor(private backService: BackService, private registrationService: RegistrationService) {}
-
-  ngOnInit() {
-    this.getWorkplaceData();
-    this.setBackLink();
+export class ConfirmWorkplaceDetailsComponent extends ConfirmWorkplaceDetails {
+  constructor(private registrationService: RegistrationService, protected backService: BackService) {
+    super(backService);
   }
 
-  private getWorkplaceData(): void {
+  protected init(): void {
+    this.flow = '/registration';
+    this.getWorkplaceData();
+  }
+
+  protected getWorkplaceData(): void {
     this.subscriptions.add(
       this.registrationService.selectedLocationAddress$.subscribe(
         (locationAddress: LocationAddress) => (this.locationAddress = locationAddress)
@@ -33,13 +31,5 @@ export class ConfirmWorkplaceDetailsComponent implements OnInit, OnDestroy {
         (workplaceService: Service) => (this.workplaceService = workplaceService)
       )
     );
-  }
-
-  private setBackLink(): void {
-    this.backService.setBackLink({ url: ['/registration/select-main-service'] });
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe();
   }
 }
--- src/app/features/workplace-find-and-select/confirm-workplace-details/confirm-workplace-details.ts
+++ src/app/features/workplace-find-and-select/confirm-workplace-details/confirm-workplace-details.ts
@@ -1,7 +1,31 @@
-import { OnInit } from '@angular/core';
+import { OnDestroy, OnInit } from '@angular/core';
+import { LocationAddress } from '@core/model/location.model';
+import { Service } from '@core/model/services.model';
+import { BackService } from '@core/services/back.service';
+import { Subscription } from 'rxjs';
 
-export class ConfirmWorkplaceDetails implements OnInit {
-  constructor() {}
+export class ConfirmWorkplaceDetails implements OnInit, OnDestroy {
+  protected flow: string;
+  protected subscriptions: Subscription = new Subscription();
+  public locationAddress: LocationAddress;
+  public workplaceService: Service;
 
-  ngOnInit() {}
+  constructor(protected backService: BackService) {}
+
+  ngOnInit() {
+    this.init();
+    this.setBackLink();
+  }
+
+  protected init(): void {}
+
+  protected getWorkplaceData(): void {}
+
+  protected setBackLink(): void {
+    this.backService.setBackLink({ url: [`${this.flow}/registration/select-main-service`] });
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
