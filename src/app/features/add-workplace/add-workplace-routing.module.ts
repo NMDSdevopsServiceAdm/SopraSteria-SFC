c5643333d7be25b7504edb53697b6616c50a92cb
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-01 14:04:50 +0100

    create change your details route

--- src/app/features/add-workplace/add-workplace-routing.module.ts
+++ src/app/features/add-workplace/add-workplace-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddWorkplaceInProgressGuard } from '@core/guards/add-workplace-in-progress/add-workplace-in-progress.guard';
 import { AddWorkplaceCompleteComponent } from '@features/add-workplace/add-workplace-complete/add-workplace-complete.component';
+import { ChangeYourDetailsComponent } from '@features/add-workplace/change-your-details/change-your-details.component';
 import { ConfirmWorkplaceDetailsComponent } from '@features/add-workplace/confirm-workplace-details/confirm-workplace-details.component';
 import { CreateUserAccountComponent } from '@features/add-workplace/create-user-account/create-user-account.component';
 import { EnterWorkplaceAddressComponent } from '@features/add-workplace/enter-workplace-address/enter-workplace-address.component';
@@ -58,6 +59,12 @@ const routes: Routes = [
     path: 'create-user-account',
     component: CreateUserAccountComponent,
     data: { title: 'Create User Account' },
+    canActivate: [AddWorkplaceInProgressGuard]
+  },
+  {
+    path: 'change-your-details',
+    component: ChangeYourDetailsComponent,
+    data: { title: 'Change Your Details' },
     canActivate: [AddWorkplaceInProgressGuard]
   },
   {
