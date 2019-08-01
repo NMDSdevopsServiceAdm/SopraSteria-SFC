918af7413ba13d4a6be313ce23fa5137200efabf
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-01 18:42:26 +0100

    updated confirm account details component

--- src/app/features/add-workplace/confirm-account-details/confirm-account-details.component.ts
+++ src/app/features/add-workplace/confirm-account-details/confirm-account-details.component.ts
@@ -1,11 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CreateAccountRequest } from '@core/model/account.model';
+import { Roles } from '@core/model/roles.enum';
+import { BackService } from '@core/services/back.service';
+import { CreateAccountService } from '@core/services/create-account/create-account.service';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { WorkplaceService } from '@core/services/workplace.service';
+import { ConfirmAccountDetails } from '@features/account/confirm-account-details/confirm-account-details';
+import { combineLatest } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-confirm-account-details',
-  templateUrl: './confirm-account-details.component.html'
+  templateUrl: './confirm-account-details.component.html',
 })
-export class ConfirmAccountDetailsComponent implements OnInit {
-  constructor() {}
+export class ConfirmAccountDetailsComponent extends ConfirmAccountDetails {
+  protected actionType = 'Account creation';
+  protected flow = '/add-workplace';
 
-  ngOnInit() {}
+  constructor(
+    private backService: BackService,
+    private createAccountService: CreateAccountService,
+    private establishmentService: EstablishmentService,
+    private router: Router,
+    private workplaceService: WorkplaceService,
+    protected errorSummaryService: ErrorSummaryService,
+    protected formBuilder: FormBuilder
+  ) {
+    super(errorSummaryService, formBuilder);
+  }
+
+  protected init() {
+    this.setupSubscriptions();
+    this.setBackLink();
+  }
+
+  protected setupSubscriptions(): void {
+    this.subscriptions.add(
+      combineLatest(this.createAccountService.userDetails$, this.workplaceService.selectedWorkplaceService$).subscribe(
+        ([userDetails, workplaceService]) => {
+          this.userDetails = userDetails;
+          this.service = workplaceService;
+          this.setAccountDetails();
+        }
+      )
+    );
+  }
+
+  private setAccountDetails(): void {
+    this.userInfo = [
+      {
+        label: 'Full name',
+        data: this.userDetails.fullname,
+        route: { url: [`${this.flow}/change-your-details`] },
+      },
+      {
+        label: 'Job title',
+        data: this.userDetails.jobTitle,
+      },
+      {
+        label: 'Email address',
+        data: this.userDetails.email,
+      },
+      {
+        label: 'Contact phone',
+        data: this.userDetails.phone,
+      },
+    ];
+  }
+
+  protected setBackLink(): void {
+    this.backService.setBackLink({ url: [`${this.flow}/create-user-account`] });
+  }
+
+  public addWorkplace(): void {
+    this.subscriptions.add(
+      this.workplaceService
+        .addWorkplace(
+          this.establishmentService.primaryWorkplace.uid,
+          this.workplaceService.generateAddWorkplaceRequest(this.locationAddress, this.service)
+        )
+        .subscribe((response: any) => this.createAccount(response), (error: HttpErrorResponse) => this.onError(error))
+    );
+  }
+
+  private generateCreateAccountRequest(): CreateAccountRequest {
+    return {
+      email: this.userDetails.email,
+      fullname: this.userDetails.fullname,
+      jobTitle: this.userDetails.jobTitle,
+      phone: this.userDetails.phone,
+      role: Roles.Read,
+    };
+  }
+
+  private createAccount(establishmentUid: string) {
+    console.log('response', establishmentUid);
+
+    return this.createAccountService
+      .createAccount(establishmentUid, this.generateCreateAccountRequest())
+      .pipe(take(1))
+      .subscribe(
+        () => this.router.navigate([`${this.flow}/complete`]),
+        (error: HttpErrorResponse) => this.onError(error)
+      );
+  }
+
+  public onSetReturn(): void {
+    this.createAccountService.setReturnTo({
+      url: [`${this.flow}/confirm-account-details`],
+    });
+  }
 }
