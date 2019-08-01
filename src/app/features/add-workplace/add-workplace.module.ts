ffa3ac2c68ce0a2fe70fc4c2b3fe91a2111cf7de
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-01 14:03:01 +0100

    created WIP change your details component

--- src/app/features/add-workplace/add-workplace.module.ts
+++ src/app/features/add-workplace/add-workplace.module.ts
@@ -6,6 +6,7 @@ import { RegulatedByCqcComponent } from '@features/add-workplace/regulated-by-cq
 import { StartComponent } from '@features/add-workplace/start/start.component';
 import { SharedModule } from '@shared/shared.module';
 import { AddWorkplaceCompleteComponent } from './add-workplace-complete/add-workplace-complete.component';
+import { ChangeYourDetailsComponent } from './change-your-details/change-your-details.component';
 import { ConfirmWorkplaceDetailsComponent } from './confirm-workplace-details/confirm-workplace-details.component';
 import { CreateUserAccountComponent } from './create-user-account/create-user-account.component';
 import { EnterWorkplaceAddressComponent } from './enter-workplace-address/enter-workplace-address.component';
@@ -18,6 +19,7 @@ import { SelectWorkplaceComponent } from './select-workplace/select-workplace.co
   imports: [CommonModule, AddWorkplaceRoutingModule, ReactiveFormsModule, SharedModule],
   declarations: [
     AddWorkplaceCompleteComponent,
+    ChangeYourDetailsComponent,
     ConfirmWorkplaceDetailsComponent,
     CreateUserAccountComponent,
     EnterWorkplaceAddressComponent,
---
+++ src/app/features/add-workplace/change-your-details/change-your-details.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-your-details',
+  templateUrl: './change-your-details.component.html'
+})
+export class ChangeYourDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
