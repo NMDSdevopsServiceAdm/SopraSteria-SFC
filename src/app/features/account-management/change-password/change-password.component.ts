86e25061812e6afdd26cf9b953bab5c11f8733d4
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-15 11:27:02 +0100

    on successful password change route to account management page just like other account change sections

--- src/app/features/account-management/change-password/change-password.component.ts
+++ src/app/features/account-management/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserDetails } from '@core/model/userDetails.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { UserService } from '@core/services/user.service';
@@ -13,18 +14,21 @@ export class ChangePasswordComponent implements OnInit, OnDestroy {
   public userDetails: UserDetails;
   private subscriptions: Subscription = new Subscription();
 
-  constructor(private userService: UserService, private breadcrumbService: BreadcrumbService) {}
+  constructor(
+    private breadcrumbService: BreadcrumbService,
+    private router: Router,
+    private userService: UserService,
+  ) {}
 
   ngOnInit() {
     this.breadcrumbService.show();
-
     this.submitted = false;
-
     this.subscriptions.add(this.userService.loggedInUser$.subscribe(user => (this.userDetails = user)));
   }
 
   public onResetPasswordSuccess(): void {
     this.submitted = true;
+    this.router.navigate(['/account-management']);
   }
 
   ngOnDestroy() {
