ed46d91a9e0a8a9823b560071b5b3de01915544e
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-16 14:37:38 +0100

    convert to behaviour subject

--- src/app/core/services/permissions/permissions.service.ts
+++ src/app/core/services/permissions/permissions.service.ts
@@ -1,21 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PermissionsResponse } from '@core/model/permissions.model';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PermissionsService {
-  public permissions: { [key: string]: boolean };
+  public permissions$ = new BehaviorSubject<{ [key: string]: boolean }>(null);
 
   constructor(private http: HttpClient) {}
 
   public getPermissions(workplaceUid: string): Observable<PermissionsResponse> {
     return this.http.get<PermissionsResponse>(`/api/establishment/${workplaceUid}/permissions`).pipe(
       tap(response => {
-        this.permissions = response.permissions;
+        this.permissions$.next(response.permissions);
       })
     );
   }
