147925e1af8fbbdd0ef4261e6a83b057dd2eae3f
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-19 23:22:22 +0100

    added PermissionType string literal type

--- src/app/core/model/permissions.model.ts
+++ src/app/core/model/permissions.model.ts
@@ -25,3 +25,25 @@ export interface Permissions {
   canViewWorker?: boolean;
   canViewWorkerStatus?: boolean;
 }
+
+export type PermissionType =
+  | 'canAddEstablishment'
+  | 'canAddUser'
+  | 'canAddWorker'
+  | 'canBulkUpload'
+  | 'canChangePermissionsForSubsidiary'
+  | 'canDeleteEstablishment'
+  | 'canDeleteUser'
+  | 'canDeleteWorker'
+  | 'canEditUser'
+  | 'canEditWorker'
+  | 'canMoveWorker'
+  | 'canOrderEstablishment'
+  | 'canOrderWorker'
+  | 'canViewEstablishment'
+  | 'canViewKeyFacts'
+  | 'canViewLastUpdatedTime'
+  | 'canViewNotifications'
+  | 'canViewWdfReport'
+  | 'canViewWorker'
+  | 'canViewWorkerStatus';
