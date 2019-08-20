09510c89ec78cb15eda05b4362068d1c32556df9
tgreyuk <tgreyuk@gmail.com>
2019-08-20 13:13:34 +0100

    Implemented WDF Summary Report - Admin download

--- src/app/features/reports/pages/reports/reports.component.ts
+++ src/app/features/reports/pages/reports/reports.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { Establishment } from '@core/model/establishment.model';
+import { Roles } from '@core/model/roles.enum';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { EstablishmentService } from '@core/services/establishment.service';
+import { ReportService } from '@core/services/report.service';
+import { UserService } from '@core/services/user.service';
+import { saveAs } from 'file-saver';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,15 +15,38 @@ import { Subscription } from 'rxjs';
 })
 export class ReportsComponent implements OnInit, OnDestroy {
   public workplace: Establishment;
+  public isAdmin: boolean;
   private subscriptions: Subscription = new Subscription();
 
-  constructor(private establishmentService: EstablishmentService, private breadcrumbService: BreadcrumbService) {}
+  constructor(
+    private establishmentService: EstablishmentService,
+    private breadcrumbService: BreadcrumbService,
+    private reportsService: ReportService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.breadcrumbService.show(JourneyType.REPORTS);
-
+    this.isAdmin = [Roles.Admin].includes(this.userService.loggedInUser.role);
     this.subscriptions.add(
       this.establishmentService.establishment$.subscribe(workplace => (this.workplace = workplace))
+    );
+  }
+
+  public downloadWdfSummaryReport(event: Event) {
+    event.preventDefault();
+    this.subscriptions.add(
+      this.reportsService.getWdfSummaryReport().subscribe(
+        response => {
+          const filenameRegEx = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+          const header = response.headers.get('content-disposition');
+          const filenameMatches = header && header.match(filenameRegEx);
+          const filename = filenameMatches && filenameMatches.length > 1 ? filenameMatches[1] : null;
+          const blob = new Blob([response.body], { type: 'text/plain;charset=utf-8' });
+          saveAs(blob, filename);
+        },
+        () => {}
+      )
     );
   }
 
