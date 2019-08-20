75c681f87d4db411bff7d7504d671a03df38e922
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-20 15:44:36 +0100

    optimize permissions interface

--- src/app/core/model/permissions.model.ts
+++ src/app/core/model/permissions.model.ts
@@ -4,26 +4,7 @@ export interface PermissionsResponse {
 }
 
 export interface Permissions {
-  canAddEstablishment?: boolean;
-  canAddUser?: boolean;
-  canAddWorker?: boolean;
-  canBulkUpload?: boolean;
-  canChangePermissionsForSubsidiary?: boolean;
-  canDeleteEstablishment?: boolean;
-  canDeleteUser?: boolean;
-  canDeleteWorker?: boolean;
-  canEditUser?: boolean;
-  canEditWorker?: boolean;
-  canMoveWorker?: boolean;
-  canOrderEstablishment?: boolean;
-  canOrderWorker?: boolean;
-  canViewEstablishment?: boolean;
-  canViewKeyFacts?: boolean;
-  canViewLastUpdatedTime?: boolean;
-  canViewNotifications?: boolean;
-  canViewWdfReport?: boolean;
-  canViewWorker?: boolean;
-  canViewWorkerStatus?: boolean;
+  [key: string]: boolean;
 }
 
 export type PermissionType =
