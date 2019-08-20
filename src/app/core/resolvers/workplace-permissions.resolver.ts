127650a80d65f715f54a8718a498e8c76d406c1b
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-20 14:40:45 +0100

    cache permissions

--- src/app/core/resolvers/workplace-permissions.resolver.ts
+++ src/app/core/resolvers/workplace-permissions.resolver.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { PermissionsResponse } from '@core/model/permissions.model';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
 import { of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
@@ -9,13 +10,20 @@ export class WorkplacePermissionsResolver implements Resolve<any> {
   constructor(private permissionsService: PermissionsService) {}
 
   resolve(route: ActivatedRouteSnapshot) {
-    return this.permissionsService
-      .getPermissions(route.paramMap.get('establishmentuid'))
-      .pipe(tap(response => this.permissionsService.setPermissions(response)))
-      .pipe(
-        catchError(() => {
-          return of(null);
-        })
-      );
+    const establishmentUid: string = route.paramMap.get('establishmentuid');
+    const cachedPermissions: PermissionsResponse | null = this.permissionsService.filterPermissions(establishmentUid);
+
+    if (cachedPermissions) {
+      return cachedPermissions;
+    } else {
+      return this.permissionsService
+        .getPermissions(establishmentUid)
+        .pipe(tap(response => this.permissionsService.setPermissions(response)))
+        .pipe(
+          catchError(() => {
+            return of(null);
+          })
+        );
+    }
   }
 }
