b554291ee57143df44b9dca9adb98aa42e02aca4
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-20 10:45:38 +0100

    commit WIP

--- src/app/shared/components/user-accounts-summary/user-accounts-summary.component.ts
+++ src/app/shared/components/user-accounts-summary/user-accounts-summary.component.ts
@@ -22,16 +22,15 @@ export class UserAccountsSummaryComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.subscriptions.add(
-      this.userService
-        .getAllUsersForEstablishment(this.workplace.uid)
-        .subscribe(users => {
-          this.users = orderBy(
-            users,
-            ['status', 'isPrimary', 'role', (user: UserDetails) => user.fullname.toLowerCase()],
-            ['desc', 'desc', 'asc', 'asc']
-          );
-          this.canAddUser = this.permissionsService.permissions.canAddUser && this.userSlotsAvailable(users);
-        })
+      this.userService.getAllUsersForEstablishment(this.workplace.uid).subscribe(users => {
+        this.users = orderBy(
+          users,
+          ['status', 'isPrimary', 'role', (user: UserDetails) => user.fullname.toLowerCase()],
+          ['desc', 'desc', 'asc', 'asc']
+        );
+        this.canAddUser =
+          this.permissionsService.can(this.workplace.uid, 'canAddUser') && this.userSlotsAvailable(users);
+      })
     );
   }
 
