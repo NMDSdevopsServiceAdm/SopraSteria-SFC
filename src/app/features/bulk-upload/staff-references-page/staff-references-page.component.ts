7ac3db18db8ff0f0289ce2b05cdd853f69f47b4e
Charlie Francis <charliefrancis@gmail.com>
2019-08-22 09:56:49 +0100

    Including own workplace in filter

--- src/app/features/bulk-upload/staff-references-page/staff-references-page.component.ts
+++ src/app/features/bulk-upload/staff-references-page/staff-references-page.component.ts
@@ -51,8 +51,11 @@ export class StaffReferencesPageComponent extends BulkUploadReferences {
           this.establishmentUid = this.activatedRoute.snapshot.paramMap.get('uid');
           this.references = data.references;
           this.workPlaceReferences = _filter(data.workplaceReferences, reference => {
-            const toMatch = this.primaryWorkplace.isParent ? WorkplaceDataOwner.Parent : WorkplaceDataOwner.Workplace;
-            return reference.dataOwner === toMatch;
+            if (this.primaryWorkplace.isParent) {
+              return reference.dataOwner === WorkplaceDataOwner.Parent || reference.uid === this.primaryWorkplace.uid;
+            } else {
+              return reference.dataOwner === WorkplaceDataOwner.Workplace;
+            }
           });
           const establishment = _filter(this.workPlaceReferences, ['uid', this.establishmentUid])[0];
           this.establishmentName = establishment.name;
