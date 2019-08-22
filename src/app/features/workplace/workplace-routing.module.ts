0e89010ae032285064c4b36ba784c59bd1d7d845
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-22 11:55:38 +0100

    use CheckPermissionsGuard on workplace details route

--- src/app/features/workplace/workplace-routing.module.ts
+++ src/app/features/workplace/workplace-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditUserPermissionsGuard } from '@core/guards/edit-user-permissions/edit-user-permissions.guard';
 import { ParentGuard } from '@core/guards/parent/parent.guard';
-import { HasPermissionsGuard } from '@core/guards/permissions/has-permissions/has-permissions.guard';
+import { CheckPermissionsGuard } from '@core/guards/permissions/check-permissions/check-permissions.guard';
 import { RoleGuard } from '@core/guards/role/role.guard';
 import { Roles } from '@core/model/roles.enum';
 import { UserAccountResolver } from '@core/resolvers/user-account.resolver';
@@ -58,7 +58,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        canActivate: [HasPermissionsGuard],
+        canActivate: [CheckPermissionsGuard],
         component: ViewWorkplaceComponent,
         data: {
           permissions: ['canViewEstablishment'],
