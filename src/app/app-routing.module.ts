d821861d7c3a7182e6fe904eb28a837f6aee0b5f
Charlie Francis <charliefrancis@gmail.com>
2019-08-23 10:30:26 +0100

    Removing duplicate AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -75,7 +75,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    canActivate: [AuthGuard, HasPermissionsGuard],
+    canActivate: [HasPermissionsGuard],
     canActivateChild: [AuthGuard],
     resolve: {
       loggedInUser: LoggedInUserResolver,
