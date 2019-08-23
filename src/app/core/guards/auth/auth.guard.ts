22c59a55d76c9a32b89b4ea181969190f87595b0
Charlie Francis <charliefrancis@gmail.com>
2019-08-23 09:53:32 +0100

    Updating Auth Guard to return URL Tree

--- src/app/core/guards/auth/auth.guard.ts
+++ src/app/core/guards/auth/auth.guard.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 import { UserService } from '@core/services/user.service';
 
@@ -9,25 +16,24 @@ import { UserService } from '@core/services/user.service';
 export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private router: Router, private authService: AuthService, private userService: UserService) {}
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
     return this.checkLogin(state);
   }
 
-  canActivateChild(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivateChild(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
     return this.canActivate(next, state);
   }
 
-  private checkLogin(state: RouterStateSnapshot): boolean {
+  private checkLogin(state: RouterStateSnapshot): boolean | UrlTree {
     if (!this.authService.token) {
       this.authService.logoutWithoutRouting();
-      this.router.navigate(['/login']);
-      return false;
+      return this.router.createUrlTree(['/login']);
     }
 
     if (!this.authService.isAuthenticated()) {
       this.authService.storeRedirectLocation();
-      this.authService.logout();
-      return false;
+      this.authService.logoutWithoutRouting();
+      return this.router.createUrlTree(['/logged-out']);
     }
 
     return true;
