a57cd1cc33278762e1c5eb8d13c4b2a3d572a7ea
tgreyuk <tgreyuk@gmail.com>
2019-08-28 15:33:07 +0100

    Added enpoints for Local Authroity reportds

--- src/app/features/reports/pages/reports/reports.component.ts
+++ src/app/features/reports/pages/reports/reports.component.ts
@@ -1,9 +1,11 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { Establishment } from '@core/model/establishment.model';
 import { Roles } from '@core/model/roles.enum';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { EstablishmentService } from '@core/services/establishment.service';
+import { PermissionsService } from '@core/services/permissions/permissions.service';
 import { ReportService } from '@core/services/report.service';
 import { UserService } from '@core/services/user.service';
 import { saveAs } from 'file-saver';
@@ -16,6 +18,7 @@ import { take } from 'rxjs/operators';
 })
 export class ReportsComponent implements OnInit, OnDestroy {
   public workplace: Establishment;
+  public canRunLocalAuthorityReport: boolean;
   public isAdmin: boolean;
   public isParent: boolean;
   public isLocalAuthority: boolean;
@@ -26,7 +29,8 @@ export class ReportsComponent implements OnInit, OnDestroy {
     private establishmentService: EstablishmentService,
     private breadcrumbService: BreadcrumbService,
     private reportsService: ReportService,
-    private userService: UserService
+    private userService: UserService,
+    private permissionsService: PermissionsService
   ) {}
 
   ngOnInit() {
@@ -37,6 +41,8 @@ export class ReportsComponent implements OnInit, OnDestroy {
         this.workplace = workplace;
         this.isParent = workplace.isParent;
         this.isLocalAuthority = workplace.employerType && workplace.employerType.value.startsWith('Local Authority');
+        this.canRunLocalAuthorityReport =
+          this.isParent && this.permissionsService.can(workplace.uid, 'canRunLocalAuthorityReport');
       })
     );
   }
@@ -44,18 +50,33 @@ export class ReportsComponent implements OnInit, OnDestroy {
   public downloadWdfSummaryReport(event: Event) {
     event.preventDefault();
     this.subscriptions.add(
-      this.reportsService.getWdfSummaryReport().subscribe(
-        response => {
-          const filenameRegEx = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-          const header = response.headers.get('content-disposition');
-          const filenameMatches = header && header.match(filenameRegEx);
-          const filename = filenameMatches && filenameMatches.length > 1 ? filenameMatches[1] : null;
-          const blob = new Blob([response.body], { type: 'text/plain;charset=utf-8' });
-          saveAs(blob, filename);
-        },
-        () => {}
-      )
+      this.reportsService.getWdfSummaryReport().subscribe(response => this.saveFile(response), () => {})
     );
+  }
+
+  public downloadLocalAuthorityReport(event: Event) {
+    event.preventDefault();
+    this.subscriptions.add(
+      this.reportsService
+        .getLocalAuthorityReport(this.workplace.uid)
+        .subscribe(response => this.saveFile(response), () => {})
+    );
+  }
+
+  public downloadLocalAuthorityAdminReport(event: Event) {
+    event.preventDefault();
+    this.subscriptions.add(
+      this.reportsService.getLocalAuthorityAdminReport().subscribe(response => this.saveFile(response), () => {})
+    );
+  }
+
+  private saveFile(response: HttpResponse<Blob>) {
+    const filenameRegEx = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+    const header = response.headers.get('content-disposition');
+    const filenameMatches = header && header.match(filenameRegEx);
+    const filename = filenameMatches && filenameMatches.length > 1 ? filenameMatches[1] : null;
+    const blob = new Blob([response.body], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, filename);
   }
 
   ngOnDestroy(): void {
