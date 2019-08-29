f47463a28153458071a601e718ed94cd3525c719
Omar Mirza <omar.creative.dev@gmail.com>
2019-08-29 16:11:30 +0100

    update has permission guard to cater for Admin users

--- src/app/core/guards/permissions/has-permissions/has-permissions.guard.ts
+++ src/app/core/guards/permissions/has-permissions/has-permissions.guard.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Roles } from '@core/model/roles.enum';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { UserService } from '@core/services/user.service';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HasPermissionsGuard implements CanActivate {
-  constructor(private permissionsService: PermissionsService, private establishmentService: EstablishmentService) {}
+  constructor(
+    private permissionsService: PermissionsService,
+    private establishmentService: EstablishmentService,
+    private userService: UserService
+  ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     const notPrimaryWorkplace: string = route.paramMap.get('establishmentuid');
     const workplaceUid: string = notPrimaryWorkplace ? notPrimaryWorkplace : this.establishmentService.establishmentId;
 
-    if (!workplaceUid) {
+    if (this.userService.loggedInUser.role === Roles.Admin) {
       return of(true);
     }
 
