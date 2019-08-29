aea68913b71f41eda5d5a8e2d060ac45cfd8ee54
Charlie Francis <charliefrancis@gmail.com>
2019-08-29 11:51:22 +0100

    Reverting back to FE dev complete, awaiting BE work

--- src/app/features/reports/pages/workplaces/workplaces.component.ts
+++ src/app/features/reports/pages/workplaces/workplaces.component.ts
@@ -3,6 +3,7 @@ import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { GetWorkplacesResponse, Workplace } from '@core/model/my-workplaces.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { UserService } from '@core/services/user.service';
+import { filter, orderBy } from 'lodash';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -24,10 +25,10 @@ export class WorkplacesComponent implements OnInit {
       this.userService.getEstablishments(true).subscribe((workplaces: GetWorkplacesResponse) => {
         if (workplaces.subsidaries) {
           this.workplaces = workplaces.subsidaries.establishments; // TODO: Filter out Workplaces with no permissions
-          // this.workplaces = orderBy(this.workplaces, ['wdf.overall', 'updated'], ['asc', 'desc']);
+          this.workplaces = orderBy(this.workplaces, ['wdf.overall', 'updated'], ['asc', 'desc']);
 
-          // this.totalEligible = filter(this.workplaces, workplace => workplace.wdf.overall).length;
-          // this.totalIneligible = this.workplaces.length - this.totalEligible;
+          this.totalEligible = filter(this.workplaces, workplace => workplace.wdf.overall).length;
+          this.totalIneligible = this.workplaces.length - this.totalEligible;
         }
       })
     );
