7d1db62988e1dfe4c167bfb205c4061a89242646
Paul Humphreys <pjshumphreys@blueyonder.co.uk>
2019-09-05 16:05:23 +0100

    workplaces tab data is in the exported spreadsheet now

--- server/routes/reports/localAuthorityReport/user.js
+++ server/routes/reports/localAuthorityReport/user.js
@@ -12,7 +12,7 @@ const workplacesSheetName = 'xl/worksheets/sheet1.xml';
 const staffRecordsSheetName = 'xl/worksheets/sheet2.xml';
 const sharedStringsName = 'xl/sharedStrings.xml';
 const schema = 'http://schemas.openxmlformats.org/spreadsheetml/2006/main';
-const isNumberRegex = /^[0-9]+(\.[0-9]+)$/;
+const isNumberRegex = /^[0-9]+(\.[0-9]+)?$/;
 
 //XML DOM manipulation helper functions
 const { DOMParser, XMLSerializer } = new (require('jsdom').JSDOM)().window;
@@ -23,7 +23,7 @@ const parseXML = fileContent =>
 const serializeXML = dom =>
   '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
   (new XMLSerializer()).serializeToString(dom);
-  
+
 //helper function to set a spreadsheet cell's value
 const putStringTemplate = (
     sheetDoc,
@@ -36,7 +36,7 @@ const putStringTemplate = (
   const hasVTag = vTag !== null;
   const textValue = String(value);
   const isNumber = isNumberRegex.test(textValue);
-  
+
   if(!hasVTag) {
     vTag = sheetDoc.createElementNS(schema, 'v');
 
@@ -49,14 +49,14 @@ const putStringTemplate = (
   }
   else{
     element.setAttribute('t', 's');
-    
+
     const sst = stringsDoc.querySelector('sst');
     const si = stringsDoc.createElementNS(schema, 'si');
     const t = stringsDoc.createElementNS(schema, 't');
 
     sst.appendChild(si);
     si.appendChild(t);
-    
+
     t.textContent = textValue;
     vTag.textContent = sharedStringsUniqueCount[0];
 
@@ -134,24 +134,23 @@ const getReportData = async (date, thisEstablishment) => {
   // first run the report, which means running the `cqc.localAuthorityReport` function.
   // this function runs in a single transaction
   // the function returns true or false; encapsulating any SQL exceptions.
+  const params = {
+    replacements: {
+      givenEstablishmentId: establishmentId,
+      givenFromDate: fromDate,
+      givenToDate: toDate,
+    },
+    type: models.sequelize.QueryTypes.SELECT
+  };
 
-  // TODO: Confirm this stored procedure is what populated the tables for the report
+  console.log("params", params);
+
   const runReport = await models.sequelize.query(
     `select cqc.localAuthorityReport(:givenEstablishmentId, :givenFromDate, :givenToDate);`,
-    {
-      replacements: {
-        givenEstablishmentId: establishmentId,
-        givenFromDate: fromDate,
-        givenToDate: toDate,
-      },
-      type: models.sequelize.QueryTypes.SELECT
-    }
+    params
   );
 
-  // if report fails return no data
-  if(!runReport || !runReport[0].localauthorityreport) {
-    return null;
-  }
+  console.log("runReport", runReport);
 
   //Construct the model data object for the report
   const reportData = {
@@ -165,11 +164,19 @@ const getReportData = async (date, thisEstablishment) => {
     workers: []
   };
 
+  // if report fails return no data
+  if(!runReport || String(runReport[0].localauthorityreport) !== 'true') {
+    return null;
+  }
+
   // now grab the establishments and format the report data
   const reportEstablishments = await models.localAuthorityReportEstablishment.findAll({
     where: {
       establishmentFk: establishmentId
-    }
+    },
+    order: [
+      ['workplaceName', 'ASC'],
+    ],
   });
 
   if(reportEstablishments && Array.isArray(reportEstablishments)) {
@@ -178,16 +185,13 @@ const getReportData = async (date, thisEstablishment) => {
 
   // now grab the workers and format the report data
   const reportWorkers = await models.localAuthorityReportWorker.findAll({
-    include: [
-      {
-        model: models.localAuthorityReportEstablishment,
-        as: 'establishment',
-        attributes: [],
-        where: {
-          establishmentFk: establishmentId
-        }
-      }
-    ]
+    where: {
+      establishmentFk: establishmentId
+    },
+    order: [
+      ['workplaceName', 'ASC'],
+      ['localId', 'ASC'],
+    ],
   });
 
   if(reportWorkers && Array.isArray(reportWorkers)) {
@@ -204,21 +208,235 @@ const updateWorkplacesSheet = (
     sharedStringsUniqueCount
 ) => {
   const putString = putStringTemplate.bind(null, workplacesSheet, sharedStrings, sharedStringsUniqueCount);
-  
+
+  //set headers
   putString(
       workplacesSheet.querySelector("c[r='B5']"),
       moment(reportData.date).format("DD/MM/YYYY")
     );
-    
+
   putString(
       workplacesSheet.querySelector("c[r='B6']"),
       reportData.reportEstablishment.localAuthority
     );
-    
+
   putString(
       workplacesSheet.querySelector("c[r='B7']"),
       reportData.reportEstablishment.name
     );
+
+  // clone the row the apropriate number of times
+  const templateRow = workplacesSheet.querySelector("row[r='13']");
+  let currentRow = templateRow;
+  let rowIndex = 14;
+
+  if(reportData.establishments.length > 1) {
+    for(let i = 0; i < reportData.establishments.length-1; i++) {
+      const tempRow = templateRow.cloneNode(true);
+      
+      tempRow.setAttribute('r', rowIndex);
+      
+      tempRow.querySelectorAll('c').forEach(elem => {
+        elem.setAttribute('r', String(elem.getAttribute('r')).replace(/\d+$/, '') + rowIndex);
+      });
+      
+      templateRow.parentNode.insertBefore(tempRow, currentRow.nextSibling);
+      
+      currentRow = tempRow;
+      rowIndex++;
+    }
+  }
+  else if(reportData.establishments.length === 0) {
+    templateRow.remove();
+    rowIndex = 13;
+  }
+  
+  //fix the last row in the table
+  const lastChild = workplacesSheet.querySelector("sheetData row:last-child");
+  lastChild.setAttribute('r', rowIndex);
+  
+  //fix the dimensions tag value
+  const dimension = workplacesSheet.querySelector("dimension");
+  dimension.setAttribute('ref', String(dimension.getAttribute('ref')).replace(/\d+$/, "") + rowIndex);
+  
+  if(reportData.establishments.length) {
+    //update the cell values
+    for(let row = 0; row < reportData.establishments.length; row++){
+      for(let column = 0; column < 23; column++) {
+        const columnText = String.fromCharCode(column + 65);
+        
+        const cellToChange = workplacesSheet.querySelector(`c[r='${columnText}${row+13}']`);
+        
+        switch(columnText) {
+          case 'A': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].workplaceName
+              );
+          } break;
+
+          case 'B': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].workplaceId
+              );            
+          } break;
+
+          case 'C': {
+            putString(
+                cellToChange,
+                moment(reportData.establishments[row].lastUpdated).format("DD/MM/YYYY")
+              );            
+          } break;
+
+          case 'D': {
+           
+          } break;
+
+          case 'E': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].establishmentType
+              );            
+          } break;
+
+          case 'F': {
+             putString(
+                cellToChange,
+                reportData.establishments[row].mainService
+              );           
+          } break;
+
+          case 'G': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].serviceUserGroups
+              );            
+          } break;
+
+          case 'H': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].capacityOfMainService
+              );            
+          } break;
+
+          case 'I': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].utilisationOfMainService
+              );            
+          } break;
+
+          case 'J': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfVacancies
+              );            
+          } break;
+
+          case 'K': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfLeavers
+              );            
+          } break;
+
+          case 'L': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfStarters
+              );            
+          } break;
+
+          case 'M': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfStaffRecords
+              );            
+          } break;
+
+          case 'N': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfStaffRecordsNotAgency
+              );            
+          } break;
+
+          case 'O': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfAgencyStaffRecords
+              );            
+          } break;
+
+          case 'P': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].workplaceComplete? "Yes": "No"
+              );            
+          } break;
+
+          case 'Q': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfIndividualStaffRecords
+              );            
+          } break;
+
+          case 'R': {
+            putString(
+                cellToChange,
+                0
+              );            
+          } break;
+
+          case 'S': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfStaffRecordsNotAgency
+              );            
+          } break;
+
+          case 'T': {
+            putString(
+                cellToChange,
+                reportData.establishments[row]. numberOfCompleteStaffNotAgency
+              );            
+          } break;
+
+          case 'U': {
+            putString(
+                cellToChange,
+                0
+              );            
+          } break;
+
+          case 'V': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfAgencyStaffRecords
+              );            
+          } break;
+
+          case 'W': {
+            putString(
+                cellToChange,
+                reportData.establishments[row].numberOfCompleteAgencyStaffRecords
+              );            
+          } break;
+
+          case 'X': {
+            putString(
+                cellToChange,
+                0
+              );            
+          } break;          
+        }
+      }
+    }
+  }
+  
 
   return workplacesSheet;
 };
@@ -235,49 +453,75 @@ const updateStaffRecordsSheet = (
       staffRecordsSheet.querySelector("c[r='B5']"),
       moment(reportData.date).format("DD/MM/YYYY")
     );
-    
+
   putString(
       staffRecordsSheet.querySelector("c[r='B6']"),
       reportData.reportEstablishment.localAuthority
     );
-    
+
   putString(
       staffRecordsSheet.querySelector("c[r='B7']"),
       reportData.reportEstablishment.name
     );
+    
+    // clone the row the apropriate number of times
+  const templateRow = staffRecordsSheet.querySelector("row[r='11']");
+  let currentRow = templateRow;
+  let rowIndex = 13;
+  
+  if(reportData.establishments.length > 1) {
+    for(let i = 0; i < reportData.workers.length-1; i++) {
+      const tempRow = templateRow.cloneNode(true);
+      
+      tempRow.setAttribute('r', rowIndex);
+      
+      tempRow.querySelectorAll('c').forEach(elem => {
+        elem.setAttribute('r', String(elem.getAttribute('r')).replace(/\d+$/, '') + rowIndex);
+      });
+      
+      templateRow.parentNode.insertBefore(tempRow, currentRow.nextSibling);
+      
+      currentRow = tempRow;
+      rowIndex++;
+    }
+  }
+  else if(reportData.workers.length === 0) {
+    templateRow.remove();
+    rowIndex = 12;
+  }
+  
+  //fix the last row in the table
+  const lastChild = staffRecordsSheet.querySelector("sheetData row:last-child");
+  lastChild.setAttribute('r', rowIndex);
+  
+  //fix the dimensions tag value
+  const dimension = staffRecordsSheet.querySelector("dimension");
+  dimension.setAttribute('ref', String(dimension.getAttribute('ref')).replace(/\d+$/, "") + rowIndex);
 
   return staffRecordsSheet;
 }
 
 const getReport = async (date, thisEstablishment) => {
-  //const reportData = await getReportData(date, thisEstablishment);
+  let reportData = await getReportData(date, thisEstablishment);
 
-  //some test data
-  const reportData = {
-    date: date.toISOString(),
-    reportEstablishment: {
-      name: 'Establishment Name',
-      nmdsId: 'Establishment NMDS Id',
-      localAuthority: 'Local Authority'
-    },
-    establishments: [],
-    workers: []
-  }
+  console.log(JSON.stringify(reportData, null, 2));
 
   if(reportData === null) {
     return null;
   }
 
   return (new Promise(resolve => {
-      const walker = walk.walk(folderName);
+      const thePath = `${__dirname}/${folderName}`
+      const walker = walk.walk(thePath);
       const outputZip = new JsZip();
 
       let workplacesSheet, staffRecordsSheet, sharedStrings;
 
       walker.on("file", (root, fileStats, next) => {
-        const zipPath = (root === folderName ? '' : root.replace(/^[^\\\/]*[\\\/]?/, '') + '/') + fileStats.name;
+        const pathName = root.replace(thePath, '').replace(/^\//, "");
+        const zipPath = (pathName === '' ? '' : pathName + '/') + fileStats.name;
 
-        fs.readFile(`${folderName}/${zipPath}`, (err, fileContent) => {
+        fs.readFile(`${thePath}/${zipPath}`, (err, fileContent) => {
           if(!err) {
             switch(zipPath) {
               case workplacesSheetName: {
@@ -303,32 +547,34 @@ const getReport = async (date, thisEstablishment) => {
       });
 
       walker.on("end", () => {
-        const sst = sharedStrings.querySelector("sst");
-        
-        const sharedStringsUniqueCount = [parseInt(sst.getAttribute('uniqueCount'), 10)];;
+        if(sharedStrings) {
+          const sst = sharedStrings.querySelector("sst");
 
-        //update the workplaces sheet with the report data and add it to the zip
-        outputZip.file(workplacesSheetName, serializeXML(updateWorkplacesSheet(
-            workplacesSheet,
-            reportData,
-            sharedStrings,
-            sharedStringsUniqueCount  //pass unique count by reference rather than by value
-          )));
-          
-        //update the staff records sheet with the report data and add it to the zip
-        outputZip.file(staffRecordsSheetName, serializeXML(updateStaffRecordsSheet(
-            staffRecordsSheet,
-            reportData,
-            sharedStrings,
-            sharedStringsUniqueCount  //pass unique count by reference rather than by value
-          )));
-          
-        //update the shared strings counts we've been keeping track of
-        sst.setAttribute('uniqueCount', sharedStringsUniqueCount[0]);
- 
-        //add the updated shared strings to the zip
-        outputZip.file(sharedStringsName, serializeXML(sharedStrings));
-        
+          const sharedStringsUniqueCount = [parseInt(sst.getAttribute('uniqueCount'), 10)];;
+
+          //update the workplaces sheet with the report data and add it to the zip
+          outputZip.file(workplacesSheetName, serializeXML(updateWorkplacesSheet(
+              workplacesSheet,
+              reportData,
+              sharedStrings,
+              sharedStringsUniqueCount  //pass unique count by reference rather than by value
+            )));
+
+          //update the staff records sheet with the report data and add it to the zip
+          outputZip.file(staffRecordsSheetName, serializeXML(updateStaffRecordsSheet(
+              staffRecordsSheet,
+              reportData,
+              sharedStrings,
+              sharedStringsUniqueCount  //pass unique count by reference rather than by value
+            )));
+
+          //update the shared strings counts we've been keeping track of
+          sst.setAttribute('uniqueCount', sharedStringsUniqueCount[0]);
+
+          //add the updated shared strings to the zip
+          outputZip.file(sharedStringsName, serializeXML(sharedStrings));
+        }
+
         resolve(outputZip);
       });
     })).
@@ -339,21 +585,20 @@ const getReport = async (date, thisEstablishment) => {
     }));
 };
 
-//*
+/*
 (async () => {
   const report = await getReport(new Date(), {});
   const filename = 'output.xlsx';
   fs.createWriteStream(filename).end(report);
   console.log(`${filename} generated!`);
 })();
-
-//*/
+/*/
 
 // gets report
 // NOTE - the Local Authority report is driven mainly by pgsql (postgres functions) and therefore does not
 //    pass through the Establishment/Worker entities. This is done for performance, as these reports
 //    are expected to operate across large sets of data
-/*
+//*
 const express = require('express');
 const router = express.Router();
 
@@ -423,14 +668,15 @@ router.route('/').get(async (req, res) => {
         if(report) {
           res.setHeader('Content-disposition',
             `attachment; filename=${moment(date).format("YYYY-MM-DD")}-SFC-Local-Authority-Report.xlsx`);
+<<<<<<< HEAD
 >>>>>>> added code to produce an xlsx spreadsheet
 
+=======
+>>>>>>> workplaces tab data is in the exported spreadsheet now
           res.setHeader('Content-Type',
             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-
           res.setHeader('Content-Length', report.length);
 
-          // the data is already BASE64 encoded
           return res.status(200).end(report);
         }
         else {
