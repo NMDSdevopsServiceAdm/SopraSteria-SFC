2f1159859900f4bee5d55a074d878e63d2a5aa7a
Omar Mirza <omar.creative.dev@gmail.com>
2019-09-17 14:23:05 +0100

    remove mock data, invoke real api call

--- src/app/core/services/notifications/notifications.service.ts
+++ src/app/core/services/notifications/notifications.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Notification, NotificationSummary } from '@core/model/notifications.model';
 import { BehaviorSubject, of } from 'rxjs';
@@ -8,16 +9,10 @@ import { BehaviorSubject, of } from 'rxjs';
 export class NotificationsService {
   public notifications$: BehaviorSubject<NotificationSummary[]> = new BehaviorSubject(null);
   public activeNotification$: BehaviorSubject<Notification> = new BehaviorSubject(null);
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getAllNotifications() {
-    return of([
-      { notificationUid: '1', type: 'Change data owner', isViewed: false, created: '2019-08-21T15:20:53.205Z' },
-      { notificationUid: '2', type: 'Request to become a parent', isViewed: false, created: '2019-08-18T15:20:53.205Z' },
-      { notificationUid: '3', type: 'Remove parent link', isViewed: true, created: '2019-08-13T15:20:53.205Z' },
-      { notificationUid: '4', type: 'Changes to Terms and Conditions', isViewed: false, created: '2019-07-13T15:20:53.205Z' },
-      { notificationUid: '6', type: 'New workplace application', isViewed: true, created: '2019-03-13T15:20:53.205Z' },
-    ] as NotificationSummary[]);
+    return this.http.get<NotificationSummary[]>('/api/user/my/notifications');
   }
 
   getNotification(workplaceUid: string, notificationUid) {
