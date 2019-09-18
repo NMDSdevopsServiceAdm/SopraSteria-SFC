9369493c0059577ec5c664c391c9e31a69d09950
Omar Mirza <omar.creative.dev@gmail.com>
2019-09-18 07:14:34 +0100

    bugfix | correct workplaces filtering (#1466)

--- src/app/features/reports/pages/workplaces/workplaces.component.ts
+++ src/app/features/reports/pages/workplaces/workplaces.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
-import { GetWorkplacesResponse, Workplace } from '@core/model/my-workplaces.model';
+import { DataPermissions, GetWorkplacesResponse, Workplace } from '@core/model/my-workplaces.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { UserService } from '@core/services/user.service';
 import { filter, orderBy } from 'lodash';
@@ -24,7 +24,13 @@ export class WorkplacesComponent implements OnInit {
     this.subscriptions.add(
       this.userService.getEstablishments(true).subscribe((workplaces: GetWorkplacesResponse) => {
         if (workplaces.subsidaries) {
-          this.workplaces = workplaces.subsidaries.establishments; // TODO: Filter out Workplaces with no permissions
+          this.workplaces = workplaces.subsidaries.establishments;
+
+          // Filter out Workplaces with no permissions
+          if (this.workplaces.length) {
+            this.workplaces = filter(this.workplaces, workplace => workplace.dataPermissions !== DataPermissions.None);
+          }
+
           this.workplaces = orderBy(this.workplaces, ['wdf.overall', 'updated'], ['asc', 'desc']);
 
           this.totalEligible = filter(this.workplaces, workplace => workplace.wdf.overall).length;
