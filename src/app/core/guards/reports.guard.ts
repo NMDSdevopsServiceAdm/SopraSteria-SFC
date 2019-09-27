a309e5a7cc621e8dbe0cc7003ce807dbde8c09e3
Omar Mirza <omar.creative.dev@gmail.com>
2019-09-27 13:47:10 +0100

    update reports guard based on primary establishment existance rather than looking at route params

--- src/app/core/guards/reports.guard.ts
+++ src/app/core/guards/reports.guard.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { EstablishmentService } from '@core/services/establishment.service';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { UserService } from '@core/services/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReportsGuard implements CanActivate {
-  constructor(private permissionsService: PermissionsService) {}
+  constructor(
+    private permissionsService: PermissionsService,
+    private establishmentService: EstablishmentService,
+    private router: Router
+  ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const workplaceUid = route.paramMap.get('establishmentuid');
-    return (
-      this.permissionsService.can(workplaceUid, 'canViewWdfReport') ||
-      this.permissionsService.can(workplaceUid, 'canRunLocalAuthorityReport')
-    );
+    const workplace = this.establishmentService.primaryWorkplace;
+    if (workplace) {
+      return (
+        this.permissionsService.can(workplace.uid, 'canViewWdfReport') ||
+        this.permissionsService.can(workplace.uid, 'canRunLocalAuthorityReport')
+      );
+    } else {
+      this.router.navigate(['/dashboard']);
+      return false;
+    }
   }
 }
