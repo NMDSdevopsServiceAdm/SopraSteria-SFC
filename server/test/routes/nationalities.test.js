2959f0a2f9f0a95735a4c593245767336579236e
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2019-10-03 13:12:59 +0100

    Added country endpoint test

--- server/test/routes/nationalities.test.js
+++ server/test/routes/nationalities.test.js
@@ -1,15 +1,17 @@
 const request = require('supertest');
 
-var app = require('../../server');
+var app = require('../../../server');
 
 describe('Nationalities', function() {
-  describe('GET /nationalities', function() {
+  describe('GET /nationality', function() {
+    // Check to make sure that the reply has the correct JSON header
     it('responds with json', function(done) {
       request(app)
         .get('/api/nationality')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/, done);
     });
+    // Check to make sure that the status is correct
     it('responds with status 200', function(done) {
       request(app)
         .get('/api/nationality')
