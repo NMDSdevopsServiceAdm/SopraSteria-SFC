d6d1d05a5e925b97526e31647482b1960c2b1111
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2019-10-10 14:23:26 +0100

    Added approval endpoint and added ability to approve a registration

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -6,7 +6,7 @@ const isAdmin = require('../../utils/security/isAuthenticated').isAdmin;
 const search = require('./search');
 const recalcWdf = require('./recalcWdf');
 const registrations = require('./registrations');
-
+const approval = require('./approval');
 
 // middleware authentication - only role=Admin from here on in
 router.use('/', isAdmin);
@@ -14,6 +14,7 @@ router.use('/', isAdmin);
 router.use('/search', search);
 router.use('/recalcWdf', recalcWdf);
 router.use('/registrations', registrations);
+router.use('/approval', approval);
 
 router.route('/').post(async function (req, res) {
   return res.status(200).send({success: "from admin"});
