b630f1027ca9996b20f848b2818a6dae1e825d89
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2019-10-10 12:05:39 +0100

    Added list of pending registrations to search admin page

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -5,14 +5,41 @@ import { AuthService } from '@core/services/auth.service';
 import { BackService } from '@core/services/back.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 
+export interface Registrations {
+  [index: number]: {
+    name: string;
+    username: string;
+    securityQuestion: string;
+    securityQuestionAnswer: string;
+    email: string;
+    phone: string;
+    created: string;
+    establishment: {
+      name: string;
+      isRegulated: boolean;
+      nmdsId: string;
+      address: string;
+      address2: string;
+      address3: string;
+      postcode: string;
+      town: string;
+      county: string;
+      locationId: string;
+      provid: string;
+      mainService: number;
+    };
+  };
+}
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
 })
 export class SearchComponent implements OnInit {
   public results = [];
+  public registrations = [];
   public form = {
     type: '',
     title: '',
@@ -46,14 +73,31 @@ export class SearchComponent implements OnInit {
       this.form.subTitle = 'User Search';
       this.form.title = 'Define your search criteria';
       this.form.buttonText = 'Search Users';
-    } else {
+    } else if (this.router.url === '/search-establishments') {
       this.form.type = 'establishments';
       this.form.usernameLabel = 'Postcode';
       this.form.nameLabel = 'NDMS ID';
       this.form.subTitle = 'Establishment Search';
       this.form.title = 'Define your search criteria';
       this.form.buttonText = 'Search Establishments';
+    } else {
+      this.form.type = 'registrations';
     }
+    this.getRegistrations().subscribe(
+      data => {
+        this.registrations = data;
+      },
+      error => this.onError(error)
+    );
+  }
+
+  public getRegistrations(): Observable<Registrations[]> {
+    return this.http.get<Registrations[]>('/api/admin/registrations/');
+  }
+
+  public userApproval(data: object) {
+    console.log('We are about to query the API');
+    return this.http.post<any>('/api/admin/approval/', data);
   }
 
   public searchType(data, type) {
@@ -134,5 +178,15 @@ export class SearchComponent implements OnInit {
 
   protected setBackLink(): void {
     this.backService.setBackLink({ url: ['/dashboard'] });
+  }
+
+  public approveUser(username: string, approved: boolean, index: number) {
+    console.log('approveUser()');
+    const data = {
+      username,
+      approve: approved,
+    };
+    this.registrations.splice(index, 1);
+    this.userApproval(data).subscribe();
   }
 }
