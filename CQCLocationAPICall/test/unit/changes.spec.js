9e17f01ed7d548f1e92edf0d609dffa67b477496
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2019-11-14 15:59:06 +0000

    Cleaned up changes spec and added update test

--- CQCLocationAPICall/test/unit/changes.spec.js
+++ CQCLocationAPICall/test/unit/changes.spec.js
@@ -1,4 +1,3 @@
-const SequelizeMock = require('sequelize-mock');
 const AWS = require('aws-sdk-mock');
 const expect = require('chai').expect;
 const fs = require('fs');
@@ -13,17 +12,11 @@ const models = require('../../models/index');
 describe('changes.js', () => {
   before(async () => {
     const mock = new MockAdapter(axios);
-    const startTimestamp = '2019-10-30T18:20:11Z';
-    sinon.stub(models.cqclog, 'findAll').returns({
-      success: true,
-      message: 'Call Successful',
-      createdAt: '2019-10-31 05:12:20.736+00',
-      lastUpdatedAt: startTimestamp
-    });
+    sinon.stub(models.cqclog, 'findAll');
 
     mock.onAny().reply(200, {
       changes: [
-        '1-108946357',
+        '1-125856395',
         '1-110299837',
         '1-1134752815',
         '1-114347996',
@@ -39,7 +32,9 @@ describe('changes.js', () => {
     AWS.mock('SQS', 'sendMessage', (params, callback) => {
       callback(null, 'successfully received message');
     });
-    AWS.mock('S3', 'getObject', Buffer.from(fs.readFileSync('test/mockdata/s3.json')));
+    AWS.mock('S3', 'getObject', (params, callback) => {
+      callback(null, { Body: Buffer.from(fs.readFileSync('test/mockdata/s3.json'))});
+    });
     AWS.mock('S3', 'putObject', (params, callback) => {
       expect(params).to.have.property('Bucket', appConfig.get('aws.bucketname').toString());
       expect(params).to.have.property('Key');
@@ -59,6 +54,5 @@ describe('changes.js', () => {
     const changesTest = await changes.handler(null, null);
     expect(changesTest.status).to.equal(200);
     expect(changesTest.body).to.equal('Call Successful');
-    console.log(test.callCount);
   });
 });
