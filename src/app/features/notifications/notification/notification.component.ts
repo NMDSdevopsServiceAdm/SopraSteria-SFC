112cf9a1dc3f737fddf100988552551aa42a9ed9
Anurag Negi <anurag.negi@soprasteria.com>
2019-11-19 10:57:32 +0530

    Added error handling to few files

--- src/app/features/notifications/notification/notification.component.ts
+++ src/app/features/notifications/notification/notification.component.ts
@@ -126,12 +126,14 @@ export class NotificationComponent implements OnInit, OnDestroy {
     this.subscriptions.add(
       this.notificationsService.setNoticationViewed(notificationUid).subscribe(
         resp => {
-          this.notificationsService.notifications.forEach((notification, i) => {
-            if (notification.notificationUid === resp.notificationUid) {
-              this.notificationsService.notifications[i] = resp;
-            }
-          });
-          this.notificationsService.notifications$.next(this.notificationsService.notifications);
+          if (resp) {
+            this.notificationsService.notifications.forEach((notification, i) => {
+              if (notification.notificationUid === resp.notificationUid) {
+                this.notificationsService.notifications[i] = resp;
+              }
+            });
+            this.notificationsService.notifications$.next(this.notificationsService.notifications);
+          }
         },
         error => console.log('Could not update notification.')
       )
