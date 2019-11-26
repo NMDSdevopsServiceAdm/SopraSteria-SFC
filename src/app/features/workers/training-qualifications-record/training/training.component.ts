fa5309749a20853a560183034b37a0fe14cb1216
Anurag Negi <55235060+Negi-Anurag@users.noreply.github.com>
2019-11-26 13:14:51 +0530

    Training and Qualifications - Staff Record Summary (#1712)

--- src/app/features/workers/training-qualifications-record/training/training.component.ts
+++ src/app/features/workers/training-qualifications-record/training/training.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Establishment } from '@core/model/establishment.model';
 import { TrainingRecord } from '@core/model/training.model';
 import { Worker } from '@core/model/worker.model';
+import { Router } from '@angular/router';
 import { DialogService } from '@core/services/dialog.service';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
 import { WorkerService } from '@core/services/worker.service';
