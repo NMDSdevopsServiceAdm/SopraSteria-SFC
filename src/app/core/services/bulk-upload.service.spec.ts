12233ec4bf51db075a0f71c8791e6a7c3b8fd06e
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2019-11-26 17:36:45 +0000

    Added some mock objects and added more to tests

--- src/app/core/services/bulk-upload.service.spec.ts
+++ src/app/core/services/bulk-upload.service.spec.ts
@@ -41,16 +41,38 @@ describe('BulkUploadService', () => {
           },
         ],
       };
+      const mockPresignedUrls = [
+        {
+          filename: 'workers.csv',
+          signedUrl:
+            'https://sfcbulkuploadfiles.s3.eu-west-2.amazonaws.com/479/latest/workers.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVJZVYRR3Y57EFIOC%2F20191125%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20191125T154950Z&X-Amz-Expires=300&X-Amz-Signature=405cbbe79b512afee1010a939ada7034b0b04642ae3fbffbb6e292e93768811b&X-Amz-SignedHeaders=host%3Bx-amz-meta-establishmentid%3Bx-amz-meta-username%3Bx-amz-meta-validationstatus&x-amz-meta-establishmentid=479&x-amz-meta-username=aylingw&x-amz-meta-validationstatus=pending',
+        },
+        {
+          filename: 'establishments.csv',
+          signedUrl:
+            'https://sfcbulkuploadfiles.s3.eu-west-2.amazonaws.com/479/latest/establishments.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVJZVYRR3Y57EFIOC%2F20191125%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20191125T154950Z&X-Amz-Expires=300&X-Amz-Signature=98880e9cba7b28eb9c090dfc42475eb06b097952450e34f6980d78cfb1e723ac&X-Amz-SignedHeaders=host%3Bx-amz-meta-establishmentid%3Bx-amz-meta-username%3Bx-amz-meta-validationstatus&x-amz-meta-establishmentid=479&x-amz-meta-username=aylingw&x-amz-meta-validationstatus=pending',
+        },
+      ];
+      const mockLock = {
+        message: 'Lock for establishment 479 acquired.',
+        requestId: 'ec67d8f8-5d05-40bb-952d-ed7f7e5fdbf0',
+      };
 
       service.getPresignedUrls(mockPresignedFiles).subscribe(presignedUrls => {
-        expect(presignedUrls).toEqual();
+        expect(presignedUrls).toEqual(mockPresignedUrls);
       });
 
-      const req = httpTestingController.expectOne('http://localhost:8089/topics/1/courses');
+      const lockReq = httpTestingController.expectOne(
+        `/api/establishment/${this.establishmentService.establishmentId}/bulkupload/uploaded`
+      );
+      const presignedUrlReq = httpTestingController.expectOne(
+        `/api/establishment/${this.establishmentService.establishmentId}/bulkupload/uploaded`
+      );
+      expect(lockReq.request.method).toEqual('POST');
+      expect(presignedUrlReq.request.method).toEqual('GET');
 
-      expect(req.request.method).toEqual('POST');
-
-      req.flush(mockCourse);
+      lockReq.flush(mockLock);
+      presignedUrlReq.flush(mockPresignedUrls);
     });
   });
 });
