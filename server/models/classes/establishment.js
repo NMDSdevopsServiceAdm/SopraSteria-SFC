46eaff5d081ed001b7410fae035ef9d4856153b5
Anurag Negi <anurag.negi@soprasteria.com>
2019-12-03 17:14:40 +0530

    Added space

--- server/models/classes/establishment.js
+++ server/models/classes/establishment.js
@@ -1995,7 +1995,7 @@ class Establishment extends EntityValidator {
             parentName: parentsAndPostcodeDetails[i].NameValue,
             postcode: parentsAndPostcodeDetails[i].postcode,
             uid: parentsAndPostcodeDetails[i].uid,
-            parentNameAndPostalcode: `${parentsAndPostcodeDetails[i].NameValue},${parentsAndPostcodeDetails[i].postcode}`
+            parentNameAndPostalcode: `${parentsAndPostcodeDetails[i].NameValue}, ${parentsAndPostcodeDetails[i].postcode}`
           });
         }
         return parentPostcodeDetailsArr;
