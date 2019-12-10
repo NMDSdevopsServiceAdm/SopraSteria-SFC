94b448460c2ae794f1b6ea02e3d990e80529702f
Anurag Negi <anurag.negi@soprasteria.com>
2019-12-10 11:43:25 +0530

    CANCEL LINK TO PARENT

--- server/data/linkToParent.js
+++ server/data/linkToParent.js
@@ -56,10 +56,10 @@ exports.checkAlreadyRequestedLinkToParent = async params =>
   });
 
 const getRecipientUserDetailsQuery = `
-select "UserUID" from cqc."Establishment" est
-LEFT JOIN cqc."Establishment" parent ON parent."EstablishmentID" = est."ParentID"
-JOIN cqc."User" individual ON individual."EstablishmentID" = COALESCE(parent."EstablishmentID", est."EstablishmentID")
-WHERE :estID = est."EstablishmentUID" AND individual."UserRoleValue" = :userRole AND est."IsParent" = :isParent `;
+  select "UserUID" from cqc."Establishment" est
+  LEFT JOIN cqc."Establishment" parent ON parent."EstablishmentID" = est."ParentID"
+  JOIN cqc."User" individual ON individual."EstablishmentID" = COALESCE(parent."EstablishmentID", est."EstablishmentID")
+  WHERE :estID = est."EstablishmentUID" AND individual."UserRoleValue" = :userRole AND est."IsParent" = :isParent `;
 
 exports.getRecipientUserDetails = async params =>
   db.query(getRecipientUserDetailsQuery, {
@@ -67,6 +67,62 @@ exports.getRecipientUserDetails = async params =>
       estID: params.parentWorkplaceUId,
       userRole: 'Edit',
       isParent: true,
+    },
+    type: db.QueryTypes.SELECT,
+  });
+
+const getLinkToParentDetailsQuery = `
+  SELECT "LinkToParentUID", "SubEstablishmentID", "ApprovalStatus", "CreatedByUserUID"
+  FROM cqc."LinkToParent"
+  WHERE "SubEstablishmentID" = :subEstId ORDER BY "created" DESC LIMIT :limit;
+  `;
+exports.getLinkToParentDetails = async params =>
+  db.query(getLinkToParentDetailsQuery, {
+    replacements: {
+      subEstId: params.subEstablishmentId,
+      limit: params.limit,
+    },
+    type: db.QueryTypes.SELECT,
+  });
+
+const getLinkToParentUidQuery = `
+  select "LinkToParentUID" from cqc."LinkToParent" where
+  "SubEstablishmentID" = :estID AND "ApprovalStatus" = :approvalStatus`;
+
+exports.getLinkToParentUid = async params =>
+  db.query(getLinkToParentUidQuery, {
+    replacements: {
+      estID: params.establishmentId,
+      approvalStatus: 'REQUESTED',
+    },
+    type: db.QueryTypes.SELECT,
+  });
+
+const cancelLinkToParentQuery = `
+  UPDATE cqc."LinkToParent"
+SET "ApprovalStatus" = :approvalStatus
+WHERE "LinkToParentUID" = :uid;`;
+
+exports.cancelLinkToParent = async params =>
+  db.query(cancelLinkToParentQuery, {
+    replacements: {
+      uid: params.linkToParentUid,
+      approvalStatus: params.approvalStatus,
+    },
+    type: db.QueryTypes.UPDATE,
+  });
+
+const updateLinkToParentQuery = `
+  select "LinkToParentUID", "ApprovalStatus", "PermissionRequest",
+  parent."NameValue" as "requstedParentName"
+  from cqc."LinkToParent" as sub
+  JOIN cqc."Establishment" as parent on sub."ParentEstablishmentID" =  parent."EstablishmentID"
+  where "LinkToParentUID" = :uid `;
+
+exports.updateLinkToParent = async params =>
+  db.query(updateLinkToParentQuery, {
+    replacements: {
+      uid: params.linkToParentUid,
     },
     type: db.QueryTypes.SELECT,
   });
