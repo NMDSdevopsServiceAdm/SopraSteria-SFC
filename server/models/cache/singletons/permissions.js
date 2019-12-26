1ba6d567b908e2f2efa7f098722dec980daff9a6
Anurag Negi <55235060+Negi-Anurag@users.noreply.github.com>
2019-12-26 14:42:25 +0530

    BE NEW PERMISSION CHECK (#1823)

--- server/models/cache/singletons/permissions.js
+++ server/models/cache/singletons/permissions.js
@@ -254,7 +254,7 @@ let ALL_PERMISSIONS = [
     description: 'Remove Parent Association',
     role: ['Edit'],
     subOwnedByWorkplaceAccessByParent: [],
-    subOwnedByParentAccessBySub: ['Workplace and Staff', 'Workplace'],
+    subOwnedByParentAccessBySub: ['Workplace and Staff', 'Workplace', 'None'],
     isAdmin: false,
   }
 ];
