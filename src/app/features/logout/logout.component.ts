9f51780643c8797b424457be4dc40c0c8b5d6e7d
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2019-12-27 11:21:59 +0000

    Added logout call to logged out component and to auth gaurd

--- src/app/features/logout/logout.component.ts
+++ src/app/features/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthService } from '@core/services/auth.service';
 import { IdleService } from '@core/services/idle.service';
 
@@ -7,7 +8,15 @@ import { IdleService } from '@core/services/idle.service';
   templateUrl: './logout.component.html',
 })
 export class LogoutComponent {
+  private jwt = new JwtHelperService();
+
   constructor(private idleService: IdleService, private authService: AuthService) {
+    if (this.authService.token) {
+      console.log('Checking token in logged out page');
+      if (this.jwt.isTokenExpired(this.authService.token)) {
+        this.authService.logout();
+      }
+    }
     this.authService.logoutWithoutRouting();
     this.idleService.clear();
   }
