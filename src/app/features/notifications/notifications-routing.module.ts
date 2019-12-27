065c6dfebb894fd43a4080daf3c717d767b41156
mksrivastava <manish-kumar.srivastava@soprasteria.com>
2019-12-27 12:12:20 +0000

    Wok in progress

--- src/app/features/notifications/notifications-routing.module.ts
+++ src/app/features/notifications/notifications-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NotificationCancelledComponent } from './notification-cancelled/notification-cancelled.component';
+import {
+  NotificationDeLinkToParentComponent,
+} from './notification-delink-to-parent/notification-delink-to-parent.component';
 import { NotificationLinkToParentComponent } from './notification-link-to-parent/notification-link-to-parent.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
 import { NotificationComponent } from './notification/notification.component';
@@ -25,7 +28,7 @@ const routes: Routes = [
   },
   {
     path: 'notification-delink-to-parent/:notificationuid',
-    component: NotificationLinkToParentComponent,
+    component: NotificationDeLinkToParentComponent,
   },
 ];
 
