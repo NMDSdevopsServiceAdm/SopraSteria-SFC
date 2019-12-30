fdfdbfe4d1fd1eeb5286ee8a2f5d30fd3714252e
rchitranshi <53287302+rchitranshi@users.noreply.github.com>
2019-12-30 15:48:10 +0530

    Revert "Completed with Training report (Including Polling from BE) (#1827)"
    This reverts commit a1b63856c36bd59bd566af7e9252d64f56972490.

--- src/app/shared/components/training-and-qualifications-tab/training-and-qualifications-tab.component.ts
+++ src/app/shared/components/training-and-qualifications-tab/training-and-qualifications-tab.component.ts
@@ -1,9 +1,7 @@
-import { HttpResponse } from '@angular/common/http';
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Establishment } from '@core/model/establishment.model';
 import { Worker } from '@core/model/worker.model';
 import { EstablishmentService } from '@core/services/establishment.service';
-import { ReportService } from '@core/services/report.service';
 import { WorkerService } from '@core/services/worker.service';
 import { Subscription } from 'rxjs';
 
@@ -20,11 +18,7 @@ export class TrainingAndQualificationsTabComponent implements OnInit, OnDestroy 
   public totalExpiredTraining;
   public totalExpiringTraining;
   public totalStaff: number;
-  constructor(
-    private workerService: WorkerService,
-    private establishmentService: EstablishmentService,
-    private reportService: ReportService
-  ) {}
+  constructor(private workerService: WorkerService, private establishmentService: EstablishmentService) {}
 
   ngOnInit() {
     this.subscriptions.add(
@@ -45,25 +39,6 @@ export class TrainingAndQualificationsTabComponent implements OnInit, OnDestroy 
         }
       )
     );
-  }
-  //Download Training Report
-  public downloadTrainingReport(event: Event) {
-    event.preventDefault();
-    this.subscriptions.add(
-      this.reportService.getTrainingReport(this.workplace.uid).subscribe(
-        response => this.saveFile(response),
-        () => {}
-      )
-    );
-  }
-  //set content type and save file
-  private saveFile(response: HttpResponse<Blob>) {
-    const filenameRegEx = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-    const header = response.headers.get('content-disposition');
-    const filenameMatches = header && header.match(filenameRegEx);
-    const filename = filenameMatches && filenameMatches.length > 1 ? filenameMatches[1] : null;
-    const blob = new Blob([response.body], { type: 'text/plain;charset=utf-8' });
-    saveAs(blob, filename);
   }
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
