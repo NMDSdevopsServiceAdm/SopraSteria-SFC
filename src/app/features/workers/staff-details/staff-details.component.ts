d870a1e6f8210e1c87cbaca4eb8ce10b78e481c3
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-01-16 12:57:33 +0000

    Added fix to remove registered nurse and nurse specialism from FE

--- src/app/features/workers/staff-details/staff-details.component.ts
+++ src/app/features/workers/staff-details/staff-details.component.ts
@@ -125,6 +125,13 @@ export class StaffDetailsComponent extends QuestionComponent implements OnInit, 
       },
     };
 
+    if (parseInt(mainJob.value, 10) !== 23) {
+      this.worker.registeredNurse = null;
+      if (this.worker.nurseSpecialism) {
+        this.worker.nurseSpecialism.specialism = null;
+      }
+    }
+
     // TODO: Removing Other Jobs should be handled by the Server
     // https://trello.com/c/x3N7dQJP
     if (this.worker && this.worker.otherJobs) {
