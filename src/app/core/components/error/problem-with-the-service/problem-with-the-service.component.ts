2f2f8ccac2e04b6275408e002de5e119467aa5ca
mksrivastava <manish-kumar.srivastava@soprasteria.com>
2020-01-21 16:45:59 +0530

    Initial commit.

--- src/app/core/components/error/problem-with-the-service/problem-with-the-service.component.ts
+++ src/app/core/components/error/problem-with-the-service/problem-with-the-service.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-problem-with-the-service',
   templateUrl: './problem-with-the-service.component.html',
 })
 export class ProblemWithTheServiceComponent {
-  constructor() {}
+  constructor(private router: Router ) {}
+
+
+  ngOnInit() {
+    console.log(this.router);
+  }
 }
