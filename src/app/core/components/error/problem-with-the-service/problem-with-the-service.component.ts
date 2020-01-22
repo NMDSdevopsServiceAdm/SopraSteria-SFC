7435d40d4e2c5dd807337ce1d2a801d4af62d0d0
mksrivastava <manish-kumar.srivastava@soprasteria.com>
2020-01-22 11:01:25 +0530

    Basic commit

--- src/app/core/components/error/problem-with-the-service/problem-with-the-service.component.ts
+++ src/app/core/components/error/problem-with-the-service/problem-with-the-service.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/pairwise';
+
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-problem-with-the-service',
   templateUrl: './problem-with-the-service.component.html',
 })
-export class ProblemWithTheServiceComponent {
-  constructor(private router: Router ) {}
-
-
+export class ProblemWithTheServiceComponent implements OnInit {
+  constructor(private router: Router) {}
+public bulkUploadError;
+public name ='Manish';
   ngOnInit() {
-    console.log(this.router);
+    this.router.events
+        .filter(e => e instanceof NavigationEnd)
+        .pairwise().subscribe((e: any) => {
+            if(e[0].urlAfterRedirects === '/bulk-upload'){
+              this.bulkUploadError = true;
+              this.name ='Srivastava';
+            }
+
+        });
+
   }
 }
