ccad27b98596fe4952da0e476d426478d520e7a6
mksrivastava <manish-kumar.srivastava@soprasteria.com>
2020-01-24 12:26:20 +0530

    Prevent 503 redirection

--- src/app/core/components/error/problem-with-the-service/problem-with-the-service.component.ts
+++ src/app/core/components/error/problem-with-the-service/problem-with-the-service.component.ts
@@ -1,27 +1,9 @@
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/pairwise';
-
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-problem-with-the-service',
   templateUrl: './problem-with-the-service.component.html',
 })
-export class ProblemWithTheServiceComponent implements OnInit {
-  constructor(private router: Router) {}
-public bulkUploadError;
-public name ='Manish';
-  ngOnInit() {
-    this.router.events
-        .filter(e => e instanceof NavigationEnd)
-        .pairwise().subscribe((e: any) => {
-            if(e[0].urlAfterRedirects === '/bulk-upload'){
-              this.bulkUploadError = true;
-              this.name ='Srivastava';
-            }
-
-        });
-
-  }
+export class ProblemWithTheServiceComponent {
+  constructor() {}
 }
