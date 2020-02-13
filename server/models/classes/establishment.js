6044a4c946e35c861a3533b49c2a8012c3d06c74
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-02-13 13:45:32 +0000

    Revert "Fixed some issue. (#1967)"
    This reverts commit 84c5be71a35fc8a84f4b0a444893a6e1ceddfa1e.

--- server/models/classes/establishment.js
+++ server/models/classes/establishment.js
@@ -1496,7 +1496,7 @@ class Establishment extends EntityValidator {
 
                 // TODO: once we have the unique worder id property, use that instead; for now, we only have the name or id.
                 // without whitespace
-                this.associateWorker(newWorker);
+                this.associateWorker(newWorker.key, newWorker);
 
                 return {};
               })
