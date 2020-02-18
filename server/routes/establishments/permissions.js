478fd95c8de7425ffd3cdaa79147f541a378f15c
Manish Kumar Srivastava <manish-kumar.srivastava@soprasteria.com>
2020-02-18 15:48:32 +0530

    Feature/new home page (#1990)
    * Home Page & Notification Shift (#1952)
    
    * BE added canDownloadWdfReport flag in permission (#1955)
    
    * Permission matrix (#1957)
    
    * canDownloadWdf permission
    
    * Added Permission
    
    Co-authored-by: Anurag Negi <55235060+Negi-Anurag@users.noreply.github.com>

--- server/routes/establishments/permissions.js
+++ server/routes/establishments/permissions.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const Establishment = require('../../models/classes/establishment');
-
+const User = require('../../models/classes/user');
 const PermissionCache = require('../../models/cache/singletons/permissions').PermissionCache;
 
 router.route('/').get(async (req, res) => {
   const establishmentId = req.establishmentId;
   const thisEstablishment = new Establishment.Establishment(req.username);
-
+  const thisUser = new User.User(establishmentId);
   try {
         if (await thisEstablishment.restore(establishmentId)) {
+          if(await thisUser.restore(null,thisEstablishment.username, null)) {
+            let userData = thisUser.toJSON();
           const permissions = await PermissionCache.myPermissions(req).map(item => {
             if (item.code === 'canChangeDataOwner' && thisEstablishment.dataOwnershipRequested !== null) {
               return { [item.code]: false };
@@ -28,12 +30,19 @@ router.route('/').get(async (req, res) => {
                   ? true
                   : false;
             }
+            if (permission.canDownloadWdfReport) {
+              permission.canDownloadWdfReport =
+                (permission.canDownloadWdfReport && thisEstablishment.isParent && userData.role === 'Edit')
+                  ? true
+                  : false;
+            }
           });
 
           return res.status(200).json({
             uid: thisEstablishment.uid,
             permissions: Object.assign({}, ...permissions),
           });
+        }
         } else {
           return res.status(404).send('Not Found');
         }
