c2f7483b5908705eeb8389442abb04721dd8aa07
Rishabh Chitranshi <rishabh.chitranshi@soprasteria.com>
2020-02-24 10:36:40 +0530

    Fixed an issue on this ticket.

--- server/models/classes/establishment.js
+++ server/models/classes/establishment.js
@@ -1492,7 +1492,7 @@ class Establishment extends EntityValidator {
 
                 // TODO: once we have the unique worder id property, use that instead; for now, we only have the name or id.
                 // without whitespace
-                this.associateWorker(newWorker.key, newWorker);
+                this.associateWorker(newWorker);
 
                 return {};
               })
