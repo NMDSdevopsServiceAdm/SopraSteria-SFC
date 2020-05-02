b5111f419037b9485cbe39aa14594fcdba1c5787
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-05-02 14:17:20 +0100

    Two simple integration tests for approving and rejecting a new user.

--- server/test/integration/routes/admin/approval/index.spec.js
+++ server/test/integration/routes/admin/approval/index.spec.js
@@ -9,6 +9,8 @@ const postcodes = require('../../../mockdata/postcodes').data;
 const registrationUtils = require('../../../utils/registration');
 const admin = require('../../../utils/admin').admin;
 
+var adminLogin = null;
+
 describe('Admin/Approval', () => {
   let nonCqcServices = null;
   let nonCQCSite = null;
@@ -34,7 +36,7 @@ describe('Admin/Approval', () => {
 
     // Login as an admin user to access approvals (would normally be accessed via "Registrations").
     if (registration) {
-      const adminLogin = await apiEndpoint
+      adminLogin = await apiEndpoint
         .post('/login')
         .send(admin)
         .expect('Content-Type', /json/)
@@ -45,20 +47,46 @@ describe('Admin/Approval', () => {
   beforeEach(async() => {});
 
   describe('/admin/approval', () => {
-    //it('should return a confirmation message and status 200 when a new user is approved', async () => {
-    //it('should return a confirmation message and status 200 when a new user is removed because the user is rejected', async () => {
-    it('should approve a new user that has an associated login, user and establishment', async () => {
+    it('should return a confirmation message and status 200 when a new user is approved', async () => {
+      // Arrange
+      const approve = true; 
       if (adminLogin.headers.authorization) {
-          const approval = await apiEndpoint
-            .post('/admin/approval')
-            .set({ Authorization: adminLogin.headers.authorization })
-            .send({
-              username: nonCQCSite.user.username.toLowerCase(),
-              approve: true,
-            })
-            .expect('Content-Type', /json/)
-            .expect(200);
-        }
+        const approval = await apiEndpoint
+
+          // Act
+          .post('/admin/approval')
+          .set({ Authorization: adminLogin.headers.authorization })
+          .send({
+            username: nonCQCSite.user.username.toLowerCase(),
+            approve: approve,
+          })
+
+          // Assert
+          .expect('Content-Type', /json/)
+          .expect(200);
+        expect(approval.body.message).to.equal('User has been set as active');
+      }
+    });
+    
+    it('should return a confirmation message and status 200 when a new user is removed because the user is rejected', async () => {
+      // Arrange
+      const approve = false; 
+      if (adminLogin.headers.authorization) {
+        const approval = await apiEndpoint
+
+          // Act
+          .post('/admin/approval')
+          .set({ Authorization: adminLogin.headers.authorization })
+          .send({
+            username: nonCQCSite.user.username.toLowerCase(),
+            approve: approve,
+          })
+
+          // Assert
+          .expect('Content-Type', /json/)
+          .expect(200);
+        expect(approval.body.message).to.equal('User has been removed');
+      }
     })
   })
 });
