2d06f935413eca11ce7ad342e2afcc64eecbd00a
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-05-03 14:37:17 +0100

    feat/change-workplaceid: Refactor: Extract two functions

--- server/routes/admin/approval/index.js
+++ server/routes/admin/approval/index.js
@@ -15,153 +15,161 @@ router.route('/').post(async (req, res) => {
 });
 
 const adminApproval = async (req, res) => {
+  if (req.body.username) {
+    await _approveNewUser(req, res);
+  } else {
+    await _approveNewWorkplace(req, res);
+  }
+};
+
+const _approveNewUser = async (req, res) => {
   // parse input - escaped to prevent SQL injection
   // Sanitize username
-  if (req.body.username) {
-    const username = escape(req.body.username.toLowerCase());
+  const username = escape(req.body.username.toLowerCase());
 
-    try {
-      // Find the user matching the username
-      const login = await models.login.findOne({
+  try {
+    // Find the user matching the username
+    const login = await models.login.findOne({
+      where: {
+        username: {
+          [models.Sequelize.Op.iLike]: username
+        }
+      },
+      attributes: ['id', 'username'],
+      include: [
+        {
+          model: models.user,
+          attributes: ['id'],
+          include: [
+            {
+              model: models.establishment,
+              attributes: ['id']
+            }
+          ]
+        }
+      ]
+    });
+    // Make sure we have the matching user
+    if ((login && login.id) && (username === login.username)) {
+      // TODO: Rename establishment to workplace.
+      const establishment = await models.establishment.findOne({
         where: {
-          username: {
-            [models.Sequelize.Op.iLike]: username
-          }
+          id: login.user.establishment.id
         },
-        attributes: ['id', 'username'],
-        include: [
-          {
-            model: models.user,
-            attributes: ['id'],
-            include: [
-              {
-                model: models.establishment,
-                attributes: ['id']
-              }
-            ]
-          }
-        ]
+        attributes: ['id']
       });
-      // Make sure we have the matching user
-      if ((login && login.id) && (username === login.username)) {
-        // TODO: Rename establishment to workplace.
-        const establishment = await models.establishment.findOne({
-          where: {
-            id: login.user.establishment.id
-          },
-          attributes: ['id']
-        });
-        const user = await models.user.findOne({
-          where: {
-            id: login.user.id
-          },
-          attributes: ['id']
-        });
-        // If approving user
-        if (req.body.approve && establishment) {
-          // TODO: Email saying they've been accepted
-          // Update their active status to true
-          try {
-            const updatedLogin = await login.update({
-              isActive: true,
-              status: null
-            });
-            const updatedestablishment = await establishment.update({
-              ustatus: null
-            });
-            if (updatedLogin && updatedestablishment) {
-              // TODO: use const string!
-              return res.status(200).json({ status: '0', message: 'User has been set as active' })
-            } else {
-              return res.status(503).send();
-            }
-          } catch (error) {
-            console.error(error);
+      const user = await models.user.findOne({
+        where: {
+          id: login.user.id
+        },
+        attributes: ['id']
+      });
+      // If approving user
+      if (req.body.approve && establishment) {
+        // TODO: Email saying they've been accepted
+        // Update their active status to true
+        try {
+          const updatedLogin = await login.update({
+            isActive: true,
+            status: null
+          });
+          const updatedestablishment = await establishment.update({
+            ustatus: null
+          });
+          if (updatedLogin && updatedestablishment) {
+            // TODO: use const string!
+            return res.status(200).json({ status: '0', message: 'User has been set as active' })
+          } else {
             return res.status(503).send();
           }
-        } else {
-          // TODO: Email saying they've been rejected
-          // Remove the user
-          try {
-            if (user && establishment) {
-              const deleteduser = await user.destroy();
-              const deletedestablishment = await establishment.destroy();
-              if (deleteduser && deletedestablishment) {
-                // TODO: use const string!
-                return res.status(200).json({ status: '0', message: 'User has been removed' });
-              } else {
-                return res.status(503).send();
-              }
-            } else {
-              return res.status(503).send();
-            }
-          } catch (error) {
-            console.error(error);
-            return res.status(503).send();
-          }
+        } catch (error) {
+          console.error(error);
+          return res.status(503).send();
         }
       } else {
-        return res.status(400).send();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    const nmdsId = req.body.nmdsId;
-    const checkEstablishmentIsUnique = await models.establishment.findOne({
-      where: {
-        id: {
-          [Sequelize.Op.ne]: req.body.establishmentId
-        },
-        nmdsId: nmdsId,
-      },
-      attributes: ['id']
-    });
-
-    if (checkEstablishmentIsUnique !== null) {
-      return res.status(400).json({
-        nmdsId: `This workplace ID (${nmdsId}) belongs to another workplace. Enter a different workplace ID.`,
-      });
-    }
-
-    const establishment = await models.establishment.findOne({
-      where: {
-        id: req.body.establishmentId
-      },
-      attributes: ['id']
-    });
-
-    if (req.body.approve && req.body.establishmentId) {
-      // Establishment:: Update their ustatus to null
-      try {
-        const updatedestablishment = await establishment.update({
-          nmdsId: nmdsId,
-          ustatus: null
-        });
-        if (updatedestablishment) {
-          // TODO: use const string!
-          return res.status(200).json({ status: '0', message: 'Workplace has been set as active' })
-        } else {
+        // TODO: Email saying they've been rejected
+        // Remove the user
+        try {
+          if (user && establishment) {
+            const deleteduser = await user.destroy();
+            const deletedestablishment = await establishment.destroy();
+            if (deleteduser && deletedestablishment) {
+              // TODO: use const string!
+              return res.status(200).json({ status: '0', message: 'User has been removed' });
+            } else {
+              return res.status(503).send();
+            }
+          } else {
+            return res.status(503).send();
+          }
+        } catch (error) {
+          console.error(error);
           return res.status(503).send();
         }
-      } catch (error) {
-        console.error(error);
-        return res.status(503).send();
       }
     } else {
-      // Remove the establishment
-      try {
-        const deletedestablishment = await establishment.destroy();
-        if (deletedestablishment) {
-          // TODO: use const string!
-          return res.status(200).json({ status: '0', message: 'Workplace has been removed' });
-        } else {
-          return res.status(503).send();
-        }
-      } catch (error) {
-        console.error(error);
+      return res.status(400).send();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const _approveNewWorkplace = async (req, res) => {
+  const nmdsId = req.body.nmdsId;
+  const checkEstablishmentIsUnique = await models.establishment.findOne({
+    where: {
+      id: {
+        [Sequelize.Op.ne]: req.body.establishmentId
+      },
+      nmdsId: nmdsId,
+    },
+    attributes: ['id']
+  });
+
+  if (checkEstablishmentIsUnique !== null) {
+    return res.status(400).json({
+      nmdsId: `This workplace ID (${nmdsId}) belongs to another workplace. Enter a different workplace ID.`,
+    });
+  }
+
+  const establishment = await models.establishment.findOne({
+    where: {
+      id: req.body.establishmentId
+    },
+    attributes: ['id']
+  });
+
+  if (req.body.approve && req.body.establishmentId) {
+    // Establishment:: Update their ustatus to null
+    try {
+      const updatedestablishment = await establishment.update({
+        nmdsId: nmdsId,
+        ustatus: null
+      });
+      if (updatedestablishment) {
+        // TODO: use const string!
+        return res.status(200).json({ status: '0', message: 'Workplace has been set as active' })
+      } else {
         return res.status(503).send();
       }
+    } catch (error) {
+      console.error(error);
+      return res.status(503).send();
+    }
+  } else {
+    // Remove the establishment
+    try {
+      const deletedestablishment = await establishment.destroy();
+      if (deletedestablishment) {
+        // TODO: use const string!
+        return res.status(200).json({ status: '0', message: 'Workplace has been removed' });
+      } else {
+        return res.status(503).send();
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(503).send();
     }
   }
 };
