160414f4f2c0a6655141b1c368e2c984738d59c1
Joe Roberts <joe.roberts@madetech.com>
2020-05-07 10:02:16 +0100

    Updated tests.

--- src/app/features/dashboard/dashboard.component.spec.ts
+++ src/app/features/dashboard/dashboard.component.spec.ts
@@ -1,11 +1,11 @@
 import { DashboardComponent } from '@features/dashboard/dashboard.component';
-import { render, within } from '@testing-library/angular';
+import { render, within, prettyDOM } from '@testing-library/angular';
 import { HomeTabComponent } from '@features/dashboard/home-tab/home-tab.component';
 import { SharedModule } from '@shared/shared.module';
 import { Router, RouterModule } from '@angular/router';
 import { WindowRef } from '@core/services/window.ref';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
 import { UserService } from '@core/services/user.service';
 import { NotificationsService } from '@core/services/notifications/notifications.service';
@@ -16,12 +16,13 @@ import { HttpClient } from '@angular/common/http';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
 import { PermissionType } from '@core/model/permissions.model';
+import { getTestBed, TestBed } from '@angular/core/testing';
 
 describe('DashboardComponent', () => {
   async function setup(isAdmin: boolean = true) {
     const permissions: PermissionType[] = isAdmin ? ['canDeleteEstablishment'] : [];
 
-    return await render(DashboardComponent, {
+    const component =  await render(DashboardComponent, {
       imports: [
         SharedModule,
         RouterModule,
@@ -54,6 +55,14 @@ describe('DashboardComponent', () => {
         }
       ]
     });
+
+    const injector = getTestBed();
+    const establishmentService = injector.get(EstablishmentService) as EstablishmentService;
+
+    return {
+      component,
+      establishmentService
+    }
   }
 
   it('should render a DashboardComponent', async () => {
@@ -61,25 +70,44 @@ describe('DashboardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display a Delete Workplace link if user is an admin', async () => {
-    const component = await setup(true);
+  describe("Archive Workplace", () => {
+    it('should display a Delete Workplace link if user is an admin', async () => {
+      const { component } = await setup(true);
 
-    component.getByText('Delete Workplace');
-  });
+      component.getByText('Delete Workplace');
+    });
 
-  it('should not display a Delete Workplace link if user not an admin', async () => {
-    const component = await setup(false);
+    it('should not display a Delete Workplace link if user not an admin', async () => {
+      const { component } = await setup(false);
 
-    expect(component.queryByText('Delete Workplace')).toBeNull();
-  });
+      expect(component.queryByText('Delete Workplace')).toBeNull();
+    });
 
-  it('should display a modal when the user clicks on Delete Workplace', async () => {
-    const component = await setup(true);
+    it('should display a modal when the user clicks on Delete Workplace', async () => {
+      const { component } = await setup(true);
 
-    const deleteWorkplace = component.getByText('Delete Workplace');
-    deleteWorkplace.click();
+      const deleteWorkplace = component.getByText('Delete Workplace');
+      deleteWorkplace.click();
 
-    await within(document.body).findByRole('dialog');
+      await within(document.body).findByRole('dialog');
+    });
+
+    it('should send a DELETE request once the user confirms to Delete Workplace', async () => {
+      const { component, establishmentService } = await setup(true);
+
+      const deleteWorkplace = component.getByText('Delete Workplace');
+      deleteWorkplace.click();
+
+      const dialog = await within(document.body).findByRole('dialog');
+      console.log(prettyDOM(dialog))
+      const confirm = within(dialog).getByText('Delete workplace')
+      console.log(prettyDOM(confirm))
+      confirm.click();
+
+      const spy = spyOn(establishmentService, 'deleteWorkplace');
+
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
 
