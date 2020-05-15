5b7238c4b925d801a98c2b457882b4018b15e09e
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-05-15 12:51:21 +0100

    Updated Jade to pug

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.use(unless('/api', 'test', xssClean()));
 
 // view engine setup
 app.set('views', path.join(__dirname, '/server/views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'dist/favicon.ico')));
 app.use(logger('dev'));
