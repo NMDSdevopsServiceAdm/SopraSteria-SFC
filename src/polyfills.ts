8efbc37387537bcd96a8da59560546e1a964bb0f
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-05-18 11:24:32 +0100

    On updating angular, the core js files changes from specifying es versions to not

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -13,20 +13,20 @@
  *
  * Learn more in https://angular.io/guide/browser-support
  */
-import 'core-js/es6/array';
-import 'core-js/es6/date';
-import 'core-js/es6/function';
-import 'core-js/es6/map';
-import 'core-js/es6/math';
-import 'core-js/es6/number';
-import 'core-js/es6/object';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/regexp';
-import 'core-js/es6/set';
-import 'core-js/es6/string';
-import 'core-js/es6/symbol';
-import 'core-js/es6/weak-map';
+import 'core-js/es/array';
+import 'core-js/es/date';
+import 'core-js/es/function';
+import 'core-js/es/map';
+import 'core-js/es/math';
+import 'core-js/es/number';
+import 'core-js/es/object';
+import 'core-js/es/parse-float';
+import 'core-js/es/parse-int';
+import 'core-js/es/regexp';
+import 'core-js/es/set';
+import 'core-js/es/string';
+import 'core-js/es/symbol';
+import 'core-js/es/weak-map';
 import 'details-element-polyfill/dist/details-element-polyfill';
 import 'zone.js/dist/zone';
 
@@ -41,13 +41,13 @@ import 'zone.js/dist/zone';
  * Googlebot uses a renderer based on Chrome 41.
  * https://developers.google.com/search/docs/guides/rendering
  **/
-// import 'core-js/es6/array';
+// import 'core-js/es/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
+// import 'core-js/es/reflect';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
