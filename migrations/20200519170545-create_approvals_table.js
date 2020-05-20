89d689191cef915349bbb0326efb1b4d724d0bf2
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-05-20 15:28:16 +0100

    feat(become-a-parent-db): Add migration for Approvals table

---
+++ migrations/20200519170545-create_approvals_table.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Approvals', {
+      EtablishmentID: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'Establishment',
+            schema: 'cqc'
+          },
+          key: 'EstablishmentID'
+        },
+        allowNull: false
+      },
+      UserID: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'User',
+            schema: 'cqc'
+          },
+          key: 'RegistrationID'
+        },
+        allowNull: false
+      },
+      RejectionReason: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: true,
+      },
+      Data: {
+        type: Sequelize.DataTypes.JSON,
+        allowNull: true,
+      },
+      createdAt: Sequelize.DataTypes.DATE,
+      updatedAt: Sequelize.DataTypes.DATE,
+    }, {
+      schema: 'cqc'
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable({
+      tableName: 'Approvals',
+      schema: 'cqc',
+    });
+  }
+}
