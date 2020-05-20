43bfcc2199a30aed40edb8374154c24874e6b175
Joe Roberts <joe.roberts@madetech.com>
2020-05-20 10:33:26 +0100

    Fix codes for Djibouti, Kosovo and Laos.

--- server/test/unit/models/Bulkimport/csv/workers.spec.js
+++ server/test/unit/models/Bulkimport/csv/workers.spec.js
@@ -5,7 +5,12 @@ const maxquals = require('../../../mockdata/workers').maxquals;
 const knownHeaders = require('../../../mockdata/workers').knownHeaders;
 const moment = require('moment');
 const filename = 'server/models/BulkImport/csv/workers.js';
-
+const rfr = require('rfr');
+const sinon = require('sinon');
+const dbmodels = rfr('server/models');
+sinon.stub(dbmodels.status, 'ready').value(false);
+const BUDI = rfr('server/models/Bulkimport/BUDI').BUDI;
+const WorkerCsvValidator = rfr('server/models/BulkImport/csv/workers').Worker;
 const testUtils = require('../../../../../utils/testUtils');
 const csv = require('csvtojson');
 
@@ -18,263 +23,6 @@ function mapCsvToWorker(worker, headers) {
   });
   return mapped;
 }
-
-const BUDI = {
-    ethnicity: (direction, originalCode) => {
-      const fixedMapping = [
-        { ASC: 1, BUDI: 31 },
-        { ASC: 3, BUDI: 32 },
-        { ASC: 4, BUDI: 33 },
-        { ASC: 5, BUDI: 34 },
-        { ASC: 6, BUDI: 35 },
-        { ASC: 7, BUDI: 36 },
-        { ASC: 8, BUDI: 37 },
-        { ASC: 9, BUDI: 38 },
-        { ASC: 10, BUDI: 39 },
-        { ASC: 11, BUDI: 40 },
-        { ASC: 12, BUDI: 41 },
-        { ASC: 13, BUDI: 42 },
-        { ASC: 14, BUDI: 43 },
-        { ASC: 15, BUDI: 44 },
-        { ASC: 16, BUDI: 45 },
-        { ASC: 17, BUDI: 46 },
-        { ASC: 18, BUDI: 47 },
-        { ASC: 19, BUDI: 98 },
-        { ASC: 2, BUDI: 99 }
-      ];
-
-      if (direction === BUDI_TO_ASC) {
-        const found = fixedMapping.find(thisEthnicity => thisEthnicity.BUDI === originalCode);
-        return found ? found.ASC : null;
-      }
-
-      const found = fixedMapping.find(thisEthnicity => thisEthnicity.ASC === originalCode);
-      return found ? found.BUDI : null;
-    },
-
-    jobRoles: (direction, originalCode) => {
-      const fixedMapping = [
-        { ASC: 26, BUDI: 1 },
-        { ASC: 15, BUDI: 2 },
-        { ASC: 13, BUDI: 3 },
-        { ASC: 22, BUDI: 4 },
-        { ASC: 28, BUDI: 5 },
-        { ASC: 27, BUDI: 6 },
-        { ASC: 25, BUDI: 7 },
-        { ASC: 10, BUDI: 8 },
-        { ASC: 11, BUDI: 9 },
-        { ASC: 12, BUDI: 10 },
-        { ASC: 3, BUDI: 11 },
-        { ASC: 18, BUDI: 15 },
-        { ASC: 23, BUDI: 16 },
-        { ASC: 4, BUDI: 17 },
-        { ASC: 29, BUDI: 22 },
-        { ASC: 20, BUDI: 23 },
-        { ASC: 14, BUDI: 24 },
-        { ASC: 2, BUDI: 25 },
-        { ASC: 5, BUDI: 26 },
-        { ASC: 21, BUDI: 27 },
-        { ASC: 1, BUDI: 34 },
-        { ASC: 24, BUDI: 35 },
-        { ASC: 19, BUDI: 36 },
-        { ASC: 17, BUDI: 37 },
-        { ASC: 16, BUDI: 38 },
-        { ASC: 7, BUDI: 39 },
-        { ASC: 8, BUDI: 40 },
-        { ASC: 9, BUDI: 41 },
-        { ASC: 6, BUDI: 42 }
-      ];
-
-      if (direction === BUDI_TO_ASC) {
-        const found = fixedMapping.find(thisJob => thisJob.BUDI === originalCode);
-        return found ? found.ASC : null;
-      }
-
-      const found = fixedMapping.find(thisJob => thisJob.ASC === originalCode);
-      return found ? found.BUDI : null;
-    },
-    nursingSpecialist: (direction, originalCode) => {
-      const fixedMapping = [
-        { BUDI: 1, ASC: 1 },
-        { BUDI: 2, ASC: 2 },
-        { BUDI: 3, ASC: 3 },
-        { BUDI: 4, ASC: 4 },
-        { BUDI: 5, ASC: 5 },
-        { BUDI: 6, ASC: 6 },
-        { BUDI: 7, ASC: 7 },
-        { BUDI: 8, ASC: 8 }
-      ];
-
-      if (direction === BUDI_TO_ASC) {
-        const found = fixedMapping.find(thisSpecialist => thisSpecialist.BUDI === originalCode);
-        return found ? found.ASC : null;
-      }
-
-      const found = fixedMapping.find(thisSpecialist => thisSpecialist.ASC === originalCode);
-      return found ? found.BUDI : null;
-    },
-    qualificationLevels: (direction, originalCode) => {
-      const fixedMapping = [
-        { BUDI: 0, ASC: 1 },
-        { BUDI: 1, ASC: 2 },
-        { BUDI: 2, ASC: 3 },
-        { BUDI: 3, ASC: 4 },
-        { BUDI: 4, ASC: 5 },
-        { BUDI: 5, ASC: 6 },
-        { BUDI: 6, ASC: 7 },
-        { BUDI: 7, ASC: 8 },
-        { BUDI: 8, ASC: 9 },
-        { BUDI: 999, ASC: 10 }
-      ];
-
-      if (direction === BUDI_TO_ASC) {
-        const found = fixedMapping.find(thisSpecialist => thisSpecialist.BUDI === originalCode);
-        return found ? found.ASC : null;
-      }
-
-      const found = fixedMapping.find(thisSpecialist => thisSpecialist.ASC === originalCode);
-      return found ? found.BUDI : null;
-    },
-    qualifications: (direction, originalCode) => {
-      const fixedMapping = [
-        { BUDI: 1, ASC: 97 },
-        { BUDI: 2, ASC: 98 },
-        { BUDI: 3, ASC: 96 },
-        { BUDI: 4, ASC: 93 },
-        { BUDI: 5, ASC: 94 },
-        { BUDI: 6, ASC: 95 },
-        { BUDI: 8, ASC: 24 },
-        { BUDI: 9, ASC: 99 },
-        { BUDI: 10, ASC: 100 },
-        { BUDI: 12, ASC: 25 },
-        { BUDI: 13, ASC: 102 },
-        { BUDI: 14, ASC: 107 },
-        { BUDI: 15, ASC: 106 },
-        { BUDI: 16, ASC: 72 },
-        { BUDI: 17, ASC: 89 },
-        { BUDI: 18, ASC: 71 },
-        { BUDI: 19, ASC: 16 },
-        { BUDI: 20, ASC: 1 },
-        { BUDI: 22, ASC: 14 },
-        { BUDI: 25, ASC: 15 },
-        { BUDI: 26, ASC: 26 },
-        { BUDI: 27, ASC: 114 },
-        { BUDI: 28, ASC: 116 },
-        { BUDI: 32, ASC: 115 },
-        { BUDI: 33, ASC: 113 },
-        { BUDI: 34, ASC: 111 },
-        { BUDI: 35, ASC: 109 },
-        { BUDI: 36, ASC: 110 },
-        { BUDI: 37, ASC: 117 },
-        { BUDI: 38, ASC: 118 },
-        { BUDI: 39, ASC: 119 },
-        { BUDI: 41, ASC: 20 },
-        { BUDI: 42, ASC: 30 },
-        { BUDI: 48, ASC: 4 },
-        { BUDI: 49, ASC: 5 },
-        { BUDI: 50, ASC: 60 },
-        { BUDI: 51, ASC: 61 },
-        { BUDI: 52, ASC: 10 },
-        { BUDI: 53, ASC: 80 },
-        { BUDI: 54, ASC: 81 },
-        { BUDI: 55, ASC: 82 },
-        { BUDI: 56, ASC: 83 },
-        { BUDI: 57, ASC: 84 },
-        { BUDI: 58, ASC: 85 },
-        { BUDI: 62, ASC: 86 },
-        { BUDI: 63, ASC: 87 },
-        { BUDI: 64, ASC: 88 },
-        { BUDI: 67, ASC: 21 },
-        { BUDI: 68, ASC: 22 },
-        { BUDI: 72, ASC: 23 },
-        { BUDI: 73, ASC: 32 },
-        { BUDI: 74, ASC: 19 },
-        { BUDI: 76, ASC: 64 },
-        { BUDI: 77, ASC: 65 },
-        { BUDI: 82, ASC: 103 },
-        { BUDI: 83, ASC: 104 },
-        { BUDI: 84, ASC: 105 },
-        { BUDI: 85, ASC: 17 },
-        { BUDI: 86, ASC: 2 },
-        { BUDI: 87, ASC: 45 },
-        { BUDI: 88, ASC: 9 },
-        { BUDI: 89, ASC: 69 },
-        { BUDI: 90, ASC: 12 },
-        { BUDI: 91, ASC: 18 },
-        { BUDI: 92, ASC: 130 },
-        { BUDI: 93, ASC: 62 },
-        { BUDI: 94, ASC: 66 },
-        { BUDI: 95, ASC: 67 },
-        { BUDI: 96, ASC: 11 },
-        { BUDI: 98, ASC: 59 },
-        { BUDI: 99, ASC: 6 },
-        { BUDI: 100, ASC: 7 },
-        { BUDI: 101, ASC: 68 },
-        { BUDI: 102, ASC: 63 },
-        { BUDI: 103, ASC: 8 },
-        { BUDI: 104, ASC: 75 },
-        { BUDI: 105, ASC: 76 },
-        { BUDI: 107, ASC: 3 },
-        { BUDI: 108, ASC: 47 },
-        { BUDI: 109, ASC: 74 },
-        { BUDI: 110, ASC: 31 },
-        { BUDI: 111, ASC: 27 },
-        { BUDI: 112, ASC: 28 },
-        { BUDI: 113, ASC: 134 },
-        { BUDI: 114, ASC: 135 },
-        { BUDI: 115, ASC: 90 },
-        { BUDI: 116, ASC: 91 },
-        { BUDI: 92, ASC: 13 },
-        { BUDI: 119, ASC: 33 },
-        { BUDI: 121, ASC: 34 },
-        { BUDI: 136, ASC: 35 },
-        { BUDI: 123, ASC: 36 },
-        { BUDI: 124, ASC: 37 },
-        { BUDI: 125, ASC: 38 },
-        { BUDI: 118, ASC: 39 },
-        { BUDI: 137, ASC: 40 },
-        { BUDI: 131, ASC: 41 },
-        { BUDI: 134, ASC: 42 },
-        { BUDI: 138, ASC: 43 },
-        { BUDI: 143, ASC: 44 },
-        { BUDI: 141, ASC: 48 },
-        { BUDI: 120, ASC: 49 },
-        { BUDI: 122, ASC: 50 },
-        { BUDI: 126, ASC: 51 },
-        { BUDI: 128, ASC: 52 },
-        { BUDI: 127, ASC: 53 },
-        { BUDI: 142, ASC: 54 },
-        { BUDI: 133, ASC: 55 },
-        { BUDI: 135, ASC: 56 },
-        { BUDI: 139, ASC: 57 },
-        { BUDI: 140, ASC: 58 },
-        { BUDI: 129, ASC: 77 },
-        { BUDI: 130, ASC: 78 },
-        { BUDI: 132, ASC: 79 },
-        { BUDI: 117, ASC: 112 },
-        { BUDI: 302, ASC: 121 },
-        { BUDI: 304, ASC: 122 },
-        { BUDI: 303, ASC: 123 },
-        { BUDI: 310, ASC: 124 },
-        { BUDI: 308, ASC: 125 },
-        { BUDI: 306, ASC: 126 },
-        { BUDI: 301, ASC: 127 },
-        { BUDI: 305, ASC: 128 },
-        { BUDI: 307, ASC: 129 },
-        { BUDI: 312, ASC: 131 },
-        { BUDI: 313, ASC: 132 },
-        { BUDI: 311, ASC: 133 }
-      ];
-
-      if (direction === BUDI_TO_ASC) {
-        const found = fixedMapping.find(thisQualification => thisQualification.BUDI === originalCode);
-        return found ? found.ASC : null;
-      }
-
-      const found = fixedMapping.find(thisQualification => thisQualification.ASC === originalCode);
-      return found ? found.BUDI : null;
-    }
-};
 
 const getUnitInstance = () => {
   const ALL_CAPACITIES = null;
@@ -1238,15 +986,147 @@ describe('/server/models/Bulkimport/csv/workers.js', () => {
         await bulkUpload.validate();
 
         // call the method
-        await bulkUpload.crossValidate({
-          csvWorkerSchemaErrors,
-          myEstablishments,
-        });
+        await bulkUpload.transform();
 
         // assert a error was returned
         expect(csvWorkerSchemaErrors.length).to.equal(0);
 
         expect(csvWorkerSchemaErrors).to.deep.equal([]);
+      });
+
+    const countryCodesToTest = [262, 418, 995];
+    countryCodesToTest.forEach(countryCode => {
+        it('should validate for COUNTRYOFBIRTH ' + countryCode, async () => {
+          const validator = new WorkerCsvValidator(
+            {
+              AMHP: "",
+              APPRENTICE: "2",
+              AVGHOURS: "",
+              BRITISHCITIZENSHIP: "",
+              CARECERT: "3",
+              CONTHOURS: "23",
+              COUNTRYOFBIRTH: `${countryCode}`,
+              DAYSSICK: "1",
+              DISABLED: "0",
+              DISPLAYID: "Aaron Russell",
+              DOB: "10/12/1982",
+              EMPLSTATUS: "1",
+              ETHNICITY: "41",
+              GENDER: "1",
+              HOURLYRATE: "",
+              LOCALESTID: "MARMA",
+              MAINJOBROLE: "3",
+              MAINJRDESC: "",
+              NATIONALITY: "862",
+              NINUMBER: "JA622112A",
+              NMCREG: "",
+              NONSCQUAL: "2",
+              NURSESPEC: "",
+              OTHERJOBROLE: "10",
+              OTHERJRDESC: "",
+              POSTCODE: "LS1 1AA",
+              QUALACH01: "",
+              QUALACH01NOTES: "",
+              QUALACH02: "",
+              QUALACH02NOTES: "",
+              QUALACH03: "",
+              QUALACH03NOTES: "",
+              RECSOURCE: "16",
+              SALARY: "20000",
+              SALARYINT: "1",
+              SCQUAL: "2",
+              STARTDATE: "12/11/2001",
+              STARTINSECT: "2001",
+              STATUS: "NEW",
+              UNIQUEWORKERID: "3",
+              YEAROFENTRY: "",
+              ZEROHRCONT: "2"
+            },
+            2,
+            []);
+
+          const myEstablishments = [{
+            key: 'MARMA',
+            status: 'UPDATE',
+            regType: 1
+          }];
+
+          // Regular validation has to run first for the establishment to populate the internal properties correctly
+          await validator.validate();
+
+          // call the method
+          await validator.transform();
+
+          // assert a error was returned
+          expect(validator._validationErrors).to.deep.equal([]);
+          expect(validator._validationErrors.length).to.equal(0);
+        });
+
+        it('should validate for NATIONALITY ' + countryCode, async () => {
+          const validator = new WorkerCsvValidator(
+            {
+              AMHP: "",
+              APPRENTICE: "2",
+              AVGHOURS: "",
+              BRITISHCITIZENSHIP: "",
+              CARECERT: "3",
+              CONTHOURS: "23",
+              COUNTRYOFBIRTH: "826",
+              DAYSSICK: "1",
+              DISABLED: "0",
+              DISPLAYID: "Aaron Russell",
+              DOB: "10/12/1982",
+              EMPLSTATUS: "1",
+              ETHNICITY: "41",
+              GENDER: "1",
+              HOURLYRATE: "",
+              LOCALESTID: "MARMA",
+              MAINJOBROLE: "3",
+              MAINJRDESC: "",
+              NATIONALITY: `${countryCode}`,
+              NINUMBER: "JA622112A",
+              NMCREG: "",
+              NONSCQUAL: "2",
+              NURSESPEC: "",
+              OTHERJOBROLE: "10",
+              OTHERJRDESC: "",
+              POSTCODE: "LS1 1AA",
+              QUALACH01: "",
+              QUALACH01NOTES: "",
+              QUALACH02: "",
+              QUALACH02NOTES: "",
+              QUALACH03: "",
+              QUALACH03NOTES: "",
+              RECSOURCE: "16",
+              SALARY: "20000",
+              SALARYINT: "1",
+              SCQUAL: "2",
+              STARTDATE: "12/11/2001",
+              STARTINSECT: "2001",
+              STATUS: "NEW",
+              UNIQUEWORKERID: "3",
+              YEAROFENTRY: "",
+              ZEROHRCONT: "2"
+            },
+            2,
+            []);
+
+          const myEstablishments = [{
+            key: 'MARMA',
+            status: 'UPDATE',
+            regType: 1
+          }];
+
+          // Regular validation has to run first for the establishment to populate the internal properties correctly
+          await validator.validate();
+
+          // call the method
+          await validator.transform();
+
+          // assert a error was returned
+          expect(validator._validationErrors).to.deep.equal([]);
+          expect(validator._validationErrors.length).to.equal(0);
+        });
       });
     });
   workers.forEach((worker, index) => {
@@ -1273,14 +1153,14 @@ describe('/server/models/Bulkimport/csv/workers.js', () => {
         let nonscqual = '';
         const mappedCsv = mapCsvToWorker(workerCSV, columnHeaders);
 
-        if (Array.isArray(worker.otherJobs.otherJobs)) {
-          worker.otherJobs.otherJobs.forEach((job, index) => {
+        if (Array.isArray(worker.otherJobs.jobs)) {
+          worker.otherJobs.jobs.forEach((job, index) => {
             otherJobs += job.budi;
             if (job.other){
               otherJobsDesc += job.other;
             }
-            index < (worker.otherJobs.otherJobs.length - 1) ? otherJobs += ';' : otherJobs += '';
-            index < (worker.otherJobs.otherJobs.length - 1) ? otherJobsDesc += ';' : otherJobsDesc += '';
+            index < (worker.otherJobs.jobs.length - 1) ? otherJobs += ';' : otherJobs += '';
+            index < (worker.otherJobs.jobs.length - 1) ? otherJobsDesc += ';' : otherJobsDesc += '';
           });
         } else {
           expect(establishment.otherServices).to.equal(null);
