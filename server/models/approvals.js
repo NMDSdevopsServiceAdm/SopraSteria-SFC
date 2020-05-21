29581ce31520a821737e0440db66ed26ebf84392
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-05-21 15:45:41 +0100

    feat(approvals-model): Add model for Approvals table

---
+++ server/models/approvals.js
@@ -0,0 +1,62 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Approvals = sequelize.define('approvals', {
+    ID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    UUID: {
+      type: DataTypes.UUID,
+      unique: true,
+      allowNull: false,
+    },
+    EstablishmentID: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    UserID: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    ApprovalType: {
+      type: DataTypes.ENUM,
+      allowNull: false,
+      values: ['BecomeAParent'],
+    },
+    Status: {
+      type: DataTypes.ENUM,
+      allowNull: false,
+      values: ['Pending', 'Approved', 'Rejected'],
+      default: 'Pending'
+    },
+    RejectionReason: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    Data: {
+      type: DataTypes.JSON,
+      allowNull: true,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  }, {
+    tableName: 'Approvals',
+    schema: 'cqc'
+  });
+
+  Approvals.associate = (models) => {
+    Approvals.belongsTo(models.user, {
+      foreignKey : 'UserID',
+      targetKey: 'id',
+    });
+
+    Approvals.belongsTo(models.establishment, {
+      foreignKey: 'EstablishmentID',
+      targetKey: 'id',
+      as: 'establishments',
+    });
+  };
+
+  return Approvals;
+};
