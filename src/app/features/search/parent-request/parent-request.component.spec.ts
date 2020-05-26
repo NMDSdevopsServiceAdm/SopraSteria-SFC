338ecce79c8dec842aae759afe9e8b3110d204cc
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-05-26 18:12:33 +0100

    feat/approve-parent: Break giant test into smaller tests.

--- src/app/features/search/parent-request/parent-request.component.spec.ts
+++ src/app/features/search/parent-request/parent-request.component.spec.ts
@@ -52,18 +52,9 @@ fdescribe('ParentRequestComponent', () => {
       }
     };
     const httpPost = spyOn(component.fixture.componentInstance.switchWorkplaceService.http, 'post').and.returnValue(of(swappedEstablishmentData));
-    const workplace = { uid: testEstablishmentUid }; 
+    const workplace = { uid: testEstablishmentUid };
     spyOn(Observable.prototype, 'pipe').and.returnValue(of(workplace));
-    /*let pipeCount = 0;
-    spyOn(Observable.prototype, 'pipe').and.callFake( () => {
-      pipeCount++;
-      if (pipeCount === 1) {
-        return workplace;
-      } else {
-        return notificationData;
-      }
-    });*/
-    const notificationData = [{ dummyNotification: 'I am a notification' }, { dummyNotification: 'I am another notification' }];    
+    const notificationData = [{ dummyNotification: 'I am a notification' }, { dummyNotification: 'I am another notification' }];  
     spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService, 'getAllNotifications').and.returnValue(of(notificationData));
     parentRequest.username = testUsername;
 
@@ -259,7 +250,7 @@ fdescribe('ParentRequestComponent', () => {
     });
   });
 
-  it('should load notifications if user name not populated.', async () => {
+  it('should load workplace-specific notifications if user name not populated when switching to new workplace.', async () => {
     // Arrange
     const { component } = await setupForSwitchWorkplace();
     parentRequest.username = null;
@@ -277,14 +268,8 @@ fdescribe('ParentRequestComponent', () => {
   });
 
   it('should clear permissions when switching to new workplace', async () => {
-    const { component, authToken, workplace, notificationData, httpPost } = await setupForSwitchWorkplace();
+    const { component } = await setupForSwitchWorkplace();
     const clearPermissions = spyOn(component.fixture.componentInstance.switchWorkplaceService.permissionsService, 'clearPermissions').and.callThrough();
-    const setPreviousToken = spyOn(component.fixture.componentInstance.switchWorkplaceService.authService, 'setPreviousToken').and.callThrough();
-    spyOn(component.fixture.componentInstance.switchWorkplaceService.http, 'get').and.returnValue(of(notificationData));
-    const setState = spyOn(component.fixture.componentInstance.switchWorkplaceService.establishmentService, 'setState').and.callThrough();
-    const setPrimaryWorkplace = spyOn(component.fixture.componentInstance.switchWorkplaceService.establishmentService, 'setPrimaryWorkplace').and.callThrough();
-    const navigate = spyOn(component.fixture.componentInstance.switchWorkplaceService.router, 'navigate').and.callThrough();
-    const notificationsNext = spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService.notifications$, 'next').and.callThrough();
     
     // Act
     component.getByText(testOrgname).click();
@@ -292,13 +277,68 @@ fdescribe('ParentRequestComponent', () => {
 
     // Assert
     expect(clearPermissions).toHaveBeenCalled();
+  });
+
+  it('should change auth tokens when switching to new workplace', async () => {
+    const { component, authToken } = await setupForSwitchWorkplace();
+    const setPreviousToken = spyOn(component.fixture.componentInstance.switchWorkplaceService.authService, 'setPreviousToken').and.callThrough();
+    
+    // Act
+    component.getByText(testOrgname).click();
+    component.fixture.detectChanges();
+
+    // Assert
     expect(setPreviousToken).toHaveBeenCalled();
     expect(component.fixture.componentInstance.switchWorkplaceService.authService.token).toEqual(authToken);
+  });
+
+  it('should swap establishments when switching to new workplace', async () => {
+    const { component, httpPost } = await setupForSwitchWorkplace();
+    
+    // Act
+    component.getByText(testOrgname).click();
+    component.fixture.detectChanges();
+
+    // Assert
     expect(httpPost).toHaveBeenCalledWith('/api/user/swap/establishment/' + parentRequest.establishmentUid, {username: parentRequest.username}, { observe: 'response' });
+  });
+
+  it('should load workplace notifications when switching to new workplace', async () => {
+    const { component, notificationData } = await setupForSwitchWorkplace();
+    const notificationsNext = spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService.notifications$, 'next').and.callThrough();
+    
+    // Act
+    component.getByText(testOrgname).click();
+    component.fixture.detectChanges();
+
+    // Assert
     expect(notificationsNext).toHaveBeenCalledWith(notificationData);
+  });
+
+  it('should switch current workplace Id when switching to new workplace', async () => {
+    const { component, workplace } = await setupForSwitchWorkplace();
+    const setState = spyOn(component.fixture.componentInstance.switchWorkplaceService.establishmentService, 'setState').and.callThrough();
+    const setPrimaryWorkplace = spyOn(component.fixture.componentInstance.switchWorkplaceService.establishmentService, 'setPrimaryWorkplace').and.callThrough();
+    
+    // Act
+    component.getByText(testOrgname).click();
+    component.fixture.detectChanges();
+
+    // Assert
     expect(setState).toHaveBeenCalledWith(workplace);
     expect(setPrimaryWorkplace).toHaveBeenCalledWith(workplace);
     expect(component.fixture.componentInstance.switchWorkplaceService.establishmentService.establishmentId).toEqual(testEstablishmentUid);
+  });
+
+  it('should navigate back to dashboard after switching to new workplace', async () => {
+    const { component } = await setupForSwitchWorkplace();
+    const navigate = spyOn(component.fixture.componentInstance.switchWorkplaceService.router, 'navigate').and.callThrough();
+    
+    // Act
+    component.getByText(testOrgname).click();
+    component.fixture.detectChanges();
+
+    // Assert
     expect(navigate).toHaveBeenCalledWith(['/dashboard']);
   });
 });
