b58a6ad0f25cf4ba283bfe71fa1b63e4450940fc
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-05-26 20:33:55 +0100

    feat/approve-parent: Now using new SwitchWorkplaceService from search component.

--- src/app/features/search/search.component.spec.ts
+++ src/app/features/search/search.component.spec.ts
@@ -48,13 +48,11 @@ fdescribe('SearchComponent', () => {
         },
       }
     };
-    //const httpPost = spyOn(component.fixture.componentInstance.switchWorkplaceService.http, 'post').and.returnValue(of(swappedEstablishmentData));
-    const httpPost = spyOn(component.fixture.componentInstance.http, 'post').and.returnValue(of(swappedEstablishmentData));
+    const httpPost = spyOn(component.fixture.componentInstance.switchWorkplaceService.http, 'post').and.returnValue(of(swappedEstablishmentData));
     const workplace = { uid: testEstablishmentUid };
     spyOn(Observable.prototype, 'pipe').and.returnValue(of(workplace));
     const notificationData = [{ dummyNotification: 'I am a notification' }, { dummyNotification: 'I am another notification' }];  
-    //spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService, 'getAllNotifications').and.returnValue(of(notificationData));
-    spyOn(component.fixture.componentInstance.notificationsService, 'getAllNotifications').and.returnValue(of(notificationData));
+    spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService, 'getAllNotifications').and.returnValue(of(notificationData));
     results[0].username = testUsername;
 
     return {
@@ -105,10 +103,8 @@ fdescribe('SearchComponent', () => {
     const { component } = await setupForSwitchWorkplace();
     results[0].username = null;
     const notificationData = { dummyNotification: 'I am a notification' };
-    //const httpGet = spyOn(component.fixture.componentInstance.switchWorkplaceService.http, 'get').and.returnValue(of(notificationData));
-    //const notificationsNext = spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService.notifications$, 'next').and.callThrough();
-    const httpGet = spyOn(component.fixture.componentInstance.http, 'get').and.returnValue(of(notificationData));
-    const notificationsNext = spyOn(component.fixture.componentInstance.notificationsService.notifications$, 'next').and.callThrough();
+    const httpGet = spyOn(component.fixture.componentInstance.switchWorkplaceService.http, 'get').and.returnValue(of(notificationData));
+    const notificationsNext = spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService.notifications$, 'next').and.callThrough();
 
     // Act
     component.getByText(testOrgname).click();
@@ -121,8 +117,7 @@ fdescribe('SearchComponent', () => {
 
   it('should clear permissions when switching to new workplace', async () => {
     const { component } = await setupForSwitchWorkplace();
-    //const clearPermissions = spyOn(component.fixture.componentInstance.switchWorkplaceService.permissionsService, 'clearPermissions').and.callThrough();
-    const clearPermissions = spyOn(component.fixture.componentInstance.permissionsService, 'clearPermissions').and.callThrough();
+    const clearPermissions = spyOn(component.fixture.componentInstance.switchWorkplaceService.permissionsService, 'clearPermissions').and.callThrough();
     
     // Act
     component.getByText(testOrgname).click();
@@ -134,8 +129,7 @@ fdescribe('SearchComponent', () => {
 
   it('should change auth tokens when switching to new workplace', async () => {
     const { component, authToken } = await setupForSwitchWorkplace();
-    //const setPreviousToken = spyOn(component.fixture.componentInstance.switchWorkplaceService.authService, 'setPreviousToken').and.callThrough();
-    const setPreviousToken = spyOn(component.fixture.componentInstance.authService, 'setPreviousToken').and.callThrough();
+    const setPreviousToken = spyOn(component.fixture.componentInstance.switchWorkplaceService.authService, 'setPreviousToken').and.callThrough();
     
     // Act
     component.getByText(testOrgname).click();
@@ -143,8 +137,7 @@ fdescribe('SearchComponent', () => {
 
     // Assert
     expect(setPreviousToken).toHaveBeenCalled();
-    //expect(component.fixture.componentInstance.switchWorkplaceService.authService.token).toEqual(authToken);
-    expect(component.fixture.componentInstance.authService.token).toEqual(authToken);
+    expect(component.fixture.componentInstance.switchWorkplaceService.authService.token).toEqual(authToken);
   });
 
   it('should swap establishments when switching to new workplace', async () => {
@@ -160,8 +153,7 @@ fdescribe('SearchComponent', () => {
 
   it('should load workplace notifications when switching to new workplace', async () => {
     const { component, notificationData } = await setupForSwitchWorkplace();
-    //const notificationsNext = spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService.notifications$, 'next').and.callThrough();
-    const notificationsNext = spyOn(component.fixture.componentInstance.notificationsService.notifications$, 'next').and.callThrough();
+    const notificationsNext = spyOn(component.fixture.componentInstance.switchWorkplaceService.notificationsService.notifications$, 'next').and.callThrough();
     
     // Act
     component.getByText(testOrgname).click();
@@ -173,10 +165,8 @@ fdescribe('SearchComponent', () => {
 
   it('should switch current workplace Id when switching to new workplace', async () => {
     const { component, workplace } = await setupForSwitchWorkplace();
-    //const setState = spyOn(component.fixture.componentInstance.switchWorkplaceService.establishmentService, 'setState').and.callThrough();
-    //const setPrimaryWorkplace = spyOn(component.fixture.componentInstance.switchWorkplaceService.establishmentService, 'setPrimaryWorkplace').and.callThrough();
-    const setState = spyOn(component.fixture.componentInstance.establishmentService, 'setState').and.callThrough();
-    const setPrimaryWorkplace = spyOn(component.fixture.componentInstance.establishmentService, 'setPrimaryWorkplace').and.callThrough();
+    const setState = spyOn(component.fixture.componentInstance.switchWorkplaceService.establishmentService, 'setState').and.callThrough();
+    const setPrimaryWorkplace = spyOn(component.fixture.componentInstance.switchWorkplaceService.establishmentService, 'setPrimaryWorkplace').and.callThrough();
     
     // Act
     component.getByText(testOrgname).click();
@@ -185,14 +175,12 @@ fdescribe('SearchComponent', () => {
     // Assert
     expect(setState).toHaveBeenCalledWith(workplace);
     expect(setPrimaryWorkplace).toHaveBeenCalledWith(workplace);
-    //expect(component.fixture.componentInstance.switchWorkplaceService.establishmentService.establishmentId).toEqual(testEstablishmentUid);
-    expect(component.fixture.componentInstance.establishmentService.establishmentId).toEqual(testEstablishmentUid);
+    expect(component.fixture.componentInstance.switchWorkplaceService.establishmentService.establishmentId).toEqual(testEstablishmentUid);
   });
 
   it('should navigate back to dashboard after switching to new workplace', async () => {
     const { component } = await setupForSwitchWorkplace();
-    //const navigate = spyOn(component.fixture.componentInstance.switchWorkplaceService.router, 'navigate').and.callThrough();
-    const navigate = spyOn(component.fixture.componentInstance.router, 'navigate').and.callThrough();
+    const navigate = spyOn(component.fixture.componentInstance.switchWorkplaceService.router, 'navigate').and.callThrough();
     
     // Act
     component.getByText(testOrgname).click();
