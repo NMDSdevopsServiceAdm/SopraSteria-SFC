b5590cf85a6a1570e4e541dc30ae74dfaba23c20
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-05-26 17:19:12 +0100

    Added unit tests for become a parent endpoint (Needs work)

--- server/test/factories/user.js
+++ server/test/factories/user.js
@@ -1,12 +1,14 @@
-const { build, fake } = require('@jackfranklin/test-data-bot');
+const { build, fake, sequence } = require('@jackfranklin/test-data-bot');
 
 const establishmentId = fake(f => f.helpers.replaceSymbolWithNumber('####'));
-
+const uid = fake(f => f.random.uuid());
 module.exports = build('user', {
   fields: {
     establishmentId: establishmentId,
     username: fake(f => f.internet.userName()),
-    userUid: fake(f => f.random.uuid()),
+    userUid: uid,
+    id: sequence(),
+    uid: uid,
     isParent: false,
     role: 'Edit',
     establishment: {
---
+++ server/test/unit/routes/establishments/permissions.spec.js
@@ -0,0 +1,81 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const httpMocks = require('node-mocks-http');
+
+const models = require('../../../../models');
+const permissions = require('../../../../routes/establishments/permissions').permissions;
+const buildUser = require('../../../factories/user');
+const {establishmentBuilder} = require('../../../factories/models');
+
+describe('permissions route', () => {
+  const user = buildUser();
+
+  user.created = {
+    toJSON: () => {}
+  };
+  user.updated = {
+    toJSON: () => {}
+  };
+
+  sinon.stub(models.user, 'findOne').callsFake(() => {
+    return user;
+  });
+  sinon.stub(models.establishment, 'findOne').callsFake(() => {
+    return establishmentBuilder();
+  });
+  sinon.stub(models.Approvals, 'findOne').callsFake(() => {
+    return null;
+  });
+
+  it('should return canBecomeParent permission if no pending requests', async () => {
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: `/api/establishment/${user.establishmentId}/permissions`,
+    });
+
+    req.username = user.username;
+    req.establishmentId = user.establishmentId;
+    req.userUid = user.userUid;
+    req.role = user.role;
+    req.isParent = user.isParent;
+    req.establishment = {
+      id: user.establishmentId,
+      uid:  user.establishment.uid
+    };
+
+    const res = httpMocks.createResponse();
+
+    await permissions(req, res);
+
+    const permissionData = res._getJSONData();
+
+    expect(res.statusCode).to.deep.equal(200);
+    expect(permissionData.permissions.canBecomeAParent).to.deep.equal(true);
+  });
+
+  it('should not return canBecomeParent permission if pending requests', async () => {
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: `/api/establishment/${user.establishmentId}/permissions`,
+    });
+
+    req.username = user.username;
+    req.establishmentId = user.establishmentId;
+    req.userUid = user.userUid;
+    req.role = user.role;
+    req.isParent = user.isParent;
+    req.establishment = {
+      id: user.establishmentId,
+      uid:  user.establishment.uid
+    };
+
+    const res = httpMocks.createResponse();
+
+    await permissions(req, res);
+
+    const permissionData = res._getJSONData();
+
+    expect(res.statusCode).to.deep.equal(200);
+    expect(permissionData.permissions.canBecomeAParent).to.deep.equal(false);
+  });
+});
