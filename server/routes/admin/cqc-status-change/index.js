c395d68eb96bb72a69f4d92d320e60d12cc6046f
George <george.taylor@madetech.com>
2020-06-04 12:13:51 +0100

    Adding the services

---
+++ server/routes/admin/cqc-status-change/index.js
@@ -0,0 +1,137 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../../../models');
+const moment = require('moment-timezone');
+const config = require('../../../config/config');
+//const notifications = require('../../../data/notifications');
+const uuid = require('uuid');
+
+const cqcStatusChangeApprovalConfirmation = 'CQC status change approved';
+const cqcStatusChangeRejectionConfirmation = 'CQC status change rejected';
+
+const getCqcStatusChanges = async (req, res) => {
+  console.log('CALEED getCQCSTATUS');
+  try {
+    let approvalResults = await models.Approvals.findAllPending('CqcStatusChange');
+    let cqcStatusChanges = await _mapResults(approvalResults);
+    return res.status(200).json(cqcStatusChanges);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send();
+  }
+};
+
+const cqcStatusChanges = async (req, res) => {
+  try {
+    if (req.body.approve) {
+      await _approveChange(req, res);
+    } else {
+      await _rejectChange(req, res);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send();
+  }
+};
+
+const _mapResults = async (approvalResults) => {
+    const promises = approvalResults.map( async approval => {
+        data = approval.Data;
+        const currentServiceID = data.currentService || null;
+        const requestedServiceID = data.requestedService || null;
+        if (!currentServiceID || !requestedServiceID) throw `Can't find request data with ID ${approval.id}`;
+        const currentServiceName = await models.services.findNameByID(currentServiceID);
+        const requestedServiceName = await models.services.findNameByID(requestedServiceID);
+        return {
+          requestId: approval.ID,
+          requestUUID: approval.UUID,
+          establishmentId: approval.EstablishmentID,
+          establishmentUid: approval.Establishment.uid,
+          userId: approval.UserID,
+          workplaceId: approval.Establishment.nmdsId,
+          userName: approval.User.FullNameValue,
+          orgName: approval.Establishment.NameValue,
+          requested: moment.utc(approval.createdAt).tz(config.get('timezone')).format('D/M/YYYY h:mma'),
+          currentService: {
+            ID: currentServiceID,
+            name: currentServiceName.name
+          },
+          requestedService: {
+            ID: requestedServiceID,
+            name: requestedServiceName.name
+          }
+        };
+    });
+    return await Promise.all(promises);
+
+
+};
+
+const _approveChange = async (req, res) => {
+  // await _notify(req.body.cqcStatusChangeId, req.userUid, req.body.establishmentId);
+  await _updateApprovalStatus(req.body.cqcStatusChangeRequestId, 'Approved');
+  await _updateMainService(req.body.cqcStatusChangeRequestId);
+
+  return res.status(200).json({ status: '0', message: cqcStatusChangeApprovalConfirmation });
+};
+
+const _rejectChange = async (req, res) => {
+  // await _notify(req.body.cqcStatusChangeId, req.userUid, req.body.establishmentId);
+  await _updateApprovalStatus(req.body.cqcStatusChangeId, 'Rejected');
+
+  return res.status(200).json({ status: '0', message: cqcStatusChangeRejectionConfirmation });
+};
+
+const _updateApprovalStatus = async (approvalId, status) => {
+  let singleApproval = await models.Approvals.findbyId(approvalId);
+  if (singleApproval) {
+    singleApproval.Status = status;
+    await singleApproval.save();
+  } else {
+    throw `Can't find approval item with id ${approvalId}`;
+  }
+};
+
+const _updateMainService = async (approvalId) => {
+  let singleApproval = await models.Approvals.findbyId(approvalId);
+  let workplace = await models.establishment.findbyId(singleApproval.EstablishmentID);
+  let requestedServiceFK = singleApproval.data.requestedService;
+  if (workplace) {
+    workplace.MainServiceFKValue = requestedServiceFK;
+    await workplace.save();
+  } else {
+    throw `Can't find workplace with id ${id}`;
+  }
+};
+
+//
+// const _notify = async (approvalId, userUid, establishmentId) => {
+//   const approval = await models.Approvals.findbyId(approvalId);
+//   const typUid = approval.UUID;
+//   const params = {
+//     type: 'CqcStatusChange',
+//     typeUid: typUid,
+//     userUid: userUid
+//   };
+//   const users = await notifications.getAllUser({establishmentId: establishmentId});
+//   await Promise.all(users.map(async (user) => {
+//     const userparams = {
+//       ...params,
+//       recipientUserUid: user.UserUID,
+//       notificationUid: uuid.v4(),
+//     };
+//     await notifications.insertNewNotification(userparams);
+//   }));
+// };
+
+router.route('/').post(cqcStatusChanges);
+router.route('/').get(getCqcStatusChanges);
+
+
+module.exports = router;
+module.exports.cqcStatusChanges = cqcStatusChanges;
+module.exports.getCqcStatusChanges = getCqcStatusChanges;
+
+
+module.exports.cqcStatusChangeApprovalConfirmation = cqcStatusChangeApprovalConfirmation;
+module.exports.cqcStatusChangeRejectionConfirmation = cqcStatusChangeRejectionConfirmation;
---
+++ src/app/core/services/cqc-status-change.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {  CqcStatusChanges } from '@core/model/cqc-status-changes.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CqcStatusChangeService {
+  constructor(private http: HttpClient) {}
+
+  public getCqcStatusChange(): Observable<CqcStatusChanges[]> {
+    console.log("calling /api/admin/cqc-status-change/");
+    return this.http.get<CqcStatusChanges[]>('/api/admin/cqc-status-change/');
+  }
+
+  public getCqcStatusChangeByEstablishmentId(establishmentId: number): Observable<boolean> {
+    return this.http.get<boolean>(`/api/approvals/cqc-status-change/establishment/${establishmentId}`);
+  }
+
+  public cqcStatusRequested(establishmentId: number): Observable<boolean> {
+    return this.getCqcStatusChangeByEstablishmentId(establishmentId)
+      .pipe(map(result => result != null));
+  }
+
+  public CqcStatusChangeApproval(data: object) {
+    return this.http.post<any>('/api/admin/cqc-approval/', data);
+  }
+
+  public CqcStatusChange() {
+    return this.http.post<any>('/api/approvals/cqc-status-change/', null);
+  }
+}
