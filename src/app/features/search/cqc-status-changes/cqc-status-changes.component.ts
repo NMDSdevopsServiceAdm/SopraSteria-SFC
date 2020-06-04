fa56220f3cdcd00a9dc94a88ac0dc315995c5a60
George <george.taylor@madetech.com>
2020-06-04 12:13:18 +0100

    Adding the views to see the CQC Status Changes
    adding the tab in the search screen.   Adding the Search search components to show up the list of status changes.    Adding the approval / rejection model.   Adding the link to the dashboard#home . adding routings

---
+++ src/app/features/search/cqc-status-changes/cqc-status-changes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CqcStatusChangeService } from '@core/services/cqc-status-change.service';
+
+@Component({
+  selector: 'app-cqc-status-changes',
+  templateUrl: './cqc-status-changes.component.html'
+})
+export class CqcStatusChangesComponent implements OnInit {
+  public cqcStatusChanges = [];
+
+  constructor(
+    public cqcStatusChangeService: CqcStatusChangeService
+  ) {}
+
+  ngOnInit() {
+    this.getCqcStatusChanges();
+  }
+
+  public getCqcStatusChanges() {
+    console.log("calling cqc service");
+    this.cqcStatusChangeService.getCqcStatusChange().subscribe(
+      data => {
+        this.cqcStatusChanges = data;
+        console.log("___________----------------------------------");
+        console.log(data);
+
+      },
+      error => this.onError(error)
+    );
+  }
+
+  public removeCqcStatusChanges(index: number) {
+    this.cqcStatusChanges.splice(index, 1);
+  }
+
+  private onError(error) {}
+}
