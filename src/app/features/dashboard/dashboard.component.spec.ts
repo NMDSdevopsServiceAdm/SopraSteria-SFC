6f2ae01c661141746ac023bc16a112d2e9c0d682
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-06-26 11:31:31 +0100

    fix(deps): Fix tests

--- src/app/features/dashboard/dashboard.component.spec.ts
+++ src/app/features/dashboard/dashboard.component.spec.ts
@@ -1,24 +1,24 @@
-import { DashboardComponent } from '@features/dashboard/dashboard.component';
-import { render, within } from '@testing-library/angular';
-import { HomeTabComponent } from '@features/dashboard/home-tab/home-tab.component';
-import { SharedModule } from '@shared/shared.module';
-import { Router, RouterModule } from '@angular/router';
-import { WindowRef } from '@core/services/window.ref';
-import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { getTestBed } from '@angular/core/testing';
+import { Router, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '@core/services/auth.service';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { NotificationsService } from '@core/services/notifications/notifications.service';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
 import { UserService } from '@core/services/user.service';
-import { NotificationsService } from '@core/services/notifications/notifications.service';
-import { MockPermissionsService } from '@core/test-utils/MockPermissionsService';
-import { MockNotificationsService } from '@core/test-utils/MockNotificationsService';
-import { MockUserService } from '@core/test-utils/MockUserService';
-import { HttpClient } from '@angular/common/http';
-import { EstablishmentService } from '@core/services/establishment.service';
-import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
-import { getTestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
-import { AuthService } from '@core/services/auth.service';
+import { WindowRef } from '@core/services/window.ref';
 import { MockAuthService } from '@core/test-utils/MockAuthService';
+import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
+import { MockNotificationsService } from '@core/test-utils/MockNotificationsService';
+import { MockPermissionsService } from '@core/test-utils/MockPermissionsService';
+import { MockUserService } from '@core/test-utils/MockUserService';
+import { DashboardComponent } from '@features/dashboard/dashboard.component';
+import { HomeTabComponent } from '@features/dashboard/home-tab/home-tab.component';
+import { SharedModule } from '@shared/shared.module';
+import { render, within } from '@testing-library/angular';
+import { of } from 'rxjs';
 
 describe('DashboardComponent', () => {
   async function setup(isAdmin = true, subsidiaries = 0) {
@@ -63,8 +63,8 @@ describe('DashboardComponent', () => {
     });
 
     const injector = getTestBed();
-    const establishmentService = injector.get(EstablishmentService) as EstablishmentService;
-    const router = injector.get(Router) as Router;
+    const establishmentService = injector.inject(EstablishmentService) as EstablishmentService;
+    const router = injector.inject(Router) as Router;
 
     return {
       component,
