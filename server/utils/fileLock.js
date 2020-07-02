bdcedf197c5631b7b4be2effb010b4eacbc73fb3
George <george.taylor@madetech.com>
2020-07-02 08:11:16 +0100

    formatting and removing ability to use unlock function as a func

--- server/utils/fileLock.js
+++ server/utils/fileLock.js
@@ -3,30 +3,30 @@ const config = require('../config/config');
 const models = require('../models/');
 const uuid = require('uuid');
 const s3 = new AWS.S3({
-  region: String(config.get('bulkupload.region')),
+  region: String(config.get('bulkupload.region'))
 });
 const Bucket = String(config.get('bulkupload.bucketname'));
 // Prevent multiple report requests from being ongoing simultaneously so we can store what was previously the http responses in the S3 bucket
 // This function can't be an express middleware as it needs to run both before and after the regular logic
-const reportsAvailable = ['la','training'];
+const reportsAvailable = ['la', 'training'];
 
-const acquireLock = async function(reportType,logic, req, res) {
+const acquireLock = async function(reportType, logic, req, res) {
   const { establishmentId } = req;
-  if (!reportsAvailable.includes(reportType)){
+  if (!reportsAvailable.includes(reportType)) {
     console.error('Lock *NOT* acquired.');
 
     res.status(500).send({
-      message: `reportType not correct`,
+      message: `reportType not correct`
     });
     return;
   }
-  const LockHeldTitle = reportType + "ReportLockHeld";
+  const LockHeldTitle = reportType + 'ReportLockHeld';
   req.startTime = new Date().toISOString();
   // attempt to acquire the lock
-  const currentLockState =  await models.establishment.update(
+  const currentLockState = await models.establishment.update(
     {
       [LockHeldTitle]: true
-    },{
+    }, {
       where: {
         id: establishmentId,
         [LockHeldTitle]: false
@@ -39,7 +39,7 @@ const acquireLock = async function(reportType,logic, req, res) {
   if (currentLockState[1] === 0) {
     console.error('Lock *NOT* acquired.');
     res.status(409).send({
-      message: `The lock for establishment ${establishmentId} was not acquired as it's already being held by another ongoing process.`,
+      message: `The lock for establishment ${establishmentId} was not acquired as it's already being held by another ongoing process.`
     });
     return;
   }
@@ -47,43 +47,42 @@ const acquireLock = async function(reportType,logic, req, res) {
   req.buRequestId = String(uuid()).toLowerCase();
   res.status(200).send({
     message: `Lock for establishment ${establishmentId} acquired.`,
-    requestId: req.buRequestId,
+    requestId: req.buRequestId
   });
 
   // run whatever the original logic was
   try {
     await logic(req, res);
-  } catch (e) {}
+  } catch (e) {
+  }
 
   // release the lock
-  await releaseLock(reportType,req, null, null);
+  await releaseLock(reportType, req, null, null);
 };
 
-const releaseLock = async (reportType,req, res, next) => {
+const releaseLock = async (reportType, req, res, next) => {
   const establishmentId = req.query.subEstId || req.establishmentId;
-  if (!reportsAvailable.includes(reportType)){
+  if (!reportsAvailable.includes(reportType)) {
     console.error('Lock *NOT* acquired.');
     res.status(500).send({
-      message: `reportType not correct`,
+      message: `reportType not correct`
     });
     return;
   }
-  const LockHeldTitle = reportType + "ReportLockHeld";
+  const LockHeldTitle = reportType + 'ReportLockHeld';
   if (Number.isInteger(establishmentId)) {
-        await models.establishment.update(
-          {
-            [LockHeldTitle]: false,
-          },{
-            where: {
-              id: establishmentId,
-            }
-          });
+    await models.establishment.update(
+      {
+        [LockHeldTitle]: false
+      }, {
+        where: {
+          id: establishmentId
+        }
+      });
   }
-  if (res !== null) {
-    res.status(200).send({
-      establishmentId,
-    });
-  }
+  res.status(200).send({
+    establishmentId
+  });
 };
 
 const saveResponse = async (req, res, statusCode, body, headers) => {
@@ -110,14 +109,14 @@ const responseGet = async (req, res) => {
   const buRequestId = String(req.params.buRequestId).toLowerCase();
   if (!uuidRegex.test(buRequestId)) {
     res.status(400).send({
-      message: 'request id must be a uuid',
+      message: 'request id must be a uuid'
     });
     return;
   }
   try {
     var data = await s3.getObject({
       Bucket,
-      Key: `${req.establishmentId}/intermediary/${buRequestId}.json`,
+      Key: `${req.establishmentId}/intermediary/${buRequestId}.json`
     }).promise();
     const jsonData = JSON.parse(data.Body.toString());
     if (Number.isInteger(jsonData.responseCode) && jsonData.responseCode > 99) {
@@ -132,28 +131,28 @@ const responseGet = async (req, res) => {
     } else {
       throw new Error('Response code was not numeric');
     }
-  } catch(err) {
-      console.error('Report::responseGet: getting data returned an error:', err);
-      res.status(404).send({
-        message: 'Not Found',
-      });
+  } catch (err) {
+    console.error('Report::responseGet: getting data returned an error:', err);
+    res.status(404).send({
+      message: 'Not Found'
+    });
   }
 };
 
-const lockStatusGet = async (reportType,req, res) => {
+const lockStatusGet = async (reportType, req, res) => {
   const { establishmentId } = req;
-  if (!reportsAvailable.includes(reportType)){
+  if (!reportsAvailable.includes(reportType)) {
     console.error('Lock *NOT* acquired.');
     res.status(500).send({
-      message: `reportType not correct`,
+      message: `reportType not correct`
     });
     return;
   }
-  const LockHeldTitle = reportType.charAt(0).toUpperCase() + reportType.slice(1) + "ReportLockHeld";
-  const currentLockState =  await models.establishment.findAll({
-    attributes: [['EstablishmentID', 'establishmentId'],[LockHeldTitle,'reportLockHeld']],
+  const LockHeldTitle = reportType.charAt(0).toUpperCase() + reportType.slice(1) + 'ReportLockHeld';
+  const currentLockState = await models.establishment.findAll({
+    attributes: [['EstablishmentID', 'establishmentId'], [LockHeldTitle, 'reportLockHeld']],
     where: {
-      id: establishmentId,
+      id: establishmentId
     }
   }).then(res => {
     return res.map(row => {
@@ -163,7 +162,7 @@ const lockStatusGet = async (reportType,req, res) => {
   if (currentLockState.length >= 0) {
     return res.status(200).send(currentLockState[0]);
   }
-  return res.status(200).send({establishmentId, reportLockHeld: true});
+  return res.status(200).send({ establishmentId, reportLockHeld: true });
 };
 module.exports.acquireLock = acquireLock;
 module.exports.releaseLock = releaseLock;
