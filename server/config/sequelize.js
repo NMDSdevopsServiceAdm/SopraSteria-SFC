8a6069f0b3c7add6456ba9ee37557f8c0e249be5
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-07-23 14:38:56 +0100

    feat(running-migrations-in-ci): Allow async config for AWS

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -1,56 +1,75 @@
-const appConfig = require('./config');
+const config = require('./config');
+const AWSSecrets = require('../aws/secrets');
 
-module.exports = {
-  production: {
-    use_env_variable: 'DATABASE_URL',
-  },
-  preproduction: {
-    use_env_variable: 'DATABASE_URL',
-  },
-  test: {
-    username: appConfig.get('db.username'),
-    password: appConfig.get('db.password'),
-    database: appConfig.get('db.database'),
-    host: appConfig.get('db.host'),
-    port: appConfig.get('db.port'),
-    dialect: appConfig.get('db.dialect'),
-    migrationStorageTableSchema: 'cqc',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized : false,
-        ca   : appConfig.get('db.client_ssl.data.ca'),
-        key  : appConfig.get('db.client_ssl.data.key'),
-        cert : appConfig.get('db.client_ssl.data.certificate'),
-      }
+module.exports = async () => {
+  if (config.get('aws.secrets.use')) {
+    await AWSSecrets.initialiseSecrets(
+      config.get('aws.region'),
+      config.get('aws.secrets.wallet')
+    );
+
+    config.set('db.host', AWSSecrets.dbHost());
+    config.set('db.password', AWSSecrets.dbPass());
+
+    if (config.get('db.client_ssl.status')) {
+      config.set('db.client_ssl.data.certificate', AWSSecrets.dbAppUserCertificate().replace(/\\n/g, "\n"));
+      config.set('db.client_ssl.data.key', AWSSecrets.dbAppUserKey().replace(/\\n/g, "\n"));
+      config.set('db.client_ssl.data.ca', AWSSecrets.dbAppRootCertificate().replace(/\\n/g, "\n"));
     }
-  },
-  development: {
-    username: appConfig.get('db.username'),
-    password: appConfig.get('db.password'),
-    database: appConfig.get('db.database'),
-    host: appConfig.get('db.host'),
-    port: appConfig.get('db.port'),
-    dialect: appConfig.get('db.dialect'),
-    migrationStorageTableSchema: 'cqc',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized : false,
-        ca   : appConfig.get('db.client_ssl.data.ca'),
-        key  : appConfig.get('db.client_ssl.data.key'),
-        cert : appConfig.get('db.client_ssl.data.certificate'),
+  }
+
+  return {
+    production: {
+      use_env_variable: 'DATABASE_URL',
+    },
+    preproduction: {
+      use_env_variable: 'DATABASE_URL',
+    },
+    test: {
+      username: config.get('db.username'),
+      password: config.get('db.password'),
+      database: config.get('db.database'),
+      host: config.get('db.host'),
+      port: config.get('db.port'),
+      dialect: config.get('db.dialect'),
+      migrationStorageTableSchema: 'cqc',
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized : false,
+          ca   : config.get('db.client_ssl.data.ca'),
+          key  : config.get('db.client_ssl.data.key'),
+          cert : config.get('db.client_ssl.data.certificate'),
+        }
       }
-    }
-  },
-  localhost: {
-    username: appConfig.get('db.username'),
-    password: appConfig.get('db.password'),
-    database: appConfig.get('db.database'),
-    host: appConfig.get('db.host'),
-    port: appConfig.get('db.port'),
-    dialect: appConfig.get('db.dialect'),
-    migrationStorageTableSchema: 'cqc',
-    dialectOptions: {
-      ssl: appConfig.get('db.ssl'),
+    },
+    development: {
+      username: config.get('db.username'),
+      password: config.get('db.password'),
+      database: config.get('db.database'),
+      host: config.get('db.host'),
+      port: config.get('db.port'),
+      dialect: config.get('db.dialect'),
+      migrationStorageTableSchema: 'cqc',
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized : false,
+          ca   : config.get('db.client_ssl.data.ca'),
+          key  : config.get('db.client_ssl.data.key'),
+          cert : config.get('db.client_ssl.data.certificate'),
+        }
+      }
+    },
+    localhost: {
+      username: config.get('db.username'),
+      password: config.get('db.password'),
+      database: config.get('db.database'),
+      host: config.get('db.host'),
+      port: config.get('db.port'),
+      dialect: config.get('db.dialect'),
+      migrationStorageTableSchema: 'cqc',
+      dialectOptions: {
+        ssl: config.get('db.ssl'),
+      }
     }
   }
 }
