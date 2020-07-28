cf7e579802bf487830154f24d44818e8d52dcaca
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-07-28 13:56:53 +0100

    Changed fdescribe to describe

--- src/app/shared/components/benchmark-tile/benchmark-tile.component.spec.ts
+++ src/app/shared/components/benchmark-tile/benchmark-tile.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BenchmarkTileComponent } from './benchmark-tile.component';
 
-fdescribe('BenchmarkTileComponent', () => {
+describe('BenchmarkTileComponent', () => {
   let component: BenchmarkTileComponent;
   let fixture: ComponentFixture<BenchmarkTileComponent>;
 
