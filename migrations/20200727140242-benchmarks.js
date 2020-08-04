c69d07f2666710f12d5509ca56a10af584de15a9
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-08-04 08:49:25 +0100

    Updated column name with FK

--- migrations/20200727140242-benchmarks.js
+++ migrations/20200727140242-benchmarks.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
    return queryInterface.createTable('Benchmarks', {
-    CssrIDFK: {
+    CssrID: {
       type: Sequelize.DataTypes.INTEGER,
       allowNull: false,
     },
