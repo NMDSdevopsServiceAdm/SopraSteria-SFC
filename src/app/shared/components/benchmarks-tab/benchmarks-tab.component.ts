05af5f13e34037e38408a6ac4ded7d893ff1fe01
George <george.taylor@madetech.com>
2020-08-05 08:48:38 +0100

    subscribing to service

--- src/app/shared/components/benchmarks-tab/benchmarks-tab.component.ts
+++ src/app/shared/components/benchmarks-tab/benchmarks-tab.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Establishment } from '@core/model/establishment.model';
+import { Subscription } from 'rxjs';
+import { BenchmarksService } from '@core/services/benchmarks.service';
+import { Benchmarks } from '@core/model/benchmarks.model';
 
 @Component({
   selector: 'app-benchmarks-tab',
   templateUrl: './benchmarks-tab.component.html'
 })
 export class BenchmarksTabComponent implements OnInit, OnDestroy {
-  @Input() workplace: Establishment;
+  protected subscriptions: Subscription = new Subscription();
 
+  @Input() workplace: Establishment;
+  public tilesData: Benchmarks;
   public tileDemo: any = {
     turnover: {
       showYourWorkplace: true,
@@ -27,12 +32,24 @@ export class BenchmarksTabComponent implements OnInit, OnDestroy {
     }
   };
   constructor(
+    private benchmarksService: BenchmarksService
   ) {
 
   }
 
   ngOnInit() {
+    this.subscriptions.add(
+      this.benchmarksService.getAllTiles(this.workplace.uid).subscribe(
+        (data) => {
+          if (data) {
+           if (data.tiles.pay ){
+             this.tilesData.tiles.pay.showYourWorkplace = !data.tiles.pay.workplaceValue.stateMessage;
+             this.tilesData.tiles.pay.showComparisonGroup = !data.tiles.pay.comparisonGroup.stateMessage;
 
+           }
+          }
+        }
+      ))
   }
 
   ngOnDestroy() {
