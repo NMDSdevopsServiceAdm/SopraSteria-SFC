db0954ad498ff40270cd15ecf43cbcc0963acb63
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-08-13 15:04:34 +0100

    feat/back-to-admin: Added tests for admin-user logic around link

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -11,12 +11,18 @@ import { MockUserService } from '@core/test-utils/MockUserService';
 
 describe('HeaderComponent', () => {
 
-  async function setup() {
+  async function setup(isAdmin = true, subsidiaries = 0) {
     const component =  await render(HeaderComponent, {
       imports: [
         RouterTestingModule,
         HttpClientTestingModule],
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
+      providers: [
+        {
+          provide: UserService,
+          useFactory: MockUserService.factory(subsidiaries, isAdmin),
+          deps: [HttpClient]
+        }]
     });
 
     const injector = getTestBed();
@@ -31,5 +37,19 @@ describe('HeaderComponent', () => {
   it('should render a HeaderComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  describe('Back to admin link', () => {
+    it('should display a Back to admin link if user is an admin', async () => {
+      const { component } = await setup(true);
+
+      component.getByText('Back to admin');
+    });
+
+    it('should not display a Back to admin link if user is not an admin', async () => {
+      const { component } = await setup(false);
+
+      expect(component.queryByText('Back to admin')).toBeNull();
+    });
   });
 });
