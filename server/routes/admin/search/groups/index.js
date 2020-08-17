f497e4f75f9058c2970d949dda84eb89328529b2
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-08-17 10:52:03 +0100

    updated endpoint to reply with establishments

--- server/routes/admin/search/groups/index.js
+++ server/routes/admin/search/groups/index.js
@@ -4,10 +4,13 @@ const models = require('../../../../models');
 
 const search = async function (req, res) {
   try {
-
-    return res.status(200).json();
+    const searchFields = req.body;
+    const establishments = await models.establishment.byEstablishmentType(searchFields.employerType);
+    // console.log(JSON.stringify(establishments));
+    return res.status(200).json(establishments);
   } catch (err) {
-    console.error(err);
+    console.error(err.type)
+    console.error(err.message);
     return res.status(503).send();
   }
 };
