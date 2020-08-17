f69538512e9d430ece801d2461f4d64cdbc32f73
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-08-17 15:20:04 +0100

    Added test for if parent is true

--- server/test/unit/routes/admin/search/groups/index.spec.js
+++ server/test/unit/routes/admin/search/groups/index.spec.js
@@ -58,17 +58,13 @@ describe('server/routes/admin/search/groups', () => {
       method: 'GET',
       url: `/api/admin/search`,
       body: {
-        postcode: 'WF14 9TS',
+        employerType: 'Local Authority (generic/other)'
       },
     });
 
     req.role = 'Admin';
     req.establishment = {
       id: establishment.id,
-    };
-
-    req.body = {
-      employerType: 'Local Authority (generic/other)'
     };
 
     const res = httpMocks.createResponse();
@@ -78,6 +74,65 @@ describe('server/routes/admin/search/groups', () => {
     const response = res._getJSONData();
 
     expect(searchEstablishments.called).to.deep.equal(true);
+    expect(response).to.deep.equal([{
+      uid: 'ad3bbca7-2913-4ba7-bb2d-01014be5c48f',
+      name: '123444',
+      nmdsId: 'J1002343',
+      locationId: '1-23456789',
+      address1: '44',
+      address2: 'Grace St',
+      town: 'Leeds',
+      county: 'West Yorkshire',
+      postcode: 'WF14 9TS',
+      employerType: {
+        value: 'Local Authority (generic/other)',
+        other: null
+      },
+      isParent: false,
+      isRegulated: false,
+      dataOwner: 'Workplace',
+      parent: {
+        uid: 'ad3bbca7-2913-4ba7-bb2d-01014be5c49f',
+        nmdsId: 'W-1234567',
+      },
+      users: [
+        {
+          uid: 'ad3bbca7-2913-4ba7-bb2d-01014be5c48f',
+          username: 'thisperson9',
+          name: 'Test Name',
+          securityQuestion: 'Test Security Question',
+          securityAnswer: 'Test Security Question Answer',
+          isLocked: true,
+        },
+      ],
+      lastUpdated: '2019-10-07T08:49:30.625Z'
+    }]);
+  });
+  it('should search establishments by employer type and isParent', async () => {
+    const establishment = establishmentBuilder();
+
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: `/api/admin/search`,
+      body: {
+        employerType: 'Local Authority (generic/other)',
+        parent: true
+      },
+    });
+
+    req.role = 'Admin';
+    req.establishment = {
+      id: establishment.id,
+    };
+
+    const res = httpMocks.createResponse();
+
+    await adminGroupsSearchRoute.search(req, res);
+
+    const response = res._getJSONData();
+
+    expect(searchEstablishments.called).to.deep.equal(true);
+    expect(searchEstablishments.args[0][0].isParent).to.deep.equal(true);
     expect(response).to.deep.equal([{
       uid: 'ad3bbca7-2913-4ba7-bb2d-01014be5c48f',
       name: '123444',
