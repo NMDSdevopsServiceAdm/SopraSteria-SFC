9e11e046e08bd7982cc733631e59403f1a020bec
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-08-18 12:20:21 +0100

    feat(searchEmployerType-tests): Add more tests for the SearchComponent Groups tab

--- src/app/features/search/search.component.spec.ts
+++ src/app/features/search/search.component.spec.ts
@@ -7,36 +7,30 @@ import { render, within } from '@testing-library/angular';
 
 import { SearchComponent } from './search.component';
 
+const getSearchComponent = async () => {
+  return render(SearchComponent, {
+    detectChanges: false,
+    imports: [
+      FormsModule,
+      ReactiveFormsModule,
+      HttpClientTestingModule,
+      SharedModule,
+      RouterTestingModule.withRoutes([
+        { path: 'search-groups', component: SearchComponent }
+      ]),
+    ],
+  });
+}
+
 describe('SearchComponent', () => {
-  it('should render the search component', async () => {
-    await render(SearchComponent, {
-      detectChanges: false,
-      imports: [
-        FormsModule,
-        ReactiveFormsModule,
-        HttpClientTestingModule,
-        SharedModule,
-        RouterTestingModule.withRoutes([
-          { path: 'search-groups', component: SearchComponent }
-        ]),
-      ],
-    });
+  afterEach(() => {
+    const httpTestingController = TestBed.inject(HttpTestingController);
+    httpTestingController.verify();
   });
 
   describe('Groups', () => {
     it('should show the Groups tab', async () => {
-      const { fixture, navigate, getByText } = await render(SearchComponent, {
-        detectChanges: false,
-        imports: [
-          FormsModule,
-          ReactiveFormsModule,
-          HttpClientTestingModule,
-          SharedModule,
-          RouterTestingModule.withRoutes([
-            { path: 'search-groups', component: SearchComponent }
-          ]),
-        ],
-      });
+      const { fixture, navigate, getByText } = await getSearchComponent();
 
       await navigate('search-groups');
 
@@ -46,18 +40,7 @@ describe('SearchComponent', () => {
     });
 
     it('should render the search results when searching for a group', async () => {
-      const { fixture, navigate, click, getByText, getByTestId } = await render(SearchComponent, {
-        detectChanges: false,
-        imports: [
-          FormsModule,
-          ReactiveFormsModule,
-          HttpClientTestingModule,
-          SharedModule,
-          RouterTestingModule.withRoutes([
-            { path: 'search-groups', component: SearchComponent }
-          ]),
-        ],
-      });
+      const { fixture, navigate, click, getByText, getByTestId } = await getSearchComponent();
 
       const httpTestingController = TestBed.inject(HttpTestingController);
 
@@ -73,11 +56,89 @@ describe('SearchComponent', () => {
         name: 'Workplace Name',
       }]);
 
-      httpTestingController.verify();
-
       fixture.detectChanges();
 
       await within(getByTestId('group-search-results')).getByText('Workplace Name');
+    });
+
+    it('should expand the Workplace details when clicking Open', async () => {
+      const { fixture, navigate, click, getByText, getByTestId } = await getSearchComponent();
+
+      const httpTestingController = TestBed.inject(HttpTestingController);
+
+      await navigate('search-groups');
+
+      fixture.detectChanges();
+
+      click(getByText('Search groups'));
+
+      const req = httpTestingController.expectOne('/api/admin/search/groups');
+      req.flush([{
+        uid: 'ad3bbca7-2913-4ba7-bb2d-01014be5c48f',
+        name: 'Workplace Name',
+        address1: '44',
+        address2: 'Grace St',
+        town: 'Leeds',
+        county: 'West Yorkshire',
+        postcode: 'WF14 9TS',
+      }]);
+
+      fixture.detectChanges();
+
+      const searchResults = within(getByTestId('group-search-results'));
+      click(searchResults.getByText('Open'));
+
+      expect(searchResults.getByText('44 Grace St, Leeds, West Yorkshire, WF14 9TS'));
+    });
+
+    it('should collapse the Workplace details when clicking Close', async () => {
+      const { fixture, navigate, click, getByText, getByTestId } = await getSearchComponent();
+
+      const httpTestingController = TestBed.inject(HttpTestingController);
+
+      await navigate('search-groups');
+
+      fixture.detectChanges();
+
+      click(getByText('Search groups'));
+
+      const req = httpTestingController.expectOne('/api/admin/search/groups');
+      req.flush([{
+        uid: 'ad3bbca7-2913-4ba7-bb2d-01014be5c48f',
+        name: 'Workplace Name',
+        address1: '44',
+        address2: 'Grace St',
+        town: 'Leeds',
+        county: 'West Yorkshire',
+        postcode: 'WF14 9TS',
+      }]);
+
+      fixture.detectChanges();
+
+      const searchResults = within(getByTestId('group-search-results'));
+      click(searchResults.getByText('Open'));
+      click(searchResults.getByText('Close'));
+
+      expect(searchResults.queryByTestId('groups-workplace-details')).toBeNull();
+    });
+
+    it('should show a warning when there are no search results', async () => {
+      const { fixture, navigate, click, getByText, getByTestId } = await getSearchComponent();
+
+      const httpTestingController = TestBed.inject(HttpTestingController);
+
+      await navigate('search-groups');
+
+      fixture.detectChanges();
+
+      click(getByText('Search groups'));
+
+      const req = httpTestingController.expectOne('/api/admin/search/groups');
+      req.flush([]);
+
+      fixture.detectChanges();
+
+      expect(getByTestId('no-search-results'));
     });
   });
 });
