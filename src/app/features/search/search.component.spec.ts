6345152a6d27554d3b649273f6ac6890e39868c8
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-08-18 11:06:24 +0100

    feat(searchEmployerType-tests): Add tests for searching by group

--- src/app/features/search/search.component.spec.ts
+++ src/app/features/search/search.component.spec.ts
@@ -1,25 +1,83 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { render, within } from '@testing-library/angular';
 
 import { SearchComponent } from './search.component';
 
-fdescribe('SearchComponent', () => {
+describe('SearchComponent', () => {
   it('should render the search component', async () => {
-    const { click, getByText, debug, fixture } = await render(SearchComponent, {
+    await render(SearchComponent, {
+      detectChanges: false,
       imports: [
+        FormsModule,
         ReactiveFormsModule,
         HttpClientTestingModule,
         SharedModule,
-        RouterTestingModule.withRoutes([{ path: 'search-groups', component: SearchComponent }]),
+        RouterTestingModule.withRoutes([
+          { path: 'search-groups', component: SearchComponent }
+        ]),
       ],
     });
+  });
 
-    click(getByText('Groups'));
-    fixture.detectChanges();
+  describe('Groups', () => {
+    it('should show the Groups tab', async () => {
+      const { fixture, navigate, getByText } = await render(SearchComponent, {
+        detectChanges: false,
+        imports: [
+          FormsModule,
+          ReactiveFormsModule,
+          HttpClientTestingModule,
+          SharedModule,
+          RouterTestingModule.withRoutes([
+            { path: 'search-groups', component: SearchComponent }
+          ]),
+        ],
+      });
 
-    debug();
+      await navigate('search-groups');
+
+      fixture.detectChanges();
+
+      expect(getByText('Search for a group'));
+    });
+
+    it('should render the search results when searching for a group', async () => {
+      const { fixture, navigate, click, getByText, getByTestId } = await render(SearchComponent, {
+        detectChanges: false,
+        imports: [
+          FormsModule,
+          ReactiveFormsModule,
+          HttpClientTestingModule,
+          SharedModule,
+          RouterTestingModule.withRoutes([
+            { path: 'search-groups', component: SearchComponent }
+          ]),
+        ],
+      });
+
+      const httpTestingController = TestBed.inject(HttpTestingController);
+
+      await navigate('search-groups');
+
+      fixture.detectChanges();
+
+      click(getByText('Search groups'));
+
+      const req = httpTestingController.expectOne('/api/admin/search/groups');
+      req.flush([{
+        uid: 'ad3bbca7-2913-4ba7-bb2d-01014be5c48f',
+        name: 'Workplace Name',
+      }]);
+
+      httpTestingController.verify();
+
+      fixture.detectChanges();
+
+      await within(getByTestId('group-search-results')).getByText('Workplace Name');
+    });
   });
 });
