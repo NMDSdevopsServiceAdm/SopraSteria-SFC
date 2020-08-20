a7599778eedf024c95370e4f0ade18eb76955851
George <george.taylor@madetech.com>
2020-08-20 11:42:02 +0100

    removing unneeded import

--- src/app/core/services/benchmarks.service.ts
+++ src/app/core/services/benchmarks.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {  BenchmarksResponse } from '@core/model/benchmarks.model';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { URLStructure } from '@core/model/url.model';
 
 @Injectable({
