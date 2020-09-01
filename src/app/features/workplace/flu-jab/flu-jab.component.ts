171d14d32195767018a121b5fd8f37415352049d
Vivian <vivian.roberts@madetech.com>
2020-09-01 12:05:51 +0100

    Added component and route for workplace flu jabs form

---
+++ src/app/features/workplace/flu-jab/flu-jab.component.ts
@@ -0,0 +1,11 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flu-jab',
+  templateUrl: './flu-jab.component.html'
+})
+export class FluJabComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+}
