5e25d83f2c91876f2ba7325a6adede9915e73de6
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-09-24 14:08:05 +0100

    feat(lngLat): Refactor cssr getAddressAPI

--- server/models/cssr.js
+++ server/models/cssr.js
@@ -1,67 +1,70 @@
 /* jshint indent: 2 */
-const postcodeHelper = require('../utils/getAddressAPI');
+const getAddressAPI = require('../utils/getAddressAPI');
 
-module.exports = function(sequelize, DataTypes) {
-  const CSSR = sequelize.define('cssr', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      field: '"CssrID"'
+module.exports = function (sequelize, DataTypes) {
+  const CSSR = sequelize.define(
+    'cssr',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        field: '"CssrID"',
+      },
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        field: '"CssR"',
+      },
+      localAuthority: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        field: '"LocalAuthority"',
+      },
+      localCustodianCode: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: '"LocalCustodianCode"',
+      },
+      region: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        field: '"Region"',
+      },
+      regionId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: '"RegionID"',
+      },
+      nmdsIdLetter: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        field: '"NmdsIDLetter"',
+      },
     },
-    name: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      field: '"CssR"'
+    {
+      tableName: '"Cssr"',
+      schema: 'cqc',
+      createdAt: false,
+      updatedAt: false,
     },
-    localAuthority: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      field: '"LocalAuthority"'
-    },
-    localCustodianCode: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: '"LocalCustodianCode"'
-    },
-    region: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      field: '"Region"'
-    },
-    regionId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: '"RegionID"'
-    },
-    nmdsIdLetter: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      field: '"NmdsIDLetter"'
-    },
+  );
 
-  }, {
-    tableName: '"Cssr"',
-    schema: 'cqc',
-    createdAt: false,
-    updatedAt: false
-  });
-
-  CSSR.getIdFromDistrict = async function(postcode) {
-    const postcodeData = await postcodeHelper.getAddressAPI.getPostcodeData(postcode);
-    if (!postcodeData || !postcodeData.addresses){
+  CSSR.getIdFromDistrict = async function (postcode) {
+    const postcodeData = await getAddressAPI.getPostcodeData(postcode);
+    if (!postcodeData || !postcodeData.addresses) {
       return false;
     }
     const district = postcodeData.addresses[0].district;
     const cssr = await this.findOne({
       attributes: ['id'],
       where: {
-         LocalAuthority: district
-      }
+        LocalAuthority: district,
+      },
     });
-    if(cssr.id){
+    if (cssr.id) {
       return cssr.id;
-    }else{
+    } else {
       return false;
     }
   };
