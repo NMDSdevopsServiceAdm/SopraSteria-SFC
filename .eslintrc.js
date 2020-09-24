8deea0622170409a79873c4ace0424e2c08b761f
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-09-24 14:06:59 +0100

    feat(lngLat): Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  extends: [
-    'plugin:@angular-eslint/recommended',
-    'prettier',
-  ],
+  env: {
+    node: true,
+    es6: true,
+    mocha: true,
+    jasmine: true,
+    commonjs: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@angular-eslint/recommended', 'prettier'],
   rules: {
-    '@angular-eslint/directive-selector': [
-      'error',
-      { type: 'attribute', prefix: 'app', style: 'camelCase' },
-    ],
-    '@angular-eslint/component-selector': [
-      'error',
-      { type: 'element', prefix: 'app', style: 'kebab-case' },
-    ],
-    "quotes": [2, "single", "avoid-escape"],
+    '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: 'app', style: 'camelCase' }],
+    '@angular-eslint/component-selector': ['error', { type: 'element', prefix: 'app', style: 'kebab-case' }],
+    quotes: [2, 'single', 'avoid-escape'],
   },
 };
