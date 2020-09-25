2e431487324e7210c546a68f664377d44e59af00
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-09-25 12:04:55 +0100

    feat(lint-staged-files): Fix eslint config and ignore cache

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
-  extends: [
-    'plugin:@angular-eslint/recommended',
-    'prettier',
-  ],
-  rules: {
-    '@angular-eslint/directive-selector': [
-      'error',
-      { type: 'attribute', prefix: 'app', style: 'camelCase' },
-    ],
-    '@angular-eslint/component-selector': [
-      'error',
-      { type: 'element', prefix: 'app', style: 'kebab-case' },
-    ],
-    "quotes": [2, "single", "avoid-escape"],
+  env: {
+    node: true,
+    es6: true,
+    mocha: true,
+    jasmine: true,
+    commonjs: true,
   },
+  extends: ['eslint:recommended', 'prettier'],
+  rules: {
+    quotes: [2, 'single', 'avoid-escape'],
+  },
+  overrides: [
+    {
+      files: ['src/**/*.ts'],
+      parserOptions: {
+        project: ['tsconfig.*?.json'],
+        createDefaultProgram: true,
+      },
+      extends: ['plugin:@angular-eslint/recommended'],
+      rules: {
+        '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: 'app', style: 'camelCase' }],
+        '@angular-eslint/component-selector': ['error', { type: 'element', prefix: 'app', style: 'kebab-case' }],
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
+      },
+    },
+  ],
 };
