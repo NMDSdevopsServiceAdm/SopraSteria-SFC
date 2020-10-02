8ea9e9edd0f41de8f880151facfc7bf31f878848
Vivian <vivian.roberts@madetech.com>
2020-10-02 12:47:34 +0100

    remove database logic

--- server/utils/fileLockS3.js
+++ server/utils/fileLockS3.js
@@ -1,16 +1,15 @@
-// const AWS = require('aws-sdk');
-// const config = require('../config/config');
-// const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const config = require('../config/config');
+const uuid = require('uuid');
 
-// const s3 = new AWS.S3({
-//   region: String(config.get('locks.region')),
-// });
-// const Bucket = String(config.get('locks.bucketname'));
+const s3 = new AWS.S3({
+  region: String(config.get('locks.region')),
+});
+const Bucket = String(config.get('locks.bucketname'));
 
-/*const lockExists = async (establishmentId, lockName) => {
+const lockExists = async (establishmentId, lockName) => {
   let lockExists = true;
   try {
-    console.log('Getting HEAD value of object in S3');
     await s3
       .headObject({
         Bucket,
@@ -22,8 +21,6 @@
       lockExists = false;
     }
   }
-
-  console.log('File exists in S3: ', lockExists);
   return lockExists;
 };
 
@@ -38,7 +35,9 @@ const lockAquired = async (establishmentId, lockName) => {
       })
       .promise();
     lockAquired = true;
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+  }
   return lockAquired;
 };
 
@@ -55,70 +54,55 @@ const lockDeleted = async (establishmentId, lockName) => {
     lockDeleted = false;
   }
   return lockDeleted;
-};*/
+};
 
 const fileLockS3 = {
-  acquireLock(lockName, logic, req, res) {
-    //const { establishmentId } = req;
+  async acquireLock(lockName, logic, req, res) {
+    const { establishmentId } = req;
 
-    //const exists = await lockExists(establishmentId, lockName);
+    const exists = await lockExists(establishmentId, lockName);
 
-    req.buRequestId = '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d';
-    res.status(200).send({
-      message: 'Lock for establishment testing acquired.',
-      requestId: req.buRequestId,
-    });
-    return;
+    if (!exists) {
+      if (await lockAquired(establishmentId, lockName)) {
+        req.buRequestId = String(uuid()).toLowerCase();
+        res.status(200).send({
+          message: `Lock for establishment ${establishmentId} acquired.`,
+          requestId: req.buRequestId,
+        });
+      }
+    } else {
+      res.status(409).send({
+        message: `The lock for establishment ${establishmentId} was not acquired as it's already being held by another ongoing process.`,
+      });
+    }
 
-    // if (!exists) {
-    //   if (await lockAquired(establishmentId, lockName)) {
-    //     req.buRequestId = String(uuid()).toLowerCase();
-    //     res.status(200).send({
-    //       message: `Lock for establishment ${establishmentId} acquired.`,
-    //       requestId: req.buRequestId,
-    //     });
-    //   }
-    // } else {
-    //   res.status(409).send({
-    //     message: `The lock for establishment ${establishmentId} was not acquired as it's already being held by another ongoing process.`,
-    //   });
-    // }
+    /*try {
+      //await logic(req, res);
+    } catch (e) {
+      console.log(e);
+    }*/
 
-    // try {
-    //   await logic(req, res);
-    // } catch (e) {
-    //   console.log(e);
-    // }
-
-    // await lockDeleted(establishmentId, lockName);
+    //await lockDeleted(establishmentId, lockName);
   },
 
-  lockStatus(lockName, req, res) {
-    res.status(200).send({ idLockOn: 'testing', reportLockHeld: true });
-    return;
+  async lockStatus(lockName, req, res) {
+    const { establishmentId } = req;
 
-    // console.log('Looking up lock status');
-    // const { establishmentId } = req;
+    const reportLockHeld = await lockExists(establishmentId, lockName);
 
-    // const reportLockHeld = await lockExists(establishmentId, lockName);
-    // console.log('Lock held status: ', reportLockHeld);
-
-    // res.status(200).send({ idLockOn: establishmentId, reportLockHeld });
+    res.status(200).send({ idLockOn: establishmentId, reportLockHeld });
   },
 
   async releaseLock(lockName, req, res) {
-    res.status(200).send({ idLockOn: 'testing', reportLockHeld: true });
-    return;
+    const { establishmentId } = req;
 
-    // const { establishmentId } = req;
-
-    // if (await lockDeleted(establishmentId, lockName)) {
-    //   res.status(200).send({
-    //     iDLockOn: establishmentId,
-    //   });
-    // } else {
-    //   res.status(404).send();
-    // }
+    if (await lockDeleted(establishmentId, lockName)) {
+      res.status(200).send({
+        iDLockOn: establishmentId,
+      });
+    } else {
+      res.status(404).send();
+    }
   },
 };
 
