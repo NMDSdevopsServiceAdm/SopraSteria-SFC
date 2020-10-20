ca24762b7f4bba379eee2c7568e93d2e74f6fba1
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-10-20 10:04:28 +0100

    Added tests for highest and lowest rank

--- src/app/shared/components/benchmark-metric/gauge/gauge.component.spec.ts
+++ src/app/shared/components/benchmark-metric/gauge/gauge.component.spec.ts
@@ -1,28 +1,28 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GaugeComponent } from './gauge.component';
 
-describe('BenchmarksTabComponent', () => {
+fdescribe('BenchmarksTabComponent', () => {
   let component: GaugeComponent;
   let fixture: ComponentFixture<GaugeComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
-      declarations: [],
-      providers: [],
-    }).compileComponents();
-  }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(GaugeComponent);
-
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', async () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show lowest rank', async () => {
+    fixture.detectChanges();
+    expect(fixture.nativeElement.getElementsByClassName('govuk-body')[1].innerText).toEqual('100Lowest ranking');
+  });
+
+  it('should show highest rank', async () => {
+    fixture.detectChanges();
+    expect(fixture.nativeElement.getElementsByClassName('govuk-body')[0].innerText).toEqual('Highest ranking 1');
   });
 });
