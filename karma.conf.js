4a549b6bd694574da24c3001cc2ae1a36c5bdebb
Vivian <vivian.roberts@madetech.com>
2020-10-30 18:19:59 +0000

    Fixed tests to work with lastest karma jasmine version

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ const isCI = require('is-ci');
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 module.exports = function (config) {
-  process.env.CHROME_BIN = require('puppeteer').executablePath()
+  process.env.CHROME_BIN = require('puppeteer').executablePath();
   process.env.NO_PROXY = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
   process.env.no_proxy = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
   config.set({
@@ -14,15 +14,15 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '/fecoverage'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -33,12 +33,12 @@ module.exports = function (config) {
     browsers: ['HeadlessChrome'],
     customLaunchers: {
       HeadlessChrome: {
-            base: 'ChromeHeadless',
-            flags: ['--no-sandbox']
-        }
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
     },
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 3,
-  	browserNoActivityTimeout: 100000
+    browserNoActivityTimeout: 100000,
   });
 };
