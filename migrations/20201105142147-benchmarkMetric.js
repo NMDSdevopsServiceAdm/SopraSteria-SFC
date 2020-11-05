9b14fa7e1f0ecd486d29ca01ea47ca380069389d
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-11-05 17:08:22 +0000

    Added script to add comparison tables and group details

---
+++ migrations/20201105142147-benchmarkMetric.js
@@ -0,0 +1,147 @@
+'use strict';
+
+const columns = [
+  'PayWorkplaces',
+  'PayStaff',
+  'TurnoverWorkplaces',
+  'TurnoverStaff',
+  'SicknessWorkplaces',
+  'SicknessStaff',
+  'QualificationsWorkplaces',
+  'QualificationsStaff',
+];
+
+const benchmarksTable = {
+  tableName: 'Benchmarks',
+  schema: 'cqc',
+};
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const addComparisonColumns = (queryInterface, columnName, transaction) => {
+      return queryInterface.addColumn(
+        benchmarksTable,
+        columnName,
+        {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 0,
+        },
+        { transaction },
+      );
+    };
+
+    const createTable = (queryInterface, tableName, column, transaction) => {
+      return queryInterface.createTable(
+        tableName,
+        {
+          CssrID: {
+            type: Sequelize.DataTypes.INTEGER,
+            allowNull: false,
+          },
+          MainServiceFK: {
+            type: Sequelize.DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: {
+                tableName: 'services',
+                schema: 'cqc',
+              },
+              key: 'reportingID',
+            },
+          },
+          ...column,
+          EstablishmentFK: {
+            type: Sequelize.DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: {
+                tableName: 'Establishment',
+                schema: 'cqc',
+              },
+              key: 'EstablishmentID',
+            },
+          },
+        },
+        {
+          schema: 'cqc',
+          transaction,
+        },
+      );
+    };
+
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        columns.map((column) => addComparisonColumns(queryInterface, column, t)),
+        createTable(
+          queryInterface,
+          'BenchmarksPay',
+          {
+            Pay: {
+              type: Sequelize.DataTypes.INTEGER,
+              allowNull: false,
+            },
+          },
+          t,
+        ),
+        createTable(
+          queryInterface,
+          'BenchmarksSickness',
+          {
+            Sickness: {
+              type: Sequelize.DataTypes.INTEGER,
+              allowNull: false,
+            },
+          },
+          t,
+        ),
+        createTable(
+          queryInterface,
+          'BenchmarksTurnover',
+          {
+            Turnover: {
+              type: Sequelize.DataTypes.DECIMAL(5, 2),
+              allowNull: false,
+            },
+          },
+          t,
+        ),
+        createTable(
+          queryInterface,
+          'BenchmarksQualifications',
+          {
+            Qualifications: {
+              type: Sequelize.DataTypes.DECIMAL(3, 2),
+              allowNull: false,
+            },
+          },
+          t,
+        ),
+      ]);
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.sequelize.transaction((transaction) => {
+      return Promise.all([
+        columns.map((column) => queryInterface.removeColumn(benchmarksTable, column, { transaction })),
+        queryInterface.dropTable({
+          tableName: 'BenchmarksPay',
+          schema: 'cqc',
+        }),
+        queryInterface.dropTable({
+          tableName: 'BenchmarksSickness',
+          schema: 'cqc',
+        }),
+        queryInterface.dropTable({
+          tableName: 'BenchmarksTurnover',
+          schema: 'cqc',
+        }),
+        queryInterface.dropTable({
+          tableName: 'BenchmarksQualifications',
+          schema: 'cqc',
+        }),
+      ]);
+    });
+  },
+};
