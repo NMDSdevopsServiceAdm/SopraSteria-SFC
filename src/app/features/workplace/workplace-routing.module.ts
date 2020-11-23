56556470899a1d494906f956e9c6b74170327889
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-11-23 16:38:54 +0000

    Started adding rankings components

--- src/app/features/workplace/workplace-routing.module.ts
+++ src/app/features/workplace/workplace-routing.module.ts
@@ -10,21 +10,28 @@ import { Roles } from '@core/model/roles.enum';
 import { UserAccountResolver } from '@core/resolvers/user-account.resolver';
 import { WorkplaceResolver } from '@core/resolvers/workplace.resolver';
 import { CreateUserAccountComponent } from '@features/workplace/create-user-account/create-user-account.component';
-import { SelectMainServiceCqcConfirmComponent } from '@features/workplace/select-main-service/select-main-service-cqc-confirm.component';
+import {
+  SelectMainServiceCqcConfirmComponent,
+} from '@features/workplace/select-main-service/select-main-service-cqc-confirm.component';
 import { SelectMainServiceCqcComponent } from '@features/workplace/select-main-service/select-main-service-cqc.component';
-import { UserAccountEditDetailsComponent } from '@features/workplace/user-account-edit-details/user-account-edit-details.component';
+import {
+  UserAccountEditDetailsComponent,
+} from '@features/workplace/user-account-edit-details/user-account-edit-details.component';
 import { UserAccountSavedComponent } from '@features/workplace/user-account-saved/user-account-saved.component';
 import { UserAccountViewComponent } from '@features/workplace/user-account-view/user-account-view.component';
 import { ViewMyWorkplacesComponent } from '@features/workplace/view-my-workplaces/view-my-workplaces.component';
 import { ViewWorkplaceComponent } from '@features/workplace/view-workplace/view-workplace.component';
 import { BenchmarksAboutTheDataComponent } from '@shared/components/benchmarks-tab/about-the-data/about-the-data.component';
 import { BenchmarksMetricComponent } from '@shared/components/benchmarks-tab/metric/metric.component';
+import { BenchmarksRankingsComponent } from '@shared/components/benchmarks-tab/rankings/rankings.component';
 
 import { CheckAnswersComponent } from './check-answers/check-answers.component';
 import { ConfirmLeaversComponent } from './confirm-leavers/confirm-leavers.component';
 import { ConfirmStartersComponent } from './confirm-starters/confirm-starters.component';
 import { ConfirmVacanciesComponent } from './confirm-vacancies/confirm-vacancies.component';
-import { DataSharingWithLocalAuthoritiesComponent } from './data-sharing-with-local-authorities/data-sharing-with-local-authorities.component';
+import {
+  DataSharingWithLocalAuthoritiesComponent,
+} from './data-sharing-with-local-authorities/data-sharing-with-local-authorities.component';
 import { DataSharingComponent } from './data-sharing/data-sharing.component';
 import { EditWorkplaceComponent } from './edit-workplace/edit-workplace.component';
 import { EnterWorkplaceAddressComponent } from './enter-workplace-address/enter-workplace-address.component';
@@ -41,7 +48,9 @@ import { StartersComponent } from './starters/starters.component';
 import { SuccessComponent } from './success/success.component';
 import { TotalStaffQuestionComponent } from './total-staff-question/total-staff-question.component';
 import { TypeOfEmployerComponent } from './type-of-employer/type-of-employer.component';
-import { UserAccountEditPermissionsComponent } from './user-account-edit-permissions/user-account-edit-permissions.component';
+import {
+  UserAccountEditPermissionsComponent,
+} from './user-account-edit-permissions/user-account-edit-permissions.component';
 import { VacanciesComponent } from './vacancies/vacancies.component';
 import { ViewAllMandatoryTrainingComponent } from './view-all-mandatory-trainings/view-all-mandatory-training.component';
 import { WorkplaceNotFoundComponent } from './workplace-not-found/workplace-not-found.component';
@@ -368,6 +377,15 @@ const routes: Routes = [
             canActivate: [CheckPermissionsGuard],
             data: {
               title: 'About the data',
+              permissions: ['canViewBenchmarks'],
+            },
+          },
+          {
+            path: 'rankings',
+            component: BenchmarksRankingsComponent,
+            canActivate: [CheckPermissionsGuard],
+            data: {
+              title: 'Rankings',
               permissions: ['canViewBenchmarks'],
             },
           },
---
+++ src/app/shared/components/benchmarks-tab/rankings/rankings.component.spec.ts
@@ -0,0 +1,135 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MetricsContent } from '@core/model/benchmarks.model';
+import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { MockBreadcrumbService } from '@core/test-utils/MockBreadcrumbService';
+import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
+import { BenchmarksModule } from '@shared/components/benchmarks-tab/benchmarks.module';
+import { BenchmarksRankingsComponent } from '@shared/components/benchmarks-tab/rankings/rankings.component';
+import { render } from '@testing-library/angular';
+import { of } from 'rxjs';
+
+const payTileData = {
+  workplaceValue: { value: 1000, hasValue: true },
+  comparisonGroup: { value: 1100, hasValue: true },
+  goodCqc: { value: 1200, hasValue: true },
+  lowTurnover: { value: 900, hasValue: true },
+};
+
+const noPayTileData = {
+  workplaceValue: { value: null, hasValue: false, stateMessage: 'no-pay-data' },
+  comparisonGroup: { value: null, hasValue: false },
+  goodCqc: { value: null, hasValue: false },
+  lowTurnover: { value: null, hasValue: false },
+};
+
+const payRankingData = {
+  currentRank: 2,
+  maxRank: 3,
+  hasValue: true,
+};
+
+const noPayRankingData = {
+  hasValue: false,
+  stateMessage: 'no-data',
+};
+
+const getBenchmarksRankingsComponent = async () => {
+  return render(BenchmarksRankingsComponent, {
+    imports: [RouterTestingModule, HttpClientTestingModule, BrowserModule, BenchmarksModule],
+    providers: [
+      {
+        provide: EstablishmentService,
+        useClass: MockEstablishmentService,
+      },
+      {
+        provide: BreadcrumbService,
+        useClass: MockBreadcrumbService,
+      },
+      {
+        provide: ActivatedRoute,
+        useValue: {
+          data: of({
+            ...MetricsContent.Pay,
+          }),
+        },
+      },
+    ],
+  });
+};
+
+const setup = (payTile, payRanking) => {
+  const establishmentUid = TestBed.inject(EstablishmentService).establishment.uid;
+  const metric = 'pay';
+
+  const httpTestingController = TestBed.inject(HttpTestingController);
+
+  const req = httpTestingController.expectOne(`/api/establishment/${establishmentUid}/benchmarks/?tiles=${metric}`);
+  req.flush({
+    pay: payTile,
+  });
+
+  const req2 = httpTestingController.expectOne(`/api/establishment/${establishmentUid}/benchmarks/rankings/${metric}`);
+  req2.flush(payRanking);
+};
+
+describe('BenchmarksRankingsComponent', () => {
+  afterEach(() => {
+    const httpTestingController = TestBed.inject(HttpTestingController);
+    httpTestingController.verify();
+  });
+
+  it('should create a barchart with workplace benchmarks data', async () => {
+    const { fixture, getByText } = await getBenchmarksRankingsComponent();
+
+    setup(payTileData, noPayRankingData);
+
+    fixture.detectChanges();
+
+    const yourWorkplace = getByText('£10.00');
+    const comparisonGroup = getByText('£11.00');
+    const goodCqc = getByText('£12.00');
+    const lowTurnover = getByText('£9.00');
+
+    expect(yourWorkplace).toBeTruthy();
+    expect(comparisonGroup).toBeTruthy();
+    expect(goodCqc).toBeTruthy();
+    expect(lowTurnover).toBeTruthy();
+  });
+
+  it('should create a barchart messages when no benchmarks data available', async () => {
+    const { fixture, getByText } = await getBenchmarksRankingsComponent();
+
+    setup(noPayTileData, noPayRankingData);
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const noYourWorkplaceDataMessage = getByText(MetricsContent.Pay.noData['no-pay-data']);
+    const noComparisonGroupsDataMessage = getByText('We do not have enough data to show these comparisons yet.');
+
+    expect(noYourWorkplaceDataMessage).toBeTruthy();
+    expect(noComparisonGroupsDataMessage).toBeTruthy();
+  });
+
+  /*it('should create a gauge with workplace rankings data', async () => {
+    const { fixture, getByText } = await getBenchmarksMetricComponent();
+
+    setup(noPayTileData, payRankingData);
+
+    fixture.detectChanges();
+
+    fixture.whenStable()
+    //const lowestRank = getByText('3Lowest ranking');
+    //const highestRank = getByText('Highest ranking 1');
+    const currentRank = getByText('Highest ranking 1');
+
+    //expect(lowestRank).toBeTruthy();
+    //expect(highestRank).toBeTruthy();
+    expect(currentRank).toBeTruthy();
+  });*/
+});
