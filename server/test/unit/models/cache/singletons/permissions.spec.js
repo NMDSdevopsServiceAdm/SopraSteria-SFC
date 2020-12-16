5ec71e5b527e50168cf24d882d93b92ac2d8fbbf
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-12-16 15:22:57 +0000

    Fixed the unit tests

--- server/test/unit/models/cache/singletons/permissions.spec.js
+++ server/test/unit/models/cache/singletons/permissions.spec.js
@@ -1,22 +1,45 @@
 const expect = require('chai').expect;
-const {PermissionCache} = require('../../../../../models/cache/singletons/permissions');
+const { PermissionCache } = require('../../../../../models/cache/singletons/permissions');
 const buildUser = require('../../../../factories/user');
+const sinon = require('sinon');
+const { Establishment } = require('../../../../../models/classes/establishment');
 
-describe('Permissions Cache', () => {
-  it ('should include become a parent permission if edit user', () => {
-    const user = buildUser();
-    const permissions = PermissionCache.myPermissions(user);
-
-    expect(permissions.filter(permission => permission.code === 'canBecomeAParent').length).to.deep.equal(1);
-  });
-  it ('should not include become a parent permission if read only user', () => {
-    const user = buildUser({
-      overrides: {
-        role: 'Read'
-      }
+describe.only('Permissions Cache', () => {
+  beforeEach(() => {
+    sinon.stub(Establishment.prototype, 'restore');
+    sinon.stub(Establishment.prototype, 'mainService').value({
+      id: 8,
     });
-    const permissions = PermissionCache.myPermissions(user);
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+  it('should include become a parent permission if edit user', async () => {
+    const user = buildUser();
+    const req = {
+      username: user.username,
+      role: 'Edit',
+      establishment: {
+        isSubsidiary: false,
+        isParent: false,
+      },
+    };
+    const permissions = await PermissionCache.myPermissions(req);
+    const filteredPerms = permissions.filter((permission) => Object.keys(permission)[0] === 'canBecomeAParent');
 
-    expect(permissions.filter(permission => permission.code === 'canBecomeAParent').length).to.deep.equal(0);
+    expect(filteredPerms.length).to.deep.equal(1);
+  });
+  it('should not include become a parent permission if read only user', async () => {
+    const req = {
+      role: 'Read',
+      establishment: {
+        isSubsidiary: false,
+        isParent: false,
+      },
+    };
+    const permissions = await PermissionCache.myPermissions(req);
+    const filteredPerms = permissions.filter((permission) => Object.keys(permission)[0] === 'canBecomeAParent');
+
+    expect(filteredPerms.length).to.deep.equal(0);
   });
 });
