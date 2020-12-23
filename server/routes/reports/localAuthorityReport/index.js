e662430999639bbd9b51468c2763afd1b70710bb
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-12-23 16:24:49 +0000

    Added permissions check to reports endpoints

--- server/routes/reports/localAuthorityReport/index.js
+++ server/routes/reports/localAuthorityReport/index.js
@@ -5,15 +5,19 @@ const router = express.Router();
 // endpoint security
 const isAdmin = require('../../../utils/security/isAuthenticated').isAdmin;
 const isAuthorisedEstablishment = require('../../../utils/security/isAuthenticated').hasAuthorisedEstablishment;
-
+const { hasPermission } = require('../../../utils/security/hasPermission');
 const userReport = require('./user');
 const adminReport = require('./admin');
 
 router.use('/admin', [isAdmin, adminReport]);
-router.use('/establishment/:id/user', [isAuthorisedEstablishment, userReport]);
+router.use('/establishment/:id/user', [
+  isAuthorisedEstablishment,
+  hasPermission('canRunLocalAuthorityReport'),
+  userReport,
+]);
 
 router.route('/').get(async (req, res) => {
-    return res.status(501).send();
+  return res.status(501).send();
 });
 
 module.exports = router;
