3f946abfb23461d216176684e5d14afa7cd44d77
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-12-23 16:23:52 +0000

    Added permission check to approvals endpoint

--- server/routes/approvals/index.js
+++ server/routes/approvals/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const models = require('../../models');
 const moment = require('moment-timezone');
 const config = require('../../config/config');
+const { hasPermission } = require('../../utils/security/hasPermission');
 
 const getApprovalRequest = async (req, res) => {
   const establishmentId = req.params.establishmentId;
@@ -23,29 +24,27 @@ const getApprovalRequest = async (req, res) => {
 };
 
 const _mapResults = async (approvalResults) => {
-  return approvalResults.map(approval => {
-      return {
-        requestId: approval.ID,
-        requestUUID: approval.UUID,
-        establishmentId: approval.EstablishmentID,
-        establishmentUid: approval.Establishment.uid,
-        userId: approval.UserID,
-        workplaceId: approval.Establishment.nmdsId,
-        userName: approval.User.FullNameValue,
-        orgName: approval.Establishment.NameValue,
-        requested: moment.utc(approval.createdAt).tz(config.get('timezone')).format('D/M/YYYY h:mma'),
-        data: approval.Data
-      };
-    }
-  );
+  return approvalResults.map((approval) => {
+    return {
+      requestId: approval.ID,
+      requestUUID: approval.UUID,
+      establishmentId: approval.EstablishmentID,
+      establishmentUid: approval.Establishment.uid,
+      userId: approval.UserID,
+      workplaceId: approval.Establishment.nmdsId,
+      userName: approval.User.FullNameValue,
+      orgName: approval.Establishment.NameValue,
+      requested: moment.utc(approval.createdAt).tz(config.get('timezone')).format('D/M/YYYY h:mma'),
+      data: approval.Data,
+    };
+  });
 };
 
 const becomeAParent = require('./becomeAParent');
 router.use('/become-a-parent', becomeAParent);
 
 // urls will look like this: api/approvals/establishment/${establishmentId}?type=becomeaparent&status=pending
-router.route('/establishment/:establishmentId').get(getApprovalRequest);
+router.route('/establishment/:establishmentId').get(hasPermission('canEditEstablishment'), getApprovalRequest);
 
 module.exports = router;
 module.exports.getApprovalRequest = getApprovalRequest;
-
