2dfcb4b962b0667fbc093eaca53c92d68cf8f8ce
duncanc19 <duncancarter93@gmail.com>
2021-01-11 12:39:01 +0000

    Refactor errorReport to use function for s3 call

--- server/routes/establishments/bulkUpload/errorReport.js
+++ server/routes/establishments/bulkUpload/errorReport.js
@@ -6,36 +6,40 @@ const s3 = require('./s3');
 const { buStates } = require('./states');
 const { getErrorWarningArray } = require('../../../utils/errorWarningArray');
 
+const getErrorReport = async (establishmentId) => {
+  const establishmentsReportURI = `${establishmentId}/validation/establishments.validation.json`;
+  const workersReportURI = `${establishmentId}/validation/workers.validation.json`;
+  const trainingReportURI = `${establishmentId}/validation/training.validation.json`;
+
+  const estReportDownload = await s3.downloadContent(establishmentsReportURI);
+  const wrkReportDownload = await s3.downloadContent(workersReportURI);
+  const trainReportDownload = await s3.downloadContent(trainingReportURI);
+
+  const estReport = estReportDownload && estReportDownload.data ? JSON.parse(estReportDownload.data) : [];
+  const wrkReport = wrkReportDownload && wrkReportDownload.data ? JSON.parse(wrkReportDownload.data) : [];
+  const trainReport = trainReportDownload && trainReportDownload.data ? JSON.parse(trainReportDownload.data) : [];
+
+  return {
+    establishments: {
+      errors: getErrorWarningArray(estReport, 'error'),
+      warnings: getErrorWarningArray(estReport, 'warning'),
+    },
+    workers: {
+      errors: getErrorWarningArray(wrkReport, 'error'),
+      warnings: getErrorWarningArray(wrkReport, 'warning'),
+    },
+    training: {
+      errors: getErrorWarningArray(trainReport, 'error'),
+      warnings: getErrorWarningArray(trainReport, 'warning'),
+    },
+  };
+};
+
 const errorReport = async (req, res) => {
-  const establishmentsReportURI = `${req.establishmentId}/validation/establishments.validation.json`;
-  const workersReportURI = `${req.establishmentId}/validation/workers.validation.json`;
-  const trainingReportURI = `${req.establishmentId}/validation/training.validation.json`;
-
   try {
-    const estReportDownload = await s3.downloadContent(establishmentsReportURI);
-    const wrkReportDownload = await s3.downloadContent(workersReportURI);
-    const trainReportDownload = await s3.downloadContent(trainingReportURI);
+    const errorReport = await getErrorReport(req.establishmentId);
 
-    const estReport = estReportDownload && estReportDownload.data ? JSON.parse(estReportDownload.data) : [];
-    const wrkReport = wrkReportDownload && wrkReportDownload.data ? JSON.parse(wrkReportDownload.data) : [];
-    const trainReport = trainReportDownload && trainReportDownload.data ? JSON.parse(trainReportDownload.data) : [];
-
-    const report = {
-      establishments: {
-        errors: getErrorWarningArray(estReport, 'error'),
-        warnings: getErrorWarningArray(estReport, 'warning'),
-      },
-      workers: {
-        errors: getErrorWarningArray(wrkReport, 'error'),
-        warnings: getErrorWarningArray(wrkReport, 'warning'),
-      },
-      training: {
-        errors: getErrorWarningArray(trainReport, 'error'),
-        warnings: getErrorWarningArray(trainReport, 'warning'),
-      },
-    };
-
-    await s3.saveResponse(req, res, 200, report);
+    await s3.saveResponse(req, res, 200, errorReport);
   } catch (error) {
     await s3.saveResponse(req, res, 404);
     throw new Error(error);
