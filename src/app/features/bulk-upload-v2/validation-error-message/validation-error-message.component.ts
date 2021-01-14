9d428a7f63969b29f2f970714673a6fa98d7a037
joannafawl <joanna.fawl@madetech.com>
2021-01-14 14:40:13 +0000

    Update styling

--- src/app/features/bulk-upload-v2/validation-error-message/validation-error-message.component.ts
+++ src/app/features/bulk-upload-v2/validation-error-message/validation-error-message.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-validation-error-message',
   templateUrl: './validation-error-message.component.html',
-  styleUrls: ['./validation-error-message.component.scss'],
+  providers: [],
 })
 export class ValidationErrorMessageComponent {
   @Input() errorMessage: string;
