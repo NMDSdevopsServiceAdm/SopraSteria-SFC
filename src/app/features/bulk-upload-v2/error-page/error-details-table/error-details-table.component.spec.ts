2656a3b4a80cdf0a805a8af7c94774824c7f595c
duncanc19 <duncancarter93@gmail.com>
2021-01-20 12:43:43 +0000

    Add list of errors test to Error Details Table component

--- src/app/features/bulk-upload-v2/error-page/error-details-table/error-details-table.component.spec.ts
+++ src/app/features/bulk-upload-v2/error-page/error-details-table/error-details-table.component.spec.ts
@@ -1,24 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ErrorReportError } from '@core/model/bulk-upload.model';
+import { render } from '@testing-library/angular';
 
 import { ErrorDetailsTableComponent } from './error-details-table.component';
 
+const { build, fake } = require('@jackfranklin/test-data-bot');
+
+const itemBuilder = build('Item', {
+  fields: {
+    lineNumber: fake((f) => f.random.number({ min: 2, max: 10 })),
+    source: '',
+    name: 'SKILLS FOR CARE',
+  },
+});
+
+const errorReportErrorBuilder = build('ErrorReportError', {
+  fields: {
+    error: fake((f) => f.lorem.sentence()),
+    errCode: fake((f) => f.random.number({ min: 1000, max: 9999 })),
+    items: [],
+  },
+});
+
 describe('ErrorDetailsTableComponent', () => {
-  let component: ErrorDetailsTableComponent;
-  let fixture: ComponentFixture<ErrorDetailsTableComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ErrorDetailsTableComponent],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ErrorDetailsTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('should create', async () => {
+    const component = await render(ErrorDetailsTableComponent);
     expect(component).toBeTruthy();
+  });
+
+  it('should show a list of errors for Workplaces', async () => {
+    const errors = [errorReportErrorBuilder()] as ErrorReportError[];
+
+    errors[0].items = [itemBuilder()];
+
+    const fileType = 'Workplace';
+
+    const { getByText } = await render(ErrorDetailsTableComponent, {
+      componentProperties: {
+        errors,
+        fileType,
+      },
+    });
+
+    expect(getByText(errors[0].error, { exact: false }));
   });
 });
