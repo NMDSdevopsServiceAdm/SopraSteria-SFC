bfde171bf4830e5f9281c533ece616e888bf94cb
duncanc19 <duncancarter93@gmail.com>
2021-01-20 16:02:57 +0000

    Add test for opening dropdown - error details table

--- src/app/features/bulk-upload-v2/error-page/error-details-table/error-details-table.component.spec.ts
+++ src/app/features/bulk-upload-v2/error-page/error-details-table/error-details-table.component.spec.ts
@@ -1,5 +1,5 @@
 import { ErrorReportError } from '@core/model/bulk-upload.model';
-import { render } from '@testing-library/angular';
+import { fireEvent, render, within } from '@testing-library/angular';
 
 import { ErrorDetailsTableComponent } from './error-details-table.component';
 
@@ -21,7 +21,7 @@ const errorReportErrorBuilder = build('ErrorReportError', {
   },
 });
 
-describe('ErrorDetailsTableComponent', () => {
+fdescribe('ErrorDetailsTableComponent', () => {
   it('should create', async () => {
     const component = await render(ErrorDetailsTableComponent);
     expect(component).toBeTruthy();
@@ -42,5 +42,27 @@ describe('ErrorDetailsTableComponent', () => {
     });
 
     expect(getByText(errors[0].error, { exact: false }));
+  });
+
+  it('should show a dropdown after clicking Open for Workplaces', async () => {
+    const errors = [errorReportErrorBuilder()] as ErrorReportError[];
+
+    errors[0].items = [itemBuilder()];
+
+    const fileType = 'Workplace';
+
+    const { getByText, queryByText, getByTestId } = await render(ErrorDetailsTableComponent, {
+      componentProperties: {
+        errors,
+        fileType,
+      },
+    });
+
+    const itemMessage = `for workplace called '${errors[0].items[0].name}' on line ${errors[0].items[0].lineNumber}`;
+    expect(queryByText(itemMessage, { exact: false })).toBeNull();
+
+    fireEvent.click(within(getByTestId('error-details-table')).getByText('Open'));
+
+    expect(getByText(itemMessage, { exact: false }));
   });
 });
