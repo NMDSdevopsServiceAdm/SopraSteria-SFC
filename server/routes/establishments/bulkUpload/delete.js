e28744cf6f72d8744a23048ac61f9e491f871b65
joannafawl <joanna.fawl@madetech.com>
2021-01-25 13:43:35 +0000

    Add test for deleting files

--- server/routes/establishments/bulkUpload/delete.js
+++ server/routes/establishments/bulkUpload/delete.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const router = require('express').Router();
+const s3 = require('./s3');
 
-// const deleteFiles = (req,res) => {
-
-// }
-// router.delete('/', deleteFiles)
+const deleteFiles = (req) => {
+  s3.deleteFilesS3(req.establishmentId, req.filename);
+};
+router.delete('/', deleteFiles);
 
 module.exports = router;
-// module.exports.deleteFiles = deleteFiles;
+module.exports.deleteFiles = deleteFiles;
---
+++ server/test/unit/routes/establishments/bulkUpload/s3.spec.js
@@ -0,0 +1,55 @@
+// const expect = require('chai').expect;
+const sinon = require('sinon');
+const { s3, deleteFilesS3 } = require('../../../../../routes/establishments/bulkUpload/s3');
+
+const filesToDelete = [
+  {
+    Key: 'filename1',
+  },
+  {
+    Key: 'filename1.metadata.json',
+  },
+];
+
+const listObjects = {
+  Contents: [
+    ...filesToDelete,
+    {
+      Key: 'filename2',
+    },
+  ],
+};
+
+const deleteFiles = {
+  Bucket: 'sfcbulkuploadfiles',
+  Delete: {
+    Objects: [...filesToDelete],
+    Quiet: true,
+  },
+};
+
+describe('s3', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  beforeEach(() => {});
+  describe('deleteFilesS3', () => {
+    it('should delete the files from s3', async () => {
+      sinon.stub(s3, 'listObjects').returns({
+        promise: async () => {
+          return listObjects;
+        },
+      });
+      const deleteObjects = sinon.stub(s3, 'deleteObjects');
+      deleteObjects.returns({
+        promise: async () => {
+          console.log(deleteFiles);
+          return;
+        },
+      });
+      await deleteFilesS3(123, 'filename1');
+      sinon.assert.calledWith(deleteObjects, deleteFiles);
+    });
+  });
+});
