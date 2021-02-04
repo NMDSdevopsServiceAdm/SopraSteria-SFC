bd80c56c18f92ca05dbf9e593702eca8ea9361e1
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-02-04 16:59:53 +0000

    Added permission service to mocks

--- src/app/shared/components/trianing-link-panel/training-link-panel.component.spec.ts
+++ src/app/shared/components/trianing-link-panel/training-link-panel.component.spec.ts
@@ -1,60 +1,50 @@
-import { DashboardComponent } from '@features/dashboard/dashboard.component';
-import { render, within } from '@testing-library/angular';
-import { HomeTabComponent } from '@features/dashboard/home-tab/home-tab.component';
-import { SharedModule } from '@shared/shared.module';
-import { Router, RouterModule } from '@angular/router';
-import { WindowRef } from '@core/services/window.ref';
-import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { PermissionsService } from '@core/services/permissions/permissions.service';
-import { UserService } from '@core/services/user.service';
-import { NotificationsService } from '@core/services/notifications/notifications.service';
-import { MockPermissionsService } from '@core/test-utils/MockPermissionsService';
-import { MockNotificationsService } from '@core/test-utils/MockNotificationsService';
-import { MockUserService } from '@core/test-utils/MockUserService';
-import { HttpClient } from '@angular/common/http';
-import { EstablishmentService } from '@core/services/establishment.service';
-import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
 import { getTestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
-import { AuthService } from '@core/services/auth.service';
-import { MockAuthService } from '@core/test-utils/MockAuthService';
-import { TrainingLinkPanelComponent } from '@shared/components/trianing-link-panel/trianing-link-panel.component.component';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { PermissionsService } from '@core/services/permissions/permissions.service';
 import { WorkerService } from '@core/services/worker.service';
+import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
+import { MockPermissionsService } from '@core/test-utils/MockPermissionsService';
 import { MockWorkerService } from '@core/test-utils/MockWorkerService';
+import { TrainingLinkPanelComponent } from '@shared/components/trianing-link-panel/trianing-link-panel.component.component';
+import { render } from '@testing-library/angular';
 
 describe('TrainingLinkPanelComponent', () => {
   async function setup() {
-    const component =  await render(TrainingLinkPanelComponent, {
-      imports: [
-        RouterModule,
-        RouterTestingModule,
-        HttpClientTestingModule
-      ],
+    const component = await render(TrainingLinkPanelComponent, {
+      imports: [RouterModule, RouterTestingModule, HttpClientTestingModule],
       providers: [
         {
           provide: EstablishmentService,
-          useClass: MockEstablishmentService
+          useClass: MockEstablishmentService,
         },
         {
           provide: WorkerService,
-          useClass: MockWorkerService
-        }
-      ]
+          useClass: MockWorkerService,
+        },
+        {
+          provide: PermissionsService,
+          useClass: MockPermissionsService,
+        },
+      ],
     });
+    const injector = getTestBed();
+    const establishmentService = injector.inject(EstablishmentService) as EstablishmentService;
 
-    return component;
+    return { component, establishmentService };
   }
 
   it('should render a TrainingLinkPanelComponent', async () => {
-    const component = await setup();
+    const { component } = await setup();
     expect(component).toBeTruthy();
   });
 
   it('should show the latest date of any training record (1/1/20)', async () => {
-    const component = await setup();
+    const { component } = await setup();
 
+    component.fixture.detectChanges();
     component.getByText('Updated 1 January 2020');
   });
 });
-
