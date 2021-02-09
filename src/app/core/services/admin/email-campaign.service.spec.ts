50c766c38f1c516ec46cba8152d4ae6b7560c5ba
Chad Tomkiss <chad.tomkiss@madetech.com>
2021-02-09 15:39:49 +0000

    feat(keep-workplaces-active-endpoints): Fix tests

--- src/app/core/services/admin/email-campaign.service.spec.ts
+++ src/app/core/services/admin/email-campaign.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { EmailCampaignService } from './email-campaign.service';
@@ -6,7 +7,9 @@ describe('EmailCampaignService', () => {
   let service: EmailCampaignService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(EmailCampaignService);
   });
 
