60e02240c31af0fdf4a5187db40ae52b17c61b5e
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-02-10 08:57:24 +0000

    Fixed test failing

--- src/app/shared/components/trianing-link-panel/training-link-panel.component.spec.ts
+++ src/app/shared/components/trianing-link-panel/training-link-panel.component.spec.ts
@@ -1,12 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { UserService } from '@core/services/user.service';
 import { WorkerService } from '@core/services/worker.service';
 import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
 import { MockPermissionsService } from '@core/test-utils/MockPermissionsService';
+import { MockUserService } from '@core/test-utils/MockUserService';
 import { MockWorkerService } from '@core/test-utils/MockWorkerService';
 import { TrainingLinkPanelComponent } from '@shared/components/trianing-link-panel/trianing-link-panel.component.component';
 import { render } from '@testing-library/angular';
@@ -27,6 +30,11 @@ describe('TrainingLinkPanelComponent', () => {
         {
           provide: PermissionsService,
           useClass: MockPermissionsService,
+        },
+        {
+          provide: UserService,
+          useFactory: MockUserService.factory(0, true),
+          deps: [HttpClient],
         },
       ],
     });
