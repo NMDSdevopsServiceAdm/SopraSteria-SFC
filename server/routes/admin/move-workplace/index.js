a13fdde92d3cfe32618dff4f38d3799e149b9b51
Ross McFadyen <rossmcfadyen1967@gmail.com>
2021-02-11 13:39:13 +0000

    Introduce celebrate - tests failing

--- server/routes/admin/move-workplace/index.js
+++ server/routes/admin/move-workplace/index.js
@@ -1,12 +1,42 @@
 'use strict';
 const router = require('express').Router();
+const { celebrate, Joi, errors } = require('celebrate');
 
 const moveWorkplaceAdmin = async (req, res) => {
-  console.log('workplace Admin');
+  // const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/;
+
+  // if (!uuidRegex.test(req.body.parentUid.toUpperCase())) {
+  //   console.error('Invalid Parent UUID');
+  //   return res.status(400).send();
+  // }
+
+  // if (!uuidRegex.test(req.body.subsidUid.toUpperCase())) {
+  //   console.error('Invalid Subsiduary UUID');
+  //   return res.status(400).send();
+  // }
+
   res.status(200).send;
 };
 
-router.route('/').post(moveWorkplaceAdmin);
+router.route('/').post(
+  celebrate({
+    body: Joi.object().keys({
+      parentUid: Joi.string()
+        .uuid({
+          version: ['uuidv4'],
+        })
+        .required(),
+      subsidUid: Joi.string()
+        .uuid({
+          version: ['uuidv4'],
+        })
+        .required(),
+    }),
+  }),
+  moveWorkplaceAdmin,
+);
+
+router.use('/', errors());
 
 module.exports = router;
 module.exports.moveWorkplaceAdmin = moveWorkplaceAdmin;
--- server/test/unit/routes/admin/move-workplace/index.spec.js
+++ server/test/unit/routes/admin/move-workplace/index.spec.js
@@ -9,12 +9,45 @@ describe('server/routes/admin/moveWorkplaceAdmin.js', () => {
     const req = httpMocks.createRequest({
       method: 'POST',
       url: '/api/admin/move-workplace',
-      params: {},
+      body: {
+        parentUid: '4698f4a4-ab82-4906-8b0e-3f4972375927',
+        subsidUid: '1238f4a4-ab82-4906-8b0e-3f4972375927',
+      },
     });
 
     const res = httpMocks.createResponse();
 
     await moveWorkplaceAdmin(req, res);
     expect(res.statusCode).to.deep.equal(200);
+  });
+
+  it('should return status 400 for invalid parentUid', async () => {
+    const req = httpMocks.createRequest({
+      method: 'POST',
+      url: '/api/admin/move-workplace',
+      body: {
+        parentUid: '1',
+      },
+    });
+
+    const res = httpMocks.createResponse();
+
+    await moveWorkplaceAdmin(req, res);
+    expect(res.statusCode).to.deep.equal(400);
+  });
+
+  it('should return status 400 for invalid subsidUID', async () => {
+    const req = httpMocks.createRequest({
+      method: 'POST',
+      url: '/api/admin/move-workplace',
+      body: {
+        subsidUid: '1',
+      },
+    });
+
+    const res = httpMocks.createResponse();
+
+    await moveWorkplaceAdmin(req, res);
+    expect(res.statusCode).to.deep.equal(400);
   });
 });
