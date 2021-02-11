f484f6ab2969c9a7297ae862169771b224edd975
Chad Tomkiss <chad.tomkiss@madetech.com>
2021-02-11 12:00:04 +0000

    feat(keep-workplaces-active-endpoints): Add tests to EmailCampaignService

--- src/app/core/services/admin/email-campaign.service.spec.ts
+++ src/app/core/services/admin/email-campaign.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { EmailCampaignService } from './email-campaign.service';
@@ -14,7 +14,38 @@ describe('EmailCampaignService', () => {
     service = TestBed.inject(EmailCampaignService);
   });
 
+  afterEach(() => {
+    TestBed.inject(HttpTestingController).verify();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get inactive workplaces', () => {
+    service.getInactiveWorkplaces().subscribe();
+
+    const http = TestBed.inject(HttpTestingController);
+    const req = http.expectOne('/api/admin/email-campaigns/inactive-workplaces');
+
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should create a campaign', () => {
+    service.createCampaign().subscribe();
+
+    const http = TestBed.inject(HttpTestingController);
+    const req = http.expectOne('/api/admin/email-campaigns/inactive-workplaces');
+
+    expect(req.request.method).toBe('POST');
+  });
+
+  it('should get the history', () => {
+    service.getHistory().subscribe();
+
+    const http = TestBed.inject(HttpTestingController);
+    const req = http.expectOne('/api/admin/email-campaigns/inactive-workplaces/history');
+
+    expect(req.request.method).toBe('GET');
   });
 });
