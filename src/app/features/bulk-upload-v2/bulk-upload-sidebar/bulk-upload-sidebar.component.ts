ff7858c05d15072c9e842909f7a9ce7eac928ee4
duncanc19 <duncancarter93@gmail.com>
2021-02-18 16:14:14 +0000

    BU(MissingRef): Delete sidebar component and replace with smaller components on bulk-upload page

--- src/app/features/bulk-upload-v2/bulk-upload-sidebar/bulk-upload-sidebar.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-bulk-upload-sidebar',
-  templateUrl: './bulk-upload-sidebar.component.html',
-})
-export class BulkUploadSidebarComponent {}
