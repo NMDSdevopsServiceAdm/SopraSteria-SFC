15761fa1d56cfd62f1472fb73e355c7cd2069fbb
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-02-19 11:07:28 +0000

    Added banners when you have more staff records than total staff (#2993)
    * Added staff mismatch banner to the homepage
    
    * changed permission to canListWorkers and added tests
    
    * Removed f describe
    
    * Added banner to workplace summary if more staff records than total staff
    
    * Fixed orange colour in wokrplace summary
    
    * Changed total staff question content

--- src/app/features/dashboard/home-tab/home-tab.component.ts
+++ src/app/features/dashboard/home-tab/home-tab.component.ts
@@ -62,6 +62,8 @@ export class HomeTabComponent implements OnInit, OnDestroy {
   public canRemoveParentAssociation: boolean;
   public canAddWorker: boolean;
   public workers: any[];
+  public workersCount: number;
+  public canViewListOfWorkers: boolean;
 
   constructor(
     private bulkUploadService: BulkUploadService,
@@ -86,6 +88,7 @@ export class HomeTabComponent implements OnInit, OnDestroy {
         this.subscriptions.add(
           this.workerService.workers$.pipe(filter((workers) => workers !== null)).subscribe((workers) => {
             this.updateStaffRecords = !(workers.length > 0);
+            this.workersCount = workers.length;
           }),
         );
       }
@@ -286,6 +289,7 @@ export class HomeTabComponent implements OnInit, OnDestroy {
     const workplaceUid: string = this.workplace ? this.workplace.uid : null;
     this.canEditEstablishment = this.permissionsService.can(workplaceUid, 'canEditEstablishment');
     this.canAddWorker = this.permissionsService.can(workplaceUid, 'canAddWorker');
+    this.canViewListOfWorkers = this.permissionsService.can(workplaceUid, 'canViewListOfWorkers');
     this.canBulkUpload = this.permissionsService.can(workplaceUid, 'canBulkUpload');
     this.canViewWorkplaces = this.workplace && this.workplace.isParent;
     this.canViewChangeDataOwner =
