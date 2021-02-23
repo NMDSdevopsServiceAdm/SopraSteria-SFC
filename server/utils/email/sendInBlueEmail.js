1247d109b3e5ee502f0fdfd3446b2c1ad2f03b4a
joannafawl <joanna.fawl@madetech.com>
2021-02-23 11:21:24 +0000

    Delete Send In Blue SDK comments

--- server/utils/email/sendInBlueEmail.js
+++ server/utils/email/sendInBlueEmail.js
@@ -2,20 +2,8 @@ const SibApiV3Sdk = require('sib-api-v3-sdk');
 const config = require('../../config/config');
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-// Configure API key authorization: api-key
 const apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = config.get('sendInBlue.apiKey');
-// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-//apiKey.apiKeyPrefix['api-key'] = "Token"
-
-// getAccount(): Get your account information, plan and credits details
-// const api = new SibApiV3Sdk.AccountApi()
-// api.getAccount().then(function(data) {
-//   console.log('API called successfully. Returned data: ' + data);
-// }, function(error) {
-//   console.error(error);
-// });
-
 
 const sendEmail = async (to, templateId, params) => {
   try {
