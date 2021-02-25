966f5f77d1d569bf1f832e6434eba6298021b4b0
duncanc19 <duncancarter93@gmail.com>
2021-02-25 10:49:29 +0000

    fix(buNewUserJourney): Remove unnecessary code from start page ts and change router link

--- src/app/features/bulk-upload-v2/bulk-upload-start-page/bulk-upload-start-page.component.ts
+++ src/app/features/bulk-upload-v2/bulk-upload-start-page/bulk-upload-start-page.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
-import { MissingReferences } from '@core/model/bulk-upload.model';
 import { URLStructure } from '@core/model/url.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
-import { BulkUploadService } from '@core/services/bulk-upload.service';
-import { EstablishmentService } from '@core/services/establishment.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bulk-upload-start-page',
@@ -13,28 +9,10 @@ import { Subscription } from 'rxjs';
 })
 export class BulkUploadStartPageComponent {
   public nextPage: URLStructure = { url: ['/bulk-upload', 'missing-workplace-references'] };
-  private subscriptions: Subscription = new Subscription();
-  public missingRefCount: MissingReferences;
 
-  constructor(
-    private bulkUploadService: BulkUploadService,
-    private establishmentService: EstablishmentService,
-    private breadcrumbService: BreadcrumbService,
-  ) {}
+  constructor(private breadcrumbService: BreadcrumbService) {}
 
   ngOnInit(): void {
     this.breadcrumbService.show(JourneyType.BULK_UPLOAD);
-    this.subscriptions.add(
-      this.bulkUploadService
-        .getMissingRef(this.establishmentService.establishmentId)
-        .subscribe((missingRef: MissingReferences) => {
-          this.missingRefCount = missingRef;
-          this.bulkUploadService.setMissingReferencesNavigation(missingRef.establishmentList);
-        }),
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
   }
 }
