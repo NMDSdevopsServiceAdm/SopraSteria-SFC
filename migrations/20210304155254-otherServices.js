d9b1174598367a297aa346e60abb54c8e3626c65
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-03-04 17:04:24 +0000

    Added other Service Value column to db

---
+++ migrations/20210304155254-otherServices.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const table = {
+  tableName: 'Establishment',
+  schema: 'cqc',
+};
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      try {
+        await Promise.all([
+          queryInterface.addColumn(
+            table,
+            'OtherServicesValue',
+            {
+              type: Sequelize.DataTypes.ENUM,
+              allowNull: true,
+              values: ['Yes', 'No'],
+            },
+            { transaction },
+          ),
+        ]);
+
+        return Promise.resolve();
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await Promise.all([queryInterface.removeColumn(table, 'OtherServicesValue')]);
+  },
+};
