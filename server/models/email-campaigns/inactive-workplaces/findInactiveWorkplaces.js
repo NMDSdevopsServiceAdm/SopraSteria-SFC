232f442ac000c95697bcd874e9200922489610c7
chadtomkiss-madetech <60967748+chadtomkiss-madetech@users.noreply.github.com>
2021-03-05 12:28:31 +0000

    Feat: Calculate which email template to send (#3071)
    * feat(calculate-email-template): Add function to calculate which template to send
    
    * feat(calculate-email-template): Filter workplaces who already have been sent an email
    
    * feat(calculate-email-template): No need for last emailed date and email count
    
    * feat(calculate-email-template): Remove .only
    
    * feat(calculate-email-template): Add config for templates
    
    * feat(calculate-email-template): Use config and fix 24 month check
    
    * feat(calculate-email-template): Make sure it returns null when no match
    
    * feat(calculate-email-template): Update tests

--- server/models/email-campaigns/inactive-workplaces/findInactiveWorkplaces.js
+++ server/models/email-campaigns/inactive-workplaces/findInactiveWorkplaces.js
@@ -1,6 +1,42 @@
 const moment = require('moment');
 
+const config = require('../../../config/config');
 const models = require('../../index');
+
+const nextEmailTemplate = (inactiveWorkplace) => {
+  const lastUpdated = moment(inactiveWorkplace.LastUpdated).startOf('day');
+
+  const sixMonths = moment().startOf('day').subtract(6, 'months');
+  const twelveMonths = moment().startOf('day').subtract(12, 'months');
+  const eighteenMonths = moment().startOf('day').subtract(18, 'months');
+  const twentyFourMonths = moment().startOf('day').subtract(24, 'months');
+
+  if (lastUpdated.isSameOrBefore(sixMonths) && lastUpdated.isAfter(twelveMonths)) {
+    const nextTemplate = config.get('sendInBlue.templates.sixMonthsInactive');
+
+    return inactiveWorkplace.LastTemplate !== nextTemplate ? nextTemplate : null;
+  }
+
+  if (lastUpdated.isSameOrBefore(twelveMonths) && lastUpdated.isAfter(eighteenMonths)) {
+    const nextTemplate = config.get('sendInBlue.templates.twelveMonthsInactive');
+
+    return inactiveWorkplace.LastTemplate !== nextTemplate ? nextTemplate : null;
+  }
+
+  if (lastUpdated.isBefore(twelveMonths) && lastUpdated.isSameOrAfter(eighteenMonths)) {
+    const nextTemplate = config.get('sendInBlue.templates.eighteenMonthsInactive');
+
+    return inactiveWorkplace.LastTemplate !== nextTemplate ? nextTemplate : null;
+  }
+
+  if (lastUpdated.isSameOrBefore(twentyFourMonths)) {
+    const nextTemplate = config.get('sendInBlue.templates.twentyFourMonthsInactive');
+
+    return inactiveWorkplace.LastTemplate !== nextTemplate ? nextTemplate : null;
+  }
+
+  return null;
+};
 
 const findInactiveWorkplaces = async () => {
   await models.sequelize.query('REFRESH MATERIALIZED VIEW cqc."LastUpdatedEstablishments"');
@@ -15,22 +51,15 @@ const findInactiveWorkplaces = async () => {
   "PrimaryUserName",
   "PrimaryUserEmail",
 	"LastUpdated",
-	(
+  (
 		SELECT
-			MAX(ech. "createdAt")
+			"template"
 		FROM
 			cqc."EmailCampaignHistories" ech
 		WHERE
-			ech. "establishmentID" = e. "EstablishmentID") AS "LastEmailedDate", (
-			SELECT
-				COUNT(*)
-			FROM
-				cqc."EmailCampaignHistories" ech
-      JOIN cqc."EmailCampaigns" ec ON ec."id" = ech."emailCampaignID"
-			WHERE
-				ec."type" = 'inactiveWorkplaces'
-				AND ech."establishmentID" = e."EstablishmentID"
-				AND ech."createdAt" > "LastUpdated") AS EmailCount
+			ech. "establishmentID" = e. "EstablishmentID"
+    ORDER BY ech. "createdAt" DESC
+    LIMIT 1) AS "LastTemplate"
 		FROM
 			cqc. "LastUpdatedEstablishments" e
 		WHERE
@@ -49,26 +78,33 @@ const findInactiveWorkplaces = async () => {
 					AND ech. "establishmentID" = e. "EstablishmentID");`,
     {
       type: models.sequelize.QueryTypes.SELECT,
-      replacements: { lastUpdated: moment().subtract(6, 'months').format('YYYY-MM-DD') },
+      replacements: {
+        lastUpdated: moment().subtract(1, 'months').endOf('month').subtract(6, 'months').format('YYYY-MM-DD'),
+      },
     },
   );
 
-  return inactiveWorkplaces.map((inactiveWorkplace) => {
-    return {
-      id: inactiveWorkplace.EstablishmentID,
-      name: inactiveWorkplace.NameValue,
-      nmdsId: inactiveWorkplace.NmdsID,
-      lastUpdated: inactiveWorkplace.LastUpdated,
-      emailTemplateId: 13,
-      dataOwner: inactiveWorkplace.DataOwner,
-      user: {
-        name: inactiveWorkplace.PrimaryUserName,
-        email: inactiveWorkplace.PrimaryUserEmail,
-      },
-    };
-  });
+  return inactiveWorkplaces
+    .filter((inactiveWorkplace) => {
+      return nextEmailTemplate(inactiveWorkplace) !== null;
+    })
+    .map((inactiveWorkplace) => {
+      return {
+        id: inactiveWorkplace.EstablishmentID,
+        name: inactiveWorkplace.NameValue,
+        nmdsId: inactiveWorkplace.NmdsID,
+        lastUpdated: inactiveWorkplace.LastUpdated,
+        emailTemplateId: nextEmailTemplate(inactiveWorkplace),
+        dataOwner: inactiveWorkplace.DataOwner,
+        user: {
+          name: inactiveWorkplace.PrimaryUserName,
+          email: inactiveWorkplace.PrimaryUserEmail,
+        },
+      };
+    });
 };
 
 module.exports = {
   findInactiveWorkplaces,
+  nextEmailTemplate,
 };
--- server/test/unit/models/email-campaigns/inactive-workplaces/findInactiveWorkplaces.spec.js
+++ server/test/unit/models/email-campaigns/inactive-workplaces/findInactiveWorkplaces.spec.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+const moment = require('moment');
 const sinon = require('sinon');
 
 const models = require('../../../../../models');
@@ -14,7 +15,7 @@ describe('server/routes/admin/email-campaigns/inactive-workplaces', () => {
       id: 478,
       name: 'Workplace Name',
       nmdsId: 'J1234567',
-      lastUpdated: '2020-06-01',
+      lastUpdated: moment().subtract(6, 'months').format('YYYY-MM-DD'),
       emailTemplateId: 13,
       dataOwner: 'Workplace',
       user: {
@@ -26,8 +27,8 @@ describe('server/routes/admin/email-campaigns/inactive-workplaces', () => {
       id: 479,
       name: 'Second Workplace Name',
       nmdsId: 'A0012345',
-      lastUpdated: '2020-01-01',
-      emailTemplateId: 13,
+      lastUpdated: moment().subtract(12, 'months').format('YYYY-MM-DD'),
+      emailTemplateId: 14,
       dataOwner: 'Workplace',
       user: {
         name: 'Name McName',
@@ -45,9 +46,8 @@ describe('server/routes/admin/email-campaigns/inactive-workplaces', () => {
         DataOwner: 'Workplace',
         PrimaryUserName: 'Test Name',
         PrimaryUserEmail: 'test@example.com',
-        LastUpdated: '2020-06-01',
-        LastEmailedDate: '2020-12-01',
-        EmailCount: 1,
+        LastUpdated: moment().subtract(6, 'months').format('YYYY-MM-DD'),
+        LastTemplate: null,
       },
       {
         EstablishmentID: 479,
@@ -56,14 +56,186 @@ describe('server/routes/admin/email-campaigns/inactive-workplaces', () => {
         DataOwner: 'Workplace',
         PrimaryUserName: 'Name McName',
         PrimaryUserEmail: 'name@mcname.com',
-        LastUpdated: '2020-01-01',
-        LastEmailedDate: '2020-06-01',
-        EmailCount: 1,
+        LastUpdated: moment().subtract(12, 'months').format('YYYY-MM-DD'),
+        LastTemplate: null,
       },
     ]);
 
     const inactiveWorkplaces = await findInactiveWorkplaces.findInactiveWorkplaces();
 
     expect(inactiveWorkplaces).to.deep.equal(dummyInactiveWorkplaces);
+  });
+
+  [
+    {
+      inactiveMonths: 6,
+      LastUpdated: moment().subtract(6, 'months'),
+      LastTemplate: 13,
+    },
+    {
+      inactiveMonths: 12,
+      LastUpdated: moment().subtract(12, 'months'),
+      LastTemplate: 14,
+    },
+    {
+      inactiveMonths: 18,
+      LastUpdated: moment().subtract(18, 'months'),
+      LastTemplate: 10,
+    },
+    {
+      inactiveMonths: 24,
+      LastUpdated: moment().subtract(24, 'months'),
+      LastTemplate: 12,
+    },
+  ].forEach(({ inactiveMonths, LastUpdated, LastTemplate }) => {
+    it(`should not include a workplace that has already been sent a ${inactiveMonths} month email`, async () => {
+      sinon.stub(models.sequelize, 'query').returns([
+        {
+          EstablishmentID: 478,
+          NameValue: 'Workplace Name',
+          NmdsID: 'J1234567',
+          DataOwner: 'Workplace',
+          PrimaryUserName: 'Test Name',
+          PrimaryUserEmail: 'test@example.com',
+          LastUpdated: LastUpdated,
+          LastTemplate: LastTemplate,
+        },
+      ]);
+
+      const inactiveWorkplaces = await findInactiveWorkplaces.findInactiveWorkplaces();
+      expect(inactiveWorkplaces.length).to.equal(0);
+    });
+  });
+
+  describe('nextEmailTemplate', () => {
+    [
+      {
+        inactiveMonths: 6,
+        LastUpdated: moment().subtract(6, 'months'),
+        NextTemplate: 13,
+      },
+      {
+        inactiveMonths: 12,
+        LastUpdated: moment().subtract(12, 'months'),
+        NextTemplate: 14,
+      },
+      {
+        inactiveMonths: 18,
+        LastUpdated: moment().subtract(18, 'months'),
+        NextTemplate: 10,
+      },
+      {
+        inactiveMonths: 24,
+        LastUpdated: moment().subtract(24, 'months'),
+        NextTemplate: 12,
+      },
+    ].forEach(({ inactiveMonths, LastUpdated, NextTemplate }) => {
+      it(`should return the correct template when ${inactiveMonths} months inactive`, async () => {
+        const inactiveWorkplace = {
+          EstablishmentID: 478,
+          NameValue: 'Workplace Name',
+          NmdsID: 'J1234567',
+          DataOwner: 'Workplace',
+          PrimaryUserName: 'Test Name',
+          PrimaryUserEmail: 'test@example.com',
+          LastUpdated: LastUpdated,
+          LastTemplate: null,
+        };
+
+        const emailTemplateId = await findInactiveWorkplaces.nextEmailTemplate(inactiveWorkplace);
+
+        expect(emailTemplateId).to.equal(NextTemplate);
+      });
+    });
+
+    [
+      {
+        inactiveMonths: 5,
+        LastUpdated: moment().subtract(5, 'months'),
+        LastTemplate: null,
+        NextTemplate: null,
+      },
+      {
+        inactiveMonths: 12,
+        LastUpdated: moment().subtract(12, 'months'),
+        LastTemplate: 13,
+        NextTemplate: 14,
+      },
+      {
+        inactiveMonths: 18,
+        LastUpdated: moment().subtract(18, 'months'),
+        LastTemplate: 14,
+        NextTemplate: 10,
+      },
+      {
+        inactiveMonths: 24,
+        LastUpdated: moment().subtract(24, 'months'),
+        LastTemplate: 10,
+        NextTemplate: 12,
+      },
+      {
+        inactiveMonths: 25,
+        LastUpdated: moment().subtract(25, 'months'),
+        LastTemplate: 12,
+        NextTemplate: null,
+      },
+    ].forEach(({ inactiveMonths, LastUpdated, LastTemplate, NextTemplate }) => {
+      it(`should return the next template when ${inactiveMonths} months inactive`, async () => {
+        const inactiveWorkplace = {
+          EstablishmentID: 478,
+          NameValue: 'Workplace Name',
+          NmdsID: 'J1234567',
+          DataOwner: 'Workplace',
+          PrimaryUserName: 'Test Name',
+          PrimaryUserEmail: 'test@example.com',
+          LastUpdated: LastUpdated,
+          LastTemplate: LastTemplate,
+        };
+
+        const emailTemplateId = await findInactiveWorkplaces.nextEmailTemplate(inactiveWorkplace);
+
+        expect(emailTemplateId).to.equal(NextTemplate);
+      });
+    });
+
+    [
+      {
+        inactiveMonths: 6,
+        LastUpdated: moment().subtract(6, 'months'),
+        LastTemplate: 13,
+      },
+      {
+        inactiveMonths: 12,
+        LastUpdated: moment().subtract(12, 'months'),
+        LastTemplate: 14,
+      },
+      {
+        inactiveMonths: 18,
+        LastUpdated: moment().subtract(18, 'months'),
+        LastTemplate: 10,
+      },
+      {
+        inactiveMonths: 24,
+        LastUpdated: moment().subtract(24, 'months'),
+        LastTemplate: 12,
+      },
+    ].forEach(({ inactiveMonths, LastUpdated, LastTemplate }) => {
+      it(`should not return a ${inactiveMonths} month template when already sent a ${inactiveMonths} month email`, async () => {
+        const inactiveWorkplace = {
+          EstablishmentID: 478,
+          NameValue: 'Workplace Name',
+          NmdsID: 'J1234567',
+          DataOwner: 'Workplace',
+          PrimaryUserName: 'Test Name',
+          PrimaryUserEmail: 'test@example.com',
+          LastUpdated: LastUpdated,
+          LastTemplate: LastTemplate,
+        };
+
+        const emailTemplateId = await findInactiveWorkplaces.nextEmailTemplate(inactiveWorkplace);
+
+        expect(emailTemplateId).to.equal(null);
+      });
+    });
   });
 });
