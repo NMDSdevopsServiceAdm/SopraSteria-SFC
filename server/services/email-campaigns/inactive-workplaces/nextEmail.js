98f442302d1a0f5a017496e42294bcd74a4530f9
joannafawl <joanna.fawl@madetech.com>
2021-03-12 12:08:30 +0000

    Refactor nextTemplate

--- server/services/email-campaigns/inactive-workplaces/nextEmail.js
+++ server/services/email-campaigns/inactive-workplaces/nextEmail.js
@@ -6,27 +6,27 @@ const lastMonth = moment().subtract(1, 'months');
 const templates = {
   sixMonths: {
     lastUpdated: lastMonth.clone().subtract(6, 'months'),
-    template: 'sendInBlue.templates.sixMonthsInactive',
+    template: config.get('sendInBlue.templates.sixMonthsInactive'),
   },
   twelveMonths: {
     lastUpdated: lastMonth.clone().subtract(12, 'months'),
-    template: 'sendInBlue.templates.twelveMonthsInactive',
+    template: config.get('sendInBlue.templates.twelveMonthsInactive'),
   },
   eighteenMonths: {
     lastUpdated: lastMonth.clone().subtract(18, 'months'),
-    template: 'sendInBlue.templates.eighteenMonthsInactive',
+    template: config.get('sendInBlue.templates.eighteenMonthsInactive'),
   },
   twentyFourMonths: {
     lastUpdated: lastMonth.clone().subtract(24, 'months'),
-    template: 'sendInBlue.templates.twentyFourMonthsInactive'
-  }
+    template: config.get('sendInBlue.templates.twentyFourMonthsInactive'),
+  },
 };
 
 const getTemplate = (inactiveWorkplace) => {
   const lastUpdated = moment(inactiveWorkplace.LastUpdated);
 
   for (const [key, month] of Object.entries(templates)) {
-    const nextTemplate = config.get(month.template);
+    const nextTemplate = month.template;
     const notReceivedTemplate = inactiveWorkplace.LastTemplate !== nextTemplate.id;
 
     if (lastUpdated.isSame(month.lastUpdated, 'month') && notReceivedTemplate) {
