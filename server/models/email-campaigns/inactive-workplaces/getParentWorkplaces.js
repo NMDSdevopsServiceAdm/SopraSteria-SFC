a43e9327923a39da8b3e1354e5892ee1dcfd432b
joannafawl <joanna.fawl@madetech.com>
2021-03-15 17:03:36 +0000

    Add query to get parent workplaces
    Co-authored-by: chadtomkiss-madetech <chadtomkiss-madetech@users.noreply.github.com>

---
+++ server/models/email-campaigns/inactive-workplaces/getParentWorkplaces.js
@@ -0,0 +1,55 @@
+const moment = require('moment');
+
+const models = require('../../index');
+
+const getParentWorkplaces = async () => {
+  const currentMonth = moment().startOf('month').format('YYYY-MM-DD');
+
+  return models.sequelize.query(
+    `
+    SELECT
+    *
+  FROM
+    cqc. "LastUpdatedEstablishments" e
+  WHERE
+    "IsParent" = TRUE
+    AND NOT EXISTS (
+      SELECT
+        ech. "establishmentID"
+      FROM
+        cqc. "EmailCampaignHistories" ech
+        JOIN cqc. "EmailCampaigns" ec ON ec. "id" = ech. "emailCampaignID"
+      WHERE
+        ec. "type" = 'inactiveWorkplaces'
+        AND ech. "createdAt" >= :currentMonth
+        AND ech. "establishmentID" = e. "EstablishmentID")
+    UNION
+    SELECT
+      *
+    FROM
+      cqc. "LastUpdatedEstablishments" e
+    WHERE
+      "ParentID" IS NOT NULL
+      AND "DataOwner" = 'Parent'
+          AND NOT EXISTS (
+        SELECT
+          ech. "establishmentID"
+        FROM
+          cqc. "EmailCampaignHistories" ech
+          JOIN cqc. "EmailCampaigns" ec ON ec. "id" = ech. "emailCampaignID"
+        WHERE
+          ec. "type" = 'inactiveWorkplaces'
+          AND ech. "createdAt" >= '2021-03-01'
+          AND ech. "establishmentID" = e. "ParentID");`,
+    {
+      type: models.sequelize.QueryTypes.SELECT,
+      replacements: {
+        currentMonth,
+      },
+    },
+  );
+};
+
+module.exports = {
+  getParentWorkplaces,
+};
