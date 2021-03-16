7b16d7d3975ac8d573fd444e61dabcde63318393
Chad Tomkiss <chad.tomkiss@madetech.com>
2021-03-16 15:49:01 +0000

    feat(keep-workplaces-active-parents-report): Add parents and subs to inactive workplace report

---
+++ server/reports/inactive-workplaces/workplaces.js
@@ -0,0 +1,38 @@
+const buildRow = (workplace) => {
+  return {
+    workplace: workplace.name,
+    workplaceId: workplace.nmdsId,
+    lastUpdated: workplace.lastUpdated,
+    emailTemplate: workplace.emailTemplate.name,
+    dataOwner: workplace.dataOwner,
+    nameOfUser: workplace.user.name,
+    userEmail: workplace.user.email,
+  };
+};
+
+const buildRows = (workplaces) => {
+  return workplaces.map(buildRow);
+};
+
+const addWorksheet = (workbook) => {
+  const worksheet = workbook.addWorksheet('Inactive workplaces');
+  worksheet.columns = [
+    { header: 'Workplace name', key: 'workplace' },
+    { header: 'Workplace ID', key: 'workplaceId' },
+    { header: 'Date last updated', key: 'lastUpdated' },
+    { header: 'Email template', key: 'emailTemplate' },
+    { header: 'Data owner', key: 'dataOwner' },
+    { header: 'Name of user', key: 'nameOfUser' },
+    { header: 'User email', key: 'userEmail' },
+  ];
+
+  const headerRow = worksheet.getRow(1);
+  headerRow.font = { bold: true, name: 'Calibri' };
+
+  return worksheet;
+};
+
+module.exports = {
+  addWorksheet,
+  buildRows,
+};
--- server/test/unit/routes/admin/email-campaigns/inactive-workplaces/report.spec.js
+++ server/test/unit/routes/admin/email-campaigns/inactive-workplaces/report.spec.js
@@ -1,4 +1,3 @@
-const excelJS = require('exceljs');
 const expect = require('chai').expect;
 const httpMocks = require('node-mocks-http');
 const sinon = require('sinon');
@@ -38,6 +37,10 @@ describe('server/routes/admin/email-campaigns/inactive-workplaces/report', () =>
     },
   ];
 
+  afterEach(() => {
+    sinon.restore();
+  });
+
   it('should generate a report', async () => {
     sinon.stub(findInactiveWorkplaces, 'findInactiveWorkplaces').returns(dummyInactiveWorkplaces);
 
@@ -54,40 +57,5 @@ describe('server/routes/admin/email-campaigns/inactive-workplaces/report', () =>
 
     expect(res.statusCode).to.equal(200);
     expect(res._headers['content-type']).to.equal('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-  });
-
-  it('should add Workplace to the worksheet', () => {
-    const workplace = {
-      id: 478,
-      name: 'Workplace Name',
-      nmdsId: 'J1234567',
-      lastUpdated: '2020-06-01',
-      emailTemplate: {
-        id: 13,
-        name: '6 months',
-      },
-      dataOwner: 'Workplace',
-      user: {
-        name: 'Test Name',
-        email: 'test@example.com',
-      },
-    };
-
-    const workbook = new excelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Inactive workplaces');
-
-    const addRow = sinon.spy(worksheet, 'addRow');
-
-    report.printRow(worksheet, workplace);
-
-    sinon.assert.calledWith(addRow, {
-      workplace: 'Workplace Name',
-      workplaceId: 'J1234567',
-      lastUpdated: '2020-06-01',
-      emailTemplate: '6 months',
-      dataOwner: 'Workplace',
-      nameOfUser: 'Test Name',
-      userEmail: 'test@example.com',
-    });
   });
 });
