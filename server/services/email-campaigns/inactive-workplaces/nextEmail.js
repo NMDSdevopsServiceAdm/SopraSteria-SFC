7ee26b661e94690ad925aa3f7c36d5fec7b17071
Chad Tomkiss <chad.tomkiss@madetech.com>
2021-03-17 12:09:59 +0000

    fix(only-whitelist-when-sending-email): Move whitelist to sendEmail logic

--- server/services/email-campaigns/inactive-workplaces/nextEmail.js
+++ server/services/email-campaigns/inactive-workplaces/nextEmail.js
@@ -25,7 +25,7 @@ const templates = {
 const getTemplate = (inactiveWorkplace) => {
   const lastUpdated = moment(inactiveWorkplace.LastUpdated);
 
-  for (const [_key, month] of Object.entries(templates)) {
+  for (const [, month] of Object.entries(templates)) {
     const nextTemplate = month.template;
     const notReceivedTemplate = inactiveWorkplace.LastTemplate !== nextTemplate.id;
 
@@ -43,7 +43,7 @@ const isWhitelisted = (email) => {
   }
 
   return config.get('sendInBlue.whitelist').split(',').includes(email);
-}
+};
 
 const shouldReceive = (inactiveWorkplace) => {
   return isWhitelisted(inactiveWorkplace.PrimaryUserEmail) === true && getTemplate(inactiveWorkplace) !== null;
