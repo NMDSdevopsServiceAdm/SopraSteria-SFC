a2a84e551b806c8c41a2e390fc394a522e51f2da
joannafawl <joanna.fawl@madetech.com>
2021-03-17 14:41:11 +0000

    Update sendEmail to send to parent workplaces

--- server/services/email-campaigns/inactive-workplaces/sendEmail.js
+++ server/services/email-campaigns/inactive-workplaces/sendEmail.js
@@ -1,17 +1,23 @@
 const sendInBlueEmail = require('../../../utils/email/sendInBlueEmail');
 
 const sendEmail = async (workplace) => {
+  const params = {
+    WORKPLACE_ID: workplace.nmdsId,
+      FULL_NAME: workplace.user.name,
+  };
+
+  if (workplace.subsidiaries) {
+    params.SUBSIDIARIES = workplace.subsidiaries;
+  };
+
   sendInBlueEmail.sendEmail(
     {
       email: workplace.user.email,
       name: workplace.user.name,
     },
     workplace.emailTemplate.id,
-    {
-      WORKPLACE_ID: workplace.nmdsId,
-      FULL_NAME: workplace.user.name,
-    },
-  );
+    params,
+  )
 };
 
 module.exports = {
--- server/test/unit/services/email-campaigns/inactive-workplaces/sendEmail.spec.js
+++ server/test/unit/services/email-campaigns/inactive-workplaces/sendEmail.spec.js
@@ -1,11 +1,15 @@
 const sinon = require('sinon');
 const sendEmail = require('../../../../../services/email-campaigns/inactive-workplaces/sendEmail');
 const sendInBlueEmail = require('../../../../../utils/email/sendInBlueEmail');
+const moment = require('moment');
 
 describe('server/routes/admin/email-campaigns/inactive-workplaces/sendEmail', () => {
   afterEach(() => {
     sinon.restore();
   });
+
+  const endOfLastMonth = moment().subtract(1, 'months').endOf('month').endOf('day');
+  const parentTemplateId = 15;
 
   it('should call sendEmails for inactive workplace', async () => {
     const inactiveWorkplace = {
@@ -36,6 +40,59 @@ describe('server/routes/admin/email-campaigns/inactive-workplaces/sendEmail', ()
       {
         WORKPLACE_ID: 'J1234567',
         FULL_NAME: 'Test Name',
+      },
+    );
+  });
+
+  it('should call sendEmails for parent workplaces', async () => {
+    const parentWorkplace = {
+      id: 1,
+      name: 'Test Name',
+      nmdsId: 'A1234567',
+      lastUpdated: endOfLastMonth.clone().subtract(6, 'months').format('YYYY-MM-DD'),
+      emailTemplate: {
+        id: parentTemplateId,
+        name: 'Parent',
+      },
+      dataOwner: 'Workplace',
+      user: {
+        name: 'Test Person',
+        email: 'test@example.com',
+      },
+      subsidiaries: [
+        {
+          id: 2,
+          name: 'Workplace Name',
+          nmdsId: 'A0045232',
+          lastUpdated: endOfLastMonth.clone().subtract(12, 'months').format('YYYY-MM-DD'),
+          dataOwner: 'Parent',
+        },
+      ],
+    };
+
+    const sendEmailStub = sinon.stub(sendInBlueEmail, 'sendEmail').returns();
+
+    await sendEmail.sendEmail(parentWorkplace);
+
+    sinon.assert.calledWith(
+      sendEmailStub,
+      {
+        email: 'test@example.com',
+        name: 'Test Person',
+      },
+      15,
+      {
+        WORKPLACE_ID: 'A1234567',
+        FULL_NAME: 'Test Person',
+        SUBSIDIARIES: [
+          {
+            id: 2,
+            name: 'Workplace Name',
+            nmdsId: 'A0045232',
+            lastUpdated: endOfLastMonth.clone().subtract(12, 'months').format('YYYY-MM-DD'),
+            dataOwner: 'Parent',
+          },
+        ],
       },
     );
   });
