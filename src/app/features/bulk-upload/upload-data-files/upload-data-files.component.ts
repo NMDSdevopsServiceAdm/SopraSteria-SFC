8a04f49f8e2165e84f80163f210d6e0c69129f2f
duncanc19 <duncancarter93@gmail.com>
2021-03-18 15:48:29 +0000

    techDebt(removeOldBulkUpload): Delete all unused bulk-upload-v1 files and remove conditional bulk upload routing from app-routing.module

--- src/app/features/bulk-upload/upload-data-files/upload-data-files.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-upload-data-files',
-  templateUrl: './upload-data-files.component.html',
-})
-export class UploadDataFilesComponent {}
