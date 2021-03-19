4d2f9c53d997dee64ec3a7f2a52e30ad11b9b0f2
Chad Tomkiss <chad.tomkiss@madetech.com>
2021-03-19 12:39:18 +0000

    feat(keep-workplaces-active-parents): Fix tests

--- server/test/unit/routes/admin/email-campaigns/inactive-workplaces/report.spec.js
+++ server/test/unit/routes/admin/email-campaigns/inactive-workplaces/report.spec.js
@@ -4,6 +4,7 @@ const sinon = require('sinon');
 
 const report = require('../../../../../../routes/admin/email-campaigns/inactive-workplaces/report');
 const findInactiveWorkplaces = require('../../../../../../services/email-campaigns/inactive-workplaces/findInactiveWorkplaces');
+const findParentWorkplaces = require('../../../../../../services/email-campaigns/inactive-workplaces/findParentWorkplaces');
 
 describe('server/routes/admin/email-campaigns/inactive-workplaces/report', () => {
   const dummyInactiveWorkplaces = [
@@ -43,6 +44,7 @@ describe('server/routes/admin/email-campaigns/inactive-workplaces/report', () =>
 
   it('should generate a report', async () => {
     sinon.stub(findInactiveWorkplaces, 'findInactiveWorkplaces').returns(dummyInactiveWorkplaces);
+    sinon.stub(findParentWorkplaces, 'findParentWorkplaces').returns([]);
 
     const req = httpMocks.createRequest({
       method: 'GET',
