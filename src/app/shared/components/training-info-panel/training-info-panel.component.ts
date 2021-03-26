80d852ea8e0269d118ea78b8512a740a43707089
duncanc19 <duncancarter93@gmail.com>
2021-03-26 12:26:28 +0000

    techDebt(fixTrianingSpelling): Refactor TrainingInfoPanelComponent - remove empty constructor and ngOnInit

--- src/app/shared/components/training-info-panel/training-info-panel.component.ts
+++ src/app/shared/components/training-info-panel/training-info-panel.component.ts
@@ -1,17 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Establishment } from '@core/model/establishment.model';
 
 @Component({
   selector: 'app-training-info-panel',
   templateUrl: './training-info-panel.component.html',
 })
-export class TrainingInfoPanelComponent implements OnInit {
+export class TrainingInfoPanelComponent {
   @Input() workplace: Establishment;
   @Input() totalExpiredTraining = 0;
   @Input() totalExpiringTraining = 0;
   @Input() totalMissingMandatoryTraining = 0;
-
-  constructor() {}
-
-  ngOnInit() {}
 }
