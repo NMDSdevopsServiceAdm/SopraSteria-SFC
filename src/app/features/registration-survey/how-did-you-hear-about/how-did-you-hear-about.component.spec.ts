94f513f113f6aa9081ba25dd2f8131d06d2cf2c7
Ross McFadyen <rossmcfadyen1967@gmail.com>
2021-04-07 15:25:32 +0100

    Add additional angular tests

--- src/app/features/registration-survey/how-did-you-hear-about/how-did-you-hear-about.component.spec.ts
+++ src/app/features/registration-survey/how-did-you-hear-about/how-did-you-hear-about.component.spec.ts
@@ -3,12 +3,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { RegistrationSurveyService } from '@core/services/registration-survey.service';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 
 import { RegistrationSurveyModule } from '../registration-survey.module';
 import { HowDidYouHearAboutComponent } from './how-did-you-hear-about.component';
 
-describe('HowDidYouHearAboutComponent', () => {
+fdescribe('HowDidYouHearAboutComponent', () => {
   async function setup() {
     return render(HowDidYouHearAboutComponent, {
       imports: [SharedModule, RegistrationSurveyModule, RouterTestingModule, HttpClientTestingModule],
@@ -31,5 +31,22 @@ describe('HowDidYouHearAboutComponent', () => {
     const component = await setup();
     const text = component.fixture.nativeElement.querySelector('h1');
     expect(text.innerText).toContain('How did you hear about the Adult Social Care Workforce Data Set service?');
+  });
+
+  it('should check the Other checkbox', async () => {
+    const component = await setup();
+
+    const checkbox = component.fixture.nativeElement.querySelector(`input[name="howDidYouHearAbout-6"]`);
+    fireEvent.click(checkbox);
+
+    expect(checkbox.checked).toBeTruthy;
+    expect(checkbox.value).toEqual('Other');
+  });
+
+  it('should navigate to the thank you page', async () => {
+    const component = await setup();
+
+    const nextPage = component.fixture.componentInstance.nextPage;
+    expect(nextPage.url).toEqual(['/registration-survey', 'thank-you']);
   });
 });
--- src/app/features/registration-survey/why-create-account/why-create-account.component.spec.ts
+++ src/app/features/registration-survey/why-create-account/why-create-account.component.spec.ts
@@ -3,12 +3,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { RegistrationSurveyService } from '@core/services/registration-survey.service';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 
 import { RegistrationSurveyModule } from '../registration-survey.module';
 import { WhyCreateAccountComponent } from './why-create-account.component';
 
-describe('WhyCreateAccountComponent', () => {
+fdescribe('WhyCreateAccountComponent', () => {
   async function setup() {
     return render(WhyCreateAccountComponent, {
       imports: [SharedModule, RegistrationSurveyModule, RouterTestingModule, HttpClientTestingModule],
@@ -31,5 +31,24 @@ describe('WhyCreateAccountComponent', () => {
     const component = await setup();
     const text = component.fixture.nativeElement.querySelector('h1');
     expect(text.innerText).toContain('Why did you create an account?');
+  });
+
+  it('should check a selected checkbox', async () => {
+    const component = await setup();
+
+    const checkbox = component.fixture.nativeElement.querySelector(`input[name="whyCreateAccount-0"]`);
+    fireEvent.click(checkbox);
+
+    expect(checkbox.checked).toBeTruthy;
+    expect(checkbox.value).toEqual(
+      'To help the Department of Health and Social Care and others to better understand the adult social care sector',
+    );
+  });
+
+  it('should navigate to the next question', async () => {
+    const component = await setup();
+
+    const nextPage = component.fixture.componentInstance.nextPage;
+    expect(nextPage.url).toEqual(['/registration-survey', 'how-did-you-hear-about']);
   });
 });
