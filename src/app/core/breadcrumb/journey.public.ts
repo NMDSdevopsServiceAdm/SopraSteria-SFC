9decfb7227b554eab8991d7617551e2c5d2e979b
duncanc19 <duncancarter93@gmail.com>
2021-04-07 15:42:27 +0100

    feat(changeBetaBanner): Add Contact us or leave feedback to breadcrumbs and change breadcrumb path of Contact Us and Leave Feedback

--- src/app/core/breadcrumb/journey.public.ts
+++ src/app/core/breadcrumb/journey.public.ts
@@ -1,27 +1,20 @@
 import { JourneyRoute } from './breadcrumb.model';
 
 enum Path {
-  FEEDBACK = '/feedback',
+  FEEDBACK = '/contact-us-or-leave-feedback/feedback',
   COOKIE_POLICY = '/cookie-policy',
-  CONTACT_US = '/contact-us',
+  CONTACT_US = '/contact-us-or-leave-feedback/contact-us',
   ACCESSIBILITY_STATEMENT = '/accessibility-statement',
   TERMS_AND_CONDITIONS = '/terms-and-conditions',
   PRIVACY_NOTICE = '/privacy-notice',
+  CONTACT_US_OR_LEAVE_FEEDBACK = '/contact-us-or-leave-feedback',
 }
 
 export const publicJourney: JourneyRoute = {
   children: [
     {
-      title: 'Feedback',
-      path: Path.FEEDBACK,
-    },
-    {
       title: 'Cookie policy',
       path: Path.COOKIE_POLICY,
-    },
-    {
-      title: 'Contact us',
-      path: Path.CONTACT_US,
     },
     {
       title: 'Accessibility statement',
@@ -34,6 +27,20 @@ export const publicJourney: JourneyRoute = {
     {
       title: 'Privacy notice',
       path: Path.PRIVACY_NOTICE,
+    },
+    {
+      title: 'Contact us or leave feedback',
+      path: Path.CONTACT_US_OR_LEAVE_FEEDBACK,
+      children: [
+        {
+          title: 'Feedback',
+          path: Path.FEEDBACK,
+        },
+        {
+          title: 'Contact us',
+          path: Path.CONTACT_US,
+        },
+      ],
     },
   ],
 };
