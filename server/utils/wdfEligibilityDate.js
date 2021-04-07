e54d235ea69aa4714332e2f1d057b1a91af364f3
George <george.taylor@madetech.com>
2021-04-07 11:21:44 +0100

    console.logs to debug wdf parent

--- server/utils/wdfEligibilityDate.js
+++ server/utils/wdfEligibilityDate.js
@@ -2,9 +2,14 @@ exports.wdfEligibilityDate = () => {
   // calculate the effective from date
   const today = new Date();
   const yearStartMonth = 3; // April (months start at 0)
+  console.log("wdfEligibilityDate");
+
   if (today.getMonth() < yearStartMonth) {
+    console.log(new Date(Date.UTC(today.getFullYear() - 1, yearStartMonth, 1)));
+
     return new Date(Date.UTC(today.getFullYear() - 1, yearStartMonth, 1));
   } else {
+    console.log( new Date(Date.UTC(today.getFullYear(), yearStartMonth, 1)));
     return new Date(Date.UTC(today.getFullYear(), yearStartMonth, 1));
   }
 };
