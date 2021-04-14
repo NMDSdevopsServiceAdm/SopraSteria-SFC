80a41b2e545e43d2ff113ab0f7ae05438f094c3b
duncanc19 <duncancarter93@gmail.com>
2021-04-14 14:15:42 +0100

    feat(wdfOverview): Add establishment service to overview component and add dynamic establishment name to html

--- src/app/features/wdf/wdf-overview/wdf-overview.component.ts
+++ src/app/features/wdf/wdf-overview/wdf-overview.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Establishment } from '@core/model/establishment.model';
+import { EstablishmentService } from '@core/services/establishment.service';
 
 @Component({
   selector: 'app-wdf-overview',
   templateUrl: './wdf-overview.component.html',
 })
 export class WdfOverviewComponent implements OnInit {
-  constructor() {}
+  public workplace: Establishment;
 
-  ngOnInit(): void {}
+  constructor(private establishmentService: EstablishmentService) {}
+
+  ngOnInit(): void {
+    this.workplace = this.establishmentService.primaryWorkplace;
+  }
 }
