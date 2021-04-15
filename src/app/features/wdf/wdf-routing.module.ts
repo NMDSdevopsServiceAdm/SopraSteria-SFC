4c551b1a0f70703f7ce3f4e9d52987879a8899da
Joanna Fawl <66678865+joannafawl@users.noreply.github.com>
2021-04-15 14:00:09 +0100

    Feat: WDF Overview (#3280)
    * Create WDF overview page
    
    * feat(wdfOverview): Add link to WDF information page
    
    * feat(wdfOverview): Add link to reports page and change overview URL to wdf root
    
    * feat(wdfOverview): Add establishment service to overview component and add dynamic establishment name to html
    
    * feat(wdfOverview): Add dynamic WDF timeframe years, add test for date conversion and create mockReportService
    
    * feat(wdfOverview): Add dynamic end date and test for date formatting
    
    * feat(wdfOverview): Refactor to use start and end dates with pipes in html file
    
    * feat(wdfOverview): Add dynamic date for date requirements met
    
    * feat(wdfOverview): Add breadcrumbs to overview page, create journey.wdf file
    
    * Fix styling on green tick
    
    Co-authored-by: duncanc19 <duncanc19@users.noreply.github.com>
    
    * feat(wdfOverview): Refactor - create setDates function to simplify ngOnInit
    
    * feat(wdfOverview): Code review fixes - add OnDestroy to unsubscribe and leave WDF link to be changed blank
    
    * feat(wdfOverview): Change wording and date of meeting requirements information and update test
    
    Co-authored-by: duncanc19 <duncancarter93@gmail.com>
    Co-authored-by: duncanc19 <duncanc19@users.noreply.github.com>

---
+++ src/app/features/wdf/wdf-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { WdfOverviewComponent } from './wdf-overview/wdf-overview.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: WdfOverviewComponent,
+    data: { title: 'Workforce Development Fund' },
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class WdfRoutingModule {}
