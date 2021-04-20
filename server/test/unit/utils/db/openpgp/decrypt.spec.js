646184c7c489d3e438476d9d769a597c7cbb4e29
g-m-taylor <62301336+g-m-taylor@users.noreply.github.com>
2021-04-20 16:01:35 +0100

    swaping tests and fixing column (#3291)

--- server/test/unit/utils/db/openpgp/decrypt.spec.js
+++ server/test/unit/utils/db/openpgp/decrypt.spec.js
@@ -1,14 +1,10 @@
 const expect = require('chai').expect;
 const encrypt = require('../../../../../utils/db/openpgp/encrypt').encrypt;
+const decrypt = require('../../../../../utils/db/openpgp/decrypt').decrypt;
 
-describe('Encrypt', () => {
-
-  it('should return an armoured response', async () => {
+describe('Decrypt', () => {
+  it('should encrypt and decrypt', async () => {
     const encrypted = await encrypt("hello moto");
-    expect( encrypted).to.contain("-BEGIN PGP MESSAGE---");
-  });
-  it('should not contain the private data', async () => {
-    const encrypted = await encrypt("hello moto");
-    expect( encrypted).to.not.contain("hello moto");
+    expect( await decrypt(encrypted)).to.equal("hello moto");
   });
 });
