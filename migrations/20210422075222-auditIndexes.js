223ad081b161d63f7e4c96fb5a703984bf28dde8
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-04-23 09:09:45 +0100

    Added concurrently but this wont work

--- migrations/20210422075222-auditIndexes.js
+++ migrations/20210422075222-auditIndexes.js
@@ -3,9 +3,10 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.sequelize.query(`
-      CREATE INDEX "worker_audit__worker_f_k__property_name__event_type__when" ON cqc."WorkerAudit" USING btree ("WorkerFK", "EventType", "When");
-      CREATE INDEX "establishment_audit__establishment_f_k__when" ON cqc."EstablishmentAudit" USING btree ("EstablishmentFK", "When");
-      CREATE INDEX "worker_audit__worker_f_k__when" ON cqc."WorkerAudit" USING btree ("WorkerFK", "When");
+      CREATE INDEX CONCURRENTLY "worker_audit__worker_f_k__property_name__event_type__when" ON cqc."WorkerAudit" USING btree ("WorkerFK", "EventType", "When");
+      CREATE INDEX CONCURRENTLY "establishment_audit__establishment_f_k__when" ON cqc."EstablishmentAudit" USING btree ("EstablishmentFK", "When");
+      CREATE INDEX CONCURRENTLY "worker_audit__worker_f_k__when" ON cqc."WorkerAudit" USING btree ("WorkerFK", "When");
+      CREATE INDEX CONCURRENTLY "user_audit__user_f_k__event_type__when" ON cqc."UserAudit" USING btree ("UserFK", "EventType", "When");
     `);
   },
 
