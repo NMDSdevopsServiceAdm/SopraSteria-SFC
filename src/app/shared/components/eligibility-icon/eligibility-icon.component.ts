1235cf00437ca9f20e5f70ae3dc20f066939b2d3
joannafawl <joanna.fawl@madetech.com>
2021-05-04 17:00:37 +0100

    Add feature flags to summary record value component and eligibility icon

--- src/app/shared/components/eligibility-icon/eligibility-icon.component.ts
+++ src/app/shared/components/eligibility-icon/eligibility-icon.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FeatureFlagsService } from '@shared/services/feature-flags.service';
 
 @Component({
   selector: 'app-eligibility-icon',
   templateUrl: './eligibility-icon.component.html',
 })
 export class EligibilityIconComponent implements OnInit {
+  @Input() overallEligibility = false;
   @Input() eligible: boolean;
   @Input() check: boolean;
   @Input() num: number = null;
@@ -12,8 +14,33 @@ export class EligibilityIconComponent implements OnInit {
   public icon: string;
   public label: string;
 
+  constructor(private featureFlagsService: FeatureFlagsService) {}
+
   ngOnInit() {
-    this.icon = this.eligible ? 'tick' : 'cross';
-    this.label = this.check ? 'Check and confirm' : this.eligible ? 'Meeting requirements' : 'Not meeting requirements';
+    this.featureFlagsService.configCatClient.getValueAsync('wdfNewDesign', false).then((value) => {
+      this.calc(value);
+    });
+  }
+
+  public calc(newDesign): void {
+    if (newDesign) {
+      if (!this.eligible && this.overallEligibility) {
+        this.icon = 'flag-orange';
+      } else if (!this.eligible && !this.overallEligibility) {
+        this.icon = 'cross-icon';
+      }
+      this.label = this.check
+        ? 'Check and confirm'
+        : this.eligible
+        ? 'Meeting requirements'
+        : 'Not meeting requirements';
+    } else {
+      this.icon = this.eligible ? 'tick-icon' : 'cross-icon';
+      this.label = this.check
+        ? 'Check and confirm'
+        : this.eligible
+        ? 'Meeting requirements'
+        : 'Not meeting requirements';
+    }
   }
 }
--- src/app/shared/components/summary-record-value/summary-record-value.component.ts
+++ src/app/shared/components/summary-record-value/summary-record-value.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Eligibility, WDFValue } from '@core/model/wdf.model';
+import { FeatureFlagsService } from '@shared/services/feature-flags.service';
 
 @Component({
   selector: 'app-summary-record-value',
   templateUrl: './summary-record-value.component.html',
 })
-export class SummaryRecordValueComponent {
+export class SummaryRecordValueComponent implements OnInit {
+  public wdfNewDesign: boolean;
   @Input() wdfView: boolean;
   @Input() wdfValue: WDFValue;
   public ELIGIBILITY = Eligibility;
+
+  constructor(private featureFlagsService: FeatureFlagsService) {}
+
+  ngOnInit() {
+    this.featureFlagsService.configCatClient.getValueAsync('wdfNewDesign', false).then((value) => {
+      this.wdfNewDesign = value;
+    });
+  }
 }
