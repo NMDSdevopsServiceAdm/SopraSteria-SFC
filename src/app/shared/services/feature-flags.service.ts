60fe60db81617ab373f2155788b6f5a6d4c3fda4
George <george.taylor@madetech.com>
2021-05-10 15:53:11 +0100

    removing logging

--- src/app/shared/services/feature-flags.service.ts
+++ src/app/shared/services/feature-flags.service.ts
@@ -1,19 +1,14 @@
-import { LogLevel } from 'configcat-common';
 import { IConfigCatClient } from 'configcat-common/lib/ConfigCatClient';
 import * as configcat from 'configcat-js';
-
 import { environment } from '../../../environments/environment';
 
 export class FeatureFlagsService {
   public configCatClient: IConfigCatClient;
-  public logger = configcat.createConsoleLogger(LogLevel.Info);
 
   constructor() {}
 
   start(){
-    this.configCatClient = configcat.createClientWithManualPoll(environment.configCatKey,
-      {
-        logger: this.logger
-      });
+    this.configCatClient = configcat.createClientWithManualPoll(environment.configCatKey,{}
+      );
   }
 }
