597f47e98a7a0d934253d49e0d1dd137c83669ff
Aaron Russell <aaron_russell@me.com>
2021-05-13 16:11:42 +0100

    check postcode against cqc (#3392)
    * Add postcode check
    
    * Updated auto scaling policy to run on live for preprod
    
    * Add additional tests for postcode check
    
    * Fixed tests
    
    Co-authored-by: Ross McFadyen <rossmcfadyen1967@gmail.com>

--- server/test/unit/routes/cqcStatusCheck/index.spec.js
+++ server/test/unit/routes/cqcStatusCheck/index.spec.js
@@ -1,38 +1,39 @@
 const expect = require('chai').expect;
 const httpMocks = require('node-mocks-http');
 const sinon = require('sinon');
-const sinon_sandbox = sinon.createSandbox();
 const CQCDataAPI = require('../../../../utils/CQCDataAPI');
 
 const { cqcStatusCheck } = require('../../../../routes/cqcStatusCheck');
 
-describe('server/routes/establishments/cqcStatus', () => {
-  afterEach(() => {
-    sinon_sandbox.restore();
+describe('server/routes/establishments/cqcStatus', async () => {
+  beforeEach(() => {
+    sinon.stub(CQCDataAPI, 'getWorkplaceCQCData').callsFake(async (locationID) => {
+      if (locationID === registeredLocationId) {
+        return registeredURLResponse;
+      } else if (locationID === deregisteredLocationId) {
+        return deregisteredURLResponse;
+      } else {
+        return null;
+      }
+    });
   });
 
-  const url = 'https://api.cqc.org.uk/public/v1/locations/';
+  afterEach(async () => {
+    sinon.restore();
+  });
 
   const registeredLocationId = '1-109009203';
   const deregisteredLocationId = '1-799116841';
 
   const registeredURLResponse = {
     registrationStatus: 'Registered',
+    postalCode: 'SK10 7ED',
   };
 
   const deregisteredURLResponse = {
     registrationStatus: 'Deregistered',
+    postalCode: 'SK10 7EB',
   };
-
-  sinon.stub(CQCDataAPI, 'getWorkplaceCQCData').callsFake(async (locationID) => {
-    if (locationID === registeredLocationId) {
-      return registeredURLResponse;
-    } else if (url === deregisteredLocationId) {
-      return deregisteredURLResponse;
-    } else {
-      return null;
-    }
-  });
 
   it('should accept a locationID in the request body', async () => {
     const request = {
@@ -40,6 +41,9 @@ describe('server/routes/establishments/cqcStatus', () => {
       url: `/api/cqcStatusCheck/${registeredLocationId}`,
       params: {
         locationID: registeredLocationId,
+      },
+      query: {
+        postcode: 'SK10 7ED',
       },
     };
 
@@ -65,12 +69,15 @@ describe('server/routes/establishments/cqcStatus', () => {
     expect(res.statusCode).to.deep.equal(500);
   });
 
-  it.only('should return a 200 status when call is successful', async () => {
+  it('should return a 200 status when call is successful', async () => {
     const request = {
       method: 'GET',
       url: `/api/cqcStatusCheck/${registeredLocationId}`,
       params: {
         locationID: registeredLocationId,
+      },
+      query: {
+        postcode: 'SK10 7ED',
       },
     };
 
@@ -82,12 +89,15 @@ describe('server/routes/establishments/cqcStatus', () => {
     expect(res.statusCode).to.deep.equal(200);
   });
 
-  it('should return a true flag is the service is registered with CQC', async () => {
+  it('should return a true flag is the service is registered with CQC and postcode matches', async () => {
     const request = {
       method: 'GET',
       url: `/api/cqcStatusCheck/${registeredLocationId}`,
       params: {
         locationID: registeredLocationId,
+      },
+      query: {
+        postcode: 'SK10 7ED',
       },
     };
 
@@ -103,12 +113,89 @@ describe('server/routes/establishments/cqcStatus', () => {
     expect(res._getData()).to.deep.equal(expectedResult);
   });
 
+  it('should return a true flag is the service is registered with CQC and ASC-WDS postcode is lower case', async () => {
+    const request = {
+      method: 'GET',
+      url: `/api/cqcStatusCheck/${registeredLocationId}`,
+      params: {
+        locationID: registeredLocationId,
+      },
+      query: {
+        postcode: 'sk10 7ed',
+      },
+    };
+
+    const expectedResult = {
+      cqcStatusMatch: true,
+    };
+
+    const req = httpMocks.createRequest(request);
+    const res = httpMocks.createResponse();
+
+    await cqcStatusCheck(req, res);
+
+    expect(res._getData()).to.deep.equal(expectedResult);
+  });
+
+  it('should return a true flag is the service is registered with CQC and CQC postcode is lower case', async () => {
+    const request = {
+      method: 'GET',
+      url: `/api/cqcStatusCheck/${registeredLocationId}`,
+      params: {
+        locationID: registeredLocationId,
+      },
+      query: {
+        postcode: 'SK10 7ED',
+      },
+    };
+
+    const expectedResult = {
+      cqcStatusMatch: true,
+    };
+
+    registeredURLResponse.postalCode = 'sk10 7ed';
+
+    const req = httpMocks.createRequest(request);
+    const res = httpMocks.createResponse();
+
+    await cqcStatusCheck(req, res);
+
+    expect(res._getData()).to.deep.equal(expectedResult);
+  });
+
   it('should return a false flag is the service is deregistered with CQC', async () => {
     const request = {
       method: 'GET',
       url: `/api/cqcStatusCheck/${deregisteredLocationId}`,
       params: {
         locationID: deregisteredLocationId,
+      },
+      query: {
+        postcode: 'SK10 7ED',
+      },
+    };
+
+    const expectedResult = {
+      cqcStatusMatch: false,
+    };
+
+    const req = httpMocks.createRequest(request);
+    const res = httpMocks.createResponse();
+
+    await cqcStatusCheck(req, res);
+
+    expect(res._getData()).to.deep.equal(expectedResult);
+  });
+
+  it('should return a false flag is the service postcode does not match with CQC', async () => {
+    const request = {
+      method: 'GET',
+      url: `/api/cqcStatusCheck/${registeredLocationId}`,
+      params: {
+        locationID: registeredLocationId,
+      },
+      query: {
+        postcode: 'WA5 6BL',
       },
     };
 
