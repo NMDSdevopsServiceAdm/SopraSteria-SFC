1b4191f7e50ca58a3ffc381c3a46d4ec692de819
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-05-19 09:57:14 +0100

    Refactored to remove duplicate code

--- server/models/cache/singletons/ready.js
+++ server/models/cache/singletons/ready.js
@@ -1,19 +1,17 @@
 const ready = (models, className, errorName) => {
-  if (models.status.ready) {
+  const initialzeClass = () =>
     className
       .initialize()
       .then()
       .catch((err) => {
         console.error(`Failed to initialise ${errorName}: `, err);
       });
+
+  if (models.status.ready) {
+    initialzeClass();
   } else {
     models.status.on(models.status.READY_EVENT, () => {
-      className
-        .initialize()
-        .then()
-        .catch((err) => {
-          console.error(`Failed to initialise ${errorName}: `, err);
-        });
+      initialzeClass();
     });
   }
 };
