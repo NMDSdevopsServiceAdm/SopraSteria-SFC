b495dbf6de53ba371feea032377e2389fefe3d52
joannafawl <joanna.fawl@madetech.com>
2021-05-19 10:01:39 +0100

    Add sort by option for workplace name

--- src/app/core/model/establishment.model.ts
+++ src/app/core/model/establishment.model.ts
@@ -223,13 +223,11 @@ export enum WdfSortStaffOptions {
   '2_not_meeting' = 'WDF requirements (not meeting)',
 }
 
-export enum WdfParentSortStaffOptions {
-  '0_asc' = 'Staff name (A to Z)',
-  '0_dsc' = 'Staff name (Z to A)',
-  '1_asc' = 'Job role (A to Z)',
-  '1_dsc' = 'Job role (Z to A)',
+export enum WdfParentSortWorkplacesOptions {
+  '1_not_meeting' = 'WDF requirements (not meeting)',
   '2_meeting' = 'WDF requirements (meeting)',
-  '2_not_meeting' = 'WDF requirements (not meeting)',
+  '3_asc' = 'Workplace name (A to Z)',
+  '4_dsc' = 'Workplace name (Z to A)',
 }
 
 export enum SortTrainingAndQualsOptionsWorker {
--- src/app/features/wdf/wdf-workplaces-summary-table/wdf-workplaces-summary-table.component.ts
+++ src/app/features/wdf/wdf-workplaces-summary-table/wdf-workplaces-summary-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { WdfParentSortStaffOptions } from '@core/model/establishment.model';
+import { WdfParentSortWorkplacesOptions } from '@core/model/establishment.model';
 import { orderBy } from 'lodash';
 
 @Component({
@@ -8,13 +8,11 @@ import { orderBy } from 'lodash';
 })
 export class WdfWorkplacesSummaryTableComponent implements OnInit {
   @Input() workplaces = [];
-  public sortStaffOptions;
+  public sortWorkplacesOptions;
   public sortBy: string;
 
-  constructor() {}
-
   ngOnInit(): void {
-    this.sortStaffOptions = WdfParentSortStaffOptions;
+    this.sortWorkplacesOptions = WdfParentSortWorkplacesOptions;
   }
 
   ngOnChanges() {
@@ -25,34 +23,25 @@ export class WdfWorkplacesSummaryTableComponent implements OnInit {
   }
 
   public sortByColumn(selectedColumn: any) {
-    localStorage.setItem('SortBy', selectedColumn);
     switch (selectedColumn) {
-      case '0_asc': {
-        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.nameOrId.toLowerCase()], ['asc']);
-        break;
-      }
-      case '0_dsc': {
-        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.nameOrId.toLowerCase()], ['desc']);
-        break;
-      }
-      case '1_asc': {
-        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.jobRole.toLowerCase()], ['asc']);
-        break;
-      }
-      case '1_dsc': {
-        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.jobRole.toLowerCase()], ['desc']);
+      case '1_not_meeting': {
+        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.name.toLowerCase()], ['asc']);
         break;
       }
       case '2_meeting': {
-        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.wdfEligible], ['desc']);
+        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.name.toLowerCase()], ['desc']);
         break;
       }
-      case '2_not_meeting': {
-        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.wdfEligible], ['asc']);
+      case '3_asc': {
+        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.name.toLowerCase()], ['asc']);
+        break;
+      }
+      case '4_dsc': {
+        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.name.toLowerCase()], ['desc']);
         break;
       }
       default: {
-        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.nameOrId.toLowerCase()], ['asc']);
+        this.workplaces = orderBy(this.workplaces, [(workplace) => workplace.name.toLowerCase()], ['asc']);
         break;
       }
     }
