a95b11dfd5e7c7dd554029d9dccc807052044d84
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-05-20 16:10:25 +0100

    Added mock worker

--- server/test/integration/utils/worker.js
+++ server/test/integration/utils/worker.js
@@ -1,21 +1,112 @@
 const faker = require('faker');
 const jobsUtils = require('./jobs');
 const Random = require('./random');
+const { oneOf, fake, build, perBuild, sequence } = require('@jackfranklin/test-data-bot');
 
 const lookupRandomContract = () => {
-    const expectedContractTypeValues = ['Permanent', 'Temporary', 'Pool/Bank', 'Agency', 'Other'];
-    const randomContractIndex = Random.randomInt(0, expectedContractTypeValues.length-1);
-    return expectedContractTypeValues[randomContractIndex];
+  const expectedContractTypeValues = ['Permanent', 'Temporary', 'Pool/Bank', 'Agency', 'Other'];
+  const randomContractIndex = Random.randomInt(0, expectedContractTypeValues.length - 1);
+  return expectedContractTypeValues[randomContractIndex];
 };
 
 exports.newWorker = (jobs) => {
-    const randomJob = jobsUtils.lookupRandomJob(jobs);
-    return  {
-        "nameOrId": faker.lorem.words(1),
-        "contract": lookupRandomContract(),
-        "mainJob": {
-            "jobId" : randomJob.id,
-            "title" : randomJob.title
-        }
-    };
+  const randomJob = jobsUtils.lookupRandomJob(jobs);
+  return {
+    nameOrId: faker.lorem.words(1),
+    contract: lookupRandomContract(),
+    mainJob: {
+      jobId: randomJob.id,
+      title: randomJob.title,
+    },
+  };
 };
+
+module.exports.apiWorkerBuilder = build('Worker', {
+  fields: {
+    id: sequence(),
+    LocalIdentifierValue: fake((f) => f.lorem.words(1)),
+    NameOrIdValue: fake((f) => f.name.findName().replace(/,+/, '')),
+    FluJabValue: oneOf('Yes', 'No', "Don't know"),
+    NationalInsuranceNumberValue: 'AB123456C',
+    PostcodeValue: fake((f) => f.address.zipCode('??# #??')),
+    DateOfBirthValue: fake((f) => f.helpers.replaceSymbolWithNumber('####-##-##')),
+    GenderValue: oneOf('Female', 'Male', 'Other', "Don't know"),
+    ethnicity: {
+      id: fake((f) => f.random.number({ min: 1, max: 19 })),
+    },
+    NationalityValue: oneOf('British', "Don't know", 'Other'),
+    BritishCitizenshipValue: oneOf('Yes', 'No', "Don't know"),
+    CountryOfBirthValue: oneOf('United Kingdom', "Don't know", 'Other'),
+    YearArrivedValue: oneOf('Yes', 'No', "Don't know"),
+    DisabilityValue: oneOf('Yes', 'No', "Don't know", 'Undisclosed'),
+    CareCertificateValue: oneOf('Yes, completed', 'No', 'Yes, in progress or partially completed'),
+    RecruitedFromValue: oneOf('Yes', 'No'),
+    MainJobStartDateValue: fake((f) => f.helpers.replaceSymbolWithNumber('####-##-##')),
+    recruitedFrom: {
+      id: fake((f) => f.random.number({ min: 1, max: 10 })),
+    },
+    SocialCareStartDateValue: oneOf('Yes', 'No'),
+    SocialCareStartDateYear: fake((f) => f.helpers.replaceSymbolWithNumber('####')),
+    ApprenticeshipTrainingValue: oneOf('Yes', 'No', "Don't know"),
+    ContractValue: oneOf('Permanent', 'Temporary', 'Pool/Bank', 'Agency', 'Other'),
+    ZeroHoursContractValue: oneOf('Yes', 'No', "Don't know"),
+    DaysSickValue: oneOf('Yes', 'No'),
+    AnnualHourlyPayValue: oneOf('Hourly', 'Anually'),
+    AnnualHourlyPayRate: fake((f) => f.random.number({ min: 1, max: 100000 })),
+    mainJob: {
+      id: fake((f) => f.random.number({ min: 1, max: 28 })),
+      other: fake((f) => f.random.boolean()),
+    },
+    MainJobFkOther: fake((f) => f.lorem.words(3)),
+    WeeklyHoursContractedValue: oneOf('Yes', 'No'),
+    WeeklyHoursContractedHours: fake((f) => f.random.number({ min: 0, max: 40 })),
+    WeeklyHoursAverageValue: oneOf('Yes', 'No'),
+    WeeklyHoursAverageHours: fake((f) => f.random.number({ min: 0, max: 40 })),
+    OtherJobsValue: oneOf('Yes', 'No'),
+    otherJobs: [
+      {
+        id: fake((f) => f.random.number({ min: 1, max: 28 })),
+        other: fake((f) => f.random.boolean()),
+        workerJobs: {
+          other: fake((f) => f.lorem.words(3)),
+        },
+      },
+    ],
+    RegisteredNurseValue: oneOf(
+      'Adult Nurse',
+      'Mental Health Nurse',
+      'Learning Disabilities Nurse',
+      "Children's Nurse",
+      'Enrolled Nurse',
+    ),
+    NurseSpecialismsValue: oneOf('Yes', 'No', "Don't know"),
+    nurseSpecialisms: [
+      {
+        id: fake((f) => f.random.number({ min: 1, max: 7 })),
+      },
+    ],
+    ApprovedMentalHealthWorkerValue: oneOf('Yes', 'No', "Don't know"),
+    QualificationInSocialCareValue: oneOf('Yes', 'No', "Don't know"),
+    socialCareQualification: [
+      {
+        id: fake((f) => f.random.number({ min: 1, max: 7 })),
+      },
+    ],
+    OtherQualificationsValue: oneOf('Yes', 'No', "Don't know"),
+    highestQualification: [
+      {
+        id: fake((f) => f.random.number({ min: 1, max: 7 })),
+      },
+    ],
+    qualifications: [
+      {
+        id: fake((f) => f.random.number({ min: 1, max: 97 })),
+        year: fake((f) => f.helpers.replaceSymbolWithNumber('####')),
+        notes: fake((f) => f.lorem.sentence()),
+        qualification: {
+          id: fake((f) => f.random.number({ min: 1, max: 97 })),
+        },
+      },
+    ],
+  },
+});
