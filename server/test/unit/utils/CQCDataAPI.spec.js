41d3d19cd3d43dd01eec518d4f8960ca09dfda15
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-05-24 11:44:12 +0100

    Removed sandbox

--- server/test/unit/utils/CQCDataAPI.spec.js
+++ server/test/unit/utils/CQCDataAPI.spec.js
@@ -1,22 +1,21 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const sinon_sandbox = sinon.createSandbox();
 const axios = require('axios');
 
 const { getWorkplaceCQCData } = require('../../../../server/utils/CQCDataAPI');
 
 describe('server/utils/CQCDataAPI', async () => {
   afterEach(async () => {
-    sinon_sandbox.restore();
+    sinon.restore();
   });
 
   const url = 'https://api.cqc.org.uk/public/v1/locations/';
 
   const locationId = '1-109009203';
 
-  const cqcAPIStub = sinon_sandbox.stub(axios, 'get').returns({ data: true });
-
   it('should call the CQC API', async () => {
+    const cqcAPIStub = sinon.stub(axios, 'get').returns({ data: true });
+
     await getWorkplaceCQCData(locationId);
 
     expect(cqcAPIStub.calledOnceWith(url + locationId + '?partnerCode=SkillsForCare')).to.equal(true);
