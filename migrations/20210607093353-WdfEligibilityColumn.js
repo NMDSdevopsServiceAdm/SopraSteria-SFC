27062229ce74a34d448e49add00007ce2ffa616f
joannafawl <joanna.fawl@madetech.com>
2021-06-07 11:03:51 +0100

    Create WdfEligible column
    Co-authored-by: Aaron Russell <aaron_russell@me.com>

---
+++ migrations/20210607093353-WdfEligibilityColumn.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const table = {
+  tableName: 'Worker',
+  schema: 'cqc',
+};
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      try {
+        await Promise.all(
+          [
+            queryInterface.addColumn(
+              table,
+              'WdfEligible',
+              {
+                type: Sequelize.DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+              },
+              { transaction },
+            ),
+          ]
+        )
+        return Promise.resolve();
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await Promise.all(
+        queryInterface.removeColumn(table, 'WdfEligible'),
+    )
+  }
+};
