21a13ad0fc00f80ab0d84d320036047d48b214f8
duncanc19 <duncancarter93@gmail.com>
2021-06-08 14:34:16 +0100

    feat(newArticles): Add breadcrumbs to article page

--- src/app/features/articles/article/article.component.ts
+++ src/app/features/articles/article/article.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { Article } from '@core/model/article.model';
+import { BreadcrumbService } from '@core/services/breadcrumb.service';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   providers: [],
 })
-export class ArticleComponent {
-  constructor(private route: ActivatedRoute) {}
+export class ArticleComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private breadcrumbService: BreadcrumbService) {}
 
   public article: Article = this.route.snapshot.data.article.data.articles[0];
+
+  ngOnInit() {
+    this.breadcrumbService.show(JourneyType.PUBLIC);
+  }
 }
