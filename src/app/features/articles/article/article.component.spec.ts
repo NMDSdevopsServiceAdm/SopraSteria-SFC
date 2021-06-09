011afc46539b4274c6d76b5e22abb2486a3aeaca
duncanc19 <duncancarter93@gmail.com>
2021-06-09 12:22:04 +0100

    feat(newArticles): Add tests to check rendering of title and content in article component

--- src/app/features/articles/article/article.component.spec.ts
+++ src/app/features/articles/article/article.component.spec.ts
@@ -2,49 +2,45 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ApolloQueryResult, NetworkStatus } from '@apollo/client/core';
 import { Article } from '@core/model/article.model';
 import { ArticlesService } from '@core/services/articles.service';
+import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { MockArticlesService } from '@core/test-utils/MockArticlesService';
+import { MockBreadcrumbService } from '@core/test-utils/MockBreadcrumbService';
+import { MockFeatureFlagsService } from '@core/test-utils/MockFeatureFlagService';
 import { BulkUploadModule } from '@features/bulk-upload/bulk-upload.module';
+import { FeatureFlagsService } from '@shared/services/feature-flags.service';
 import { SharedModule } from '@shared/shared.module';
 import { render } from '@testing-library/angular';
 
 import { ArticleComponent } from './article.component';
 
 describe('ArticleComponent', () => {
-  const networkStatus = {} as NetworkStatus;
-  const article = {
-    data: {
-      articles: [
-        {
-          title: 'test',
-          content: 'Testing',
-          id: 1,
-          date_created: new Date(),
-          date_updated: new Date(),
-          user_created: 'duhefwiuh',
-          user_updated: 'fhewoihf',
-        },
-      ],
+  const articles = [
+    {
+      title: 'test',
+      content: 'Testing',
+      id: 1,
+      date_created: new Date(),
+      date_updated: new Date(),
+      user_created: 'duhefwiuh',
+      user_updated: 'fhewoihf',
     },
-    loading: false,
-    networkStatus,
-  } as ApolloQueryResult<Article>;
+  ] as Article[];
+
   async function setup() {
-    const component = await render(ArticleComponent, {
+    const { fixture, getByText } = await render(ArticleComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, BulkUploadModule],
       providers: [
-        {
-          provide: ArticlesService,
-          useClass: MockArticlesService,
-        },
+        { provide: ArticlesService, useClass: MockArticlesService },
+        { provide: BreadcrumbService, useClass: MockBreadcrumbService },
+        { provide: FeatureFlagsService, useClass: MockFeatureFlagsService },
         {
           provide: ActivatedRoute,
           useValue: {
             snapshot: {
               data: {
-                article,
+                articles,
               },
             },
           },
@@ -55,16 +51,27 @@ describe('ArticleComponent', () => {
     const injector = getTestBed();
 
     const router = injector.inject(Router) as Router;
-    const componentInstance = component.fixture.componentInstance;
+    const component = fixture.componentInstance;
     return {
       component,
+      fixture,
       router,
-      componentInstance,
+      getByText,
     };
   }
 
   it('should render a ArticleComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should display title of article', async () => {
+    const { getByText } = await setup();
+    expect(getByText(articles[0].title)).toBeTruthy();
+  });
+
+  it('should display content of article', async () => {
+    const { getByText } = await setup();
+    expect(getByText(articles[0].content)).toBeTruthy();
   });
 });
