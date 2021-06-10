1b80888f3438104c4e2b7d2715aaaa0491027890
duncanc19 <duncancarter93@gmail.com>
2021-06-10 12:07:50 +0100

    feat(newArticles): Add subscriptions to url

--- src/app/features/articles/article-list/article-list.component.ts
+++ src/app/features/articles/article-list/article-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Article } from '@core/model/article.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-article-list',
@@ -8,8 +9,16 @@ import { Article } from '@core/model/article.model';
   providers: [],
 })
 export class ArticleListComponent {
-  constructor(private route: ActivatedRoute) {}
+  public articleList: Article[];
+  public currentArticleSlug: string;
+  public subscriptions = new Subscription();
 
-  public articleList: Article[] = this.route.snapshot.data.articleList.data;
-  public currentArticleSlug: string = this.route.snapshot.data.articles.data[0].slug;
+  constructor(private route: ActivatedRoute) {
+    this.subscriptions.add(this.route.url.subscribe(() => this.updateArticleListAndCurrentArticleSlug()));
+  }
+
+  updateArticleListAndCurrentArticleSlug(): void {
+    this.articleList = this.route.snapshot.data.articleList.data;
+    this.currentArticleSlug = this.route.snapshot.data.articles.data[0].slug;
+  }
 }
--- src/app/features/articles/article/article.component.ts
+++ src/app/features/articles/article/article.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { Article } from '@core/model/article.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   providers: [],
 })
-export class ArticleComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private breadcrumbService: BreadcrumbService) {}
-
+export class ArticleComponent implements OnInit, OnDestroy {
+  public subscriptions = new Subscription();
   public article: Article = this.route.snapshot.data.articles.data[0];
 
+  constructor(private route: ActivatedRoute, private breadcrumbService: BreadcrumbService) {}
+
   ngOnInit() {
+    this.subscriptions.add(this.route.url.subscribe(() => this.updateArticleAndBreadcrumbs()));
+  }
+
+  updateArticleAndBreadcrumbs() {
+    this.article = this.route.snapshot.data.articles[0];
     this.breadcrumbService.show(JourneyType.PUBLIC);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
