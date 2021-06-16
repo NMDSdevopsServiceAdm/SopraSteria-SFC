a989a45c6a86f4c499c55c66981e4ad4181c13d0
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-06-16 14:42:05 +0100

    Added new endpoint to get the templates

--- server/routes/admin/email-campaigns/targeted-emails/index.js
+++ server/routes/admin/email-campaigns/targeted-emails/index.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const getTargetedTotalEmails = async (req, res) => {
-  return res.status(200).send({totalEmails: 1500});
-}
+  return res.status(200).send({ totalEmails: 1500 });
+};
+
+const getTargetedEmailTemplates = async (req, res) => {
+  return res.status(200).send({
+    templates: [
+      {
+        id: 3,
+        name: 'Email about something important',
+      },
+    ],
+  });
+};
 
 router.route('/total').get(getTargetedTotalEmails);
+router.route('/templates').get(getTargetedEmailTemplates);
 
 module.exports = router;
 module.exports.getTargetedTotalEmails = getTargetedTotalEmails;
+module.exports.getTargetedEmailTemplates = getTargetedEmailTemplates;
--- server/test/unit/routes/admin/email-campaigns/targeted-emails/index.spec.js
+++ server/test/unit/routes/admin/email-campaigns/targeted-emails/index.spec.js
@@ -1,39 +1,76 @@
 const expect = require('chai').expect;
 const httpMocks = require('node-mocks-http');
-const targetedEmailsRoutes = require('../../../../../../routes/admin/email-campaigns/targeted-emails')
+const targetedEmailsRoutes = require('../../../../../../routes/admin/email-campaigns/targeted-emails');
 
 describe('server/routes/admin/email-campaigns/targeted-emails', () => {
-    describe('getTargetedTotalEmails()', () => {
-      it('should return 200', async () => {
-        const req = httpMocks.createRequest({
-          method: 'GET',
-          url: '/api/admin/email-campaigns/targeted-emails',
-        });
-
-        req.role = 'Admin';
-        req.query.groupType = 'primaryUsers';
-
-        const res = httpMocks.createResponse();
-        await targetedEmailsRoutes.getTargetedTotalEmails(req, res);
-
-        expect(res.statusCode).to.deep.equal(200);
+  describe('getTargetedTotalEmails()', () => {
+    it('should return 200', async () => {
+      const req = httpMocks.createRequest({
+        method: 'GET',
+        url: '/api/admin/email-campaigns/targeted-emails/total',
       });
 
-      it('should return the total number of users', async () => {
-        const req = httpMocks.createRequest({
-          method: 'GET',
-          url: '/api/admin/email-campaigns/targeted-emails',
-        });
+      req.role = 'Admin';
+      req.query.groupType = 'primaryUsers';
 
-        req.role = 'Admin';
-        req.query.groupType = 'primaryUsers';
+      const res = httpMocks.createResponse();
+      await targetedEmailsRoutes.getTargetedTotalEmails(req, res);
 
-        const res = httpMocks.createResponse();
-        await targetedEmailsRoutes.getTargetedTotalEmails(req, res);
-
-        const response = res._getData();
-
-        expect(response.totalEmails).to.deep.equal(1500);
-      })
+      expect(res.statusCode).to.deep.equal(200);
     });
+
+    it('should return the total number of users', async () => {
+      const req = httpMocks.createRequest({
+        method: 'GET',
+        url: '/api/admin/email-campaigns/targeted-emails/total',
+      });
+
+      req.role = 'Admin';
+      req.query.groupType = 'primaryUsers';
+
+      const res = httpMocks.createResponse();
+      await targetedEmailsRoutes.getTargetedTotalEmails(req, res);
+
+      const response = res._getData();
+
+      expect(response.totalEmails).to.deep.equal(1500);
+    });
+  });
+  describe('getTargetedEmailTemplates()', () => {
+    it('should return 200', async () => {
+      const req = httpMocks.createRequest({
+        method: 'GET',
+        url: '/api/admin/email-campaigns/targeted-emails/templates',
+      });
+
+      req.role = 'Admin';
+
+      const res = httpMocks.createResponse();
+      await targetedEmailsRoutes.getTargetedEmailTemplates(req, res);
+
+      expect(res.statusCode).to.deep.equal(200);
+    });
+
+    it('should return the templates from send in blue', async () => {
+      const req = httpMocks.createRequest({
+        method: 'GET',
+        url: '/api/admin/email-campaigns/targeted-emails/templates',
+      });
+
+      req.role = 'Admin';
+
+      const templates = [
+        {
+          id: 3,
+          name: 'Email about something important',
+        },
+      ];
+      const res = httpMocks.createResponse();
+      await targetedEmailsRoutes.getTargetedEmailTemplates(req, res);
+
+      const response = res._getData();
+
+      expect(response.templates).to.deep.equal(templates);
+    });
+  });
 });
