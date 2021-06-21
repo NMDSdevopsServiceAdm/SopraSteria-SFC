aba8c2551641b7e9ca7ede4416c13f61cf6eaba3
joannafawl <joanna.fawl@madetech.com>
2021-06-21 14:14:57 +0100

    Update comment on rate limit

--- server/services/email-campaigns/limit.js
+++ server/services/email-campaigns/limit.js
@@ -2,7 +2,7 @@ const { pRateLimit } = require('p-ratelimit');
 
 const limit = pRateLimit({
       interval: 1000,
-      rate: 15, // 5 emails per second
+      rate: 15, // 15 emails per second
 });
 
 exports.limit = limit;
