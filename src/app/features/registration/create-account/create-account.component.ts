c919b1d8b324137f01ca5c4728756de29e39ece7
duncanc19 <duncancarter93@gmail.com>
2021-06-29 13:40:22 +0100

    feat(createAccountStartPage): Create create-account component and set up routing in registration module

---
+++ src/app/features/registration/create-account/create-account.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account',
+  templateUrl: './create-account.component.html',
+})
+export class CreateAccountComponent {}
