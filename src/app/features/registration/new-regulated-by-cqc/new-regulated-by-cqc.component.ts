b9d3107cfefabdaafcc6e889d4f9eec01595f40b
Richard Pentecost <richard.pentecost@madetech.com>
2021-06-30 16:07:39 +0100

    add error messaging to the component if neither radio button is selected

--- src/app/features/registration/new-regulated-by-cqc/new-regulated-by-cqc.component.ts
+++ src/app/features/registration/new-regulated-by-cqc/new-regulated-by-cqc.component.ts
@@ -1,13 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ErrorDetails } from '@core/model/errorSummary.model';
+import { BackService } from '@core/services/back.service';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
+import { RegistrationService } from '@core/services/registration.service';
 
 @Component({
   selector: 'app-new-regulated-by-cqc',
   templateUrl: './new-regulated-by-cqc.component.html',
 })
 export class NewRegulatedByCqcComponent implements OnInit {
-  //   constructor() {}
+  public form: FormGroup;
+  public formErrorsMap: Array<ErrorDetails>;
+  public submitted = false;
+  private flow: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private errorSummaryService: ErrorSummaryService,
+    private registrationService: RegistrationService,
+    private backService: BackService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
-    console.log('something here');
+    this.flow = this.route.snapshot.parent.url[0].path;
+    this.setupForm();
+    this.setupFormErrorsMap();
+    this.backService.setBackLink({ url: [this.flow, 'create-account'] });
+  }
+
+  private setupForm(): void {
+    this.form = this.formBuilder.group({
+      regulatedByCQC: [null, Validators.required],
+    });
+  }
+
+  private setupFormErrorsMap(): void {
+    this.formErrorsMap = [
+      {
+        item: 'regulatedByCQC',
+        type: [
+          {
+            name: 'required',
+            message: `Select yes if the main service you provide is regulated by the Care Quality Commission.`,
+          },
+        ],
+      },
+    ];
+  }
+
+  public getErrorMessage(item: string): string {
+    const errorType = Object.keys(this.form.get(item).errors)[0];
+    return this.errorSummaryService.getFormErrorMessage(item, errorType, this.formErrorsMap);
+  }
+
+  public onSubmit(): void {
+    const regulatedByCQC = this.form.get('regulatedByCQC');
+
+    this.submitted = true;
+    this.registrationService.isRegulated$.next(regulatedByCQC.value === 'yes');
+    this.registrationService.isCqcRegulated$.next(regulatedByCQC.value === 'yes');
+    this.registrationService.registrationInProgress$.next(true);
+
+    if (regulatedByCQC.value === 'yes') {
+      this.router.navigate([`${this.flow}/find-workplace`]);
+    } else {
+      this.router.navigate([`${this.flow}/workplace-name`]);
+    }
   }
 }
