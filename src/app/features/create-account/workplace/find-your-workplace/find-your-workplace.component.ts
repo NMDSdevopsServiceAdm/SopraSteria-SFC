c1dcf221ef580143893441d9fabe7ce2da48ef71
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-07-01 11:22:07 +0100

    Added a new component

---
+++ src/app/features/create-account/workplace/find-your-workplace/find-your-workplace.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-your-workplace',
+  templateUrl: './find-your-workplace.component.html',
+})
+export class FindYourWorkplaceComponent {}
