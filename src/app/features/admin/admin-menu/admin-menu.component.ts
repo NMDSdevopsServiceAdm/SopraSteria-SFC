d486ebc8daea4f5c5169d1b86e6c2ca2c6fd758b
Richard Pentecost <richard.pentecost@madetech.com>
2021-07-21 12:32:11 +0100

    render both admin menu and local authorities component

--- src/app/features/admin/admin-menu/admin-menu.component.ts
+++ src/app/features/admin/admin-menu/admin-menu.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-menu',
   templateUrl: './admin-menu.component.html',
 })
-export class AdminMenuComponent {}
+export class AdminMenuComponent implements OnInit {
+  menuComponents: string[];
+
+  ngOnInit(): void {
+    this.setupMenu();
+  }
+
+  setupMenu(): void {
+    this.menuComponents = [
+      'Search',
+      'Registration requests',
+      'CQC status changes',
+      'Emails',
+      'Admin reports',
+      'Local authority returns',
+      'Admin users',
+      'External links',
+    ];
+  }
+}
--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '@shared/shared.module';
 
+import { AdminMenuComponent } from './admin-menu/admin-menu.component';
 import { AdminRoutingModule } from './admin.routing.module';
 import { LocalAuthoritiesReturnComponent } from './local-authorities-return/local-authorities-return.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, SharedModule, OverlayModule, AdminRoutingModule],
-  declarations: [LocalAuthoritiesReturnComponent],
+  declarations: [AdminMenuComponent, LocalAuthoritiesReturnComponent],
   providers: [],
 })
-export class AdminModuke {}
+export class AdminModule {}
