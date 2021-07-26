9aee1824a2d76e7005d04f97f8507b210d8dd75d
duncanc19 <duncancarter93@gmail.com>
2021-07-26 09:29:08 +0100

    feat(sharedServiceForCreateAccountAndAddWorkplace): Change name-of-workplace to have directive and extend in add-workplace and create-account folders

--- src/app/features/add-workplace/add-workplace.module.ts
+++ src/app/features/add-workplace/add-workplace.module.ts
@@ -12,6 +12,7 @@ import { ConfirmAccountDetailsComponent } from './confirm-account-details/confir
 import { ConfirmWorkplaceDetailsComponent } from './confirm-workplace-details/confirm-workplace-details.component';
 import { CreateUserAccountComponent } from './create-user-account/create-user-account.component';
 import { FindWorkplaceAddressComponent } from './find-workplace-address/find-workplace-address.component';
+import { NameOfWorkplaceComponent } from './name-of-workplace/name-of-workplace.component';
 import { NewRegulatedByCqcComponent } from './new-regulated-by-cqc/new-regulated-by-cqc.component';
 import { SelectMainServiceComponent } from './select-main-service/select-main-service.component';
 import { SelectWorkplaceAddressComponent } from './select-workplace-address/select-workplace-address.component';
@@ -36,6 +37,7 @@ import { WorkplaceNotFoundComponent } from './workplace-not-found/workplace-not-
     StartComponent,
     WorkplaceNotFoundComponent,
     WorkplaceNameAddressComponent,
+    NameOfWorkplaceComponent,
   ],
 })
 export class AddWorkplaceModule {}
