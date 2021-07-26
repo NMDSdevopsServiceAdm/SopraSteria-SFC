bd442a152f2c65a830496cc28c1849e0c7f94921
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-07-26 10:30:04 +0100

    Started adding dates code

--- server/routes/admin/local-authority-return/dates/index.js
+++ server/routes/admin/local-authority-return/dates/index.js
@@ -1,11 +1,17 @@
 'use strict';
 const router = require('express').Router();
-// const models = require('../../../models');
+const models = require('../../../../models');
+
 const getLAReturnDates = async (req, res) => {
   try {
+    const dates = await models.AdminSettings.getLADates();
+
+    console.log(dates);
     return res.status(200).send();
-    //   const getLAReturnDates = await models.dates.
-  } catch (error) {}
+  } catch (error) {
+    console.error(error);
+    return res.status(503).send();
+  }
 };
 
 const setLAReturnDates = async (req, res) => {
