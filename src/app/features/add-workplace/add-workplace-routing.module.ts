a71e947eaf532d0c4f97cd6513cca90683188d69
duncanc19 <duncancarter93@gmail.com>
2021-07-27 10:38:27 +0100

    feat(findYourWorkplaceDirective): Create add-workplace find-your-workplace component and change add-workplace routing to use new component

--- src/app/features/add-workplace/add-workplace-routing.module.ts
+++ src/app/features/add-workplace/add-workplace-routing.module.ts
@@ -14,10 +14,10 @@ import { SelectWorkplaceAddressComponent } from '@features/add-workplace/select-
 import { SelectWorkplaceComponent } from '@features/add-workplace/select-workplace/select-workplace.component';
 import { WorkplaceNameAddressComponent } from '@features/add-workplace/workplace-name-address/workplace-name-address.component';
 import { WorkplaceNotFoundComponent } from '@features/add-workplace/workplace-not-found/workplace-not-found.component';
-import { FindYourWorkplaceComponent } from '@features/create-account/workplace/find-your-workplace/find-your-workplace.component';
 import { IsThisYourWorkplaceComponent } from '@features/create-account/workplace/is-this-your-workplace/is-this-your-workplace.component';
 import { NewSelectMainServiceComponent } from '@features/create-account/workplace/new-select-main-service/new-select-main-service.component';
 
+import { FindYourWorkplaceComponent } from './find-your-workplace/find-your-workplace.component';
 import { NameOfWorkplaceComponent } from './name-of-workplace/name-of-workplace.component';
 import { NewWorkplaceNotFoundComponent } from './new-workplace-not-found/new-workplace-not-found.component';
 import { StartComponent } from './start/start.component';
