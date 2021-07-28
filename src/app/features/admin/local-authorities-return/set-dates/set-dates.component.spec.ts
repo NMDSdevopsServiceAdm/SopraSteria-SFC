9348feac45b9a94ddec22141f509faafd2f6bd94
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-07-28 11:04:45 +0100

    Added test for value check

--- src/app/features/admin/local-authorities-return/set-dates/set-dates.component.spec.ts
+++ src/app/features/admin/local-authorities-return/set-dates/set-dates.component.spec.ts
@@ -1,7 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { LocalAuthoritiesReturnService } from '@core/services/admin/local-authorities-return/local-authorities-return.service';
+import {
+  LocalAuthoritiesReturnService,
+} from '@core/services/admin/local-authorities-return/local-authorities-return.service';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { MockBreadcrumbService } from '@core/test-utils/MockBreadcrumbService';
 import { SharedModule } from '@shared/shared.module';
@@ -25,8 +27,8 @@ describe('SetDatesComponent', () => {
             snapshot: {
               data: {
                 dates: {
-                  laReturnStartDate: new Date('01/01/2001'),
-                  laReturnEndDate: new Date('01/01/2002'),
+                  laReturnStartDate: new Date('2001-01-01'),
+                  laReturnEndDate: new Date('2002-02-02'),
                 },
               },
             },
@@ -43,5 +45,19 @@ describe('SetDatesComponent', () => {
   it('should render a SetDatesComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should display the current dates in the input boxes', async () => {
+    const { component } = await setup();
+
+    const form = component.fixture.componentInstance.form;
+
+    expect(form.get('laReturnStartDate').value.day).toEqual(1);
+    expect(form.get('laReturnStartDate').value.month).toEqual(1);
+    expect(form.get('laReturnStartDate').value.year).toEqual(2001);
+
+    expect(form.get('laReturnEndDate').value.day).toEqual(2);
+    expect(form.get('laReturnEndDate').value.month).toEqual(2);
+    expect(form.get('laReturnEndDate').value.year).toEqual(2002);
   });
 });
