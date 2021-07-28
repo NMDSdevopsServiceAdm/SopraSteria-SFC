28a1beeda2c05faf8b1d33831bd2385789dc37aa
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-07-28 15:33:05 +0100

    Added tests

--- src/app/features/admin/local-authorities-return/set-dates/set-dates.component.spec.ts
+++ src/app/features/admin/local-authorities-return/set-dates/set-dates.component.spec.ts
@@ -27,7 +27,7 @@ describe('SetDatesComponent', () => {
             snapshot: {
               data: {
                 dates: {
-                  laReturnStartDate: new Date('2001-01-01'),
+                  laReturnStartDate: new Date('2020-01-01'),
                   laReturnEndDate: new Date('2002-02-02'),
                 },
               },
@@ -54,10 +54,141 @@ describe('SetDatesComponent', () => {
 
     expect(form.get('laReturnStartDate').value.day).toEqual(1);
     expect(form.get('laReturnStartDate').value.month).toEqual(1);
-    expect(form.get('laReturnStartDate').value.year).toEqual(2001);
+    expect(form.get('laReturnStartDate').value.year).toEqual(2020);
 
     expect(form.get('laReturnEndDate').value.day).toEqual(2);
     expect(form.get('laReturnEndDate').value.month).toEqual(2);
     expect(form.get('laReturnEndDate').value.year).toEqual(2002);
+  });
+
+  it('should show a blank error when nothing in the start date input boxes', async () => {
+    const { component } = await setup();
+
+    expect(component.queryByText('Start date is required', { exact: false })).toBe(null);
+
+    const form = component.fixture.componentInstance.form;
+    form.setValue({
+      laReturnStartDate: {
+        day: null,
+        month: null,
+        year: null,
+      },
+      laReturnEndDate: {
+        day: 12,
+        month: 12,
+        year: 2012,
+      },
+    });
+    component.fixture.componentInstance.onSubmit();
+
+    component.fixture.detectChanges();
+
+    expect(form.invalid).toBeTruthy();
+    expect(component.getAllByText('Start date is required', { exact: false }).length).toBe(2);
+  });
+
+  it('should show a an error if an invalid date', async () => {
+    const { component } = await setup();
+
+    expect(component.queryByText('Start date is not a valid date', { exact: false })).toBe(null);
+
+    const form = component.fixture.componentInstance.form;
+    form.setValue({
+      laReturnStartDate: {
+        day: 50,
+        month: 14,
+        year: 2098,
+      },
+      laReturnEndDate: {
+        day: 12,
+        month: 12,
+        year: 2012,
+      },
+    });
+    component.fixture.componentInstance.onSubmit();
+
+    component.fixture.detectChanges();
+
+    expect(form.invalid).toBeTruthy();
+    expect(component.getAllByText('Start date is not a valid date', { exact: false }).length).toBe(2);
+  });
+
+  it('should show a blank error when nothing in the end date input boxes', async () => {
+    const { component } = await setup();
+
+    expect(component.queryByText('End date is required', { exact: false })).toBe(null);
+
+    const form = component.fixture.componentInstance.form;
+    form.setValue({
+      laReturnStartDate: {
+        day: 11,
+        month: 11,
+        year: 2011,
+      },
+      laReturnEndDate: {
+        day: null,
+        month: null,
+        year: null,
+      },
+    });
+    component.fixture.componentInstance.onSubmit();
+
+    component.fixture.detectChanges();
+
+    expect(form.invalid).toBeTruthy();
+    expect(component.getAllByText('End date is required', { exact: false }).length).toBe(2);
+  });
+
+  it('should show a an error if an invalid date', async () => {
+    const { component } = await setup();
+
+    expect(component.queryByText('End date is not a valid date', { exact: false })).toBe(null);
+
+    const form = component.fixture.componentInstance.form;
+    form.setValue({
+      laReturnStartDate: {
+        day: 11,
+        month: 11,
+        year: 2011,
+      },
+      laReturnEndDate: {
+        day: 50,
+        month: 89,
+        year: 2029,
+      },
+    });
+    component.fixture.componentInstance.onSubmit();
+
+    component.fixture.detectChanges();
+
+    expect(form.invalid).toBeTruthy();
+    expect(component.getAllByText('End date is not a valid date', { exact: false }).length).toBe(2);
+  });
+
+  it('should ahow an error if the end date is before the start date', async () => {
+    const { component } = await setup();
+
+    expect(component.queryByText('End date must be after start date', { exact: false })).toBe(null);
+
+    const form = component.fixture.componentInstance.form;
+    form.setValue({
+      laReturnStartDate: {
+        day: 11,
+        month: 11,
+        year: 2011,
+      },
+      laReturnEndDate: {
+        day: 10,
+        month: 10,
+        year: 2010,
+      },
+    });
+
+    component.fixture.componentInstance.onSubmit();
+
+    component.fixture.detectChanges();
+
+    expect(form.invalid).toBeTruthy();
+    expect(component.getAllByText('End date must be after start date', { exact: false }).length).toBe(2);
   });
 });
--- src/app/features/admin/local-authorities-return/set-dates/set-dates.component.ts
+++ src/app/features/admin/local-authorities-return/set-dates/set-dates.component.ts
@@ -1,10 +1,12 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { SetDates } from '@core/model/admin/local-authorities-return.model';
-import { ErrorDetails } from '@core/model/errorSummary.model';
-import { LocalAuthoritiesReturnService } from '@core/services/admin/local-authorities-return/local-authorities-return.service';
+import { ErrorDefinition, ErrorDetails } from '@core/model/errorSummary.model';
+import {
+  LocalAuthoritiesReturnService,
+} from '@core/services/admin/local-authorities-return/local-authorities-return.service';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { DateValidator } from '@shared/validators/date.validator';
@@ -18,6 +20,7 @@ export class SetDatesComponent implements OnInit, AfterViewInit {
   public form: FormGroup;
   public submitted: boolean;
   public serverError: string;
+  protected serverErrorsMap: Array<ErrorDefinition>;
   public formErrorsMap: Array<ErrorDetails>;
 
   constructor(
@@ -26,6 +29,7 @@ export class SetDatesComponent implements OnInit, AfterViewInit {
     private errorSummaryService: ErrorSummaryService,
     private breadcrumbService: BreadcrumbService,
     private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
@@ -91,6 +95,10 @@ export class SetDatesComponent implements OnInit, AfterViewInit {
     return this.errorSummaryService.getFormErrorMessage(item, errorType, this.formErrorsMap);
   }
 
+  private formatSingleDigit(value: number): string {
+    return String(value < 10 ? `0${value}` : value);
+  }
+
   public onSubmit(): void {
     this.submitted = true;
     this.errorSummaryService.syncFormErrorsEvent.next(true);
@@ -98,15 +106,23 @@ export class SetDatesComponent implements OnInit, AfterViewInit {
     if (this.form.invalid) {
       this.errorSummaryService.scrollToErrorSummary();
       return;
+    } else {
+      const { year: startYear, month: startMonth, day: startDay } = this.form.get('laReturnStartDate').value;
+      const { year: endYear, month: endMonth, day: endDay } = this.form.get('laReturnEndDate').value;
+
+      const laReturnDates: SetDates = {
+        laReturnStartDate: new Date(
+          `${this.formatSingleDigit(startYear)}-${this.formatSingleDigit(startMonth)}-${this.formatSingleDigit(
+            startDay,
+          )}`,
+        ),
+        laReturnEndDate: new Date(
+          `${this.formatSingleDigit(endYear)}-${this.formatSingleDigit(endMonth)}-${this.formatSingleDigit(endDay)}`,
+        ),
+      };
+      this.localAuthoritiesReturnService
+        .setDates(laReturnDates)
+        .subscribe(() => this.router.navigate(['sfcadmin', 'local-authorities-return']));
     }
-
-    const { year: startYear, month: startMonth, day: startDay } = this.form.get('laReturnStartDate').value;
-    const { year: endYear, month: endMonth, day: endDay } = this.form.get('laReturnEndDate').value;
-
-    const laReturnDates: SetDates = {
-      laReturnStartDate: new Date(`${startYear}-${startMonth}-${startDay}`),
-      laReturnEndDate: new Date(`${endYear}-${endMonth}-${endDay}`),
-    };
-    this.localAuthoritiesReturnService.setDates(laReturnDates).subscribe();
   }
 }
