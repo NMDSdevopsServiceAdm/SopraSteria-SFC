69307b2ccc74f53c5abec898d5e76a76323fbba1
joannafawl <joanna.fawl@madetech.com>
2021-07-29 11:17:16 +0100

    Move setWorkplaceDetails function into directive

--- src/app/features/registration/confirm-workplace-details/confirm-workplace-details.component.ts
+++ src/app/features/registration/confirm-workplace-details/confirm-workplace-details.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SummaryList } from '@core/model/summary-list.model';
 import { BackService } from '@core/services/back.service';
 import { RegistrationService } from '@core/services/registration.service';
 import {
@@ -12,13 +11,8 @@ import { FeatureFlagsService } from '@shared/services/feature-flags.service';
   templateUrl: './confirm-workplace-details.component.html',
 })
 export class ConfirmWorkplaceDetailsComponent extends ConfirmWorkplaceDetails {
-  public workplaceName: SummaryList[];
-  public workplaceAddress: SummaryList[];
-  public mainService: SummaryList[];
-  private address: string;
-
   constructor(
-    private registrationService: RegistrationService,
+    protected registrationService: RegistrationService,
     protected backService: BackService,
     protected featureFlagsService: FeatureFlagsService,
   ) {
@@ -28,97 +22,10 @@ export class ConfirmWorkplaceDetailsComponent extends ConfirmWorkplaceDetails {
   protected async init(): Promise<void> {
     this.flow = '/registration';
     this.getWorkplaceData();
-    this.setAddress();
-    this.setWorkplaceDetails();
   }
 
   protected getWorkplaceData(): void {
     this.locationAddress = this.registrationService.selectedLocationAddress$.value;
     this.workplace = this.registrationService.selectedWorkplaceService$.value;
-  }
-
-  public setWorkplaceDetails(): void {
-    if (this.workplace.isCQC && this.locationAddress.locationId) {
-      this.setCqcLocationIdWorkplaceDetails();
-    }
-    if (this.workplace.isCQC && !this.locationAddress.locationId) {
-      this.setCqcWorkplaceDetails();
-    }
-    if (!this.workplace.isCQC) {
-      this.setNonCqcWorkplaceDetails();
-    }
-    this.mainService = [
-      {
-        label: 'Main service',
-        data: this.workplace.name,
-        route: { url: ['/'] },
-      },
-    ];
-  }
-
-  private setCqcLocationIdWorkplaceDetails(): void {
-    this.workplaceAddress = [
-      {
-        label: 'CQC location ID',
-        data: this.locationAddress.locationId,
-        route: { url: ['/'] },
-      },
-      {
-        label: 'Name and address',
-        data: `${this.locationAddress.locationName}
-        ${this.address}`,
-      },
-    ];
-  }
-
-  private setCqcWorkplaceDetails(): void {
-    this.workplaceAddress = [
-      {
-        label: 'Name',
-        data: this.locationAddress.locationName,
-        route: { url: ['/'] },
-      },
-      {
-        label: 'Address',
-        data: this.address,
-      },
-    ];
-  }
-
-  private setNonCqcWorkplaceDetails(): void {
-    this.workplaceName = [
-      {
-        label: 'Name',
-        data: this.locationAddress.locationName,
-        route: { url: ['/'] },
-      },
-    ];
-    this.workplaceAddress = [
-      {
-        label: 'Address',
-        data: this.address,
-        route: { url: ['/'] },
-      },
-    ];
-  }
-
-  private setAddress(): void {
-    this.address = [
-      this.locationAddress.addressLine1,
-      this.locationAddress.addressLine2,
-      this.locationAddress.addressLine3,
-      this.locationAddress.townCity,
-      this.locationAddress.county,
-      this.locationAddress.postalCode,
-    ].join('\n');
-    console.log(this.address);
-
-    // this.address = `
-    // ${this.locationAddress.addressLine1}\n
-    // ${this.locationAddress.addressLine2}\n
-    // ${this.locationAddress.addressLine3}\n
-    // ${this.locationAddress.townCity}\n
-    // ${this.locationAddress.county}\n
-    // ${this.locationAddress.postalCode}`;
   }
 }
--- src/app/features/workplace-find-and-select/confirm-workplace-details/confirm-workplace-details.ts
+++ src/app/features/workplace-find-and-select/confirm-workplace-details/confirm-workplace-details.ts
@@ -1,6 +1,7 @@
 import { Directive, OnDestroy, OnInit } from '@angular/core';
 import { LocationAddress } from '@core/model/location.model';
 import { Service } from '@core/model/services.model';
+import { SummaryList } from '@core/model/summary-list.model';
 import { BackService } from '@core/services/back.service';
 import { FeatureFlagsService } from '@shared/services/feature-flags.service';
 import { Subscription } from 'rxjs';
@@ -11,6 +12,10 @@ export class ConfirmWorkplaceDetails implements OnInit, OnDestroy {
   public locationAddress: LocationAddress;
   public workplace: Service;
   public createAccountNewDesign: boolean;
+  public workplaceName: SummaryList[];
+  public workplaceAddress: SummaryList[];
+  public mainService: SummaryList[];
+  protected address: string;
   protected subscriptions: Subscription = new Subscription();
 
   constructor(protected backService: BackService, protected featureFlagsService: FeatureFlagsService) {}
@@ -21,6 +26,8 @@ export class ConfirmWorkplaceDetails implements OnInit, OnDestroy {
       false,
     );
     this.init();
+    this.setAddress();
+    this.setWorkplaceDetails();
     this.setBackLink();
   }
 
@@ -30,6 +37,91 @@ export class ConfirmWorkplaceDetails implements OnInit, OnDestroy {
 
   protected setBackLink(): void {
     this.backService.setBackLink({ url: [`${this.flow}/select-main-service`] });
+  }
+
+  public setWorkplaceDetails(): void {
+    if (this.workplace.isCQC && this.locationAddress.locationId) {
+      this.setCqcLocationIdWorkplaceDetails();
+    }
+    if (this.workplace.isCQC && !this.locationAddress.locationId) {
+      this.setCqcWorkplaceDetails();
+    }
+    if (!this.workplace.isCQC) {
+      this.setNonCqcWorkplaceDetails();
+    }
+    this.mainService = [
+      {
+        label: 'Main service',
+        data: this.workplace.name,
+        route: { url: ['/'] },
+      },
+    ];
+  }
+
+  protected setCqcLocationIdWorkplaceDetails(): void {
+    this.workplaceAddress = [
+      {
+        label: 'CQC location ID',
+        data: this.locationAddress.locationId,
+        route: { url: ['/'] },
+      },
+      {
+        label: 'Name and address',
+        data: `${this.locationAddress.locationName}
+        ${this.address}`,
+      },
+    ];
+  }
+
+  protected setCqcWorkplaceDetails(): void {
+    this.workplaceAddress = [
+      {
+        label: 'Name',
+        data: this.locationAddress.locationName,
+        route: { url: ['/'] },
+      },
+      {
+        label: 'Address',
+        data: this.address,
+      },
+    ];
+  }
+
+  protected setNonCqcWorkplaceDetails(): void {
+    this.workplaceName = [
+      {
+        label: 'Name',
+        data: this.locationAddress.locationName,
+        route: { url: ['/'] },
+      },
+    ];
+    this.workplaceAddress = [
+      {
+        label: 'Address',
+        data: this.address,
+        route: { url: ['/'] },
+      },
+    ];
+  }
+
+  protected setAddress(): void {
+    this.address = [
+      this.locationAddress.addressLine1,
+      this.locationAddress.addressLine2,
+      this.locationAddress.addressLine3,
+      this.locationAddress.townCity,
+      this.locationAddress.county,
+      this.locationAddress.postalCode,
+    ].join('\n');
+    console.log(this.address);
+
+    // this.address = `
+    // ${this.locationAddress.addressLine1}\n
+    // ${this.locationAddress.addressLine2}\n
+    // ${this.locationAddress.addressLine3}\n
+    // ${this.locationAddress.townCity}\n
+    // ${this.locationAddress.county}\n
+    // ${this.locationAddress.postalCode}`;
   }
 
   ngOnDestroy() {
