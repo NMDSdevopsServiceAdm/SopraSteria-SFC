c83547e16ccb79ed07450bf51b45abf7d65d19d0
duncanc19 <duncancarter93@gmail.com>
2021-08-03 16:04:40 +0100

    feat(couldntFindWorkplaceAddress): Add form to add-workplace flow

--- src/app/features/create-account/workplace/could-not-find-workplace-address/could-not-find-workplace-address.component.ts
+++ src/app/features/create-account/workplace/could-not-find-workplace-address/could-not-find-workplace-address.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BackService } from '@core/services/back.service';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
+import { EstablishmentService } from '@core/services/establishment.service';
 import { RegistrationService } from '@core/services/registration.service';
 import { CouldNotFindWorkplaceAddressDirective } from '@shared/directives/create-workplace/could-not-find-workplace-address/could-not-find-workplace-address.directive';
 
@@ -12,8 +15,12 @@ export class CouldNotFindWorkplaceAddressComponent extends CouldNotFindWorkplace
   constructor(
     protected registrationService: RegistrationService,
     public backService: BackService,
+    protected establishmentService: EstablishmentService,
+    protected formBuilder: FormBuilder,
+    protected errorSummaryService: ErrorSummaryService,
+    protected router: Router,
     protected route: ActivatedRoute,
   ) {
-    super(registrationService, backService, route);
+    super(registrationService, backService, establishmentService, formBuilder, errorSummaryService, router, route);
   }
 }
