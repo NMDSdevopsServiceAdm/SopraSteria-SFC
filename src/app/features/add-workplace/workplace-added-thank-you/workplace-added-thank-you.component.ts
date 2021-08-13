95a54101e5032466ea782f23b0b4a04a7ba7daa4
duncanc19 <duncancarter93@gmail.com>
2021-08-13 12:18:00 +0100

    feat(parentThankYouPage): Refactor thank you components to remove ngOnInit

--- src/app/features/add-workplace/workplace-added-thank-you/workplace-added-thank-you.component.ts
+++ src/app/features/add-workplace/workplace-added-thank-you/workplace-added-thank-you.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { WorkplaceService } from '@core/services/workplace.service';
 
 @Component({
   selector: 'app-workplace-added-thank-you',
   templateUrl: './workplace-added-thank-you.component.html',
 })
-export class WorkplaceAddedThankYouComponent implements OnInit {
-  constructor(private workplaceService: WorkplaceService) {}
-
-  ngOnInit(): void {
+export class WorkplaceAddedThankYouComponent {
+  constructor(private workplaceService: WorkplaceService) {
     this.workplaceService.addWorkplaceInProgress$.next(false);
   }
 }
