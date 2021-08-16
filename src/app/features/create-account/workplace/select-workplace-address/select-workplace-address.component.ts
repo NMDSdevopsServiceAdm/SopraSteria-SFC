b891606ca659fd4396be119bb17e288c8b89ffdc
Joanna Fawl <66678865+joannafawl@users.noreply.github.com>
2021-08-16 16:50:44 +0100

    Set workplaceNotFound$ to false on select-workplace-address page (#3888)

--- src/app/features/create-account/workplace/select-workplace-address/select-workplace-address.component.ts
+++ src/app/features/create-account/workplace/select-workplace-address/select-workplace-address.component.ts
@@ -27,6 +27,7 @@ export class SelectWorkplaceAddressComponent extends SelectWorkplaceAddressDirec
   protected init(): void {
     this.flow = '/registration';
     this.returnToConfirmDetails = this.registrationService.returnTo$.value;
+    this.registrationService.workplaceNotFound$.next(false);
     this.registrationService.manuallyEnteredWorkplace$.next(false);
     this.registrationService.manuallyEnteredWorkplaceName$.next(false);
     this.setupSubscriptions();
