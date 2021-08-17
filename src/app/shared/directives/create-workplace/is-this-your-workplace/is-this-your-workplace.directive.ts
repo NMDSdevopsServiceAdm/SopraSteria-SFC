a79b28a0ba7bd51d9623df8eb8dbc697be5ed1f2
Richard Pentecost <richard.pentecost@madetech.com>
2021-08-17 09:04:16 +0100

    refactor if statement check and turn off feature flag by default

--- src/app/shared/directives/create-workplace/is-this-your-workplace/is-this-your-workplace.directive.ts
+++ src/app/shared/directives/create-workplace/is-this-your-workplace/is-this-your-workplace.directive.ts
@@ -67,10 +67,7 @@ export class IsThisYourWorkplaceDirective implements OnInit, AfterViewInit {
   }
 
   protected prefillForm(): void {
-    if (
-      this.workplaceInterfaceService.selectedLocationAddress$.value &&
-      this.workplaceInterfaceService.selectedLocationAddress$.value.locationId === this.locationData.locationId
-    ) {
+    if (this.workplaceInterfaceService.selectedLocationAddress$.value?.locationId === this.locationData.locationId) {
       this.form.patchValue({
         yourWorkplace: 'yes',
       });
