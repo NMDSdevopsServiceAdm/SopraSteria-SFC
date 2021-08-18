c711af7189ef17a258443739d97236149f5be4d5
joannafawl <joanna.fawl@madetech.com>
2021-08-18 16:05:23 +0100

    Fix naming of health and social care degree

--- migrations/20210818130914-addNewQualification.js
+++ migrations/20210818130914-addNewQualification.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query(
       `INSERT INTO cqc."Qualifications" VALUES
-          (136, 685, 'Degree', 'Health and Social Care degree 6', 144, null, null, 6, false, false, 'QL144ACHQ6');`
+          (136, 685, 'Degree', 'Health and Social Care degree', 144, null, null, 6, false, false, 'QL144ACHQ6');`
     );
   },
 
