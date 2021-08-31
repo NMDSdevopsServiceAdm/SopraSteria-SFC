6962ab1d169481e67518705d68aed351d0df840c
Richard Pentecost <richard.pentecost@madetech.com>
2021-08-31 11:12:18 +0100

    create cancelBecomeAParent function in the parent request service

--- src/app/shared/components/become-a-parent-cancel/become-a-parent-cancel-dialog.component.ts
+++ src/app/shared/components/become-a-parent-cancel/become-a-parent-cancel-dialog.component.ts
@@ -1,19 +1,18 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { DialogComponent } from '@core/components/dialog.component';
 import { ErrorDefinition } from '@core/model/errorSummary.model';
 import { DataPermissions, Workplace } from '@core/model/my-workplaces.model';
 import { AlertService } from '@core/services/alert.service';
 import { Dialog, DIALOG_DATA } from '@core/services/dialog.service';
-import { ErrorSummaryService } from '@core/services/error-summary.service';
-import { EstablishmentService } from '@core/services/establishment.service';
+import { ParentRequestsService } from '@core/services/parent-requests.service';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-become-a-parent-cancel-dialog',
   templateUrl: './become-a-parent-cancel-dialog.component.html',
 })
-export class BecomeAParentCancelDialogComponent extends DialogComponent implements OnInit, OnDestroy {
+export class BecomeAParentCancelDialogComponent extends DialogComponent implements OnDestroy {
   public dataPermissions: DataPermissions[];
   public workplace: Workplace;
   protected subscriptions: Subscription = new Subscription();
@@ -24,8 +23,7 @@ export class BecomeAParentCancelDialogComponent extends DialogComponent implemen
 
   constructor(
     @Inject(DIALOG_DATA) public data,
-    private establishmentService: EstablishmentService,
-    private errorSummaryService: ErrorSummaryService,
+    private parentRequestsService: ParentRequestsService,
     public dialog: Dialog<BecomeAParentCancelDialogComponent>,
     private alertService: AlertService,
     private router: Router,
@@ -33,63 +31,27 @@ export class BecomeAParentCancelDialogComponent extends DialogComponent implemen
     super(data, dialog);
   }
 
-  ngOnInit() {
-    this.workplace = this.data;
-    this.setupServerErrorsMap();
-  }
-
-  //setup server error message
-  private setupServerErrorsMap(): void {
-    this.serverErrorsMap = [
-      {
-        name: 503,
-        message: 'We could not send request to parent. You can try again or contact us.',
-      },
-      {
-        name: 400,
-        message: 'Unable to send request to parent.',
-      },
-      {
-        name: 404,
-        message: 'Send request to parent service not found. You can try again or contact us.',
-      },
-    ];
-  }
-
   /**
    * Function is used to close dialog window after successful confirmation
    * @param {any} true to close dialog after response or null to close without action
    * @return {void}
    */
-  public closeDialogWindow(confirm: any) {
+  public closeDialogWindow(confirm: any): void {
     this.dialog.close(confirm);
   }
 
-  /**
-   * Function is used to send cancel request to server for parent linking with request payload selected parent's uid and permission type
-   * @param {void}
-   * @return {void}
-   */
-  public cancelRequestToParent() {
+  public canelRequestToBecomeAParent(): void {
     this.subscriptions.add(
-      this.establishmentService
-        .cancelRequestToParentForLink(this.workplace.uid, { approvalStatus: 'CANCELLED' })
-        .subscribe(
-          (data) => {
-            if (data) {
-              const parentName = data[0].requstedParentName;
-              this.router.navigate(['/dashboard']);
-              this.alertService.addAlert({
-                type: 'success',
-                message: `Request to link to ${parentName} has been cancelled.`,
-              });
-              this.closeDialogWindow(true);
-            }
-          },
-          (error) => {
-            this.serverError = this.errorSummaryService.getServerErrorMessage(error.status, this.serverErrorsMap);
-          },
-        ),
+      this.parentRequestsService.cancelBecomeAParent().subscribe((data) => {
+        if (data) {
+          this.router.navigate(['/dashboard']);
+          this.alertService.addAlert({
+            type: 'success',
+            message: 'Request to become a parent organisation has been cancelled.',
+          });
+        }
+        this.closeDialogWindow(true);
+      }),
     );
   }
 
