404cbb57bc85495be6fe0226a546d40d8f1bd530
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-09-06 13:45:32 +0100

    Refactored add-edit-training component to use directive
    Co-authored-by: Zuhal Ayob <zuhal.ayob@skillsforcare.org.uk>

---
+++ src/app/core/test-utils/MockTrainingService.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+import { TrainingService } from '@core/services/training.service';
+
+@Injectable()
+export class MockTrainingService extends TrainingService {}
