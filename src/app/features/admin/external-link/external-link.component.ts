df147557edd4e3f285b40470cea2010a8c9d0407
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2021-09-16 15:32:08 +0100

    Add external page and clickable links to the page

---
+++ src/app/features/admin/external-link/external-link.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-external-link',
+  templateUrl: './external-link.component.html',
+  styleUrls: ['./external-link.component.scss'],
+})
+export class ExternalLinkComponent {}
