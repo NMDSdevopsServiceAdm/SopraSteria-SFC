14726f0f552ca208b04a7f782a49c42bfc90045b
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2021-09-17 11:57:59 +0100

    remove scss file

--- src/app/features/admin/external-link/external-link.component.ts
+++ src/app/features/admin/external-link/external-link.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-external-link',
   templateUrl: './external-link.component.html',
-  styleUrls: ['./external-link.component.scss'],
 })
 export class ExternalLinkComponent {}
