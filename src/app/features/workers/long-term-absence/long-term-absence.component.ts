3b6ebfc30385c6d7621aba24a19e91d802f2e858
joannafawl <joanna.fawl@madetech.com>
2021-09-20 16:15:58 +0100

    Set up basic form

--- src/app/features/workers/long-term-absence/long-term-absence.component.ts
+++ src/app/features/workers/long-term-absence/long-term-absence.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ErrorDetails } from '@core/model/errorSummary.model';
 import { Establishment } from '@core/model/establishment.model';
 import { URLStructure } from '@core/model/url.model';
 import { Worker } from '@core/model/worker.model';
 import { BackService } from '@core/services/back.service';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { WorkerService } from '@core/services/worker.service';
 
 @Component({
@@ -11,19 +14,64 @@ import { WorkerService } from '@core/services/worker.service';
   templateUrl: './long-term-absence.component.html',
 })
 export class LongTermAbsenceComponent implements OnInit {
+  @ViewChild('formEl') formEl: ElementRef;
   public worker: Worker;
   public returnToUrl: URLStructure;
+  public form: FormGroup;
+  public submitted: boolean;
+  public longTermAbsenceReasons = [];
+  private formErrorsMap: Array<ErrorDetails>;
   private workplace: Establishment;
-  constructor(private route: ActivatedRoute, private backService: BackService, private workerService: WorkerService) {}
+
+  constructor(
+    private route: ActivatedRoute,
+    private backService: BackService,
+    private workerService: WorkerService,
+    private formBuilder: FormBuilder,
+    private errorSummaryService: ErrorSummaryService,
+  ) {}
 
   ngOnInit(): void {
     this.worker = this.route.snapshot.data.worker;
     this.workplace = this.route.snapshot.data.establishment;
     this.returnToUrl = this.workerService.returnTo;
+    this.longTermAbsenceReasons = ['Maternity leave', 'Paternity leave', 'Illness', 'Injury', 'Other'];
+    this.setupForm();
     this.setBackLink();
   }
 
+  ngAfterViewInit(): void {
+    this.errorSummaryService.formEl$.next(this.formEl);
+  }
+
+  get longTermAbsence(): FormArray {
+    return this.form.get('longTermAbsence') as FormArray;
+  }
+
+  private setupForm = () => {
+    const longTermAbsenceFormArray = this.longTermAbsenceReasons.map((reason) => {
+      return this.formBuilder.control({
+        name: reason,
+      });
+    });
+
+    this.form = this.formBuilder.group(
+      {
+        longTermAbsence: this.formBuilder.array(longTermAbsenceFormArray),
+      },
+      {
+        validator: Validators.required,
+      },
+    );
+    console.log(this.form);
+  };
+
   public setBackLink(): void {
     this.backService.setBackLink(this.returnToUrl);
+  }
+
+  public onSubmit(): void {
+    this.submitted = true;
+    console.log('submitted');
   }
 }
