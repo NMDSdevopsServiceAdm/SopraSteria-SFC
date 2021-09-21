c46bf5f598e7a2f6589eacc60056b3e898085ce6
Zuhal-Ayob <90700942+Zuhal-Ayob@users.noreply.github.com>
2021-09-21 10:31:49 +0100

    Feat/add admin reports page (#4016)
    * add admain report page
    
    * change the name of component
    
    * Add downloadable links to the Admin Report page.
    
    * Add focus on downloadable links
    
    * fix test
    
    * fix typo

--- src/app/features/admin/admin.routing.module.ts
+++ src/app/features/admin/admin.routing.module.ts
@@ -15,6 +15,7 @@ import { MonitorComponent } from './local-authorities-return/monitor/monitor.com
 import { SetDatesComponent } from './local-authorities-return/set-dates/set-dates.component';
 import { RegistrationRequestComponent } from './registration-requests/registration-request/registration-request.component';
 import { RegistrationRequestsComponent } from './registration-requests/registration-requests.component';
+import { ReportComponent } from './report/admin-report.component';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
@@ -48,6 +49,16 @@ const routes: Routes = [
         resolve: {
           registration: GetSingleRegistrationResolver,
         },
+      },
+    ],
+  },
+  {
+    path: 'admin-reports',
+    children: [
+      {
+        path: '',
+        component: ReportComponent,
+        data: { title: 'Admin reports' },
       },
     ],
   },
