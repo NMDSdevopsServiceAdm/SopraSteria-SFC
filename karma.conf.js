e5733880b3ae0d5723fc5bfd62aa58f8d0c67168
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-09-24 15:14:26 +0100

    Removed !isCI

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
   process.env.no_proxy = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
   config.set({
     basePath: '',
-    frameworks: !isCI
+    frameworks: isCI
       ? ['parallel', 'jasmine', '@angular-devkit/build-angular']
       : ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
