9eeb65b378bffb33963b3814eeeb1f2e1d4317ba
duncanc19 <duncancarter93@gmail.com>
2021-09-29 15:50:06 +0100

    feat(trainingAndQualsHowToTab): Add text content to how to worksheet

--- server/routes/reports/trainingAndQualifications/generateTrainingAndQualificationsReport.js
+++ server/routes/reports/trainingAndQualifications/generateTrainingAndQualificationsReport.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const excelJS = require('exceljs');
 const express = require('express');
 const router = express.Router({ mergeParams: true });
@@ -9,7 +10,53 @@ const generateTrainingAndQualificationsReport = async (_, res) => {
   workbook.creator = 'Skills-For-Care';
   workbook.properties.date1904 = true;
 
-  workbook.addWorksheet('How to...');
+  const howToTab = workbook.addWorksheet('How to...');
+
+  setColourBars(howToTab);
+
+  howToTab.mergeCells('B5:K5');
+  howToTab.getCell('B5').value = 'Training and qualifications report';
+  howToTab.getCell('B5').font = {
+    family: 4,
+    size: 14,
+    bold: true,
+    color: '123387',
+  };
+
+  const filteringInstructionsTitle = 'How to sort and filter the table columns';
+
+  howToTab.mergeCells('B7:K7');
+  howToTab.getCell('B7').value = filteringInstructionsTitle;
+  howToTab.getCell('B7').alignment = { vertical: 'top', horizontal: 'left', wrapText: true };
+
+  const filteringInstructions =
+    'Click on the arrow in the header of the column you want to sort or filter. In the menu displayed, you can sort the data to suit your needs (for example, you can sort it alphabetically). The menu also lets you select what you want to view by filtering the data so that only certain rows are shown.';
+
+  howToTab.mergeCells('B9:K12');
+  howToTab.getCell('B9').value = filteringInstructions;
+  howToTab.getCell('B9').alignment = { vertical: 'top', horizontal: 'left', wrapText: true };
+
+  const howToDeleteTitle = 'How to delete unwanted sheets';
+
+  howToTab.mergeCells('B14:F14');
+  howToTab.getCell('B14').value = howToDeleteTitle;
+
+  const howToDelete =
+    "To delete a sheet in this report, right click on the tab at the bottom of the sheet and select delete. Please note, you cannot undo this action. Only delete a sheet if you're sure you do not need that information. If you accidentally delete a sheet, re-open the report if you've not saved over the file or download the report again from ASC-WDS.";
+
+  howToTab.mergeCells('B16:F19');
+  howToTab.getCell('B16').value = howToDelete;
+
+  const howToPrintTitle = 'How to print the information in this file';
+
+  howToTab.mergeCells('B21:F21');
+  howToTab.getCell('B21').value = howToPrintTitle;
+
+  const howToPrint =
+    "To print the information in this report, click on the 'File' menu and then click 'Print'. You can choose to only print the page you're currently looking at or perhaps change the settings to print the whole report.";
+
+  howToTab.mergeCells('B23:F25');
+  howToTab.getCell('B23').value = howToPrint;
 
   res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
   res.setHeader(
@@ -19,6 +66,21 @@ const generateTrainingAndQualificationsReport = async (_, res) => {
 
   await workbook.xlsx.write(res);
   return res.status(200).end();
+};
+
+const setColourBars = (worksheet) => {
+  setColourLine(worksheet, 1, '123387');
+  setColourLine(worksheet, 2, '6a88d4');
+  setColourLine(worksheet, 3, '638eab');
+};
+
+const setColourLine = (worksheet, lineNumber, colour) => {
+  worksheet.mergeCells(`A${lineNumber}:K${lineNumber}`);
+  worksheet.getCell(`A${lineNumber}`).fill = {
+    type: 'pattern',
+    pattern: 'solid',
+    fgColor: { argb: colour },
+  };
 };
 
 router.route('/').get(generateTrainingAndQualificationsReport);
