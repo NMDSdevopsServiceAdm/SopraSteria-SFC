b7c1a8eeb3aa2015483b8342ac7ffb437b8662f0
duncanc19 <duncancarter93@gmail.com>
2021-10-04 15:29:01 +0100

    feat(trainingAndQualsSummaryTab): Create expired table and refactor to use helper functions

--- server/routes/reports/trainingAndQualifications/summaryTab.js
+++ server/routes/reports/trainingAndQualifications/summaryTab.js
@@ -2,12 +2,38 @@ const { addHeading, addLine } = require('../../../utils/excelUtils');
 const models = require('../../../models');
 
 const generateSummaryTab = async (workbook, establishmentId) => {
+  const workers = await models.worker.workersAndTraining(establishmentId);
+  const convertedWorkers = workers.map((worker) => {
+    return convertWorker(worker);
+  });
+
   const summaryTab = workbook.addWorksheet('Training (summary)', { views: [{ showGridLines: false }] });
 
   addHeading(summaryTab, 'B2', 'E2', 'Training (summary)');
   addLine(summaryTab, 'A4', 'E4');
 
-  const expiringSoonTable = summaryTab.addTable({
+  const expiringSoonTable = createExpiringSoonTable(summaryTab);
+
+  let currentLineNumber = convertedWorkers.length + 9;
+
+  const expiredTable = createExpiredTable(summaryTab, currentLineNumber);
+
+  currentLineNumber = currentLineNumber + convertedWorkers.length + 3;
+  const missingTable = createMissingTable(summaryTab, currentLineNumber);
+
+  for (let worker of convertedWorkers) {
+    expiringSoonTable.addRow([worker.name, worker.expiringTrainingCount]);
+    expiredTable.addRow([worker.name, worker.expiredTrainingCount]);
+    missingTable.addRow([worker.name, worker.missingMandatoryTrainingCount]);
+  }
+
+  expiringSoonTable.commit();
+  expiredTable.commit();
+  missingTable.commit();
+};
+
+const createExpiringSoonTable = (tab) => {
+  return tab.addTable({
     name: 'expiringSoonTable',
     ref: 'B6',
     headerRow: true,
@@ -19,23 +45,27 @@ const generateSummaryTab = async (workbook, establishmentId) => {
     ],
     rows: [],
   });
+};
 
-  const workers = await models.worker.workersAndTraining(establishmentId);
-  const convertedWorkers = workers.map((worker) => {
-    return {
-      name: worker.get('NameOrIdValue'),
-      trainingCount: worker.get('trainingCount'),
-      qualificationCount: worker.get('qualificationCount'),
-      expiredTrainingCount: worker.get('expiredTrainingCount'),
-      expiringTrainingCount: worker.get('expiringTrainingCount'),
-      missingMandatoryTrainingCount: worker.get('missingMandatoryTrainingCount'),
-    };
+const createExpiredTable = (tab, lineNumber) => {
+  return tab.addTable({
+    name: 'expiredTable',
+    ref: 'B' + lineNumber,
+    headerRow: true,
+    columns: [
+      { name: 'Expired', filterButton: false },
+      { name: 'Total', filterButton: false },
+      { name: 'Mandatory', filterButton: false },
+      { name: 'Non-mandatory', filterButton: false },
+    ],
+    rows: [],
   });
+};
 
-  let currentLineNumber = convertedWorkers.length + 9;
-  const missingTable = summaryTab.addTable({
+const createMissingTable = (tab, lineNumber) => {
+  return tab.addTable({
     name: 'missingTable',
-    ref: 'B' + currentLineNumber,
+    ref: 'B' + lineNumber,
     headerRow: true,
     columns: [
       { name: 'Missing', filterButton: false },
@@ -43,14 +73,17 @@ const generateSummaryTab = async (workbook, establishmentId) => {
     ],
     rows: [],
   });
+};
 
-  for (let worker of convertedWorkers) {
-    expiringSoonTable.addRow([worker.name, worker.expiringTrainingCount]);
-    missingTable.addRow([worker.name, worker.missingMandatoryTrainingCount]);
-  }
-
-  expiringSoonTable.commit();
-  missingTable.commit();
+const convertWorker = (worker) => {
+  return {
+    name: worker.get('NameOrIdValue'),
+    trainingCount: worker.get('trainingCount'),
+    qualificationCount: worker.get('qualificationCount'),
+    expiredTrainingCount: worker.get('expiredTrainingCount'),
+    expiringTrainingCount: worker.get('expiringTrainingCount'),
+    missingMandatoryTrainingCount: worker.get('missingMandatoryTrainingCount'),
+  };
 };
 
 module.exports.generateSummaryTab = generateSummaryTab;
