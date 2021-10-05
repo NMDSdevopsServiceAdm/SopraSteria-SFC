7df213fa26fbac4fc6cd67f08662c0b2be8dbcd0
Joanna Fawl <66678865+joannafawl@users.noreply.github.com>
2021-10-05 16:33:34 +0100

    Feat: Remove quals validation (#4090)
    * Remove custom validation on year achieved for qualifications
    
    * Update error messages
    
    * Add updateOn: 'submit' and static character count

--- src/app/features/workers/add-edit-qualification/qualification-form/qualification-form.component.ts
+++ src/app/features/workers/add-edit-qualification/qualification-form/qualification-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { INT_PATTERN } from '@core/constants/constants';
 import { ErrorDetails } from '@core/model/errorSummary.model';
 import { Establishment } from '@core/model/establishment.model';
@@ -7,7 +7,6 @@ import { Qualification, QualificationType } from '@core/model/qualification.mode
 import { Worker } from '@core/model/worker.model';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { WorkerService } from '@core/services/worker.service';
-import * as moment from 'moment';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -20,7 +19,6 @@ export class QualificationFormComponent implements OnInit, OnDestroy {
   @Input() form: FormGroup;
   @Input() type: { key: string; value: string };
   @Input() preselectedQualification: Qualification;
-  @Input() yearValidators: ValidatorFn[];
   @Input() notesMaxLength: number;
   @Input() submitted: boolean;
   @Input() formErrorsMap: Array<ErrorDetails>;
@@ -42,41 +40,16 @@ export class QualificationFormComponent implements OnInit, OnDestroy {
       this.workerService
         .getAvailableQualifcations(this.workplace.uid, this.worker.uid, this.type.value as QualificationType)
         .subscribe(
-          qualifications => {
+          (qualifications) => {
             if (qualifications) {
               this.qualifications = qualifications;
             }
           },
-          error => {
+          (error) => {
             console.error(error.error);
-          }
-        )
+          },
+        ),
     );
-
-    this.group.get('qualification').valueChanges.subscribe(value => {
-      const year = this.group.get('year');
-      const validators: ValidatorFn[] = [];
-      year.clearValidators();
-
-      if (value) {
-        const selectedQualification = this.qualifications.find(
-          qualification => qualification.id === parseInt(value, 10)
-        );
-
-        if (selectedQualification && selectedQualification.from) {
-          const from = moment(selectedQualification.from);
-          validators.push(Validators.min(from.year()));
-        }
-
-        if (selectedQualification && selectedQualification.until) {
-          const until = moment(selectedQualification.until);
-          validators.push(Validators.max(until.year()));
-        }
-      }
-
-      year.setValidators(this.yearValidators.concat(validators));
-      year.updateValueAndValidity();
-    });
   }
 
   public getFirstErrorMessage(item: string): string {
