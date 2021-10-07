a9935feb155a3e8c8d376197500077e8443ac278
duncanc19 <duncancarter93@gmail.com>
2021-10-07 12:25:09 +0100

    feat(trainingAndQualsSummaryTab): Add tests for expiring and expired tables

--- server/test/unit/routes/reports/trainingAndQualificationsReport/summaryTab.spec.js
+++ server/test/unit/routes/reports/trainingAndQualificationsReport/summaryTab.spec.js
@@ -29,9 +29,9 @@ describe('generateTrainingAndQualificationsReport', () => {
       expiredTrainingCount: 6,
       expiredMandatoryTrainingCount: 3,
       expiredNonMandatoryTrainingCount: 3,
-      expiringTrainingCount: 4,
-      expiringMandatoryTrainingCount: 2,
-      expiringNonMandatoryTrainingCount: 2,
+      expiringTrainingCount: 0,
+      expiringMandatoryTrainingCount: 0,
+      expiringNonMandatoryTrainingCount: 0,
       missingMandatoryTrainingCount: 0,
       mandatoryTrainingCount: 5,
     },
@@ -39,9 +39,9 @@ describe('generateTrainingAndQualificationsReport', () => {
       name: 'Andrew Test',
       trainingCount: 13,
       qualificationCount: 0,
-      expiredTrainingCount: 4,
-      expiredMandatoryTrainingCount: 1,
-      expiredNonMandatoryTrainingCount: 3,
+      expiredTrainingCount: 0,
+      expiredMandatoryTrainingCount: 0,
+      expiredNonMandatoryTrainingCount: 0,
       expiringTrainingCount: 2,
       expiringMandatoryTrainingCount: 0,
       expiringNonMandatoryTrainingCount: 2,
@@ -52,9 +52,9 @@ describe('generateTrainingAndQualificationsReport', () => {
       name: 'Daniel Craig',
       trainingCount: 6,
       qualificationCount: 0,
-      expiredTrainingCount: 0,
-      expiredMandatoryTrainingCount: 0,
-      expiredNonMandatoryTrainingCount: 0,
+      expiredTrainingCount: 4,
+      expiredMandatoryTrainingCount: 1,
+      expiredNonMandatoryTrainingCount: 3,
       expiringTrainingCount: 2,
       expiringMandatoryTrainingCount: 0,
       expiringNonMandatoryTrainingCount: 2,
@@ -123,6 +123,124 @@ describe('generateTrainingAndQualificationsReport', () => {
       expect(mockSummaryTab.getCell('C10').value).to.equal(mockTrainingRecordTotals.expired.total);
       expect(mockSummaryTab.getCell('D10').value).to.equal(mockTrainingRecordTotals.expired.mandatory);
       expect(mockSummaryTab.getCell('E10').value).to.equal(mockTrainingRecordTotals.expired.nonMandatory);
+    });
+  });
+
+  describe('Expiring soon table', () => {
+    it('should add Expiring soon table headings to row 13', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B13').value).to.equal('Expiring soon');
+      expect(mockSummaryTab.getCell('C13').value).to.equal('Total');
+      expect(mockSummaryTab.getCell('D13').value).to.equal('Mandatory');
+      expect(mockSummaryTab.getCell('E13').value).to.equal('Non-mandatory');
+    });
+
+    it('should add total row to Expiring soon table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B14').value).to.equal('Total');
+      expect(mockSummaryTab.getCell('C14').value).to.equal(mockTrainingRecordTotals.expiringSoon.total);
+      expect(mockSummaryTab.getCell('D14').value).to.equal(mockTrainingRecordTotals.expiringSoon.mandatory);
+      expect(mockSummaryTab.getCell('E14').value).to.equal(mockTrainingRecordTotals.expiringSoon.nonMandatory);
+    });
+
+    it('should add first worker with expiring training to Expiring soon table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B15').value).to.equal(mockWorkerTrainingBreakdowns[0].name);
+      expect(mockSummaryTab.getCell('C15').value).to.equal(mockWorkerTrainingBreakdowns[0].expiringTrainingCount);
+      expect(mockSummaryTab.getCell('D15').value).to.equal(
+        mockWorkerTrainingBreakdowns[0].expiringMandatoryTrainingCount,
+      );
+      expect(mockSummaryTab.getCell('E15').value).to.equal(
+        mockWorkerTrainingBreakdowns[0].expiringNonMandatoryTrainingCount,
+      );
+    });
+
+    it('should skip worker with no expiring training and add next worker to Expiring soon table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B16').value).to.equal(mockWorkerTrainingBreakdowns[2].name);
+      expect(mockSummaryTab.getCell('C16').value).to.equal(mockWorkerTrainingBreakdowns[2].expiringTrainingCount);
+      expect(mockSummaryTab.getCell('D16').value).to.equal(
+        mockWorkerTrainingBreakdowns[2].expiringMandatoryTrainingCount,
+      );
+      expect(mockSummaryTab.getCell('E16').value).to.equal(
+        mockWorkerTrainingBreakdowns[2].expiringNonMandatoryTrainingCount,
+      );
+    });
+
+    it('should add next worker to Expiring soon table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B17').value).to.equal(mockWorkerTrainingBreakdowns[3].name);
+      expect(mockSummaryTab.getCell('C17').value).to.equal(mockWorkerTrainingBreakdowns[3].expiringTrainingCount);
+      expect(mockSummaryTab.getCell('D17').value).to.equal(
+        mockWorkerTrainingBreakdowns[3].expiringMandatoryTrainingCount,
+      );
+      expect(mockSummaryTab.getCell('E17').value).to.equal(
+        mockWorkerTrainingBreakdowns[3].expiringNonMandatoryTrainingCount,
+      );
+    });
+  });
+
+  describe('Expired table', () => {
+    it('should add Expired table headings three rows below last row of Expiring soon table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B20').value).to.equal('Expired');
+      expect(mockSummaryTab.getCell('C20').value).to.equal('Total');
+      expect(mockSummaryTab.getCell('D20').value).to.equal('Mandatory');
+      expect(mockSummaryTab.getCell('E20').value).to.equal('Non-mandatory');
+    });
+
+    it('should add total row to Expired table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B21').value).to.equal('Total');
+      expect(mockSummaryTab.getCell('C21').value).to.equal(mockTrainingRecordTotals.expired.total);
+      expect(mockSummaryTab.getCell('D21').value).to.equal(mockTrainingRecordTotals.expired.mandatory);
+      expect(mockSummaryTab.getCell('E21').value).to.equal(mockTrainingRecordTotals.expired.nonMandatory);
+    });
+
+    it('should add first worker with expired training to Expired table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B22').value).to.equal(mockWorkerTrainingBreakdowns[0].name);
+      expect(mockSummaryTab.getCell('C22').value).to.equal(mockWorkerTrainingBreakdowns[0].expiredTrainingCount);
+      expect(mockSummaryTab.getCell('D22').value).to.equal(
+        mockWorkerTrainingBreakdowns[0].expiredMandatoryTrainingCount,
+      );
+      expect(mockSummaryTab.getCell('E22').value).to.equal(
+        mockWorkerTrainingBreakdowns[0].expiredNonMandatoryTrainingCount,
+      );
+    });
+
+    it('should add next worker to Expired table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B23').value).to.equal(mockWorkerTrainingBreakdowns[1].name);
+      expect(mockSummaryTab.getCell('C23').value).to.equal(mockWorkerTrainingBreakdowns[1].expiredTrainingCount);
+      expect(mockSummaryTab.getCell('D23').value).to.equal(
+        mockWorkerTrainingBreakdowns[1].expiredMandatoryTrainingCount,
+      );
+      expect(mockSummaryTab.getCell('E23').value).to.equal(
+        mockWorkerTrainingBreakdowns[1].expiredNonMandatoryTrainingCount,
+      );
+    });
+
+    it('should skip worker with no expired training and add next worker to Expired table', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockWorkerTrainingBreakdowns, mockTrainingRecordTotals);
+
+      expect(mockSummaryTab.getCell('B24').value).to.equal(mockWorkerTrainingBreakdowns[3].name);
+      expect(mockSummaryTab.getCell('C24').value).to.equal(mockWorkerTrainingBreakdowns[3].expiredTrainingCount);
+      expect(mockSummaryTab.getCell('D24').value).to.equal(
+        mockWorkerTrainingBreakdowns[3].expiredMandatoryTrainingCount,
+      );
+      expect(mockSummaryTab.getCell('E24').value).to.equal(
+        mockWorkerTrainingBreakdowns[3].expiredNonMandatoryTrainingCount,
+      );
     });
   });
 });
