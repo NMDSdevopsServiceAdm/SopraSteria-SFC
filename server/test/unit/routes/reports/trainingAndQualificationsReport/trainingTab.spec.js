a8c2db8ea92ff29fb050fa5403b62ac9a5537e1c
duncanc19 <duncancarter93@gmail.com>
2021-10-13 14:26:16 +0100

    feat(trainingAndQualsReportTrainingTab): Refactor test

--- server/test/unit/routes/reports/trainingAndQualificationsReport/trainingTab.spec.js
+++ server/test/unit/routes/reports/trainingAndQualificationsReport/trainingTab.spec.js
@@ -20,16 +20,18 @@ describe('addContentToTrainingTab', () => {
   it('should add table headings to row 6', async () => {
     addContentToTrainingTab(mockTrainingTab, mockWorkerTrainingRecords);
 
-    expect(mockTrainingTab.getCell('B6').value).to.equal('Worker ID');
-    expect(mockTrainingTab.getCell('C6').value).to.equal('Job role');
-    expect(mockTrainingTab.getCell('D6').value).to.equal('Training category');
-    expect(mockTrainingTab.getCell('E6').value).to.equal('Training name');
-    expect(mockTrainingTab.getCell('F6').value).to.equal('Mandatory');
-    expect(mockTrainingTab.getCell('G6').value).to.equal('Status');
-    expect(mockTrainingTab.getCell('H6').value).to.equal('Expiry date');
-    expect(mockTrainingTab.getCell('I6').value).to.equal('Date completed');
-    expect(mockTrainingTab.getCell('J6').value).to.equal('Long-term absence');
-    expect(mockTrainingTab.getCell('K6').value).to.equal('Accredited');
+    const expectedLine = 6;
+
+    expect(mockTrainingTab.getCell(`B${expectedLine}`).value).to.equal('Worker ID');
+    expect(mockTrainingTab.getCell(`C${expectedLine}`).value).to.equal('Job role');
+    expect(mockTrainingTab.getCell(`D${expectedLine}`).value).to.equal('Training category');
+    expect(mockTrainingTab.getCell(`E${expectedLine}`).value).to.equal('Training name');
+    expect(mockTrainingTab.getCell(`F${expectedLine}`).value).to.equal('Mandatory');
+    expect(mockTrainingTab.getCell(`G${expectedLine}`).value).to.equal('Status');
+    expect(mockTrainingTab.getCell(`H${expectedLine}`).value).to.equal('Expiry date');
+    expect(mockTrainingTab.getCell(`I${expectedLine}`).value).to.equal('Date completed');
+    expect(mockTrainingTab.getCell(`J${expectedLine}`).value).to.equal('Long-term absence');
+    expect(mockTrainingTab.getCell(`K${expectedLine}`).value).to.equal('Accredited');
   });
 
   it('should add first training record to top row of table', async () => {
