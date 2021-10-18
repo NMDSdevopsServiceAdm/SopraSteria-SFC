38606467d8aa1f32680178d418f8205622c4f029
duncanc19 <duncancarter93@gmail.com>
2021-10-18 14:10:23 +0100

    techDebt(removeCreateAccountFeatureFlag): Delete registration flow old regulated-by-cqc

--- src/app/features/registration/registration-routing.module.ts
+++ src/app/features/registration/registration-routing.module.ts
@@ -2,49 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterGuard } from '@core/guards/register/register.guard';
 import { PageResolver } from '@core/resolvers/page.resolver';
-import {
-  ConfirmAccountDetailsComponent,
-} from '@features/create-account/user/confirm-account-details/confirm-account-details.component';
-import {
-  SecurityQuestionComponent,
-} from '@features/create-account/user/create-security-question/create-security-question.component';
+import { ConfirmAccountDetailsComponent } from '@features/create-account/user/confirm-account-details/confirm-account-details.component';
+import { SecurityQuestionComponent } from '@features/create-account/user/create-security-question/create-security-question.component';
 import { UsernamePasswordComponent } from '@features/create-account/user/username-password/username-password.component';
 import { YourDetailsComponent } from '@features/create-account/user/your-details/your-details.component';
 import { ConfirmDetailsComponent } from '@features/create-account/workplace/confirm-details/confirm-details.component';
-import {
-  ConfirmWorkplaceDetailsComponent,
-} from '@features/create-account/workplace/confirm-workplace-details/confirm-workplace-details.component';
-import {
-  CouldNotFindWorkplaceAddressComponent,
-} from '@features/create-account/workplace/could-not-find-workplace-address/could-not-find-workplace-address.component';
-import {
-  FindWorkplaceAddressComponent,
-} from '@features/create-account/workplace/find-workplace-address/find-workplace-address.component';
+import { ConfirmWorkplaceDetailsComponent } from '@features/create-account/workplace/confirm-workplace-details/confirm-workplace-details.component';
+import { CouldNotFindWorkplaceAddressComponent } from '@features/create-account/workplace/could-not-find-workplace-address/could-not-find-workplace-address.component';
+import { FindWorkplaceAddressComponent } from '@features/create-account/workplace/find-workplace-address/find-workplace-address.component';
 import { NameOfWorkplaceComponent } from '@features/create-account/workplace/name-of-workplace/name-of-workplace.component';
-import {
-  NewSelectMainServiceComponent,
-} from '@features/create-account/workplace/new-select-main-service/new-select-main-service.component';
-import {
-  SelectWorkplaceAddressComponent,
-} from '@features/create-account/workplace/select-workplace-address/select-workplace-address.component';
+import { NewSelectMainServiceComponent } from '@features/create-account/workplace/new-select-main-service/new-select-main-service.component';
+import { SelectWorkplaceAddressComponent } from '@features/create-account/workplace/select-workplace-address/select-workplace-address.component';
 import { SelectWorkplaceComponent } from '@features/create-account/workplace/select-workplace/select-workplace.component';
 import { ThankYouComponent } from '@features/create-account/workplace/thank-you/thank-you.component';
-import {
-  WorkplaceNameAddressComponent,
-} from '@features/create-account/workplace/workplace-name-address/workplace-name-address.component';
+import { WorkplaceNameAddressComponent } from '@features/create-account/workplace/workplace-name-address/workplace-name-address.component';
 import { ChangeYourDetailsComponent } from '@features/registration/change-your-details/change-your-details.component';
 import { RegistrationCompleteComponent } from '@features/registration/registration-complete/registration-complete.component';
-import { RegulatedByCqcComponent } from '@features/registration/regulated-by-cqc/regulated-by-cqc.component';
 import { SelectMainServiceComponent } from '@features/registration/select-main-service/select-main-service.component';
 
 import { FindYourWorkplaceComponent } from '../create-account/workplace/find-your-workplace/find-your-workplace.component';
-import {
-  IsThisYourWorkplaceComponent,
-} from '../create-account/workplace/is-this-your-workplace/is-this-your-workplace.component';
+import { IsThisYourWorkplaceComponent } from '../create-account/workplace/is-this-your-workplace/is-this-your-workplace.component';
 import { NewRegulatedByCqcComponent } from '../create-account/workplace/new-regulated-by-cqc/new-regulated-by-cqc.component';
-import {
-  NewWorkplaceNotFoundComponent,
-} from '../create-account/workplace/new-workplace-not-found/new-workplace-not-found.component';
+import { NewWorkplaceNotFoundComponent } from '../create-account/workplace/new-workplace-not-found/new-workplace-not-found.component';
 import { AboutUsRegistrationComponent } from './about-us/about-us.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { WorkplaceNotFoundComponent } from './workplace-not-found/workplace-not-found.component';
@@ -65,11 +44,6 @@ const routes: Routes = [
     resolve: {
       pages: PageResolver,
     },
-  },
-  {
-    path: 'regulated-by-cqc',
-    component: RegulatedByCqcComponent,
-    data: { title: 'Regulated by CQC' },
   },
   {
     path: 'find-workplace',
