6d058f8cffdd43511f86185cd5eb4e355863ca4d
duncanc19 <duncancarter93@gmail.com>
2021-10-20 10:51:35 +0100

    fix(trainingAndQualsReportEmptyTables): Add addBlankRowIfTableEmpty to qualifications tab

--- server/test/unit/routes/reports/trainingAndQualificationsReport/qualificationsTab.spec.js
+++ server/test/unit/routes/reports/trainingAndQualificationsReport/qualificationsTab.spec.js
@@ -2,14 +2,18 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const excelJS = require('exceljs');
 
-const { addContentToQualificationsTab } = require('../../../../../routes/reports/trainingAndQualifications/qualificationsTab');
-const { mockWorkerQualificationRecords } = require('../../../mockdata/trainingAndQualifications')
+const {
+  addContentToQualificationsTab,
+} = require('../../../../../routes/reports/trainingAndQualifications/qualificationsTab');
+const { mockWorkerQualificationRecords } = require('../../../mockdata/trainingAndQualifications');
 
 describe('addContentToQualificationsTab', () => {
   let mockQualificationsTab;
 
   beforeEach(() => {
-    mockQualificationsTab = new excelJS.Workbook().addWorksheet('Qualifications', { views: [{ showGridLines: false }] });
+    mockQualificationsTab = new excelJS.Workbook().addWorksheet('Qualifications', {
+      views: [{ showGridLines: false }],
+    });
   });
 
   afterEach(() => {
@@ -17,13 +21,13 @@ describe('addContentToQualificationsTab', () => {
   });
 
   it('should add tab title to cell B2', async () => {
-    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords, []);
+    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords);
 
     expect(mockQualificationsTab.getCell('B2').value).to.equal('Qualifications');
   });
 
   it('should add table headings to row 6', async () => {
-    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords, []);
+    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords);
 
     const expectedLine = 6;
 
@@ -35,13 +39,28 @@ describe('addContentToQualificationsTab', () => {
     expect(mockQualificationsTab.getCell(`G${expectedLine}`).value).to.equal('Year achieved');
   });
 
+  it('should add empty row to table when no data', async () => {
+    addContentToQualificationsTab(mockQualificationsTab, []);
+
+    const expectedLine = 7;
+
+    expect(mockQualificationsTab.getCell(`B${expectedLine}`).value).to.equal('');
+    expect(mockQualificationsTab.getCell(`C${expectedLine}`).value).to.equal('');
+    expect(mockQualificationsTab.getCell(`D${expectedLine}`).value).to.equal('');
+    expect(mockQualificationsTab.getCell(`E${expectedLine}`).value).to.equal('');
+    expect(mockQualificationsTab.getCell(`F${expectedLine}`).value).to.equal('');
+    expect(mockQualificationsTab.getCell(`G${expectedLine}`).value).to.equal('');
+  });
+
   it('should add first training record to top row of table', async () => {
-    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords, []);
+    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords);
 
     const expectedLine = 7;
 
     expect(mockQualificationsTab.getCell(`B${expectedLine}`).value).to.equal('Helen Jones');
-    expect(mockQualificationsTab.getCell(`C${expectedLine}`).value).to.equal('Administrative / office staff not care-providing');
+    expect(mockQualificationsTab.getCell(`C${expectedLine}`).value).to.equal(
+      'Administrative / office staff not care-providing',
+    );
     expect(mockQualificationsTab.getCell(`D${expectedLine}`).value).to.equal('Degree');
     expect(mockQualificationsTab.getCell(`E${expectedLine}`).value).to.equal('Health and Social Care degree');
     expect(mockQualificationsTab.getCell(`F${expectedLine}`).value).to.equal('Level 6');
@@ -49,7 +68,7 @@ describe('addContentToQualificationsTab', () => {
   });
 
   it("should add worker's next training record to next row of table", async () => {
-    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords, []);
+    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords);
 
     const expectedLine = 8;
 
@@ -62,7 +81,7 @@ describe('addContentToQualificationsTab', () => {
   });
 
   it('should leave the cells blank where values are null', async () => {
-    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords, []);
+    addContentToQualificationsTab(mockQualificationsTab, mockWorkerQualificationRecords);
 
     const expectedLine = 9;
 
