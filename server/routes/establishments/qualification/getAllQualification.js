c4c002a317cdfa641ffa6a06731cdeafa8853a0b
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2021-10-22 12:28:35 +0100

    Created utils for qualification and add test Co-authored-by: duncanc19 <duncanc19@users.noreply.github.com>

--- server/routes/establishments/qualification/getAllQualification.js
+++ server/routes/establishments/qualification/getAllQualification.js
@@ -2,44 +2,27 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const Qualification = require('../../../models/classes/qualification').Qualification;
-
+const { sortQualificationsByGroup } = require('../../../utils/qualificationRecordsUtils');
 const { hasPermission } = require('../../../utils/security/hasPermission');
 
-
 const getAllQualification = async (req, res) => {
-    const establishmentId = req.establishmentId;
-    const workerUid = req.params.workerId;
+  const establishmentId = req.establishmentId;
+  const workerUid = req.params.workerId;
 
-    const qualificationRecords = [];
+  try {
+    const allQualificationRecords = await Qualification.fetch(establishmentId, workerUid);
+    const qualificationsSortedByGroup = sortQualificationsByGroup(
+      allQualificationRecords.qualifications,
+      allQualificationRecords.count,
+    );
+    return res.status(200).json(qualificationsSortedByGroup);
+  } catch (err) {
+    console.error('Qualification::root - failed', err);
+    return res.status(500).send(`Failed to get Qualification Records for Worker having uid: ${escape(workerUid)}`);
+  }
+};
 
-    try {
-      const allQualificationRecords = await Qualification.fetch(establishmentId, workerUid);
-    //   console.log(allQualificationRecords.qualifications);
-      // const groups = allQualificationRecords.qualifications.map(g => {
-      //     return {
-      //         id: g.qualification.id,
-      //         group: g.qualification.group
-      //     }
-      // });
-      const qualificationsSortedByGroup = allQualificationRecords.qualifications.reduce((r, a) =>{
-        r[a.qualification.group] = r[a.qualification.group] || [];
-        const qualification = {
-          title: a.qualification.title,
-          year: a.year,
-          notes: a.notes,
-          uid: a.uid,
-        };
-        r[a.qualification.group].push(qualification);
-        return r;
-        }, Object.create(null));
-      return res.status(200).json(allQualificationRecords);
-    } catch (err) {
-      console.error('Qualification::root - failed', err);
-      return res.status(500).send(`Failed to get Qualification Records for Worker having uid: ${escape(workerUid)}`);
-    }
-  };
+router.route('/').get(hasPermission('canEditWorker'), getAllQualification);
 
-  router.route('/').get(hasPermission('canEditWorker'), getAllQualification);
-
-  module.exports = router;
-  module.exports.getAllQualification = getAllQualification;
+module.exports = router;
+module.exports.getAllQualification = getAllQualification;
---
+++ server/utils/qualificationRecordsUtils.js
@@ -0,0 +1,25 @@
+exports.sortQualificationsByGroup = (qualifications, count) => {
+  return qualifications.reduce(
+    (sortedQualifications, record) => {
+      const qualification = {
+        title: record.qualification.title,
+        year: record.year,
+        notes: record.notes,
+        uid: record.uid,
+      };
+
+      const existingGroup = sortedQualifications.groups.find((obj) => obj.group === record.qualification.group);
+
+      if (existingGroup) {
+        existingGroup.records.push(qualification);
+      } else {
+        sortedQualifications.groups.push({
+          group: record.qualification.group,
+          records: [qualification],
+        });
+      }
+      return sortedQualifications;
+    },
+    { count, groups: [] },
+  );
+};
