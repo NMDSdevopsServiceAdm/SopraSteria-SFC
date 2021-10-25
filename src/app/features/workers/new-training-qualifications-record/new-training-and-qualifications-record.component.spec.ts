68010299ce9569f066a07e8e8b55d3ff970e2738
duncanc19 <duncancarter93@gmail.com>
2021-10-25 15:53:34 +0100

    feat(TrainingAndQualification): Add tests for router links to qualification pages

--- src/app/features/workers/new-training-qualifications-record/new-training-and-qualifications-record.component.spec.ts
+++ src/app/features/workers/new-training-qualifications-record/new-training-and-qualifications-record.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Establishment } from '@core/model/establishment.model';
@@ -92,7 +93,7 @@ describe('NewTrainingAndQualificationsRecordComponent', () => {
                                   year: '2020',
                                   notes: 'This is a test note for the first row in the Health group',
                                   title: 'Health qualification',
-                                  uid: 'someuid',
+                                  uid: 'firstHealthQualUid',
                                 },
                               ],
                             },
@@ -104,13 +105,13 @@ describe('NewTrainingAndQualificationsRecordComponent', () => {
                                   year: '2021',
                                   notes: 'Test notes needed',
                                   title: 'Cert qualification',
-                                  uid: 'someuid',
+                                  uid: 'firstCertificateUid',
                                 },
                                 {
                                   year: '2012',
                                   notes: 'These are some more notes in the second row of the cert table',
                                   title: 'Another name for qual',
-                                  uid: 'qualUid',
+                                  uid: 'secondCertificateUid',
                                 },
                               ],
                             },
@@ -295,6 +296,15 @@ describe('NewTrainingAndQualificationsRecordComponent', () => {
       expect(getByText('This is a test note for the first row in the Health group')).toBeTruthy();
     });
 
+    it('should contain link in qualification name in first Health table row', async () => {
+      const { fixture } = await setup();
+
+      const healthQualificationTitle = fixture.debugElement.query(
+        By.css('[data-testid="Title-firstHealthQualUid"]'),
+      ).nativeElement;
+      expect(healthQualificationTitle.getAttribute('href')).toBe('/qualification/firstHealthQualUid');
+    });
+
     it('should show Certificate table first row with details of record', async () => {
       const { getByText } = await setup();
 
@@ -303,12 +313,30 @@ describe('NewTrainingAndQualificationsRecordComponent', () => {
       expect(getByText('Test notes needed')).toBeTruthy();
     });
 
+    it('should contain link in qualification name in first certificate table row', async () => {
+      const { fixture } = await setup();
+
+      const healthQualificationTitle = fixture.debugElement.query(
+        By.css('[data-testid="Title-firstCertificateUid"]'),
+      ).nativeElement;
+      expect(healthQualificationTitle.getAttribute('href')).toBe('/qualification/firstCertificateUid');
+    });
+
     it('should show Certificate table second row with details of record', async () => {
       const { getByText } = await setup();
 
       expect(getByText('Another name for qual')).toBeTruthy();
       expect(getByText('2012')).toBeTruthy();
       expect(getByText('These are some more notes in the second row of the cert table')).toBeTruthy();
+    });
+
+    it('should contain link in qualification name in second certificate table row', async () => {
+      const { fixture } = await setup();
+
+      const healthQualificationTitle = fixture.debugElement.query(
+        By.css('[data-testid="Title-secondCertificateUid"]'),
+      ).nativeElement;
+      expect(healthQualificationTitle.getAttribute('href')).toBe('/qualification/secondCertificateUid');
     });
   });
 });
