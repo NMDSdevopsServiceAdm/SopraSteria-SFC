c810cf64ce61d413e4f692e9148f125790a409cc
joannafawl <joanna.fawl@madetech.com>
2021-10-27 11:51:58 +0100

    Add title

---
+++ src/app/features/benefits-bundle/benefits-bundle.component.spec.ts
@@ -0,0 +1,38 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '@shared/shared.module';
+import { getByTestId, queryByText, render } from '@testing-library/angular';
+
+import { BenefitsBundleComponent } from './benefits-bundle.component';
+
+describe('BenefitsBundleComponent', () => {
+  async function setup() {
+    const { fixture, getByText, getByTestId, getAllByText, queryByText } = await render(BenefitsBundleComponent, {
+      imports: [SharedModule, RouterTestingModule, HttpClientTestingModule],
+      providers: [],
+    });
+
+    const component = fixture.componentInstance;
+
+    return {
+      fixture,
+      component,
+      getAllByText,
+      queryByText,
+      getByText,
+      getByTestId,
+    };
+  }
+
+  it('should create BenefitsBundleComponent', async () => {
+    const { component } = await setup();
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the title', async () => {
+    const { getByText } = await setup();
+
+    const title = getByText('The ASC-WDS Benefits Bundle');
+    expect(title).toBeTruthy();
+  });
+});
