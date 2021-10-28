2d9ee21affe700e37b7c3ccadea90d1360af7567
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2021-10-28 09:37:08 +0100

    Add test for benefit bundle link

--- src/app/features/dashboard/home-tab/home-tab.component.spec.ts
+++ src/app/features/dashboard/home-tab/home-tab.component.spec.ts
@@ -235,13 +235,19 @@ describe('HomeTabComponent', () => {
     it('should navigate to `/benefits-bundle` when pressing the "Benefite Bundle and NEW link" button', async () => {
       const { component } = await setup();
       component.fixture.detectChanges();
-      const healthTrainingTitle = component.fixture.debugElement.query(
+
+      const benefiteBundleRoute = component.fixture.debugElement.query(
         By.css('[data-testid="benefitBundleLink"]'),
       ).nativeElement;
 
-      expect(healthTrainingTitle).toBe('/benefits-bundle');
-      // const benefitsBundleLink = component.getByTestId('benefitBundleLink');
-      // expect(benefitsBundleLink.getAttribute('href')).toBe('/benefits-bundle');
+      expect(benefiteBundleRoute.getAttribute('href')).toBe('/benefits-bundle');
+    });
+
+    it('should navigate to `/benefits-bundle` when pressing the "Benefite Bundle and NEW link" button', async () => {
+      const { component } = await setup();
+      component.fixture.detectChanges();
+
+      expect(component.queryByText('View the ASC-WDS Benefits Bundle')).toBeTruthy();
     });
   });
   describe('Other links', () => {
@@ -511,52 +517,3 @@ describe('HomeTabComponent', () => {
     });
   });
 });
-
-// describe('Long-Term Absence', () => {
-//   it('should display the Long-Term Absence if the worker is currently flagged as long term absent', async () => {
-//     const { component, fixture, getByText, queryByText } = await setup();
-
-//     component.worker.longTermAbsence = 'Illness';
-//     fixture.detectChanges();
-
-//     expect(getByText('Long-term absent')).toBeTruthy();
-//     expect(queryByText('Flag long-term absence')).toBeFalsy();
-//   });
-
-//   it('should navigate to `/long-term-absence` when pressing the "view" button', async () => {
-//     const { component, fixture, getByTestId, workplaceUid, workerUid } = await setup();
-
-//     component.worker.longTermAbsence = 'Illness';
-//     fixture.detectChanges();
-
-//     const longTermAbsenceLink = getByTestId('longTermAbsence');
-//     expect(longTermAbsenceLink.getAttribute('href')).toBe(
-//       `/workplace/${workplaceUid}/training-and-qualifications-record/${workerUid}/long-term-absence`,
-//     );
-//   });
-// });
-// //
-// describe('Flag long-term absence', () => {
-//   it('should display the "Flag long-term absence" link if the worker is not currently flagged as long term absent', async () => {
-//     const { component, fixture, getByText } = await setup();
-
-//     component.worker.longTermAbsence = null;
-//     component.canEditWorker = true;
-//     fixture.detectChanges();
-
-//     expect(getByText('Flag long-term absence')).toBeTruthy();
-//   });
-
-//   it('should navigate to `/long-term-absence` when pressing the "Flag long-term absence" button', async () => {
-//     const { component, fixture, getByTestId, workplaceUid, workerUid } = await setup();
-
-//     component.worker.longTermAbsence = null;
-//     component.canEditWorker = true;
-//     fixture.detectChanges();
-
-//     const flagLongTermAbsenceLink = getByTestId('flagLongTermAbsence');
-//     expect(flagLongTermAbsenceLink.getAttribute('href')).toBe(
-//       `/workplace/${workplaceUid}/training-and-qualifications-record/${workerUid}/long-term-absence`,
-//     );
-//   });
-// });
