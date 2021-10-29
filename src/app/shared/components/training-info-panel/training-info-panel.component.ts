05f53b4ed8fdd9c306ba7fc91b60542fee3fc0b3
Richard Pentecost <richard.pentecost@madetech.com>
2021-10-29 11:34:40 +0100

    add tests for training-info-panel and conditional rendering if there are records to be shown

--- src/app/shared/components/training-info-panel/training-info-panel.component.ts
+++ src/app/shared/components/training-info-panel/training-info-panel.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { Establishment } from '@core/model/establishment.model';
 
 @Component({
   selector: 'app-training-info-panel',
   templateUrl: './training-info-panel.component.html',
+  styleUrls: ['./training-info-panel.component.scss'],
 })
 export class TrainingInfoPanelComponent {
-  @Input() workplace: Establishment;
   @Input() totalExpiredTraining = 0;
   @Input() totalExpiringTraining = 0;
   @Input() totalMissingMandatoryTraining = 0;
