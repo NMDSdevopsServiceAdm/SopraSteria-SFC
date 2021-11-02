0e006c760e4a8fea33b3cc3beedeb54ca8fc0418
joannafawl <joanna.fawl@madetech.com>
2021-11-02 11:55:25 +0000

    Add tests for table content

--- server/test/unit/routes/reports/trainingAndQualificationsReport/parentReport/parentSummaryTab.spec.js
+++ server/test/unit/routes/reports/trainingAndQualificationsReport/parentReport/parentSummaryTab.spec.js
@@ -4,9 +4,9 @@ const excelJS = require('exceljs');
 
 const { addContentToSummaryTab } = require('../../../../../../routes/reports/trainingAndQualifications/parentReport/parentSummaryTab');
 const { getTrainingTotals } = require('../../../../../../utils/trainingAndQualificationsUtils');
-const { mockWorkerTrainingBreakdowns } = require('../../../../mockdata/trainingAndQualifications');
+const { mockWorkerTrainingBreakdowns, secondMockWorkerTrainingBreakdowns } = require('../../../../mockdata/trainingAndQualifications');
 
-describe('addContentToSummaryTab', () => {
+describe.only('addContentToSummaryTab', () => {
   let mockSummaryTab;
 
   const mockEstablishmentTrainingTotals = [
@@ -15,7 +15,13 @@ describe('addContentToSummaryTab', () => {
       totals: {
         ...getTrainingTotals(mockWorkerTrainingBreakdowns)
       }
-    }
+    },
+    {
+      establishmentId: 2321,
+      totals: {
+        ...getTrainingTotals(secondMockWorkerTrainingBreakdowns)
+      }
+    },
   ]
 
   beforeEach(() => {
@@ -40,6 +46,36 @@ describe('addContentToSummaryTab', () => {
       expect(mockSummaryTab.getCell('F6').value).to.equal('Expiring soon');
       expect(mockSummaryTab.getCell('I6').value).to.equal('Expired');
       expect(mockSummaryTab.getCell('L6').value).to.equal('Missing');
+    });
+
+    it('should add training totals for the first workplace to row 9', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockEstablishmentTrainingTotals);
+
+      expect(mockSummaryTab.getCell('C9').value).to.equal(15); // up to date - total
+      expect(mockSummaryTab.getCell('D9').value).to.equal(5); // up to date - mandatory
+      expect(mockSummaryTab.getCell('E9').value).to.equal(10); // up to date - non-mandatory
+      expect(mockSummaryTab.getCell('F9').value).to.equal(8); // expiring soon - total
+      expect(mockSummaryTab.getCell('G9').value).to.equal(2); // expiring soon - mandatory
+      expect(mockSummaryTab.getCell('H9').value).to.equal(6); // expiring soon - non-mandatory
+      expect(mockSummaryTab.getCell('I9').value).to.equal(12); // expired - total
+      expect(mockSummaryTab.getCell('J9').value).to.equal(4); // expired - mandatory
+      expect(mockSummaryTab.getCell('K9').value).to.equal(8); // expired - non-mandatory
+      expect(mockSummaryTab.getCell('L9').value).to.equal(5); // missing
+    });
+
+    it('should add training totals for the first workplace to row 10', async () => {
+      addContentToSummaryTab(mockSummaryTab, mockEstablishmentTrainingTotals);
+
+      expect(mockSummaryTab.getCell('C10').value).to.equal(13); // up to date - total
+      expect(mockSummaryTab.getCell('D10').value).to.equal(5); // up to date - mandatory
+      expect(mockSummaryTab.getCell('E10').value).to.equal(8); // up to date - non-mandatory
+      expect(mockSummaryTab.getCell('F10').value).to.equal(6); // expiring soon - total
+      expect(mockSummaryTab.getCell('G10').value).to.equal(2); // expiring soon - mandatory
+      expect(mockSummaryTab.getCell('H10').value).to.equal(4); // expiring soon - non-mandatory
+      expect(mockSummaryTab.getCell('I10').value).to.equal(4); // expired - total
+      expect(mockSummaryTab.getCell('J10').value).to.equal(0); // expired - mandatory
+      expect(mockSummaryTab.getCell('K10').value).to.equal(4); // expired - non-mandatory
+      expect(mockSummaryTab.getCell('L10').value).to.equal(2); // missing
     });
   });
 });
