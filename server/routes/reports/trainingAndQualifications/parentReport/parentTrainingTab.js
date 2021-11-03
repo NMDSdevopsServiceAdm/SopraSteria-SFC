a3a4726c0d2326541c8ed7cb16a67847e762ab75
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2021-11-03 16:19:45 +0000

    Add Table to parent training tab

--- server/routes/reports/trainingAndQualifications/parentReport/parentTrainingTab.js
+++ server/routes/reports/trainingAndQualifications/parentReport/parentTrainingTab.js
@@ -1,8 +1,56 @@
-
+const {
+  addHeading,
+  addLine,
+  backgroundColours,
+  textColours,
+  setTableHeadingsStyle,
+  fitColumnsToSize,
+} = require('../../../../utils/excelUtils');
+const models = require('../../../../models');
 const generateTrainingTab = async( workbook, establishmentId) =>{
 
     const trainingTab = workbook.addWorksheet('Training', { views: [{ showGridLines: false }] });
+    const rawWorkersWithTraining = await models.establishment.getEstablishmentTrainingRecords( [establishmentId],
+      true,);
+
+
+  addContentToTrainingTab(trainingTab);
 };
 
+const addContentToTrainingTab = (trainingTab) => {
+  addHeading(trainingTab, 'B2', 'E2', 'Training');
+  addLine(trainingTab, 'A4', 'L4');
+
+
+    const trainingTable = createTrainingTable(trainingTab);
+
+    fitColumnsToSize(trainingTab, 2, 5.5);
+  };
+
+
+const createTrainingTable = (trainingTab) => {
+    const tableColumns = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L'];
+    setTableHeadingsStyle(trainingTab, 6, backgroundColours.blue, textColours.white, tableColumns);
+
+    return trainingTab.addTable({
+      name: 'trainingTable',
+      ref: 'B6',
+      columns: [
+        { name: 'Workplace', filterButton: true },
+        { name: 'Worker ID', filterButton: true },
+        { name: 'Job role', filterButton: true },
+        { name: 'Training category', filterButton: true },
+        { name: 'Training name', filterButton: true },
+        { name: 'Mandatory', filterButton: true },
+        { name: 'Status', filterButton: true },
+        { name: 'Expiry date', filterButton: true },
+        { name: 'Date completed', filterButton: true },
+        { name: 'Long-term absence', filterButton: true },
+        { name: 'Accredited', filterButton: true },
+      ],
+      rows: [],
+    });
+  };
 
 module.exports.generateTrainingTab = generateTrainingTab;
+module.exports.addContentToTrainingTab = addContentToTrainingTab;
