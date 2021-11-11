979c0f97da3e7acb9ead5771dadb6062d0faa29e
duncanc19 <duncancarter93@gmail.com>
2021-11-11 15:58:39 +0000

    Remove message from care certificate tab in parent training report

--- server/test/unit/routes/reports/trainingAndQualificationsReport/careCertificateTab.spec.js
+++ server/test/unit/routes/reports/trainingAndQualificationsReport/careCertificateTab.spec.js
@@ -109,28 +109,18 @@ describe('generateCareCertificateTab', () => {
       expect(mockCareCertificateTab.getCell('B2').value).to.equal('Care Certificate');
     });
 
-    it('should add question to cell B6', async () => {
-      addContentToCareCertificateTab(
-        mockCareCertificateTab,
-        mockParentWorkplaceWorkersWithCareCerticateStatus,
-        isParent,
-      );
-      const question = 'Have they started or completed the Care Certificate?';
-      expect(mockCareCertificateTab.getCell('B6').value).to.equal(question);
-    });
-
     describe('Care Certificate table', () => {
-      it('should add care certificate records table headings to row 9', async () => {
+      it('should add care certificate records table headings to row 6', async () => {
         addContentToCareCertificateTab(
           mockCareCertificateTab,
           mockParentWorkplaceWorkersWithCareCerticateStatus,
           isParent,
         );
 
-        expect(mockCareCertificateTab.getCell('B9').value).to.equal('Workplace');
-        expect(mockCareCertificateTab.getCell('C9').value).to.equal('Worker ID');
-        expect(mockCareCertificateTab.getCell('D9').value).to.equal('Job role');
-        expect(mockCareCertificateTab.getCell('E9').value).to.equal('Status');
+        expect(mockCareCertificateTab.getCell('B6').value).to.equal('Workplace');
+        expect(mockCareCertificateTab.getCell('C6').value).to.equal('Worker ID');
+        expect(mockCareCertificateTab.getCell('D6').value).to.equal('Job role');
+        expect(mockCareCertificateTab.getCell('E6').value).to.equal('Status');
       });
 
       it('should add first worker with care certificate and workplace for first workplace to care certificate table', async () => {
@@ -140,12 +130,12 @@ describe('generateCareCertificateTab', () => {
           isParent,
         );
 
-        expect(mockCareCertificateTab.getCell('B10').value).to.equal(
+        expect(mockCareCertificateTab.getCell('B7').value).to.equal(
           mockParentWorkplaceWorkersWithCareCerticateStatus[0].establishmentName,
         );
-        expect(mockCareCertificateTab.getCell('C10').value).to.equal(mockWorkersWithCareCertificateStatus[0].workerId);
-        expect(mockCareCertificateTab.getCell('D10').value).to.equal(mockWorkersWithCareCertificateStatus[0].jobRole);
-        expect(mockCareCertificateTab.getCell('E10').value).to.equal(mockWorkersWithCareCertificateStatus[0].status);
+        expect(mockCareCertificateTab.getCell('C7').value).to.equal(mockWorkersWithCareCertificateStatus[0].workerId);
+        expect(mockCareCertificateTab.getCell('D7').value).to.equal(mockWorkersWithCareCertificateStatus[0].jobRole);
+        expect(mockCareCertificateTab.getCell('E7').value).to.equal(mockWorkersWithCareCertificateStatus[0].status);
       });
 
       it('should add second worker with care certificate and workplace for first workplace to care certificate table', async () => {
@@ -155,12 +145,12 @@ describe('generateCareCertificateTab', () => {
           isParent,
         );
 
-        expect(mockCareCertificateTab.getCell('B11').value).to.equal(
+        expect(mockCareCertificateTab.getCell('B8').value).to.equal(
           mockParentWorkplaceWorkersWithCareCerticateStatus[0].establishmentName,
         );
-        expect(mockCareCertificateTab.getCell('C11').value).to.equal(mockWorkersWithCareCertificateStatus[1].workerId);
-        expect(mockCareCertificateTab.getCell('D11').value).to.equal(mockWorkersWithCareCertificateStatus[1].jobRole);
-        expect(mockCareCertificateTab.getCell('E11').value).to.equal(mockWorkersWithCareCertificateStatus[1].status);
+        expect(mockCareCertificateTab.getCell('C8').value).to.equal(mockWorkersWithCareCertificateStatus[1].workerId);
+        expect(mockCareCertificateTab.getCell('D8').value).to.equal(mockWorkersWithCareCertificateStatus[1].jobRole);
+        expect(mockCareCertificateTab.getCell('E8').value).to.equal(mockWorkersWithCareCertificateStatus[1].status);
       });
 
       it('should add first worker with care certificate and workplace for second workplace to care certificate table', async () => {
@@ -170,16 +160,16 @@ describe('generateCareCertificateTab', () => {
           isParent,
         );
 
-        expect(mockCareCertificateTab.getCell('B12').value).to.equal(
+        expect(mockCareCertificateTab.getCell('B9').value).to.equal(
           mockParentWorkplaceWorkersWithCareCerticateStatus[1].establishmentName,
         );
-        expect(mockCareCertificateTab.getCell('C12').value).to.equal(
+        expect(mockCareCertificateTab.getCell('C9').value).to.equal(
           secondMockWorkersWithCareCertificateStatus[0].workerId,
         );
-        expect(mockCareCertificateTab.getCell('D12').value).to.equal(
+        expect(mockCareCertificateTab.getCell('D9').value).to.equal(
           secondMockWorkersWithCareCertificateStatus[0].jobRole,
         );
-        expect(mockCareCertificateTab.getCell('E12').value).to.equal(
+        expect(mockCareCertificateTab.getCell('E9').value).to.equal(
           secondMockWorkersWithCareCertificateStatus[0].status,
         );
       });
@@ -191,16 +181,16 @@ describe('generateCareCertificateTab', () => {
           isParent,
         );
 
-        expect(mockCareCertificateTab.getCell('B13').value).to.equal(
+        expect(mockCareCertificateTab.getCell('B10').value).to.equal(
           mockParentWorkplaceWorkersWithCareCerticateStatus[1].establishmentName,
         );
-        expect(mockCareCertificateTab.getCell('C13').value).to.equal(
+        expect(mockCareCertificateTab.getCell('C10').value).to.equal(
           secondMockWorkersWithCareCertificateStatus[1].workerId,
         );
-        expect(mockCareCertificateTab.getCell('D13').value).to.equal(
+        expect(mockCareCertificateTab.getCell('D10').value).to.equal(
           secondMockWorkersWithCareCertificateStatus[1].jobRole,
         );
-        expect(mockCareCertificateTab.getCell('E13').value).to.equal(
+        expect(mockCareCertificateTab.getCell('E10').value).to.equal(
           secondMockWorkersWithCareCertificateStatus[1].status,
         );
       });
@@ -213,7 +203,7 @@ describe('generateCareCertificateTab', () => {
             isParent,
           );
 
-          const expectedLine = 14;
+          const expectedLine = 11;
 
           expect(mockCareCertificateTab.getCell(`B${expectedLine}`).value).to.equal(null);
           expect(mockCareCertificateTab.getCell(`C${expectedLine}`).value).to.equal(null);
@@ -223,7 +213,7 @@ describe('generateCareCertificateTab', () => {
         it('should add empty row to table when no data', async () => {
           addContentToCareCertificateTab(mockCareCertificateTab, [], isParent);
 
-          const expectedLine = 10;
+          const expectedLine = 7;
 
           expect(mockCareCertificateTab.getCell(`B${expectedLine}`).value).to.equal('');
           expect(mockCareCertificateTab.getCell(`C${expectedLine}`).value).to.equal('');
