ca46e17a2c91a811f2325e6c3383e69b2fd34e92
joannafawl <joanna.fawl@madetech.com>
2021-11-25 15:59:56 +0000

    Create first login wizard component

---
+++ src/app/features/first-login-wizard/first-login-wizard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-login-wizard',
+  templateUrl: './first-login-wizard.component.html',
+})
+export class FirstLoginWizardComponent {
+  constructor() {}
+}
