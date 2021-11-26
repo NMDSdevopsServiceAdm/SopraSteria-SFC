a9a19b6e2cd560d4107c7a38543fde5cf10e1b10
Richard Pentecost <richard.pentecost@madetech.com>
2021-11-26 16:09:06 +0000

    add form to search-for-workplace component

--- src/app/features/admin/search/search-for-workplace/search-for-workplace.component.ts
+++ src/app/features/admin/search/search-for-workplace/search-for-workplace.component.ts
@@ -1,7 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search-for-workplace',
   templateUrl: './search-for-workplace.component.html',
 })
-export class SearchForWorkplaceComponent {}
+export class SearchForWorkplaceComponent implements OnInit {
+  @ViewChild('formEl') formEl: ElementRef;
+  public form: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.setupForm();
+  }
+
+  public setupForm(): void {
+    this.form = this.formBuilder.group({
+      workplaceName: '',
+      postcode: '',
+      workplaceId: '',
+      locationId: '',
+      providerId: '',
+    });
+  }
+
+  public onSubmit(): void {
+    console.log('onSubmit');
+  }
+}
