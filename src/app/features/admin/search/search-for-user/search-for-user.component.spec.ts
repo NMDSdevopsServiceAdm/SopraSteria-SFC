33137ea0672caee1303324d6d85bd264b6a173c2
duncanc19 <duncancarter93@gmail.com>
2021-11-26 12:01:15 +0000

    feat(adminSearchUsers): Set up search user form with stub submit

--- src/app/features/admin/search/search-for-user/search-for-user.component.spec.ts
+++ src/app/features/admin/search/search-for-user/search-for-user.component.spec.ts
@@ -1,24 +1,92 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 
 import { SearchForUserComponent } from './search-for-user.component';
 
 describe('SearchForUserComponent', () => {
   async function setup() {
-    const component = await render(SearchForUserComponent, {
-      imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
+    const { fixture, getByText, getByTestId } = await render(SearchForUserComponent, {
+      imports: [
+        SharedModule,
+        RouterModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
     });
+
+    const component = fixture.componentInstance;
 
     return {
       component,
+      getByText,
+      getByTestId,
     };
   }
 
   it('should render a SearchForUserComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should call searchUsers with fields set to null when clicking search button with nothing entered', async () => {
+    const { component, getByTestId } = await setup();
+
+    const searchUsersSpy = spyOn(component, 'searchUsers');
+
+    const searchButton = getByTestId('searchButton');
+    fireEvent.click(searchButton);
+
+    expect(searchUsersSpy).toHaveBeenCalledWith({
+      username: null,
+      name: null,
+      emailAddress: null,
+    });
+  });
+
+  it('should call searchUsers with entered username when clicking search button', async () => {
+    const { component, getByTestId } = await setup();
+
+    const searchUsersSpy = spyOn(component, 'searchUsers');
+    const form = component.form;
+
+    form.controls['username'].setValue('bob');
+    form.controls['username'].markAsDirty();
+
+    const searchButton = getByTestId('searchButton');
+    fireEvent.click(searchButton);
+
+    expect(searchUsersSpy).toHaveBeenCalledWith({
+      username: 'bob',
+      name: null,
+      emailAddress: null,
+    });
+  });
+
+  it('should call searchUsers with entered name and emailAddress when clicking search button', async () => {
+    const { component, getByTestId } = await setup();
+
+    const searchUsersSpy = spyOn(component, 'searchUsers');
+    const form = component.form;
+
+    form.controls['name'].setValue('Bob Monkhouse');
+    form.controls['name'].markAsDirty();
+
+    form.controls['emailAddress'].setValue('bob@email.com');
+    form.controls['emailAddress'].markAsDirty();
+
+    const searchButton = getByTestId('searchButton');
+    fireEvent.click(searchButton);
+
+    expect(searchUsersSpy).toHaveBeenCalledWith({
+      username: null,
+      name: 'Bob Monkhouse',
+      emailAddress: 'bob@email.com',
+    });
   });
 });
--- src/app/features/admin/search/search-for-user/search-for-user.component.ts
+++ src/app/features/admin/search/search-for-user/search-for-user.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search-for-user',
   templateUrl: './search-for-user.component.html',
 })
-export class SearchForUserComponent {}
+export class SearchForUserComponent implements OnInit {
+  public form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: null,
+      username: null,
+      emailAddress: null,
+    });
+  }
+
+  public searchUsers(data: UserSearchRequest): void {
+    console.log(data);
+  }
+
+  public onSubmit(): void {
+    const data = this.getRequestData();
+    this.searchUsers(data);
+  }
+
+  private getRequestData(): UserSearchRequest {
+    return {
+      username: this.form.controls.username.value,
+      name: this.form.controls.name.value,
+      emailAddress: this.form.controls.emailAddress.value,
+    };
+  }
+}
+
+interface UserSearchRequest {
+  username: string;
+  name: string;
+  emailAddress: string;
+}
