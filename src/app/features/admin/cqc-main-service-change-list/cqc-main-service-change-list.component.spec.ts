8f7a581c4c355c4d6f830daf604e42a630094437
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2021-12-06 16:39:36 +0000

    Add header for cqc main service change table and add test for them

--- src/app/features/admin/cqc-main-service-change-list/cqc-main-service-change-list.component.spec.ts
+++ src/app/features/admin/cqc-main-service-change-list/cqc-main-service-change-list.component.spec.ts
@@ -6,22 +6,39 @@ import { render } from '@testing-library/angular';
 
 import { CQCMainServiceChangeListComponent } from './cqc-main-service-change-list.component';
 
-describe('CQCMainServiceChangeListComponent', () => {
+fdescribe('CQCMainServiceChangeListComponent', () => {
   async function setup() {
-    const component = await render(CQCMainServiceChangeListComponent, {
+    const { fixture, getByText, getAllByText } = await render(CQCMainServiceChangeListComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
+      providers: [],
     });
 
-    const fixture = component.fixture;
+    const component = fixture.componentInstance;
 
     return {
-      component,
       fixture,
+      component,
+      getByText,
+      getAllByText,
     };
   }
 
   it('should render a CQCMainServiceChangeListComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should show `CQC main service change` header', async () => {
+    const { getByText } = await setup();
+
+    expect(getByText('CQC main service change')).toBeTruthy();
+  });
+
+  it('should show `CQC main service change` table headings', async () => {
+    const { getByText } = await setup();
+
+    expect(getByText('Workplace')).toBeTruthy();
+    expect(getByText('Received')).toBeTruthy();
+    expect(getByText('Status')).toBeTruthy();
   });
 });
