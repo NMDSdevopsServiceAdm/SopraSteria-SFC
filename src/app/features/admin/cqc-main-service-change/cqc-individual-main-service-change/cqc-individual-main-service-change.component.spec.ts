2cceabac69acc5042ea604ce4f3f91cac96bdebc
duncanc19 <duncancarter93@gmail.com>
2021-12-07 15:04:43 +0000

    feat(cqcMainServiceApprovalAndRejectionButtons): Add approval/rejection popups to new cqc main service change page

--- src/app/features/admin/cqc-main-service-change/cqc-individual-main-service-change/cqc-individual-main-service-change.component.spec.ts
+++ src/app/features/admin/cqc-main-service-change/cqc-individual-main-service-change/cqc-individual-main-service-change.component.spec.ts
@@ -2,23 +2,61 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { fireEvent, render, within } from '@testing-library/angular';
 
 import { CqcIndividualMainServiceChangeComponent } from './cqc-individual-main-service-change.component';
 
-describe('SearchComponent', () => {
+describe('CqcIndividualMainServiceChangeComponent', () => {
   async function setup() {
-    const component = await render(CqcIndividualMainServiceChangeComponent, {
+    const { fixture, getByText } = await render(CqcIndividualMainServiceChangeComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
     });
 
+    const component = fixture.componentInstance;
+
     return {
       component,
+      fixture,
+      getByText,
     };
   }
 
-  // it('should render a SearchComponent', async () => {
-  //   const { component } = await setup();
-  //   expect(component).toBeTruthy();
-  // });
+  it('should render a CqcIndividualMainServiceChangeComponent', async () => {
+    const { component } = await setup();
+    expect(component).toBeTruthy();
+  });
+
+  describe('Approvals', () => {
+    it('shows dialog with approval confirmation message when Approve button is clicked', async () => {
+      const { fixture, getByText } = await setup();
+
+      const approveButton = getByText('Approve');
+      const dialogMessage = `You're about to approve this CQC main service change`;
+
+      fireEvent.click(approveButton);
+      fixture.detectChanges();
+
+      const dialog = await within(document.body).findByRole('dialog');
+
+      expect(dialog).toBeTruthy();
+      expect(within(dialog).getByText(dialogMessage, { exact: false })).toBeTruthy();
+    });
+  });
+
+  describe('Rejections', () => {
+    it('shows dialog with rejection confirmation message when Reject button is clicked', async () => {
+      const { fixture, getByText } = await setup();
+
+      const rejectButton = getByText('Reject');
+      const dialogMessage = `You're about to reject this CQC main service change`;
+
+      fireEvent.click(rejectButton);
+      fixture.detectChanges();
+
+      const dialog = await within(document.body).findByRole('dialog');
+
+      expect(dialog).toBeTruthy();
+      expect(within(dialog).getByText(dialogMessage, { exact: false })).toBeTruthy();
+    });
+  });
 });
--- src/app/features/admin/cqc-main-service-change/cqc-individual-main-service-change/cqc-individual-main-service-change.component.ts
+++ src/app/features/admin/cqc-main-service-change/cqc-individual-main-service-change/cqc-individual-main-service-change.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
+import { Dialog, DialogService } from '@core/services/dialog.service';
+import { ApprovalOrRejectionDialogComponent } from '@features/admin/approval-or-rejection-dialog/approval-or-rejection-dialog.component';
 
 @Component({
   selector: 'app-cqc-individual-main-service-change',
   templateUrl: './cqc-individual-main-service-change.component.html',
 })
-export class CqcIndividualMainServiceChangeComponent {}
+export class CqcIndividualMainServiceChangeComponent {
+  constructor(private dialogService: DialogService) {}
+
+  public approveOrRejectCqcChange(isApproval: boolean): void {
+    const dialog = this.openApprovalOrRejectionDialog(isApproval);
+
+    dialog.afterClosed.subscribe((confirmed) => {
+      if (confirmed) {
+        console.log('confirmed');
+      }
+    });
+  }
+
+  private openApprovalOrRejectionDialog(isApproval: boolean): Dialog<ApprovalOrRejectionDialogComponent> {
+    return this.dialogService.open(ApprovalOrRejectionDialogComponent, {
+      workplaceName: 'Stub Workplace',
+      approvalName: 'CQC main service change',
+      approvalType: 'change',
+      isApproval,
+    });
+  }
+}
