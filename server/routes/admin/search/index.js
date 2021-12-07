341a2edb9eac63e3e93d712160991559cbdf3b58
Richard Pentecost <richard.pentecost@madetech.com>
2021-12-07 14:23:24 +0000

    change establishment search route exports to remove router from exports object and reconfigure tests and routing to accomodate changes

--- server/routes/admin/search/index.js
+++ server/routes/admin/search/index.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
-const establishments = require('./establishments');
+const { fileExports } = require('./establishments');
 const users = require('./users');
 const groups = require('./groups');
 
-router.use('/establishments', establishments.search);
+router.use('/establishments', fileExports.search);
 router.use('/users', users);
 router.use('/groups', groups);
 
