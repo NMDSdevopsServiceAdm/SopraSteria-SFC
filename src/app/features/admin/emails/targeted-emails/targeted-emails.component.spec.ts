b5ad2e20e7c24dad78e7aeff49c5f20de66c78e5
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2021-12-14 09:58:06 +0000

    Add test for targeted emails

--- src/app/features/admin/emails/targeted-emails/targeted-emails.component.spec.ts
+++ src/app/features/admin/emails/targeted-emails/targeted-emails.component.spec.ts
@@ -1,16 +1,20 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { EmailCampaignService } from '@core/services/admin/email-campaign.service';
 import { WindowRef } from '@core/services/window.ref';
+import { SearchModule } from '@features/search/search.module';
 import { SharedModule } from '@shared/shared.module';
 import { render } from '@testing-library/angular';
+import { of } from 'rxjs';
 
 import { TargetedEmailsComponent } from './targeted-emails.component';
 
 describe('EmailsComponent', () => {
   async function setup() {
     return render(TargetedEmailsComponent, {
-      imports: [SharedModule, HttpClientTestingModule, RouterTestingModule],
+      imports: [SharedModule, HttpClientTestingModule, RouterTestingModule, SearchModule],
       providers: [
         {
           provide: WindowRef,
@@ -32,4 +36,74 @@ describe('EmailsComponent', () => {
       ],
     });
   }
+
+  describe('Targeted emails', () => {
+    it('should display the total number of emails to be sent', async () => {
+      const component = await setup();
+
+      component.fixture.componentInstance.totalEmails = 1500;
+      component.fixture.detectChanges();
+
+      const totalEmail = component.getByTestId('totalEmails');
+      expect(totalEmail.innerHTML).toContain('1,500');
+    });
+
+    it("should display 0 emails to be sent when the group and template haven't been selected", async () => {
+      const component = await setup();
+
+      component.fixture.componentInstance.emailGroup = null;
+      component.fixture.componentInstance.selectedTemplateId = null;
+      component.fixture.detectChanges();
+
+      const numInactiveWorkplaces = component.getByTestId('totalEmails');
+      expect(numInactiveWorkplaces.innerHTML).toContain('0');
+    });
+
+    it("should disable the Send emails button when the group and template haven't been selected", async () => {
+      const component = await setup();
+
+      component.fixture.componentInstance.emailGroup = '';
+      component.fixture.componentInstance.selectedTemplateId = '';
+      component.fixture.detectChanges();
+
+      const sendEmailsButton = component.fixture.nativeElement.querySelectorAll('button');
+      expect(sendEmailsButton[0].disabled).toBeTruthy();
+    });
+
+    it('should update the total emails when updateTotalEmails() is called', async () => {
+      const component = await setup();
+      const emailCampaignService = TestBed.inject(EmailCampaignService);
+      const getTargetedTotalEmailsSpy = spyOn(emailCampaignService, 'getTargetedTotalEmails').and.callFake(() =>
+        of({ totalEmails: 1500 }),
+      );
+
+      component.fixture.componentInstance.updateTotalEmails('primaryUsers');
+      component.fixture.detectChanges();
+
+      expect(component.fixture.componentInstance.totalEmails).toEqual(1500);
+      expect(getTargetedTotalEmailsSpy).toHaveBeenCalled();
+    });
+
+    it('should display the template names as options', async () => {
+      const component = await setup();
+      const templates = [
+        {
+          id: 1,
+          name: 'Template 1',
+        },
+        {
+          id: 2,
+          name: 'Template 2',
+        },
+      ];
+
+      component.fixture.componentInstance.templates = templates;
+      component.fixture.detectChanges();
+
+      const templateDropdown = component.getByTestId('selectedTemplateId');
+
+      expect(templateDropdown.childNodes[1].textContent).toEqual('Template 1');
+      expect(templateDropdown.childNodes[2].textContent).toEqual('Template 2');
+    });
+  });
 });
