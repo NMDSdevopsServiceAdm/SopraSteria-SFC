62560d86e89fc45ae3a2a9bfbfcfac7bea3842d3
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2021-12-15 13:04:36 +0000

    Add refactoring to  the inactive workplaces component

--- src/app/features/admin/emails/inactive-emails/inactive-emails.component.ts
+++ src/app/features/admin/emails/inactive-emails/inactive-emails.component.ts
@@ -2,11 +2,9 @@ import { DecimalPipe } from '@angular/common';
 import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EmailType, TotalEmailsResponse } from '@core/model/emails.model';
 import { EmailCampaignService } from '@core/services/admin/email-campaign.service';
 import { AlertService } from '@core/services/alert.service';
 import { DialogService } from '@core/services/dialog.service';
-import { ReportService } from '@core/services/report.service';
 import { Subscription } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
@@ -19,15 +17,9 @@ import { SendEmailsConfirmationDialogComponent } from '../dialogs/send-emails-co
 })
 export class InactiveEmailsComponent {
   public inactiveWorkplaces = this.route.snapshot.data.inactiveWorkplaces.inactiveWorkplaces;
-  public totalEmails = 0;
-  public emailGroup = '';
-  public selectedTemplateId = '';
   public templates = this.route.snapshot.data.emailTemplates.templates;
   public history = this.route.snapshot.data.emailCampaignHistory;
-  public isAdmin: boolean;
-  public now: Date = new Date();
   private subscriptions: Subscription = new Subscription();
-  public emailType = EmailType;
 
   constructor(
     public alertService: AlertService,
@@ -35,26 +27,13 @@ export class InactiveEmailsComponent {
     private route: ActivatedRoute,
     private emailCampaignService: EmailCampaignService,
     private decimalPipe: DecimalPipe,
-    private reportsService: ReportService,
   ) {}
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }
 
-  public updateTotalEmails(groupType: string): void {
-    if (groupType) {
-      this.subscriptions.add(
-        this.emailCampaignService
-          .getTargetedTotalEmails(groupType)
-          .subscribe((totalEmails: TotalEmailsResponse) => (this.totalEmails = totalEmails.totalEmails)),
-      );
-    } else {
-      this.totalEmails = 0;
-    }
-  }
-
-  public confirmSendEmails(event: Event, emailCount: number, type: EmailType): void {
+  public confirmSendEmails(event: Event, emailCount: number): void {
     event.preventDefault();
 
     this.subscriptions.add(
@@ -62,18 +41,10 @@ export class InactiveEmailsComponent {
         .open(SendEmailsConfirmationDialogComponent, { emailCount })
         .afterClosed.subscribe((hasConfirmed) => {
           if (hasConfirmed) {
-            this.sendEmails(type);
+            this.sendInactiveEmails();
           }
         }),
     );
-  }
-
-  private sendEmails(type: EmailType): void {
-    switch (type) {
-      case EmailType.InactiveWorkplaces:
-        this.sendInactiveEmails();
-        break;
-    }
   }
 
   private sendInactiveEmails(): void {
