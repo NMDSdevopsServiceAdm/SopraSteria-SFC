0ab78bb156cd3b1970582b56a9e9007020d78fbd
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-01-11 12:15:45 +0000

    Add model and service to get the lastupdated for data change page

---
+++ src/app/core/services/data-change.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { DataChange } from '@core/model/data-change.model';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataChangeService {
+  private path = 'data-change';
+
+  constructor(private http: HttpClient) {}
+
+  public getDataChange(lastupdated: Date): Observable<DataChange> {
+    let params = new HttpParams();
+    const lastUpdate = {
+      last_updated: { _eq: lastupdated },
+    };
+    params = params.set('filter', JSON.stringify(lastUpdate));
+    params = params.set('limit', '1');
+    params = params.set('fields', 'content,title,last_updated');
+
+    return this.http.get<DataChange>(`${environment.cmsUri}/items/${this.path}`, { params });
+  }
+}
