350aff213ee72e8491423f82a5e7ad1f7696e9ef
Richard Pentecost <richard.pentecost@madetech.com>
2022-01-12 11:40:34 +0000

    add return type to bulk upload top tip list resolver and rename the bulk upload top tips service spec file

--- src/app/core/resolvers/bulk-upload/bulk-upload-top-tips-list.resolver.ts
+++ src/app/core/resolvers/bulk-upload/bulk-upload-top-tips-list.resolver.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
+import { BulkUploadTopTip } from '@core/model/bulk-upload-top-tips.model';
 import { BulkUploadTopTipsService } from '@core/services/bulk-upload/bulk-upload-top-tips.service';
 import { Observable, of } from 'rxjs';
 import { catchError, take } from 'rxjs/operators';
@@ -8,7 +9,7 @@ import { catchError, take } from 'rxjs/operators';
 export class BulkUploadTopTipsListResolver implements Resolve<any> {
   constructor(private bulkUploadTopTipsService: BulkUploadTopTipsService) {}
 
-  resolve(): Observable<null | any> {
+  resolve(): Observable<null | BulkUploadTopTip[]> {
     return this.bulkUploadTopTipsService.getTopTipsTitles().pipe(
       take(1),
       catchError(() => {
