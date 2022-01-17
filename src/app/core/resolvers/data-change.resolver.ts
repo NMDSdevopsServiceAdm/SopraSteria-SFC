051f35eb715ea38594b0eacc7366b47d51e0bd19
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-01-17 15:16:58 +0000

    Revert the resolver changes

--- src/app/core/resolvers/data-change.resolver.ts
+++ src/app/core/resolvers/data-change.resolver.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { DataChange } from '@core/model/data-change.model';
 import { DataChangeService } from '@core/services/data-change.service';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, take } from 'rxjs/operators';
 
 @Injectable()
 export class DataChangeResolver implements Resolve<any> {
-  constructor(private router: Router, private dataChangeService: DataChangeService) {}
+  constructor(private dataChangeService: DataChangeService) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<null | DataChange> {
     return this.dataChangeService.getDataChange().pipe(
+      take(1),
       catchError(() => {
-        this.router.navigate(['/problem-with-the-service']);
         return of(null);
       }),
     );
--- src/app/core/resolvers/data-changes-lastupdated.resolver.ts
+++ src/app/core/resolvers/data-changes-lastupdated.resolver.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Resolve, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { DataChangeService } from '@core/services/data-change.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, take } from 'rxjs/operators';
 
 @Injectable()
 export class DataChangeLastUpdatedResolver implements Resolve<any> {
-  constructor(
-    private dataChangeService: DataChangeService,
-    private establishmentService: EstablishmentService,
-    private router: Router,
-  ) {}
+  constructor(private dataChangeService: DataChangeService, private establishmentService: EstablishmentService) {}
 
-  resolve() {
+  resolve(route: ActivatedRouteSnapshot) {
     const establishmentId = this.establishmentService.establishmentId;
     return this.dataChangeService.getDataChangesLastUpdate(establishmentId).pipe(
+      take(1),
       catchError(() => {
-        this.router.navigate(['/problem-with-the-service']);
         return of(null);
       }),
     );
