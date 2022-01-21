53448d6975b20d0a1b054416cb1174b8c0425692
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-01-21 14:17:27 +0000

    Add validation for total staff input

--- src/app/shared/directives/create-workplace/add-total-staff/add-total-staff.directive.ts
+++ src/app/shared/directives/create-workplace/add-total-staff/add-total-staff.directive.ts
@@ -1,14 +1,14 @@
-import { Directive, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AfterViewInit, Directive, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ErrorDefinition } from '@core/model/errorSummary.model';
+import { ErrorDefinition, ErrorDetails } from '@core/model/errorSummary.model';
 import { URLStructure } from '@core/model/url.model';
 import { BackService } from '@core/services/back.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { RegistrationService } from '@core/services/registration.service';
 
 @Directive()
-export class AddTotalStaffDirective implements OnInit {
+export class AddTotalStaffDirective implements OnInit, AfterViewInit {
   @ViewChild('formEl') formEl: ElementRef;
 
   public flow: string;
@@ -16,6 +16,7 @@ export class AddTotalStaffDirective implements OnInit {
   public submitted = false;
   public form: FormGroup;
   public serverError: string;
+  public formErrorsMap: Array<ErrorDetails>;
   public returnToConfirmDetails: URLStructure;
 
   constructor(
@@ -31,17 +32,44 @@ export class AddTotalStaffDirective implements OnInit {
     this.flow = this.route.snapshot.parent.url[0].path;
     this.setupForm();
     this.setBackLink();
+    this.setupFormErrorsMap();
+  }
+
+  public ngAfterViewInit(): void {
+    this.errorSummaryService.formEl$.next(this.formEl);
   }
 
   private setupForm(): void {
     this.form = this.formBuilder.group({
-      numberofstaff: '12',
+      totalStaff: [null, [Validators.required]],
     });
   }
 
   public onSubmit(): void {
     this.submitted = true;
-    this.navigateToNextRoute();
+    if (this.form.valid) {
+      this.navigateToNextRoute();
+    }
+
+    console.log(this.form);
+  }
+  private setupFormErrorsMap(): void {
+    this.formErrorsMap = [
+      {
+        item: 'totalStaff',
+        type: [
+          {
+            name: 'required',
+            message: `Enter total staff number`,
+          },
+        ],
+      },
+    ];
+  }
+
+  public getErrorMessage(item: string): string {
+    const errorType = Object.keys(this.form.get(item).errors)[0];
+    return this.errorSummaryService.getFormErrorMessage(item, errorType, this.formErrorsMap);
   }
 
   private navigateToNextRoute(): void {
