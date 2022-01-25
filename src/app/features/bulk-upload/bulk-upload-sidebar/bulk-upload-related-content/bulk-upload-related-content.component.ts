fe1e5e49a7e97660fc6086a66a950a0230b3eae7
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-01-25 16:27:56 +0000

    change the function  for data changes flag

--- src/app/features/bulk-upload/bulk-upload-sidebar/bulk-upload-related-content/bulk-upload-related-content.component.ts
+++ src/app/features/bulk-upload/bulk-upload-sidebar/bulk-upload-related-content/bulk-upload-related-content.component.ts
@@ -20,11 +20,14 @@ export class BulkUploadRelatedContentComponent {
   constructor(public authService: AuthService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.datachange = new Date(this.route.snapshot.data.dataChange.data.last_updated);
-    this.dataChangeLastUpdated = new Date(this.route.snapshot.data.dataChangeLastUpdated.dataChangesLastUpdate);
+    this.datachange = this.route.snapshot.data.dataChange.data.last_updated;
+    this.dataChangeLastUpdated = this.route.snapshot.data.dataChangeLastUpdated.dataChangesLastUpdate;
     this.getShowFlagForBUDataChanges();
   }
   public getShowFlagForBUDataChanges() {
-    this.showFlagBUChanges = +this.dataChangeLastUpdated !== +this.datachange;
+    const exsitedDate = new Date(this.dataChangeLastUpdated);
+    const newDate = new Date(this.datachange);
+
+    this.showFlagBUChanges = this.dataChangeLastUpdated == null && +exsitedDate !== +newDate;
   }
 }
