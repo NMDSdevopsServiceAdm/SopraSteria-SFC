ee1d17e1802c9eb4aac5902abfd3d52945736529
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-02-03 14:39:10 +0000

    Add header, paragraph , reveal and checkboxes for grant letter component

--- src/app/features/wdf/wdf-claims/wdf-grant-letter/wdf-grant-letter.component.ts
+++ src/app/features/wdf/wdf-claims/wdf-grant-letter/wdf-grant-letter.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Establishment } from '@core/model/establishment.model';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-wdf-grant-letter',
   templateUrl: './wdf-grant-letter.component.html',
 })
 export class WdfGrantLetterComponent implements OnInit {
-  constructor() {}
+  public workplace: Establishment;
+  public workplaceUid: string;
+  public primaryWorkplaceUid: string;
+  public form: FormGroup;
+  public submitted = false;
+  public revealTitle = "What's a grant letter?";
+
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    protected formBuilder: FormBuilder,
+    private establishmentService: EstablishmentService,
+    private permissionsService: PermissionsService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {}
+  public onSubmit(): void {
+    this.submitted = true;
+  }
 }
