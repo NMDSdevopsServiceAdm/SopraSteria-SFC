4c8086304e99b52ef8a6912552df559a52d23966
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-02-04 10:35:19 +0000

    Add the text boxes for radio button

--- src/app/features/wdf/wdf-claims/wdf-grant-letter/wdf-grant-letter.component.ts
+++ src/app/features/wdf/wdf-claims/wdf-grant-letter/wdf-grant-letter.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ErrorDetails } from '@core/model/errorSummary.model';
 import { Establishment } from '@core/model/establishment.model';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
@@ -11,11 +12,14 @@ import { Subscription } from 'rxjs';
   templateUrl: './wdf-grant-letter.component.html',
 })
 export class WdfGrantLetterComponent implements OnInit {
+  public options = ['Myself', 'Somebody else'];
+
   public workplace: Establishment;
   public workplaceUid: string;
   public primaryWorkplaceUid: string;
   public form: FormGroup;
   public submitted = false;
+  public formErrorsMap: Array<ErrorDetails> = [];
   public revealTitle = "What's a grant letter?";
 
   private subscriptions: Subscription = new Subscription();
@@ -25,10 +29,38 @@ export class WdfGrantLetterComponent implements OnInit {
     private establishmentService: EstablishmentService,
     private permissionsService: PermissionsService,
     private route: ActivatedRoute,
-  ) {}
+  ) {
+    this.form = this.formBuilder.group({
+      grantLetter: [null],
+      Somebodyelse: [],
+    });
+  }
 
   ngOnInit(): void {}
   public onSubmit(): void {
     this.submitted = true;
+  }
+
+  protected setupFormErrorsMap(): void {
+    this.formErrorsMap = [
+      {
+        item: 'employerType',
+        type: [
+          {
+            name: 'required',
+            message: 'Please select an Employer type',
+          },
+        ],
+      },
+      {
+        item: 'other',
+        type: [
+          {
+            name: 'maxlength',
+            message: `Other Employer type must be characters or less`,
+          },
+        ],
+      },
+    ];
   }
 }
