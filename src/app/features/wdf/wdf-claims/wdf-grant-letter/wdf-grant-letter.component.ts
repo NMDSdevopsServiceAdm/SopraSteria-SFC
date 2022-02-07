93f8da8e3447657e4bf5d4021ccbfd909a9afcd5
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-02-07 16:22:57 +0000

    Add validation for somebodyelse input

--- src/app/features/wdf/wdf-claims/wdf-grant-letter/wdf-grant-letter.component.ts
+++ src/app/features/wdf/wdf-claims/wdf-grant-letter/wdf-grant-letter.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { EMAIL_PATTERN } from '@core/constants/constants';
 import { ErrorDetails } from '@core/model/errorSummary.model';
 import { Establishment } from '@core/model/establishment.model';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
@@ -34,14 +35,9 @@ export class WdfGrantLetterComponent implements OnInit, OnDestroy {
     public grantLetterService: GrantLetterService,
     public establishmentService: EstablishmentService,
   ) {
-    this.form = this.formBuilder.group(
-      {
-        grantLetter: [null, Validators.required],
-        fullName: [null],
-        emailAddress: [null],
-      },
-      { Validators: this.postcodeValidator },
-    );
+    this.form = this.formBuilder.group({
+      grantLetter: [null, Validators.required],
+    });
   }
 
   ngOnInit(): void {
@@ -49,7 +45,45 @@ export class WdfGrantLetterComponent implements OnInit, OnDestroy {
     this.establishmentId = this.establishmentService.primaryWorkplace.uid;
     this.flow = 'wdf-claims';
   }
+  public addControl(answer) {
+    if (answer === 'Somebody else') {
+      this.form.addControl(
+        'fullName',
+        new FormControl(null, { validators: [Validators.required, Validators.maxLength(120)], updateOn: 'submit' }),
+      );
+      this.form.addControl(
+        'emailAddress',
+        new FormControl(null, {
+          validators: [Validators.required, Validators.pattern(EMAIL_PATTERN), Validators.maxLength(120)],
+          updateOn: 'submit',
+        }),
+      );
 
+      this.formErrorsMap.push(
+        {
+          item: 'fullName',
+          type: [
+            {
+              name: 'required',
+              message: 'Please enter full name',
+            },
+          ],
+        },
+        {
+          item: 'emailAddress',
+          type: [
+            {
+              name: 'required',
+              message: 'Please enter email address',
+            },
+          ],
+        },
+      );
+    } else if (answer === 'Myself') {
+      this.form.controls['fullName'].disable();
+      this.form.controls['emailAddress'].disable();
+    }
+  }
   public onSubmit(): void {
     this.submitted = true;
 
@@ -74,24 +108,6 @@ export class WdfGrantLetterComponent implements OnInit, OnDestroy {
           },
         ],
       },
-      {
-        item: 'fullName',
-        type: [
-          {
-            name: 'required',
-            message: 'Please enter full name',
-          },
-        ],
-      },
-      {
-        item: 'emailAddress',
-        type: [
-          {
-            name: 'required',
-            message: 'Please enter email address',
-          },
-        ],
-      },
     ];
   }
   public getErrorMessage(item: string): string {
@@ -102,6 +118,7 @@ export class WdfGrantLetterComponent implements OnInit, OnDestroy {
     const grantletter = this.form.value.grantLetter;
     return grantletter ? 'Myself' : null;
   }
+
   public navigateToNextPage(): void {
     this.router.navigate([this.flow, 'grant-letter-sent']);
   }
