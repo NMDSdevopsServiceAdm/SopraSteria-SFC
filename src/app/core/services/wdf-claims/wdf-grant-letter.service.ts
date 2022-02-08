5fa51eed6975d3454848ae931b68c397aa45760b
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-02-08 13:28:29 +0000

    Add model to the end point

--- src/app/core/services/wdf-claims/wdf-grant-letter.service.ts
+++ src/app/core/services/wdf-claims/wdf-grant-letter.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GrantLetter } from '@core/model/wdf-claims/wdf-grant-letter.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +9,7 @@ import { Injectable } from '@angular/core';
 export class GrantLetterService {
   constructor(private http: HttpClient) {}
 
-  public sendEmailGrantLetter(establishmentId: string, grantLetter: string) {
-    return this.http.post<any>(`/api/establishment/${establishmentId}/wdfClaims/grantLetter`, grantLetter);
+  public sendEmailGrantLetter(establishmentId: string, grantLetter: string): Observable<GrantLetter> {
+    return this.http.post<GrantLetter>(`/api/establishment/${establishmentId}/wdfClaims/grantLetter`, grantLetter);
   }
 }
