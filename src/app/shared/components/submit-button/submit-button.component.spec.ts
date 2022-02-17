30d6dfd96351736ab4338f4594c071f4082802e8
Matthew Daley <matthew.daley@Matthews-MacBook-Pro.local>
2022-02-17 14:05:01 +0000

    adds dynamic text for summary text on submit button with fallback

--- src/app/shared/components/submit-button/submit-button.component.spec.ts
+++ src/app/shared/components/submit-button/submit-button.component.spec.ts
@@ -1,15 +1,28 @@
 import { RouterTestingModule } from '@angular/router/testing';
 import { render, fireEvent, screen } from '@testing-library/angular';
 
+import { Router } from '@angular/router';
 import { SubmitButtonComponent } from './submit-button.component';
 
-describe('SubmitButtonComponent', () => {
+fdescribe('SubmitButtonComponent', () => {
+  let url = '';
+
+  beforeEach(() => {
+    url = '';
+  });
+
   const setup = async () =>
     render(SubmitButtonComponent, {
       imports: [RouterTestingModule],
       componentProperties: {
         exitText: 'Exit',
       },
+      providers: [
+        {
+          provide: Router,
+          useValue: { url },
+        },
+      ],
     });
 
   it('should create', async () => {
@@ -25,5 +38,18 @@ describe('SubmitButtonComponent', () => {
     // update directive
     rerender({ exitText: 'Cancel FooBar' });
     expect(screen.getByText('Cancel FooBar')).toBeTruthy();
+  });
+
+  it('should render the default summary text based', async () => {
+    await setup();
+
+    expect(screen.getByText('View record summary')).toBeTruthy();
+  });
+
+  it('should render the correct summary text based if "staff-record" route', async () => {
+    url = '/staff-record';
+    await setup();
+
+    expect(screen.getByText('View this staff record')).toBeTruthy();
   });
 });
--- src/app/shared/components/submit-button/submit-button.component.ts
+++ src/app/shared/components/submit-button/submit-button.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-submit-button',
   templateUrl: './submit-button.component.html',
 })
-export class SubmitButtonComponent {
+export class SubmitButtonComponent implements OnInit {
   @Input() return: boolean;
   @Input() saveCallback: any;
   @Input() callToAction = 'Save and continue';
@@ -12,6 +13,14 @@ export class SubmitButtonComponent {
   @Input() canExit = true;
   @Input() exitText = 'Exit';
   @Output() clicked = new EventEmitter<{ action: string; save: boolean }>();
+
+  isStaffRecord = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    if (this.router.url.includes('staff-record')) this.isStaffRecord = true;
+  }
 
   onClick(event: Event, action: string, save: boolean): void {
     event.preventDefault();
