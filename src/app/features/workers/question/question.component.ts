2d7bc390bf60d1aa974c0c2f079803ae9a3eac98
Richard Pentecost <richard.pentecost@madetech.com>
2022-02-21 14:10:36 +0000

    add tests to the other qualifications component

--- src/app/features/workers/question/question.component.ts
+++ src/app/features/workers/question/question.component.ts
@@ -40,7 +40,7 @@ export class QuestionComponent implements OnInit, OnDestroy, AfterViewInit {
     protected workerService: WorkerService,
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.return = this.workerService.returnTo;
     this.workplace = this.route.parent.snapshot.data.establishment;
     this.primaryWorkplace = this.route.parent.snapshot.data.primaryWorkplace;
