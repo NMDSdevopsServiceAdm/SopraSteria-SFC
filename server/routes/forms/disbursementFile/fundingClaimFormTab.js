0c64afba9a7ab0c3fe2dd8afedf11bdecba56b8d
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-02-21 17:34:07 +0000

    Add image to the ecxell file with hyperlink

--- server/routes/forms/disbursementFile/fundingClaimFormTab.js
+++ server/routes/forms/disbursementFile/fundingClaimFormTab.js
@@ -1,15 +1,32 @@
 const { addHeadingBlack, addSmallHeadingBlack, addLine } = require('../../../utils/excelUtils');
+const path = require('path');
 
 const generateFundingClaimFormTab = (workbook) => {
   const fundingClaimFormTab = workbook.addWorksheet('Funding Claim Form', { views: [{ showGridLines: false }] });
-  addContentToFundingClaimFormTab(fundingClaimFormTab);
+  addContentToFundingClaimFormTab(workbook, fundingClaimFormTab);
 };
-const addContentToFundingClaimFormTab = (fundingClaimFormTab) => {
+const addContentToFundingClaimFormTab = (workbook, fundingClaimFormTab) => {
   addHeadingBlack(fundingClaimFormTab, 'A1', 'C1', 'FUNDING CLAIM FORM');
   addSmallHeadingBlack(fundingClaimFormTab, 'A3', 'C3', 'Grant Holder Name:');
   addLine(fundingClaimFormTab, 'A5', 'H5');
   addSmallHeadingBlack(fundingClaimFormTab, 'A6', 'C6', 'Grant Number:');
   addLine(fundingClaimFormTab, 'A8', 'H8');
+  addSFCLogo(workbook, fundingClaimFormTab);
 };
 
+const addSFCLogo = (workbook, tab) => {
+  const dir = path.join(__dirname, '/logo.png');
+  const logo = workbook.addImage({
+    filename: dir,
+    extension: 'png',
+  });
+  tab.addImage(logo, {
+    tl: { col: 15, row: 1 },
+    ext: { width: 200, height: 100 },
+    hyperlinks: {
+      hyperlink: 'https://www.skillsforcare.org.uk/wdfleadpartners',
+      tooltip: 'https://www.skillsforcare.org.uk/wdfleadpartners',
+    },
+  });
+};
 module.exports.generateFundingClaimFormTab = generateFundingClaimFormTab;
