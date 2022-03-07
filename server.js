e91091c90d78045ee0e92bbc849bc7eb706b9c45
Matthew Daley <matthew.daley@Matthews-MacBook-Pro.local>
2022-03-07 09:43:56 +0000

    adds next parameter to ensure that express treats as an error handler

--- server.js
+++ server.js
@@ -290,7 +290,7 @@ app.get('*', function (req, res) {
 
 app.use(Sentry.Handlers.errorHandler());
 // Optional fallthrough error handler
-app.use(function onError(err, req, res) {
+app.use(function onError(err, req, res, next) {
   // The error id is attached to `res.sentry` to be returned
   // and optionally displayed to the user for support.
   res.statusCode = 500;
@@ -298,7 +298,7 @@ app.use(function onError(err, req, res) {
 });
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -308,7 +308,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -319,7 +319,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
