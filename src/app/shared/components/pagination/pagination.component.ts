45fc041c1fa4180a0f8eb4296893d174dfed45a6
popey2700 <alexanderpope27@outlook.com>
2022-03-11 12:58:54 +0000

    feat(staffRecordsPagination) Created pagination styling

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent implements OnInit {
   public pages: Array<number>;
