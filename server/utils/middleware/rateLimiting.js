ca0d2db9a8c6770973d2939e8393111d1521eb8d
Richard Pentecost <richard.pentecost@madetech.com>
2022-03-15 12:16:28 +0000

    conditionally create RedisStore depending on env variable

--- server/utils/middleware/rateLimiting.js
+++ server/utils/middleware/rateLimiting.js
@@ -2,13 +2,13 @@ const config = require('../../config/config');
 const RateLimit = require('express-rate-limit');
 const RedisStore = require('rate-limit-redis');
 
-const store = new RedisStore({
-  redisURL: config.get('redis.uri'),
-});
+const store =
+  process.env.NODE_ENV !== 'example'
+    ? new RedisStore({
+        redisURL: config.get('redis.uri'),
+      })
+    : {};
 
-console.log('****************');
-console.log(process.env.NODE_ENV);
-console.log(config.get('redis.uri'));
 const rateLimiterConfig = {
   store,
   delayMs: 0, // disable delaying - full speed until the max limit is reached
@@ -27,15 +27,7 @@ const dbLimiter = new RateLimit({
   prefix: 'db:',
 });
 
-module.exports.authLimiter =
-  process.env.NODE_ENV !== 'example'
-    ? authLimiter
-    : () => {
-        return;
-      };
-module.exports.dbLimiter =
-  process.env.NODE_ENV !== 'example'
-    ? dbLimiter
-    : () => {
-        return;
-      };
+module.exports.authLimiter = authLimiter;
+module.exports.dbLimiter = dbLimiter;
+// module.exports.authLimiter = process.env.NODE_ENV !== 'example' ? authLimiter : () => {};
+// module.exports.dbLimiter = process.env.NODE_ENV !== 'example' ? dbLimiter : () => {};
