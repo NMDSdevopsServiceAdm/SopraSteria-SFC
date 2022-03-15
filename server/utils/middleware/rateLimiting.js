c9395c1e48c80f8d5dfd1df779a6436856c39722
Richard Pentecost <richard.pentecost@madetech.com>
2022-03-15 12:25:49 +0000

    authLimiter and dbLimiter return empty object if NODE_ENV is example

--- server/utils/middleware/rateLimiting.js
+++ server/utils/middleware/rateLimiting.js
@@ -2,12 +2,9 @@ const config = require('../../config/config');
 const RateLimit = require('express-rate-limit');
 const RedisStore = require('rate-limit-redis');
 
-const store =
-  process.env.NODE_ENV !== 'example'
-    ? new RedisStore({
-        redisURL: config.get('redis.uri'),
-      })
-    : {};
+const store = new RedisStore({
+  redisURL: config.get('redis.uri'),
+});
 
 const rateLimiterConfig = {
   store,
@@ -15,19 +12,26 @@ const rateLimiterConfig = {
   passIfNotConnected: true,
 };
 
-const authLimiter = new RateLimit({
-  ...rateLimiterConfig,
-  max: 100,
-  prefix: 'auth:',
-});
+const authLimiter =
+  process.env.NODE_ENVE !== 'example'
+    ? new RateLimit({
+        ...rateLimiterConfig,
+        max: 100,
+        prefix: 'auth:',
+      })
+    : {};
 
-const dbLimiter = new RateLimit({
-  ...rateLimiterConfig,
-  max: 200,
-  prefix: 'db:',
-});
+const dbLimiter =
+  process.env.NODE_ENVE !== 'example'
+    ? new RateLimit({
+        ...rateLimiterConfig,
+        max: 200,
+        prefix: 'db:',
+      })
+    : {};
 
 module.exports.authLimiter = authLimiter;
 module.exports.dbLimiter = dbLimiter;
+
 // module.exports.authLimiter = process.env.NODE_ENV !== 'example' ? authLimiter : () => {};
 // module.exports.dbLimiter = process.env.NODE_ENV !== 'example' ? dbLimiter : () => {};
