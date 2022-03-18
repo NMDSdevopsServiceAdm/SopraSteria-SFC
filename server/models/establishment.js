c796e465ebbb8a81f09352e57652816d70bc50c4
Matthew Daley <matthew.daley@Matthews-MacBook-Pro.local>
2022-03-18 16:23:31 +0000

    update search to be case insensitive

--- server/models/establishment.js
+++ server/models/establishment.js
@@ -1653,7 +1653,7 @@ module.exports = function (sequelize, DataTypes) {
           attributes,
           where: {
             archived: false,
-            ...(searchTerm ? { NameOrIdValue: { [Op.substring]: searchTerm } } : {}),
+            ...(searchTerm ? { NameOrIdValue: { [Op.iLike]: `%${searchTerm}%` } } : {}),
           },
           required: false,
           include: [
