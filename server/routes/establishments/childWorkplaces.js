9cb9447b78334cc4ac567d4fa0340ea83abfc426
duncanc19 <duncancarter93@gmail.com>
2022-03-23 13:09:31 +0000

    feat(childWorkplaceRecordPagination): Format child workplaces before returning from API endpoint

--- server/routes/establishments/childWorkplaces.js
+++ server/routes/establishments/childWorkplaces.js
@@ -5,14 +5,32 @@ const models = require('../../models');
 const getChildWorkplaces = async (req, res) => {
   try {
     const childWorkplaces = await models.establishment.getChildWorkplaces(req.params.id);
-    return res.status(200).json({ childWorkplaces });
+    const formattedChildWorkplaces = formatChildWorkplaces(childWorkplaces);
+
+    return res.status(200).json({ childWorkplaces: formattedChildWorkplaces });
   } catch (error) {
     console.error(error);
     return res.status(500).send();
   }
 };
 
+const formatChildWorkplaces = (childWorkplaces) => {
+  return childWorkplaces.map((workplace) => {
+    return {
+      dataOwner: workplace.dataOwner,
+      dataOwnershipRequested: workplace.dataOwnershipRequested,
+      dataPermissions: workplace.dataPermissions,
+      mainService: workplace.mainService.name,
+      name: workplace.NameValue,
+      uid: workplace.uid,
+      updated: workplace.updated,
+      ustatus: workplace.ustatus,
+    };
+  });
+};
+
 router.route('/').get(getChildWorkplaces);
 
 module.exports = router;
 module.exports.getChildWorkplaces = getChildWorkplaces;
+module.exports.formatChildWorkplaces = formatChildWorkplaces;
