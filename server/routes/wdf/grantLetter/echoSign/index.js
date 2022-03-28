0369b219daa0d79f152ccffb2e27ad2f9d6c6a9c
Matthew Daley <matthew.daley@Matthews-MacBook-Pro.local>
2022-03-28 16:42:21 +0100

    adds scaffold for creating adobe sign agreements

--- server/routes/wdf/grantLetter/echoSign/index.js
+++ server/routes/wdf/grantLetter/echoSign/index.js
@@ -21,3 +21,77 @@ module.exports.generateToken = async () => {
       return err;
     });
 };
+
+module.exports.createAgreement = async (claimData) => {
+  const { name, email, address, town, county, postcode, contractNumber, organisation, partnershipName } = claimData;
+  const body = {
+    fileInfos: [
+      {
+        libraryDocumentId: '',
+      },
+    ],
+    participantSetsInfo: [
+      {
+        role: 'SIGNER',
+        order: 1,
+        memberInfos: [
+          {
+            email: email,
+            name: name,
+          },
+        ],
+      },
+    ],
+    signatureType: 'ESIGN',
+    state: 'IN_PROCESS',
+    status: 'OUT_FOR_SIGNATURE',
+    name,
+    mergeFieldInfo: [
+      {
+        defaultValue: name.split(' ')[0],
+        fieldName: 'forename',
+      },
+      {
+        defaultValue: name,
+        fieldName: 'full_name',
+      },
+      {
+        defaultValue: organisation,
+        fieldName: 'organisation',
+      },
+      {
+        defaultValue: partnershipName,
+        fieldName: 'partnership_name',
+      },
+      {
+        defaultValue: address,
+        fieldName: 'address',
+      },
+      {
+        defaultValue: town,
+        fieldName: 'town',
+      },
+      {
+        defaultValue: county,
+        fieldName: 'county',
+      },
+      {
+        defaultValue: postcode,
+        fieldName: 'postcode',
+      },
+      {
+        defaultValue: contractNumber,
+        fieldName: 'contract_number',
+      },
+    ],
+  };
+
+  return axios
+    .post(`${adobeSignBaseUrl}/api/rest/v6/agreements`, body, {
+      headers: {
+        Authorization: `Bearer ${process.env.ADOBE_SIGN_KEY}`,
+      },
+    })
+    .then((response) => response)
+    .catch((err) => err);
+};
--- server/test/unit/routes/wdf/grantLetter/index.spec.js
+++ server/test/unit/routes/wdf/grantLetter/index.spec.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 // const httpMocks = require('node-mocks-http');
 const config = require('../../../../../config/config');
 
-const { generateToken } = require('../../../../../../server/routes/wdf/grantLetter/echoSign');
+const { generateToken, createAgreement } = require('../../../../../../server/routes/wdf/grantLetter/echoSign');
 
 describe.only('GrantLetter', () => {
   const adobeSignBaseUrl = config.get('adobeSign.apiBaseUrl');
@@ -19,40 +19,113 @@ describe.only('GrantLetter', () => {
   });
 
   describe('Adobe Signs Utils', () => {
-    it('returns the generated token from adobe sign', async () => {
-      axiosStub.resolves({
-        access_token: 'access_token',
-        refresh_token: 'refresh_token',
-        api_access_point: 'https://api.eu2.adobesign.com/',
-        web_access_point: 'https://secure.eu2.adobesign.com/',
-        token_type: 'Bearer',
-        expires_in: 3600,
+    describe('generateToken', () => {
+      it('returns the generated token from adobe sign', async () => {
+        axiosStub.resolves({
+          access_token: 'access_token',
+          refresh_token: 'refresh_token',
+          api_access_point: 'https://api.eu2.adobesign.com/',
+          web_access_point: 'https://secure.eu2.adobesign.com/',
+          token_type: 'Bearer',
+          expires_in: 3600,
+        });
+
+        const token = await generateToken();
+
+        expect(axiosStub).to.have.been.calledOnceWith(`${adobeSignBaseUrl}/oauth/v2/token`);
+        expect(token).to.equal('access_token');
       });
 
-      const token = await generateToken();
+      it('returns a token error message if Adobe Sign fails to return a token', async () => {
+        axiosStub.resolves({
+          message: 'something wrong',
+        });
 
-      expect(axiosStub).to.have.been.calledOnceWith(`${adobeSignBaseUrl}/oauth/v2/token`);
-      expect(token).to.equal('access_token');
-    });
+        const output = await generateToken();
 
-    it('returns a token error message if Adobe Sign fails to return a token', async () => {
-      axiosStub.resolves({
-        message: 'something wrong',
+        expect(axiosStub).to.have.been.calledOnceWith(`${adobeSignBaseUrl}/oauth/v2/token`);
+        expect(output.message).to.equal('token not returned');
       });
 
-      const output = await generateToken();
+      it('returns error returned if Adobe Sign API call fails', async () => {
+        axiosStub.rejects(Error('some error returned'));
 
-      expect(axiosStub).to.have.been.calledOnceWith(`${adobeSignBaseUrl}/oauth/v2/token`);
-      expect(output.message).to.equal('token not returned');
+        const output = await generateToken();
+
+        expect(axiosStub).to.have.been.calledOnceWith(`${adobeSignBaseUrl}/oauth/v2/token`);
+        expect(output.message).to.equal('some error returned');
+      });
     });
 
-    it('returns error returned if Adobe Sign API call fails', async () => {
-      axiosStub.rejects(Error('some error returned'));
+    describe('createAgreement', () => {
+      const data = {
+        name: 'name',
+        email: 'email',
+        address: 'address',
+        town: 'town',
+        county: 'county',
+        postcode: 'postcode',
+        contractNumber: 'contractNumber',
+        organisation: 'org',
+        partnershipName: 'partnership',
+      };
+      it('calls the adobe agreements endpoint with passed data and returns an ID for the agreement', async () => {
+        axiosStub.resolves({ id: 'an-id-goes-here' });
 
-      const output = await generateToken();
+        const output = await createAgreement(data);
 
-      expect(axiosStub).to.have.been.calledOnceWith(`${adobeSignBaseUrl}/oauth/v2/token`);
-      expect(output.message).to.equal('some error returned');
+        expect(output).to.eql({ id: 'an-id-goes-here' });
+        expect(axiosStub).to.be.calledOnceWithExactly(
+          `${adobeSignBaseUrl}/api/rest/v6/agreements`,
+          {
+            fileInfos: [
+              {
+                libraryDocumentId: 'CBJCHBCAABAAWaloyEh2SuKJ7y-NFqAe7CwlouyqBFjj',
+              },
+            ],
+            participantSetsInfo: [
+              {
+                role: 'SIGNER',
+                order: 1,
+                memberInfos: [
+                  {
+                    email: 'email',
+                    name: 'name',
+                  },
+                ],
+              },
+            ],
+            signatureType: 'ESIGN',
+            state: 'IN_PROCESS',
+            status: 'OUT_FOR_SIGNATURE',
+            name: 'name',
+            mergeFieldInfo: [
+              { defaultValue: 'name', fieldName: 'forename' },
+              { defaultValue: 'name', fieldName: 'full_name' },
+              { defaultValue: 'org', fieldName: 'organisation' },
+              { defaultValue: 'partnership', fieldName: 'partnership_name' },
+              { defaultValue: 'address', fieldName: 'address' },
+              { defaultValue: 'town', fieldName: 'town' },
+              { defaultValue: 'county', fieldName: 'county' },
+              { defaultValue: 'postcode', fieldName: 'postcode' },
+              { defaultValue: 'contractNumber', fieldName: 'contract_number' },
+            ],
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.ADOBE_SIGN_KEY}`,
+            },
+          },
+        );
+      });
+
+      it('returns an error if Adobe Sign rejects request', async () => {
+        axiosStub.rejects(Error('something went wrong'));
+
+        const output = await createAgreement(data);
+        expect(output).to.be.an('Error');
+        expect(output.message).to.equal('something went wrong');
+      });
     });
   });
 });
