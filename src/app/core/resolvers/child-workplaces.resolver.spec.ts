1def434aa66d0d2d456b41e218d08b1c12c8f7b7
popey2700 <alexanderpope27@outlook.com>
2022-03-28 09:54:20 +0100

    feat(childWorkplaceRecordPagination) Created a child-workplaces resolver

---
+++ src/app/core/resolvers/child-workplaces.resolver.spec.ts
@@ -0,0 +1,49 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
+
+import { ChildWorkplacesResolver } from './child-workplaces.resolver';
+
+describe('ChildWorkplacesResolver', () => {
+  function setup() {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([])],
+      providers: [
+        ChildWorkplacesResolver,
+        {
+          provide: EstablishmentService,
+          useClass: MockEstablishmentService,
+        },
+      ],
+    });
+
+    const resolver = TestBed.inject(ChildWorkplacesResolver);
+
+    const establishmentService = TestBed.inject(EstablishmentService);
+    spyOn(establishmentService, 'getChildWorkplaces').and.callThrough();
+
+    return {
+      resolver,
+      establishmentService,
+    };
+  }
+
+  it('should create', () => {
+    const { resolver } = setup();
+
+    expect(resolver).toBeTruthy();
+  });
+
+  it('should call getChildWorkplaces with id from establishmentService', () => {
+    const { resolver, establishmentService } = setup();
+
+    const primaryWorkplaceUid = '98a83eef-e1e1-49f3-89c5-b1287a3cc8de';
+    const queryParams = { pageIndex: 0, itemsPerPage: 15 };
+
+    resolver.resolve();
+
+    expect(establishmentService.getChildWorkplaces).toHaveBeenCalledWith(primaryWorkplaceUid, queryParams);
+  });
+});
