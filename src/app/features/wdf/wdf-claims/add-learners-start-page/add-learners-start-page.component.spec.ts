dcc88425a7a01cd1b6ad51ccc700c43eb4fe4b73
duncanc19 <duncancarter93@gmail.com>
2022-03-31 12:05:28 +0100

    feat(wdfClaimAddLearnersStartPage): Add dynamic staff record requirements based on claimType passed in through state

--- src/app/features/wdf/wdf-claims/add-learners-start-page/add-learners-start-page.component.spec.ts
+++ src/app/features/wdf/wdf-claims/add-learners-start-page/add-learners-start-page.component.spec.ts
@@ -7,18 +7,55 @@ import { render } from '@testing-library/angular';
 import { AddLearnersStartPageComponent } from './add-learners-start-page.component';
 
 describe('AddLearnersStartPageComponent', () => {
-  const setup = async () => {
-    const { fixture } = await render(AddLearnersStartPageComponent, {
+  const setup = async (claimType = 'learningProgramme') => {
+    window.history.pushState({ claimType }, '');
+
+    const { fixture, getByText } = await render(AddLearnersStartPageComponent, {
       imports: [RouterTestingModule, HttpClientTestingModule, BrowserModule, SharedModule],
     });
 
     const component = fixture.componentInstance;
 
-    return { component };
+    return { component, getByText };
   };
+
+  afterEach(() => {
+    window.history.replaceState(undefined, '');
+  });
 
   it('should render a WdfGrantLetterComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  describe('Staff record requirements message', () => {
+    const messageWithDOB =
+      'Staff records will need to include a name that matches the one shown on their learning award certificate, not an ID number, and a date of birth.';
+    const messageWithoutDOB =
+      'Staff records will need to include a name that matches the one shown on their learning award certificate, not an ID number.';
+
+    it('should include DOB when claimType is learningProgramme', async () => {
+      const { getByText } = await setup('learningProgramme');
+
+      expect(getByText(messageWithDOB)).toBeTruthy();
+    });
+
+    it('should include DOB when claimType is digitalModule', async () => {
+      const { getByText } = await setup('digitalModule');
+
+      expect(getByText(messageWithDOB)).toBeTruthy();
+    });
+
+    it('should not include DOB when claimType is apprenticeship', async () => {
+      const { getByText } = await setup('apprenticeship');
+
+      expect(getByText(messageWithoutDOB)).toBeTruthy();
+    });
+
+    it('should not include DOB when claimType is qualification', async () => {
+      const { getByText } = await setup('qualification');
+
+      expect(getByText(messageWithoutDOB)).toBeTruthy();
+    });
   });
 });
--- src/app/features/wdf/wdf-claims/add-learners-start-page/add-learners-start-page.component.ts
+++ src/app/features/wdf/wdf-claims/add-learners-start-page/add-learners-start-page.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-learners-start-page',
   templateUrl: './add-learners-start-page.component.html',
 })
-export class AddLearnersStartPageComponent {}
+export class AddLearnersStartPageComponent implements OnInit {
+  public claimType: string;
+  public dobRequired: boolean;
+
+  ngOnInit(): void {
+    this.claimType = history.state?.claimType;
+    this.dobRequired = ['learningProgramme', 'digitalModule'].includes(this.claimType);
+  }
+}
