b0c3fedc4116ce66b105e2b2847a5ed25c9c6901
duncanc19 <duncancarter93@gmail.com>
2022-04-04 12:50:37 +0100

    feat(wdfClaimAddLearnersStartPage): Set up stub tags for status icons in progress bar

--- src/app/features/wdf/wdf-claims/make-a-claim-progress-bar/make-a-claim-progress-bar.component.spec.ts
+++ src/app/features/wdf/wdf-claims/make-a-claim-progress-bar/make-a-claim-progress-bar.component.spec.ts
@@ -7,18 +7,104 @@ import { render } from '@testing-library/angular';
 import { MakeAClaimProgressBarComponent } from './make-a-claim-progress-bar.component';
 
 describe('MakeAClaimProgressBarComponent', () => {
-  const setup = async () => {
-    const { fixture, getByText } = await render(MakeAClaimProgressBarComponent, {
+  const setup = async (stepIndex = 0) => {
+    const { fixture, getByText, getByTestId } = await render(MakeAClaimProgressBarComponent, {
       imports: [RouterTestingModule, HttpClientTestingModule, BrowserModule, SharedModule],
+      componentProperties: {
+        stepIndex,
+      },
     });
 
     const component = fixture.componentInstance;
 
-    return { component, getByText };
+    return { component, getByText, getByTestId };
   };
 
   it('should render a MakeAClaimProgressBarComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should render all step names', async () => {
+    const { getByText } = await setup();
+
+    expect(getByText('Select qualification')).toBeTruthy();
+    expect(getByText('Add learners')).toBeTruthy();
+    expect(getByText('Upload certificate')).toBeTruthy();
+    expect(getByText('Review and submit')).toBeTruthy();
+  });
+
+  describe('First step in progress', () => {
+    it('should show currentStep icon next to first step name', async () => {
+      const { getByTestId } = await setup();
+
+      expect(getByTestId('currentStep-0')).toBeTruthy();
+    });
+
+    it('should show notCompleted icon next to second, third and fourth steps', async () => {
+      const { getByTestId } = await setup();
+
+      expect(getByTestId('notCompleted-1')).toBeTruthy();
+      expect(getByTestId('notCompleted-2')).toBeTruthy();
+      expect(getByTestId('notCompleted-3')).toBeTruthy();
+    });
+  });
+
+  describe('Second step in progress', () => {
+    it('should show completed icon next to first step name', async () => {
+      const { getByTestId } = await setup(1);
+
+      expect(getByTestId('completed-0')).toBeTruthy();
+    });
+
+    it('should show currentStep icon next to second step name', async () => {
+      const { getByTestId } = await setup(1);
+
+      expect(getByTestId('currentStep-1')).toBeTruthy();
+    });
+
+    it('should show notCompleted icon next to second, third and fourth steps', async () => {
+      const { getByTestId } = await setup(1);
+
+      expect(getByTestId('notCompleted-2')).toBeTruthy();
+      expect(getByTestId('notCompleted-3')).toBeTruthy();
+    });
+  });
+
+  describe('Third step in progress', () => {
+    it('should show completed icon next to first and second step names', async () => {
+      const { getByTestId } = await setup(2);
+
+      expect(getByTestId('completed-0')).toBeTruthy();
+      expect(getByTestId('completed-1')).toBeTruthy();
+    });
+
+    it('should show currentStep icon next to third step name', async () => {
+      const { getByTestId } = await setup(2);
+
+      expect(getByTestId('currentStep-2')).toBeTruthy();
+    });
+
+    it('should show notCompleted icon next to fourth step name', async () => {
+      const { getByTestId } = await setup(2);
+
+      expect(getByTestId('notCompleted-3')).toBeTruthy();
+    });
+  });
+
+  describe('Fourth step in progress', () => {
+    it('should show completed icon next to first and second step names', async () => {
+      const { getByTestId } = await setup(3);
+
+      expect(getByTestId('completed-0')).toBeTruthy();
+      expect(getByTestId('completed-1')).toBeTruthy();
+      expect(getByTestId('completed-2')).toBeTruthy();
+    });
+
+    it('should show currentStep icon next to third step name', async () => {
+      const { getByTestId } = await setup(3);
+
+      expect(getByTestId('currentStep-3')).toBeTruthy();
+    });
   });
 });
--- src/app/features/wdf/wdf-claims/make-a-claim-progress-bar/make-a-claim-progress-bar.component.ts
+++ src/app/features/wdf/wdf-claims/make-a-claim-progress-bar/make-a-claim-progress-bar.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-make-a-claim-progress-bar',
   templateUrl: './make-a-claim-progress-bar.component.html',
 })
-export class MakeAClaimProgressBarComponent {}
+export class MakeAClaimProgressBarComponent {
+  @Input() stepIndex: number;
+  public steps = ['Select qualification', 'Add learners', 'Upload certificate', 'Review and submit'];
+}
