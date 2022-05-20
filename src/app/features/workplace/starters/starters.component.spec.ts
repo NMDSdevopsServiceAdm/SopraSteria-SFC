433cd59858b32735721c9f5630569f3045e9fd22
duncanc19 <duncancarter93@gmail.com>
2022-05-20 15:28:48 +0100

    feat(workplaceDetailsNewStartersUpdate): Add line break and update button text

--- src/app/features/workplace/starters/starters.component.spec.ts
+++ src/app/features/workplace/starters/starters.component.spec.ts
@@ -4,24 +4,30 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EstablishmentService } from '@core/services/establishment.service';
+import { JobService } from '@core/services/job.service';
 import {
   MockEstablishmentService,
   MockEstablishmentServiceWithoutReturn,
 } from '@core/test-utils/MockEstablishmentService';
+import { MockJobService } from '@core/test-utils/MockJobService';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 
 import { StartersComponent } from './starters.component';
 
 describe('StartersComponent', () => {
   async function setup(returnUrl = true) {
-    const { fixture, getByText, getAllByText, getByLabelText } = await render(StartersComponent, {
+    const { fixture, getByText, getAllByText, getByLabelText, getByTestId } = await render(StartersComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
       providers: [
         FormBuilder,
         {
           provide: EstablishmentService,
           useClass: returnUrl ? MockEstablishmentService : MockEstablishmentServiceWithoutReturn,
+        },
+        {
+          provide: JobService,
+          useClass: MockJobService,
         },
       ],
     });
@@ -36,6 +42,7 @@ describe('StartersComponent', () => {
       getByText,
       getAllByText,
       getByLabelText,
+      getByTestId,
       establishmentServiceSpy,
     };
   }
@@ -50,6 +57,22 @@ describe('StartersComponent', () => {
 
     expect(getByLabelText('There have been no new starters in the last 12 months')).toBeTruthy();
     expect(getByLabelText('I do not know how many new starters there have been')).toBeTruthy();
+  });
+
+  it('should add another input when row when the add another job role button is clicked', async () => {
+    const { fixture, getByText, getByTestId } = await setup();
+
+    const button = getByText('Add another job role');
+    fireEvent.click(button);
+    fixture.detectChanges();
+
+    const firstInputRow = getByTestId('row-0');
+    const secondInputRow = getByTestId('row-1');
+
+    expect(firstInputRow).toBeTruthy();
+    expect(firstInputRow.innerHTML).toContain('Job role 1');
+    expect(secondInputRow).toBeTruthy();
+    expect(secondInputRow.innerHTML).toContain('Job role 2');
   });
 
   describe('Submit buttons', () => {
