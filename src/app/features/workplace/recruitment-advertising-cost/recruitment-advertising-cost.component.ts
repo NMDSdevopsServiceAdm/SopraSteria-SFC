4be29aca828a4bf7522bfb20946011918f3a6d13
Richard Pentecost <richard.pentecost@madetech.com>
2022-06-09 17:06:14 +0100

    setup form in the html for recruitment advertising cost component

--- src/app/features/workplace/recruitment-advertising-cost/recruitment-advertising-cost.component.ts
+++ src/app/features/workplace/recruitment-advertising-cost/recruitment-advertising-cost.component.ts
@@ -1,7 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BackService } from '@core/services/back.service';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
+import { EstablishmentService } from '@core/services/establishment.service';
+
+import { Question } from '../question/question.component';
 
 @Component({
   selector: 'app-recruitment-advertising-cost',
   templateUrl: './recruitment-advertising-cost.component.html',
 })
-export class RecruitmentAdvertisingCostComponent {}
+export class RecruitmentAdvertisingCostComponent extends Question implements OnInit, OnDestroy {
+  public amountSpentKnownOptions = [
+    {
+      label: 'Nothing has been spent on advertising for staff in the last 4 weeks',
+      value: 'None',
+    },
+    {
+      label: 'I do not know how much has been spent on advertising for staff',
+      value: 'Unknown',
+    },
+  ];
+
+  constructor(
+    protected formBuilder: FormBuilder,
+    protected router: Router,
+    protected backService: BackService,
+    protected errorSummaryService: ErrorSummaryService,
+    protected establishmentService: EstablishmentService,
+  ) {
+    super(formBuilder, router, backService, errorSummaryService, establishmentService);
+  }
+
+  protected init(): void {
+    this.setupForm();
+  }
+
+  private setupForm(): void {
+    this.form = this.formBuilder.group({
+      amountSpent: ['', [Validators.required]],
+      amountSpentKnown: null,
+    });
+  }
+}
