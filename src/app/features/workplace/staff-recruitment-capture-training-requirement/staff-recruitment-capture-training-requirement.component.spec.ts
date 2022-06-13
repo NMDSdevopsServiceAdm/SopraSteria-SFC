ff2e0b7ba4bfb92f75494c74c7f97f895319705d
popey2700 <alexanderpope27@outlook.com>
2022-06-13 12:35:33 +0100

    feat(staffRecruitmentCaptureTrainingRequirement): Created the form to capture training requirement

--- src/app/features/workplace/staff-recruitment-capture-training-requirement/staff-recruitment-capture-training-requirement.component.spec.ts
+++ src/app/features/workplace/staff-recruitment-capture-training-requirement/staff-recruitment-capture-training-requirement.component.spec.ts
@@ -1,26 +1,112 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { getTestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 
 import { StaffRecruitmentCaptureTrainingRequirementComponent } from './staff-recruitment-capture-training-requirement.component';
 
 describe('StaffRecruitmentCaptureTrainingRequirement', () => {
   async function setup() {
-    const { fixture } = await render(StaffRecruitmentCaptureTrainingRequirementComponent, {
+    const { fixture, getByText, getByLabelText } = await render(StaffRecruitmentCaptureTrainingRequirementComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
-      providers: [FormBuilder],
+      providers: [
+        FormBuilder,
+        {
+          provide: EstablishmentService,
+          useClass: MockEstablishmentService,
+        },
+      ],
     });
 
     const component = fixture.componentInstance;
+    const injector = getTestBed();
+    const establishmentService = injector.inject(EstablishmentService) as EstablishmentService;
+    const establishmentServiceSpy = spyOn(establishmentService, 'postStaffRecruitmentData').and.callThrough();
 
-    return { component };
+    return { component, fixture, getByText, getByLabelText, establishmentServiceSpy };
   }
 
   it('should render StaffRecruitmentCaptureTrainingRequirementComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should show the save and continue button', async () => {
+    const { getByText } = await setup();
+
+    expect(getByText('Save and return')).toBeTruthy();
+  });
+
+  it('should submit the form with the correct value when the "Yes, always" radio button is selected and the form is submitted', async () => {
+    const { fixture, getByText, getByLabelText, establishmentServiceSpy } = await setup();
+
+    const radioButton = getByLabelText('Yes, always');
+    fireEvent.click(radioButton);
+    fixture.detectChanges();
+
+    const submitButton = getByText('Save and return');
+    fireEvent.click(submitButton);
+    fixture.detectChanges();
+
+    expect(establishmentServiceSpy).toHaveBeenCalledWith('mocked-uid', {
+      staffRecruitmentColumn: 'doNewStartersRepeatMandatoryTrainingFromPreviousEmployment',
+      staffRecruitmentData: 'Yes, always',
+    });
+  });
+
+  it('should submit the form with the correct value when the "Yes, very often" radio button is selected and the form is submitted', async () => {
+    const { fixture, getByText, getByLabelText, establishmentServiceSpy } = await setup();
+
+    const radioButton = getByLabelText('Yes, very often');
+    fireEvent.click(radioButton);
+    fixture.detectChanges();
+
+    const submitButton = getByText('Save and return');
+    fireEvent.click(submitButton);
+    fixture.detectChanges();
+
+    expect(establishmentServiceSpy).toHaveBeenCalledWith('mocked-uid', {
+      staffRecruitmentColumn: 'doNewStartersRepeatMandatoryTrainingFromPreviousEmployment',
+      staffRecruitmentData: 'Yes, very often',
+    });
+  });
+
+  it('should submit the form with the correct value when the "Yes, but not very often" radio button is selected and the form is submitted', async () => {
+    const { fixture, getByText, getByLabelText, establishmentServiceSpy } = await setup();
+
+    const radioButton = getByLabelText('Yes, but not very often');
+    fireEvent.click(radioButton);
+    fixture.detectChanges();
+
+    const submitButton = getByText('Save and return');
+    fireEvent.click(submitButton);
+    fixture.detectChanges();
+
+    expect(establishmentServiceSpy).toHaveBeenCalledWith('mocked-uid', {
+      staffRecruitmentColumn: 'doNewStartersRepeatMandatoryTrainingFromPreviousEmployment',
+      staffRecruitmentData: 'Yes, but not very often',
+    });
+  });
+
+  it('should submit the form with the correct value when the "No, never" radio button is selected and the form is submitted', async () => {
+    const { fixture, getByText, getByLabelText, establishmentServiceSpy } = await setup();
+
+    const radioButton = getByLabelText('No, never');
+    fireEvent.click(radioButton);
+    fixture.detectChanges();
+
+    const submitButton = getByText('Save and return');
+    fireEvent.click(submitButton);
+    fixture.detectChanges();
+
+    expect(establishmentServiceSpy).toHaveBeenCalledWith('mocked-uid', {
+      staffRecruitmentColumn: 'doNewStartersRepeatMandatoryTrainingFromPreviousEmployment',
+      staffRecruitmentData: 'No, never',
+    });
   });
 });
--- src/app/features/workplace/staff-recruitment-capture-training-requirement/staff-recruitment-capture-training-requirement.component.ts
+++ src/app/features/workplace/staff-recruitment-capture-training-requirement/staff-recruitment-capture-training-requirement.component.ts
@@ -1,7 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BackService } from '@core/services/back.service';
+import { ErrorSummaryService } from '@core/services/error-summary.service';
+import { EstablishmentService } from '@core/services/establishment.service';
+
+import { Question } from '../question/question.component';
 
 @Component({
   selector: 'app-staff-recruitment-capture-training-requirement',
   templateUrl: './staff-recruitment-capture-training-requirement.component.html',
 })
-export class StaffRecruitmentCaptureTrainingRequirementComponent {}
+export class StaffRecruitmentCaptureTrainingRequirementComponent extends Question implements OnInit, OnDestroy {
+  public trainingRequiredOptions = [
+    {
+      label: 'Yes, always',
+      value: 'Yes, always',
+    },
+    {
+      label: 'Yes, very often',
+      value: 'Yes, very often',
+    },
+    {
+      label: 'Yes, but not very often',
+      value: 'Yes, but not very often',
+    },
+    {
+      label: 'No, never',
+      value: 'No, never',
+    },
+  ];
+
+  public emptyForm = true;
+
+  constructor(
+    protected formBuilder: FormBuilder,
+    protected router: Router,
+    protected backService: BackService,
+    protected errorSummaryService: ErrorSummaryService,
+    protected establishmentService: EstablishmentService,
+  ) {
+    super(formBuilder, router, backService, errorSummaryService, establishmentService);
+  }
+  protected init(): void {
+    this.setupForm();
+
+    this.nextRoute = ['/workplace', `${this.establishment.uid}`, 'accept-previous-care-certificate'];
+    this.previousRoute = ['/workplace', `${this.establishment.uid}`, 'number-of-people-interviewed'];
+  }
+
+  private setupForm(): void {
+    this.form = this.formBuilder.group(
+      {
+        trainingRequired: ['', Validators.required],
+      },
+      { updateOn: 'submit' },
+    );
+  }
+
+  protected generateUpdateProps() {
+    const { trainingRequired } = this.form.value;
+    return trainingRequired;
+  }
+
+  protected updateEstablishment(props): void {
+    this.subscriptions.add(
+      this.establishmentService
+        .postStaffRecruitmentData(this.establishment.uid, {
+          staffRecruitmentColumn: 'doNewStartersRepeatMandatoryTrainingFromPreviousEmployment',
+          staffRecruitmentData: props,
+        })
+        .subscribe(),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+}
