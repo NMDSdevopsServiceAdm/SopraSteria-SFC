6d6c06c0a5d379eeb89ad2a3aa893d8d87b1ba6d
popey2700 <alexanderpope27@outlook.com>
2022-07-01 17:05:13 +0100

    feat(createAdminUserDetailsPage): Created font-end to display user details and added logic of whether to show change and delete links

--- src/app/features/admin/admin-users/admin-account-view/admin-account-view.component.spec.ts
+++ src/app/features/admin/admin-users/admin-account-view/admin-account-view.component.spec.ts
@@ -1,22 +1,33 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { AdminManagerUser, AdminUser } from '@core/test-utils/admin/MockAdminUsersService';
 import { MockBreadcrumbService } from '@core/test-utils/MockBreadcrumbService';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { queryByText, render } from '@testing-library/angular';
 
 import { AdminAccountViewComponent } from './admin-account-view.component';
 
 describe('AdminAccountViewComponent', () => {
-  async function setup() {
-    const { fixture, getByText } = await render(AdminAccountViewComponent, {
+  async function setup(isAdminManagerType = true) {
+    const { fixture, getByText, queryByText } = await render(AdminAccountViewComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
       providers: [
         {
           provide: BreadcrumbService,
           useClass: MockBreadcrumbService,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                adminUser: isAdminManagerType ? AdminManagerUser() : AdminUser(),
+              },
+            },
+          },
         },
       ],
     });
@@ -33,6 +44,7 @@ describe('AdminAccountViewComponent', () => {
       routerSpy,
       router,
       getByText,
+      queryByText,
     };
   }
 
@@ -42,25 +54,96 @@ describe('AdminAccountViewComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  //TODO: update this test when link functionality is implemented
-  it('should render a Resend the user email link', async () => {
-    const { getByText } = await setup();
+  describe('buttons and links', async () => {
+    //TODO: update this test when link functionality is implemented
+    xit('should render a Resend the user email link if user is pending', async () => {
+      const { queryByText } = await setup();
 
-    expect(getByText('Resend the user set-up email')).toBeTruthy();
+      expect(queryByText('Resend the user set-up email')).toBeTruthy();
+    });
+
+    xit('shouldnt render a Resend the user email link if user is not pending', async () => {
+      const { queryByText } = await setup(false);
+
+      expect(queryByText('Resend the user set-up email')).toBeTruthy();
+    });
+
+    it('shouldnt render a delete this user link when user is not an AdminManager', async () => {
+      const { queryByText } = await setup(false);
+
+      expect(queryByText('Delete this admin user')).toBeFalsy();
+    });
+
+    it('should render a delete this user link when user is an AdminManager', async () => {
+      const { queryByText } = await setup();
+
+      expect(queryByText('Delete this admin user')).toBeTruthy();
+    });
+
+    it('shouldnt render a change link when user is not an AdminManager', async () => {
+      const { queryByText } = await setup(false);
+
+      expect(queryByText('Change')).toBeFalsy();
+    });
+
+    it('should render a change link when user is an AdminManager', async () => {
+      const { queryByText } = await setup();
+
+      expect(queryByText('Change')).toBeTruthy();
+    });
+
+    it('Should navigate to admin users summary page when view admin users button is clicked', async () => {
+      const { getByText } = await setup();
+
+      const button = getByText('View admin users');
+
+      expect(button.getAttribute('href')).toBe('/sfcadmin/users');
+    });
   });
 
-  //TODO: update this test when link functionality is implemented
-  it('should render a delte this user link', async () => {
-    const { getByText } = await setup();
+  describe('Admin user details', async () => {
+    it('should render Admin User details with AdminManager Role', async () => {
+      const { component, queryByText } = await setup();
 
-    expect(getByText('Delete this admin user')).toBeTruthy();
-  });
+      expect(queryByText('Full name')).toBeTruthy();
+      expect(queryByText(component.user.fullname)).toBeTruthy();
 
-  it('Should navigate to admin users summary page when view admin users button is clicked', async () => {
-    const { getByText } = await setup();
+      expect(queryByText('Job title')).toBeTruthy();
+      expect(queryByText(component.user.jobTitle)).toBeTruthy();
 
-    const button = getByText('View admin users');
+      expect(queryByText('Email address')).toBeTruthy();
+      expect(queryByText(component.user.email)).toBeTruthy();
 
-    expect(button.getAttribute('href')).toBe('/sfcadmin/users');
+      expect(queryByText('Phone number')).toBeTruthy();
+      expect(queryByText(component.user.phone)).toBeTruthy();
+
+      expect(queryByText('Username')).toBeTruthy();
+      expect(queryByText(component.user.username)).toBeTruthy();
+
+      expect(queryByText('Permissions')).toBeTruthy();
+      expect(queryByText('Admin manager')).toBeTruthy();
+    });
+
+    it('should render Admin User details with Admin Role', async () => {
+      const { component, queryByText } = await setup(false);
+
+      expect(queryByText('Full name')).toBeTruthy();
+      expect(queryByText(component.user.fullname)).toBeTruthy();
+
+      expect(queryByText('Job title')).toBeTruthy();
+      expect(queryByText(component.user.jobTitle)).toBeTruthy();
+
+      expect(queryByText('Email address')).toBeTruthy();
+      expect(queryByText(component.user.email)).toBeTruthy();
+
+      expect(queryByText('Phone number')).toBeTruthy();
+      expect(queryByText(component.user.phone)).toBeTruthy();
+
+      expect(queryByText('Username')).toBeTruthy();
+      expect(queryByText(component.user.username)).toBeTruthy();
+
+      expect(queryByText('Permissions')).toBeTruthy();
+      expect(queryByText('Admin')).toBeTruthy();
+    });
   });
 });
--- src/app/features/admin/admin-users/admin-account-view/admin-account-view.component.ts
+++ src/app/features/admin/admin-users/admin-account-view/admin-account-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
+import { SummaryList } from '@core/model/summary-list.model';
 import { UserDetails } from '@core/model/userDetails.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 
@@ -10,6 +11,8 @@ import { BreadcrumbService } from '@core/services/breadcrumb.service';
 })
 export class AdminAccountViewComponent implements OnInit {
   public user: UserDetails;
+  public userDetails: SummaryList[];
+  public canNavigate: boolean;
 
   constructor(private route: ActivatedRoute, private router: Router, public breadcrumbService: BreadcrumbService) {
     this.user = this.route.snapshot.data.adminUser;
@@ -17,6 +20,40 @@ export class AdminAccountViewComponent implements OnInit {
 
   public ngOnInit(): void {
     this.breadcrumbService.show(JourneyType.ADMIN_USERS);
-    console.log(this.user);
+    this.setAdminUserDetails();
+  }
+
+  public isAdminManager(): boolean {
+    return this.user.role === 'AdminManager';
+  }
+
+  private setAdminUserDetails(): void {
+    this.userDetails = [
+      {
+        label: 'Full name',
+        data: this.user.fullname,
+        route: this.isAdminManager() ? { url: ['edit'] } : undefined,
+      },
+      {
+        label: 'Job title',
+        data: this.user.jobTitle,
+      },
+      {
+        label: 'Email address',
+        data: this.user.email,
+      },
+      {
+        label: 'Phone number',
+        data: this.user.phone,
+      },
+      {
+        label: 'Username',
+        data: this.user.username || '-',
+      },
+      {
+        label: 'Permissions',
+        data: this.user.role === 'AdminManager' ? 'Admin manager' : this.user.role,
+      },
+    ];
   }
 }
