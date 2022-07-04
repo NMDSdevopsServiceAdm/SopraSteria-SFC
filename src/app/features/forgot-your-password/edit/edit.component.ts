db2bc13013f2b12414166eaa35d017bea97c7076
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2022-07-04 17:05:57 +0100

    removed unnecessary punctuation from error message

--- src/app/features/forgot-your-password/edit/edit.component.ts
+++ src/app/features/forgot-your-password/edit/edit.component.ts
@@ -37,7 +37,7 @@ export class ForgotYourPasswordEditComponent implements OnInit, AfterViewInit {
         type: [
           {
             name: 'required',
-            message: 'Enter the username or email address.',
+            message: 'Enter the username or email address',
           },
         ],
       },
