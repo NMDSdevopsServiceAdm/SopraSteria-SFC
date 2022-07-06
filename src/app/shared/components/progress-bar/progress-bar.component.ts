58b21bad5dbd3674bb871895af120efde91f2350
Richard Pentecost <richard.pentecost@madetech.com>
2022-07-06 16:54:30 +0100

    complete functionality of the progress bar and write tests

--- src/app/shared/components/progress-bar/progress-bar.component.ts
+++ src/app/shared/components/progress-bar/progress-bar.component.ts
@@ -3,12 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.scss'],
 })
 export class ProgressBarComponent implements OnInit {
   @Input() sections: string[];
-  public stepIndex = 1;
+  @Input() currentSection: string;
+  public currentSectionIndex: number;
 
   public ngOnInit(): void {
-    console.log('******* progress bar *********');
+    this.currentSectionIndex = this.sections.indexOf(this.currentSection);
   }
 }
