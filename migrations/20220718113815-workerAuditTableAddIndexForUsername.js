6d358a5183bed03db17f43d13d8ef1cb26086e53
Richard Pentecost <richard.pentecost@madetech.com>
2022-07-20 13:26:48 +0100

    modify worker audit migration

--- migrations/20220718113815-workerAuditTableAddIndexForUsername.js
+++ migrations/20220718113815-workerAuditTableAddIndexForUsername.js
@@ -1,23 +1,16 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.sequelize.transaction(async (transaction) => {
-      await Promise.all([
-        queryInterface.sequelize.query('DROP INDEX IF EXISTS cqc."worker_audit__username" ON cqc."WorkerAudit";', {
-          transaction,
-        }),
-        queryInterface.addIndex(
-          {
-            tableName: 'WorkerAudit',
-            schema: 'cqc',
-          },
-          {
-            fields: ['Username'],
-          },
-        ),
-      ]);
-    });
+  up: async (queryInterface) => {
+    await queryInterface.addIndex(
+      {
+        tableName: 'WorkerAudit',
+        schema: 'cqc',
+      },
+      {
+        fields: ['Username'],
+      },
+    );
   },
 
   down: async (queryInterface) => {
