e3a3c478908f17e1910c327db71346167ca58c0b
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-07-26 17:16:21 +0100

    Add test for submit button and skip question

--- src/app/features/workplace/staff-benefit-cash-loyalty/staff-benefit-cash-loyalty.component.spec.ts
+++ src/app/features/workplace/staff-benefit-cash-loyalty/staff-benefit-cash-loyalty.component.spec.ts
@@ -114,4 +114,108 @@ describe('StaffBenefitCashLoyaltyComponent', () => {
     expect(radioButton.checked).toBeTruthy();
     expect(component.form.value).toEqual({ cashLoyalty: 'No' });
   });
+
+  describe('submit buttons and submitting form', () => {
+    it(`should show 'Save and continue' cta button and 'Skip this question'`, async () => {
+      const { getByText } = await setup(false);
+
+      expect(getByText('Save and continue')).toBeTruthy();
+      expect(getByText('Skip this question')).toBeTruthy();
+    });
+
+    it(`should call the setSubmitAction function with an action of continue and save as true when clicking 'Save and continue' button`, async () => {
+      const { component, fixture, getByText } = await setup(false);
+
+      const setSubmitActionSpy = spyOn(component, 'setSubmitAction').and.callThrough();
+
+      const button = getByText('Save and continue');
+      fireEvent.click(button);
+      fixture.detectChanges();
+
+      expect(setSubmitActionSpy).toHaveBeenCalledWith({ action: 'continue', save: true });
+    });
+
+    it(`should call the setSubmitAction function with an action of skip and save as false when clicking 'Skip this question' link`, async () => {
+      const { component, fixture, getByText } = await setup(false);
+
+      const setSubmitActionSpy = spyOn(component, 'setSubmitAction').and.callThrough();
+
+      const link = getByText('Skip this question');
+      fireEvent.click(link);
+      fixture.detectChanges();
+
+      expect(setSubmitActionSpy).toHaveBeenCalledWith({ action: 'skip', save: false });
+    });
+
+    it('should not call the updateSingleEstablishmentField when submitting form when the form has not been filled out', async () => {
+      const { fixture, getByText, establishmentServiceSpy } = await setup();
+
+      const button = getByText('Save and return');
+      fireEvent.click(button);
+      fixture.detectChanges();
+
+      expect(establishmentServiceSpy).not.toHaveBeenCalled();
+    });
+
+    it('should navigate to the next page when submitting from the flow', async () => {
+      const { fixture, getByText, routerSpy } = await setup(false);
+
+      const button = getByText('Save and continue');
+      fireEvent.click(button);
+      fixture.detectChanges();
+
+      expect(routerSpy).toHaveBeenCalledWith(['/workplace', 'mocked-uid', 'benefits-statutory-sick-pay']);
+    });
+
+    it('should navigate to the next page when submitting from the flow', async () => {
+      const { component, fixture, getByText, routerSpy } = await setup(false);
+
+      const link = getByText('Skip this question');
+      fireEvent.click(link);
+      fixture.detectChanges();
+
+      expect(routerSpy).toHaveBeenCalledWith(['/workplace', 'mocked-uid', 'benefits-statutory-sick-pay']);
+    });
+
+    it(`should show 'Save and return' cta button and 'Cancel' link if a return url is provided`, async () => {
+      const { getByText } = await setup();
+
+      expect(getByText('Save and return')).toBeTruthy();
+      expect(getByText('Cancel')).toBeTruthy();
+    });
+
+    it(`should call the setSubmitAction function with an action of return and save as true when clicking 'Save and return' button`, async () => {
+      const { component, fixture, getByText } = await setup();
+
+      const setSubmitActionSpy = spyOn(component, 'setSubmitAction').and.callThrough();
+
+      const button = getByText('Save and return');
+      fireEvent.click(button);
+      fixture.detectChanges();
+
+      expect(setSubmitActionSpy).toHaveBeenCalledWith({ action: 'return', save: true });
+    });
+
+    it(`should call the setSubmitAction function with an action of exit and save as false when clicking 'Cancel' link`, async () => {
+      const { component, fixture, getByText } = await setup();
+
+      const setSubmitActionSpy = spyOn(component, 'setSubmitAction').and.callThrough();
+
+      const link = getByText('Cancel');
+      fireEvent.click(link);
+      fixture.detectChanges();
+
+      expect(setSubmitActionSpy).toHaveBeenCalledWith({ action: 'return', save: false });
+    });
+
+    it('should navigate to the summary page when submitting', async () => {
+      const { fixture, getByText, routerSpy } = await setup();
+
+      const button = getByText('Save and return');
+      fireEvent.click(button);
+      fixture.detectChanges();
+
+      expect(routerSpy).toHaveBeenCalledWith(['/dashboard'], { fragment: 'workplace', queryParams: undefined });
+    });
+  });
 });
