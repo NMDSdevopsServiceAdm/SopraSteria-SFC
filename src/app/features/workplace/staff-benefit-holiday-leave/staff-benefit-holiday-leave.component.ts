a8f3a42511e3afba134e74534a5ccb112c1dcfe9
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-07-28 14:33:29 +0100

    Add validation for holiday leave

--- src/app/features/workplace/staff-benefit-holiday-leave/staff-benefit-holiday-leave.component.ts
+++ src/app/features/workplace/staff-benefit-holiday-leave/staff-benefit-holiday-leave.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BackService } from '@core/services/back.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
@@ -14,6 +14,9 @@ import { Question } from '../question/question.component';
 export class StaffBenefitHolidayLeaveComponent extends Question implements OnInit, OnDestroy {
   public inStaffRecruitmentFlow: boolean;
   public section: string;
+  private numberCheckRegex = /^-?\d*(\.\d*)?$/;
+  private wholeNumberCheckRegex = /^-?[A-Za-z0-9]*$/;
+  private positiveNumberCheckRegex = /^[A-Za-z\d*(.\d*)]*$/;
   constructor(
     protected formBuilder: FormBuilder,
     protected router: Router,
@@ -36,10 +39,50 @@ export class StaffBenefitHolidayLeaveComponent extends Question implements OnIni
 
   private setupForm(): void {
     this.form = this.formBuilder.group({
-      holidayLeave: null,
+      holidayLeave: [
+        null,
+        [
+          this.customValidator(this.numberCheckRegex, 'nonInteger'),
+          this.customValidator(this.positiveNumberCheckRegex, 'negativeNumber'),
+          this.customValidator(this.wholeNumberCheckRegex, 'nonWholeNumber'),
+        ],
+      ],
     });
   }
 
+  private customValidator(regexp: RegExp, error: string): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const { value } = control;
+      const validControlValue = regexp.test(value);
+      if (value) {
+        return !validControlValue ? { [error]: { value } } : null;
+      }
+      return null;
+    };
+  }
+
+  protected setupFormErrorsMap(): void {
+    this.formErrorsMap = [
+      {
+        item: 'holidayLeave',
+        type: [
+          {
+            name: 'nonInteger',
+            message: 'Number of days must be a number, like 7',
+          },
+          {
+            name: 'nonWholeNumber',
+            message: 'Number of days must be a whole number, like 7',
+          },
+
+          {
+            name: 'negativeNumber',
+            message: 'Number of days must be a positive number, like 7',
+          },
+        ],
+      },
+    ];
+  }
   private prefill(): void {
     if (this.establishment.careWorkersLeaveDaysPerYear) {
       this.form.patchValue({
