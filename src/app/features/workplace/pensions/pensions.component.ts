8fc9ec663cca02735b0bc37a25e7456167a921db
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2022-08-01 10:10:56 +0100

    corrected current section for progress bar on pensions

--- src/app/features/workplace/pensions/pensions.component.ts
+++ src/app/features/workplace/pensions/pensions.component.ts
@@ -44,83 +44,53 @@ export class PensionsComponent extends Question implements OnInit, OnDestroy {
   }
 
   protected init(): void {
-    // this.setupFormValueSubscriptions();
+    this.setupForm();
     this.inStaffRecruitmentFlow = this.establishmentService.inStaffRecruitmentFlow;
     this.setRoutes();
     this.prefill();
-    this.section = this.inStaffRecruitmentFlow ? 'Staff benefits' : 'Recruitment';
+    this.section = this.inStaffRecruitmentFlow ? 'Pensions' : 'Staff benefits';
   }
 
   private setRoutes(): void {
     this.previousRoute = this.inStaffRecruitmentFlow
-      ? ['/workplace', `${this.establishment.uid}`, 'staff-recruitment-start']
-      : ['/workplace', `${this.establishment.uid}`, 'leavers'];
-    this.skipRoute = ['/workplace', `${this.establishment.uid}`, 'number-of-interviews'];
+      ? ['/workplace', `${this.establishment.uid}`, 'benefits-statutory-sick-pay']
+      : ['/workplace', `${this.establishment.uid}`, 'benefits-statutory-sick-pay'];
+    this.skipRoute = ['/workplace', `${this.establishment.uid}`, ''];
+  }
+
+  private setupForm(): void {
+    this.form = this.formBuilder.group(
+      {
+        pension: null,
+      },
+      { updateOn: 'submit' },
+    );
   }
 
   private prefill(): void {
     if (this.establishment.doCareWorkersGetMoreWorkplacePensionContributionThanTheMinimumThreePercent) {
-      this.form
-        .get('amountSpent')
-        .setValue(this.establishment.doCareWorkersGetMoreWorkplacePensionContributionThanTheMinimumThreePercent);
+      this.form.patchValue({
+        pension: this.establishment.doCareWorkersGetMoreWorkplacePensionContributionThanTheMinimumThreePercent,
+      });
     }
-  }
-
-  //   private setupFormValueSubscriptions(): void {
-  //     this.subscriptions.add(
-  //       this.form.get('amountSpentKnown').valueChanges.subscribe((value) => {
-  //         this.form.get('amountSpent').clearValidators();
-  //         this.form.get('amountSpent').setValue(null, { emitEvent: false });
-  //         this.form.get('amountSpentKnown').setValue(value, { emitEvent: false });
-  //       }),
-  //     );
-
-  //     this.subscriptions.add(
-  //       this.form.get('amountSpent').valueChanges.subscribe(() => {
-  //         this.form
-  //           .get('amountSpent')
-  //           .setValidators([Validators.pattern(FLOAT_PATTERN), this.greaterThanTwoDecimalPlacesValidator()]);
-
-  //         if (this.form.get('amountSpentKnown').value !== null) {
-  //           this.form.get('amountSpentKnown').setValue(null, { emitEvent: false });
-  //         }
-
-  //         this.addErrorLinkFunctionality();
-  //       }),
-  //     );
-  //   }
-
-  protected setupFormErrorsMap(): void {
-    this.formErrorsMap = [
-      {
-        item: 'amountSpent',
-        type: [
-          {
-            name: 'greaterThanTwoDecimalPlaces',
-            message: 'Amount spent must only include pence, like 132.00 or 150.40',
-          },
-          {
-            name: 'pattern',
-            message: 'Amount spent must be a positive number, like 132 or 150.40',
-          },
-        ],
-      },
-    ];
   }
 
   protected generateUpdateProps(): any {
-    const { amountSpent, amountSpentKnown } = this.form.value;
-
-    if (amountSpent || amountSpentKnown) {
-      return amountSpentKnown ? { amountSpent: amountSpentKnown } : { amountSpent };
+    const { pension } = this.form.value;
+    if (pension) {
+      return { pension };
     }
-
     return null;
   }
 
   protected updateEstablishment(props: any): void {
+    const pensionData = {
+      property: 'pension',
+      value: props.statutorySickPay,
+    };
+
     this.subscriptions.add(
-      this.establishmentService.postStaffRecruitmentData(this.establishment.uid, props).subscribe(
+      this.establishmentService.updateSingleEstablishmentField(this.establishment.uid, pensionData).subscribe(
         (data) => this._onSuccess(data),
         (error) => this.onError(error),
       ),
@@ -142,13 +112,6 @@ export class PensionsComponent extends Question implements OnInit, OnDestroy {
 
   protected onSuccess(): void {
     this.updateEstablishmentService();
-    this.nextRoute = ['/workplace', `${this.establishment.uid}`, 'number-of-interviews'];
-  }
-
-  protected addErrorLinkFunctionality(): void {
-    if (!this.errorSummaryService.formEl$.value) {
-      this.errorSummaryService.formEl$.next(this.formEl);
-      this.submitted = false;
-    }
+    this.nextRoute = ['/workplace', `${this.establishment.uid}`, 'pensions'];
   }
 }
--- src/app/features/workplace/workplace.module.ts
+++ src/app/features/workplace/workplace.module.ts
@@ -31,6 +31,7 @@ import { EditWorkplaceComponent } from './edit-workplace/edit-workplace.componen
 import { LeaversComponent } from './leavers/leavers.component';
 import { NumberOfInterviewsComponent } from './number-of-interviews/number-of-interviews.component';
 import { OtherServicesComponent } from './other-services/other-services.component';
+import { PensionsComponent } from './pensions/pensions.component';
 import { RecruitmentAdvertisingCostComponent } from './recruitment-advertising-cost/recruitment-advertising-cost.component';
 import { RegulatedByCqcComponent } from './regulated-by-cqc/regulated-by-cqc.component';
 import { SelectMainServiceComponent } from './select-main-service/select-main-service.component';
@@ -104,6 +105,7 @@ import { WorkplaceRoutingModule } from './workplace-routing.module';
     ConfirmStaffRecruitmentComponent,
     StaffBenefitCashLoyaltyComponent,
     BenefitsStatutorySickPayComponent,
+    PensionsComponent,
   ],
   providers: [
     DialogService,
