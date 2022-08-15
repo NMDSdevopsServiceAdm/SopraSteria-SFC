c78c39a0b4bd23cd1e4579c24756e7b790ddd1e2
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-08-15 15:20:55 +0100

    Rewrite the archive inactive workplaces query so when a parent workplace have ac active child it shouldn’t be deleted

--- server/models/email-campaigns/inactive-workplaces/getInactiveWorkplacesForDeletion.js
+++ server/models/email-campaigns/inactive-workplaces/getInactiveWorkplacesForDeletion.js
@@ -30,19 +30,21 @@ const getInactiveWorkplacesForDeletion = async () => {
   WHERE
   	e."LastLogin" <= :twentyFourLastMonths
 	  AND e."LastUpdated" <= :twentyFourLastMonths
-  	AND ("IsParent" = false OR
-	NOT EXISTS(
-		SELECT
-		  "EstablishmentID"
-	  FROM
-		  cqc."EstablishmentLastActivity" s
-	  WHERE
-		  e."EstablishmentID" = s."ParentID"
-		  AND s."LastLogin" > :twentyFourLastMonths
-		  AND s."LastUpdated" > :twentyFourLastMonths
-	  )
+    AND NOT EXISTS
+    (
+       SELECT s."EstablishmentID" AS EstablishmentID
+          FROM cqc."EstablishmentLastActivity" s
+          WHERE  s."IsParent" = true AND EXISTS
+          (
+            SELECT c."EstablishmentID"
+             FROM cqc."EstablishmentLastActivity" c
+              WHERE s."EstablishmentID" = c."ParentID"
+              AND c."LastLogin" > :twentyFourLastMonths
+              AND c."LastUpdated" > :twentyFourLastMonths
+              AND c."IsParent"= false
+           ) AND s."EstablishmentID"  =  e."EstablishmentID"
+   )
 
-  )
       `,
     {
       type: models.sequelize.QueryTypes.SELECT,
