21d9d79048a11b00b4a1116d2a5d6183723476bb
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-08-19 14:07:39 +0100

    add the new enum to the worker model and fix the tests

--- server/routes/establishments/bulkUpload/download/workerCSV.js
+++ server/routes/establishments/bulkUpload/download/workerCSV.js
@@ -203,6 +203,14 @@ const toCSV = (establishmentId, entity, MAX_QUALIFICATIONS, downloadType) => {
     case "Don't know":
       apprenticeship = 999;
       break;
+
+    case 'Think ahead':
+      apprenticeship = 314;
+      break;
+
+    case 'Social worker integrated Degree':
+      apprenticeship = 315;
+      break;
   }
   columns.push(apprenticeship);
 
