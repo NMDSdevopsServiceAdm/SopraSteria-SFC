b2aaf15cdee04483d7e284a189700b09e35dc0d3
Richard Pentecost <richard.pentecost@madetech.com>
2022-08-23 10:33:18 +0100

    create select-workplace-radio-button-form component

---
+++ src/app/shared/components/select-workplace-radio-button-form/select-workplace-radio-button-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-workplace-radio-button-form',
+  templateUrl: './select-workplace-radio-button-form.component.html',
+})
+export class SelectWorkplaceRadioButtonFormComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('Here');
+  }
+}
