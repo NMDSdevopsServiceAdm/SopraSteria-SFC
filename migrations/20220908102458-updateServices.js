e13780a819c567b4840c388fc944ecf6782f0de4
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2022-09-08 11:49:47 +0100

    add down proc to migration

--- migrations/20220908102458-updateServices.js
+++ migrations/20220908102458-updateServices.js
@@ -27,4 +27,31 @@ module.exports = {
       ]);
     });
   },
+
+  down: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await Promise.all([
+        queryInterface.sequelize.query(
+          'update cqc."services" set "name"=\'Disability adaptations / assistive technology services\' where "id"=3;',
+          { transaction },
+        ),
+        queryInterface.sequelize.query(
+          'update cqc."services" set "name"=\'Occupational / employment-related services\' where "id"=5;',
+          { transaction },
+        ),
+        queryInterface.sequelize.query(
+          'update cqc."services" set "name"=\'Long term conditions services\' where "id"=32;',
+          { transaction },
+        ),
+        queryInterface.sequelize.query(
+          'update cqc."services" set "name"=\'Residential substance misuse treatment / rehabilitation services\' where "id"=34;',
+          { transaction },
+        ),
+        queryInterface.sequelize.query(
+          'update cqc."services" set "name"=\'Any children\'\'s / young people\'\'s services\' where "id"=14;',
+          { transaction },
+        ),
+      ]);
+    });
+  },
 };
