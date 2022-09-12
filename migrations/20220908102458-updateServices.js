ec494685339a156baa23f68a6bcf9b5c0d4fd16b
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2022-09-12 14:37:51 +0100

    removed unnecessary migration logic

--- migrations/20220908102458-updateServices.js
+++ migrations/20220908102458-updateServices.js
@@ -12,30 +12,6 @@ module.exports = {
           'update cqc."services" set "name"=\'Occupational, employment-related services\' where "id"=5;',
           { transaction },
         ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Long-term conditions services\' where "id"=32;',
-          { transaction },
-        ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Residential substance misuse treatment, rehabilitation services\' where "id"=34;',
-          { transaction },
-        ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Any children\'\'s, young people\'\'s service\' where "id"=14;',
-          { transaction },
-        ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Short breaks, respite care\' where "id"=7;',
-          { transaction },
-        ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Other adult residential care service\' where "id"=12;',
-          { transaction },
-        ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Other adult day care service\' where "id"=10;',
-          { transaction },
-        ),
       ]);
     });
   },
@@ -61,18 +37,6 @@ module.exports = {
         ),
         queryInterface.sequelize.query(
           'update cqc."services" set "name"=\'Any children\'\'s / young people\'\'s services\' where "id"=14;',
-          { transaction },
-        ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Short breaks / respite care\' where "id"=7;',
-          { transaction },
-        ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Other adult residential care services\' where "id"=12;',
-          { transaction },
-        ),
-        queryInterface.sequelize.query(
-          'update cqc."services" set "name"=\'Other adult day care services\' where "id"=10;',
           { transaction },
         ),
       ]);
