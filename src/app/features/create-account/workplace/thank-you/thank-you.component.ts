0149cb6ee424372f326ac05efffb0e30e12dc32c
popey2700 <alex.pope@madetech.com>
2022-09-16 09:46:28 +0100

    feat(snaggingListCreateAccount): Fixed a bug where the user details page would pre-fill details of a previous accounts creation

--- src/app/features/create-account/workplace/thank-you/thank-you.component.ts
+++ src/app/features/create-account/workplace/thank-you/thank-you.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RegistrationService } from '@core/services/registration.service';
+import { UserService } from '@core/services/user.service';
 
 @Component({
   selector: 'app-thank-you',
   templateUrl: './thank-you.component.html',
 })
 export class ThankYouComponent {
-  constructor(public registrationService: RegistrationService) {
+  constructor(public registrationService: RegistrationService, private userService: UserService) {
     this.registrationService.registrationInProgress$.next(false);
     this.registrationService.resetService();
+    this.userService.resetUserDetails();
   }
 }
