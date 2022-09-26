4cbe0b200a5b048e2d831ea42f14eb62e64cbbec
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2022-09-26 15:51:06 +0100

    Fixed failing tests

--- src/app/features/workers/national-insurance-number/national-insurance-number.component.spec.ts
+++ src/app/features/workers/national-insurance-number/national-insurance-number.component.spec.ts
@@ -11,29 +11,32 @@ import { NationalInsuranceNumberComponent } from './national-insurance-number.co
 
 describe('NationalInsuranceNumberComponent', () => {
   async function setup(returnUrl = true) {
-    const { fixture, getByText, getAllByText, getByLabelText } = await render(NationalInsuranceNumberComponent, {
-      imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
-      providers: [
-        FormBuilder,
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            parent: {
-              snapshot: {
-                data: {
-                  establishment: { uid: 'mocked-uid' },
-                  primaryWorkplace: {},
+    const { fixture, getByText, getAllByText, getByLabelText, queryByTestId, getByTestId } = await render(
+      NationalInsuranceNumberComponent,
+      {
+        imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
+        providers: [
+          FormBuilder,
+          {
+            provide: ActivatedRoute,
+            useValue: {
+              parent: {
+                snapshot: {
+                  data: {
+                    establishment: { uid: 'mocked-uid' },
+                    primaryWorkplace: {},
+                  },
                 },
               },
             },
           },
-        },
-        {
-          provide: WorkerService,
-          useClass: returnUrl ? MockWorkerService : MockWorkerServiceWithoutReturnUrl,
-        },
-      ],
-    });
+          {
+            provide: WorkerService,
+            useClass: returnUrl ? MockWorkerService : MockWorkerServiceWithoutReturnUrl,
+          },
+        ],
+      },
+    );
 
     const component = fixture.componentInstance;
 
@@ -43,12 +46,26 @@ describe('NationalInsuranceNumberComponent', () => {
       getByText,
       getAllByText,
       getByLabelText,
+      queryByTestId,
+      getByTestId,
     };
   }
 
   it('should render the NationalInsuranceNumberComponent', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should render the progress bar', async () => {
+    const { queryByTestId } = await setup();
+
+    expect(queryByTestId('progress-bar-1')).toBeTruthy();
+  });
+
+  it('should render the input field', async () => {
+    const { getByTestId } = await setup();
+
+    expect(getByTestId('ni-input'));
   });
 
   describe('submit buttons', () => {
