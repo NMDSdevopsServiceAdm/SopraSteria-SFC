dbbee67eb9bcd02715ac36ba34ef123efd6d8ff6
Richard Pentecost <richard.pentecost@madetech.com>
2022-09-27 16:53:38 +0100

    add tests for navigation on the date-of-birth component

--- src/app/features/workers/date-of-birth/date-of-birth.component.spec.ts
+++ src/app/features/workers/date-of-birth/date-of-birth.component.spec.ts
@@ -1,11 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { getTestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { WorkerService } from '@core/services/worker.service';
-import { MockWorkerService } from '@core/test-utils/MockWorkerService';
+import { MockWorkerServiceWithUpdateWorker } from '@core/test-utils/MockWorkerService';
 import { SharedModule } from '@shared/shared.module';
 import { render } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 
 import { DateOfBirthComponent } from './date-of-birth.component';
 
@@ -33,7 +35,7 @@ describe('DateOfBirthComponent', () => {
           },
           {
             provide: WorkerService,
-            useClass: MockWorkerService,
+            useClass: MockWorkerServiceWithUpdateWorker,
           },
         ],
       },
@@ -41,9 +43,20 @@ describe('DateOfBirthComponent', () => {
 
     const component = fixture.componentInstance;
 
+    const injector = getTestBed();
+    const router = injector.inject(Router) as Router;
+    const workerService = injector.inject(WorkerService);
+
+    const submitSpy = spyOn(component, 'onSubmit').and.callThrough();
+    const routerSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
+    const workerServiceSpy = spyOn(workerService, 'updateWorker').and.callThrough();
+
     return {
       component,
       fixture,
+      routerSpy,
+      submitSpy,
+      workerServiceSpy,
       getByText,
       getAllByText,
       getByLabelText,
@@ -80,11 +93,108 @@ describe('DateOfBirthComponent', () => {
       expect(getByText('Skip this question')).toBeTruthy();
     });
 
+    it(`should call submit data and navigate with the correct url when 'Save and continue' is clicked`, async () => {
+      const { component, fixture, getByText, getByLabelText, submitSpy, workerServiceSpy, routerSpy } = await setup();
+
+      userEvent.type(getByLabelText('Day'), '11');
+      userEvent.type(getByLabelText('Month'), '6');
+      userEvent.type(getByLabelText('Year'), '1993');
+      userEvent.click(getByText('Save and continue'));
+      fixture.detectChanges();
+
+      const updatedFormData = component.form.value;
+      expect(updatedFormData).toEqual({ dob: { day: 11, month: 6, year: 1993 } });
+
+      expect(submitSpy).toHaveBeenCalledWith({ action: 'continue', save: true });
+      expect(workerServiceSpy).toHaveBeenCalledWith(component.workplace.uid, component.worker.uid, {
+        dateOfBirth: '1993-06-11',
+      });
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        fixture.componentInstance.worker.uid,
+        'national-insurance-number',
+      ]);
+    });
+
+    it('allows the user to view the staff record summary', async () => {
+      const { fixture, getByText, submitSpy, routerSpy, workerServiceSpy } = await setup();
+
+      userEvent.click(getByText('View this staff record'));
+      expect(submitSpy).toHaveBeenCalledWith({ action: 'summary', save: false });
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        fixture.componentInstance.worker.uid,
+        'staff-record-summary',
+      ]);
+      expect(workerServiceSpy).not.toHaveBeenCalled();
+    });
+
+    it('allows the user to skip the question', async () => {
+      const { fixture, getByText, submitSpy, routerSpy, workerServiceSpy } = await setup();
+
+      userEvent.click(getByText('Skip this question'));
+      expect(submitSpy).toHaveBeenCalledWith({ action: 'skip', save: false });
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        fixture.componentInstance.worker.uid,
+        'national-insurance-number',
+      ]);
+      expect(workerServiceSpy).not.toHaveBeenCalled();
+    });
+
     it(`should show 'Save and return' cta button and 'Cancel' link if a return url is provided`, async () => {
       const { getByText } = await setup(false);
 
       expect(getByText('Save and return')).toBeTruthy();
       expect(getByText('Cancel')).toBeTruthy();
+    });
+
+    it(`should call submit data and navigate with the correct url when 'Save and return' is clicked`, async () => {
+      const { component, fixture, getByText, getByLabelText, submitSpy, workerServiceSpy, routerSpy } = await setup(
+        false,
+      );
+
+      userEvent.type(getByLabelText('Day'), '11');
+      userEvent.type(getByLabelText('Month'), '6');
+      userEvent.type(getByLabelText('Year'), '1993');
+      userEvent.click(getByText('Save and return'));
+      fixture.detectChanges();
+
+      const updatedFormData = component.form.value;
+      expect(updatedFormData).toEqual({ dob: { day: 11, month: 6, year: 1993 } });
+
+      expect(submitSpy).toHaveBeenCalledWith({ action: 'return', save: true });
+      expect(workerServiceSpy).toHaveBeenCalledWith(component.workplace.uid, component.worker.uid, {
+        dateOfBirth: '1993-06-11',
+      });
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        fixture.componentInstance.worker.uid,
+        'staff-record-summary',
+      ]);
+    });
+
+    it('return tot the staff record summary when cancel is clicked', async () => {
+      const { fixture, getByText, submitSpy, routerSpy, workerServiceSpy } = await setup(false);
+
+      userEvent.click(getByText('Cancel'));
+      expect(submitSpy).toHaveBeenCalledWith({ action: 'return', save: false });
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        fixture.componentInstance.worker.uid,
+        'staff-record-summary',
+      ]);
+      expect(workerServiceSpy).not.toHaveBeenCalled();
     });
   });
 });
