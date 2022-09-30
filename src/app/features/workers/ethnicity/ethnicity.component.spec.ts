b9f7e8c2c8de7f49da202386490bee4511b10ba3
Gabriele Bernotaite <gabriele.bernotaite@gabrieles-mbp.home>
2022-09-30 14:24:39 +0100

    added tests

--- src/app/features/workers/ethnicity/ethnicity.component.spec.ts
+++ src/app/features/workers/ethnicity/ethnicity.component.spec.ts
@@ -1,11 +1,12 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { getTestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { WorkerService } from '@core/services/worker.service';
 import { MockWorkerService, MockWorkerServiceWithoutReturnUrl } from '@core/test-utils/MockWorkerService';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 
 import { EthnicityComponent } from './ethnicity.component';
 
@@ -18,6 +19,11 @@ describe('EthnicityComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
+            snapshot: {
+              parent: {
+                url: [{ path: returnUrl ? 'staff-record-summary' : 'mocked-uid' }],
+              },
+            },
             parent: {
               snapshot: {
                 data: {
@@ -35,11 +41,17 @@ describe('EthnicityComponent', () => {
       ],
     });
 
+    const injector = getTestBed();
+
     const component = fixture.componentInstance;
+    const router = injector.inject(Router) as Router;
+
+    const routerSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
 
     return {
       component,
       fixture,
+      routerSpy,
       getByText,
       getAllByText,
       getByLabelText,
@@ -52,14 +64,22 @@ describe('EthnicityComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render the progress bar', async () => {
-    const { queryByTestId } = await setup();
+  describe('progress bar', () => {
+    it('should render the progress bar', async () => {
+      const { queryByTestId } = await setup(false);
 
-    expect(queryByTestId('progress-bar-1')).toBeTruthy();
+      expect(queryByTestId('progress-bar-1')).toBeTruthy();
+    });
+
+    it('should not render the progress bar if not inside the flow', async () => {
+      const { queryByTestId } = await setup();
+
+      expect(queryByTestId('progress-bar-1')).toBeFalsy();
+    });
   });
 
   describe('submit buttons', () => {
-    it(`should show 'Save and continue' cta button and 'View this staff record' link, if a return url is not provided`, async () => {
+    it(`should show 'Save and continue' cta button and 'View this staff record' link, and skip skip this question link, if a return url is not provided`, async () => {
       const { getByText } = await setup(false);
 
       expect(getByText('Save and continue')).toBeTruthy();
@@ -68,10 +88,24 @@ describe('EthnicityComponent', () => {
     });
 
     it(`should show 'Save and return' cta button and 'Cancel' link if a return url is provided`, async () => {
-      const { getByText } = await setup();
+      const { getByText } = await setup(true);
 
       expect(getByText('Save and return')).toBeTruthy();
       expect(getByText('Cancel')).toBeTruthy();
+    });
+  });
+
+  describe('navigation', () => {
+    it('should navigate to nationality page when submitting from flow', async () => {
+      const { component, routerSpy, getByText } = await setup(false);
+
+      const workerId = component.worker.uid;
+      const workplaceId = component.workplace.uid;
+
+      const saveButton = getByText('Save and continue');
+      fireEvent.click(saveButton);
+
+      expect(routerSpy).toHaveBeenCalledWith(['/workplace', workplaceId, 'staff-record', workerId, 'nationality']);
     });
   });
 });
