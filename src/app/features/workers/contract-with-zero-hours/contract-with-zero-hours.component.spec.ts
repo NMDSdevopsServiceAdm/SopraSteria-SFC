c335bcb9ec7db9c5b2ea5c9d7e6c5f057f402307
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-10-07 12:34:26 +0100

    Add test for call to action button of the zero contract hour

--- src/app/features/workers/contract-with-zero-hours/contract-with-zero-hours.component.spec.ts
+++ src/app/features/workers/contract-with-zero-hours/contract-with-zero-hours.component.spec.ts
@@ -1,41 +1,79 @@
+import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { getTestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BackService } from '@core/services/back.service';
 import { WorkerService } from '@core/services/worker.service';
-import { MockWorkerService, MockWorkerServiceWithoutReturnUrl } from '@core/test-utils/MockWorkerService';
+import { MockWorkerServiceWithoutReturnUrl } from '@core/test-utils/MockWorkerService';
+import { build, fake } from '@jackfranklin/test-data-bot';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 
 import { ContractWithZeroHoursComponent } from './contract-with-zero-hours.component';
 
+const workerBuilder = build('Worker', {
+  fields: {
+    uid: fake((f) => f.datatype.uuid()),
+    contract: 'Permanent',
+  },
+});
+
+const noPermanentContract = () =>
+  workerBuilder({
+    overrides: {
+      contract: 'Other',
+    },
+  });
+
 describe('ContractWithZeroHoursComponent', () => {
-  async function setup(returnUrl = true) {
-    const { fixture, getByText, getAllByText, getByLabelText } = await render(ContractWithZeroHoursComponent, {
-      imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
-      providers: [
-        FormBuilder,
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            parent: {
-              snapshot: {
-                data: {
-                  establishment: { uid: 'mocked-uid' },
+  async function setup(insideFlow = true, contractType = 'permanent') {
+    let contract;
+
+    if (contractType === 'permanent') {
+      contract = workerBuilder();
+    } else if (contractType === 'other') {
+      contract = noPermanentContract();
+    }
+
+    const { fixture, getByText, getAllByText, getByLabelText, getByTestId, queryByTestId } = await render(
+      ContractWithZeroHoursComponent,
+      {
+        imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
+        providers: [
+          FormBuilder,
+          {
+            provide: ActivatedRoute,
+            useValue: {
+              parent: {
+                snapshot: {
+                  url: [{ path: insideFlow ? 'staff-uid' : 'staff-record-summary' }],
+                  data: {
+                    establishment: { uid: 'mocked-uid' },
+                    primaryWorkplace: {},
+                  },
                 },
-                url: [{ path: '' }],
               },
             },
           },
-        },
-        {
-          provide: WorkerService,
-          useClass: returnUrl ? MockWorkerService : MockWorkerServiceWithoutReturnUrl,
-        },
-      ],
-    });
+          {
+            provide: WorkerService,
+            useFactory: MockWorkerServiceWithoutReturnUrl.factory(contract),
+            deps: [HttpClient],
+          },
+        ],
+      },
+    );
 
     const component = fixture.componentInstance;
+
+    const injector = getTestBed();
+    const router = injector.inject(Router) as Router;
+    const backService = injector.inject(BackService);
+
+    const routerSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
+    const backLinkSpy = spyOn(backService, 'setBackLink');
 
     return {
       component,
@@ -43,6 +81,10 @@ describe('ContractWithZeroHoursComponent', () => {
       getByText,
       getAllByText,
       getByLabelText,
+      routerSpy,
+      backLinkSpy,
+      getByTestId,
+      queryByTestId,
     };
   }
 
@@ -53,17 +95,132 @@ describe('ContractWithZeroHoursComponent', () => {
 
   describe('submit buttons', () => {
     it(`should show 'Save and continue' cta button and 'View this staff record' link, if a return url is not provided`, async () => {
-      const { getByText } = await setup(false);
+      const { getByText } = await setup();
 
       expect(getByText('Save and continue')).toBeTruthy();
       expect(getByText('View this staff record')).toBeTruthy();
+      expect(getByText('Skip this question')).toBeTruthy();
     });
 
     it(`should show 'Save and return' cta button and 'Cancel' link if a return url is provided`, async () => {
-      const { getByText } = await setup();
+      const { getByText } = await setup(false);
 
       expect(getByText('Save and return')).toBeTruthy();
       expect(getByText('Cancel')).toBeTruthy();
+    });
+
+    it(`should call submit data and navigate with the 'average-weekly-hours' url when 'Save and continue' is clicked and contract type is 'other' ,'Agency' and 'Pool,Bank' `, async () => {
+      const { component, getByText, routerSpy } = await setup(true, 'other');
+
+      const button = getByText('Save and continue');
+      fireEvent.click(button);
+
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        component.worker.uid,
+        'average-weekly-hours',
+      ]);
+    });
+
+    it(`should call submit data and navigate with the  'weekly-contracted-hours' url when 'Save and continue' is clicked and contract type is permanent 0r temporary`, async () => {
+      const { component, getByText, routerSpy } = await setup(true, 'permanent');
+
+      const button = getByText('Save and continue');
+      fireEvent.click(button);
+
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        component.worker.uid,
+        'weekly-contracted-hours',
+      ]);
+    });
+
+    it(`should call submit data and navigate with the   'weekly-contracted-hours' url when 'Skip this question' is clicked and contract type is permanent pr temporary`, async () => {
+      const { component, getByText, routerSpy } = await setup(true, 'permanent');
+
+      const button = getByText('Skip this question');
+      fireEvent.click(button);
+
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        component.worker.uid,
+        'weekly-contracted-hours',
+      ]);
+    });
+
+    it(`should call submit data and navigate with the 'average-weekly-hours' url when 'Skip this question' is clicked and the contract typs is not permanent or temporary`, async () => {
+      const { component, getByText, routerSpy } = await setup(true, 'other');
+
+      const button = getByText('Skip this question');
+      fireEvent.click(button);
+
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        'mocked-uid',
+        'staff-record',
+        component.worker.uid,
+        'average-weekly-hours',
+      ]);
+    });
+
+    it(`should navigate to 'staff-summary-page' page when clicking 'View this staff record' link `, async () => {
+      const { component, routerSpy, getByText } = await setup();
+
+      const workerId = component.worker.uid;
+      const workplaceId = component.workplace.uid;
+
+      const viewStaffRecord = getByText('View this staff record');
+      fireEvent.click(viewStaffRecord);
+
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        workplaceId,
+        'staff-record',
+        workerId,
+        'staff-record-summary',
+      ]);
+    });
+
+    it('should navigate to staff-summary-page page when pressing save and return', async () => {
+      const { component, routerSpy, getByText } = await setup(false);
+
+      const workerId = component.worker.uid;
+      const workplaceId = component.workplace.uid;
+
+      const link = getByText('Save and return');
+      fireEvent.click(link);
+
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        workplaceId,
+        'staff-record',
+        workerId,
+        'staff-record-summary',
+      ]);
+    });
+
+    it('should navigate to staff-summary-page page when pressing cancel', async () => {
+      const { component, routerSpy, getByText } = await setup(false);
+
+      const workerId = component.worker.uid;
+      const workplaceId = component.workplace.uid;
+
+      const link = getByText('Cancel');
+      fireEvent.click(link);
+
+      expect(routerSpy).toHaveBeenCalledWith([
+        '/workplace',
+        workplaceId,
+        'staff-record',
+        workerId,
+        'staff-record-summary',
+      ]);
     });
   });
 });
