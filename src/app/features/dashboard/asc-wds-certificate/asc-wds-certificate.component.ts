d638d805d4af05541ac0a9c4f139013682927ec4
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2022-10-27 11:41:21 +0100

    preliminary changes for certificates

---
+++ src/app/features/dashboard/asc-wds-certificate/asc-wds-certificate.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asc-wds-certificate',
+  templateUrl: './asc-wds-certificate.component.html',
+})
+export class AscWdsCertificateComponent {
+  constructor() {}
+}
