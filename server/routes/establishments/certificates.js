3fa113ceb0a503389da33dd0911c1a203c6148f6
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2022-11-02 16:19:53 +0000

    Succesfully generating and downloading certificates

--- server/routes/establishments/certificates.js
+++ server/routes/establishments/certificates.js
@@ -1,4 +1,3 @@
-
 const config = require('../../config/config');
 const s3 = new (require('aws-sdk').S3)({
   region: String(config.get('certificate.region')),
@@ -6,128 +5,124 @@ const s3 = new (require('aws-sdk').S3)({
 // const s3Client = require('./libs/s3Client.js');
 // const path = require('path');
 const Bucket = String(config.get('certificate.bucketname'));
-const PDFDocument = require('pdf-lib');
+const pdfLib = require('pdf-lib');
 // const fetch = require('node-fetch');
-// const Establishment = require('../../models/classes/establishment');
+const Establishment = require('../../models/classes/establishment');
 // const Authorization = require('../../utils/security/isAuthenticated');
 const express = require('express');
 const router = express.Router();
 // const { s3, Bucket } = require('./s3');
 
-const filePathBase = 'public/download/Certificates/';
-const fileNameBase = 'ASC-WDS+certificate';
+const filePathBase = 'public/download/Certificates';
+let fileNameBase = 'ASC-WDS certificate';
+let Key;
 
-const params = (fileName) => {
-  let key = fileName.replace('+', ' ');
-  console.log(key);
+const params = () => {
   return {
     Bucket,
-    Key: key,
+    Key,
   };
 };
 
 const getCertificate = async (req, res) => {
-    // Determine file name
-    const fileName = getFileName(req.params.Id);
-    console.log(fileName);
-    // Check file exists
-    if(!fileExists(fileName)) {
-      // const thisEstablishment = new Establishment.Establishment(req.username);
-      // await thisEstablishment.restore();
+  // Determine file name
+  const fileName = getFileNameYears();
+  const establishmentFileName = `${req.establishment.uid} ${fileName}`;
+  Key = `${filePathBase}/${establishmentFileName}`;
 
-      // let newFile;
-      // modifyPdf(fileName, thisEstablishment.name).subscribe(x => newFile = x);
+  // Check file exists
+  const exists = await fileExists();
+  if (!exists) {
+    const thisEstablishment = new Establishment.Establishment(req.username);
+    await thisEstablishment.restore(req.establishment.uid);
 
-      // const uploadParams = {
-      //     Bucket: 'sfc_public_staging',
-      //     Key: path.basename(fileName),
-      //     Body: newFile,
-      // };
-      // uploadToS3(uploadParams);
-    }
+    console.log('About to Modify');
+    const newFile = await modifyPdf(thisEstablishment.name, fileName);
 
-    // TODO: Return download link
+    console.log('newFile');
+    const uploadParams = {
+      Bucket,
+      Key,
+      Body: newFile,
+      ContentDisposition: `attachment; filename="${fileName}"`,
+      ACL: 'public-read',
+    };
+    uploadToS3(uploadParams);
+  }
+  res.status(200).send({ data: Key });
+};
 
-}
+const fileExists = async () => {
+  let result;
+  const objectParams = params();
+  await s3
+    .getObject(objectParams)
+    .promise()
+    .then((data) => {
+      console.log(data);
+      result = true;
+    })
+    .catch(() => (result = false));
+  return result;
+};
 
-const fileExists = (fileName) => {
-  const objectParams = params(fileName);
-  // const bucketContents = await s3.getObject(objectParams).promise();
-
-    // s3.getObject({
-    //     Bucket,
-    //     Key: fileName,
-    //   })
-    //     .promise()
-    //     .then((data) => {
-    //       const jsonData = JSON.parse(data.Body.toString());
-
-    //       return jsonData;
-    //     //   if (Number.isInteger(jsonData.responseCode) && jsonData.responseCode > 99) {
-    //     //     if (jsonData.responseHeaders) {
-    //     //       res.set(jsonData.responseHeaders);
-    //     //     }
-
-    //     //     res.status(jsonData.responseCode).send(jsonData.responseBody);
-    //     //   } else {
-    //     //     console.log('bulkUpload::responseGet: Response code was not numeric', jsonData);
-
-    //     //     throw new Error('Response code was not numeric');
-    //     //   }
-    //     })
-    //     .catch((err) => {
-    //       console.error('getting data returned an error:', err);
-
-    //     //   res.status(404).send({
-    //     //     message: 'Not Found',
-    //     //   });
-    //     });
-}
-
-const getFileName = (establishmentId) => {
-    const years = getYears();
-    fileNameBase = `${fileNameBase}+${years}.pdf`
-    return `${filePathBase}/${establishmentId}+${fileNameBase}`;
-}
+const getFileNameYears = () => {
+  const years = getYears();
+  return `${fileNameBase} ${years}.pdf`;
+};
 
 const getYears = () => {
-    const date = new Date();
-    const currentMonth = date.getMonth();
-    const currentYear = date.getFullYear() - 2000;
+  const date = new Date();
+  const currentMonth = date.getMonth();
+  const currentYear = date.getFullYear() - 2000;
 
-    if(currentMonth >= 4) {
-      return `${currentYear}-${currentYear+1}`
-    } else {
-      return `${currentYear-1}-${currentYear}`
-    }
-}
-
-const modifyPdf = async (establishmentName) => {
-    const url = `https://sfc-public-staging.s3.eu-west-2.amazonaws.com/${filePathBase}/Templates/${fileNameBase}`
-    const existingPdfBytes = null //await fetch(url).then(res => res.arrayBuffer())
-
-    const pdfDoc = await PDFDocument.load(existingPdfBytes)
-
-    const form = pdfDoc.getForm();
-    const fields = form.getFields();
-    const textField = form.getTextField(fields[0].getName());
-    textField.setText(establishmentName);
-
-    return await pdfDoc.save();
-    // writeFile('output.pdf', pdfBytes, (err) => {
-    //   if(err) throw err;
-    // });
+  if (currentMonth >= 4) {
+    return `${currentYear}-${currentYear + 1}`;
+  } else {
+    return `${currentYear - 1}-${currentYear}`;
   }
+};
+
+const modifyPdf = async (establishmentName, fileName) => {
+  const params = {
+    Key: `${filePathBase}/Templates/${fileName}`,
+    Bucket,
+  };
+
+  let existingPdfBytes;
+  console.log(params);
+  await s3
+    .getObject(params)
+    .promise()
+    .then((data) => {
+      existingPdfBytes = data.Body.buffer;
+    })
+    .catch((err) => console.log(err));
+
+  console.log(existingPdfBytes);
+  const pdfDoc = await pdfLib.PDFDocument.load(existingPdfBytes);
+
+  const form = pdfDoc.getForm();
+  const fields = form.getFields();
+  const textField = form.getTextField(fields[0].getName());
+  textField.setText(establishmentName);
+
+  return pdfDoc.save();
+};
 
 const uploadToS3 = async (uploadParams) => {
-    // try {
-    //     const data = await s3Client.send(new AWS.(uploadParams));
-    //     console.log("Success", data);
-    //     return data; // For unit tests.
-    //   } catch (err) {
-    //     console.log("Error", err);
-    //   }
-}
+  s3.putObject(uploadParams)
+    .promise()
+    .then((x) => console.log(x))
+    .catch((err) => console.log(err));
+  // try {
+  //     const data = await s3Client.send(new AWS.(uploadParams));
+  //     console.log("Success", data);
+  //     return data; // For unit tests.
+  //   } catch (err) {
+  //     console.log("Error", err);
+  //   }
+};
 
 router.route('/').get(getCertificate);
 
--- src/app/features/dashboard/asc-wds-certificate/asc-wds-certificate.component.ts
+++ src/app/features/dashboard/asc-wds-certificate/asc-wds-certificate.component.ts
@@ -7,16 +7,13 @@ import { EstablishmentService } from '@core/services/establishment.service';
   templateUrl: './asc-wds-certificate.component.html',
 })
 export class AscWdsCertificateComponent implements OnInit {
-
-  constructor(
-    protected establishmentService: EstablishmentService,
-  )
-  {}
+  constructor(protected establishmentService: EstablishmentService) {}
 
   private defaultFileName = `ASC-WDS+certificate`;
   isDev = environment.dev ?? false;
   public certificateName: string;
   public years: string;
+  public downloadLink;
 
   ngOnInit(): void {
     this.getCertificateName();
@@ -27,24 +24,25 @@ export class AscWdsCertificateComponent implements OnInit {
 
     const establishmentId = this.establishmentService.establishmentId;
 
-    this.certificateName = `${establishmentId}+${this.defaultFileName}+${this.years}.pdf`
-  }
+    this.certificateName = `${establishmentId}+${this.defaultFileName}+${this.years}.pdf`;
+  };
 
   private getYears = () => {
     const date = new Date();
     const currentMonth = date.getMonth();
     const currentYear = date.getFullYear() - 2000;
 
-    if(currentMonth >= 4) {
-      return `${currentYear}-${currentYear+1}`
+    if (currentMonth >= 4) {
+      return `${currentYear}-${currentYear + 1}`;
     } else {
-      return `${currentYear-1}-${currentYear}`
+      return `${currentYear - 1}-${currentYear}`;
     }
-  }
+  };
 
-  downloadCertificate(): void {
+  async downloadCertificate(): Promise<any> {
     console.log(this.establishmentService.establishmentId);
-    this.establishmentService.getCertificate(this.establishmentService.establishmentId);
+    this.establishmentService
+      .getCertificate(this.establishmentService.establishmentId)
+      .subscribe((x) => window.open(x.data));
   }
-
 }
