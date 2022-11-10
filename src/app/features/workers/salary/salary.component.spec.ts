9d5c3cfc2e7bb13feda76ca09105d9f592304972
popey2700 <alex.pope@madetech.com>
2022-11-10 11:23:02 +0000

    feat(seniorManagementMaxPayChange): Updated failing test

--- src/app/features/workers/salary/salary.component.spec.ts
+++ src/app/features/workers/salary/salary.component.spec.ts
@@ -284,7 +284,7 @@ describe('SalaryComponent', () => {
       fixture.detectChanges();
       fireEvent.click(getByText('Save and return'));
       fixture.detectChanges();
-      expect(getAllByText('Enter their standard annual salary').length).toEqual(2);
+      expect(getAllByText('Enter their standard annual rate').length).toEqual(2);
     });
 
     it('returns an error if Annual salary is selected and an out of range annual salary is entered when job role is not senior management', async () => {
@@ -299,7 +299,7 @@ describe('SalaryComponent', () => {
       fireEvent.click(getByText('Save and return'));
       fixture.detectChanges();
       expect(true).toBeTruthy;
-      expect(getAllByText('Standard annual salary must be between £500 and £200,000').length).toEqual(2);
+      expect(getAllByText('Standard annual rate must be between £500 and £200,000').length).toEqual(2);
     });
 
     it('returns an error if Annual salary is selected and an out of range annual salary is entered when job role is senior management', async () => {
@@ -318,7 +318,7 @@ describe('SalaryComponent', () => {
       fireEvent.click(getByText('Save and return'));
       fixture.detectChanges();
       expect(true).toBeTruthy;
-      expect(getAllByText('Standard annual salary must be between £500 and £250,000').length).toEqual(2);
+      expect(getAllByText('Standard annual rate must be between £500 and £250,000').length).toEqual(2);
     });
 
     it('returns an error if Annual salary is selected and a decimal number is entered', async () => {
@@ -333,7 +333,7 @@ describe('SalaryComponent', () => {
       fireEvent.click(getByText('Save and continue'));
       fixture.detectChanges();
       expect(true).toBeTruthy;
-      expect(getAllByText('Standard annual salary must not include pence').length).toEqual(2);
+      expect(getAllByText('Standard annual rate must not include pence').length).toEqual(2);
     });
   });
 
