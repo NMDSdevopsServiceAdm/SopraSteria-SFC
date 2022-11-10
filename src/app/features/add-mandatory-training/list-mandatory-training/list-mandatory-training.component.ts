42bfe4c45f083104a7a72ed7958d4c40e48e438b
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-11-10 17:06:50 +0000

    create new component and add the routing for the listing mandatory trainings

---
+++ src/app/features/add-mandatory-training/list-mandatory-training/list-mandatory-training.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-mandatory-training',
+  templateUrl: './list-mandatory-training.component.html',
+})
+export class ListMandatoryTrainingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
