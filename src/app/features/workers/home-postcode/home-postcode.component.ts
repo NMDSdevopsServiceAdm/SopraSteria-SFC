890a78b0ab852c8eefdba28622778182c87ba9d2
Gabriele Bernotaite <gabriele.bernotaite@Gabrieles-MacBook-Pro.local>
2022-11-10 15:35:38 +0000

    Postcode: Changed error message, fixed HTML label, added test, fixed validation

--- src/app/features/workers/home-postcode/home-postcode.component.ts
+++ src/app/features/workers/home-postcode/home-postcode.component.ts
@@ -25,9 +25,12 @@ export class HomePostcodeComponent extends QuestionComponent {
   ) {
     super(formBuilder, router, route, backService, errorSummaryService, workerService, establishmentService);
 
-    this.form = this.formBuilder.group({
-      postcode: [null, this.postcodeValidator],
-    });
+    this.form = this.formBuilder.group(
+      {
+        postcode: [null, this.postcodeValidator],
+      },
+      { updateOn: 'submit' },
+    );
   }
 
   init() {
@@ -48,7 +51,7 @@ export class HomePostcodeComponent extends QuestionComponent {
         type: [
           {
             name: 'validPostcode',
-            message: 'Enter a real postcode.',
+            message: 'Enter a real postcode',
           },
         ],
       },
