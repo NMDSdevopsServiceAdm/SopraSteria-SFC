10fbbf27f8784e859132a30b9f799a76ca8a37c9
Gabriele Bernotaite <gabriele.bernotaite@gabrieles-mbp.home>
2022-11-15 10:48:29 +0000

    Changed the width of the Save button inside summary flow

--- src/app/shared/components/submit-button/submit-button.component.ts
+++ src/app/shared/components/submit-button/submit-button.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-submit-button',
   templateUrl: './submit-button.component.html',
+  styleUrls: ['./submit-button.component.scss'],
 })
 export class SubmitButtonComponent {
   @Input() showSaveAndCancelButton = false;
