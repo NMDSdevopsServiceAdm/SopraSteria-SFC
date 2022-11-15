bfb1795822947ac928f6ceec64d67c39ba61f56e
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-11-15 15:21:59 +0000

    Add functionality to list the mandatory category into table

--- src/app/features/add-mandatory-training/list-mandatory-training/list-mandatory-training.component.ts
+++ src/app/features/add-mandatory-training/list-mandatory-training/list-mandatory-training.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { allMandatoryTrainingCategories, Establishment } from '@core/model/establishment.model';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-mandatory-training',
   templateUrl: './list-mandatory-training.component.html',
 })
 export class ListMandatoryTrainingComponent implements OnInit {
-  constructor() {}
+  private subscriptions: Subscription = new Subscription();
+  public establishment: Establishment;
 
-  ngOnInit(): void {}
+  public existingMandatoryTrainings: allMandatoryTrainingCategories;
+  constructor(public establishmentService: EstablishmentService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.establishment = this.route.parent.snapshot.data.establishment;
+
+    this.subscriptions.add(
+      this.establishmentService.getAllMandatoryTrainings(this.establishment.uid).subscribe((trainings) => {
+        console.log({ trainings });
+
+        this.existingMandatoryTrainings = trainings;
+      }),
+    );
+  }
 }
