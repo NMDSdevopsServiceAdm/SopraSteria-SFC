f0487afb527eb3aec9eadce70fb590c7684dcf3f
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-12-02 09:29:23 +0000

    fix routing for view training component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -69,6 +69,7 @@ import { TabsComponent } from './components/tabs/tabs.component';
 import { TotalStaffPanelComponent } from './components/total-staff-panel/total-staff-panel.component';
 import { TotalStaffComponent } from './components/total-staff/total-staff.component';
 import { TrainingAndQualificationsCategoriesComponent } from './components/training-and-qualifications-categories/training-and-qualifications-categories.component';
+import { ViewTrainingAndQualifications } from './components/training-and-qualifications-categories/view-trainings/view-trainings.component';
 import { TrainingAndQualificationsSummaryComponent } from './components/training-and-qualifications-summary/training-and-qualifications-summary.component';
 import { TrainingAndQualificationsTabComponent } from './components/training-and-qualifications-tab/training-and-qualifications-tab.component';
 import { TrainingInfoPanelComponent } from './components/training-info-panel/training-info-panel.component';
@@ -197,6 +198,7 @@ import { WorkplacePermissionsBearerPipe } from './pipes/workplace-permissions-be
     SelectWorkplaceDropdownFormComponent,
     RegistrationSubmitButtonsComponent,
     NewBackLinkComponent,
+    ViewTrainingAndQualifications,
   ],
   exports: [
     AlertComponent,
@@ -291,6 +293,7 @@ import { WorkplacePermissionsBearerPipe } from './pipes/workplace-permissions-be
     SelectWorkplaceDropdownFormComponent,
     RegistrationSubmitButtonsComponent,
     NewBackLinkComponent,
+    ViewTrainingAndQualifications,
   ],
   providers: [DialogService, TotalStaffComponent, ArticleListResolver, PageResolver],
 })
