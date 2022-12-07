f6cdde0d5df8aba810ad21e445afb622f7c20e5b
popey2700 <alex.pope@madetech.com>
2022-12-07 11:08:41 +0000

    fix(preProdStaffRecordsSnaggingList): Changed how worker and workplace resolvers are working so they dont require a route.paramMap to get an establishmentId

--- src/app/core/resolvers/workplace.resolver.ts
+++ src/app/core/resolvers/workplace.resolver.ts
@@ -9,11 +9,11 @@ export class WorkplaceResolver implements Resolve<any> {
   constructor(private router: Router, private establishmentService: EstablishmentService) {}
 
   resolve(route: ActivatedRouteSnapshot) {
-    return this.establishmentService.getEstablishment(route.paramMap.get('establishmentuid')).pipe(
+    return this.establishmentService.getEstablishment(this.establishmentService.establishmentId).pipe(
       catchError(() => {
         this.router.navigate(['/dashboard'], { fragment: 'workplace' });
         return of(null);
-      })
+      }),
     );
   }
 }
