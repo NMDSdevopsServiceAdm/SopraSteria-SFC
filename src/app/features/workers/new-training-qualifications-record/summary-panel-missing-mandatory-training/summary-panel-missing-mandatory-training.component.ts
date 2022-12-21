cc23ef38abb57427fe7823f31c517516b3d2d262
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2022-12-21 13:19:18 +0000

    Add new component for missing mandatory panel

---
+++ src/app/features/workers/new-training-qualifications-record/summary-panel-missing-mandatory-training/summary-panel-missing-mandatory-training.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Establishment } from '@core/model/establishment.model';
+import { BackLinkService } from '@core/services/backLink.service';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { TrainingCategoryService } from '@core/services/training-category.service';
+import { TrainingStatusService } from '@core/services/trainingStatus.service';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-summary-panel-missing-mandatory-training',
+  templateUrl: './summary-panel-missing-mandatory-training.component.html',
+})
+export class SummaryPanelMissingMandatoryTraining implements OnInit {
+  public workplace: Establishment;
+  public category: any;
+  public canEditWorker = false;
+  public trainingCategoryId;
+  private subscriptions: Subscription = new Subscription();
+
+  public trainings;
+
+  constructor(
+    private permissionsService: PermissionsService,
+    public trainingStatusService: TrainingStatusService,
+    private router: Router,
+    private establishmentService: EstablishmentService,
+    protected trainingCategoryService: TrainingCategoryService,
+    protected backLinkService: BackLinkService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.workplace = this.establishmentService.primaryWorkplace;
+    this.canEditWorker = this.permissionsService.can(this.workplace.uid, 'canEditWorker');
+
+    this.trainingCategoryId = this.route.snapshot.params.categoryId;
+    this.getAllTrainingByCategory();
+    this.setBackLink();
+  }
+
+  private getAllTrainingByCategory(): void {
+    this.subscriptions.add(
+      this.trainingCategoryService
+        .getCategoriesWithTraining(this.workplace.id)
+        .pipe(take(1))
+        .subscribe((categories: any) => {
+          this.trainings = categories;
+          console.log({ categories });
+        }),
+    );
+  }
+
+  protected setBackLink(): void {
+    this.backLinkService.showBackLink();
+  }
+
+  public trainingStatus(training) {
+    return this.trainingStatusService.trainingStatusForRecord(training);
+  }
+
+  public returnToHome(): void {
+    this.router.navigate(['/dashboard'], { fragment: 'training-and-qualifications' });
+  }
+}
