1f3718b41fe7072129f7795421addab92981099e
Richard Pentecost <richard.pentecost@madetech.com>
2023-01-11 16:22:49 +0000

    fix intermittent routing issue in total staff change component when pressing cancel

--- src/app/features/workers/total-staff-change/total-staff-change.component.ts
+++ src/app/features/workers/total-staff-change/total-staff-change.component.ts
@@ -42,7 +42,7 @@ export class TotalStaffChangeComponent implements OnInit, OnDestroy, AfterViewIn
 
   ngOnInit() {
     this.workplace = this.route.parent.snapshot.data.establishment;
-    const primaryWorkplaceUid = this.route.snapshot.data.primaryWorkplace?.uid;
+    const primaryWorkplaceUid = this.establishmentService.primaryWorkplace.uid;
 
     this.return =
       this.workplace.uid === primaryWorkplaceUid
