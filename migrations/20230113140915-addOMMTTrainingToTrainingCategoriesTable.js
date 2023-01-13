3bf67483e21b930dfce0d978b0a672374c26a040
Richard Pentecost <richard.pentecost@madetech.com>
2023-01-13 14:28:35 +0000

    change the seq numbers of new training in the migration so they are sorted correctly in the frontend

--- migrations/20230113140915-addOMMTTrainingToTrainingCategoriesTable.js
+++ migrations/20230113140915-addOMMTTrainingToTrainingCategoriesTable.js
@@ -4,9 +4,9 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query(
       `INSERT INTO cqc."TrainingCategories" ("ID", "Seq", "Category") VALUES
-        (38, 380, 'Oliver McGowan Mandatory Training (elearning)'),
-        (39, 390, 'Oliver McGowan Mandatory Training (Tier 1)'),
-        (40, 400, 'Oliver McGowan Mandatory Training (Tier 2)');`,
+        (38, 284, 'Oliver McGowan Mandatory Training (elearning)'),
+        (39, 285, 'Oliver McGowan Mandatory Training (Tier 1)'),
+        (40, 286, 'Oliver McGowan Mandatory Training (Tier 2)');`,
     );
   },
 
