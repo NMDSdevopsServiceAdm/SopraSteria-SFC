34fb8e4a861ab3d41ec63341fd6fadaf8327623c
Richard Pentecost <richard.pentecost@madetech.com>
2023-01-13 14:16:53 +0000

    create migration for adding OMMT training to Training Category table

---
+++ migrations/20230113140915-addOMMTTrainingToTrainingCategoriesTable.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.query(
+      `INSERT INTO cqc."TrainingCategories" ("ID", "Seq", "Category") VALUES
+        (38, 380, 'Oliver McGowan Mandatory Training (elearning)),
+        (39, 390, 'Oliver McGowan Mandatory Training (Tier 1)),
+        (40, 400, 'Oliver McGowan Mandatory Training (Tier 2));`,
+    );
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.sequelize.query('DELETE FROM cqc."TrainingCategories" WHERE ID IN (38, 39, 40);');
+  },
+};
