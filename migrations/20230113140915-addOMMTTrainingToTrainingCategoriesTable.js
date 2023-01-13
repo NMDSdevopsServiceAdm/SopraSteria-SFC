3dd690821a3360186dbe28d9ab28800551fd5209
Richard Pentecost <richard.pentecost@madetech.com>
2023-01-13 14:19:53 +0000

    fix errors in the migration

--- migrations/20230113140915-addOMMTTrainingToTrainingCategoriesTable.js
+++ migrations/20230113140915-addOMMTTrainingToTrainingCategoriesTable.js
@@ -4,13 +4,13 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query(
       `INSERT INTO cqc."TrainingCategories" ("ID", "Seq", "Category") VALUES
-        (38, 380, 'Oliver McGowan Mandatory Training (elearning)),
-        (39, 390, 'Oliver McGowan Mandatory Training (Tier 1)),
-        (40, 400, 'Oliver McGowan Mandatory Training (Tier 2));`,
+        (38, 380, 'Oliver McGowan Mandatory Training (elearning)'),
+        (39, 390, 'Oliver McGowan Mandatory Training (Tier 1)'),
+        (40, 400, 'Oliver McGowan Mandatory Training (Tier 2)');`,
     );
   },
 
   down: async (queryInterface) => {
-    await queryInterface.sequelize.query('DELETE FROM cqc."TrainingCategories" WHERE ID IN (38, 39, 40);');
+    await queryInterface.sequelize.query('DELETE FROM cqc."TrainingCategories" WHERE "ID" IN (38, 39, 40);');
   },
 };
