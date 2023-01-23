258a1d6d6cf5fd3c34b0102faa0f5c695d6b4ed3
Richard Pentecost <richard.pentecost@madetech.com>
2023-01-23 15:10:35 +0000

    alter messages when no training under staff and training tab, change logic under training to check for total training not total training and qualifications

--- src/app/shared/components/training-and-qualifications-categories/training-and-qualifications-categories.component.ts
+++ src/app/shared/components/training-and-qualifications-categories/training-and-qualifications-categories.component.ts
@@ -12,7 +12,7 @@ import { Subscription } from 'rxjs';
 export class TrainingAndQualificationsCategoriesComponent implements OnInit, OnDestroy {
   @Input() workplace: Establishment;
   @Input() trainingCategories: Array<any>;
-  @Input() totalRecords: number;
+  @Input() totalTraining: number;
 
   public workerDetails = [];
   public workerDetailsLabel = [];
