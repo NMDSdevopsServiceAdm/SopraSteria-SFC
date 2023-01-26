78cdb9649a8a9c6a3da9994fcb5268171ed1bda3
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2023-01-26 16:01:12 +0000

    added validation tests

--- src/app/features/login/login.component.spec.ts
+++ src/app/features/login/login.component.spec.ts
@@ -1,5 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { getTestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import { Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from '@core/services/auth.service';
@@ -9,12 +10,13 @@ import { MockUserService } from '@core/test-utils/MockUserService';
 import { FeatureFlagsService } from '@shared/services/feature-flags.service';
 import { SharedModule } from '@shared/shared.module';
 import { render } from '@testing-library/angular';
+import { throwError } from 'rxjs';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
-  async function setup(isAdmin = false, employerTypeSet = true) {
-    const { fixture, getByText, queryByText, getByTestId } = await render(LoginComponent, {
+  async function setup(isAdmin = false, employerTypeSet = true, isAuthenticated = true) {
+    const { fixture, getAllByText, getByText, queryByText, getByTestId } = await render(LoginComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
       providers: [
         FeatureFlagsService,
@@ -36,14 +38,26 @@ describe('LoginComponent', () => {
     spy.and.returnValue(Promise.resolve(true));
 
     const authService = injector.inject(AuthService) as AuthService;
+    let authSpy;
+    if (isAuthenticated) {
+      authSpy = spyOn(authService, 'authenticate');
+      authSpy.and.callThrough();
+    } else {
+      const mockErrorResponse = new HttpErrorResponse({
+        status: 401,
+        statusText: 'Unauthorized',
+        error: {},
+      });
 
-    const authSpy = spyOn(authService, 'authenticate');
-    authSpy.and.callThrough();
+      const authService = TestBed.inject(AuthService);
+      authSpy = spyOn(authService, 'authenticate').and.returnValue(throwError(mockErrorResponse));
+    }
 
     const component = fixture.componentInstance;
     return {
       component,
       fixture,
+      getAllByText,
       getByText,
       queryByText,
       getByTestId,
@@ -108,5 +122,48 @@ describe('LoginComponent', () => {
     expect(component.form.valid).toBeTruthy();
     expect(authSpy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledWith(['workplace', `mockuid`, 'type-of-employer']);
+  });
+
+  describe('validation', async () => {
+    it('should display enter your username message', async () => {
+      const { component, fixture, getAllByText } = await setup(false, false, false);
+
+      component.form.markAsDirty();
+      component.form.get('password').setValue('1');
+      component.form.get('password').markAsDirty();
+
+      component.onSubmit();
+
+      fixture.detectChanges();
+      expect(getAllByText('Enter your username')).toBeTruthy();
+    });
+
+    it('should display enter your password message', async () => {
+      const { component, fixture, getAllByText } = await setup(false, false, false);
+
+      component.form.markAsDirty();
+      component.form.get('username').setValue('1');
+      component.form.get('username').markAsDirty();
+
+      component.onSubmit();
+
+      fixture.detectChanges();
+      expect(getAllByText('Enter your password')).toBeTruthy();
+    });
+
+    it('should display invalid username/password message', async () => {
+      const { component, fixture, getAllByText } = await setup(false, false, false);
+
+      component.form.markAsDirty();
+      component.form.get('username').setValue('1');
+      component.form.get('username').markAsDirty();
+      component.form.get('password').setValue('1');
+      component.form.get('password').markAsDirty();
+
+      component.onSubmit();
+
+      fixture.detectChanges();
+      expect(getAllByText('Your username or your password is incorrect')).toBeTruthy();
+    });
   });
 });
