e702a3d50e7e7b4c36471ccd3980d52739b038f8
ZuhalAb <zuhal.ayob@skillsforcare.org.uk>
2023-01-30 15:40:22 +0000

    Move the pages from summary page behind a new url

--- src/app/core/services/create-account/create-account.service.ts
+++ src/app/core/services/create-account/create-account.service.ts
@@ -1,3 +1,6 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ADD_USER_API } from '@core/constants/constants';
 import {
   ActivateAccountRequest,
   CreateAccountRequest,
@@ -5,14 +8,11 @@ import {
   ValidateAccountActivationTokenRequest,
   ValidateAccountActivationTokenResponse,
 } from '@core/model/account.model';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { LoginCredentials } from '@core/model/login-credentials.model';
 import { SecurityDetails } from '@core/model/security-details.model';
-import { UserDetails } from '@core/model/userDetails.model';
 import { URLStructure } from '@core/model/url.model';
-import { ADD_USER_API } from '@core/constants/constants';
+import { UserDetails } from '@core/model/userDetails.model';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -22,14 +22,14 @@ export class CreateAccountService {
   public loginCredentials$: BehaviorSubject<LoginCredentials> = new BehaviorSubject(null);
   public securityDetails$: BehaviorSubject<SecurityDetails> = new BehaviorSubject(null);
   public activationComplete$: BehaviorSubject<boolean> = new BehaviorSubject(null);
-  public returnTo$ = new BehaviorSubject<URLStructure>(null);
+  public returnTo$: BehaviorSubject<URLStructure> = new BehaviorSubject<URLStructure>(null);
   public token: string;
 
   constructor(private http: HttpClient) {}
 
   public createAccount(
     establishmentUid: string,
-    requestPayload: CreateAccountRequest
+    requestPayload: CreateAccountRequest,
   ): Observable<CreateAccountResponse> {
     return this.http.post<CreateAccountResponse>(`/api/user/add/establishment/${establishmentUid}`, requestPayload);
   }
@@ -39,7 +39,7 @@ export class CreateAccountService {
   }
 
   public validateAccountActivationToken(
-    requestPayload: ValidateAccountActivationTokenRequest
+    requestPayload: ValidateAccountActivationTokenRequest,
   ): Observable<HttpResponse<ValidateAccountActivationTokenResponse>> {
     return this.http.post<ValidateAccountActivationTokenResponse>('/api/user/validateAddUser', requestPayload, {
       observe: 'response',
