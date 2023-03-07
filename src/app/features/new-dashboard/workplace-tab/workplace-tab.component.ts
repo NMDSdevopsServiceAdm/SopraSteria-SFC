be6347ff8792f1216ee5f85b21b6ac7ff4599917
Richard Pentecost <richard.pentecost@madetech.com>
2023-03-07 10:36:26 +0000

    set staff recruitment flow flag in workplace tab to allow correct behaviour for recruitment and staff benefits questions

--- src/app/features/new-dashboard/workplace-tab/workplace-tab.component.ts
+++ src/app/features/new-dashboard/workplace-tab/workplace-tab.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Establishment } from '@core/model/establishment.model';
 import { URLStructure } from '@core/model/url.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { EstablishmentService } from '@core/services/establishment.service';
 
 @Component({
   selector: 'app-new-workplace-tab',
@@ -13,9 +14,10 @@ export class NewWorkplaceTabComponent implements OnInit {
 
   public summaryReturnUrl: URLStructure = { url: ['/dashboard'], fragment: 'workplace' };
 
-  constructor(private breadcrumbService: BreadcrumbService) {}
+  constructor(private breadcrumbService: BreadcrumbService, private establishmentService: EstablishmentService) {}
 
   ngOnInit(): void {
+    this.establishmentService.setInStaffRecruitmentFlow(false);
     console.log('****');
     // this.breadcrumbService.show(JourneyType.WORKPLACE_TAB);
   }
