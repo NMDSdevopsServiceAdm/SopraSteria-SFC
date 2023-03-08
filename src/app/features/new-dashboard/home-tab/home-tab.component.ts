f5a8676c00762c542a29c5d267cd60ae23a68d8a
Richard Pentecost <richard.pentecost@madetech.com>
2023-03-08 16:53:28 +0000

    remove logs from new components

--- src/app/features/new-dashboard/home-tab/home-tab.component.ts
+++ src/app/features/new-dashboard/home-tab/home-tab.component.ts
@@ -1,15 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-home-tab',
   templateUrl: './home-tab.component.html',
 })
-export class NewHomeTabComponent implements OnInit {
-  constructor(private breadcrumbService: BreadcrumbService) {}
-
-  ngOnInit(): void {
-    console.log('**** home tab ****');
-    // this.breadcrumbService.show(null);
-  }
-}
+export class NewHomeTabComponent {}
