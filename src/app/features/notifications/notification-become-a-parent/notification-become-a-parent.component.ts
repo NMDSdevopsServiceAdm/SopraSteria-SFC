766eb483243824e9280961994ebac109395ff86b
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2023-03-23 18:24:26 +0000

    fixed failing test

--- src/app/features/notifications/notification-become-a-parent/notification-become-a-parent.component.ts
+++ src/app/features/notifications/notification-become-a-parent/notification-become-a-parent.component.ts
@@ -1,12 +1,5 @@
 import { Overlay } from '@angular/cdk/overlay';
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
-import { Establishment } from '@core/model/establishment.model';
-import { BreadcrumbService } from '@core/services/breadcrumb.service';
-import { EstablishmentService } from '@core/services/establishment.service';
-import { NotificationsService } from '@core/services/notifications/notifications.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-notification-become-a-parent',
--- src/app/features/notifications/notification-become-a-parent/notification-become-parent.component.spec.ts
+++ src/app/features/notifications/notification-become-a-parent/notification-become-parent.component.spec.ts
@@ -2,9 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AlertService } from '@core/services/alert.service';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { NotificationsService } from '@core/services/notifications/notifications.service';
+import { WindowRef } from '@core/services/window.ref';
 import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
 import { MockNotificationsService } from '@core/test-utils/MockNotificationsService';
 import { NotificationBecomeAParentComponent } from '@features/notifications/notification-become-a-parent/notification-become-a-parent.component';
@@ -19,6 +21,8 @@ describe('NotificationBecomeAParentComponent', () => {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
       declarations: [NotificationTypePipe],
       providers: [
+        AlertService,
+        WindowRef,
         {
           provide: NotificationsService,
           useFactory: MockNotificationsService.factory(approved),
