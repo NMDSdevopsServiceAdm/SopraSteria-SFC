ea58a9942d0664f7609dda602de5a54db951bcbe
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2023-04-13 13:18:07 +0100

    applied feature flag and  add the green and red cross
    Co-authored-by: Sabrina <ssrome@users.noreply.github.com>

--- src/app/shared/components/new-wdf-tabs/new-wdf-tab.component.ts
+++ src/app/shared/components/new-wdf-tabs/new-wdf-tab.component.ts
@@ -1,17 +1,27 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import * as slugify from 'slugify';
 
 @Component({
   selector: 'app-new-wdf-tab',
   templateUrl: './new-wdf-tab.component.html',
   styleUrls: ['./new-wdf-tab.component.scss'],
 })
-export class WDFTabComponent {
+export class WDFTabComponent implements OnInit {
+  @Input() title;
+  @Input() active = false;
   @Input() alert = false;
   @Input() redAlert = false;
   @Input() greenTick = false;
   @Input() redCross = false;
   @Input() viewWDFData: boolean;
   @Output() handleViewToggle: EventEmitter<boolean> = new EventEmitter();
+
+  public slug: string;
+
+  ngOnInit() {
+    this.slug = slugify.default(this.title, { lower: true });
+    console.log({ viewTrainingByCategory: this.viewWDFData });
+  }
 
   public handleViewChange(event: Event): void {
     event.preventDefault();
