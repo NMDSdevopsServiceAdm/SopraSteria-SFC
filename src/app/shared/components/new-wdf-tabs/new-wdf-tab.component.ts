f9bc443cef2e759145030ce100f642d6ab267a1d
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2023-04-17 11:39:28 +0100

    remove unused logs

--- src/app/shared/components/new-wdf-tabs/new-wdf-tab.component.ts
+++ src/app/shared/components/new-wdf-tabs/new-wdf-tab.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-wdf-tab',
   templateUrl: './new-wdf-tab.component.html',
   styleUrls: ['./new-wdf-tab.component.scss'],
 })
-export class WDFTabComponent implements OnInit {
+export class WDFTabComponent {
   @Input() workplaceGreenTick = false;
   @Input() workplaceAlert = false;
   @Input() workplaceRedCross = false;
@@ -16,12 +16,6 @@ export class WDFTabComponent implements OnInit {
 
   @Input() viewWDFData: boolean;
   @Output() handleViewToggle: EventEmitter<boolean> = new EventEmitter();
-
-  public slug: string;
-
-  ngOnInit() {
-    console.log({ viewTrainingByCategory: this.viewWDFData });
-  }
 
   public handleViewChange(event: Event): void {
     event.preventDefault();
