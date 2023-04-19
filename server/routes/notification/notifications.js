3565c8c9d179792ca3cfdc37c73c48d4d8e89bd8
Jonathan Hardy <Jonathan.Hardy@madetech.com>
2023-04-19 14:31:07 +0100

    added other order conditions

--- server/routes/notification/notifications.js
+++ server/routes/notification/notifications.js
@@ -1,34 +1,32 @@
 const notifications = require('../../data/notifications');
 
-
 class Notifications {
   static async GetByEstablishment(establishmentUid, sort) {
-
     const allUsers = await notifications.getAllUsers(establishmentUid);
 
     const sortOrder = this.getSortOrder(sort);
 
     const params = {
       establishmentUid: establishmentUid,
-      userUids: allUsers.map(x => x.UserUID),
+      userUids: allUsers.map((x) => x.UserUID),
       created: 'created',
       order: sortOrder,
-    }
+    };
 
-    const resp =  await notifications.selectNotificationByEstablishment(params);
+    const resp = await notifications.selectNotificationByEstablishment(params);
     return resp;
   }
 
   static getSortOrder(sort) {
-    if(sort) {
+    if (sort) {
       sort = sort.toUpperCase();
-      switch(sort) {
+      switch (sort) {
         case 'LATEST':
           return 'created DESC';
         case 'UNREAD':
-          return '"';
+          return '"isViewed" ASC';
         case 'READ':
-          return '"';
+          return '"isViewed" DESC';
         default:
           return '';
       }
