0ef31ebf280badff622a987caf07e79266f07025
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2023-05-04 11:03:02 +0100

    add backlink instead of breadcrumb

--- src/app/features/new-dashboard/staff-tab/staff-basic-record/staff-basic-record.component.ts
+++ src/app/features/new-dashboard/staff-tab/staff-basic-record/staff-basic-record.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
+
 import { Establishment } from '@core/model/establishment.model';
 import { Worker } from '@core/model/worker.model';
-import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { BackLinkService } from '@core/services/backLink.service';
+
+import { PermissionsService } from '@core/services/permissions/permissions.service';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-staff-basic-record',
@@ -13,21 +16,46 @@ export class StaffBasicRecord implements OnInit, OnDestroy {
   public workplace: Establishment;
 
   public workerCount: number;
+  public canEditWorker: boolean;
+  public canViewWorker: boolean;
   public workerNotCompleted: Worker[];
 
-  constructor(private router: Router, private breadcrumbService: BreadcrumbService, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    protected backLinkService: BackLinkService,
+    private route: ActivatedRoute,
+    private permissionsService: PermissionsService,
+  ) {}
 
   ngOnInit(): void {
     this.workplace = this.route.snapshot.data.primaryWorkplace;
     const { workersNotCompleted, workerCount } = this.route.snapshot.data.workers;
-    this.workerCount = workerCount;
+    this.workerCount = workersNotCompleted?.length;
+    this.canEditWorker = this.permissionsService.can(this.workplace.uid, 'canEditWorker');
+    this.canViewWorker = this.permissionsService.can(this.workplace.uid, 'canViewWorker');
 
     this.workerNotCompleted = this.getWorkersNotCompleted(workersNotCompleted);
-    this.breadcrumbService.show(JourneyType.STAFF_RECORDS_MANDATORY);
+
+    this.setBackLink();
   }
 
   getWorkersNotCompleted(workersNotCompleted: any) {
     return workersNotCompleted.map((worker: any) => worker);
+  }
+
+  public getWorkerRecordPath(event: Event, worker: Worker) {
+    event.preventDefault();
+    this.router.navigate(['/workplace', this.workplace.uid, 'staff-record', worker.uid, 'staff-record-summary']);
+  }
+
+  public lastUpdated(timestamp: string): string {
+    const lastUpdated: dayjs.Dayjs = dayjs(timestamp);
+    const isToday: boolean = dayjs().isSame(lastUpdated, 'day');
+    return isToday ? 'Today' : lastUpdated.format('D MMMM YYYY');
+  }
+
+  protected setBackLink(): void {
+    this.backLinkService.showBackLink();
   }
 
   public returnToHome(): void {
