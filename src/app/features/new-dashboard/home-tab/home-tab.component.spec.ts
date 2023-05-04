f768f567f597f53b7e5d85963deaa8a7f2a035d9
Sabrina <sabrina.samuel@madetech.com>
2023-05-04 16:20:33 +0100

    add unit test to cover condition to view reports on new home tab

--- src/app/features/new-dashboard/home-tab/home-tab.component.spec.ts
+++ src/app/features/new-dashboard/home-tab/home-tab.component.spec.ts
@@ -139,10 +139,19 @@ describe('NewHomeTabComponent', () => {
 
     describe('Does your data meet WDF requirements link', () => {
       it('should render the link with the correct href', async () => {
-        const { getByText } = await setup();
+        const { getByText, component, fixture } = await setup();
+        component.canViewReports = true;
+        fixture.detectChanges();
         const link = getByText('Does your data meet WDF requirements?');
         expect(link).toBeTruthy();
         expect(link.getAttribute('href')).toEqual('/wdf');
+      });
+
+      it('should not render the link with the correct href when view reports is false', async () => {
+        const { queryByText, component, fixture } = await setup();
+        component.canViewReports = false;
+        fixture.detectChanges();
+        expect(queryByText('Does your data meet WDF requirements?')).toBeFalsy();
       });
     });
 
