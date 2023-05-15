9bd997d3c102cbfb925c4f789f42cd29f7305033
Robert McHugh <robert@madetech.com>
2023-05-15 17:22:53 +0100

    Add new data area component and initial page elements

---
+++ src/app/features/new-dashboard/data-area-tab/data-area-tab.component.ts
@@ -0,0 +1,65 @@
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
+import { BenchmarksResponse, MetricsContent } from '@core/model/benchmarks.model';
+import { Establishment } from '@core/model/establishment.model';
+import { BenchmarksService } from '@core/services/benchmarks.service';
+import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { PdfService } from '@core/services/pdf.service';
+import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { BenchmarksAboutTheDataComponent } from '@shared/components/benchmarks-tab/about-the-data/about-the-data.component';
+
+@Component({
+  selector: 'app-data-area-tab',
+  templateUrl: './data-area-tab.component.html',
+})
+export class DataAreaTabComponent implements OnInit, OnDestroy {
+  @Input() workplace: Establishment;
+  @Input() tilesData: BenchmarksResponse;
+  @ViewChild('aboutData') private aboutData: BenchmarksAboutTheDataComponent;
+
+  public canViewFullBenchmarks: boolean;
+  public payContent = MetricsContent.Pay;
+  public turnoverContent = MetricsContent.Turnover;
+  public qualificationsContent = MetricsContent.Qualifications;
+  public sicknessContent = MetricsContent.Sickness;
+  public viewBenchmarksByCategory = false;
+
+  constructor(
+    private permissionsService: PermissionsService,
+    private breadcrumbService: BreadcrumbService,
+    private pdfService: PdfService,
+    private elRef: ElementRef,
+    protected benchmarksService: BenchmarksService,
+    protected router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.canViewFullBenchmarks = this.permissionsService.can(this.workplace.uid, 'canViewBenchmarks');
+    this.breadcrumbService.show(JourneyType.BENCHMARKS_TAB);
+  }
+
+  public async downloadAsPDF() {
+    return await this.pdfService.BuildBenchmarksPdf(
+      this.elRef,
+      this.aboutData.aboutData,
+      this.workplace,
+      'Benchmarks.pdf',
+    );
+  }
+
+  public setReturn(): void {
+    this.benchmarksService.setReturnTo({
+      url: [this.router.url.split('#')[0]],
+      fragment: 'benchmarks',
+    });
+  }
+
+  public handleViewBenchmarksByCategory(visible: boolean): void {
+    this.viewBenchmarksByCategory = visible;
+  }
+
+  ngOnDestroy(): void {
+    this.breadcrumbService.removeRoutes();
+  }
+}
---
+++ src/app/shared/components/benchmarks-select-view-panel/benchmarks-select-view-panel.component.spec.ts
@@ -0,0 +1,50 @@
+import { render } from '@testing-library/angular';
+import { spy } from 'sinon';
+
+import { BenchmarksSelectViewPanelComponent } from './benchmarks-select-view-panel.component';
+
+describe('BenchmarksSelectViewPanelComponent', () => {
+  async function setup() {
+    const { fixture, getByText, getByTestId } = await render(BenchmarksSelectViewPanelComponent, {
+      imports: [],
+      declarations: [],
+      providers: [],
+      componentProperties: {
+        viewBenchmarksByCategory: false,
+        handleViewToggle: {
+          emit: spy(),
+        } as any,
+      },
+    });
+
+    const component = fixture.componentInstance;
+    const toggleViewSpy = spyOn(component.handleViewToggle, 'emit').and.callThrough();
+
+    return {
+      component,
+      fixture,
+      getByText,
+      getByTestId,
+      toggleViewSpy,
+    };
+  }
+
+  it('should render the component', async () => {
+    const { component } = await setup();
+    expect(component).toBeTruthy();
+  });
+
+  it('should show the pay link active when viewBenchmarksByCategory is false', async () => {
+    const { getByText, getByTestId } = await setup();
+
+    const payListItem = getByTestId('payListItem');
+    const payLink = getByText('Pay');
+    const recruitmentListItem = getByTestId('recruitmentListItem');
+    const recruitmentLink = getByText('Recruitment and retention');
+
+    expect(payListItem.getAttribute('class')).toContain('asc-tabs__list-item--active');
+    expect(payLink.getAttribute('class')).toContain('asc-tabs__link--active');
+    expect(recruitmentListItem.getAttribute('class')).not.toContain('asc-tabs__list-item--active');
+    expect(recruitmentLink.getAttribute('class')).not.toContain('asc-tabs__link--active');
+  });
+});
