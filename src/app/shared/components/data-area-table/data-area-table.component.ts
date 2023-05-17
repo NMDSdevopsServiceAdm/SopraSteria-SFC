4fa97475c9bccb4a0e7fe23f48a4087385eb225b
Sabrina <sabrina.samuel@madetech.com>
2023-05-17 12:07:38 +0100

    Add comparision table to data area

---
+++ src/app/shared/components/data-area-table/data-area-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-area-table',
+  templateUrl: './data-area-table.component.html',
+  styleUrls: ['./data-area-table.component.scss'],
+})
+export class DataAreaTableComponent {}
