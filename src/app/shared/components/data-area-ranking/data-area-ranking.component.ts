8d537e4ddebd263ab52f8bfc1170dec0119f3aa0
Robert McHugh <robert@madetech.com>
2023-05-17 17:08:46 +0100

    Update data-area-ranking component to render highchart ranking gauge

--- src/app/shared/components/data-area-ranking/data-area-ranking.component.ts
+++ src/app/shared/components/data-area-ranking/data-area-ranking.component.ts
@@ -1,12 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+import { GaugeOptionsBuilder } from './data-area-ranking-options-builder';
 
 @Component({
   selector: 'app-data-area-ranking',
   templateUrl: './data-area-ranking.component.html',
   styleUrls: ['./data-area-ranking.component.scss'],
 })
-export class DataAreaRankingComponent {
+export class DataAreaRankingComponent implements OnInit {
+  Highcharts: typeof Highcharts = Highcharts;
   @Input() rankingTitle: string;
   @Input() workplaceRankNumber: number;
   @Input() workplacesNumber: number;
+
+  public options: Highcharts.Options;
+
+  constructor(private builder: GaugeOptionsBuilder) {}
+
+  ngOnInit() {
+    this.options = this.builder.buildChartOptions(this.workplacesNumber, this.workplaceRankNumber);
+  }
 }
