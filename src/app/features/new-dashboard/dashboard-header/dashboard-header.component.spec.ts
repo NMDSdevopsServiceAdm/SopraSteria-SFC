e30168ee59e1eac19f57f5fb17e5069a3ce337ae
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2023-05-24 15:23:05 +0100

    Add delete workplace for admin

--- src/app/features/new-dashboard/dashboard-header/dashboard-header.component.spec.ts
+++ src/app/features/new-dashboard/dashboard-header/dashboard-header.component.spec.ts
@@ -1,13 +1,30 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { render, within } from '@testing-library/angular';
+import { AuthService } from '@core/services/auth.service';
+import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { UserService } from '@core/services/user.service';
+import { WindowToken } from '@core/services/window';
+import { WindowRef } from '@core/services/window.ref';
+import { MockAuthService } from '@core/test-utils/MockAuthService';
 
 import { NewDashboardHeaderComponent } from './dashboard-header.component';
+import { MockPermissionsService } from '@core/test-utils/MockPermissionsService';
+import { HttpClient } from '@angular/common/http';
+import { MockUserService } from '@core/test-utils/MockUserService';
+import { Roles } from '@core/model/roles.enum';
+const MockWindow = {
+  dataLayer: {
+    push: () => {
+      return;
+    },
+  },
+};
 
 describe('NewDashboardHeaderComponent', () => {
   const setup = async (
@@ -16,7 +33,10 @@ describe('NewDashboardHeaderComponent', () => {
     canAddWorker = true,
     canEditWorker = false,
     hasWorkers = true,
+    isAdmin = true,
+    subsidiaries = 0,
   ) => {
+    const role = isAdmin ? Roles.Admin : Roles.Edit;
     const updatedDate = updateDate ? '01/02/2023' : null;
     const { fixture, getByTestId, queryByTestId, getByText, queryByText } = await render(NewDashboardHeaderComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
@@ -25,6 +45,26 @@ describe('NewDashboardHeaderComponent', () => {
           provide: EstablishmentService,
           useClass: MockEstablishmentService,
         },
+        {
+          provide: WindowRef,
+          useClass: WindowRef,
+        },
+        {
+          provide: PermissionsService,
+          useFactory: MockPermissionsService.factory([], isAdmin),
+          deps: [HttpClient, Router, UserService],
+        },
+        {
+          provide: UserService,
+          useFactory: MockUserService.factory(subsidiaries, role),
+          deps: [HttpClient],
+        },
+        {
+          provide: AuthService,
+          useFactory: MockAuthService.factory(true, isAdmin),
+          deps: [HttpClient, Router, EstablishmentService, UserService, PermissionsService],
+        },
+        { provide: WindowToken, useValue: MockWindow },
       ],
       componentProperties: {
         tab,
@@ -259,4 +299,67 @@ describe('NewDashboardHeaderComponent', () => {
       expect(column2.getAttribute('class')).toContain('govuk-grid-column-one-third');
     });
   });
+  // describe('Archive Workplace', () => {
+  //   it('should display a Delete Workplace link if user is an admin', async () => {
+  //     const { component } = await setup(true);
+
+  //     component.getByText('Delete Workplace');
+  //   });
+
+  //   it('should not display a Delete Workplace link if the workplace has subsidiaries', async () => {
+  //     const { component } = await setup(true, 1);
+
+  //     expect(component.queryByText('Delete Workplace')).toBeNull();
+  //   });
+
+  //   it('should not display a Delete Workplace link if user not an admin', async () => {
+  //     const { component } = await setup(false);
+
+  //     expect(component.queryByText('Delete Workplace')).toBeNull();
+  //   });
+
+  //   it('should display a modal when the user clicks on Delete Workplace', async () => {
+  //     const { component } = await setup(true);
+
+  //     const deleteWorkplace = component.getByText('Delete Workplace');
+  //     deleteWorkplace.click();
+
+  //     const dialog = await within(document.body).findByRole('dialog');
+
+  //     const cancel = within(dialog).getByText('Cancel');
+  //     cancel.click();
+  //   });
+
+  //   it('should send a DELETE request once the user confirms to Delete Workplace', async () => {
+  //     const { component, establishmentService } = await setup(true);
+
+  //     const spy = spyOn(establishmentService, 'deleteWorkplace').and.returnValue(of({}));
+
+  //     const deleteWorkplace = component.getByText('Delete Workplace');
+  //     deleteWorkplace.click();
+
+  //     const dialog = await within(document.body).findByRole('dialog');
+  //     const confirm = within(dialog).getByText('Delete workplace');
+  //     confirm.click();
+
+  //     expect(spy).toHaveBeenCalled();
+  //   });
+
+  //   it('should redirect the user after deleting a workplace', async () => {
+  //     const { component, establishmentService, router } = await setup(true);
+
+  //     spyOn(establishmentService, 'deleteWorkplace').and.returnValue(of({}));
+  //     const spy = spyOn(router, 'navigate');
+  //     spy.and.returnValue(Promise.resolve(true));
+
+  //     const deleteWorkplace = component.getByText('Delete Workplace');
+  //     deleteWorkplace.click();
+
+  //     const dialog = await within(document.body).findByRole('dialog');
+  //     const confirm = within(dialog).getByText('Delete workplace');
+  //     confirm.click();
+
+  //     expect(spy).toHaveBeenCalled();
+  //   });
+  // });
 });
--- src/app/features/new-dashboard/dashboard-header/dashboard-header.component.ts
+++ src/app/features/new-dashboard/dashboard-header/dashboard-header.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Establishment } from '@core/model/establishment.model';
+import { AuthService } from '@core/services/auth.service';
+import { DialogService } from '@core/services/dialog.service';
 import { EstablishmentService } from '@core/services/establishment.service';
+import { PermissionsService } from '@core/services/permissions/permissions.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { AlertService } from '@core/services/alert.service';
+import { DeleteWorkplaceDialogComponent } from '@features/workplace/delete-workplace-dialog/delete-workplace-dialog.component';
+import { UserService } from '@core/services/user.service';
 
 @Component({
   selector: 'app-new-dashboard-header',
@@ -8,6 +17,7 @@ import { EstablishmentService } from '@core/services/establishment.service';
   styleUrls: ['./dashboard-header.component.scss'],
 })
 export class NewDashboardHeaderComponent implements OnInit {
+  private subscriptions: Subscription = new Subscription();
   @Input() tab: string;
   @Input() canAddWorker = false;
   @Input() updatedDate: string;
@@ -16,6 +26,9 @@ export class NewDashboardHeaderComponent implements OnInit {
   @Input() hasWorkers = false;
 
   public workplace: Establishment;
+  public canDeleteEstablishment: boolean;
+  public workplaceUid: string;
+  public subsidiaryCount: number;
   public showLastUpdatedDate: boolean;
   public tabsMap = {
     workplace: 'Workplace',
@@ -25,11 +38,99 @@ export class NewDashboardHeaderComponent implements OnInit {
   };
   public header: string;
 
-  constructor(private establishmentService: EstablishmentService) {}
+  constructor(
+    private establishmentService: EstablishmentService,
+    private dialogService: DialogService,
+    private permissionsService: PermissionsService,
+    private authService: AuthService,
+    private router: Router,
+    private alertService: AlertService,
+    private userService: UserService,
+  ) {}
 
   ngOnInit(): void {
     this.workplace = this.establishmentService.primaryWorkplace;
+    this.workplaceUid = this.workplace ? this.workplace.uid : null;
     this.getHeader();
+    this.getPermissions();
+
+    if (this.workplace) {
+      this.setSubsidiaryCount();
+    }
+  }
+
+  public onDeleteWorkplace(event: Event): void {
+    event.preventDefault();
+
+    if (!this.canDeleteEstablishment) {
+      return;
+    }
+
+    this.dialogService
+      .open(DeleteWorkplaceDialogComponent, { workplaceName: this.workplace.name })
+      .afterClosed.subscribe((deleteConfirmed) => {
+        if (deleteConfirmed) {
+          this.deleteWorkplace();
+        }
+      });
+  }
+
+  private deleteWorkplace(): void {
+    if (!this.canDeleteEstablishment) {
+      return;
+    }
+
+    this.subscriptions.add(
+      this.establishmentService.deleteWorkplace(this.workplace.uid).subscribe(
+        () => {
+          this.permissionsService.clearPermissions();
+          this.authService.restorePreviousToken();
+
+          if (this.authService.getPreviousToken().EstablishmentUID) {
+            this.establishmentService
+              .getEstablishment(this.authService.getPreviousToken().EstablishmentUID)
+              .pipe(take(1))
+              .subscribe((workplace) => {
+                this.establishmentService.setState(workplace);
+                this.establishmentService.setPrimaryWorkplace(workplace);
+                this.establishmentService.establishmentId = workplace.uid;
+                this.router.navigate(['/search-establishments']).then(() => {
+                  this.alertService.addAlert({
+                    type: 'success',
+                    message: `${this.workplace.name} has been permanently deleted.`,
+                  });
+                });
+              });
+          } else {
+            this.router.navigate(['/search-establishments']).then(() => {
+              this.alertService.addAlert({
+                type: 'success',
+                message: `${this.workplace.name} has been permanently deleted.`,
+              });
+            });
+          }
+        },
+        (e) => {
+          console.error(e);
+          this.alertService.addAlert({
+            type: 'warning',
+            message: 'There was an error deleting the workplace.',
+          });
+        },
+      ),
+    );
+  }
+
+  private getPermissions(): void {
+    this.canDeleteEstablishment = this.permissionsService.can(this.workplaceUid, 'canDeleteAllEstablishments');
+  }
+
+  private setSubsidiaryCount(): void {
+    this.subscriptions.add(
+      this.userService.getEstablishments().subscribe((res) => {
+        this.subsidiaryCount = res.subsidaries ? res.subsidaries.count : 0;
+      }),
+    );
   }
 
   private getHeader(): void {
@@ -38,5 +139,9 @@ export class NewDashboardHeaderComponent implements OnInit {
     } else {
       this.header = this.tabsMap[this.tab];
     }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
