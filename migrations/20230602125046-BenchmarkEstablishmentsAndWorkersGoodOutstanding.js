b190fed2b1be7d8b49c31a1e8cfd68923566aeef
Richard Pentecost <richard.pentecost@madetech.com>
2023-06-02 13:54:01 +0100

    create migration and model for the new benchmarks establishment and workers good outstanding table

---
+++ migrations/20230602125046-BenchmarkEstablishmentsAndWorkersGoodOutstanding.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'BenchmarksEstablishmentsAndWorkersGoodOutstanding',
+      {
+        LocalAuthorityArea: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+        MainServiceFK: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: {
+              tableName: 'services',
+              schema: 'cqc',
+            },
+            key: 'reportingID',
+          },
+        },
+        CQCGoodOutstandingRating: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+        BaseEstablishments: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+        WorkerCount: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+      },
+      { schema: 'cqc' },
+    );
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable({
+      tableName: 'BenchmarksEstablishmentsAndWorkersGoodOutstanding',
+      schema: 'cqc',
+    });
+  },
+};
