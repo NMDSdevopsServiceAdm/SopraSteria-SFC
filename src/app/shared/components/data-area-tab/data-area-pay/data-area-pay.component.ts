fa02d0a36ffb24d27a9598371bce7875a8da69e2
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2023-06-07 12:50:31 +0100

    Create a new component for pay data area

---
+++ src/app/shared/components/data-area-tab/data-area-pay/data-area-pay.component.ts
@@ -0,0 +1,90 @@
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
+import { BenchmarksResponse, MetricsContent } from '@core/model/benchmarks.model';
+import { Establishment } from '@core/model/establishment.model';
+import { BenchmarksService } from '@core/services/benchmarks.service';
+import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { PdfService } from '@core/services/pdf.service';
+import { PermissionsService } from '@core/services/permissions/permissions.service';
+
+import { DataAreaAboutTheDataComponent } from '../about-the-data/about-the-data.component';
+
+@Component({
+  selector: 'app-data-area-pay',
+  templateUrl: './data-area-pay.component.html',
+  styleUrls: ['../data-area-tab.component.scss'],
+})
+export class DataAreaPayComponent implements OnInit, OnDestroy {
+  @Input() workplace: Establishment;
+  @Input() newDashboard: boolean;
+  @ViewChild('aboutData') private aboutData: DataAreaAboutTheDataComponent;
+
+  public canViewFullBenchmarks: boolean;
+  public payContent = MetricsContent.Pay;
+  public turnoverContent = MetricsContent.Turnover;
+  public qualificationsContent = MetricsContent.Qualifications;
+  public sicknessContent = MetricsContent.Sickness;
+  public viewBenchmarksByCategory = false;
+  public viewBenchmarksComparisonGroups = false;
+  public viewBenchmarksPosition = false;
+  public downloadPayBenchmarksText = 'Download pay benchmarks';
+  public tilesData: BenchmarksResponse;
+
+  constructor(
+    private permissionsService: PermissionsService,
+    private breadcrumbService: BreadcrumbService,
+    private pdfService: PdfService,
+    private elRef: ElementRef,
+    protected benchmarksService: BenchmarksService,
+    protected router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.tilesData = this.benchmarksService.benchmarksData;
+    this.canViewFullBenchmarks = this.permissionsService.can(this.workplace.uid, 'canViewBenchmarks');
+    this.breadcrumbService.show(JourneyType.BENCHMARKS_TAB);
+    this.setDownloadBenchmarksText();
+    console.log(this.tilesData);
+  }
+
+  public async downloadAsPDF() {
+    return await this.pdfService.BuildBenchmarksPdf(
+      this.elRef,
+      this.aboutData.aboutData,
+      this.workplace,
+      'Benchmarks.pdf',
+    );
+  }
+
+  public setDownloadBenchmarksText(): void {
+    const pagesPay = '2';
+    const fileSizePay = '430KB';
+    const pagesRecruitment = '2';
+    const fileSizeRecruitment = '385KB';
+    this.downloadPayBenchmarksText = `${this.downloadPayBenchmarksText} (PDF, ${fileSizePay}, ${pagesPay} pages)`;
+  }
+
+  public setReturn(): void {
+    this.benchmarksService.setReturnTo({
+      url: [this.router.url.split('#')[0]],
+      fragment: 'benchmarks',
+    });
+  }
+
+  public handleViewBenchmarksByCategory(visible: boolean): void {
+    this.viewBenchmarksByCategory = visible;
+  }
+
+  public handleViewComparisonGroups(visible: boolean): void {
+    this.viewBenchmarksComparisonGroups = visible;
+  }
+
+  public handleViewBenchmarkPosition(visible: boolean): void {
+    this.viewBenchmarksPosition = visible;
+  }
+
+  ngOnDestroy(): void {
+    this.breadcrumbService.removeRoutes();
+  }
+}
