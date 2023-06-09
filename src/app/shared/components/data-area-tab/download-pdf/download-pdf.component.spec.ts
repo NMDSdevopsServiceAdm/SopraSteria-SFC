b69fa0784e92829d9bf8db6de569ac4150e6f815
Richard Pentecost <richard.pentecost@madetech.com>
2023-06-09 14:57:09 +0100

    reformat the download links to have a line break and align icon when for retention and recruitment, remove typo

--- src/app/shared/components/data-area-tab/download-pdf/download-pdf.component.spec.ts
+++ src/app/shared/components/data-area-tab/download-pdf/download-pdf.component.spec.ts
@@ -4,12 +4,13 @@ import { spy } from 'sinon';
 import { DownloadPdfComponent } from './download-pdf.component';
 
 describe('DownloadPdfComponent', () => {
-  async function setup() {
-    const { fixture, getByText, getByTestId } = await render(DownloadPdfComponent, {
+  const setup = async (viewBenchmarksByCategory = false) => {
+    const { fixture, getByText, getByTestId, queryByTestId } = await render(DownloadPdfComponent, {
       imports: [],
       declarations: [],
       providers: [],
       componentProperties: {
+        viewBenchmarksByCategory,
         downloadPDF: {
           emit: spy(),
         } as any,
@@ -24,17 +25,32 @@ describe('DownloadPdfComponent', () => {
       fixture,
       getByText,
       getByTestId,
+      queryByTestId,
       toggleViewSpy,
     };
-  }
+  };
 
   it('should render the component', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
   });
 
+  it('should render the pay download link when viewBenchmarksByCategory is false', async () => {
+    const { getByTestId, queryByTestId } = await setup();
+
+    expect(getByTestId('payDownloadLink')).toBeTruthy();
+    expect(queryByTestId('rAndRDownloadLink')).toBeFalsy;
+  });
+
+  it('should render the recruiment and retention download link when viewBenchmarksByCategory is true', async () => {
+    const { getByTestId, queryByTestId } = await setup(true);
+
+    expect(getByTestId('rAndRDownloadLink')).toBeTruthy();
+    expect(queryByTestId('payDownloadLink')).toBeFalsy;
+  });
+
   it('should emit printPdf when the download link is clicked', async () => {
-    const { component, getByTestId, toggleViewSpy } = await setup();
+    const { getByTestId, toggleViewSpy } = await setup();
 
     const downloadLink = getByTestId('download-link');
     fireEvent.click(downloadLink);
--- src/app/shared/components/data-area-tab/download-pdf/download-pdf.component.ts
+++ src/app/shared/components/data-area-tab/download-pdf/download-pdf.component.ts
@@ -3,11 +3,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-download-pdf',
   templateUrl: './download-pdf.component.html',
-  styleUrls: ['./download-pdf.component.scss'],
 })
 export class DownloadPdfComponent {
   @Input() linkUrl: string;
-  @Input() linkTitle: string;
+  @Input() viewBenchmarksByCategory: boolean;
   @Output() downloadPDF = new EventEmitter();
 
   public downloadAsPDF(event: Event): void {
