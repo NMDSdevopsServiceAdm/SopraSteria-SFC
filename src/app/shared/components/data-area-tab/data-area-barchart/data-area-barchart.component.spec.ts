ca3ec7779c33c7a9f1d0a0f5d1c44a1d5b9a5741
Robert McHugh <robert@madetech.com>
2023-06-20 15:22:38 +0100

    Add tests for displaying messages depending on data

--- src/app/shared/components/data-area-tab/data-area-barchart/data-area-barchart.component.spec.ts
+++ src/app/shared/components/data-area-tab/data-area-barchart/data-area-barchart.component.spec.ts
@@ -1,18 +1,65 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RankingsResponse } from '@core/model/benchmarks.model';
+import { SharedModule } from '@shared/shared.module';
+import { render } from '@testing-library/angular';
 
 import { DataAreaBarchartComponent } from './data-area-barchart.component';
 
 describe('DataAreaBarchartComponent', () => {
-  let component: DataAreaBarchartComponent;
-  let fixture: ComponentFixture<DataAreaBarchartComponent>;
+  const setup = async () => {
+    const { fixture, getByText, getByTestId, queryByTestId } = await render(DataAreaBarchartComponent, {
+      imports: [SharedModule],
+      providers: [],
+      schemas: [NO_ERRORS_SCHEMA],
+      componentProperties: {
+        rankingsData: {
+          maxRank: 14,
+          currentRank: 7,
+          hasValue: true,
+          allValues: [],
+        } as RankingsResponse,
+      },
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DataAreaBarchartComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    const component = fixture.componentInstance;
+
+    return {
+      component,
+      fixture,
+      getByText,
+      getByTestId,
+      queryByTestId,
+    };
+  };
 
   it('should create', async () => {
+    const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should show the default message when all data is provided', async () => {
+    const { component, queryByTestId } = await setup();
+
+    expect(queryByTestId('all-data')).toBeTruthy();
+  });
+
+  it('should show the no comparison group message when no comparsion group data is provided', async () => {
+    const { component, queryByTestId } = await setup();
+    (component.rankingsData = {
+      stateMessage: 'no-comparison-data',
+      hasValue: false,
+      allValues: [],
+    } as RankingsResponse),
+      expect(queryByTestId('no-comparison-data')).toBeTruthy();
+  });
+
+  it('should show the no workplace data message when no workplace data is provided', async () => {
+    const { component, queryByTestId } = await setup();
+    (component.rankingsData = {
+      stateMessage: 'no-pay-data',
+      hasValue: false,
+      allValues: [],
+    } as RankingsResponse),
+      expect(queryByTestId('no-workplace-data')).toBeTruthy();
   });
 });
