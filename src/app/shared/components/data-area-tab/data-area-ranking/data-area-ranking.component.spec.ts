f22754d5bbfb63f932514e6d700611b13db4a845
Robert McHugh <robert@madetech.com>
2023-06-20 11:16:26 +0100

    Update rankings component to lowercase text in no data message

--- src/app/shared/components/data-area-tab/data-area-ranking/data-area-ranking.component.spec.ts
+++ src/app/shared/components/data-area-tab/data-area-ranking/data-area-ranking.component.spec.ts
@@ -1,4 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
+import { render } from '@testing-library/angular';
 
 import { DataAreaRankingComponent } from './data-area-ranking.component';
 
@@ -6,13 +8,48 @@ describe('DataAreaRankingComponent', () => {
   let component: DataAreaRankingComponent;
   let fixture: ComponentFixture<DataAreaRankingComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DataAreaRankingComponent);
-    component = fixture.componentInstance;
+  const setup = async () => {
+    const { fixture, getByText, getByTestId, queryByTestId } = await render(DataAreaRankingComponent, {
+      imports: [],
+      providers: [],
+      declarations: [],
+      schemas: [NO_ERRORS_SCHEMA],
+      componentProperties: {
+        rankingTitle: 'Test ranking',
+      },
+    });
     fixture.detectChanges();
-  });
+    component = fixture.componentInstance;
+
+    return {
+      component,
+      fixture,
+      getByText,
+      getByTestId,
+      queryByTestId,
+    };
+  };
 
   it('should create', async () => {
+    const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should show rankings when data is provided', async () => {
+    const { fixture, component, queryByTestId, getByTestId } = await setup();
+    component.workplaceRankNumber = 7;
+    component.workplacesNumber = 10;
+    fixture.detectChanges();
+
+    expect(getByTestId('ranking-data')).toBeTruthy();
+  });
+
+  it('should show no workplace data message when no workplace data is provided', async () => {
+    const { fixture, component, queryByTestId, getByTestId } = await setup();
+    component.workplaceRankNumber = null;
+    component.workplacesNumber = 10;
+    fixture.detectChanges();
+
+    expect(getByTestId('no-workplace-data')).toBeTruthy();
   });
 });
