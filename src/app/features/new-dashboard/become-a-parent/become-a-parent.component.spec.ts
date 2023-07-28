418d66a306735dfce29cab4ff9a6efed4f45d1f3
Sabrina <sabrina.samuel@madetech.com>
2023-07-28 17:38:01 +0100

    Add addition content to become a parent

--- src/app/features/new-dashboard/become-a-parent/become-a-parent.component.spec.ts
+++ src/app/features/new-dashboard/become-a-parent/become-a-parent.component.spec.ts
@@ -1,14 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { ParentRequestsService } from '@core/services/parent-requests.service';
 import { BenchmarksService } from '@core/services/benchmarks.service';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
 import { MockBenchmarksService } from '@core/test-utils/MockBenchmarkService';
 import { MockPermissionsService } from '@core/test-utils/MockPermissionsService';
 import { render } from '@testing-library/angular';
-
 import { BecomeAParentComponent } from './become-a-parent.component';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
@@ -17,7 +16,7 @@ import { MockBreadcrumbService } from '@core/test-utils/MockBreadcrumbService';
 import { SharedModule } from '@shared/shared.module';
 import { getTestBed } from '@angular/core/testing';
 
-fdescribe('BecomeAParentComponent', () => {
+describe('BecomeAParentComponent', () => {
   async function setup() {
     const { getByRole, getByText, getByLabelText, getByTestId, fixture } = await render(BecomeAParentComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
@@ -48,6 +47,9 @@ fdescribe('BecomeAParentComponent', () => {
       ],
     });
     const component = fixture.componentInstance;
+
+    const parentsRequestService = TestBed.inject(ParentRequestsService);
+
     const injector = getTestBed();
     const router = injector.inject(Router) as Router;
     const routerSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
@@ -59,6 +61,7 @@ fdescribe('BecomeAParentComponent', () => {
       fixture,
       component,
       routerSpy,
+      parentsRequestService,
     };
   }
   it('should create', async () => {
@@ -81,5 +84,45 @@ fdescribe('BecomeAParentComponent', () => {
     });
 
     expect(heading).toBeTruthy();
+  });
+
+  it('should show the sub heading', async () => {
+    const { getByRole } = await setup();
+
+    const heading = getByRole('heading', {
+      name: /what's a parent workplace\?/i,
+    });
+
+    expect(heading).toBeTruthy();
+  });
+
+  it('should show the reveal', async () => {
+    const { getByTestId } = await setup();
+
+    expect(getByTestId('becomeAParentRevealTitle')).toBeTruthy();
+    expect(getByTestId('becomeAParentRevealText')).toBeTruthy();
+  });
+
+  it('should render the link with the correct href', async () => {
+    const { getByText } = await setup();
+    const link = getByText('Contact us');
+    expect(link).toBeTruthy();
+    expect(link.getAttribute('href')).toEqual('/contact-us');
+  });
+
+  it('should show the parent request button ', async () => {
+    const { getByText } = await setup();
+
+    const parentRequestButton = getByText('Send parent request');
+
+    expect(parentRequestButton).toBeTruthy();
+  });
+
+  it('should show the cancel link', async () => {
+    const { getByText } = await setup();
+
+    const cancelRequestLink = getByText('Cancel');
+
+    expect(cancelRequestLink).toBeTruthy();
   });
 });
--- src/app/features/new-dashboard/become-a-parent/become-a-parent.component.ts
+++ src/app/features/new-dashboard/become-a-parent/become-a-parent.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Establishment } from '@core/model/establishment.model';
 import { EstablishmentService } from '@core/services/establishment.service';
+import { BreadcrumbService } from '@core/services/breadcrumb.service';
+
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-become-a-parent',
   templateUrl: './become-a-parent.component.html',
 })
 export class BecomeAParentComponent implements OnInit {
+  protected subscriptions: Subscription = new Subscription();
   public workplace: Establishment;
 
-  constructor(private establishmentService: EstablishmentService) {}
+  constructor(
+    protected router: Router,
+    protected route: ActivatedRoute,
 
-  ngOnInit(): void {
+    private establishmentService: EstablishmentService,
+  ) {}
+
+  public async ngOnInit(): Promise<void> {
     this.workplace = this.establishmentService.primaryWorkplace;
   }
 }
