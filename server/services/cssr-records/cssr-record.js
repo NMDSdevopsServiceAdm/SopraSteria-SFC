da781c31c817c2297c49ca8c73a361b32ede34a1
Milo <milo.bascombe@ofqual.gov.uk>
2023-09-26 14:59:51 +0100

    Make postcode matching fuzzier

--- server/services/cssr-records/cssr-record.js
+++ server/services/cssr-records/cssr-record.js
@@ -17,29 +17,37 @@ const GetCssrRecordFromPostcode = async (postcode) => {
     ],
   });
 
-  if (!cssrRecord) {
-    //try matching ignoring last character of postcode
-    // The UK postcode consists of five to seven alphanumeric characters
-    cssrRecord = await pcodedata.findOne({
-      where: {
-        postcode: {
-          [Op.like]: postcode.slice(0, -1).substring(0, 7) + '%', // 'SR2 7T%' limit to avoid injection
+  // no match so try nearest authority
+  // TODO Allow to fail
+  // The UK postcode consists of five to seven alphanumeric characters
+  if(postcode.contains(" ")) {
+    let [outwardCode, inwardCode] = postcode.substring(0, 7).split(" "); //limit to avoid injection
+
+    while (!cssrRecord && inwardCode.length > 0) {
+      inwardCode = inwardCode.slice(0, -1);
+      //try matching ignoring last character of postcode
+      cssrRecord = await pcodedata.findOne({
+        where: {
+          postcode: {
+            [Op.like]: `${outwardCode} ${inwardCode}%`,
+          },
         },
-      },
-      include: [
-        {
-          model: cssr,
-          as: 'theAuthority',
-          attributes: ['id', 'name', 'nmdsIdLetter'],
-          // where: {} // ?TODO
-          required: true,
-        },
-      ],
-    });
+        include: [
+          {
+            model: cssr,
+            as: 'theAuthority',
+            attributes: ['id', 'name', 'nmdsIdLetter'],
+            required: true,
+          },
+        ],
+      });
+    }
+  } else {
+    console.error("Postcode: ${postcode} does not contain spaces so invalid!");
   }
 
   if (!cssrRecord) {
-    console.error('Could not obtain CSSR record from postcode');
+    console.error('Could not obtain CSSR record from postcode non local custodian match');
   }
 
   return cssrRecord;
