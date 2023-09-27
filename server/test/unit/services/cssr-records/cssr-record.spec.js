a8a3f04efc8bd5becb1b0cc2268158e92d1f2437
jonathan.hardy <Jonathan.Hardy97@gmail.com>
2023-09-27 14:55:51 +0100

    added initial test for partial matches

--- server/test/unit/services/cssr-records/cssr-record.spec.js
+++ server/test/unit/services/cssr-records/cssr-record.spec.js
@@ -13,19 +13,46 @@ const la = {
 };
 
 describe('/server/services/cssr-records/cssr-record', async () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
   describe('GetCssrRecordFromPostcode', async () => {
     it('should return a cssr record when a matching postcode with a corresponding cssr entry is found', async () => {
       const stubCompleteMatch = sinon.stub(cssrRecordData, 'getCssrRecordCompleteMatch').callsFake(async () => {
         return la;
       });
-      const stubSimilarMatch = sinon.stub(cssrRecordData, 'getCssrRecordWithLikePostcode').callsFake(async () => {
+      const stubPartialMatch = sinon.stub(cssrRecordData, 'getCssrRecordWithLikePostcode').callsFake(async () => {
         console.log('This should not be hit');
       });
 
       const localAuth = await cssrRecord.GetCssrRecordFromPostcode('HD1 1DA');
       expect(localAuth).to.deep.equal(la);
       expect(stubCompleteMatch.calledOnce);
-      expect(stubSimilarMatch.notCalled);
+      expect(stubCompleteMatch.withArgs('HD1 1DA'));
+      expect(stubPartialMatch.notCalled);
+    });
+
+    describe('when no exact match is found', async () => {
+      let stubCompleteMatch;
+      beforeEach(() => {
+        stubCompleteMatch = sinon.stub(cssrRecordData, 'getCssrRecordCompleteMatch').callsFake(async () => {
+          return null;
+        });
+      });
+
+      it('should attempt to find a cssr corresponding to similar postcodes', async () => {
+        const stubPartialMatch = sinon.stub(cssrRecordData, 'getCssrRecordWithLikePostcode').callsFake(async () => {
+          return null;
+        });
+
+        await cssrRecord.GetCssrRecordFromPostcode('HD1 1DA');
+        expect(stubCompleteMatch.calledOnce);
+        expect(stubPartialMatch.calledThrice);
+        expect(stubPartialMatch.calledWith('HD1 1D'));
+        expect(stubPartialMatch.calledWith('HD1 1'));
+        expect(stubPartialMatch.calledWith('HD1'));
+      });
     });
   });
 });
