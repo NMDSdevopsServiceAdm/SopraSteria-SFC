ef8c258b9fd6a58bc5a6f7e9d7b254451b67cfe9
jonathan.hardy <Jonathan.Hardy97@gmail.com>
2023-11-01 14:44:26 +0000

    Added mapping for responses

--- src/app/core/resolvers/rankings.resolver.ts
+++ src/app/core/resolvers/rankings.resolver.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { BenchmarksServiceBase } from '@core/services/benchmarks-base.service';
-import { BenchmarksV2Service } from '@core/services/benchmarks-v2.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { of } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -11,13 +10,14 @@ export class RankingsResolver implements Resolve<any> {
   constructor(private establishmentService: EstablishmentService, private benchmarksService: BenchmarksServiceBase) {}
 
   resolve(route: ActivatedRouteSnapshot) {
+    console.log('*********** RANKINGS RESOLVER ***********');
     const workplaceUid = route.paramMap.get('establishmentuid')
       ? route.paramMap.get('establishmentuid')
       : this.establishmentService.establishmentId;
-
     if (workplaceUid) {
       return this.benchmarksService.getAllRankingData(workplaceUid).pipe(
         tap((rankingsData) => {
+          console.log(rankingsData);
           this.benchmarksService.rankingsData = rankingsData;
         }),
       );
--- src/app/core/services/benchmarks-v2.service.ts
+++ src/app/core/services/benchmarks-v2.service.ts
@@ -3,19 +3,23 @@ import { Injectable } from '@angular/core';
 import {
   AllRankingsResponse,
   BenchmarksResponse,
+  BenchmarkValue,
+  CombinedResponse,
   CompareGroupsRankingsResponse,
   PayRankingsResponse,
+  Tile,
 } from '@core/model/benchmarks-v2.model';
 import { URLStructure } from '@core/model/url.model';
 import { Observable } from 'rxjs';
 import { BenchmarksServiceBase } from './benchmarks-base.service';
+import { Tile as OldTile, BenchmarkValue as OldBenchmarkValue } from '@core/model/benchmarks.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BenchmarksV2Service implements BenchmarksServiceBase {
   private returnToURL: URLStructure;
-  private _benchmarksData$: BenchmarksResponse = null;
+  private _benchmarksData$: CombinedResponse = null;
   private _rankingsData$: AllRankingsResponse = null;
   constructor(private http: HttpClient) {}
 
@@ -27,12 +31,45 @@ export class BenchmarksV2Service implements BenchmarksServiceBase {
     this.returnToURL = returnTo;
   }
 
-  public get benchmarksData(): BenchmarksResponse {
+  private initialiseOldTile(tileData: Tile): OldTile {
+    function initialiseOldBenchmarkValue(benchmarkData: BenchmarkValue): OldBenchmarkValue {
+      return {
+        value: benchmarkData?.value ? benchmarkData.value : 0,
+        stateMessage: benchmarkData?.stateMessage ? benchmarkData.stateMessage : undefined,
+        hasValue: benchmarkData?.hasValue ? benchmarkData.hasValue : false,
+      };
+    }
+
+    return {
+      workplaceValue: initialiseOldBenchmarkValue(tileData.workplaceValue),
+      comparisonGroup: initialiseOldBenchmarkValue(tileData.comparisonGroup),
+      goodCqc: initialiseOldBenchmarkValue(tileData.goodCqc),
+      lowTurnover: initialiseOldBenchmarkValue(tileData.lowTurnover),
+      workplaces: tileData.workplaces ? tileData.workplaces : 0,
+      staff: tileData.staff ? tileData.staff : 0,
+    };
+  }
+
+  public get benchmarksData(): CombinedResponse {
     return this._benchmarksData$;
   }
 
-  public set benchmarksData(benchmarksData) {
-    this._benchmarksData$ = benchmarksData;
+  public set benchmarksData(benchmarksData: any) {
+    this._benchmarksData$ = {
+      newBenchmarks: benchmarksData,
+      oldBenchmarks: {
+        pay: this.initialiseOldTile(benchmarksData.careWorkerPay),
+        sickness: this.initialiseOldTile(benchmarksData.sickness),
+        qualifications: this.initialiseOldTile(benchmarksData.qualifications),
+        turnover: this.initialiseOldTile(benchmarksData.turnoverRate),
+        meta: {
+          workplaces: benchmarksData.meta.workplaces,
+          staff: benchmarksData.meta.staff,
+          lastUpdated: benchmarksData.meta.lastUpdated,
+          localAuthority: benchmarksData.meta.localAuthority,
+        },
+      },
+    };
   }
 
   public get rankingsData(): AllRankingsResponse {
