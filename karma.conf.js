208b3853f03824be3ad07e19c8b90f6ecb051324
Milo <milobascombe@gmail.com>
2023-11-27 15:58:40 +0000

    Karma step debugging and vscode config

--- karma.conf.js
+++ karma.conf.js
@@ -63,10 +63,21 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: !isCI,
     singleRun: isCI,
-    browsers: ['HeadlessChrome'],
+    browsers: ['ChromeHeadlessNoSandbox', 'Chrome'],
     customLaunchers: {
-      HeadlessChrome: {
+      ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--user-data-dir=/tmp/chrome-test-profile',
+          '--disable-web-security',
+          '--remote-debugging-address=0.0.0.0',
+          '--remote-debugging-port=9222',
+        ],
+        debug: true,
+      },
+      Chrome: {
+        base: 'Chrome',
         flags: ['--no-sandbox'],
       },
     },
