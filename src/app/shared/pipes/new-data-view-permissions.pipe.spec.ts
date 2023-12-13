6fe3e864692acb0aea8abf589158cfa3bd9cd85b
Sabrina <sabrina.samuel@madetech.com>
2023-12-13 13:34:48 +0000

    Update new-data-permissions pipe with isParent condition

--- src/app/shared/pipes/new-data-view-permissions.pipe.spec.ts
+++ src/app/shared/pipes/new-data-view-permissions.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { NewDataViewPermissionsPipe } from './new-data-view-permissions.pipe';
+import { DataPermissions } from '@core/model/my-workplaces.model';
 
 describe('NewDataViewPermissionsPipe', () => {
   it('create an instance', () => {
     const pipe = new NewDataViewPermissionsPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('shows the correct text if isParent is true', () => {
+    const pipe = new NewDataViewPermissionsPipe();
+
+    expect(pipe.transform(DataPermissions.Workplace, true)).toEqual('Only their workplace details');
+  });
+
+  it('shows the correct text if isParent is false', () => {
+    const pipe = new NewDataViewPermissionsPipe();
+
+    expect(pipe.transform(DataPermissions.Workplace, false)).toEqual('Only your workplace details');
   });
 });
