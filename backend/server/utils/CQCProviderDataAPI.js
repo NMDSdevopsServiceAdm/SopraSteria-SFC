fce07bec3bc7db73e52e326362c432720fbb398f
Sabrina <sabrina.samuel@madetech.com>
2024-02-08 16:43:36 +0000

    Add util to call cqc providers api

---
+++ backend/server/utils/CQCProviderDataAPI.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+module.exports = {
+  getCQCProviderData: async function (locationID) {
+    const { data } = await axios.get(
+      'https://api.cqc.org.uk/public/v1/providers/' + locationID + '?partnerCode=SkillsForCare',
+    );
+    return data;
+  },
+};
