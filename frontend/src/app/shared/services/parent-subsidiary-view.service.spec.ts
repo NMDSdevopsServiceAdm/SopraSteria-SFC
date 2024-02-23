32fb4bc12d60eb84f910c5bafa8baea6a5acd391
Sabrina <sabrina.samuel@madetech.com>
2024-02-23 16:00:59 +0000

    Fix failing tests

--- frontend/src/app/shared/services/parent-subsidiary-view.service.spec.ts
+++ frontend/src/app/shared/services/parent-subsidiary-view.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ParentSubsidiaryViewService } from './parent-subsidiary-view.service';
 
 describe('ParentSubsidiaryViewService', () => {
   let service: ParentSubsidiaryViewService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ParentSubsidiaryViewService],
+    });
     service = TestBed.inject(ParentSubsidiaryViewService);
   });
 
