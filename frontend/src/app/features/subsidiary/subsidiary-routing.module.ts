95abbafb76de2871ee96e8c7fdb9d47f91451887
jaseera@madetech.com <jaseera@madetech.com>
2024-02-27 15:06:32 +0000

    Add resolvers to the subsidiary routing module

--- frontend/src/app/features/subsidiary/subsidiary-routing.module.ts
+++ frontend/src/app/features/subsidiary/subsidiary-routing.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EditUserPermissionsGuard } from '@core/guards/edit-user-permissions/edit-user-permissions.guard';
-import { ParentGuard } from '@core/guards/parent/parent.guard';
-import { CheckPermissionsGuard } from '@core/guards/permissions/check-permissions/check-permissions.guard';
-import { HasPermissionsGuard } from '@core/guards/permissions/has-permissions/has-permissions.guard';
-import { ChildWorkplacesResolver } from '@core/resolvers/child-workplaces.resolver';
-import { ViewSubsidiaryHomeComponent } from './home/view-subsidiary-home.component';
-import { ViewSubsidiaryWorkplaceComponent } from './workplace/view-subsidiary-workplace.component';
-import { ViewSubsidiaryStaffRecordsComponent } from './staff-records/view-subsidiary-staff-records.component';
-import { ViewSubsidiaryTrainingAndQualificationsComponent } from './training-and-qualifications/view-subsidiary-training-and-qualifications.component';
-import { ViewSubsidiaryBenchmarksComponent } from './benchmarks/view-subsidiary-benchmarks.component';
-import { ViewSubsidiaryWorkplaceUsersComponent } from './workplace-users/view-subsidiary-workplace-users.component';
-
-import { GetMissingCqcLocationsResolver } from '@core/resolvers/getMissingCqcLocations/getMissingCqcLocations.resolver';
+import { ArticleListResolver } from '@core/resolvers/article-list.resolver';
+import { BenchmarksResolver } from '@core/resolvers/benchmarks.resolver';
 import { AllUsersForEstablishmentResolver } from '@core/resolvers/dashboard/all-users-for-establishment.resolver';
+import { RankingsResolver } from '@core/resolvers/rankings.resolver';
 import { SubsidiaryResolver } from '@core/resolvers/subsidiary.resolver';
 import { WorkersResolver } from '@core/resolvers/workers.resolver';
-import { ArticleListResolver } from '@core/resolvers/article-list.resolver';
-import { SubsidiaryWorkerResolver } from '@core/resolvers/subsidiary-worker.resolver';
+
+import { ViewSubsidiaryBenchmarksComponent } from './benchmarks/view-subsidiary-benchmarks.component';
+import { ViewSubsidiaryHomeComponent } from './home/view-subsidiary-home.component';
+import { ViewSubsidiaryStaffRecordsComponent } from './staff-records/view-subsidiary-staff-records.component';
+import {
+  ViewSubsidiaryTrainingAndQualificationsComponent,
+} from './training-and-qualifications/view-subsidiary-training-and-qualifications.component';
+import { ViewSubsidiaryWorkplaceUsersComponent } from './workplace-users/view-subsidiary-workplace-users.component';
+import { ViewSubsidiaryWorkplaceComponent } from './workplace/view-subsidiary-workplace.component';
 
 // eslint-disable-next-line max-len
 const routes: Routes = [
@@ -75,6 +72,9 @@ const routes: Routes = [
     data: { title: 'Benchmarks' },
     resolve: {
       subsidiaryResolver: SubsidiaryResolver,
+      benchmarksResolver: BenchmarksResolver,
+      rankingsResolver: RankingsResolver,
+
     },
   },
   {
