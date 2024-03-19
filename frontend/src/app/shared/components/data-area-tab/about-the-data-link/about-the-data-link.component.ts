502bbe8b6e03e6fab2d2b61fc09f986f5c4a99a4
Duncan Carter <duncancarter93@gmail.com>
2024-03-19 15:26:57 +0000

    Simplify about the data link component

--- frontend/src/app/shared/components/data-area-tab/about-the-data-link/about-the-data-link.component.ts
+++ frontend/src/app/shared/components/data-area-tab/about-the-data-link/about-the-data-link.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { BenchmarksServiceBase } from '@core/services/benchmarks-base.service';
 import { EstablishmentService } from '@core/services/establishment.service';
-import { ParentSubsidiaryViewService } from '@shared/services/parent-subsidiary-view.service';
 
 @Component({
   selector: 'app-about-the-data-link',
@@ -11,19 +8,9 @@ import { ParentSubsidiaryViewService } from '@shared/services/parent-subsidiary-
 export class AboutTheDataLinkComponent implements OnInit {
   public workplaceUid: string;
 
-  constructor(
-    private establishmentService: EstablishmentService,
-    protected benchmarksService: BenchmarksServiceBase,
-    private parentSubsidiaryViewService: ParentSubsidiaryViewService,
-    protected router: Router,
-  ) {}
+  constructor(private establishmentService: EstablishmentService) {}
 
   ngOnInit(): void {
     this.workplaceUid = this.establishmentService ? this.establishmentService.primaryWorkplace.uid : null;
-
-  }
-
-  public setReturn(): void {
-    this.router.navigate(['/workplace', this.workplaceUid, 'data-area', 'about-the-data']);
   }
 }
