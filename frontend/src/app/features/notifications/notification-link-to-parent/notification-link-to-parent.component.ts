882ac647ed2346c744984887b0aad89cf899a7af
Sabrina <sabrina.samuel@madetech.com>
2024-03-26 11:13:39 +0000

    Remove alertmessage after link to parent request actioned

--- frontend/src/app/features/notifications/notification-link-to-parent/notification-link-to-parent.component.ts
+++ frontend/src/app/features/notifications/notification-link-to-parent/notification-link-to-parent.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { Establishment } from '@core/model/establishment.model';
-import { AlertService } from '@core/services/alert.service';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { DialogService } from '@core/services/dialog.service';
 import { EstablishmentService } from '@core/services/establishment.service';
@@ -42,7 +41,6 @@ export class NotificationLinkToParentComponent implements OnInit, OnDestroy {
     private establishmentService: EstablishmentService,
     private router: Router,
     private permissionsService: PermissionsService,
-    private alertService: AlertService,
     private notificationsService: NotificationsService,
     private dialogService: DialogService,
   ) {}
@@ -104,15 +102,7 @@ export class NotificationLinkToParentComponent implements OnInit, OnDestroy {
                         this.permissionsService.setPermissions(this.workplace.uid, hasPermission.permissions);
                         this.establishmentService.setState(workplace);
                         this.establishmentService.setPrimaryWorkplace(workplace);
-                        this.router.navigate(['/dashboard'], {
-                          state: {
-                            alertMessage: `Your decision to link to you has been sent to ${this.notification.typeContent.requestorName} `,
-                          },
-                        });
-                        // this.alertService.addAlert({
-                        //   type: 'success',
-                        //   message: `Your decision to link to you has been sent to ${this.notification.typeContent.requestorName} `,
-                        // });
+                        this.router.navigate(['/dashboard']);
                       }
                     });
                   }
@@ -175,15 +165,7 @@ export class NotificationLinkToParentComponent implements OnInit, OnDestroy {
             this.notificationsService.getAllNotifications(this.workplace.uid).subscribe((notify) => {
               this.notificationsService.notifications = notify.notifications;
             });
-            this.router.navigate(['/dashboard'], {
-              state: {
-                alertMessage: `Your decision to link to you has been sent to ${this.notification.typeContent.requestorName} `,
-              },
-            });
-            // this.alertService.addAlert({
-            //   type: 'success',
-            //   message: `Your decision to link to you has been sent to ${this.notification.typeContent.requestorName} `,
-            // });
+            this.router.navigate(['/dashboard']);
           }
         }),
     );
