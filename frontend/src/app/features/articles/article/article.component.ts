b0e32879e652f9429647b48480c3ee98d47fd9ee
jaseera@madetech.com <jaseera@madetech.com>
2024-03-27 12:10:53 +0000

    Fix news article links

--- frontend/src/app/features/articles/article/article.component.ts
+++ frontend/src/app/features/articles/article/article.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { Article } from '@core/model/article.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { ParentSubsidiaryViewService } from '@shared/services/parent-subsidiary-view.service';
 import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
@@ -14,11 +15,21 @@ import { filter, map } from 'rxjs/operators';
 export class ArticleComponent implements OnInit, OnDestroy {
   public subscriptions = new Subscription();
   public article: Article;
-
-  constructor(private route: ActivatedRoute, private breadcrumbService: BreadcrumbService, private router: Router) {}
+  public workplaceID: string;
+  public isParentSubsidiaryView: boolean;
+  constructor(
+    private route: ActivatedRoute,
+    private breadcrumbService: BreadcrumbService,
+    private router: Router,
+    public parentSubsidiaryViewService: ParentSubsidiaryViewService,
+  ) {}
 
   ngOnInit(): void {
     this.breadcrumbService.show(JourneyType.PAGES_ARTICLES);
+    this.isParentSubsidiaryView = this.parentSubsidiaryViewService.getViewingSubAsParent();
+    this.parentSubsidiaryViewService.getObservableSubsidiaryUid().subscribe((subsidiaryUid) => {
+      this.workplaceID = subsidiaryUid;
+    });
     this.setArticle();
     this.addSubscriptionToUpdateBreadcrumbs();
   }
@@ -33,6 +44,13 @@ export class ArticleComponent implements OnInit, OnDestroy {
         this.article = this.route.snapshot.data.articles?.data[0];
       }),
     );
+  }
+
+  public returnToHome(): Promise<boolean> {
+    if (this.isParentSubsidiaryView) {
+      return this.router.navigate(['subsidiary/home', this.workplaceID]);
+    }
+    return this.router.navigate(['/dashboard']);
   }
 
   addSubscriptionToUpdateBreadcrumbs(): void {
