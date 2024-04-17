e26fbbe3b475e27aa28b87ae41c566bf6a7a6f60
Duncan Carter <duncancarter93@gmail.com>
2024-04-17 16:39:02 +0100

    Update CQC API call to use new endpoint and add subscription key as header

--- backend/server/test/unit/utils/CQCDataAPI.spec.js
+++ backend/server/test/unit/utils/CQCDataAPI.spec.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const axios = require('axios');
+const config = require('../../../config/config.js');
 
 const { getWorkplaceCQCData } = require('../../../../server/utils/CQCDataAPI');
 
@@ -9,15 +10,18 @@ describe('server/utils/CQCDataAPI', async () => {
     sinon.restore();
   });
 
-  const url = 'https://api.cqc.org.uk/public/v1/locations/';
+  const url = 'https://api.service.cqc.org.uk/public/v1/locations/';
 
   const locationId = '1-109009203';
 
-  it('should call the CQC API', async () => {
+  it('should call the CQC API with CQC subscription key as header', async () => {
     const cqcAPIStub = sinon.stub(axios, 'get').returns({ data: true });
+    const cqcSubscriptionKey = config.get('cqcApi.subscriptionKey');
 
     await getWorkplaceCQCData(locationId);
 
-    expect(cqcAPIStub.calledOnceWith(url + locationId + '?partnerCode=SkillsForCare')).to.equal(true);
+    expect(
+      cqcAPIStub.calledOnceWith(url + locationId, { headers: { 'Ocp-Apim-Subscription-Key': cqcSubscriptionKey } }),
+    ).to.equal(true);
   });
 });
--- backend/server/utils/CQCDataAPI.js
+++ backend/server/utils/CQCDataAPI.js
@@ -1,10 +1,14 @@
 const axios = require('axios');
+const config = require('../config/config');
+
+const cqcEndpoint = config.get('cqcApi.url');
+const cqcSubscriptionKey = config.get('cqcApi.subscriptionKey');
 
 module.exports = {
   getWorkplaceCQCData: async function (locationID) {
-    const { data } = await axios.get(
-      'https://api.cqc.org.uk/public/v1/locations/' + locationID + '?partnerCode=SkillsForCare',
-    );
+    const { data } = await axios.get(cqcEndpoint + '/locations/' + locationID, {
+      headers: { 'Ocp-Apim-Subscription-Key': cqcSubscriptionKey },
+    });
 
     return data;
   },
