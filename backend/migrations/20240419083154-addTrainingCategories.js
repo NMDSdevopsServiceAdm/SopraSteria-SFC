e7c028c1c82228dff81c0d759b23701bbad8bc98
Duncan Carter <duncancarter93@gmail.com>
2024-04-19 11:44:31 +0100

    Set IDs to be increments instead of bulk upload mappings

--- backend/migrations/20240419083154-addTrainingCategories.js
+++ backend/migrations/20240419083154-addTrainingCategories.js
@@ -4,21 +4,21 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query(
       `INSERT INTO cqc."TrainingCategories" ("ID", "Seq", "Category") VALUES
-        (44, 0, 'Assisting and moving people'),
-        (46, 0, 'Oral health'),
-        (47, 0, 'Moving and handling objects'),
-        (49, 0, 'Assisted digital and accessibility'),
-        (50, 0, 'Digital leadership skills'),
-        (51, 0, 'In-house systems and applications'),
-        (52, 0, 'Online safety and security'),
-        (53, 0, 'Social media and communications'),
-        (54, 0, 'Working with digital technology');`,
+        (41, 0, 'Assisting and moving people'),
+        (42, 0, 'Oral health'),
+        (43, 0, 'Moving and handling objects'),
+        (44, 0, 'Assisted digital and accessibility'),
+        (45, 0, 'Digital leadership skills'),
+        (46, 0, 'In-house systems and applications'),
+        (47, 0, 'Online safety and security'),
+        (48, 0, 'Social media and communications'),
+        (49, 0, 'Working with digital technology');`,
     );
   },
 
   down: async (queryInterface) => {
     await queryInterface.sequelize.query(
-      'DELETE FROM cqc."TrainingCategories" WHERE "ID" IN (44, 46, 47, 49, 50, 51, 52, 53, 54);',
+      'DELETE FROM cqc."TrainingCategories" WHERE "ID" IN (41, 42, 43, 44, 45, 46, 47, 48, 49);',
     );
   },
 };
