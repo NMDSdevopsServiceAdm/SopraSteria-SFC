cbb60d178092a83f1cb01ea0188d942d618e995a
Duncan Carter <duncancarter93@gmail.com>
2024-04-25 16:06:19 +0100

    Revert migration to no transactions

--- backend/migrations/20240111143932-modifyEstablishmentLastActivity.js
+++ backend/migrations/20240111143932-modifyEstablishmentLastActivity.js
@@ -2,178 +2,144 @@
 
 module.exports = {
   up: async (queryInterface) => {
-    return queryInterface.sequelize.transaction(async (transaction) => {
-      await Promise.all([
-        queryInterface.sequelize.query('DROP MATERIALIZED VIEW IF EXISTS cqc."EstablishmentLastActivity"', {
-          transaction,
-        }),
+    await queryInterface.sequelize.query('DROP MATERIALIZED VIEW IF EXISTS cqc."EstablishmentLastActivity"');
 
-        queryInterface.sequelize.query(
-          `
-            CREATE MATERIALIZED VIEW cqc."EstablishmentLastActivity" AS (
-              SELECT e."EstablishmentID",
-                e."NameValue",
-                e."ParentID",
-                e."IsParent",
-                e."NmdsID",
-                e."DataOwner",
-                e."Address1",
-                e."Town",
-                e."County",
-                e."PostCode",
-                e."LocationID",
-                (
-                  SELECT u."FullNameValue"
-                    FROM cqc."User" u
-                    WHERE u."IsPrimary" = true AND (e."EstablishmentID" = u."EstablishmentID") AND u."Archived" = false
-                    LIMIT 1
-                ) AS "PrimaryUserName",
-                (
-                  SELECT u."EmailValue"
-                    FROM cqc."User" u
-                    WHERE u."IsPrimary" = true AND (e."EstablishmentID" = u."EstablishmentID") AND u."Archived" = false
-                    LIMIT 1
-                ) AS "PrimaryUserEmail",
-                GREATEST(e.updated, (
-                  SELECT w.updated
-                    FROM cqc."Worker" w
-                    WHERE w."EstablishmentFK" = e."EstablishmentID" AND w."Archived" = false
-                    ORDER BY w.updated DESC
-                    LIMIT 1
-                )) AS "LastUpdated",
-                (
-                  SELECT MAX(l."LastLoggedIn")
-                    FROM cqc."User" u
-                    LEFT JOIN cqc."Login" l
-                    ON u."RegistrationID" = l."RegistrationID"
-                    WHERE e."EstablishmentID" = u."EstablishmentID"
-                    LIMIT 1
-                ) as "LastLogin",
-                (
-                  SELECT p."NmdsID"
-                    FROM cqc."Establishment" p
-                    WHERE e."ParentID" = p."EstablishmentID"
-                ) AS "ParentNmdsID"
-                FROM cqc."Establishment" e
-                WHERE e."Archived" = false
-            );
-          `,
-          {
-            transaction,
-          },
-        ),
+    await queryInterface.sequelize.query(`
+      CREATE MATERIALIZED VIEW cqc."EstablishmentLastActivity" AS (
+        SELECT e."EstablishmentID",
+          e."NameValue",
+          e."ParentID",
+          e."IsParent",
+          e."NmdsID",
+          e."DataOwner",
+          e."Address1",
+          e."Town",
+          e."County",
+          e."PostCode",
+          e."LocationID",
+          (
+            SELECT u."FullNameValue"
+              FROM cqc."User" u
+              WHERE u."IsPrimary" = true AND (e."EstablishmentID" = u."EstablishmentID") AND u."Archived" = false
+              LIMIT 1
+          ) AS "PrimaryUserName",
+          (
+            SELECT u."EmailValue"
+              FROM cqc."User" u
+              WHERE u."IsPrimary" = true AND (e."EstablishmentID" = u."EstablishmentID") AND u."Archived" = false
+              LIMIT 1
+          ) AS "PrimaryUserEmail",
+          GREATEST(e.updated, (
+            SELECT w.updated
+              FROM cqc."Worker" w
+              WHERE w."EstablishmentFK" = e."EstablishmentID" AND w."Archived" = false
+              ORDER BY w.updated DESC
+              LIMIT 1
+          )) AS "LastUpdated",
+          (
+            SELECT MAX(l."LastLoggedIn")
+              FROM cqc."User" u
+              LEFT JOIN cqc."Login" l
+              ON u."RegistrationID" = l."RegistrationID"
+              WHERE e."EstablishmentID" = u."EstablishmentID"
+              LIMIT 1
+          ) as "LastLogin",
+          (
+            SELECT p."NmdsID"
+              FROM cqc."Establishment" p
+              WHERE e."ParentID" = p."EstablishmentID"
+          ) AS "ParentNmdsID"
+          FROM cqc."Establishment" e
+          WHERE e."Archived" = false
+      );
+    `);
 
-        queryInterface.addIndex(
-          {
-            tableName: 'EstablishmentLastActivity',
-            schema: 'cqc',
-          },
-          {
-            fields: ['IsParent', 'PrimaryUserEmail', 'LastLogin', 'LastUpdated', 'DataOwner'],
-          },
-          {
-            transaction,
-          },
-        ),
+    await queryInterface.addIndex(
+      {
+        tableName: 'EstablishmentLastActivity',
+        schema: 'cqc',
+      },
+      {
+        fields: ['IsParent', 'PrimaryUserEmail', 'LastLogin', 'LastUpdated', 'DataOwner'],
+      },
+    );
 
-        queryInterface.addIndex(
-          {
-            tableName: 'EstablishmentLastActivity',
-            schema: 'cqc',
-          },
-          {
-            fields: ['IsParent', 'PrimaryUserEmail'],
-          },
-          {
-            transaction,
-          },
-        ),
-      ]);
-    });
+    await queryInterface.addIndex(
+      {
+        tableName: 'EstablishmentLastActivity',
+        schema: 'cqc',
+      },
+      {
+        fields: ['IsParent', 'PrimaryUserEmail'],
+      },
+    );
   },
 
   down: async (queryInterface) => {
-    return queryInterface.sequelize.transaction(async (transaction) => {
-      await Promise.all([
-        queryInterface.sequelize.query('DROP MATERIALIZED VIEW IF EXISTS cqc."EstablishmentLastActivity"', {
-          transaction,
-        }),
+    await queryInterface.sequelize.query('DROP MATERIALIZED VIEW IF EXISTS cqc."EstablishmentLastActivity"');
 
-        queryInterface.sequelize.query(
-          `
-          CREATE MATERIALIZED VIEW cqc."EstablishmentLastActivity" AS (
-            SELECT e."EstablishmentID",
-              e."NameValue",
-              e."ParentID",
-              e."IsParent",
-              e."NmdsID",
-              e."DataOwner",
-              e."Address1",
-              e."Town",
-              e."County",
-              e."PostCode",
-              (
-                SELECT u."FullNameValue"
-                  FROM cqc."User" u
-                  WHERE u."IsPrimary" = true AND (e."EstablishmentID" = u."EstablishmentID") AND u."Archived" = false
-                  LIMIT 1
-              ) AS "PrimaryUserName",
-              (
-                SELECT u."EmailValue"
-                  FROM cqc."User" u
-                  WHERE u."IsPrimary" = true AND (e."EstablishmentID" = u."EstablishmentID") AND u."Archived" = false
-                  LIMIT 1
-              ) AS "PrimaryUserEmail",
-              GREATEST(e.updated, (
-                SELECT w.updated
-                  FROM cqc."Worker" w
-                  WHERE w."EstablishmentFK" = e."EstablishmentID" AND w."Archived" = false
-                  ORDER BY w.updated DESC
-                  LIMIT 1
-              )) AS "LastUpdated",
-              (
-                SELECT MAX(l."LastLoggedIn")
-                  FROM cqc."User" u
-                  LEFT JOIN cqc."Login" l
-                  ON u."RegistrationID" = l."RegistrationID"
-                  WHERE e."EstablishmentID" = u."EstablishmentID"
-                  LIMIT 1
-              ) as "LastLogin"
-              FROM cqc."Establishment" e
-              WHERE e."Archived" = false
-          );
-        `,
-          {
-            transaction,
-          },
-        ),
+    await queryInterface.sequelize.query(`
+      CREATE MATERIALIZED VIEW cqc."EstablishmentLastActivity" AS (
+        SELECT e."EstablishmentID",
+          e."NameValue",
+          e."ParentID",
+          e."IsParent",
+          e."NmdsID",
+          e."DataOwner",
+          e."Address1",
+          e."Town",
+          e."County",
+          e."PostCode",
+          (
+            SELECT u."FullNameValue"
+              FROM cqc."User" u
+              WHERE u."IsPrimary" = true AND (e."EstablishmentID" = u."EstablishmentID") AND u."Archived" = false
+              LIMIT 1
+          ) AS "PrimaryUserName",
+          (
+            SELECT u."EmailValue"
+              FROM cqc."User" u
+              WHERE u."IsPrimary" = true AND (e."EstablishmentID" = u."EstablishmentID") AND u."Archived" = false
+              LIMIT 1
+          ) AS "PrimaryUserEmail",
+          GREATEST(e.updated, (
+            SELECT w.updated
+              FROM cqc."Worker" w
+              WHERE w."EstablishmentFK" = e."EstablishmentID" AND w."Archived" = false
+              ORDER BY w.updated DESC
+              LIMIT 1
+          )) AS "LastUpdated",
+          (
+            SELECT MAX(l."LastLoggedIn")
+              FROM cqc."User" u
+              LEFT JOIN cqc."Login" l
+              ON u."RegistrationID" = l."RegistrationID"
+              WHERE e."EstablishmentID" = u."EstablishmentID"
+              LIMIT 1
+          ) as "LastLogin"
+          FROM cqc."Establishment" e
+          WHERE e."Archived" = false
+      );
+    `);
 
-        queryInterface.addIndex(
-          {
-            tableName: 'EstablishmentLastActivity',
-            schema: 'cqc',
-          },
-          {
-            fields: ['IsParent', 'PrimaryUserEmail', 'LastLogin', 'LastUpdated', 'DataOwner'],
-          },
-          {
-            transaction,
-          },
-        ),
+    await queryInterface.addIndex(
+      {
+        tableName: 'EstablishmentLastActivity',
+        schema: 'cqc',
+      },
+      {
+        fields: ['IsParent', 'PrimaryUserEmail', 'LastLogin', 'LastUpdated', 'DataOwner'],
+      },
+    );
 
-        queryInterface.addIndex(
-          {
-            tableName: 'EstablishmentLastActivity',
-            schema: 'cqc',
-          },
-          {
-            fields: ['IsParent', 'PrimaryUserEmail'],
-          },
-          {
-            transaction,
-          },
-        ),
-      ]);
-    });
+    await queryInterface.addIndex(
+      {
+        tableName: 'EstablishmentLastActivity',
+        schema: 'cqc',
+      },
+      {
+        fields: ['IsParent', 'PrimaryUserEmail'],
+      },
+    );
   },
 };
