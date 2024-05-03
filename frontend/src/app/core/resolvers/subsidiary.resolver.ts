6ff0ca6e7f32820f62731adc0e0fd91369b628bd
Duncan Carter <duncancarter93@gmail.com>
2024-05-03 11:25:21 +0100

    Remove setting workplace in subsidiary resolver as workplace is set in call to setViewingSubAsParent

--- frontend/src/app/core/resolvers/subsidiary.resolver.ts
+++ frontend/src/app/core/resolvers/subsidiary.resolver.ts
@@ -1,30 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { EstablishmentService } from '@core/services/establishment.service';
 import { ParentSubsidiaryViewService } from '@shared/services/parent-subsidiary-view.service';
 import { of } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SubsidiaryResolver implements Resolve<any> {
-  constructor(
-    private parentSubsidiaryViewService: ParentSubsidiaryViewService,
-    private establishmentService: EstablishmentService,
-  ) {}
+  constructor(private parentSubsidiaryViewService: ParentSubsidiaryViewService) {}
 
   resolve(route: ActivatedRouteSnapshot) {
     const subsidiaryUid = route.paramMap.get('establishmentuid');
 
     if (subsidiaryUid) {
       this.parentSubsidiaryViewService.setViewingSubAsParent(subsidiaryUid);
-
-      return this.establishmentService.getEstablishment(subsidiaryUid).pipe(
-        tap((workplace) => {
-          this.establishmentService.setWorkplace(workplace);
-        }),
-      );
     }
 
     return of(null);
