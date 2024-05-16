9dba11f6e0e7b843d8f715dc1be2cab0c79f01f0
Sabrina <sabrina.samuel@madetech.com>
2024-05-16 17:56:18 +0100

    Update previous route service to set the lastSelectedTab

--- frontend/src/app/core/services/previous-route.service.spec.ts
+++ frontend/src/app/core/services/previous-route.service.spec.ts
@@ -14,33 +14,34 @@ describe('PreviousRouteService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should set the previous url', () => {
-    const previousUrl = '/workplace';
-    service.setPreviousTab(previousUrl);
-    expect(service.getPreviousUrl()).toEqual(previousUrl);
+  it('should return the lastSelectedTab if the previousUrl includes `dashboard`', () => {
+    const lastSelectedTab = 'workplace';
+    service.previousUrl = '/dashboard#workplace';
+    service.setLastSelectedTab(lastSelectedTab);
+    expect(service.getPreviousPage()).toEqual('workplace');
   });
 
-  it('should return the second part of the url if there is a hashtag in between', () => {
-    const previousUrl = '/dashboard#workplace';
-    service.setPreviousTab(previousUrl);
+  it('should return the previousUrl if the previousUrl doesn`t include `dashboard`', () => {
+    const lastSelectedTab = 'staff-records';
+    service.previousUrl = '/workplace';
+    service.setLastSelectedTab(lastSelectedTab);
     expect(service.getPreviousPage()).toEqual('workplace');
   });
 
   it('should return the second part of the url if there is a slash in between', () => {
-    const previousUrl = '/workplace/view-all-workplaces';
-    service.setPreviousTab(previousUrl);
+    service.previousUrl = '/workplace/view-all-workplaces';
     expect(service.getPreviousPage()).toEqual('view-all-workplaces');
   });
 
   it('should return null when an empty string is set as previous page', () => {
-    const previousUrl = '';
-    service.setPreviousTab(previousUrl);
+    const lastSelectedTab = '';
+    service.setLastSelectedTab(lastSelectedTab);
     expect(service.getPreviousPage()).toEqual(null);
   });
 
   it('should return null if there is not a previous page', () => {
-    const previousUrl = null;
-    service.setPreviousTab(previousUrl);
-    expect(service.getPreviousPage()).toEqual(previousUrl);
+    const lastSelectedTab = null;
+    service.setLastSelectedTab(lastSelectedTab);
+    expect(service.getPreviousPage()).toEqual(lastSelectedTab);
   });
 });
