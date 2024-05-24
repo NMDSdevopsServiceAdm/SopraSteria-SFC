d37ec36423683fbfa821c39897135650163d5833
jaseera@madetech.com <jaseera@madetech.com>
2024-05-24 09:06:38 +0100

    Use grid in article component to space out the elements better

--- frontend/src/app/features/articles/article/article.component.ts
+++ frontend/src/app/features/articles/article/article.component.ts
@@ -4,6 +4,7 @@ import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { Article } from '@core/model/article.model';
 import { Establishment } from '@core/model/establishment.model';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
+import { EstablishmentService } from '@core/services/establishment.service';
 import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
@@ -17,10 +18,11 @@ export class ArticleComponent implements OnInit, OnDestroy {
   public article: Article;
   public workplace: Establishment;
 
-  constructor(private route: ActivatedRoute, private breadcrumbService: BreadcrumbService, private router: Router) {}
+  constructor(private route: ActivatedRoute, private breadcrumbService: BreadcrumbService, private router: Router,  private establishmentService: EstablishmentService) {}
 
   ngOnInit(): void {
     this.breadcrumbService.show(JourneyType.PAGES_ARTICLES);
+    this.workplace = this.establishmentService.establishment;
     this.setArticle();
     this.addSubscriptionToUpdateBreadcrumbs();
   }
@@ -32,7 +34,6 @@ export class ArticleComponent implements OnInit, OnDestroy {
   setArticle(): void {
     this.subscriptions.add(
       this.route.url.subscribe(() => {
-        this.workplace = this.route.snapshot.data.primaryWorkplace;
         this.article = this.route.snapshot.data.articles?.data[0];
       }),
     );
