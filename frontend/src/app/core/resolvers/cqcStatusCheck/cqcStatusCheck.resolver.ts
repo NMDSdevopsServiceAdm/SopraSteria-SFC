0743fed6e2e09454828df5d211f0bd12894c5ec4
Duncan Carter <duncancarter93@gmail.com>
2024-07-02 17:35:19 +0100

    Use route data instead of setting in service for showing CQC banner

--- frontend/src/app/core/resolvers/cqcStatusCheck/cqcStatusCheck.resolver.ts
+++ frontend/src/app/core/resolvers/cqcStatusCheck/cqcStatusCheck.resolver.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class CqcStatusCheckResolver implements Resolve<any> {
@@ -17,11 +17,6 @@ export class CqcStatusCheckResolver implements Resolve<any> {
           postcode,
           mainService: mainService.name,
         })
-        .pipe(
-          tap(({ cqcStatusMatch }) => {
-            this.establishmentService.setCheckCQCDetailsBanner(cqcStatusMatch === false);
-          }),
-        )
         .pipe(
           catchError(() => {
             return of(null);
