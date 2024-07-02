cb24c6ab00225aa80a52ee4c357f03057925b358
Duncan Carter <duncancarter93@gmail.com>
2024-07-02 17:38:43 +0100

    Remove unneeded activated route and add return type to resolver

--- frontend/src/app/core/resolvers/cqcStatusCheck/cqcStatusCheck.resolver.ts
+++ frontend/src/app/core/resolvers/cqcStatusCheck/cqcStatusCheck.resolver.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve } from '@angular/router';
 import { EstablishmentService } from '@core/services/establishment.service';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
+export class CQCRegistrationStatusResponse {
+  cqcStatusMatch: boolean;
+}
 
 @Injectable()
 export class CqcStatusCheckResolver  {
   constructor(private establishmentService: EstablishmentService) {}
 
-  resolve(route: ActivatedRouteSnapshot) {
+  resolve(): Observable<CQCRegistrationStatusResponse> {
     const { locationId, postcode, mainService } = this.establishmentService.primaryWorkplace;
 
     if (locationId) {
