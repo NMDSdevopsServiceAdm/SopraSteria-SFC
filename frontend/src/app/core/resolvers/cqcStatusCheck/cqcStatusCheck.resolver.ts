c3dffdd81d18c4ad348234b06a14a31df4db1672
Duncan Carter <duncancarter93@gmail.com>
2024-07-03 16:20:14 +0100

    Update resolver to get establishent from params instead of establishment service

--- frontend/src/app/core/resolvers/cqcStatusCheck/cqcStatusCheck.resolver.ts
+++ frontend/src/app/core/resolvers/cqcStatusCheck/cqcStatusCheck.resolver.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
 
 export class CQCRegistrationStatusResponse {
   cqcStatusMatch: boolean;
@@ -12,20 +12,24 @@ export class CQCRegistrationStatusResponse {
 export class CqcStatusCheckResolver  {
   constructor(private establishmentService: EstablishmentService) {}
 
-  resolve(): Observable<CQCRegistrationStatusResponse> {
-    const { locationId, postcode, mainService } = this.establishmentService.establishment;
+  resolve(route: ActivatedRouteSnapshot): Observable<CQCRegistrationStatusResponse> {
+    const workplaceUid = route.paramMap.get('establishmentuid')
+      ? route.paramMap.get('establishmentuid')
+      : this.establishmentService.establishmentId;
 
-    if (locationId) {
-      return this.establishmentService
-        .getCQCRegistrationStatus(locationId, {
-          postcode,
-          mainService: mainService.name,
-        })
-        .pipe(
-          catchError(() => {
-            return of(null);
-          }),
-        );
-    }
+    return this.establishmentService.getEstablishment(workplaceUid).pipe(
+      switchMap(({ locationId, postcode, mainService }) => {
+        if (locationId) {
+          return this.establishmentService.getCQCRegistrationStatus(locationId, {
+            postcode,
+            mainService: mainService.name,
+          });
+        }
+        return of(null);
+      }),
+      catchError(() => {
+        return of(null);
+      }),
+    );
   }
 }
