28a0b4e2faf00030d6c3c24ccb52d26e25d3e236
Duncan Carter <duncancarter93@gmail.com>
2024-07-03 16:21:02 +0100

    Update check CQC details banner to use establishment instead of primary workplace

--- frontend/src/app/shared/components/check-cqc-details/check-cqc-details.component.spec.ts
+++ frontend/src/app/shared/components/check-cqc-details/check-cqc-details.component.spec.ts
@@ -2,7 +2,6 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EstablishmentService } from '@core/services/establishment.service';
-import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
 import { SharedModule } from '@shared/shared.module';
 import { render } from '@testing-library/angular';
 
@@ -10,18 +9,23 @@ import { CheckCQCDetailsComponent } from './check-cqc-details.component';
 
 describe('CheckCQCDetailsComponent', () => {
   const setup = async () => {
+    const locationId = '1-11111111';
+
     const { fixture, getByText } = await render(CheckCQCDetailsComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
       providers: [
         {
           provide: EstablishmentService,
-          useClass: MockEstablishmentService,
+          useValue: {
+            establishment: { locationId },
+          },
         },
       ],
     });
+
     const component = fixture.componentInstance;
 
-    return { component, fixture, getByText };
+    return { component, fixture, getByText, locationId };
   };
 
   it('should render a CheckCQCDetailsComponent', async () => {
@@ -30,10 +34,10 @@ describe('CheckCQCDetailsComponent', () => {
   });
 
   it('should render the link with a href that navigates to the correct cqc page', async () => {
-    const { component, getByText } = await setup();
+    const { getByText, locationId } = await setup();
 
     expect(getByText('Please check your CQC details').getAttribute('href')).toEqual(
-      `https://www.cqc.org.uk/location/${component.locationId}`,
+      `https://www.cqc.org.uk/location/${locationId}`,
     );
   });
 });
