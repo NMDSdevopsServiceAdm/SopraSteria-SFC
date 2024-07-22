c4ac0c6dd328f5ea0fd43e031a7058e6c8011942
Duncan Carter <duncancarter93@gmail.com>
2024-07-22 11:37:22 +0100

    Add tab setting changes with logging to test in staging

--- frontend/src/app/shared/components/new-tabs/new-tabs.component.ts
+++ frontend/src/app/shared/components/new-tabs/new-tabs.component.ts
@@ -37,24 +37,52 @@ export class NewTabsComponent implements OnInit, OnDestroy {
 
     this.selectedTabSubscription();
     this.setTabOnInit();
-    this.trackRouterEventsToSetTabInSubView();
+    this.trackRouterEventsToSetTab();
   }
 
-  private trackRouterEventsToSetTabInSubView(): void {
+  private trackRouterEventsToSetTab(): void {
     this.subscriptions.add(
       this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((route: NavigationEnd) => {
         if (this.isParentViewingSub) {
-          const tabInUrl = this.getTabSlugFromNavigationEvent(route);
+          const tabInUrl = this.getTabSlugFromSubsidiaryUrl(route);
 
           if (tabInUrl) {
             this.tabsService.selectedTab = tabInUrl.slug;
+          }
+        } else {
+          const tabInUrl = this.getTabSlugFromMainDashboardUrl(route);
+          console.log('tabInUrl: ', tabInUrl);
+          console.log('this.tabs[this.currentTab]: ', this.tabs[this.currentTab]);
+
+          if (tabInUrl && this.tabs[this.currentTab].slug !== tabInUrl) {
+            this.tabsService.selectedTab = tabInUrl;
           }
         }
       }),
     );
   }
 
-  public getTabSlugFromNavigationEvent(route: NavigationEnd) {
+  public getTabSlugFromMainDashboardUrl(route: NavigationEnd): string {
+    const url = route.urlAfterRedirects;
+    console.log('****************  INSIDE getTabSlugFromMainDashboardUrl ************************');
+    console.log('url: ', url);
+    const hashIndex = url.indexOf('#');
+    console.log('hashIndex: ', hashIndex);
+    if (hashIndex !== -1 && hashIndex < url.length - 1) {
+      const urlWithoutFragment = url.substring(0, hashIndex);
+      if (!urlWithoutFragment.includes('dashboard')) return null;
+      console.log('urlWithoutFragment: ', urlWithoutFragment);
+
+      const tabSlug = url.substring(hashIndex + 1);
+      console.log('tabSlug: ', tabSlug);
+      console.log('****************  END OF getTabSlugFromMainDashboardUrl ************************');
+
+      return this.tabs.find((tab) => tab.slug === tabSlug) ? tabSlug : null;
+    }
+    return null;
+  }
+
+  public getTabSlugFromSubsidiaryUrl(route: NavigationEnd) {
     const urlArray = route.urlAfterRedirects.split('/').filter((section) => section.length > 0);
     if (urlArray.length > 2) {
       return this.tabs.find((tab) => urlArray[2] === tab.slug);
