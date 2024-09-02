b3cc4040648a59fdb303e26df2a65fde2da27ca9
Duncan Carter <duncancarter93@gmail.com>
2024-09-02 16:05:01 +0100

    Update delete report to include address lines 1 and 3 in address column

--- backend/server/utils/concatenateAddress.js
+++ backend/server/utils/concatenateAddress.js
@@ -1,10 +1,11 @@
-exports.concatenateAddress = function (addressLine1, addressLine2, townAndCity, county) {
-
+exports.concatenateAddress = function (addressLine1, addressLine2, addressLine3, townAndCity, county) {
+  console.log({ addressLine1, addressLine2, townAndCity, county });
   //Remove whitespaces and any non alphanumeric characters and then cast to upper case.
   let concatAddress = '';
 
   if (addressLine1) concatAddress += addressLine1;
   if (addressLine2) concatAddress += ' ' + addressLine2;
+  if (addressLine3) concatAddress += ' ' + addressLine3;
   if (townAndCity) concatAddress += ' ' + townAndCity;
   if (county) concatAddress += ' ' + county;
 
