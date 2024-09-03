fb921a23eadafc537d64ac9a268dcb120b00ee2c
Duncan Carter <duncancarter93@gmail.com>
2024-09-03 10:08:11 +0100

    Update the NHS API endpoints to use existing rate limiting config

--- backend/server/routes/nhsBsaApi/apiDocs.js
+++ backend/server/routes/nhsBsaApi/apiDocs.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const { authLimiter } = require('../../utils/middleware/rateLimitingNHSBSAAPI');
+const { authLimiter } = require('../../utils/middleware/rateLimiting');
 
 const nhsBsaApiDocumentation = (req, res) => {
   try {
