630161ed19fad5b5dedef3b7f6cbdf95a47a7241
Jonathan Hardy <Jonathan.Hardy97@gmail.com>
2024-09-04 15:18:08 +0100

    updated BUDI Ids

--- backend/migrations/20240903124901-addNewQuals.js
+++ backend/migrations/20240903124901-addNewQuals.js
@@ -8,25 +8,25 @@ module.exports = {
     return queryInterface.sequelize.transaction(async (transaction) => {
       const newQuals = {
         "Award": [
-          { id: 139, code: 316, title: "Epilepsy", level: '3' },
-          { id: 140, code: 317, title: "TAQA assessment", level: '3' },
-          { id: 141, code: 318, title: "TAQA internal quality assurance", level: '4' },
+          { id: 139, code: 145, title: "Epilepsy", level: '3' },
+          { id: 140, code: 146, title: "TAQA assessment", level: '3' },
+          { id: 141, code: 147, title: "TAQA internal quality assurance", level: '4' },
         ],
         "Certificate": [
-          { id: 142, code: 319, title: "Falls prevention", level: '2' },
-          { id: 143, code: 320, title: "Nutrition", level: '2' },
-          { id: 144, code: 321, title: "Oral health", level: '2' },
+          { id: 142, code: 148, title: "Falls prevention", level: '2' },
+          { id: 143, code: 149, title: "Nutrition", level: '2' },
+          { id: 144, code: 150, title: "Oral health", level: '2' },
         ],
         "Degree": [
-          { id: 145, code: 322, title: "Occupational therapy degree", level: '6' },
-          { id: 146, code: 323, title: "Physiotherapy degree", level: '6' },
-          { id: 147, code: 324, title: "Any nursing degree", level: '6' },
+          { id: 145, code: 151, title: "Occupational therapy degree", level: '6' },
+          { id: 146, code: 152, title: "Physiotherapy degree", level: '6' },
+          { id: 147, code: 153, title: "Any nursing degree", level: '6' },
         ],
         "Any other qualification": [
-          { id: 148, code: 325, title: "Functional skills in english", level: '1' },
-          { id: 149, code: 326, title: "Functional skills in english", level: '2' },
-          { id: 150, code: 327, title: "Functional skills in maths", level: '1' },
-          { id: 151, code: 328, title: "Functional skills in maths", level: '2' },
+          { id: 148, code: 154, title: "Functional skills in english", level: '1' },
+          { id: 149, code: 155, title: "Functional skills in english", level: '2' },
+          { id: 150, code: 156, title: "Functional skills in maths", level: '1' },
+          { id: 151, code: 157, title: "Functional skills in maths", level: '2' },
         ]
       }
 
