eadc06042d07811c6f8c480bf74ebf555204ff12
Joe Fong <joe.fong@madetech.com>
2024-09-12 13:28:50 +0100

    Add a new model for TrainingCertificates
    Co-authored-by: Sabrina <ssrome@users.noreply.github.com>

---
+++ backend/server/models/trainingCertificates.js
@@ -0,0 +1,64 @@
+/* jshint indent: 2 */
+
+module.exports = function (sequelize, DataTypes) {
+  const TrainingCertificates = sequelize.define(
+    'TrainingCertificates',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        field: '"ID"',
+      },
+      uid: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        unique: true,
+        field: '"UID"',
+      },
+      workerFk: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: '"WorkerFK"',
+      },
+      workerTrainingFk: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: '"WorkerTrainingFK"',
+      },
+      filename: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        field: '"FileName"',
+      },
+      uploadDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: '"UploadDate"',
+      },
+    },
+    {
+      tableName: 'TrainingCertificates',
+      schema: 'cqc',
+      createdAt: false,
+      updatedAt: false,
+    },
+  );
+
+  TrainingCertificates.associate = (models) => {
+    TrainingCertificates.belongsTo(models.worker, {
+      foreignKey: 'workerFk',
+      targetKey: 'id',
+      as: 'worker',
+    });
+
+    TrainingCertificates.belongsTo(models.workerTraining, {
+      foreignKey: 'workerTrainingFk',
+      targetKey: 'id',
+      as: 'workerTraining',
+    });
+  };
+
+  return TrainingCertificates;
+};
