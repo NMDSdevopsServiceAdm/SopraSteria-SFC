d1bc55582581acf3e2600e564a06f9023d48a182
Sabrina <sabrina.samuel@madetech.com>
2024-09-17 10:57:21 +0100

    Move styling into scss and remove comments

--- frontend/src/app/shared/components/certifications-table/certifications-table.component.ts
+++ frontend/src/app/shared/components/certifications-table/certifications-table.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-certifications-table',
   templateUrl: './certifications-table.component.html',
+  styleUrls: ['./certifications-table.component.scss'],
 })
 export class CertificationsTableComponent implements OnInit {
   @Input() certificates: any;
