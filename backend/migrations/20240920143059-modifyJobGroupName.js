1d3bb9f5d7d9d228546fa8b44e833eb340eae1a2
Jonathan Hardy <Jonathan.Hardy97@gmail.com>
2024-09-23 16:47:00 +0100

    renamed and reassigned job role groups

--- backend/migrations/20240920143059-modifyJobGroupName.js
+++ backend/migrations/20240920143059-modifyJobGroupName.js
@@ -1,36 +1,36 @@
 'use strict';
 
+const table = {
+  tableName: 'Job',
+  schema: 'cqc',
+};
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+
     return queryInterface.sequelize.transaction(async (transaction) => {
-      await models.job.update(
-        {
-          jobRoleGroup: 'Managerial and supervisory roles'
-        },
-        {
-          where: {
-            jobRoleGroup: 'Managerial and Supervisory roles'
-          },
-          transaction,
-        },
-      );
-    });
+      queryInterface.removeColumn(table, 'JobRoleGroup', transaction)
+      .then(async () => {
+        await queryInterface.sequelize.query('DROP TYPE IF EXISTS cqc."enum_Job_JobRoleGroup";', transaction);
+      })
+      .then(async () => {
+        await queryInterface.addColumn(table, 'JobRoleGroup', {
+          type: Sequelize.DataTypes.ENUM,
+          values: [
+            'Care providing roles',
+            'Professional and related roles',
+            'Managerial and supervisory roles',
+            'IT, digital and date roles',
+            'Other roles',
+          ],
+          transaction
+        });
+      });
+  });
   },
 
   async down (queryInterface, Sequelize) {
-return queryInterface.sequelize.transaction(async (transaction) => {
-      await models.job.update(
-        {
-          jobRoleGroup: 'Managerial and Supervisory roles'
-        },
-        {
-          where: {
-            jobRoleGroup: 'Managerial and supervisory roles'
-          },
-          transaction,
-        },
-      );
-    });
+
   }
 };
