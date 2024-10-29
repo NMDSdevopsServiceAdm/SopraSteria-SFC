0cc3e75f82d2ffbf6c1731ea282b21667fba032d
Duncan Carter <duncancarter93@gmail.com>
2024-10-29 09:05:47 +0000

    WIP: Update wdf-field-confirmation to to have grey background and not wrap when full screen

--- frontend/src/app/shared/components/wdf-field-confirmation/wdf-field-confirmation.component.ts
+++ frontend/src/app/shared/components/wdf-field-confirmation/wdf-field-confirmation.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-wdf-field-confirmation',
   templateUrl: './wdf-field-confirmation.component.html',
+  styleUrls: ['./wdf-field-confirmation.component.scss'],
 })
 export class WdfFieldConfirmationComponent {
   public confirmButtonClicked = false;
