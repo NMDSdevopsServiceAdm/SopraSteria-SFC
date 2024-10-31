77e526b32ff7cb1db5b67be17d2516e0aace91f6
Joe Fong <joe.fong@madetech.com>
2024-10-31 13:26:37 +0000

    (WIP) adding logic to handle transferStaffRecord

--- backend/server/routes/establishments/bulkUpload/data/workerHeaders.js
+++ backend/server/routes/establishments/bulkUpload/data/workerHeaders.js
@@ -45,9 +45,57 @@ const workerHeaders = [
   'QUALACH03NOTES',
 ];
 
+const workerHeadersWithTransferStaff = [
+  'LOCALESTID',
+  'UNIQUEWORKERID',
+  'TRANSFERSTAFFRECORD',
+  'STATUS',
+  'DISPLAYID',
+  'NINUMBER',
+  'POSTCODE',
+  'DOB',
+  'GENDER',
+  'ETHNICITY',
+  'NATIONALITY',
+  'BRITISHCITIZENSHIP',
+  'COUNTRYOFBIRTH',
+  'YEAROFENTRY',
+  'DISABLED',
+  'CARECERT',
+  'L2CARECERT',
+  'RECSOURCE',
+  'HANDCVISA',
+  'INOUTUK',
+  'STARTDATE',
+  'STARTINSECT',
+  'APPRENTICE',
+  'EMPLSTATUS',
+  'ZEROHRCONT',
+  'DAYSSICK',
+  'SALARYINT',
+  'SALARY',
+  'HOURLYRATE',
+  'MAINJOBROLE',
+  'MAINJRDESC',
+  'CONTHOURS',
+  'AVGHOURS',
+  'NMCREG',
+  'NURSESPEC',
+  'AMHP',
+  'SCQUAL',
+  'NONSCQUAL',
+  'QUALACH01',
+  'QUALACH01NOTES',
+  'QUALACH02',
+  'QUALACH02NOTES',
+  'QUALACH03',
+  'QUALACH03NOTES',
+];
+
 const workerHeadersWithoutCHGUNIQUEWRKIDasArray = workerHeaders.filter((header) => header !== 'CHGUNIQUEWRKID');
 
 exports.workerHeadersWithCHGUNIQUEWRKID = workerHeaders.join(',');
 exports.workerHeadersWithoutCHGUNIQUEWRKID = workerHeaders.filter((header) => header !== 'CHGUNIQUEWRKID').join(',');
+exports.workerHeadersWithTransferStaff = workerHeadersWithTransferStaff;
 exports.getWorkerColumnIndex = (columnName) =>
   workerHeadersWithoutCHGUNIQUEWRKIDasArray.findIndex((header) => header === columnName);
