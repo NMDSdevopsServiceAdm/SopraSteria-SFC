f999a7e867a406bd90d77895b8a1a1ac6310591c
Joe Fong <joe.fong@madetech.com>
2024-11-20 11:55:38 +0000

    extract common parts into a directive for later reuse

--- frontend/src/app/features/workplace/how-many-vacancies/how-many-vacancies.component.ts
+++ frontend/src/app/features/workplace/how-many-vacancies/how-many-vacancies.component.ts
@@ -1,29 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { UntypedFormArray, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 import { UpdateJobsRequest, Vacancy } from '@core/model/establishment.model';
 
-import { Question } from '../question/question.component';
+import { HowManyStartersLeaversVacanciesDirective } from '../vacancies-and-turnover/how-many-starters-leavers-vacancies.directive';
 
 @Component({
   selector: 'app-how-many-vacancies',
-  templateUrl: './how-many-vacancies.component.html',
+  templateUrl: '../vacancies-and-turnover/how-many-starters-leavers-vacancies.html',
 })
-export class HowManyVacanciesComponent extends Question implements OnInit, OnDestroy {
+export class HowManyVacanciesComponent extends HowManyStartersLeaversVacanciesDirective {
   public heading = 'How many current staff vacancies do you have for each job role?';
   public section = 'Vacancies and turnover';
-  public totalVacancies: number = 0;
+  public instruction = 'Only add the number of vacancies for permanent and temporary job roles.';
+  public revealTextContent =
+    'To show DHSC and others how the level of staff vacancies and the number employed affects the sector over time.';
+  public jobRoleType = 'vacancies';
 
   protected localStorageKey = 'updated-vacancies';
-
-  private selectedJobRoles: Array<Vacancy> = [];
-  private minNumber = 1;
-  private maxNumber = 999;
-
-  protected init(): void {
-    this.loadSelectedJobRoles();
-    this.setPreviousRoute();
-    this.setupForm();
-  }
+  protected selectedJobRoles: Array<Vacancy> = [];
 
   public loadSelectedJobRoles(): void {
     try {
@@ -33,7 +26,7 @@ export class HowManyVacanciesComponent extends Question implements OnInit, OnDes
       this.returnToFirstPage();
     }
 
-    if (!(Array.isArray(this.selectedJobRoles) && this.selectedJobRoles?.length)) {
+    if (!Array.isArray(this.selectedJobRoles) || this.selectedJobRoles?.length === 0) {
       this.returnToFirstPage();
     }
   }
@@ -42,101 +35,28 @@ export class HowManyVacanciesComponent extends Question implements OnInit, OnDes
     this.router.navigate(['/workplace', `${this.establishment.uid}`, 'do-you-have-vacancies']);
   }
 
-  private setPreviousRoute(): void {
+  protected setPreviousRoute(): void {
     this.previousRoute = ['/workplace', `${this.establishment.uid}`, 'select-vacancy-job-roles'];
-  }
-
-  protected setupForm(): void {
-    this.form = this.formBuilder.group({
-      vacancyNumbers: this.formBuilder.array([]),
-    });
-    this.selectedJobRoles.forEach((jobRole) => {
-      this.vacancyNumbers.push(
-        this.formBuilder.control(jobRole.total, [
-          Validators.required,
-          Validators.min(this.minNumber),
-          Validators.max(this.maxNumber),
-        ]),
-      );
-    });
-
-    this.subscriptions.add(
-      this.vacancyNumbers.valueChanges.subscribe(() => {
-        this.updateTotalNumber();
-      }),
-    );
-  }
-
-  get vacancyNumbers(): UntypedFormArray {
-    return this.form.get('vacancyNumbers') as UntypedFormArray;
-  }
-
-  updateTotalNumber(): void {
-    const inputValues = this.vacancyNumbers.value as Array<number | null>;
-    this.totalVacancies = inputValues.reduce((total, current) => (current ? total + current : total), 0);
   }
 
   protected generateUpdateProps(): UpdateJobsRequest {
     const updatedVacancies = this.selectedJobRoles.map((job, index) => {
-      const updatedFields: Vacancy = {
+      const fieldsToUpdate: Vacancy = {
         jobId: Number(job.jobId),
-        total: parseInt(this.vacancyNumbers.value[index]),
+        total: parseInt(this.jobRoleNumbers.value[index]),
       };
       if (job.other) {
-        updatedFields.other = job.other;
+        fieldsToUpdate.other = job.other;
       }
-      return updatedFields;
+      return fieldsToUpdate;
     });
 
     return { vacancies: updatedVacancies };
-  }
-
-  protected updateEstablishment(props: UpdateJobsRequest): void {
-    this.subscriptions.add(
-      this.establishmentService.updateJobs(this.establishment.uid, props).subscribe(
-        (data) => this._onSuccess(data),
-        (error) => this.onError(error),
-      ),
-    );
   }
 
   protected onSuccess(): void {
     this.nextRoute = ['/workplace', `${this.establishment.uid}`, 'starters'];
     // TODO: change to 'do-you-have-starters' page after #1560 complete
     this.clearLocalStorageData();
-  }
-
-  protected clearLocalStorageData(): void {
-    localStorage.removeItem(this.localStorageKey);
-  }
-
-  protected setupFormErrorsMap(): void {
-    this.formErrorsMap = [];
-
-    this.vacancyNumbers.controls.forEach((_, index) => {
-      const jobRoleTitle = this.selectedJobRoles[index].title.toLowerCase();
-      this.formErrorsMap.push({
-        item: `vacancyNumbers.${index}`,
-        type: [
-          {
-            name: 'required',
-            message: `Enter the number of vacancies (${jobRoleTitle})`,
-          },
-          {
-            name: 'min',
-            message: `Number of vacancies must be between ${this.minNumber} and ${this.maxNumber} (${jobRoleTitle})`,
-          },
-          {
-            name: 'max',
-            message: `Number of vacancies must be between ${this.minNumber} and ${this.maxNumber} (${jobRoleTitle})`,
-          },
-        ],
-      });
-    });
-  }
-
-  public getFirstErrorMessage(item: string): string {
-    const errorType = Object.keys(this.form.get(item).errors)[0];
-    return this.errorSummaryService.getFormErrorMessage(item, errorType, this.formErrorsMap);
   }
 }
---
+++ frontend/src/app/features/workplace/vacancies-and-turnover/how-many-starters-leavers-vacancies.directive.ts
@@ -0,0 +1,115 @@
+import { Directive, OnDestroy, OnInit } from '@angular/core';
+import { UntypedFormArray, Validators } from '@angular/forms';
+import { Leaver, Starter, UpdateJobsRequest, Vacancy } from '@core/model/establishment.model';
+
+import { Question } from '../question/question.component';
+
+@Directive({})
+export class HowManyStartersLeaversVacanciesDirective extends Question implements OnInit, OnDestroy {
+  public heading: string;
+  public section: string;
+  public instruction: string;
+  public revealTextContent: string;
+  public jobRoleType: string;
+
+  public totalNumber = 0;
+
+  protected localStorageKey: string;
+  protected selectedJobRoles: Array<Starter | Leaver | Vacancy> = [];
+
+  private minNumberPerJobRole = 1;
+  private maxNumberPerJobRole = 999;
+
+  protected init(): void {
+    this.loadSelectedJobRoles();
+    this.setPreviousRoute();
+    this.setupForm();
+  }
+
+  public loadSelectedJobRoles(): void {}
+
+  protected returnToFirstPage(): void {}
+
+  protected setPreviousRoute(): void {}
+
+  protected setupForm(): void {
+    this.form = this.formBuilder.group({
+      jobRoleNumbers: this.formBuilder.array([]),
+    });
+
+    this.selectedJobRoles.forEach((jobRole) => {
+      const initialValue = jobRole.total ?? '';
+      this.jobRoleNumbers.push(
+        this.formBuilder.control(initialValue, [
+          Validators.required,
+          Validators.min(this.minNumberPerJobRole),
+          Validators.max(this.maxNumberPerJobRole),
+        ]),
+      );
+    });
+
+    this.subscriptions.add(
+      this.jobRoleNumbers.valueChanges.subscribe(() => {
+        this.updateTotalNumber();
+      }),
+    );
+  }
+
+  get jobRoleNumbers(): UntypedFormArray {
+    return this.form.get('jobRoleNumbers') as UntypedFormArray;
+  }
+
+  updateTotalNumber(): void {
+    const inputValues = this.jobRoleNumbers.value as Array<number | null>;
+    this.totalNumber = inputValues.reduce((total, current) => (current ? total + current : total), 0);
+  }
+
+  protected generateUpdateProps(): UpdateJobsRequest {
+    throw new Error('To be implemented at component');
+  }
+
+  protected updateEstablishment(props: UpdateJobsRequest): void {
+    this.subscriptions.add(
+      this.establishmentService.updateJobs(this.establishment.uid, props).subscribe(
+        (data) => this._onSuccess(data),
+        (error) => this.onError(error),
+      ),
+    );
+  }
+
+  protected onSuccess(): void {}
+
+  protected clearLocalStorageData(): void {
+    localStorage.removeItem(this.localStorageKey);
+  }
+
+  protected setupFormErrorsMap(): void {
+    this.formErrorsMap = [];
+
+    this.jobRoleNumbers.controls.forEach((_, index) => {
+      const jobRoleTitle = this.selectedJobRoles[index].title.toLowerCase();
+      this.formErrorsMap.push({
+        item: `jobRoleNumbers.${index}`,
+        type: [
+          {
+            name: 'required',
+            message: `Enter the number of ${this.jobRoleType} (${jobRoleTitle})`,
+          },
+          {
+            name: 'min',
+            message: `Number of ${this.jobRoleType} must be between ${this.minNumberPerJobRole} and ${this.maxNumberPerJobRole} (${jobRoleTitle})`,
+          },
+          {
+            name: 'max',
+            message: `Number of ${this.jobRoleType} must be between ${this.minNumberPerJobRole} and ${this.maxNumberPerJobRole} (${jobRoleTitle})`,
+          },
+        ],
+      });
+    });
+  }
+
+  public getFirstErrorMessage(item: string): string {
+    const errorType = Object.keys(this.form.get(item).errors)[0];
+    return this.errorSummaryService.getFormErrorMessage(item, errorType, this.formErrorsMap);
+  }
+}
