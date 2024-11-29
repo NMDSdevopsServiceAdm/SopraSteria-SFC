25f8051ac6910bc6dfacc0601f035e988796d178
Duncan Carter <duncancarter93@gmail.com>
2024-11-29 09:10:00 +0000

    Create Do you have leavers page using DoYouHaveStartersLeaversVacancies directive

---
+++ frontend/src/app/features/workplace/do-you-have-leavers/do-you-have-leavers.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DoYouHaveStartersLeaversVacanciesDirective } from '@shared/directives/do-you-have-starters-leavers-vacancies/do-you-have-starters-leavers-vacancies.directive';
+
+@Component({
+  selector: 'app-do-you-have-leavers',
+  templateUrl:
+    '../../../shared/directives/do-you-have-starters-leavers-vacancies/do-you-have-starters-leavers-vacancies.component.html',
+})
+export class DoYouHaveLeaversComponent extends DoYouHaveStartersLeaversVacanciesDirective {
+  public heading = 'Have you had any staff leave in the last 12 months?';
+  public hintText = 'We only want to know about leavers who have left permanent and temporary job roles.';
+  public revealText =
+    "To see if the care sector is attracting new workers and see whether DHSC and the government's national and local recruitment plans are working.";
+  public localStorageKey = 'hasLeavers';
+  public valueToUpdate = 'leavers';
+
+  protected setupRoutes(): void {
+    this.previousRoute = ['/workplace', this.establishment?.uid, 'how-many-starters'];
+    this.skipRoute = ['/workplace', `${this.establishment?.uid}`, 'recruitment-advertising-cost'];
+    this.startersLeaversOrVacanciesPageTwo = 'select-leaver-job-roles';
+  }
+
+  protected getDataFromEstablishment(): any {
+    return this.establishment?.leavers;
+  }
+}
