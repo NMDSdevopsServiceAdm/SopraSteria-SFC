117719cc1682bf4fe37bc0ceae1acb9ff0671eb5
Jonathan Hardy <Jonathan.Hardy97@gmail.com>
2024-12-04 11:55:22 +0000

    removed unnecessary duplication in SelectStarterJobRolesComponent

--- frontend/src/app/features/workplace/select-starter-job-roles/select-starter-job-roles.component.ts
+++ frontend/src/app/features/workplace/select-starter-job-roles/select-starter-job-roles.component.ts
@@ -11,31 +11,5 @@ export class SelectStarterJobRolesComponent extends SelectJobRolesDirective {
   public heading = 'Select job roles for all your new starters';
   protected localStorageKey = 'startersJobRoles';
   protected prefillData: Starter[] = [];
-  protected setupRoutes(): void {
-    this.nextRoute = ['/workplace', `${this.establishment.uid}`, 'how-many-starters'];
-    this.previousRoute = ['/workplace', `${this.establishment.uid}`, 'do-you-have-starters'];
-  }
-  protected getPrefillData() {
-    const previousData = this.loadFromLocal();
-    if (previousData?.establishmentUid === this.establishment.uid && Array.isArray(previousData?.starters)) {
-      this.prefillData = previousData.starters;
-    } else if (Array.isArray(this.establishment.starters)) {
-      this.prefillData = this.establishment.starters;
-    }
-  }
-  protected onSuccess(): void {
-    const selectedJobIds: number[] = this.form.get('selectedJobRoles').value;
-    const otherCareProvidingRoleName: string = this.form.get('otherCareProvidingRoleName').value;
-    const startersFromDatabase = Array.isArray(this.establishment.starters) ? this.establishment.starters : [];
-    const updatedStarters: Starter[] = selectedJobIds.map((jobId) => {
-      const job = this.jobsAvailable.find((job) => job.id === jobId);
-      const starterCount = startersFromDatabase.find((starter) => starter.jobId === jobId)?.total ?? null;
-      if (job.id === this.jobIdOfOtherCareProvidingRole && otherCareProvidingRoleName) {
-        return { jobId, title: job.title, total: starterCount, other: otherCareProvidingRoleName };
-      }
-      return { jobId, title: job.title, total: starterCount };
-    });
-    const dataToStore = { establishmentUid: this.establishment.uid, starters: updatedStarters };
-    this.saveToLocal(dataToStore);
-  }
+  protected field = 'starters';
 }
