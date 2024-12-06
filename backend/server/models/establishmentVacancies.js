27954f6372dac8a0e960b5035cf3248c6df662f8
Joe Fong <joe.fong@madetech.com>
2024-12-06 14:24:50 +0000

    formatter

--- backend/server/models/establishmentVacancies.js
+++ backend/server/models/establishmentVacancies.js
@@ -1,41 +1,45 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
-  const EstablishmentVacancies = sequelize.define('establishmentVacancies', {
-    jobId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: '"JobID"'
+module.exports = function (sequelize, DataTypes) {
+  const EstablishmentVacancies = sequelize.define(
+    'establishmentVacancies',
+    {
+      jobId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: '"JobID"',
+      },
+      establishmentId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: '"EstablishmentID"',
+      },
+      type: {
+        type: DataTypes.ENUM,
+        allowNull: false,
+        values: ['Vacancies'],
+        default: 'Vacancies',
+        field: '"JobType"',
+      },
+      total: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: '"Total"',
+      },
     },
-    establishmentId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: '"EstablishmentID"'
+    {
+      tableName: '"VacanciesVW"',
+      schema: 'cqc',
+      createdAt: false,
+      updatedAt: false,
     },
-    type: {
-      type: DataTypes.ENUM,
-      allowNull: false,
-      values: ['Vacancies'],
-      default: 'Vacancies',
-      field: '"JobType"'
-    },
-    total: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: '"Total"'
-    }
-  }, {
-    tableName: '"VacanciesVW"',
-    schema: 'cqc',
-    createdAt: false,
-    updatedAt: false
-  });
+  );
 
   EstablishmentVacancies.associate = (models) => {
     EstablishmentVacancies.belongsTo(models.job, {
       foreignKey: 'jobId',
       targetKey: 'id',
-      as: 'reference'
+      as: 'reference',
     });
   };
 
