e219b66e37090023ffe8d5aa1ede0ad82aeea080
Joe Fong <joe.fong@madetech.com>
2024-12-06 16:23:10 +0000

    amend folder structure, add forgot-your-username component

---
+++ frontend/src/app/features/forgot-your-username-or-password/forgot-your-username/forgot-your-username.component.spec.ts
@@ -0,0 +1,39 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { getTestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '@shared/shared.module';
+import { render } from '@testing-library/angular';
+
+import { ForgotYourUsernameComponent } from './forgot-your-username.component';
+
+describe('ForgotYourUsernameComponent', () => {
+  const setup = async () => {
+    const setupTools = await render(ForgotYourUsernameComponent, {
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule, RouterTestingModule, SharedModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {},
+          },
+        },
+      ],
+    });
+
+    const component = setupTools.fixture.componentInstance;
+
+    const injector = getTestBed();
+    const router = injector.inject(Router) as Router;
+    const routerSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
+
+    return { ...setupTools, component, routerSpy };
+  };
+
+  it('should create', async () => {
+    const { component } = await setup();
+
+    expect(component).toBeTruthy();
+  });
+});
