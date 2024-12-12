786cc01f528d6a0eb3e10da93aa6e6f80be6fc3c
Joe Fong <joe.fong@madetech.com>
2024-12-12 09:37:35 +0000

    display security question in find username component

--- frontend/src/app/features/forgot-your-username-or-password/forgot-your-username/find-username/find-username.component.ts
+++ frontend/src/app/features/forgot-your-username-or-password/forgot-your-username/find-username/find-username.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { UntypedFormGroup } from '@angular/forms';
 import { ErrorDetails } from '@core/model/errorSummary.model';
 
@@ -6,9 +6,19 @@ import { ErrorDetails } from '@core/model/errorSummary.model';
   selector: 'app-find-username',
   templateUrl: './find-username.component.html',
 })
-export class FindUsernameComponent {
+export class FindUsernameComponent implements OnInit {
+  @Input() accountUid: string;
+  @Input() securityQuestion: string;
+
+  @Output() setCurrentForm = new EventEmitter<FindUsernameComponent>();
+
   @ViewChild('formEl') formEl: ElementRef;
+
   public form: UntypedFormGroup;
   public submitted = false;
   public formErrorsMap: Array<ErrorDetails>;
+
+  ngOnInit(): void {
+    this.setCurrentForm.emit(this);
+  }
 }
