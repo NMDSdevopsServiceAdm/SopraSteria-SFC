04a64a32ff30e570aae27dd07cc52f4a11bcee9b
Duncan Carter <duncancarter93@gmail.com>
2025-03-05 17:15:22 +0000

    Add tests for editing mandatory details

--- frontend/cypress/e2e/standAloneEstablishment/EditUser/staffRecordsPage.spec.cy.js
+++ frontend/cypress/e2e/standAloneEstablishment/EditUser/staffRecordsPage.spec.cy.js
@@ -15,25 +15,40 @@ describe('Standalone staff records page as edit user', () => {
     cy.get('[data-testid="staff-records"]').should('exist');
   });
 
-  describe('Adding a staff record', () => {
+  describe('Adding mandatory details for a staff record', () => {
     it('should add a new staff record successfully', () => {
-      cy.get('a[role="button"]').contains('Add a staff record').click();
+      const name = 'Mr Cool';
+      const contractType = 'Permanent';
+      const mainJobRole = 'Care worker';
 
-      // staff-details
-      cy.get('label').contains('Name or ID number').parent().find('input').type('Mr Cool');
-      cy.get('label').contains('Permanent').parent().find('input').click();
-      cy.contains('button', 'Continue').click();
+      createStaffRecordWithMandatoryDetails(name, contractType, mainJobRole);
 
-      // main-job-role
-      cy.get('button').contains('span', 'Care providing roles').click();
-      cy.get('label').contains('Care worker').parent().find('input').click();
-      cy.contains('button', 'Save this staff record').click();
+      expectContentToBeDisplayedOnMandatoryDetailsPage(name, contractType, mainJobRole);
+    });
 
-      // mandatory-details
-      cy.contains('h1', 'Add a staff record').should('be.visible');
-      cy.contains('.govuk-inset-text', 'Staff record saved').should('be.visible');
-      cy.contains('.govuk-summary-list__value', 'Permanent').should('be.visible');
-      cy.contains('.govuk-summary-list__value', 'Care worker').should('be.visible');
+    it('should update name and contract type when changed after creating new record', () => {
+      const updatedName = 'Mr Smith';
+      const updatedContractType = 'Temporary';
+
+      const { name } = createStaffRecordWithMandatoryDetails();
+
+      cy.contains('.govuk-summary-list__row', name).find('.govuk-summary-list__actions a').click();
+
+      inputNameAndContractType(updatedName, updatedContractType, 'Save and return');
+      cy.contains('.govuk-summary-list__value', updatedName).should('be.visible');
+      cy.contains('.govuk-summary-list__value', updatedContractType).should('be.visible');
+    });
+
+    it('should update job role when changed after creating new record', () => {
+      const updatedJobRole = 'Support worker';
+
+      const { mainJobRole } = createStaffRecordWithMandatoryDetails();
+
+      cy.contains('.govuk-summary-list__row', mainJobRole).find('.govuk-summary-list__actions a').click();
+
+      cy.get('label').contains(updatedJobRole).parent().find('input').click();
+      cy.contains('button', 'Save and return').click();
+      cy.contains('.govuk-summary-list__value', updatedJobRole).should('be.visible');
     });
 
     it('should show validation error if Name or ID number is empty', () => {
@@ -50,7 +65,7 @@ describe('Standalone staff records page as edit user', () => {
       cy.contains('.govuk-error-message', 'Select the type of contract they have').should('be.visible');
     });
 
-    it('should show validation error if no care providing role is selected', () => {
+    it('should show validation error if no job role is selected', () => {
       cy.get('a[role="button"]').contains('Add a staff record').click();
 
       // staff-details
@@ -76,4 +91,37 @@ describe('Standalone staff records page as edit user', () => {
       cy.get('label').contains('Permanent').parent().find('input').should('be.checked');
     });
   });
+
+  const createStaffRecordWithMandatoryDetails = (
+    name = 'Bob',
+    contractType = 'Permanent',
+    mainJobRole = 'Care worker',
+  ) => {
+    cy.get('a[role="button"]').contains('Add a staff record').click();
+
+    inputNameAndContractType(name, contractType);
+    inputMainJobRole(mainJobRole);
+
+    return { name, contractType, mainJobRole };
+  };
+
+  const inputNameAndContractType = (name = 'Bob', contractType = 'Permanent', buttonText = 'Continue') => {
+    cy.get('label').contains('Name or ID number').parent().find('input').type(name);
+    cy.get('label').contains(contractType).parent().find('input').click();
+    cy.contains('button', buttonText).click();
+  };
+
+  const inputMainJobRole = (mainJobRole, buttonText = 'Save this staff record') => {
+    cy.get('button').contains('span', 'Care providing roles').click();
+    cy.get('label').contains(mainJobRole).parent().find('input').click();
+    cy.contains('button', buttonText).click();
+  };
+
+  const expectContentToBeDisplayedOnMandatoryDetailsPage = (name, contractType, mainJobRole) => {
+    cy.contains('h1', 'Add a staff record').should('be.visible');
+    cy.contains('.govuk-inset-text', 'Staff record saved').should('be.visible');
+    cy.contains('.govuk-summary-list__value', name).should('be.visible');
+    cy.contains('.govuk-summary-list__value', contractType).should('be.visible');
+    cy.contains('.govuk-summary-list__value', mainJobRole).should('be.visible');
+  };
 });
