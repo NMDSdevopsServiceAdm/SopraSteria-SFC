c52fbd6851ac22aad58963cd293de43b861d4763
Joe Fong <joe.fong@madetech.com>
2025-03-05 17:00:27 +0000

    unify the "Tab" type across different files

--- frontend/src/app/core/services/tabs.service.ts
+++ frontend/src/app/core/services/tabs.service.ts
@@ -2,22 +2,43 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { PreviousRouteService } from './previous-route.service';
 
-interface Tab {
+export interface Tab {
   title: string;
   slug: string;
   active: boolean;
 }
 
+export const MainDashboardTabs = {
+  homeTab: { title: 'Home', slug: 'home', active: false },
+  workplaceTab: { title: 'Workplace', slug: 'workplace', active: false },
+  staffRecordsTab: { title: 'Staff records', slug: 'staff-records', active: false },
+  tAndQTab: { title: 'Training and qualifications', slug: 'training-and-qualifications', active: false },
+  benchmarksTab: { title: 'Benchmarks', slug: 'benchmarks', active: false },
+};
+
+export const SubsidiaryAccountTabs = {
+  workplaceUsersTab: { title: 'Workplace users', slug: 'workplace-users', active: false },
+};
+
+export const UrlPartsRelatedToTabs = [
+  { urlPart: 'staff-record', tabSlug: MainDashboardTabs.staffRecordsTab.slug },
+  { urlPart: 'staff-records', tabSlug: MainDashboardTabs.staffRecordsTab.slug },
+  { urlPart: 'training-and-qualifications-record', tabSlug: MainDashboardTabs.tAndQTab.slug },
+  { urlPart: 'training-and-qualifications', tabSlug: MainDashboardTabs.tAndQTab.slug },
+  { urlPart: 'benchmarks', tabSlug: MainDashboardTabs.benchmarksTab.slug },
+  { urlPart: 'workplace-users', tabSlug: SubsidiaryAccountTabs.workplaceUsersTab.slug },
+];
+
 @Injectable({
   providedIn: 'root',
 })
 export class TabsService {
-  public homeTab: Tab = { title: 'Home', slug: 'home', active: false };
-  public workplaceTab: Tab = { title: 'Workplace', slug: 'workplace', active: false };
-  public staffRecordsTab: Tab = { title: 'Staff records', slug: 'staff-records', active: false };
-  public tAndQTab: Tab = { title: 'Training and qualifications', slug: 'training-and-qualifications', active: false };
-  public benchmarksTab: Tab = { title: 'Benchmarks', slug: 'benchmarks', active: false };
-  public workplaceUsers: Tab = { title: 'Workplace users', slug: 'workplace-users', active: false };
+  public homeTab: Tab = MainDashboardTabs.homeTab;
+  public workplaceTab: Tab = MainDashboardTabs.workplaceTab;
+  public staffRecordsTab: Tab = MainDashboardTabs.staffRecordsTab;
+  public tAndQTab: Tab = MainDashboardTabs.tAndQTab;
+  public benchmarksTab: Tab = MainDashboardTabs.benchmarksTab;
+  public workplaceUsersTab: Tab = SubsidiaryAccountTabs.workplaceUsersTab;
 
   constructor(private previousRouteService: PreviousRouteService) {}
 
