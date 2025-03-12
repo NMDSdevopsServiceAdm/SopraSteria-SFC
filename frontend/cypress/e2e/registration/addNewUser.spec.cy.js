2b792cbf90ed98e7c5da489320bad6099506d120
Joe Fong <joe.fong@madetech.com>
2025-03-12 12:03:35 +0000

    add a test for edit user in addNewUser.spec.cy.js

--- frontend/cypress/e2e/registration/addNewUser.spec.cy.js
+++ frontend/cypress/e2e/registration/addNewUser.spec.cy.js
@@ -12,14 +12,7 @@ describe('As a workplace primary user I want to register a new user', () => {
     cy.deleteTestUserFromDb(userFullName);
   });
 
-  after(() => {
-    cy.deleteTestUserFromDb(userFullName);
-  });
-
-  beforeEach(() => {
-    cy.loginAsUser(Cypress.env('editStandAloneUser'), Cypress.env('userPassword'));
-
-    // get pass the vacancies and turnover login message if necessary
+  const getPassVacanciesAndTurnoverLoginMessage = () => {
     cy.get('h1').should('not.contain', 'Sign in');
     cy.get('h1')
       .invoke('text')
@@ -28,43 +21,61 @@ describe('As a workplace primary user I want to register a new user', () => {
           cy.get('a').contains('Continue').click();
         }
       });
+  };
+
+  beforeEach(() => {
+    cy.loginAsUser(Cypress.env('editStandAloneUser'), Cypress.env('userPassword'));
+    getPassVacanciesAndTurnoverLoginMessage();
   });
 
-  it('should be able to create a new user for the workplace', () => {
-    // Add new user to workplace
-    cy.contains('a', 'Users').click();
-    cy.contains('a', 'Add a user').click();
-    cy.get('h1').should('be.visible').and('contain.text', 'Add a user');
+  afterEach(() => {
+    cy.deleteTestUserFromDb(userFullName);
+  });
 
-    cy.getByLabel('Full name').type(userFullName);
-    cy.getByLabel('Job title ').type('Manager');
-    cy.getByLabel('Email address').type('test@example.com');
-    cy.getByLabel('Phone number').type('0123456789');
+  const userTypes = ['edit', 'read'];
 
-    cy.getByLabel('Read').check();
-    cy.get('button').contains('Save user').click();
-    cy.get('a').contains('Sign out').click();
+  userTypes.forEach((userType) => {
+    it(`should be able to add a new ${userType} user for the workplace`, () => {
+      // Add new user to workplace
+      cy.contains('a', 'Users').click();
+      cy.contains('a', 'Add a user').click();
+      cy.get('h1').should('be.visible').and('contain.text', 'Add a user');
 
-    cy.get('a').contains('Sign in').should('be.visible');
+      cy.getByLabel('Full name').type(userFullName);
+      cy.getByLabel('Job title ').type('Manager');
+      cy.getByLabel('Email address').type('test@example.com');
+      cy.getByLabel('Phone number').type('0123456789');
 
-    // register username and password
-    cy.getNewUserUuidToken().then((uuidToken) => {
-      const registrationUrl = `/activate-account/${uuidToken}/create-username`;
-      cy.visit(registrationUrl);
+      const checkboxLabelForUserType = userType[0].toUpperCase() + userType.slice(1);
+      cy.getByLabel(checkboxLabelForUserType).check();
+      cy.get('button').contains('Save user').click();
+      cy.get('a').contains('Sign out').click();
+
+      cy.get('a').contains('Sign in').should('be.visible');
+
+      // register username and password
+      cy.getNewUserUuidToken().then((uuidToken) => {
+        const registrationUrl = `/activate-account/${uuidToken}/create-username`;
+        cy.visit(registrationUrl);
+      });
+
+      fillUserRegistrationForm({ username: loginId, password: mockPassword });
+
+      cy.get('h1').should('contain.text', 'Confirm your account details');
+      cy.get('input#termsAndConditions').check();
+
+      cy.get('button').contains('Submit details').click();
+
+      cy.get('span').contains('Your account is now active').should('be.visible');
+
+      // try login as the new user
+      cy.loginAsUser(loginId, mockPassword);
+      if (userType === 'edit') {
+        getPassVacanciesAndTurnoverLoginMessage();
+      }
+
+      cy.get('h1').contains('df').should('be.visible');
+      onHomePage.allTabs(userType);
     });
-
-    fillUserRegistrationForm({ username: loginId, password: mockPassword });
-
-    cy.get('h1').should('contain.text', 'Confirm your account details');
-    cy.get('input#termsAndConditions').check();
-
-    cy.get('button').contains('Submit details').click();
-
-    cy.get('span').contains('Your account is now active').should('be.visible');
-
-    // try login as the new user
-    cy.loginAsUser(loginId, mockPassword);
-    cy.get('h1').contains('df').should('be.visible');
-    onHomePage.allTabs('read');
   });
 });
