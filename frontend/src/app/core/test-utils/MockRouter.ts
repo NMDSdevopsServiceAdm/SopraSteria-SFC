ea615851b72a9a9d9715f97b648f4e9fbd80acec
Duncan Carter <duncancarter93@gmail.com>
2025-03-17 09:55:24 +0000

    Simplify MockRouter

--- frontend/src/app/core/test-utils/MockRouter.ts
+++ frontend/src/app/core/test-utils/MockRouter.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterState, RouterStateSnapshot } from '@angular/router';
+import { Router, RouterState, RouterStateSnapshot } from '@angular/router';
 
 @Injectable()
 export class MockRouter extends Router {
@@ -17,17 +17,8 @@ export class MockRouter extends Router {
 }
 
 export const setUpRouterState = (url: string, router: Router) => {
-  const mockActivatedRouteSnapshot: Partial<ActivatedRouteSnapshot> = {
-    url: [],
-  };
-
-  const mockRouterStateSnapshot: Partial<RouterStateSnapshot> = {
-    url,
-    root: mockActivatedRouteSnapshot as ActivatedRouteSnapshot,
-  };
-
   const mockRouterState: Partial<RouterState> = {
-    snapshot: mockRouterStateSnapshot as RouterStateSnapshot,
+    snapshot: { url, root: { url: [] } } as RouterStateSnapshot,
   };
 
   return spyOnProperty(router, 'routerState', 'get').and.returnValue(mockRouterState as RouterState);
