6dc8fa6def1b8e90642cb672eca95454faf1c750
Duncan Carter <duncancarter93@gmail.com>
2025-03-17 15:28:02 +0000

    Add total staff question to page

--- frontend/src/app/features/workers/update-workplace-details-after-staff-changes/update-workplace-details-after-staff-changes.component.spec.ts
+++ frontend/src/app/features/workers/update-workplace-details-after-staff-changes/update-workplace-details-after-staff-changes.component.spec.ts
@@ -1,17 +1,26 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { establishmentBuilder } from '@core/test-utils/MockEstablishmentService';
 import { SharedModule } from '@shared/shared.module';
-import { render } from '@testing-library/angular';
+import { render, within } from '@testing-library/angular';
 
 import { UpdateWorkplaceDetailsAfterStaffChangesComponent } from './update-workplace-details-after-staff-changes.component';
 
 describe('UpdateWorkplaceDetailsAfterStaffChangesComponent', () => {
-  async function setup() {
+  async function setup(overrides: any = {}) {
+    const workplace = { ...establishmentBuilder(), ...overrides.workplace };
+
     const setupTools = await render(UpdateWorkplaceDetailsAfterStaffChangesComponent, {
       imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
-      providers: [UntypedFormBuilder],
+      providers: [
+        {
+          provide: EstablishmentService,
+          useValue: { establishment: workplace },
+        },
+      ],
     });
 
     const component = setupTools.fixture.componentInstance;
@@ -19,6 +28,7 @@ describe('UpdateWorkplaceDetailsAfterStaffChangesComponent', () => {
     return {
       ...setupTools,
       component,
+      workplace,
     };
   }
 
@@ -32,5 +42,27 @@ describe('UpdateWorkplaceDetailsAfterStaffChangesComponent', () => {
 
     expect(getByText('Check this information and make any changes before you continue')).toBeTruthy();
     expect(getByText('Total number of staff, vacancies and starters')).toBeTruthy();
+  });
+
+  describe('Number of staff', () => {
+    it('should display the number of staff and a Change link when question answered', async () => {
+      const { queryByTestId, workplace } = await setup({ workplace: { numberOfStaff: 4 } });
+
+      const numberOfStaffRow = queryByTestId('numberOfStaff');
+      const changeLink = within(numberOfStaffRow).queryByText('Change');
+
+      expect(changeLink.getAttribute('href')).toEqual(`/workplace/${workplace.uid}/update-total-staff`);
+      expect(within(numberOfStaffRow).queryByText('4')).toBeTruthy();
+    });
+
+    it('should display dash and an Add link if there is no value for number of staff', async () => {
+      const { queryByTestId, workplace } = await setup({ workplace: { numberOfStaff: null } });
+
+      const numberOfStaffRow = queryByTestId('numberOfStaff');
+      const addLink = within(numberOfStaffRow).queryByText('Add');
+
+      expect(addLink.getAttribute('href')).toEqual(`/workplace/${workplace.uid}/update-total-staff`);
+      expect(within(numberOfStaffRow).queryByText('-')).toBeTruthy();
+    });
   });
 });
--- frontend/src/app/features/workers/update-workplace-details-after-staff-changes/update-workplace-details-after-staff-changes.component.ts
+++ frontend/src/app/features/workers/update-workplace-details-after-staff-changes/update-workplace-details-after-staff-changes.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { Establishment } from '@core/model/establishment.model';
+import { EstablishmentService } from '@core/services/establishment.service';
 
 @Component({
   selector: 'app-update-workplace-details-after-staff-changes',
   templateUrl: './update-workplace-details-after-staff-changes.component.html',
 })
-export class UpdateWorkplaceDetailsAfterStaffChangesComponent {}
+export class UpdateWorkplaceDetailsAfterStaffChangesComponent {
+  constructor(private establishmentService: EstablishmentService) {}
+
+  public workplace: Establishment;
+
+  ngOnInit(): void {
+    this.workplace = this.establishmentService.establishment;
+  }
+}
