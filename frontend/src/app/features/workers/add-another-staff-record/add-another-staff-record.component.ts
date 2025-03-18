73eb06c2945b3ed9b1b3b77a880de4fa8d8214aa
Jonathan Hardy <Jonathan.Hardy97@gmail.com>
2025-03-18 16:41:37 +0000

    added back link

--- frontend/src/app/features/workers/add-another-staff-record/add-another-staff-record.component.ts
+++ frontend/src/app/features/workers/add-another-staff-record/add-another-staff-record.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UntypedFormArray, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { BackLinkService } from '@core/services/backLink.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 
 @Component({
   selector: 'app-add-another-staff-record',
   templateUrl: './add-another-staff-record.component.html',
 })
-export class AddAnotherStaffRecordComponent {
+export class AddAnotherStaffRecordComponent implements OnInit{
   public form: UntypedFormGroup;
 
   constructor(
+    protected backLinkService: BackLinkService,
     protected formBuilder: UntypedFormBuilder,
     protected router: Router,
     protected establishmentService: EstablishmentService,
@@ -18,6 +20,9 @@ export class AddAnotherStaffRecordComponent {
     this.form = this.formBuilder.group({
       addAnotherStaffRecord: null,
     });
+  }
+  ngOnInit(): void {
+    this.backLinkService.showBackLink();
   }
 
   public onSubmit() {
