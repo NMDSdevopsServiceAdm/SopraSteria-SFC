5aeea91cb87de1e9e21df8b285a8550749262cd6
Jonathan Hardy <Jonathan.Hardy97@gmail.com>
2025-03-18 15:22:06 +0000

    navigates to staff changes summary when nothing or no is selected

--- frontend/src/app/features/workers/add-another-staff-record/add-another-staff-record.component.spec.ts
+++ frontend/src/app/features/workers/add-another-staff-record/add-another-staff-record.component.spec.ts
@@ -1,20 +1,44 @@
 import { render, getByText, getByLabelText } from '@testing-library/angular';
 import { AddAnotherStaffRecordComponent } from './add-another-staff-record.component'
+import userEvent from '@testing-library/user-event';
+import { getTestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { WorkplaceService } from '@core/services/workplace.service';
+import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
+import { ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
 
 
 fdescribe('AddAnotherStaffRecordComponent', () => {
   async function setup() {
     const { fixture, getByLabelText, getByText } = await render(
-      AddAnotherStaffRecordComponent
+      AddAnotherStaffRecordComponent,
+      {
+        imports: [RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
+        providers: [
+          UntypedFormBuilder,
+          {
+            provide: EstablishmentService,
+            useClass: MockEstablishmentService
+          },
+        ]
+      }
     );
 
     const component = fixture.componentInstance;
+
+    const injector = getTestBed();
+    const router = injector.inject(Router) as Router;
+    const navigateSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
 
     return {
       component,
       fixture,
       getByLabelText,
       getByText,
+      navigateSpy
     };
   }
 
@@ -56,6 +80,49 @@ fdescribe('AddAnotherStaffRecordComponent', () => {
       expect(getByText('Continue')).toBeTruthy();
     });
 
+    it(`navigates to staff record flow when 'yes' is selected`, async () => {
+      const { fixture, getByText, navigateSpy } = await setup();
 
+      userEvent.click(getByText('Yes'));
+      userEvent.click(getByText('Continue'));
+      fixture.detectChanges();
+
+      expect(navigateSpy).toHaveBeenCalledWith([
+        '/workplace',
+        '98a83eef-e1e1-49f3-89c5-b1287a3cc8de',
+        'staff-record',
+        'create-staff-record',
+        'staff-details'
+      ]);
+    });
+
+    it(`navigates to staff changes summary when 'no' is selected`, async () => {
+      const { fixture, getByText, navigateSpy } = await setup();
+
+      userEvent.click(getByText('No'));
+      userEvent.click(getByText('Continue'));
+      fixture.detectChanges();
+
+      expect(navigateSpy).toHaveBeenCalledWith([
+        '/workplace',
+        '98a83eef-e1e1-49f3-89c5-b1287a3cc8de',
+        'staff-record',
+        'update-workplace-details-after-staff-changes'
+      ]);
+    });
+
+    it(`navigates to staff changes summary when neither option is selected`, async () => {
+      const { fixture, getByText, navigateSpy } = await setup();
+
+      userEvent.click(getByText('Continue'));
+      fixture.detectChanges();
+
+      expect(navigateSpy).toHaveBeenCalledWith([
+        '/workplace',
+        '98a83eef-e1e1-49f3-89c5-b1287a3cc8de',
+        'staff-record',
+        'update-workplace-details-after-staff-changes'
+      ]);
+    });
   });
 })
--- frontend/src/app/features/workers/add-another-staff-record/add-another-staff-record.component.ts
+++ frontend/src/app/features/workers/add-another-staff-record/add-another-staff-record.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { UntypedFormArray, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EstablishmentService } from '@core/services/establishment.service';
 
 @Component({
   selector: 'app-add-another-staff-record',
   templateUrl: './add-another-staff-record.component.html',
 })
 export class AddAnotherStaffRecordComponent {
+  public form: UntypedFormGroup;
+
+  constructor(
+    protected formBuilder: UntypedFormBuilder,
+    protected router: Router,
+    protected establishmentService: EstablishmentService,
+  ) {
+    this.form = this.formBuilder.group({
+      addAnotherStaffRecord: null,
+    });
+  }
+
+  public onSubmit() {
+    if (this.form.controls['addAnotherStaffRecord'].value === 'YES') {
+      this.router.navigate(['/workplace', this.establishmentService.primaryWorkplace.uid, 'staff-record', 'create-staff-record', 'staff-details']);
+    } else {
+      this.router.navigate(['/workplace', this.establishmentService.primaryWorkplace.uid, 'staff-record', 'update-workplace-details-after-staff-changes']);
+    }
+  }
+
 
 }
