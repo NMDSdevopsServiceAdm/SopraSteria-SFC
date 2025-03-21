0c3c9d54286363e6d869b57dcd21faefd7227b48
Joe Fong <joe.fong@madetech.com>
2025-03-21 16:49:28 +0000

    temp store current progress before amending app-number-input-with-buttons

--- frontend/src/app/features/workers/update-workplace-details-after-staff-changes/update-vacancies/update-vacancies.component.ts
+++ frontend/src/app/features/workers/update-workplace-details-after-staff-changes/update-vacancies/update-vacancies.component.ts
@@ -1,10 +1,13 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { UntypedFormArray, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ErrorDetails } from '@core/model/errorSummary.model';
+import { jobOptionsEnum, Vacancy } from '@core/model/establishment.model';
 import { BackLinkService } from '@core/services/backLink.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
+import { EstablishmentService } from '@core/services/establishment.service';
 import { UpdateWorkplaceAfterStaffChangesService } from '@core/services/update-workplace-after-staff-changes.service';
+import lodash from 'lodash';
 
 @Component({
   selector: 'app-update-vacancies',
@@ -13,26 +16,45 @@ import { UpdateWorkplaceAfterStaffChangesService } from '@core/services/update-w
 })
 export class UpdateVacanciesComponent {
   @ViewChild('formEl') formEl: ElementRef;
+
   public heading: string;
   public form: UntypedFormGroup;
   public formErrorsMap: Array<ErrorDetails> = [];
   public submitted = false;
+  public selectedJobRoles: Array<Vacancy> = [];
+  public totalNumber: number = 0;
+
+  public minNumberPerJobRole = 1;
+  public maxNumberPerJobRole = 999;
+
+  public knownOptions = [
+    {
+      label: 'There are no current staff vacancies',
+      value: jobOptionsEnum.NONE,
+    },
+    {
+      label: 'I do not know if there are any current staff vacancies',
+      value: jobOptionsEnum.DONT_KNOW,
+    },
+  ];
 
   constructor(
     protected formBuilder: UntypedFormBuilder,
     protected router: Router,
     protected backlinkService: BackLinkService,
     protected errorSummaryService: ErrorSummaryService,
+    protected establishmentService: EstablishmentService,
     protected updateWorkplaceAfterStaffChangesService: UpdateWorkplaceAfterStaffChangesService,
     protected route: ActivatedRoute,
   ) {}
 
   ngOnInit() {
     this.setupTexts();
+    this.prefill();
+
     this.setupForm();
     this.setupFormErrorsMap();
     this.setBackLink();
-    this.prefill();
   }
 
   public setupTexts() {
@@ -40,8 +62,30 @@ export class UpdateVacanciesComponent {
   }
 
   public setupForm() {
-    this.form = this.formBuilder.group({});
+    this.form = this.formBuilder.group({
+      jobRoleNumbers: this.formBuilder.array([]),
+    });
+
+    this.selectedJobRoles.forEach((jobRole) => {
+      const initialValue = jobRole.total ?? '';
+      this.jobRoleNumbers.push(
+        this.formBuilder.control(initialValue, {
+          validators: [
+            Validators.required,
+            Validators.min(this.minNumberPerJobRole),
+            Validators.max(this.maxNumberPerJobRole),
+          ],
+          updateOn: 'submit',
+        }),
+      );
+    });
   }
+
+  get jobRoleNumbers(): UntypedFormArray {
+    return this.form.get('jobRoleNumbers') as UntypedFormArray;
+  }
+
+  public handleNumberChange(event: Event) {}
 
   public setupFormErrorsMap() {}
 
@@ -49,7 +93,17 @@ export class UpdateVacanciesComponent {
     this.backlinkService.showBackLink();
   }
 
-  public prefill() {}
+  public prefill() {
+    const vacanciesFromBackend = this.establishmentService.establishment.vacancies;
+    if (Array.isArray(vacanciesFromBackend)) {
+      this.selectedJobRoles = vacanciesFromBackend;
+      this.updateTotalNumber();
+    }
+  }
+
+  protected updateTotalNumber(): void {
+    this.totalNumber = lodash.sumBy(this.selectedJobRoles, 'total');
+  }
 
   public onSubmit() {}
 
