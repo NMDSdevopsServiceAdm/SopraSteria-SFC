a1c1ae4c906b0c7cae6d422a91d874e805846ddb
Duncan Carter <duncancarter93@gmail.com>
2025-04-01 11:15:25 +0100

    Duplicate update-vacancies to set up skeleton of update-starters page

---
+++ frontend/src/app/features/workers/update-workplace-details-after-staff-changes/update-starters/update-starters.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {
+  UpdateStartersLeaversVacanciesDirective,
+} from '@shared/directives/update-starters-leavers-vacancies/update-starters-leavers-vacancies.directive';
+
+@Component({
+  selector: 'app-update-starters',
+  templateUrl:
+    '../../../../shared/directives/update-starters-leavers-vacancies/update-starters-leavers-vacancies.component.html',
+  styleUrl:
+    '../../../../shared/directives/update-starters-leavers-vacancies/update-starters-leavers-vacancies.component.scss',
+})
+export class UpdateStartersComponent extends UpdateStartersLeaversVacanciesDirective {}
