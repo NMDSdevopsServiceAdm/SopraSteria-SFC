8544dc074af03b281959d6c6a6b2b8891cfdb453
Sabrina <sabrina.samuel@madetech.com>
2025-04-03 15:11:37 +0100

    Delete migration files to put in separate branch

--- backend/migrations/20250402130009-removeWdfUserConstraintToNoneRole.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  up: async (queryInterface) => {
-    return queryInterface.sequelize.query('ALTER TABLE cqc."User" DROP CONSTRAINT none_role_must_be_wdf_user');
-  },
-};
