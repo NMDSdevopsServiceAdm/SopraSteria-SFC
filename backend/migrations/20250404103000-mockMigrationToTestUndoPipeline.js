72648296d3f56cea6ec11daf57655d306b92bbb9
Joe Fong <joe.fong@madetech.com>
2025-04-04 10:34:20 +0100

    add a mock migration file to test pipeline undo

---
+++ backend/migrations/20250404103000-mockMigrationToTestUndoPipeline.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async () => {
+    console.log('running migration up');
+    return;
+  },
+
+  down: async () => {
+    console.log('running migration down');
+    return;
+  },
+};
