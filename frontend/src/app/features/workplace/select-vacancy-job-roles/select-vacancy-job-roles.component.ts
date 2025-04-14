4d51ed9fc1bfd10e9c2b501912046e763212596a
Joe Fong <joe.fong@madetech.com>
2025-04-14 15:08:06 +0100

    amend SelectVacancyJobRolesComponent to use VacanciesAndTurnoverService

--- frontend/src/app/features/workplace/select-vacancy-job-roles/select-vacancy-job-roles.component.ts
+++ frontend/src/app/features/workplace/select-vacancy-job-roles/select-vacancy-job-roles.component.ts
@@ -15,4 +15,30 @@ export class SelectVacancyJobRolesComponent extends SelectJobRolesDirective impl
   protected hasStartersLeaversVacanciesField = 'hasVacancies';
   protected prefillData: Vacancy[] = [];
   protected field = 'vacancies';
+
+  protected getPrefillData(): void {
+    const previousData = this.vacanciesAndTurnoverService.selectedVacancies;
+    if (Array.isArray(previousData)) {
+      this.prefillData = previousData;
+    } else if (Array.isArray(this.establishment[this.field])) {
+      this.prefillData = this.establishment[this.field] as Array<Vacancy>;
+    }
+  }
+
+  protected onSuccess(): void {
+    const selectedJobIds: number[] = this.form.get('selectedJobRoles').value;
+
+    const updatedJobRoles: Vacancy[] = selectedJobIds.map((jobId) => {
+      const job = this.jobsAvailable.find((job) => job.id === jobId);
+      const fieldCount = this.prefillData.find((field) => field.jobId === jobId)?.total ?? null;
+
+      return { jobId, title: job.title, total: fieldCount };
+    });
+    this.vacanciesAndTurnoverService.selectedVacancies = updatedJobRoles;
+  }
+
+  protected clearLocalStorageData(): void {
+    localStorage.removeItem(this.hasStartersLeaversVacanciesField);
+    this.vacanciesAndTurnoverService.clearAllSelectedJobRoles();
+  }
 }
