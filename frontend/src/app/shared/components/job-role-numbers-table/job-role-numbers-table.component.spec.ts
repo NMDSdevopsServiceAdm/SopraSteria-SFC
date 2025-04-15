7d5194995771bf3316c996b89ddaaf633e29bb59
Joe Fong <joe.fong@madetech.com>
2025-04-15 14:07:33 +0100

    add unit tests for job-role-numbers-table component

--- frontend/src/app/shared/components/job-role-numbers-table/job-role-numbers-table.component.spec.ts
+++ frontend/src/app/shared/components/job-role-numbers-table/job-role-numbers-table.component.spec.ts
@@ -4,23 +4,29 @@ import { SharedModule } from '@shared/shared.module';
 import { ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Vacancy } from '@core/model/establishment.model';
+import userEvent from '@testing-library/user-event';
+import lodash from 'lodash';
 
 fdescribe('JobRolesNumberTableComponent', () => {
   const mockSelectedJobRoles: Vacancy[] = [
     {
       jobId: 10,
       title: 'Care worker',
-      total: 1,
+      total: 2,
     },
     {
       jobId: 23,
       title: 'Registered nurse',
-      total: 2,
+      total: 3,
     },
   ];
+  const totalNumberAtStart = lodash.sum(mockSelectedJobRoles.map((job) => job.total));
 
   const setup = async (override: any = {}) => {
-    const selectedJobRoles: Vacancy[] = override.selectedJobRoles ?? mockSelectedJobRoles;
+    const { tableTitle, addJobRoleButtonText, totalNumberDescription, messageWhenNoJobRoleSelected } = override;
+    const selectedJobRoles: Vacancy[] = override.selectedJobRoles ?? [...mockSelectedJobRoles];
+    const allowRemoveJobRole = override.allowRemoveJobRole ?? true;
+
     const formBuilder = new UntypedFormBuilder();
     const jobRoleNumbers = formBuilder.array(
       selectedJobRoles.map((job) => job.total),
@@ -33,6 +39,11 @@ fdescribe('JobRolesNumberTableComponent', () => {
       componentProperties: {
         jobRoleNumbers,
         selectedJobRoles,
+        tableTitle,
+        addJobRoleButtonText,
+        totalNumberDescription,
+        allowRemoveJobRole,
+        messageWhenNoJobRoleSelected,
       },
     });
 
@@ -42,7 +53,142 @@ fdescribe('JobRolesNumberTableComponent', () => {
   };
 
   it('should create', async () => {
-    const component = await setup();
+    const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should show the table title', async () => {
+    const { getByText } = await setup({ tableTitle: 'Starters in the last 12 months' });
+    expect(getByText('Starters in the last 12 months')).toBeTruthy();
+  });
+
+  it('should show the table title', async () => {
+    const { getByText } = await setup({ tableTitle: 'Starters in the last 12 months' });
+    expect(getByText('Starters in the last 12 months')).toBeTruthy();
+  });
+
+  it('should show an add job role button with the given text', async () => {
+    const { getByRole } = await setup({ addJobRoleButtonText: 'Add more job roles' });
+    expect(getByRole('button', { name: 'Add more job roles' })).toBeTruthy();
+  });
+
+  it('should show a description for total number', async () => {
+    const { getByText, getByTestId } = await setup({ totalNumberDescription: 'Total number of leavers' });
+
+    const totalNumberDescription = getByText('Total number of leavers');
+
+    expect(totalNumberDescription).toBeTruthy();
+    expect(getByTestId('total-number').textContent).toEqual(`${totalNumberAtStart}`);
+  });
+
+  describe('job role numbers', async () => {
+    it('should display a job role input for every selected job roles', async () => {
+      const { getByLabelText } = await setup();
+
+      mockSelectedJobRoles.forEach((jobRole) => {
+        const jobRoleInput = getByLabelText(jobRole.title) as HTMLInputElement;
+        expect(jobRoleInput).toBeTruthy();
+        expect(jobRoleInput.value).toEqual(`${jobRole.total}`);
+      });
+    });
+
+    it('should increase the selected job number when "+" is clicked', async () => {
+      const { getByLabelText, getByTestId, fixture } = await setup();
+
+      const plusButton = getByTestId('plus-button-job-0');
+
+      const jobRoleInput = getByLabelText(mockSelectedJobRoles[0].title) as HTMLInputElement;
+      expect(jobRoleInput.value).toBe(`${mockSelectedJobRoles[0].total}`);
+
+      userEvent.click(plusButton);
+      fixture.detectChanges();
+
+      expect(jobRoleInput.value).toBe(`${mockSelectedJobRoles[0].total + 1}`);
+      expect(getByTestId('total-number').textContent).toEqual(`${totalNumberAtStart + 1}`);
+    });
+
+    it('should describe the selected job number when "-" is clicked', async () => {
+      const { getByLabelText, getByTestId, fixture } = await setup();
+
+      const minusButton = getByTestId('minus-button-job-0');
+
+      const jobRoleInput = getByLabelText(mockSelectedJobRoles[0].title) as HTMLInputElement;
+      expect(jobRoleInput.value).toBe(`${mockSelectedJobRoles[0].total}`);
+
+      userEvent.click(minusButton);
+      fixture.detectChanges();
+
+      expect(jobRoleInput.value).toBe(`${mockSelectedJobRoles[0].total - 1}`);
+      expect(getByTestId('total-number').textContent).toEqual(`${totalNumberAtStart - 1}`);
+    });
+
+    it('should update the total number when user typed in a job role number', async () => {
+      const { getByLabelText, getByTestId, fixture } = await setup();
+
+      const jobRoleInput = getByLabelText(mockSelectedJobRoles[0].title) as HTMLInputElement;
+
+      userEvent.clear(jobRoleInput);
+      userEvent.type(jobRoleInput, '10');
+      fixture.detectChanges();
+
+      const expectedNewTotalNumber = 10 + mockSelectedJobRoles[1].total;
+
+      expect(getByTestId('total-number').textContent).toEqual(`${expectedNewTotalNumber}`);
+    });
+
+    it('should show the messageWhenNoJobRoleSelected about no job role is given', async () => {
+      const messageWhenNoJobRoleSelected = "You've not added any staff who've started since 15 April 2024.";
+      const { getByText } = await setup({
+        selectedJobRoles: [],
+        messageWhenNoJobRoleSelected,
+      });
+
+      expect(getByText(messageWhenNoJobRoleSelected)).toBeTruthy();
+    });
+  });
+
+  describe('remove buttons', async () => {
+    it('should show an remove button for each job role', async () => {
+      const { getAllByText } = await setup();
+
+      expect(getAllByText('Remove')).toHaveSize(2);
+    });
+
+    it('should not display remove buttons if allowRemoveJobRole is given as false', async () => {
+      const { queryAllByText } = await setup({ allowRemoveJobRole: false });
+
+      expect(queryAllByText('Remove')).toHaveSize(0);
+    });
+
+    it('should call removeJobRole with the job role index when the remove button is clicked', async () => {
+      const { component, fixture, getAllByText } = await setup();
+      const removeJobRoleSpy = spyOn(component.removeJobRole, 'emit');
+
+      userEvent.click(getAllByText('Remove')[1]);
+
+      fixture.detectChanges();
+
+      expect(removeJobRoleSpy).toHaveBeenCalledWith(1);
+    });
+
+    it('should update the total number when a job role is removed', async () => {
+      const { component, fixture, getByTestId, getAllByText } = await setup();
+
+      const removeJobRoleSpy = spyOn(component.removeJobRole, 'emit');
+      removeJobRoleSpy.and.callFake((index) => {
+        component.selectedJobRoles.splice(index, 1);
+        component.jobRoleNumbers.removeAt(index);
+      });
+
+      userEvent.click(getAllByText('Remove')[1]);
+      fixture.detectChanges();
+
+      expect(getByTestId('total-number').textContent).toEqual(`${mockSelectedJobRoles[0].total}`);
+
+      userEvent.click(getAllByText('Remove')[0]);
+      fixture.detectChanges();
+
+      expect(getByTestId('total-number').textContent).toEqual('0');
+    });
   });
 });
--- frontend/src/app/shared/components/job-role-numbers-table/job-role-numbers-table.component.ts
+++ frontend/src/app/shared/components/job-role-numbers-table/job-role-numbers-table.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, QueryList, ViewChildren } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+  ViewChildren,
+  OnInit,
+  AfterViewInit,
+} from '@angular/core';
 import { UntypedFormArray } from '@angular/forms';
 import { Leaver, Starter, Vacancy } from '@core/model/establishment.model';
 import lodash from 'lodash';
@@ -9,7 +19,7 @@ import { NumberInputWithButtonsComponent } from '../number-input-with-buttons/nu
   templateUrl: './job-role-numbers-table.component.html',
   styleUrl: './job-role-numbers-table.component.scss',
 })
-export class JobRoleNumbersTableComponent {
+export class JobRoleNumbersTableComponent implements OnInit, AfterViewInit {
   @ViewChildren('numberInputRef') numberInputs: QueryList<NumberInputWithButtonsComponent>;
   @Input() tableTitle: string;
   @Input() addJobRoleButtonText: string = 'Add job roles';
@@ -19,33 +29,54 @@ export class JobRoleNumbersTableComponent {
   @Input() jobRoleNumbers: UntypedFormArray;
   @Input() jobRoleErrorMessages: Array<string>;
   @Input() selectedJobRoles: Array<Vacancy | Starter | Leaver>;
-  @Input() allowRemoveJobRole: false;
+  @Input() allowRemoveJobRole: boolean = true;
   @Input() submitted: boolean = false;
+
+  @Output() addJobRole = new EventEmitter();
+  @Output() removeJobRole = new EventEmitter();
 
   public minNumberPerJobRole = 1;
   public maxNumberPerJobRole = 999;
   public totalNumber = 0;
 
+  constructor(protected cd: ChangeDetectorRef) {}
+
   ngOnInit() {
     const inputValues = this.jobRoleNumbers.value as Array<number | null>;
-    this.totalNumber = inputValues.reduce((total, current) => (current ? total + current : total), 0);
+    this.totalNumber = lodash.sum(inputValues);
   }
 
   ngAfterViewInit() {
     this.numberInputs.forEach((input) => input.registerOnChange(() => this.updateTotalNumber()));
     this.numberInputs.changes.subscribe(() => {
-      // this.setupFormErrorsMap(); // rebuild form errors map as job role index changed
       this.updateTotalNumber();
     });
-
-    // this.errorSummaryService.formEl$.next(this.formEl);
   }
 
-  public handleAddJobRole(): void {}
+  public handleAddJobRole(): void {
+    this.addJobRole.emit();
+  }
+
+  public handleRemoveJobRole(index: number): void {
+    this.removeJobRole.emit(index);
+  }
 
   private updateTotalNumber(): void {
     const allJobRoleNumbers =
       this.numberInputs?.map((input) => input.currentNumber).filter((number) => !isNaN(number)) ?? [];
     this.totalNumber = lodash.sum(allJobRoleNumbers);
+    this.cd.detectChanges();
+  }
+
+  public get currentValues(): Vacancy[] {
+    return this.selectedJobRoles.map((job, index) => {
+      const { jobId, title } = job;
+      const currentNumber = this.numberInputs.get(index).currentNumber;
+      return {
+        jobId,
+        title,
+        total: isNaN(currentNumber) ? null : currentNumber,
+      };
+    });
   }
 }
