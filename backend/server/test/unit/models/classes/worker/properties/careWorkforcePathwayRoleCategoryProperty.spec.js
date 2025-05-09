842b172779499d7c4a5dbc4b37092c384f338c8f
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2025-05-09 12:56:33 +0100

    Add test for savePropertyToSequelize  of care workforce pathway role category

--- backend/server/test/unit/models/classes/worker/properties/careWorkforcePathwayRoleCategoryProperty.spec.js
+++ backend/server/test/unit/models/classes/worker/properties/careWorkforcePathwayRoleCategoryProperty.spec.js
@@ -2,21 +2,6 @@ const expect = require('chai').expect;
 const careWorkforcePathwayRoleCategoryPropertyClass =
   require('../../../../../../models/classes/worker/properties/careWorkforcePathwayRoleCategoryProperty').CareWorkforcePathwayRoleCategoryProperty;
 
-const careWorkforcePathwayRoleCategories = [
-  {
-    ID: 1,
-    Seq: 10,
-    Title: 'New to care',
-    Description: "Is in a care-providing role that's a start point for a career in social care",
-    AnalysisFileCode: 1,
-    BulkUploadCode: 1,
-  },
-];
-
-const careWorkforcePayload = {
-  Title: 'Care or support worker',
-  Description: "Is established in their role, they've consolidated their skills and experience",
-};
 
 describe('careWorkforcePathwayRoleCategories Property', () => {
   describe('restoreFromJSON', async () => {
@@ -46,30 +31,51 @@ describe('careWorkforcePathwayRoleCategories Property', () => {
 
   describe('restorePropertyFromSequelize()', async () => {
     it("shouldn't return anything if undefined", async () => {
-        const careWorkforcePathwayRoleCategoryProperty = new careWorkforcePathwayRoleCategoryPropertyClass();
+      const careWorkforcePathwayRoleCategoryProperty = new careWorkforcePathwayRoleCategoryPropertyClass();
       const document = {};
-      await careWorkforcePathwayRoleCategoryProperty.restorePropertyFromSequelize(document);
+      careWorkforcePathwayRoleCategoryProperty.restorePropertyFromSequelize(document);
       expect(careWorkforcePathwayRoleCategoryProperty.property).to.deep.equal(null);
     });
 
-
     it('should return with correct value for care workforce pathway role category ', async () => {
+      const careWorkforcePathwayRoleCategoryProperty = new careWorkforcePathwayRoleCategoryPropertyClass();
 
-        const careWorkforcePathwayRoleCategoryProperty = new careWorkforcePathwayRoleCategoryPropertyClass();
-
-        const document = {
-          careWorkforcePathwayRoleCategory: { id: 1, title: 'New to care', description: "Is in a care-providing role that's a start point for a career in social care"},
-
-        };
-
-        const expectedReturnValue = {
-          roleCategoryId: 1,
+      const document = {
+        careWorkforcePathwayRoleCategory: {
+          id: 1,
           title: 'New to care',
           description: "Is in a care-providing role that's a start point for a career in social care",
-        };
-        const restoredProperty = await careWorkforcePathwayRoleCategoryProperty.restorePropertyFromSequelize(document);
-        expect(restoredProperty).to.deep.equal(expectedReturnValue);
+        },
+      };
 
+      const expectedReturnValue = {
+        roleCategoryId: 1,
+        title: 'New to care',
+        description: "Is in a care-providing role that's a start point for a career in social care",
+      };
+      const restoredProperty = await careWorkforcePathwayRoleCategoryProperty.restorePropertyFromSequelize(document);
+      expect(restoredProperty).to.deep.equal(expectedReturnValue);
+    });
+  });
+
+  describe('savePropertyToSequelize()', async () => {
+    it('should save in correct format as if saving into database for care workforce pathway role category ', () => {
+      const careWorkforcePathwayRoleCategoryProperty = new careWorkforcePathwayRoleCategoryPropertyClass();
+      const document = {
+        careWorkforcePathwayRoleCategory: {
+          id: 1,
+          title: 'New to care',
+          description: "Is in a care-providing role that's a start point for a career in social care",
+        },
+      };
+      const property = {
+        roleCategoryId: 1,
+        title: 'New to care',
+        description: "Is in a care-providing role that's a start point for a career in social care",
+      };
+      careWorkforcePathwayRoleCategoryProperty.property = property;
+      const saved = careWorkforcePathwayRoleCategoryProperty.savePropertyToSequelize();
+      expect(saved.CareWorkforcePathwayRoleCategoryFK).to.equal(document.careWorkforcePathwayRoleCategory.id);
     });
   });
 });
