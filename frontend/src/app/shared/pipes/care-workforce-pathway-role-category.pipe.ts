9dfcf013613e24a6bb9aeda57c1998acdd45fbaa
Joe Fong <joe.fong@madetech.com>
2025-05-15 17:15:33 +0100

    add a pipe to convert CWP role category titles for summary page

---
+++ frontend/src/app/shared/pipes/care-workforce-pathway-role-category.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { CareWorkforcePathwayRoleCategory } from '@core/model/careWorkforcePathwayCategory.model';
+
+@Pipe({
+  name: 'CWPRoleCategoryTitle',
+})
+export class CareWorkforcePathwayRoleCategoryPipe implements PipeTransform {
+  transform(value: CareWorkforcePathwayRoleCategory): string {
+    if (!value) {
+      return '-';
+    }
+
+    switch (value.title) {
+      case 'I do not know':
+        return 'Not known';
+
+      case 'None of the above':
+        return 'Role not included yet';
+
+      default:
+        return value.title;
+    }
+  }
+}
