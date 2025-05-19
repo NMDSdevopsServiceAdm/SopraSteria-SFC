9cb4bf7d4373287410cea73668335d268cc16f46
Joe Fong <joe.fong@madetech.com>
2025-05-19 16:27:34 +0100

    start adding a component for the new page

---
+++ frontend/src/app/features/workers/care-workforce-pathway-summary/care-workforce-pathway-summary.component.spec.ts
@@ -0,0 +1,68 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { getTestBed } from '@angular/core/testing';
+import { Router, RouterModule } from '@angular/router';
+import { EstablishmentService } from '@core/services/establishment.service';
+import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
+import { SharedModule } from '@shared/shared.module';
+import { render } from '@testing-library/angular';
+
+import { CareWorkforcePathwaySummaryComponent } from './care-workforce-pathway-summary.component';
+
+fdescribe('CareWorkforcePathwaySummaryComponent', () => {
+  const setup = async (overrides: any = {}) => {
+    const setuptools = await render(CareWorkforcePathwaySummaryComponent, {
+      imports: [SharedModule, RouterModule, HttpClientTestingModule],
+      providers: [
+        {
+          provide: EstablishmentService,
+          useClass: MockEstablishmentService,
+        },
+      ],
+    });
+
+    const fixture = setuptools.fixture;
+    const component = fixture.componentInstance;
+
+    const injector = getTestBed();
+    const establishmentService = injector.inject(EstablishmentService) as EstablishmentService;
+    const router = injector.inject(Router) as Router;
+
+    return {
+      ...setuptools,
+      component,
+      fixture,
+      establishmentService,
+      router,
+    };
+  };
+
+  it('should create', async () => {
+    const { component } = await setup();
+    expect(component).toBeTruthy();
+  });
+
+  it('should show a h1 heading', async () => {
+    const { getByRole } = await setup();
+
+    const h1Heading = getByRole('heading', { level: 1 });
+    expect(h1Heading).toBeTruthy();
+    expect(h1Heading.textContent).toEqual('Where are your staff on the care workforce pathway?');
+  });
+
+  it('should show a reveal text to explain what is the care workforce pathway', async () => {
+    const reveal = "What's the care workforce pathway (CWP)?";
+    const revealText = [
+      'The care workforce pathway outlines the knowledge, skills, values and behaviours needed for a career in adult social care. It provides a clear career structure for your staff.',
+      "You'll use the pathway to set out how staff can gain skills, learn and develop, and progress in their careers.",
+      'Read more about the care workforce pathway',
+    ];
+
+    const { getByTestId } = await setup();
+
+    const revealElement = getByTestId('reveal-whatsCareWorkforcePathway');
+    expect(revealElement.textContent).toContain(reveal);
+    revealText.forEach((paragraph) => {
+      expect(revealElement.textContent).toContain(paragraph);
+    });
+  });
+});
---
+++ frontend/src/app/features/workers/care-workforce-pathway-summary/care-workforce-pathway-summary.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-care-workforce-pathway-summary',
+  templateUrl: './care-workforce-pathway-summary.component.html',
+  styleUrl: './care-workforce-pathway-summary.component.scss',
+})
+export class CareWorkforcePathwaySummaryComponent {}
