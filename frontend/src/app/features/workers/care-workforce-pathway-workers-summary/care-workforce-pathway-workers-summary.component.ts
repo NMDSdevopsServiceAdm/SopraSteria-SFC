e9354c7af53edf6e8027a695d30f9c9c30fa8336
Joe Fong <joe.fong@madetech.com>
2025-05-21 09:29:50 +0100

    remove unwanted scss file, minor refactor for component

--- frontend/src/app/features/workers/care-workforce-pathway-workers-summary/care-workforce-pathway-workers-summary.component.ts
+++ frontend/src/app/features/workers/care-workforce-pathway-workers-summary/care-workforce-pathway-workers-summary.component.ts
@@ -1,51 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { JobRole } from '@core/model/job.model';
 import { BackLinkService } from '@core/services/backLink.service';
 import { CareWorkforcePathwayService } from '@core/services/care-workforce-pathway.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { WorkerService } from '@core/services/worker.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-care-workforce-pathway-workers-summary',
   templateUrl: './care-workforce-pathway-workers-summary.component.html',
-  styleUrl: './care-workforce-pathway-workers-summary.component.scss',
 })
-export class CareWorkforcePathwayWorkersSummaryComponent implements OnInit {
-  private workplaceUid: string;
+export class CareWorkforcePathwayWorkersSummaryComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription = new Subscription();
   public workersToShow: Array<{ uid: string; nameOrId: string; mainJob: JobRole }> = [];
 
   constructor(
     private establishmentService: EstablishmentService,
     private workerService: WorkerService,
     private backLinkService: BackLinkService,
-    private route: ActivatedRoute,
     private router: Router,
     private careWorkforcePathwayService: CareWorkforcePathwayService,
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.backLinkService.showBackLink();
-    this.workplaceUid = this.establishmentService.establishment.uid;
     this.getWorkers();
   }
 
-  private getWorkers() {
-    this.careWorkforcePathwayService
-      .getAllWorkersWhoRequireCareWorkforcePathwayRoleAnswer(this.workplaceUid)
-      .subscribe((response) => {
-        if (response.workers?.length) {
-          this.workersToShow = response.workers;
-        }
-      });
+  private getWorkers(): void {
+    const workplaceUid = this.establishmentService.establishment.uid;
+
+    this.subscriptions.add(
+      this.careWorkforcePathwayService
+        .getAllWorkersWhoRequireCareWorkforcePathwayRoleAnswer(workplaceUid)
+        .subscribe((response) => {
+          if (response?.workers?.length) {
+            this.workersToShow = response.workers;
+          }
+        }),
+    );
   }
 
-  public setReturnToThisPage() {
+  public setReturnToThisPage(): void {
     const urlOfThisPage = this.router.url;
     this.workerService.setReturnTo({ url: [urlOfThisPage] });
   }
 
-  public returnToHome() {
+  public returnToHome(): void {
     this.router.navigate(['/dashboard'], { fragment: 'home' });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
