cf190e2352f156791fa29b74600a6461d04ccf82
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2025-05-27 17:04:52 +0100

    Add test for the careWorkforcePathwayRoleCategory  findAll

--- backend/server/test/unit/routes/establishments/bulkUpload/download.spec.js
+++ backend/server/test/unit/routes/establishments/bulkUpload/download.spec.js
@@ -72,6 +72,7 @@ describe('download', () => {
     };
     const worker = apiWorkerBuilder();
     const downloadType = 'workers';
+    sinon.stub(models.careWorkforcePathwayRoleCategory, 'findAll').resolves([]);
     const downloadWorkers = sinon.stub(models.establishment, 'downloadWorkers').returns([
       {
         ...establishment,
