848399f4fd33a1fb08ecd15637cb2701d0002d38
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2025-05-28 11:35:16 +0100

    remove test

--- backend/server/test/unit/models/Bulkimport/csv/index.spec.js
+++ backend/server/test/unit/models/Bulkimport/csv/index.spec.js
@@ -11,33 +11,34 @@ describe('/server/models/Bulkimport/BUDI/index.js', () => {
     sinon.restore();
   });
 
-  describe('Care Workforce Pathway Category', async () => {
-    const careWorkforcePathwayRoleCategory = [
-      {
-        id: 1,
-        seq: 10,
-        title: 'New to care',
-        description: "Is in a care-providing role that's a start point for a career in social care",
-        analysisFileCode: 1,
-        bulkUploadCode: 1,
-      },
-      {
-        id: 7,
-        seq: 70,
-        title: 'Registered manager',
-        description: 'Is focussed on regulatory and legal requirements, and runs the service',
-        analysisFileCode: 7,
-        bulkUploadCode: 7,
-      },
-    ];
+  // describe('Care Workforce Pathway Category', async () => {
+  //   const careWorkforcePathwayRoleCategory = [
+  //     {
+  //       id: 1,
+  //       seq: 10,
+  //       title: 'New to care',
+  //       description: "Is in a care-providing role that's a start point for a career in social care",
+  //       analysisFileCode: 1,
+  //       bulkUploadCode: 1,
+  //     },
+  //     {
+  //       id: 7,
+  //       seq: 70,
+  //       title: 'Registered manager',
+  //       description: 'Is focussed on regulatory and legal requirements, and runs the service',
+  //       analysisFileCode: 7,
+  //       bulkUploadCode: 7,
+  //     },
+  //   ];
 
-    it('should return bulk upload code for the care workforce pathway category', async () => {
-      sinon.stub(dbmodels.careWorkforcePathwayRoleCategory, 'findAll').resolves(careWorkforcePathwayRoleCategory);
-      await BUDI.initialize();
+  //   it('should return bulk upload code for the care workforce pathway category', async () => {
+  //     sinon.stub(dbmodels.careWorkforcePathwayRoleCategory, 'findAll').resolves(careWorkforcePathwayRoleCategory);
 
-      const roleCategory = 1;
-      const bulkUploadCode = BUDI.careWorkforcePathwayRoleCategory(BUDI.FROM_ASC, roleCategory);
-      expect(bulkUploadCode).to.equal(1);
-    });
-  });
+  //     await BUDI.initialize();
+
+  //     const roleCategory = 1;
+  //     const bulkUploadCode = BUDI.careWorkforcePathwayRoleCategory(BUDI.FROM_ASC, roleCategory);
+  //     expect(bulkUploadCode).to.equal(1);
+  //   });
+  // });
 });
