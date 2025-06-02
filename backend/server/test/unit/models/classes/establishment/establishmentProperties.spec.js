beb7d968674085dda845715fe7f5ab189b86e18f
Joe Fong <joe.fong@madetech.com>
2025-06-02 16:19:33 +0100

    replace hardcoded value in test with properties.length

--- backend/server/test/unit/models/classes/establishment/establishmentProperties.spec.js
+++ backend/server/test/unit/models/classes/establishment/establishmentProperties.spec.js
@@ -13,7 +13,7 @@ describe('EstablishmentPropertyManager', () => {
     'ServiceUsersProperty',
     'CapacityProperty',
     'ShareWithProperty',
-    'ShareWithLAProperty',
+    // 'ShareWithLAProperty',    // seems to be not using anymore
     'VacanciesProperty',
     'StartersProperty',
     'LeaversProperty',
@@ -31,7 +31,7 @@ describe('EstablishmentPropertyManager', () => {
   ];
   it('should have the correct property types', () => {
     const establishmentProperties = new EstablishmentPropertyManager();
-    expect(establishmentProperties._thisManager._propertyTypes.length).to.deep.equal(21);
+    expect(establishmentProperties._thisManager._propertyTypes.length).to.deep.equal(properties.length);
 
     establishmentProperties._thisManager._propertyTypes.forEach((propertyType) => {
       expect(typeof propertyType).to.deep.equal('function');
