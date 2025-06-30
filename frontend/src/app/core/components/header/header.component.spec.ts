9995f9c41227ba4d734020fcc74b72a0ad5aa405
Duncan Carter <duncancarter93@gmail.com>
2025-06-30 10:09:57 +0100

    Refactor header spec file

--- frontend/src/app/core/components/header/header.component.spec.ts
+++ frontend/src/app/core/components/header/header.component.spec.ts
@@ -20,15 +20,19 @@ import { of } from 'rxjs';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  async function setup(isAdmin = false, subsidiaries = 0, isLoggedIn = false, showNotificationsLink = true) {
+  async function setup(overrides: any = {}) {
+    const isAdmin = overrides.isAdmin ?? false;
     const role = isAdmin ? Roles.Admin : Roles.Edit;
-    const { fixture, getByText, queryByText, getByTestId, queryByTestId } = await render(HeaderComponent, {
+    const isLoggedIn = overrides.isLoggedIn ?? false;
+    const showNotificationsLink = overrides.showNotificationsLink ?? true;
+
+    const setupTools = await render(HeaderComponent, {
       imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [HeaderComponent],
       providers: [
         {
           provide: UserService,
-          useFactory: MockUserService.factory(subsidiaries, role),
+          useFactory: MockUserService.factory(overrides.subsidiaries ?? 0, role),
           deps: [HttpClient],
         },
         {
@@ -54,15 +58,11 @@ describe('HeaderComponent', () => {
     const router = injector.inject(Router) as Router;
     const userService = injector.inject(UserService) as UserService;
 
-    const component = fixture.componentInstance;
+    const component = setupTools.fixture.componentInstance;
 
     return {
+      ...setupTools,
       component,
-      fixture,
-      getByText,
-      queryByText,
-      getByTestId,
-      queryByTestId,
       router,
       userService,
     };
@@ -75,13 +75,13 @@ describe('HeaderComponent', () => {
 
   describe('My account link', () => {
     it('should show my name when logged in', async () => {
-      const { getByText } = await setup(false, 0, true);
+      const { getByText } = await setup({ isLoggedIn: true });
 
       expect(getByText('John')).toBeTruthy();
     });
 
     it('should render my name link with the correct href', async () => {
-      const { getByText } = await setup(false, 0, true);
+      const { getByText } = await setup({ isLoggedIn: true });
       const nameLink = getByText('John');
 
       expect(nameLink.getAttribute('href')).toEqual('/account-management');
@@ -90,26 +90,26 @@ describe('HeaderComponent', () => {
 
   describe('Back to admin link', () => {
     it('should display a Back to admin link if user is an admin and is logged in', async () => {
-      const { getByText } = await setup(true, 0, true);
+      const { getByText } = await setup({ isLoggedIn: true, isAdmin: true });
 
       expect(getByText('Back to admin')).toBeTruthy();
     });
 
     it('admin link should go to the admin area if user is an admin and is logged in', async () => {
-      const { getByText } = await setup(true, 0, true);
+      const { getByText } = await setup({ isLoggedIn: true, isAdmin: true });
 
       const adminText = getByText('Back to admin');
       expect(adminText.getAttribute('href')).toEqual('/sfcadmin');
     });
 
     it('should not display a Back to admin link if user is logged in but not an admin', async () => {
-      const { queryByText } = await setup(false, 0, true);
+      const { queryByText } = await setup({ isLoggedIn: true });
 
       expect(queryByText('Back to admin')).toBeNull();
     });
 
     it('should not display a Back to admin link if user is not logged in', async () => {
-      const { queryByText } = await setup(true, 0, false);
+      const { queryByText } = await setup({ isLoggedIn: false, isAdmin: true });
 
       expect(queryByText('Back to admin')).toBeNull();
     });
@@ -117,13 +117,13 @@ describe('HeaderComponent', () => {
 
   describe('Users link', () => {
     it('should show a users link when logged in and on a workplace', async () => {
-      const { getByText } = await setup(false, 0, true);
+      const { getByText } = await setup({ isLoggedIn: true });
 
       expect(getByText('Users')).toBeTruthy();
     });
 
     it('should render users link with the correct href when on a workplace', async () => {
-      const { component, getByText } = await setup(false, 0, true);
+      const { component, getByText } = await setup({ isLoggedIn: true });
 
       const workplaceId = component.workplaceId;
       const usersLink = getByText('Users');
@@ -132,7 +132,7 @@ describe('HeaderComponent', () => {
     });
 
     it('should render users link with the correct href and a notification flag when on a workplace and only one user is registered', async () => {
-      const { component, getByText, getByTestId } = await setup(false, 0, true);
+      const { component, getByText, getByTestId } = await setup({ isLoggedIn: true });
 
       const workplaceId = component.workplaceId;
       const usersLink = getByText('Users');
@@ -142,7 +142,7 @@ describe('HeaderComponent', () => {
     });
 
     it('should render users link with the correct href and no notification flag when on a workplace and more than one user is registered', async () => {
-      const { component, fixture, getByText, queryByTestId, userService } = await setup(false, 0, true);
+      const { component, fixture, getByText, queryByTestId, userService } = await setup({ isLoggedIn: true });
 
       spyOn(userService, 'getAllUsersForEstablishment').and.returnValue(of([EditUser(), EditUser()] as UserDetails[]));
       component.getUsers();
@@ -156,7 +156,7 @@ describe('HeaderComponent', () => {
     });
 
     it('should not show a users link when logged in and on the admin pages', async () => {
-      const { component, fixture, queryByText } = await setup(true, 0, true);
+      const { component, fixture, queryByText } = await setup({ isLoggedIn: true, isAdmin: true });
 
       component.isOnAdminScreen = true;
       fixture.detectChanges();
@@ -165,7 +165,7 @@ describe('HeaderComponent', () => {
     });
 
     it('should not show a users link when logged in and there is no workplace id', async () => {
-      const { component, fixture, queryByText } = await setup(true, 0, true);
+      const { component, fixture, queryByText } = await setup({ isLoggedIn: true, isAdmin: true });
 
       component.workplaceId = '';
       fixture.detectChanges();
@@ -176,7 +176,7 @@ describe('HeaderComponent', () => {
 
   describe('notifications link', () => {
     it('should not show the notifications link when logged in, in a stand alone account but not on admin screen', async () => {
-      const { component, fixture, queryByText } = await setup(false, 0, true);
+      const { component, fixture, queryByText } = await setup({ isLoggedIn: true });
 
       component.isOnAdminScreen = true;
       fixture.detectChanges();
@@ -185,7 +185,7 @@ describe('HeaderComponent', () => {
     });
 
     it('should show the new notifications flag if there are new notifications', async () => {
-      const { component, fixture, getByTestId } = await setup(false, 0, true);
+      const { component, fixture, getByTestId } = await setup({ isLoggedIn: true });
 
       spyOnProperty(component, 'numberOfNewNotifications', 'get').and.returnValue(1);
       fixture.detectChanges();
@@ -194,7 +194,7 @@ describe('HeaderComponent', () => {
     });
 
     it('should not show the new notifications flag if there are no new notifications', async () => {
-      const { component, fixture, queryByTestId } = await setup(false, 0, true);
+      const { component, fixture, queryByTestId } = await setup({ isLoggedIn: true });
 
       spyOnProperty(component, 'numberOfNewNotifications', 'get').and.returnValue(0);
       fixture.detectChanges();
