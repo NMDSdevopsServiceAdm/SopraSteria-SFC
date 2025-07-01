73cd6e14b74ae0cdeee94e5fb70da11ab4b06cc8
Sabrina <sabrina.samuel@madetech.com>
2025-07-01 15:28:55 +0100

    Remove shareData endpoint

--- frontend/src/app/features/workplace/data-sharing/data-sharing.component.ts
+++ frontend/src/app/features/workplace/data-sharing/data-sharing.component.ts
@@ -67,15 +67,14 @@ export class DataSharingComponent extends Question {
 
   protected updateEstablishment(props: ShareWithRequest): void {
     const completeUpdateEstablishment = () => {
+      const data = { property: 'ShareData', objectToUpdate: props };
       this.subscriptions.add(
-        this.establishmentService.updateDataSharing(this.establishment.uid, props).subscribe(
-          (data) => {
-            this._onSuccess(data);
-          },
+        this.establishmentService.updateEstablishmentFieldWithAudit(this.establishment.uid, data).subscribe(
+          (data) => this._onSuccess(data),
           (error) => this.onError(error),
         ),
       );
-    };
+    };;
 
     this.establishment.showSharingPermissionsBanner
       ? this.removeSharingPermissionsBanner(completeUpdateEstablishment)
