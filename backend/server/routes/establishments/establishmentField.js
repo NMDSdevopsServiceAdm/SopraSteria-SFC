d6f5c9e5149e70d4810c6215711cd348c0587936
Sabrina <sabrina.samuel@madetech.com>
2025-07-01 14:27:11 +0100

    Add endpoint to update establishment field with audit

---
+++ backend/server/routes/establishments/establishmentField.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const Establishment = require('../../models/classes/establishment');
+const { hasPermission } = require('../../utils/security/hasPermission');
+const HttpError = require('../../utils/errors/httpError');
+
+let filteredProperties = ['Name']
+
+const getEstablishmentField = async (req, res) => {
+  const establishmentId = req.establishmentId;
+  const property = req.query?.property;
+
+  filteredProperties.push(property)
+
+  const showHistory =
+    req.query.history === 'full' || req.query.history === 'property' || req.query.history === 'timeline' ? true : false;
+  const showHistoryTime = req.query.history === 'timeline' ? true : false;
+  const showPropertyHistoryOnly = req.query.history === 'property' ? true : false;
+
+  const thisEstablishment = new Establishment.Establishment(req.username);
+  try {
+    if (await thisEstablishment.restore(establishmentId, showHistory)) {
+      return res
+        .status(200)
+        .json(
+          thisEstablishment.toJSON(
+            showHistory,
+            showPropertyHistoryOnly,
+            showHistoryTime,
+            false,
+            false,
+            filteredProperties,
+          ),
+        );
+    } else {
+      return res.status(404).send('Not Found');
+    }
+  } catch (error) {
+    const thisError = new Establishment.EstablishmentExceptions.EstablishmentRestoreException(
+      thisEstablishment.id,
+      thisEstablishment.uid,
+      null,
+      error,
+      null,
+      `Failed to retrieve Establishment with id/uid: ${establishmentId}`,
+    );
+
+    console.error(`establishment::${property} GET/:eID - failed`, thisError.message);
+    return res.status(500).send(thisError.safe);
+  }
+};
+
+const updateEstablishmentFieldWithAudit = async (req, res) => {
+  const establishmentId = req.establishmentId;
+  const thisEstablishment = new Establishment.Establishment(req.username);
+
+  const property = req.body.property;
+  const objectToUpdate = req.body.objectToUpdate
+
+  filteredProperties.push(property)
+
+  try {
+    const establishmentFound = await thisEstablishment.restore(establishmentId);
+
+    if (!establishmentFound) {
+      throw new HttpError('Establishment not found', 404);
+    }
+
+    const isValidEstablishment = await thisEstablishment.load(objectToUpdate);
+
+    if (!isValidEstablishment) {
+      throw new HttpError('Request is invalid', 400);
+    }
+
+    await thisEstablishment.save(req.username);
+    return res.status(200).json(thisEstablishment.toJSON(false, false, false, true, false, filteredProperties));
+
+
+  } catch (error) {
+    console.error(`Establishment::${property} POST: `, error.message);
+    if (error instanceof HttpError) {
+      return res.status(error.statusCode).send(error.message);
+    }
+    return res.status(500).send(`Failed to update ${property} for workplace`);
+  }
+};
+
+router.route('/').get(hasPermission('canViewEstablishment'), getEstablishmentField);
+router.route('/').post(hasPermission('canEditEstablishment'), updateEstablishmentFieldWithAudit);
+
+module.exports = router;
+module.exports.getEstablishmentField = getEstablishmentField;
+module.exports.updateEstablishmentFieldWithAudit = updateEstablishmentFieldWithAudit;
