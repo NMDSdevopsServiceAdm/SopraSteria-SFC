f52653a5cb253e878b473fb885f1a8bd267ef200
Sabrina <sabrina.samuel@madetech.com>
2025-07-07 16:50:20 +0100

    Add functionto summary section for your other workplaces sort

---
+++ frontend/src/app/core/services/workplace.service.spec.ts
@@ -0,0 +1,34 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { WorkplaceService } from './workplace.service';
+import { TestBed } from '@angular/core/testing';
+
+describe('WorkplaceService', () => {
+  let service: WorkplaceService;
+  let http: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [WorkplaceService],
+    });
+    service = TestBed.inject(WorkplaceService);
+
+    http = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    TestBed.inject(HttpTestingController).verify();
+  });
+
+  it('should create the service', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should set the international recruitment worker answers', () => {
+    const allWorkplaceSortValue = 'workplaceToCheckAsc';
+
+    service.setAllWorkplaceSortValue(allWorkplaceSortValue);
+
+    expect(service.getAllWorkplaceSortValue()).toBe(allWorkplaceSortValue);
+  });
+});
