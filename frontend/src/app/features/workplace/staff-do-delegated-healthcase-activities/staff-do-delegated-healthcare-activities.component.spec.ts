db6fe120e073889494b4d1536cd864c867a9af81
Duncan Carter <duncancarter93@gmail.com>
2025-07-16 17:03:13 +0100

    Add progress bar and set previous and next pages

--- frontend/src/app/features/workplace/staff-do-delegated-healthcase-activities/staff-do-delegated-healthcare-activities.component.spec.ts
+++ frontend/src/app/features/workplace/staff-do-delegated-healthcase-activities/staff-do-delegated-healthcare-activities.component.spec.ts
@@ -1,30 +1,34 @@
-import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { getTestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import { CareWorkforcePathwayService } from '@core/services/care-workforce-pathway.service';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { BackService } from '@core/services/back.service';
 import { EstablishmentService } from '@core/services/establishment.service';
-import { MockCareWorkforcePathwayService } from '@core/test-utils/MockCareWorkforcePathwayService';
 import { MockEstablishmentServiceWithOverrides } from '@core/test-utils/MockEstablishmentService';
+import { MockRouter } from '@core/test-utils/MockRouter';
 import { SharedModule } from '@shared/shared.module';
 import { render, within } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+import { of } from 'rxjs';
 
 import { StaffDoDelegatedHealthcareActivitiesComponent } from './staff-do-delegated-healthcare-activities.component';
 
 fdescribe('StaffDoDelegatedHealthcareActivitiesComponent', () => {
   async function setup(overrides: any = {}) {
+    const routerSpy = jasmine.createSpy().and.resolveTo(true);
+    const backServiceSpy = jasmine.createSpyObj('BackService', ['setBackLink']);
+
     const setupTools = await render(StaffDoDelegatedHealthcareActivitiesComponent, {
       imports: [SharedModule, RouterModule, HttpClientTestingModule, ReactiveFormsModule],
       providers: [
         UntypedFormBuilder,
         {
           provide: EstablishmentService,
-          useFactory: MockEstablishmentServiceWithOverrides.factory(overrides),
-          deps: [HttpClient],
+          useFactory: MockEstablishmentServiceWithOverrides.factory(overrides.establishmentService ?? {}),
         },
         {
-          provide: CareWorkforcePathwayService,
-          useClass: MockCareWorkforcePathwayService,
+          provide: BackService,
+          useValue: backServiceSpy,
         },
         {
           provide: ActivatedRoute,
@@ -34,14 +38,29 @@ fdescribe('StaffDoDelegatedHealthcareActivitiesComponent', () => {
             },
           },
         },
+        {
+          provide: Router,
+          useFactory: MockRouter.factory({
+            navigate: routerSpy,
+          }),
+        },
       ],
     });
 
     const component = setupTools.fixture.componentInstance;
 
+    const injector = getTestBed();
+    const establishmentService = injector.inject(EstablishmentService);
+    const establishmentServiceSpy = spyOn(establishmentService, 'updateSingleEstablishmentField').and.returnValue(
+      of({ ...establishmentService.establishment }),
+    );
+
     return {
       ...setupTools,
       component,
+      routerSpy,
+      establishmentServiceSpy,
+      backServiceSpy,
     };
   }
 
@@ -67,6 +86,45 @@ fdescribe('StaffDoDelegatedHealthcareActivitiesComponent', () => {
       ['Yes', 'No', 'I do not know'].forEach((label) => {
         expect(getByRole('radio', { name: label })).toBeTruthy();
       });
+    });
+  });
+
+  describe('When in new workplace workflow', async () => {
+    const overrides = { establishmentService: { returnTo: null } };
+
+    it('should show a progress bar', async () => {
+      const { getByTestId } = await setup(overrides);
+
+      expect(getByTestId('progress-bar')).toBeTruthy();
+    });
+
+    it('should show a "Save and continue" cta button and "Skip this question" link', async () => {
+      const { getByText } = await setup(overrides);
+
+      expect(getByText('Save and continue')).toBeTruthy();
+      expect(getByText('Skip this question')).toBeTruthy();
+    });
+
+    it('should set the previous page to service users question page', async () => {
+      const { component } = await setup(overrides);
+
+      expect(component.previousRoute).toEqual(['/workplace', 'mocked-uid', 'service-users']);
+    });
+
+    it('should set the back link to the service users question', async () => {
+      const { backServiceSpy } = await setup(overrides);
+
+      expect(backServiceSpy.setBackLink).toHaveBeenCalledWith({
+        url: ['/workplace', 'mocked-uid', 'service-users'],
+      });
+    });
+
+    it('should navigate to repeat-training page when user skips the question', async () => {
+      const { getByText, routerSpy } = await setup(overrides);
+
+      userEvent.click(getByText('Skip this question'));
+
+      expect(routerSpy).toHaveBeenCalledWith(['/workplace', 'mocked-uid', 'repeat-training']);
     });
   });
 });
