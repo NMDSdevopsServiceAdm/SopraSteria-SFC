e228564ed660a50fc8dfba83d61c5f2cbd751be6
Joe Fong <joe.fong@madetech.com>
2025-07-17 14:21:40 +0100

    add a component for the worker title in new format

---
+++ frontend/src/app/features/pdf/pdf-worker-title/pdf-worker-title.component.ts
@@ -0,0 +1,18 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Establishment } from '@core/model/establishment.model';
+import { Worker } from '@core/model/worker.model';
+import { PdfComponent } from '@core/services/pdf.service';
+
+@Component({
+  selector: 'app-pdf-worker-title',
+  templateUrl: './pdf-worker-title.component.html',
+  styleUrls: ['./pdf-worker-title.component.scss'],
+})
+export class PdfWorkerTitleComponent implements PdfComponent {
+  @ViewChild('content') public content: ElementRef;
+  @Input() workplace: Establishment;
+  @Input() worker: Worker;
+  @Input() lastUpdatedDate: Date;
+
+  constructor() {}
+}
