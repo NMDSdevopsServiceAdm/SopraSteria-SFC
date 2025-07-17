99bb61afab10ff347ed6d5c59c197065484e82fb
Sabrina <sabrina.samuel@madetech.com>
2025-07-17 16:27:37 +0100

    Refactor tests

--- frontend/src/app/shared/components/staff-summary/staff-summary.component.spec.ts
+++ frontend/src/app/shared/components/staff-summary/staff-summary.component.spec.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
-import { Router, RouterModule } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter, Router, RouterModule } from '@angular/router';
 import { Establishment } from '@core/model/establishment.model';
 import { Worker } from '@core/model/worker.model';
 import { PermissionsService } from '@core/services/permissions/permissions.service';
@@ -12,35 +11,35 @@ import { establishmentBuilder } from '@core/test-utils/MockEstablishmentService'
 import { MockPermissionsService } from '@core/test-utils/MockPermissionsService';
 import { workerBuilder } from '@core/test-utils/MockWorkerService';
 import { SharedModule } from '@shared/shared.module';
-import { fireEvent, render } from '@testing-library/angular';
+import { fireEvent, render, within } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
 import { of } from 'rxjs';
-
 import { PaginationComponent } from '../pagination/pagination.component';
 import { TablePaginationWrapperComponent } from '../table-pagination-wrapper/table-pagination-wrapper.component';
 import { StaffSummaryComponent } from './staff-summary.component';
+import { PermissionType } from '@core/model/permissions.model';
 
 describe('StaffSummaryComponent', () => {
-  async function setup(isWdf = false) {
+  async function setup(overrides: any = {}) {
     const establishment = establishmentBuilder() as Establishment;
     const workers = [workerBuilder(), workerBuilder(), workerBuilder()] as Worker[];
-
-    const component = await render(StaffSummaryComponent, {
-      imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
+    const setupTools = await render(StaffSummaryComponent, {
+      imports: [SharedModule, RouterModule, HttpClientTestingModule],
       declarations: [PaginationComponent, TablePaginationWrapperComponent],
       providers: [
         {
           provide: PermissionsService,
-          useFactory: MockPermissionsService.factory(),
+          useFactory: MockPermissionsService.factory(overrides.permissions as PermissionType[]),
           deps: [HttpClient, Router, UserService],
         },
         WorkerService,
+        provideRouter([]),
       ],
       componentProperties: {
         workplace: establishment,
         workers: workers,
-        wdfView: isWdf,
-        workerCount: workers.length,
+        wdfView: overrides.isWdf ?? false,
+        workerCount: overrides.workerCount ?? workers.length,
       },
     });
 
@@ -52,7 +51,10 @@ describe('StaffSummaryComponent', () => {
       of({ workers: [...workers, ...workers, ...workers, ...workers, ...workers, ...workers], workerCount: 18 }),
     );
 
+    const component = setupTools.fixture.componentInstance;
+
     return {
+      ...setupTools,
       router,
       component,
       workers,
@@ -66,28 +68,29 @@ describe('StaffSummaryComponent', () => {
   });
 
   it('should render the correct information for given workers', async () => {
-    const { component, workers } = await setup();
+    const overrides = { permissions: ['canEditWorker'] };
+    const { workers, fixture, getByText, getByTestId } = await setup(overrides);
 
-    component.fixture.componentInstance.canEditWorker = true;
     // update one of the fake workers
     workers[0].nameOrId = 'joe mocked';
     workers[0].mainJob.jobRoleName = 'fake doctor';
     workers[0].completed = false;
-    component.detectChanges();
+    fixture.detectChanges();
 
-    expect(component.getByText('joe mocked')).toBeTruthy();
-    expect(component.getByText('fake doctor')).toBeTruthy();
-    expect(component.getAllByText('Add more details').length).toBe(3);
+    const staffTableTestId = getByTestId('staff-table');
+
+    expect(getByText('joe mocked')).toBeTruthy();
+    expect(getByText('fake doctor')).toBeTruthy();
+    expect(within(staffTableTestId).getAllByText('Add more details').length).toBe(3);
   });
 
   it('should render the add more details link with the correct routing', async () => {
-    const { component, workers } = await setup();
+    const overrides = { permissions: ['canEditWorker'] };
 
-    component.fixture.componentInstance.canEditWorker = true;
-    component.detectChanges();
+    const { component, workers, getAllByText } = await setup(overrides);
 
-    const workplace = component.fixture.componentInstance.workplace;
-    const addMoreDetailsLinks = component.getAllByText('Add more details');
+    const workplace = component.workplace;
+    const addMoreDetailsLinks = getAllByText('Add more details');
 
     workers.map((worker, index) => {
       expect(addMoreDetailsLinks[index].getAttribute('href')).toEqual(
@@ -97,47 +100,48 @@ describe('StaffSummaryComponent', () => {
   });
 
   describe('Calling getAllWorkers when sorting', () => {
-    const sortByOptions = [
-      ['0_asc', 'staffNameAsc', 'Staff name (A to Z)'],
-      ['0_dsc', 'staffNameDesc', 'Staff name (Z to A)'],
-      ['1_asc', 'jobRoleAsc', 'Job role (A to Z)'],
-      ['1_dsc', 'jobRoleDesc', 'Job role (Z to A)'],
-      ['2_meeting', 'wdfMeeting', 'WDF requirements (meeting)'],
-      ['2_not_meeting', 'wdfNotMeeting', 'WDF requirements (not meeting)'],
-    ];
+    describe('in wdf view', () => {
+      const sortByOptions = [
+        ['0_asc', 'staffNameAsc', 'Staff name (A to Z)'],
+        ['0_dsc', 'staffNameDesc', 'Staff name (Z to A)'],
+        ['1_asc', 'jobRoleAsc', 'Job role (A to Z)'],
+        ['1_dsc', 'jobRoleDesc', 'Job role (Z to A)'],
+        ['2_meeting', 'wdfMeeting', 'Funding requirements (meeting)'],
+        ['2_not_meeting', 'wdfNotMeeting', 'Funding requirements (not meeting)'],
+      ];
 
-    for (const option of sortByOptions) {
-      it(`should call getAllWorkers with sortBy set to ${option[1]} when sorting by ${option[2]}`, async () => {
-        const { component, getAllWorkersSpy } = await setup(true);
+      for (const option of sortByOptions) {
+        it(`should call getAllWorkers with sortBy set to ${option[1]} when sorting by ${option[2]}`, async () => {
+          const { component, getAllWorkersSpy, getByLabelText } = await setup({ isWdf: true });
 
-        const select = component.getByLabelText('Sort by', { exact: false });
-        fireEvent.change(select, { target: { value: option[0] } });
+          const select = getByLabelText('Sort by', { exact: false });
+          fireEvent.change(select, { target: { value: option[0] } });
 
-        const establishmentUid = component.fixture.componentInstance.workplace.uid;
-        const paginationEmission = { pageIndex: 0, itemsPerPage: 15, sortBy: option[1] };
+          const establishmentUid = component.workplace.uid;
+          const paginationEmission = { pageIndex: 0, itemsPerPage: 15, sortBy: option[1] };
 
-        expect(getAllWorkersSpy.calls.mostRecent().args[0]).toEqual(establishmentUid);
-        expect(getAllWorkersSpy.calls.mostRecent().args[1]).toEqual(paginationEmission);
-      });
-    }
+          expect(getAllWorkersSpy.calls.mostRecent().args[0]).toEqual(establishmentUid);
+          expect(getAllWorkersSpy.calls.mostRecent().args[1]).toEqual(paginationEmission);
+        });
+      }
+    });
   });
 
   describe('Calling getAllWorkers when using search', () => {
     it('it does not render the search bar when pagination threshold is not met', async () => {
-      const { component } = await setup();
+      const { queryByLabelText } = await setup();
 
-      const searchInput = component.queryByLabelText('Search staff training records');
+      const searchInput = queryByLabelText('Search staff training records');
       expect(searchInput).toBeNull();
     });
     it('should call getAllWorkers with correct search term if passed and workerCount is greater than itemsPerPage', async () => {
-      const { component, getAllWorkersSpy } = await setup();
+      const overrides = { workerCount: 16 };
 
-      await component.fixture.whenStable();
-      // show search
-      component.fixture.componentInstance.totalWorkerCount = 16;
-      component.fixture.detectChanges();
+      const { getAllWorkersSpy, fixture, getByLabelText } = await setup(overrides);
 
-      const searchInput = component.getByLabelText('Search by name or ID number for staff records');
+      await fixture.whenStable();
+
+      const searchInput = getByLabelText('Search by name or ID number for staff records');
       expect(searchInput).toBeTruthy();
       userEvent.type(searchInput, 'search term here{enter}');
 
@@ -146,42 +150,34 @@ describe('StaffSummaryComponent', () => {
     });
 
     it('should reset the pageIndex before calling getAllWorkers when handling search', async () => {
-      const { component, getAllWorkersSpy } = await setup();
+      const overrides = { workerCount: 16 };
 
-      component.fixture.componentInstance.totalWorkerCount = 16;
-      component.fixture.detectChanges();
+      const { getAllWorkersSpy, getByLabelText } = await setup(overrides);
 
-      userEvent.type(
-        component.getByLabelText('Search by name or ID number for staff records'),
-        'search term here{enter}',
-      );
+      userEvent.type(getByLabelText('Search by name or ID number for staff records'), 'search term here{enter}');
       expect(getAllWorkersSpy.calls.mostRecent().args[1].pageIndex).toEqual(0);
     });
 
     it('should render the message that no workers were found if workerCount is falsy', async () => {
-      const { component } = await setup();
+      const overrides = { workerCount: 0 };
+      const { getByText } = await setup(overrides);
 
-      component.fixture.componentInstance.totalWorkerCount = 16;
-      component.fixture.componentInstance.workerCount = 0;
-      component.fixture.detectChanges();
-
-      expect(component.getByText('There are no matching results')).toBeTruthy();
-      expect(component.getByText('Make sure that your spelling is correct.')).toBeTruthy();
+      expect(getByText('There are no matching results')).toBeTruthy();
+      expect(getByText('Make sure that your spelling is correct.')).toBeTruthy();
     });
   });
 
   describe('getWorkerRecordPath', () => {
     it('navigates to the staff record summary when not in the wdf view', async () => {
-      const { component, router, workers } = await setup();
+      const overrides = { permissions: ['canViewWorker'] };
+      const { component, router, workers, getByText } = await setup(overrides);
 
-      component.fixture.componentInstance.canViewWorker = true;
-      component.detectChanges();
       const routerSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
       const worker = workers[0];
-      const nameLink = component.getByText(worker.nameOrId);
+      const nameLink = getByText(worker.nameOrId);
       fireEvent.click(nameLink);
 
-      const workplace = component.fixture.componentInstance.workplace;
+      const workplace = component.workplace;
       expect(routerSpy).toHaveBeenCalledWith([
         '/workplace',
         workplace.uid,
