485a6a976374271e59519e7527f75260a61974a0
Duncan Carter <duncancarter93@gmail.com>
2025-07-17 15:13:18 +0100

    Refactor service-users spec file before adding changes to component

--- frontend/src/app/features/workplace/service-users/service-users.component.spec.ts
+++ frontend/src/app/features/workplace/service-users/service-users.component.spec.ts
@@ -1,19 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
-import { UntypedFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { ServiceUsersService } from '@core/services/service-users.service';
 import { MockBreadcrumbService } from '@core/test-utils/MockBreadcrumbService';
-import { MockEstablishmentService } from '@core/test-utils/MockEstablishmentService';
+import { MockEstablishmentServiceWithOverrides } from '@core/test-utils/MockEstablishmentService';
 import { QuestionComponent } from '@features/workers/question/question.component';
 import { SubmitButtonComponent } from '@shared/components/submit-button/submit-button.component';
 import { SharedModule } from '@shared/shared.module';
-import { fireEvent, queryByText, render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 import { of } from 'rxjs';
 
 import { ServiceUsersComponent } from './service-users.component';
@@ -62,34 +62,37 @@ const mockServiceUser = [
     ],
   },
 ];
-describe('ServiceUsersComponent', () => {
-  const setup = async () => {
-    const { fixture, getByText, getAllByText, getByTestId, queryByText, queryByTestId } = await render(
-      ServiceUsersComponent,
-      {
-        imports: [RouterTestingModule, HttpClientTestingModule, BrowserModule, SharedModule, ReactiveFormsModule],
-        providers: [
-          { provide: BreadcrumbService, useClass: MockBreadcrumbService },
-          {
-            provide: EstablishmentService,
-            useClass: MockEstablishmentService,
-          },
 
-          UntypedFormBuilder,
-          ErrorSummaryService,
-          SubmitButtonComponent,
-          QuestionComponent,
-        ],
-      },
-    );
-    const component = fixture.componentInstance;
+describe('ServiceUsersComponent', () => {
+  const setup = async (overrides: any = {}) => {
+    const getServiceUsersSpy = jasmine.createSpy('getServiceUsers').and.returnValue(of(mockServiceUser));
+
+    const setupTools = await render(ServiceUsersComponent, {
+      imports: [HttpClientTestingModule, BrowserModule, SharedModule, ReactiveFormsModule],
+      providers: [
+        { provide: BreadcrumbService, useClass: MockBreadcrumbService },
+        {
+          provide: EstablishmentService,
+          useFactory: MockEstablishmentServiceWithOverrides.factory(overrides),
+          deps: [HttpClient],
+        },
+        {
+          provide: ServiceUsersService,
+          useValue: { getServiceUsers: getServiceUsersSpy },
+        },
+        UntypedFormBuilder,
+        ErrorSummaryService,
+        SubmitButtonComponent,
+        QuestionComponent,
+      ],
+    });
+    const component = setupTools.fixture.componentInstance;
     const injector = getTestBed();
-    const userServiceSpy = injector.inject(ServiceUsersService) as ServiceUsersService;
-    spyOn(userServiceSpy, 'getServiceUsers').and.returnValue(of(mockServiceUser));
+
     const router = injector.inject(Router) as Router;
     const routerSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
 
-    return { component, fixture, getByText, getAllByText, getByTestId, queryByText, queryByTestId, routerSpy };
+    return { ...setupTools, component, routerSpy };
   };
 
   it('should render an ServiceUsersComponent', async () => {
@@ -98,45 +101,31 @@ describe('ServiceUsersComponent', () => {
   });
 
   it('should render the section, the question but not the progress bar when not in the flow', async () => {
-    const { getByText, getByTestId, queryByTestId, fixture, component } = await setup();
-    component['init']();
+    const { getByText, getByTestId, queryByTestId } = await setup();
 
-    fixture.detectChanges();
     expect(getByTestId('section-heading')).toBeTruthy();
     expect(getByText('Who are your service users?')).toBeTruthy();
     expect(queryByTestId('progress-bar')).toBeFalsy();
   });
 
   it('should render the progress bar when in the flow', async () => {
-    const { component, fixture, getByTestId } = await setup();
-    component['init']();
-
-    component.return = null;
-    fixture.detectChanges();
+    const { getByTestId } = await setup({ returnToUrl: null });
 
     expect(getByTestId('progress-bar')).toBeTruthy();
   });
 
   describe(`'Save and continue' and skip question`, () => {
     it(`should show 'Save and continue' cta button and 'Skip this question' link`, async () => {
-      const { getByText, component, fixture, queryByText } = await setup();
-      component['init']();
-
-      component.return = null;
-      fixture.detectChanges();
+      const { getByText } = await setup({ returnToUrl: null });
 
       expect(getByText('Save and continue')).toBeTruthy();
       expect(getByText('Skip this question')).toBeTruthy();
     });
 
     it(`should call the setSubmitAction function with an action of skip and save as false when clicking 'Skip this question' link`, async () => {
-      const { component, fixture, getByText } = await setup();
-      component['init']();
+      const { component, getByText } = await setup({ returnToUrl: null });
 
       const setSubmitActionSpy = spyOn(component, 'setSubmitAction').and.callThrough();
-
-      component.return = null;
-      fixture.detectChanges();
 
       const link = getByText('Skip this question');
       fireEvent.click(link);
@@ -145,11 +134,7 @@ describe('ServiceUsersComponent', () => {
     });
 
     it('should navigate to the next page when skip the question', async () => {
-      const { fixture, getByText, routerSpy, component } = await setup();
-      component['init']();
-
-      component.return = null;
-      fixture.detectChanges();
+      const { getByText, routerSpy } = await setup({ returnToUrl: null });
 
       const link = getByText('Skip this question');
       fireEvent.click(link);
