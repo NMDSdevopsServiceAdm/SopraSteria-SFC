7c665421b26feddda848defcd69b3c596f74e93f
Joe Fong <joe.fong@madetech.com>
2025-08-11 15:54:32 +0100

    add a sequelize hook to clear worker DHA answer on staffDoDelegatedHealthcareActivities change

--- backend/server/models/classes/establishment.js
+++ backend/server/models/classes/establishment.js
@@ -1133,6 +1133,7 @@ class Establishment extends EntityValidator {
             },
             attributes: ['id', 'updated'],
             transaction: thisTransaction,
+            savedBy: savedBy.toLowerCase(),
           });
 
           if (updatedRecordCount === 1) {
