a49cb3e2e7eb6ca76991030b2e0f68f6d5370912
Sabrina <sabrina.samuel@madetech.com>
2025-08-13 11:20:26 +0100

    Add model and endpoint to establishment service

--- frontend/src/app/core/model/delegated-healthcare-activites.model.ts
+++ frontend/src/app/core/model/delegated-healthcare-activites.model.ts
@@ -8,3 +8,20 @@ export interface DelegatedHealthcareActivity {
 export interface DelegatedHealthcareActivitiesResponse {
   allDHAs: DelegatedHealthcareActivity[];
 }
+
+export interface StaffKindDelegatedHealthcareActivity {
+  id: number;
+}
+
+export type StaffWhatKindDelegatedHealthcareActivities = YES | DONT_KNOW | null;
+export type UpdateStaffKindDelegatedHealthcareActivitiesPayload = YES | DONT_KNOW;
+
+type YES = {
+  whatDelegateHealthcareActivities: 'Yes';
+  activities: Array<StaffKindDelegatedHealthcareActivity> | null;
+};
+
+type DONT_KNOW = {
+  whatDelegateHealthcareActivities: "Don't know";
+  activities: null;
+};
