0f409a8d8cce15e79adb1aa96120475f3fe98d69
Sabrina <sabrina.samuel@madetech.com>
2025-08-18 16:27:26 +0100

    Add pipe to format what DHA on workplace summary

---
+++ frontend/src/app/shared/pipes/format-what-dha.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { StaffWhatKindDelegatedHealthcareActivities } from '@core/model/delegated-healthcare-activities.model';
+
+@Pipe({
+  name: 'formatWhatDHA',
+})
+export class FormatWhatDhaPipe implements PipeTransform {
+  transform(whatDHA: StaffWhatKindDelegatedHealthcareActivities): string | Array<string> {
+    if (!whatDHA) {
+      return 'None selected';
+    }
+
+    const { knowWhatActivities } = whatDHA;
+
+    switch (knowWhatActivities) {
+      case "Don't know":
+        return 'Not known';
+
+      default:
+        return 'None selected';
+    }
+  }
+}
