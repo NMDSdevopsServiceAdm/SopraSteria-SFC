0475ee8c55fd539c3df29f04743696623dba9abe
Joe Fong <joe.fong@madetech.com>
2025-08-29 17:02:06 +0100

    add topic folder for delegated healthcare activities, separate dhaHomeTabFlag spec file for each kind of establishment

---
+++ frontend/cypress/e2e/delegatedHealthcareActivities/dhaHomeTabFlagForParentViewSub.spec.cy.js
@@ -0,0 +1,17 @@
+import { ParentEstablishment, SubEstablishmentNotDataOwner } from '../../support/mockEstablishmentData';
+import { runTestsForDHAHomeTabFlag } from './runTestsForDHAHomeTabFlag';
+
+describe('Delegated healthcare activities journey from home tab flag', () => {
+  const subsidaryToView = SubEstablishmentNotDataOwner;
+
+  beforeEach(() => {
+    cy.loginAsUser(ParentEstablishment.editUserLoginName, Cypress.env('userPassword'));
+
+    cy.get('app-navigate-to-workplace-dropdown select').select(subsidaryToView.name);
+
+    cy.url().should('contain', 'subsidiary');
+    cy.get('h1').should('contain', subsidaryToView.name);
+  });
+
+  runTestsForDHAHomeTabFlag(subsidaryToView);
+});
