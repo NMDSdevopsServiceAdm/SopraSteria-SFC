f9ef5ebd8e04d25e53d60ada103fcddfdb38ea3e
Joe Fong <joe.fong@madetech.com>
2025-08-29 17:03:06 +0100

    amend setup of existing tests, ensure there are worker in workplace before test start

--- frontend/cypress/e2e/trainingAndQualifications/trainingAndQualificationsPageForSub.spec.cy.js
+++ frontend/cypress/e2e/trainingAndQualifications/trainingAndQualificationsPageForSub.spec.cy.js
@@ -1,9 +1,21 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
 
+const { SubEstablishment } = require('../../support/mockEstablishmentData');
 const { onHomePage } = require('../../support/page_objects/onHomePage');
 
 describe('Sub training and quals page as edit user', () => {
+  const workerName = 'test worker for training & quals';
+  const establishmentID = SubEstablishment.id;
+
+  before(() => {
+    cy.insertTestWorker({ establishmentID, workerName });
+  });
+
+  after(() => {
+    cy.deleteTestWorkerFromDb(workerName);
+  });
+
   beforeEach(() => {
     cy.loginAsUser(Cypress.env('editSubUser'), Cypress.env('userPassword'));
     onHomePage.clickTab('Training and qualifications');
