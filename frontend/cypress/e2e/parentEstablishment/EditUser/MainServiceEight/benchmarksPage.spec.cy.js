8633830f0549f129dbc7f46fb78edb35b6509867
Joe Fong <joe.fong@madetech.com>
2025-08-29 18:38:49 +0100

    amend benchmarksPage.spec.cy.js to setup the necessary main service before test start

--- frontend/cypress/e2e/parentEstablishment/EditUser/MainServiceEight/benchmarksPage.spec.cy.js
+++ frontend/cypress/e2e/parentEstablishment/EditUser/MainServiceEight/benchmarksPage.spec.cy.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
 
+import { ParentEstablishment } from '../../../../support/mockEstablishmentData';
 import { onBenchmarksPage } from '../../../../support/page_objects/onBenchmarksPage';
 import { onHomePage } from '../../../../support/page_objects/onHomePage';
 
 describe('Parent benchmark page, main service 8, as edit user', () => {
+  before(() => {
+    cy.setWorkplaceMainService(ParentEstablishment.id, '20'); // the ID for main service which have "reportingID" =  8
+  });
+
   beforeEach(() => {
     cy.loginAsUser(Cypress.env('editParentMainServiceEight'), Cypress.env('userPassword'));
     onHomePage.clickTab('Benchmarks');
