4e987295e2d627992d177e76900e4eed15e73634
tess-barnes-mt <tess.barnes@madetech.com>
2025-09-01 13:13:40 +0100

    Add new shared flexible pagination component

---
+++ frontend/src/app/shared/components/worker-pagination/worker-pagination.component.spec.ts
@@ -0,0 +1,98 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { WorkerPaginationComponent } from '@shared/components/worker-pagination/worker-pagination.component';
+import { SharedModule } from '@shared/shared.module';
+import { render } from '@testing-library/angular';
+import { Observable, Subject } from 'rxjs';
+
+describe('WorkerPagination', () => {
+  const setup = async (overrides: any = {}) => {
+    const { fixture, getByText, getAllByText, getByTestId, queryByText } = await render(WorkerPaginationComponent, {
+      imports: [RouterTestingModule, HttpClientTestingModule, BrowserModule, SharedModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: Observable.from([{ id: overrides.id ?? '123' }]),
+
+            snapshot: {
+              params: {
+                establishmentuid: '',
+              },
+              paramMap: {
+                get(id) {
+                  return overrides.id ?? '123';
+                },
+              },
+            },
+          },
+        },
+      ],
+      componentProperties: {
+        workerList: ['1', '2', '3', '4'],
+        exitUrl: overrides.exitUrl ?? { url: 'dashboard', fragment: 'staff-records' },
+        staffSummaryBaseUrl: overrides.staffSummaryBaseUrl ?? {url : ['/workplace,1,staff-record']},
+        staffSummaryUrlSuffix: overrides.staffSummaryUrlSuffix ?? 'staff-record-summary'
+      },
+    });
+
+    const injector = getTestBed();
+    const event = new NavigationEnd(42, '/', '/');
+    (injector.inject(Router).events as unknown as Subject<RouterEvent>).next(event);
+
+    const component = fixture.componentInstance;
+
+    return { component, fixture, getByText, getAllByText, getByTestId, queryByText };
+  };
+
+  it('should render a WorkerPagination', async () => {
+    const { component, fixture } = await setup();
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+  it('should be able to find next and previous ids', async () => {
+    const { component, fixture } = await setup({ id: '2', staffSummaryBaseUrl: {url: ['..']},  });
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    const previousHref = links[1].nativeElement.getAttribute('ng-reflect-router-link');
+    const nextHref = links[2].nativeElement.getAttribute('ng-reflect-router-link');
+    fixture.detectChanges();
+
+    expect(component.previousID).toEqual('1');
+    expect(component.nextID).toEqual('3');
+    expect(previousHref).toEqual('..,1,staff-record-summary');
+    expect(nextHref).toEqual('..,3,staff-record-summary');
+  });
+
+  it('should only show Next staff record when current staff record is first ID in list', async () => {
+    const { fixture, queryByText } = await setup({ id: '1' });
+    fixture.detectChanges();
+
+    expect(queryByText('Next staff record')).toBeTruthy();
+    expect(queryByText('Previous staff record')).toBeFalsy();
+  });
+
+  it('should only show Previous staff record when current staff record is last ID in list', async () => {
+    const { fixture, queryByText } = await setup({ id: '4' });
+    fixture.detectChanges();
+
+    expect(queryByText('Next staff record')).toBeFalsy();
+    expect(queryByText('Previous staff record')).toBeTruthy();
+  });
+
+  it('should show a Close this staff record link with refs passed in', async () => {
+    const exitUrl = { url: 'dashboard', fragment: 'staff-records' };
+
+    const { component, fixture, getByText } = await setup({ exitUrl });
+
+    const closeLink = getByText('Close this staff record');
+    const closeLinkAnchorElement: DebugElement = fixture.debugElement.query(By.css('a.govuk-button'));
+
+    expect(closeLinkAnchorElement.attributes['ng-reflect-router-link']).toContain(exitUrl.url);
+    expect(closeLinkAnchorElement.attributes['ng-reflect-fragment']).toBe(exitUrl.fragment);
+  });
+});
---
+++ frontend/src/app/shared/components/worker-pagination/worker-pagination.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Establishment } from '@core/model/establishment.model';
+import { URLStructure } from '@core/model/url.model';
+
+@Component({
+  selector: 'app-worker-pagination',
+  templateUrl: './worker-pagination.component.html',
+})
+export class WorkerPaginationComponent implements OnInit {
+  @Input() workerList: string[];
+  @Input() workplaceUid: Establishment;
+  @Input() exitUrl: URLStructure;
+  @Input() staffSummaryBaseUrl: URLStructure;
+  @Input() staffSummaryUrlSuffix: string;
+
+  public isFirst = false;
+  public isLast = false;
+  public nextID: string;
+  public previousID: string;
+  public previousLink: URLStructure;
+  public nextLink: URLStructure;
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((data) => {
+      this.setVariables(data);
+    });
+  }
+  public setVariables(data) {
+    const workerUID = data.id;
+    const workerIndex = this.workerList.findIndex((uid) => uid === workerUID);
+    this.isFirst = workerIndex === 0;
+    this.isLast = workerIndex === this.workerList.length - 1;
+    this.nextID = this.workerList[workerIndex + 1];
+    this.previousID = this.workerList[workerIndex - 1];
+    this.nextLink = this.isLast ? null : this.setLink(this.nextID);
+    this.previousLink = this.isFirst ? null : this.setLink(this.previousID);
+  }
+
+  private setLink(workerUID) {
+
+/**
+ *             this.route.snapshot.params.establishmentuid
+              ? ['/funding', 'workplaces', workplaceUid, 'staff-record', nextID]
+              : ['/funding', 'staff-record', nextID]
+ */
+
+
+    const urlStructure = {url: [].concat(this.staffSummaryBaseUrl.url)};
+    urlStructure.url.push(workerUID);
+    if (this.staffSummaryUrlSuffix) urlStructure.url.push(this.staffSummaryUrlSuffix);
+    return urlStructure;
+  }
+}
