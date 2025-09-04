e5f6aa1867cebc894a3f98945c604a59eba5102f
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2025-09-04 17:02:48 +0100

    Add the pdfmake  styles for the training

--- frontend/src/app/core/services/pdf-make.service.ts
+++ frontend/src/app/core/services/pdf-make.service.ts
@@ -56,16 +56,31 @@ export class PdfMakeService {
         {
           columns: [
             { image: 'sfcLogo', alignment: 'left' },
+
             {
-              text: 'Adult Social Care Workforce Data Set',
-              margin: [20, 20, 40, 0],
-              width: '75%',
-              padding: 15,
-              bold: true,
-              background: '#1d70b8', // todo: use govuk vars
-              color: '#ffffff',
+              table: {
+                body: [
+                  [
+                    {
+                      text: 'Adult Social Care Workforce Data Set',
+                      bold: true,
+                      color: '#ffffff',
+                    },
+                  ],
+                ],
+              },
+              layout: {
+                fillColor: '#1d70b8',
+                paddingTop: () => 5,
+                paddingBottom: () => 5,
+                hLineWidth: () => 0,
+                vLineWidth: () => 0,
+              },
+              width: '70%',
+              margin: [5, 20, 0, 20],
             },
           ],
+
           margin: [0, -50, 20, 20],
         },
         {
@@ -81,14 +96,11 @@ export class PdfMakeService {
             },
           ],
         },
-        {
-          text: 'Training and qualifications',
-          style: 'h1Heading',
-          margin: [0, 20],
-        },
+        ...this.contentBody.content,
       ],
 
       styles: {
+        ...this.contentBody.styles,
         header: {
           fontSize: 18,
           bold: true,
@@ -111,6 +123,239 @@ export class PdfMakeService {
       },
     };
   }
+
+  public sectionHeader(title: string) {
+    return {
+      stack: [
+        {
+          text: title,
+          bold: true,
+          style: 'header',
+          margin: [0, 15, 0, 15], // space below title before line
+        },
+        {
+          canvas: [
+            {
+              type: 'line',
+              x1: 0,
+              y1: 0,
+              x2: 515, // ~A4 width minus margins
+              y2: 0,
+              lineWidth: 1,
+              lineColor: '#cccccc', // grey line
+            },
+          ],
+          margin: [0, 0, 0, 15], // space after line
+        },
+      ],
+    };
+  }
+
+  public staffInfoSection(staff) {
+    return {
+      table: {
+        widths: ['*', '*'],
+        body: [
+          [
+            { text: 'Workplace name', bold: true },
+            { text: 'Staff name', bold: true },
+          ],
+          [staff.workplace, staff.name],
+          [
+            { text: 'Care certificate', bold: true, margin: [0, 10, 0, 0] },
+            { text: 'Last updated', bold: true, margin: [0, 10, 0, 0] },
+          ],
+
+          [staff.certificate, staff.lastUpdated],
+        ],
+      },
+      layout: 'headerLineOnly',
+      margin: [0, 5, 0, 20],
+    };
+  }
+
+  public trainingSection(trainings) {
+    return [
+      {
+        stack: [
+          {
+            text: 'Mandatory training',
+            style: 'subheader',
+            margin: [0, 10, 0, 10],
+          },
+          {
+            canvas: [
+              {
+                type: 'line',
+                x1: 0,
+                y1: 0,
+                x2: 515, // ~A4 width minus margins
+                y2: 0,
+                lineWidth: 1,
+                lineColor: '#cccccc', // grey line
+              },
+            ],
+            margin: [0, 0, 0, 15], // space after line
+          },
+        ],
+      },
+      ...trainings.flatMap((training) => [
+        { text: training.title, style: 'subheader', margin: [0, 10, 0, 5] },
+        {
+          table: {
+            widths: ['*', '*', '*', '*', '*'],
+            body: [
+              [
+                { text: 'Training name', bold: true },
+                { text: 'Accredited', bold: true },
+                { text: 'Completion date', bold: true },
+                { text: 'Expiry date', bold: true },
+                { text: 'Certificate', bold: true },
+              ],
+              [
+                training.details.Training,
+                training.details.Accredited,
+                training.details.Completion,
+                training.details.Expiry,
+                training.details.Certificate,
+              ],
+            ],
+          },
+          layout: 'headerLineOnly',
+        },
+        {
+          canvas: [
+            {
+              type: 'line',
+              x1: 0,
+              y1: 0,
+              x2: 515,
+              y2: 0, // full width (A4 minus margins)
+              lineWidth: 1,
+              lineColor: '#cccccc', // grey
+            },
+          ],
+          margin: [0, 5, 0, 10], // spacing above and below line
+        },
+      ]),
+    ];
+  }
+
+  public staffInfo = {
+    name: 'Adele Singh',
+    workplace: 'Highfield Hall',
+    certificate: 'Yes, completed',
+    lastUpdated: '26 June 2025',
+  };
+
+  public trainings = [
+    {
+      title: 'Basic life support and first aid',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '15 Aug 2024',
+        Expiry: '14 Aug 2027',
+        Certificate: 'See download',
+      },
+    },
+    {
+      title: 'Dementia care',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '2 Oct 2023',
+        Expiry: '1 Oct 2025',
+        Certificate: 'No',
+      },
+    },
+    {
+      title: 'Dementia care',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '2 Oct 2023',
+        Expiry: '1 Oct 2025',
+        Certificate: 'No',
+      },
+    },
+    {
+      title: 'Dementia care',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '2 Oct 2023',
+        Expiry: '1 Oct 2025',
+        Certificate: 'No',
+      },
+    },
+    {
+      title: 'Dementia care',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '2 Oct 2023',
+        Expiry: '1 Oct 2025',
+        Certificate: 'No',
+      },
+    },
+    {
+      title: 'Dementia care',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '2 Oct 2023',
+        Expiry: '1 Oct 2025',
+        Certificate: 'No',
+      },
+    },
+    {
+      title: 'Dementia care',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '2 Oct 2023',
+        Expiry: '1 Oct 2025',
+        Certificate: 'No',
+      },
+    },
+    {
+      title: 'Dementia care',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '2 Oct 2023',
+        Expiry: '1 Oct 2025',
+        Certificate: 'No',
+      },
+    },
+    {
+      title: 'Dementia care',
+      details: {
+        Training: 'Level 1',
+        Accredited: 'Yes',
+        Completion: '2 Oct 2023',
+        Expiry: '1 Oct 2025',
+        Certificate: 'No',
+      },
+    },
+  ];
+
+  public sections = [
+    () => this.sectionHeader('Training and qualifications'),
+    () => this.staffInfoSection(this.staffInfo),
+    () => this.trainingSection(this.trainings),
+  ];
+
+  public contentBody = {
+    content: this.sections.flatMap((fn) => fn()),
+    styles: {
+      header: { fontSize: 18, bold: true },
+      subheader: { fontSize: 14, bold: true },
+    },
+  };
+
+  // pdfMake.createPdf(docDefinition).download("staff-training.pdf");
 
   public viewPdf(docDefinition: TDocumentDefinitions): void {
     const pdf = pdfMake.createPdf(docDefinition);
