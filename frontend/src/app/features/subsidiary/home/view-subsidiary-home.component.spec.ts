29134bd9fdf765360908cda832fb94193522d443
Joe Fong <joe.fong@madetech.com>
2025-09-04 16:43:42 +0100

    add saveAllWorkerIdsInLocalStorage method to ViewSubsidiaryHomeComponent

--- frontend/src/app/features/subsidiary/home/view-subsidiary-home.component.spec.ts
+++ frontend/src/app/features/subsidiary/home/view-subsidiary-home.component.spec.ts
@@ -3,7 +3,6 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { getTestBed, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
 import { BenchmarksResponse } from '@core/model/benchmarks-v2.model';
 import { Roles } from '@core/model/roles.enum';
 import { TrainingCounts } from '@core/model/trainingAndQualifications.model';
@@ -23,12 +22,14 @@ import { NewArticleListComponent } from '@features/articles/new-article-list/new
 import { SummarySectionComponent } from '@shared/components/summary-section/summary-section.component';
 import { FeatureFlagsService } from '@shared/services/feature-flags.service';
 import { SharedModule } from '@shared/shared.module';
-import { fireEvent, queryByText, render } from '@testing-library/angular';
+import { fireEvent, render } from '@testing-library/angular';
 import { of } from 'rxjs';
 
 import { Establishment } from '../../../../mockdata/establishment';
-import { NewDashboardHeaderComponent } from '../../../shared/components/new-dashboard-header/dashboard-header.component';
+import { NewDashboardHeaderComponent } from '@shared/components/new-dashboard-header/dashboard-header.component';
 import { ViewSubsidiaryHomeComponent } from './view-subsidiary-home.component';
+import { workerBuilder } from '@core/test-utils/MockWorkerService';
+import { Worker } from '@core/model/worker.model';
 
 const MockWindow = {
   dataLayer: {
@@ -38,81 +39,83 @@ const MockWindow = {
   },
 };
 
-describe('ViewSubsidiaryHomeComponent', () => {
-  const setup = async (
-    cqcStatusMatch = true,
-    establishment = Establishment,
-    comparisonDataAvailable = true,
-    noOfWorkplaces = 9,
-  ) => {
-    const { fixture, getByText, queryByText, getByTestId, queryByTestId, getByRole, getByLabelText } = await render(
-      ViewSubsidiaryHomeComponent,
-      {
-        imports: [SharedModule, RouterModule, RouterTestingModule, HttpClientTestingModule],
-        providers: [
-          WindowRef,
-          {
-            provide: FeatureFlagsService,
-            useClass: MockFeatureFlagsService,
-          },
-          {
-            provide: PermissionsService,
-            useFactory: MockPermissionsService.factory(['canViewEstablishment', 'canViewListOfWorkers']),
-            deps: [HttpClient, Router, UserService],
-          },
-          {
-            provide: UserService,
-            useFactory: MockUserService.factory(1, Roles.Admin),
-            deps: [HttpClient],
-          },
-          {
-            provide: BenchmarksV2Service,
-            useValue: {
-              benchmarksData: {
-                newBenchmarks: {
-                  meta: comparisonDataAvailable
-                    ? { workplaces: noOfWorkplaces, staff: 4, localAuthority: 'Test LA' }
-                    : { workplaces: 0, staff: 0, localAuthority: 'Test LA' },
-                } as BenchmarksResponse,
-              },
-            },
-          },
-          {
-            provide: ActivatedRoute,
-            useValue: {
-              snapshot: {
-                data: {
-                  establishment: establishment,
-                  workers: {
-                    workersCreatedDate: [],
-                    workerCount: 0,
-                    trainingCounts: {} as TrainingCounts,
-                    workersNotCompleted: [],
-                  },
-                  cqcStatusCheck: { cqcStatusMatch },
-                },
-              },
-              queryParams: of({ view: null }),
-              url: of(null),
-            },
-          },
-          {
-            provide: EstablishmentService,
-            useClass: MockEstablishmentService,
-          },
-          { provide: WindowToken, useValue: MockWindow },
-        ],
-        declarations: [NewDashboardHeaderComponent, NewArticleListComponent, SummarySectionComponent],
-        componentProperties: {
-          subsidiaryWorkplace: establishment,
-          canViewListOfWorkers: true,
-          canViewEstablishment: true,
-        },
-        schemas: [NO_ERRORS_SCHEMA],
-      },
-    );
+fdescribe('ViewSubsidiaryHomeComponent', () => {
+  const setup = async (overrides: any = {}) => {
+    const cqcStatusMatch = overrides?.cqcStatusMatch ?? true;
+    const establishment = overrides?.establishment ?? Establishment;
+    const comparisonDataAvailable = overrides?.comparisonDataAvailable ?? true;
+    const noOfWorkplaces = overrides?.noOfWorkplaces ?? 9;
 
-    const component = fixture.componentInstance;
+    const workers = [workerBuilder(), workerBuilder(), workerBuilder()] as Worker[];
+    const listOfAllWorkers = overrides?.listOfAllWorkers ?? workers;
+    const localStorageSetSpy = spyOn(localStorage, 'setItem');
+
+    const setupTools = await render(ViewSubsidiaryHomeComponent, {
+      imports: [SharedModule, RouterModule, HttpClientTestingModule],
+      providers: [
+        WindowRef,
+        {
+          provide: FeatureFlagsService,
+          useClass: MockFeatureFlagsService,
+        },
+        {
+          provide: PermissionsService,
+          useFactory: MockPermissionsService.factory(['canViewEstablishment', 'canViewListOfWorkers']),
+          deps: [HttpClient, Router, UserService],
+        },
+        {
+          provide: UserService,
+          useFactory: MockUserService.factory(1, Roles.Admin),
+          deps: [HttpClient],
+        },
+        {
+          provide: BenchmarksV2Service,
+          useValue: {
+            benchmarksData: {
+              newBenchmarks: {
+                meta: comparisonDataAvailable
+                  ? { workplaces: noOfWorkplaces, staff: 4, localAuthority: 'Test LA' }
+                  : { workplaces: 0, staff: 0, localAuthority: 'Test LA' },
+              } as BenchmarksResponse,
+            },
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                establishment: establishment,
+                workers: {
+                  workersCreatedDate: [],
+                  workerCount: 0,
+                  trainingCounts: {} as TrainingCounts,
+                  workersNotCompleted: [],
+                  listOfAllWorkers,
+                },
+                cqcStatusCheck: { cqcStatusMatch },
+              },
+            },
+            queryParams: of({ view: null }),
+            url: of(null),
+          },
+        },
+        {
+          provide: EstablishmentService,
+          useClass: MockEstablishmentService,
+        },
+        { provide: WindowToken, useValue: MockWindow },
+      ],
+      declarations: [NewDashboardHeaderComponent, NewArticleListComponent, SummarySectionComponent],
+      componentProperties: {
+        subsidiaryWorkplace: establishment,
+        canViewListOfWorkers: true,
+        canViewEstablishment: true,
+      },
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+
+    const component = setupTools.fixture.componentInstance;
 
     const parentsRequestService = TestBed.inject(ParentRequestsService);
 
@@ -124,17 +127,12 @@ describe('ViewSubsidiaryHomeComponent', () => {
     const routerSpy = spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
 
     return {
+      ...setupTools,
       component,
-      fixture,
-      getByText,
-      queryByText,
-      getByTestId,
-      queryByTestId,
       parentsRequestService,
       tabsServiceSpy,
-      getByRole,
-      getByLabelText,
       routerSpy,
+      localStorageSetSpy,
     };
   };
 
@@ -207,7 +205,11 @@ describe('ViewSubsidiaryHomeComponent', () => {
         };
 
         it('should show the benchmarks card text non pluralised if there is only one workplace in the comparison data', async () => {
-          const { getByText } = await setup(false, establishment, true, 1);
+          const { getByText } = await setup({
+            cqcStatusMatch: false,
+            establishment,
+            noOfWorkplaces: 1,
+          });
 
           const benchmarksCardText = getByText('There is 1 workplace providing day care and day services in Test LA.');
 
@@ -215,7 +217,11 @@ describe('ViewSubsidiaryHomeComponent', () => {
         });
 
         it('without comparison data, should show a card with a link that takes you to the benchmarks tab', async () => {
-          const { getByText, tabsServiceSpy } = await setup(false, establishment, false);
+          const { getByText, tabsServiceSpy } = await setup({
+            cqcStatusMatch: false,
+            establishment,
+            comparisonDataAvailable: false,
+          });
 
           const benchmarksLink = getByText('See how you compare against other workplaces');
           const benchmarksCardText = getByText(
@@ -241,7 +247,7 @@ describe('ViewSubsidiaryHomeComponent', () => {
           };
 
           it('with comparison data, should show a card with a link that takes you to the benchmarks tab', async () => {
-            const { getByText, tabsServiceSpy } = await setup(false, establishment);
+            const { getByText, tabsServiceSpy } = await setup({ cqcStatusMatch: false, establishment });
 
             const benchmarksLink = getByText(
               'See how your pay, recruitment and retention compares against other workplaces',
@@ -268,7 +274,7 @@ describe('ViewSubsidiaryHomeComponent', () => {
         };
 
         it('should show a card with a link that takes you to the benchmarks tab', async () => {
-          const { getByText, tabsServiceSpy } = await setup(false, establishment);
+          const { getByText, tabsServiceSpy } = await setup({ cqcStatusMatch: false, establishment });
 
           const benchmarksLink = getByText('See how you compare against other workplaces');
           const benchmarksCardText = getByText('There are 9 workplaces providing adult social care in Test LA.');
@@ -280,7 +286,7 @@ describe('ViewSubsidiaryHomeComponent', () => {
         });
 
         it('should show the benchmarks card text non pluralised if there is only one workplace in the comparison data', async () => {
-          const { getByText } = await setup(false, establishment, true, 1);
+          const { getByText } = await setup({ cqcStatusMatch: false, establishment, noOfWorkplaces: 1 });
 
           const benchmarksCardText = getByText('There is 1 workplace providing adult social care in Test LA.');
 
@@ -288,7 +294,11 @@ describe('ViewSubsidiaryHomeComponent', () => {
         });
 
         it('without comparison data, should show a card with a link that takes you to the benchmarks tab', async () => {
-          const { getByText, tabsServiceSpy } = await setup(false, establishment, false);
+          const { getByText, tabsServiceSpy } = await setup({
+            cqcStatusMatch: false,
+            establishment,
+            comparisonDataAvailable: false,
+          });
 
           const benchmarksLink = getByText('See how you compare against other workplaces');
           const benchmarksCardText = getByText(
@@ -334,7 +344,10 @@ describe('ViewSubsidiaryHomeComponent', () => {
 
       it('should show a warning link which should navigate to the workplace tab', async () => {
         const establishment = { ...Establishment, showAddWorkplaceDetailsBanner: true };
-        const { getByText, tabsServiceSpy } = await setup(false, establishment);
+        const { getByText, tabsServiceSpy } = await setup({
+          cqcStatusMatch: false,
+          establishment,
+        });
 
         const link = getByText('Add more details to your workplace');
 
@@ -364,6 +377,30 @@ describe('ViewSubsidiaryHomeComponent', () => {
 
         expect(tabsServiceSpy).toHaveBeenCalledWith('training-and-qualifications');
       });
+    });
+  });
+
+  describe('should prep for individual staff record pagination', () => {
+    beforeEach(() => {
+      localStorage.clear();
+    });
+
+    it('should store a list of all worker ids in localstorage', async () => {
+      const mockWorkers = [workerBuilder(), workerBuilder(), workerBuilder()] as Worker[];
+      const eighteenWorkers = [
+        ...mockWorkers,
+        ...mockWorkers,
+        ...mockWorkers,
+        ...mockWorkers,
+        ...mockWorkers,
+        ...mockWorkers,
+      ];
+      const overrides = { workers: mockWorkers, listOfAllWorkers: eighteenWorkers };
+      const { localStorageSetSpy } = await setup(overrides);
+
+      const expectedStaffRecordIds = JSON.stringify(eighteenWorkers.map((worker) => worker.uid));
+
+      expect(localStorageSetSpy.calls.allArgs()).toContain(['ListOfWorkers', expectedStaffRecordIds]);
     });
   });
 });
