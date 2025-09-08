13741017d841ec1035cb7a81bc184b98919fdbd4
Joe Fong <joe.fong@madetech.com>
2025-09-08 11:35:23 +0100

    add resolved data type to WorkerHasAnyTrainingOrQualificationsResolver

--- frontend/src/app/core/resolvers/worker-has-any-training-or-qualifications.resolver.ts
+++ frontend/src/app/core/resolvers/worker-has-any-training-or-qualifications.resolver.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Router } from '@angular/router';
 import { WorkerService } from '@core/services/worker.service';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+interface ResolvedData {
+  hasAnyTrainingOrQualifications: boolean;
+}
 @Injectable()
-export class WorkerHasAnyTrainingOrQualificationsResolver  {
+export class WorkerHasAnyTrainingOrQualificationsResolver {
   constructor(private router: Router, private workerService: WorkerService) {}
 
-  resolve(route: ActivatedRouteSnapshot) {
+  resolve(route: ActivatedRouteSnapshot): Observable<ResolvedData> {
     return this.workerService
-      .workerHasAnyTrainingOrQualifications(
-        route.paramMap.get('establishmentuid'),
-        route.paramMap.get('id')
-      )
+      .workerHasAnyTrainingOrQualifications(route.paramMap.get('establishmentuid'), route.paramMap.get('id'))
       .pipe(
         catchError(() => {
           this.router.navigate(['/problem-with-the-service']);
