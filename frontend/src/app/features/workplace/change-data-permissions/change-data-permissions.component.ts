b236415baf0af3cbca2f0bc1567852ee311820d6
Sabrina <sabrina.samuel@madetech.com>
2025-09-18 17:13:48 +0100

    Update text to be dynamic for different journeys

--- frontend/src/app/features/workplace/change-data-permissions/change-data-permissions.component.ts
+++ frontend/src/app/features/workplace/change-data-permissions/change-data-permissions.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ErrorDetails, ErrorDefinition } from '@core/model/errorSummary.model';
 import { Establishment } from '@core/model/establishment.model';
 import { DataPermissions } from '@core/model/my-workplaces.model';
+import { AlertService } from '@core/services/alert.service';
 import { BackService } from '@core/services/back.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { EstablishmentService } from '@core/services/establishment.service';
@@ -21,22 +22,28 @@ export class ChangeDataPermissionsComponent implements OnInit, AfterViewInit {
   public formErrorsMap: Array<ErrorDetails>;
   public serverErrorsMap: Array<ErrorDefinition>;
   public submitted = false;
-  public primaryWorkplace: Establishment;
-  public subsidiaryUid: string;
+  public workplace: Establishment;
+  public uidToChangeDataPermissionsFor: string;
   public dataPermissions: DataPermissions[];
   public serverError: string;
   public isParent: boolean;
   public previousRoute: string[];
-  public subsidiaryWorkplace: Establishment;
+  public workplaceNameToChangeDataPermissionsFor: string;
+  public permissionType: string;
+  public currentDataPermission: string;
+  public currentDataOwner: string;
+  public parentName: string;
+  public parentUid: string;
 
   constructor(
     private errorSummaryService: ErrorSummaryService,
     private establishmentService: EstablishmentService,
     private formBuilder: UntypedFormBuilder,
-    // private router: Router,
+    private router: Router,
     public route: ActivatedRoute,
     protected backService: BackService,
-    // private previousRouteService: PreviousRouteService,
+    private previousRouteService: PreviousRouteService,
+    private alertService: AlertService,
   ) {}
 
   ngAfterViewInit() {
@@ -44,11 +51,11 @@ export class ChangeDataPermissionsComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.subsidiaryUid = this.route.snapshot.queryParams?.changeDataPermissionsFor;
-    this.primaryWorkplace = this.route.snapshot.data.establishment;
-    this.isParent = this.primaryWorkplace?.isParent;
-    this.previousRoute = ['/workplace', 'view-all-workplaces'];
-    this.getSubsidiaryWorkplace();
+    this.uidToChangeDataPermissionsFor = this.route.snapshot?.queryParams?.changeDataPermissionsFor;
+    this.workplace = this.route.snapshot.data.establishment;
+    this.isParent = this.workplace?.isParent;
+    this.setPreviousRoute();
+    this.getWorkplaceToChangeDataPermissionsFor();
     this.setupForm();
     this.setupFormErrorsMap();
     this.setupServerErrorsMap();
@@ -58,8 +65,32 @@ export class ChangeDataPermissionsComponent implements OnInit, AfterViewInit {
 
   private setupForm(): void {
     this.form = this.formBuilder.group({
-      dataPermission: [null, { validators: [Validators.required], updateOn: 'submit' }],
+      dataPermission: ['', { validators: [Validators.required], updateOn: 'submit' }],
     });
+  }
+
+  public setPreviousRoute(): void {
+    const previousUrl = this.previousRouteService.getPreviousUrl();
+
+    if (previousUrl === '/workplace/view-all-workplaces') {
+      this.previousRoute = ['/workplace', 'view-all-workplaces'];
+    } else if (previousUrl === '/' || previousUrl === 'home') {
+      this.previousRoute = ['/dashboard'];
+    }
+  }
+
+  public setupVariables(workplace): void {
+    this.currentDataPermission = workplace.dataPermissions;
+    this.currentDataOwner = workplace.dataOwner;
+    this.parentName = workplace.parentName;
+    this.parentUid = workplace.parentUid;
+
+    if (this.uidToChangeDataPermissionsFor) {
+      this.workplaceNameToChangeDataPermissionsFor = workplace.name;
+    } else {
+      this.workplaceNameToChangeDataPermissionsFor = workplace.parentName;
+      this.uidToChangeDataPermissionsFor = workplace.uid;
+    }
   }
 
   private setDataPermissions(): void {
@@ -73,22 +104,58 @@ export class ChangeDataPermissionsComponent implements OnInit, AfterViewInit {
       this.errorSummaryService.scrollToErrorSummary();
       return;
     }
+    this.setPermissions();
   }
 
   protected setBackLink(): void {
     this.backService.setBackLink({ url: this.previousRoute });
   }
 
-  public getSubsidiaryWorkplace(): void {
-    if (this.subsidiaryUid) {
+  public getWorkplaceToChangeDataPermissionsFor(): void {
+    if (this.uidToChangeDataPermissionsFor) {
       this.subscriptions.add(
-        this.establishmentService.getEstablishment(this.subsidiaryUid).subscribe((workplace) => {
+        this.establishmentService.getEstablishment(this.uidToChangeDataPermissionsFor).subscribe((workplace) => {
           if (workplace) {
-            this.subsidiaryWorkplace = workplace;
+            this.setupVariables(workplace);
           }
         }),
       );
+    } else {
+      this.setupVariables(this.workplace);
     }
+  }
+
+  public sendAlert(): void {
+    this.alertService.addAlert({
+      type: 'success',
+      message: `You've changed data permissions for ${this.workplaceNameToChangeDataPermissionsFor}`,
+    });
+  }
+
+  public navigateToUrl(): void {
+    this.router.navigate(this.previousRoute).then(() => {
+      this.sendAlert();
+    });
+  }
+
+  public setPermissions() {
+    this.permissionType = this.form.value.dataPermission;
+
+    const setPermission = {
+      permissionToSet: this.permissionType,
+    };
+    this.subscriptions.add(
+      this.establishmentService.setDataPermission(this.uidToChangeDataPermissionsFor, setPermission).subscribe(
+        (data) => {
+          if (data) {
+            this.navigateToUrl();
+          }
+        },
+        (error) => {
+          this.serverError = this.errorSummaryService.getServerErrorMessage(error.status, this.serverErrorsMap);
+        },
+      ),
+    );
   }
 
   public getFormErrorMessage(item: string, errorType: string): string {
