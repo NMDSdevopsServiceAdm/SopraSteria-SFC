03d2dcac1f72df375456e909fd254efaadb1aec7
Sabrina <sabrina.samuel@madetech.com>
2025-09-19 17:13:39 +0100

    Add e2e tests

---
+++ frontend/cypress/e2e/changeDataPermissions/subChangingDataPermissionsForParent.spec.cy.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+import { SubEstablishment } from '../../support/mockEstablishmentData';
+
+describe('Subsidiary changing data permissions for their parent to view their workplace', () => {
+  beforeEach(() => {
+    cy.loginAsUser(Cypress.env('editSubUser'), Cypress.env('userPassword'));
+    cy.get('[data-cy="tab-list"]').contains('Home').click();
+  });
+
+  const parentName = SubEstablishment.parentName;
+  const radioButtonLabels = [
+    'Only your workplace details',
+    'Your workplace details and your staff records',
+    'No access to your data, linked only',
+  ];
+
+  radioButtonLabels.forEach((radioButtonLabel, index) => {
+    it(`parent permission is changed to ${radioButtonLabel}`, () => {
+      cy.get('a').contains('Change data permissions').click();
+
+      //Change data permissions
+      cy.contains(SubEstablishment.name);
+      cy.contains(parentName);
+
+      cy.getByLabel(radioButtonLabel).click();
+      cy.contains('Save and return').click();
+
+      cy.contains(`You've changed data permissions for ${parentName}`);
+    });
+  });
+});
