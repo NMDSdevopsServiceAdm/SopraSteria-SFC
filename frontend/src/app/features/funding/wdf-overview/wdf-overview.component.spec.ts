0ec06c6b042587d3436dd5482e1f0c90af6b2766
Joe Fong <joe.fong@madetech.com>
2025-10-01 16:22:19 +0100

    fix unit tests for WdfOverviewComponent

--- frontend/src/app/features/funding/wdf-overview/wdf-overview.component.spec.ts
+++ frontend/src/app/features/funding/wdf-overview/wdf-overview.component.spec.ts
@@ -2,7 +2,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { UserService } from '@core/services/user.service';
@@ -14,6 +14,7 @@ import { fireEvent, render } from '@testing-library/angular';
 import { of } from 'rxjs';
 
 import { WdfOverviewComponent } from './wdf-overview.component';
+import { provideActivatedRouteWithRouterLink } from '@core/test-utils/MockActivatedRoute';
 
 describe('WdfOverviewComponent', () => {
   const setup = async (overrides: any = {}) => {
@@ -34,14 +35,19 @@ describe('WdfOverviewComponent', () => {
             },
           },
         },
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              data: { report: createMockWdfReport(overrides) },
-            },
+        provideActivatedRouteWithRouterLink({
+          snapshot: {
+            data: { report: createMockWdfReport(overrides) },
           },
-        },
+        }),
+        // {
+        //   provide: ActivatedRoute,
+        //   useValue: {
+        //     snapshot: {
+        //       data: { report: createMockWdfReport(overrides) },
+        //     },
+        //   },
+        // },
         {
           provide: UserService,
           useValue: {
@@ -140,7 +146,7 @@ describe('WdfOverviewComponent', () => {
       const keepYourDataCurrentLink = getByText('Keep your data current');
 
       expect(dataMetFundingParagraph).toBeTruthy();
-      expect(keepYourDataCurrentLink.getAttribute('ng-reflect-router-link')).toEqual('data');
+      expect(keepYourDataCurrentLink.getAttribute('href')).toEqual('/data');
     });
 
     it('should navigate to your data when Check your data is clicked', async () => {
@@ -160,7 +166,7 @@ describe('WdfOverviewComponent', () => {
       const learnMoreLink = getByText('Learn more about the funds that you can claim from');
 
       expect(learnMoreLink).toBeTruthy();
-      expect(learnMoreLink.getAttribute('ng-reflect-router-link')).toEqual('learn-more');
+      expect(learnMoreLink.getAttribute('href')).toEqual('/learn-more');
     });
 
     it('should show the funding requirements link', async () => {
@@ -172,7 +178,7 @@ describe('WdfOverviewComponent', () => {
       );
 
       expect(fundingRequirementsLink).toBeTruthy();
-      expect(fundingRequirementsLink.getAttribute('ng-reflect-router-link')).toEqual('funding-requirements');
+      expect(fundingRequirementsLink.getAttribute('href')).toEqual('/funding-requirements');
     });
   });
 
@@ -196,7 +202,7 @@ describe('WdfOverviewComponent', () => {
       );
 
       expect(fundingRequirementsLink).toBeTruthy();
-      expect(fundingRequirementsLink.getAttribute('ng-reflect-router-link')).toEqual('funding-requirements');
+      expect(fundingRequirementsLink.getAttribute('href')).toEqual('/funding-requirements');
     });
   });
 
@@ -247,7 +253,7 @@ describe('WdfOverviewComponent', () => {
         const keepYourDataCurrentLink = getByText('Keep your data current');
 
         expect(dataMetFundingParagraph).toBeTruthy();
-        expect(keepYourDataCurrentLink.getAttribute('ng-reflect-router-link')).toEqual('data');
+        expect(keepYourDataCurrentLink.getAttribute('href')).toEqual('/data');
       });
 
       it('should display overall eligibility date from parent when parent has latest eligibility date', async () => {
@@ -340,7 +346,7 @@ describe('WdfOverviewComponent', () => {
       );
 
       expect(fundingRequirementsLink).toBeTruthy();
-      expect(fundingRequirementsLink.getAttribute('ng-reflect-router-link')).toEqual('funding-requirements');
+      expect(fundingRequirementsLink.getAttribute('href')).toEqual('/funding-requirements');
     });
 
     it('should display some data not meeting message when not eligible overall but some sub workplaces are eligible', async () => {
