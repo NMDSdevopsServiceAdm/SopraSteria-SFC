483e1dd01709f103f2282a4b9b8e1985ff19c87e
Joe Fong <joe.fong@madetech.com>
2025-10-03 13:38:13 +0100

    amend eslintrc to ignore prefer-standalone and prefer-inject

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -26,6 +26,8 @@ module.exports = {
       rules: {
         '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: 'app', style: 'camelCase' }],
         '@angular-eslint/component-selector': ['error', { type: 'element', prefix: 'app', style: 'kebab-case' }],
+        '@angular-eslint/prefer-standalone': 'off',
+        '@angular-eslint/prefer-inject': 'off',
         '@typescript-eslint/no-unused-vars': [
           'warn',
           {
