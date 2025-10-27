d87976a6221eb4992f6fe68d433da296baea9667
Joe Fong <joe.fong@madetech.com>
2025-10-27 16:11:28 +0000

    add a shared seperator component, adjust the margin between seperator and other elements

---
+++ frontend/src/app/shared/components/seperator-line/seperator-line.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seperator-line',
+  templateUrl: './seperator-line.component.html',
+})
+export class SeperatorLineComponent {}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -144,6 +144,7 @@ import { WorkerPaginationComponent } from './components/worker-pagination/worker
 import { CookieBannerComponent } from './components/cookie-banner/cookie-banner.component';
 import { A11yModule } from '@angular/cdk/a11y';
 import { ButtonMenuComponent } from '@shared/components/button-menu/button-menu.component';
+import { SeperatorLineComponent } from './components/seperator-line/seperator-line.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule, OverlayModule, A11yModule],
@@ -287,6 +288,7 @@ import { ButtonMenuComponent } from '@shared/components/button-menu/button-menu.
     FormatWhatDhaPipe,
     CookieBannerComponent,
     ButtonMenuComponent,
+    SeperatorLineComponent,
   ],
   exports: [
     AbsoluteNumberPipe,
@@ -420,6 +422,7 @@ import { ButtonMenuComponent } from '@shared/components/button-menu/button-menu.
     DetailsDhaExamplesComponent,
     FormatWhatDhaPipe,
     CookieBannerComponent,
+    SeperatorLineComponent,
   ],
   providers: [
     DialogService,
