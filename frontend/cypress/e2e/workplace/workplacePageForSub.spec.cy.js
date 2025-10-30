f1bfc45ba55f281c63dce918c37008441732dd80
Sabrina <sabrina.samuel@madetech.com>
2025-10-30 13:53:24 +0000

    Update workplace e2e tests for sub

--- frontend/cypress/e2e/workplace/workplacePageForSub.spec.cy.js
+++ frontend/cypress/e2e/workplace/workplacePageForSub.spec.cy.js
@@ -1,12 +1,26 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
+import { SubEstablishment } from '../../support/mockEstablishmentData';
 import { onWorkplacePage } from '../../support/page_objects/onWorkplacePage';
 
 describe('Sub home page as edit user', () => {
+  const establishmentId = SubEstablishment.id;
+
+  before(() => {
+    cy.resetEstablishmentCapacity(establishmentId);
+    cy.setWorkplaceMainService(establishmentId, 9);
+    cy.insertDummyAnswerForWorkplaceWDFAnswers(establishmentId);
+  });
+
   beforeEach(() => {
     cy.loginAsUser(Cypress.env('editSubUser'), Cypress.env('userPassword'));
     cy.get('[data-cy="tab-list"]').contains('Workplace').click();
     cy.reload();
+  });
+
+  after(() => {
+    cy.resetEstablishmentCapacity(establishmentId);
+    cy.setWorkplaceMainService(establishmentId, 1);
   });
 
   it('should see the sub establishment workplace page', () => {
--- frontend/cypress/e2e/workplace/workplacePageForSubReadOnly.spec.cy.js
+++ frontend/cypress/e2e/workplace/workplacePageForSubReadOnly.spec.cy.js
@@ -1,12 +1,26 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
+import { SubEstablishment } from '../../support/mockEstablishmentData';
 import { onWorkplacePage } from '../../support/page_objects/onWorkplacePage';
 
 describe('Standalone home page as edit user', () => {
+  const establishmentId = SubEstablishment.id;
+
+  before(() => {
+    cy.resetEstablishmentCapacity(establishmentId);
+    cy.setWorkplaceMainService(establishmentId, 9);
+    cy.insertDummyAnswerForWorkplaceWDFAnswers(establishmentId);
+  });
+
   beforeEach(() => {
     cy.loginAsUser(Cypress.env('readOnlySubUser'), Cypress.env('userPassword'));
     cy.get('[data-cy="tab-list"]').contains('Workplace').click();
     cy.reload();
+  });
+
+  after(() => {
+    cy.resetEstablishmentCapacity(establishmentId);
+    cy.setWorkplaceMainService(establishmentId, 1);
   });
 
   it('should see the standalone establishment workplace page', () => {
