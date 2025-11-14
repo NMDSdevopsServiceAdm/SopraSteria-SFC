26fbaacf12630cf851d5294ef819f00fd2586733
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2025-11-14 16:24:03 +0000

    Add the cerificate functionality

--- frontend/src/app/features/training-and-qualifications/training-course/training-course-matching-layout/training-course-matching-layout.component.ts
+++ frontend/src/app/features/training-and-qualifications/training-course/training-course-matching-layout/training-course-matching-layout.component.ts
@@ -3,14 +3,19 @@ import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms
 import { ActivatedRoute, Router } from '@angular/router';
 import { DATE_PARSE_FORMAT } from '@core/constants/constants';
 import { Establishment } from '@core/model/establishment.model';
+import { TrainingCertificate } from '@core/model/training.model';
+import { CertificateDownload } from '@core/model/trainingAndQualifications.model';
 import { Worker } from '@core/model/worker.model';
 import { AlertService } from '@core/services/alert.service';
 import { BackLinkService } from '@core/services/backLink.service';
+import { TrainingCertificateService } from '@core/services/certificate.service';
 import { ErrorSummaryService } from '@core/services/error-summary.service';
 import { EstablishmentService } from '@core/services/establishment.service';
 import { TrainingService } from '@core/services/training.service';
 import { WorkerService } from '@core/services/worker.service';
+import { CustomValidators } from '@shared/validators/custom-form-validators';
 import dayjs from 'dayjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-training-course-matching-layout',
@@ -21,17 +26,23 @@ export class TrainingCourseMatchingLayoutComponent implements OnInit {
   public form: UntypedFormGroup;
   public workplace: Establishment;
   public worker: Worker;
+  public establishmentUid: string;
+  public workerId: string;
   public submitted = false;
   public submitButtonDisabled: boolean = false;
   public buttonText: string;
   public notesOpen = false;
   public notesMaxLength = 1000;
   public remainingCharacterCount: number = this.notesMaxLength;
+  public subscriptions: Subscription = new Subscription();
   public notesValue = '';
-  public multipleTrainingDetails: boolean;
   public trainingRecord: any;
+  public trainingRecordId: string;
   public expiryMismatchWarning: any;
+  public certificateErrors: string[] | null;
   private _filesToUpload: File[];
+  public trainingCertificates: TrainingCertificate[] = [];
+  public filesToRemove: TrainingCertificate[] = [];
 
   constructor(
     private workerService: WorkerService,
@@ -43,11 +54,15 @@ export class TrainingCourseMatchingLayoutComponent implements OnInit {
     protected backLinkService: BackLinkService,
     public trainingService: TrainingService,
     private alertService: AlertService,
+    private certificateService: TrainingCertificateService,
   ) {}
 
   ngOnInit(): void {
     this.trainingRecord = this.route.snapshot.data.trainingRecord;
+    this.trainingRecordId = this.route.snapshot.params.trainingRecordId;
     this.worker = this.workerService.worker;
+    this.establishmentUid = this.route.snapshot.params?.establishmentuid;
+    this.workerId = this.route.snapshot.params?.id;
     this.workplace = this.establishmentService.establishment;
     this.setupForm();
     this.fillForm();
@@ -92,7 +107,9 @@ export class TrainingCourseMatchingLayoutComponent implements OnInit {
   }
 
   private fillForm(): void {
-    const { completed, expires, notes } = this.trainingRecord;
+    const { completed, expires, notes, trainingCertificates } = this.trainingRecord;
+
+    this.trainingCertificates = trainingCertificates;
 
     this.form.patchValue({
       completed: this.toFormDate(this.parseDate(completed)),
@@ -165,8 +182,111 @@ export class TrainingCourseMatchingLayoutComponent implements OnInit {
     });
   }
 
+  get filesToUpload(): File[] {
+    return this._filesToUpload ?? [];
+  }
+
+  private set filesToUpload(files: File[]) {
+    this._filesToUpload = files ?? [];
+  }
+  private resetUploadFilesError(): void {
+    this.certificateErrors = null;
+  }
+
+  public getUploadComponentAriaDescribedBy(): string {
+    if (this.certificateErrors) {
+      return 'uploadCertificate-errors uploadCertificate-aria-text';
+    } else if (this.filesToUpload?.length > 0) {
+      return 'uploadCertificate-aria-text';
+    } else {
+      return 'uploadCertificate-hint uploadCertificate-aria-text';
+    }
+  }
+
+  public onSelectFiles(newFiles: File[]): void {
+    this.resetUploadFilesError();
+    const errors = CustomValidators.validateUploadCertificates(newFiles);
+
+    if (errors) {
+      this.certificateErrors = errors;
+      return;
+    }
+
+    const combinedFiles = [...newFiles, ...this.filesToUpload];
+    this.filesToUpload = combinedFiles;
+  }
+
+  public removeFileToUpload(fileIndexToRemove: number): void {
+    const filesToKeep = this.filesToUpload.filter((_file, index) => index !== fileIndexToRemove);
+    this.filesToUpload = filesToKeep;
+    this.certificateErrors = [];
+  }
+  private uploadNewCertificate(trainingRecordResponse: any) {
+    const trainingRecordId = this.trainingRecordId ?? trainingRecordResponse.uid;
+
+    return this.certificateService.addCertificates(
+      this.workplace.uid,
+      this.worker.uid,
+      trainingRecordId,
+      this.filesToUpload,
+    );
+  }
+
+  public removeSavedFile(fileIndexToRemove: number): void {
+    let tempTrainingCertificates = this.trainingCertificates.filter(
+      (certificate, index) => index !== fileIndexToRemove,
+    );
+
+    this.filesToRemove.push(this.trainingCertificates[fileIndexToRemove]);
+
+    this.trainingCertificates = tempTrainingCertificates;
+  }
+
+  public downloadCertificates(fileIndex: number): void {
+    const filesToDownload =
+      fileIndex != null
+        ? [this.formatForCertificateDownload(this.trainingCertificates[fileIndex])]
+        : this.trainingCertificates.map((certificate) => {
+            return this.formatForCertificateDownload(certificate);
+          });
+    this.subscriptions.add(
+      this.certificateService
+        .downloadCertificates(this.workplace.uid, this.worker.uid, this.trainingRecordId, filesToDownload)
+        .subscribe(
+          () => {
+            this.certificateErrors = [];
+          },
+          (_error) => {
+            this.certificateErrors = ["There's a problem with this download. Try again later or contact us for help."];
+          },
+        ),
+    );
+  }
+  private formatForCertificateDownload(certificate: TrainingCertificate): CertificateDownload {
+    return { uid: certificate.uid, filename: certificate.filename };
+  }
+
+  private deleteTrainingCertificate(files: TrainingCertificate[]) {
+    this.subscriptions.add(
+      this.certificateService
+        .deleteCertificates(this.establishmentUid, this.workerId, this.trainingRecordId, files)
+        .subscribe(() => {}),
+    );
+  }
   public setBackLink(): void {
     this.backLinkService.showBackLink();
+  }
+
+  protected navigateToDeleteTrainingRecord(): void {
+    this.router.navigate([
+      '/workplace',
+      this.workplace.uid,
+      'training-and-qualifications-record',
+      this.worker.uid,
+      'training',
+      this.trainingRecordId,
+      'delete',
+    ]);
   }
 
   public onCancel(event: Event): void {
