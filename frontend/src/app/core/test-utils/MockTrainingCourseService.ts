092000f08866af17be4804260acc667fe0c16edd
Joe Fong <joe.fong@madetech.com>
2025-11-25 15:36:13 +0000

    amend trainingCourseBuilder to use coherent training category

--- frontend/src/app/core/test-utils/MockTrainingCourseService.ts
+++ frontend/src/app/core/test-utils/MockTrainingCourseService.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TrainingCourse } from '@core/model/training-course.model';
-import { DeliveredBy } from '@core/model/training.model';
-import { YesNoDontKnow } from '@core/model/YesNoDontKnow.enum';
+import { DeliveredBy, TrainingCategory } from '@core/model/training.model';
 import { TrainingCourseService } from '@core/services/training-course.service';
 import { build, sequence, oneOf, BuildTimeConfig, fake } from '@jackfranklin/test-data-bot';
+import { trainingCategories } from '@core/test-utils/MockTrainingCategoriesService';
 
 export const trainingCourseBuilder = build('TrainingCourse', {
   fields: {
     id: sequence(),
     uid: fake((f) => f.datatype.uuid()),
     name: fake((f) => f.lorem.words()),
-    trainingCategoryId: fake((f) => f.datatype.number({ min: 1, max: 48 })),
-    trainingCategoryName: fake((f) => f.lorem.words()),
-    accredited: oneOf(...Object.values(YesNoDontKnow)),
+    trainingCategory: oneOf(...trainingCategories),
+    trainingCategoryName: null,
+    trainingCategoryId: null,
+    accredited: oneOf('Yes', 'No'),
     deliveredBy: oneOf(...Object.values(DeliveredBy)),
     externalProviderName: null,
     trainingProviderId: null,
@@ -21,6 +22,11 @@ export const trainingCourseBuilder = build('TrainingCourse', {
     howWasItDelivered: null,
     doesNotExpire: oneOf(true),
     validityPeriodInMonth: null,
+  },
+  postBuild: (trainingCourse) => {
+    trainingCourse.trainingCategoryName = (trainingCourse.trainingCategory as TrainingCategory).category;
+    trainingCourse.trainingCategoryId = (trainingCourse.trainingCategory as TrainingCategory).id;
+    return trainingCourse;
   },
 }) as unknown as (buildTimeConfig?: BuildTimeConfig<any>) => TrainingCourse;
 
