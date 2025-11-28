fd90879a03381d505847c2661836160fd9619694
Joe Fong <joe.fong@madetech.com>
2025-11-28 13:50:37 +0000

    move helper functions to a separate file

--- frontend/cypress/e2e/trainingAndQualifications/trainingCourse/runTestsForTrainingCourse.js
+++ frontend/cypress/e2e/trainingAndQualifications/trainingCourse/runTestsForTrainingCourse.js
@@ -2,6 +2,7 @@
 /// <reference types="cypress" />
 
 import { onHomePage } from '../../../support/page_objects/onHomePage';
+import { clickIntoTrainingCourse, expectPageToHaveDetails, fillInCourseDetails } from './helpers';
 
 export const runTestsForTrainingCourseJourney = (mockEstablishmentData) => {
   describe('Add and manage training course', () => {
@@ -210,77 +211,5 @@ export const runTestsForTrainingCourseJourney = (mockEstablishmentData) => {
         cy.get('a').contains(trainingRecordTitle).should('exist');
       });
     });
-
-    // helper functions below
-
-    const clickIntoTrainingCourse = (courseName) => {
-      cy.get('a').not(':contains("Remove")').contains(courseName).click();
-    };
-
-    const fillInCourseDetails = (courseDetails) => {
-      const {
-        courseName,
-        accredited,
-        deliveredBy,
-        providerName,
-        howWasItDelivered,
-        doesNotExpire,
-        validityPeriodInMonth,
-      } = courseDetails;
-
-      cy.getByLabel('Training course name').as('courseName').clear();
-      cy.get('@courseName').type(courseName);
-      const accreditedLabel = accredited === "Don't know" ? 'I do not know' : accredited;
-      cy.getByLabel(accreditedLabel).click();
-      cy.getByLabel(deliveredBy).click();
-
-      if (deliveredBy === 'External provider') {
-        cy.getByLabel('Provider name').as('providerName').clear();
-        cy.get('@providerName').type(providerName);
-      }
-
-      cy.getByLabel(howWasItDelivered).click();
-
-      if (doesNotExpire) {
-        cy.getByLabel('This training does not expire').click();
-      } else {
-        cy.getByLabel(/How many months/).type(validityPeriodInMonth);
-      }
-    };
-
-    const expectPageToHaveDetails = (details) => {
-      const {
-        courseName,
-        trainingRecordTitle,
-        accredited,
-        deliveredBy,
-        providerName,
-        howWasItDelivered,
-        doesNotExpire,
-        validityPeriodInMonth,
-      } = details;
-
-      if (courseName) {
-        cy.getByLabel('Training course name').should('have.value', courseName);
-      } else if (trainingRecordTitle) {
-        cy.getByLabel('Training record name').should('have.value', trainingRecordTitle);
-      }
-
-      const accreditedLabel = accredited === "Don't know" ? 'I do not know' : accredited;
-      cy.getByLabel(accreditedLabel).should('be.checked');
-      cy.getByLabel(deliveredBy).should('be.checked');
-
-      if (deliveredBy === 'External provider') {
-        cy.getByLabel('Provider name').should('have.value', providerName);
-      }
-
-      cy.getByLabel(howWasItDelivered).should('be.checked');
-
-      if (doesNotExpire) {
-        cy.getByLabel('This training does not expire').should('be.checked');
-      } else {
-        cy.getByLabel(/How many months/).should('have.value', validityPeriodInMonth.toString());
-      }
-    };
   });
 };
