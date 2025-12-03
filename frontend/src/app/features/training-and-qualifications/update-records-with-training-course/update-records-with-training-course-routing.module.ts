4d75bebed3fb4a0622a60819418cb6ff6bbc5e08
Joe Fong <joe.fong@madetech.com>
2025-12-03 17:13:34 +0000

    add new component to routing table

--- frontend/src/app/features/training-and-qualifications/update-records-with-training-course/update-records-with-training-course-routing.module.ts
+++ frontend/src/app/features/training-and-qualifications/update-records-with-training-course/update-records-with-training-course-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UpdateRecordsSelectTrainingCourseComponent } from './update-records-select-training-course/update-records-select-training-course.component';
 import { TrainingCoursesWithLinkableRecordsResolver } from '@core/resolvers/training/training-courses-with-linkable-records-resolver';
+import { SelectTrainingRecordsToUpdateComponent } from './select-training-records-to-update/select-training-records-to-update.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,16 @@ const routes: Routes = [
         path: 'select-a-training-course',
         component: UpdateRecordsSelectTrainingCourseComponent,
         data: { title: 'Select a training course' },
+      },
+      {
+        path: ':trainingCourseUid',
+        children: [
+          {
+            path: 'select-training-records',
+            component: SelectTrainingRecordsToUpdateComponent,
+            data: { title: 'Select the training records that you want to update' },
+          },
+        ],
       },
     ],
   },
