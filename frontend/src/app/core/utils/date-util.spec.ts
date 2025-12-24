c5428e83f65598e5d0d5d87412df6a76c531adfa
Joe Fong <joe.fong@madetech.com>
2025-12-24 18:01:05 +0000

    extract logic about expiry date calculation to DateUtil for reusability

--- frontend/src/app/core/utils/date-util.spec.ts
+++ frontend/src/app/core/utils/date-util.spec.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import { DateUtil } from './date-util';
 import { FormatDate } from './date-util';
+import { DATE_PARSE_FORMAT } from '@core/constants/constants';
 
 describe('DateUtil', () => {
   describe('getDateForOneYearAgo', () => {
@@ -129,6 +130,147 @@ describe('DateUtil', () => {
           const actual = DateUtil.toFormDate(invalidInput);
 
           expect(actual).toBeNull();
+        });
+      });
+    });
+  });
+
+  describe('expectedExpiryDate', () => {
+    describe('should calculate the expected expiry date from given completed date and validity months', () => {
+      const testCases = [
+        { completed: '2025-11-01', validityPeriodInMonth: 1, expected: '2025-12-01' },
+        { completed: '2025-11-15', validityPeriodInMonth: 1, expected: '2025-12-15' },
+        { completed: '2025-11-30', validityPeriodInMonth: 1, expected: '2025-12-30' },
+        { completed: '2025-12-31', validityPeriodInMonth: 1, expected: '2026-01-31' },
+        { completed: '2025-12-31', validityPeriodInMonth: 2, expected: '2026-02-28' },
+        { completed: '2024-02-29', validityPeriodInMonth: 12, expected: '2025-02-28' },
+      ];
+
+      testCases.forEach(({ completed, validityPeriodInMonth, expected }) => {
+        it(`completed: ${completed}, validityPeriodInMonth: ${validityPeriodInMonth}`, () => {
+          const completedInDayJs = DateUtil.toDayjs(completed);
+
+          const output = DateUtil.expectedExpiryDate(completedInDayJs, validityPeriodInMonth);
+          const outputInStringForm = output.format(DATE_PARSE_FORMAT);
+
+          expect(outputInStringForm).toEqual(expected);
+        });
+      });
+    });
+
+    describe('should return null for invalid input', () => {
+      const testCases = [
+        { completed: DateUtil.toDayjs('2025-11-01'), validityPeriodInMonth: null },
+        { completed: DateUtil.toDayjs('2025-11-01'), validityPeriodInMonth: 0 },
+        { completed: DateUtil.toDayjs('2025-11-01'), validityPeriodInMonth: -20 },
+        { completed: undefined, validityPeriodInMonth: 12 },
+        { completed: null, validityPeriodInMonth: 12 },
+        { completed: 'some random string', validityPeriodInMonth: 12 },
+        { completed: undefined, validityPeriodInMonth: undefined },
+      ];
+
+      testCases.forEach(({ completed, validityPeriodInMonth }) => {
+        it(`invalidInput`, () => {
+          // @ts-expect-error
+          const actual = DateUtil.expectedExpiryDate(completed, validityPeriodInMonth);
+
+          expect(actual).toBeNull();
+        });
+      });
+    });
+  });
+
+  describe('expiryDateDoesNotMatch', () => {
+    describe('should return true if all inputs have valid values but does not agree with each other', () => {
+      const testCases = [
+        {
+          completed: DateUtil.toDayjs('2025-11-01'),
+          expires: DateUtil.toDayjs('2026-11-02'),
+          validityPeriodInMonth: 12,
+        },
+        {
+          completed: DateUtil.toDayjs('2025-04-15'),
+          expires: DateUtil.toDayjs('2025-06-30'),
+          validityPeriodInMonth: 2,
+        },
+      ];
+
+      testCases.forEach(({ completed, expires, validityPeriodInMonth }) => {
+        it(`completed: ${completed}, expires: ${expires}, validityPeriodInMonth: ${validityPeriodInMonth}`, () => {
+          const actual = DateUtil.expiryDateDoesNotMatch(completed, expires, validityPeriodInMonth);
+
+          expect(actual).toBeTrue();
+        });
+      });
+    });
+
+    describe('should return false if any of the given inputs are invalid', () => {
+      const testCases = [
+        {
+          completed: undefined,
+          expires: DateUtil.toDayjs('2026-11-01'),
+          validityPeriodInMonth: 12,
+        },
+        {
+          completed: DateUtil.toDayjs('2025-11-01'),
+          expires: undefined,
+          validityPeriodInMonth: 12,
+        },
+        {
+          completed: DateUtil.toDayjs('2025-11-01'),
+          expires: DateUtil.toDayjs('2026-11-01'),
+          validityPeriodInMonth: null,
+        },
+        {
+          completed: DateUtil.toDayjs('2025-11-01'),
+          expires: DateUtil.toDayjs('2025-11-01'),
+          validityPeriodInMonth: 0,
+        },
+        {
+          completed: DateUtil.toDayjs('2025-11-01'),
+          expires: DateUtil.toDayjs('2024-11-01'),
+          validityPeriodInMonth: -12,
+        },
+      ];
+
+      testCases.forEach(({ completed, expires, validityPeriodInMonth }) => {
+        it(`completed: ${completed}, expires: ${expires}, validityPeriodInMonth: ${validityPeriodInMonth}`, () => {
+          const actual = DateUtil.expiryDateDoesNotMatch(completed, expires, validityPeriodInMonth);
+
+          expect(actual).toBeFalse();
+        });
+      });
+    });
+
+    describe('should return false if the given dates and validity agree with each other', () => {
+      const testCases = [
+        {
+          completed: DateUtil.toDayjs('2025-11-01'),
+          expires: DateUtil.toDayjs('2026-11-01'),
+          validityPeriodInMonth: 12,
+        },
+        {
+          completed: DateUtil.toDayjs('2025-04-15'),
+          expires: DateUtil.toDayjs('2025-06-15'),
+          validityPeriodInMonth: 2,
+        },
+        {
+          completed: DateUtil.toDayjs('2025-01-31'),
+          expires: DateUtil.toDayjs('2025-04-30'),
+          validityPeriodInMonth: 3,
+        },
+        {
+          completed: DateUtil.toDayjs('2024-12-31'),
+          expires: DateUtil.toDayjs('2025-02-28'),
+          validityPeriodInMonth: 2,
+        },
+      ];
+
+      testCases.forEach(({ completed, expires, validityPeriodInMonth }) => {
+        it(`completed: ${completed}, expires: ${expires}, validityPeriodInMonth: ${validityPeriodInMonth}`, () => {
+          const actual = DateUtil.expiryDateDoesNotMatch(completed, expires, validityPeriodInMonth);
+
+          expect(actual).toBeFalse();
         });
       });
     });
