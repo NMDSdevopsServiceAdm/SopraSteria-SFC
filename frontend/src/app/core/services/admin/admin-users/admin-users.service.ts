b71537598c6534ee9738dece4140eb7fe2d7bd82
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2026-01-07 16:37:33 +0000

    Add service to get the parent requests

--- frontend/src/app/core/services/admin/admin-users/admin-users.service.ts
+++ frontend/src/app/core/services/admin/admin-users/admin-users.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CreateAccountRequest, CreateAccountResponse } from '@core/model/account.model';
 import { UserDetails } from '@core/model/userDetails.model';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -35,5 +35,38 @@ export class AdminUsersService {
     return this.http.post(`${environment.appRunnerEndpoint}/api/user/${useruid}/resend-activation-admin`, null, {
       responseType: 'text' as 'json',
     });
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class ParentRequestsStateService {
+  private readonly STORAGE_KEY = 'parentRequests';
+  private readonly parentRequests$ = new BehaviorSubject<any[] | null>(this.loadFromStorage());
+
+  private loadFromStorage(): any[] | null {
+    const json = localStorage.getItem(this.STORAGE_KEY);
+    return json ? JSON.parse(json) : null;
+  }
+
+  private saveToStorage(data: any[]): void {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(data));
+  }
+
+  set(data: any[]): void {
+    this.saveToStorage(data);
+    this.parentRequests$.next(data);
+  }
+
+  get$(): Observable<any[] | null> {
+    return this.parentRequests$.asObservable();
+  }
+
+  getSnapshot(): any[] | null {
+    return this.parentRequests$.value;
+  }
+
+  clear(): void {
+    localStorage.removeItem(this.STORAGE_KEY);
+    this.parentRequests$.next(null);
   }
 }
