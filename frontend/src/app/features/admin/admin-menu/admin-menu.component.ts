cf4d65ebc880c5a72c219f8221939dd8c6b930f4
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2026-01-07 16:47:07 +0000

    get the parent request data to show the flag when there is a pending request

--- frontend/src/app/features/admin/admin-menu/admin-menu.component.ts
+++ frontend/src/app/features/admin/admin-menu/admin-menu.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParentRequestsStateService } from '@core/services/admin/admin-users/admin-users.service';
+import { ParentRequestsService } from '@core/services/parent-requests.service';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-admin-menu',
-    templateUrl: './admin-menu.component.html',
-    styleUrls: ['./admin-menu.component.scss'],
-    standalone: false
+  selector: 'app-admin-menu',
+  templateUrl: './admin-menu.component.html',
+  styleUrls: ['./admin-menu.component.scss'],
+  standalone: false,
 })
-export class AdminMenuComponent {}
+export class AdminMenuComponent implements OnInit {
+  public showFlag: any;
+
+  constructor(
+    private parentRequestsState: ParentRequestsStateService,
+    private parentRequestsService: ParentRequestsService,
+  ) {}
+
+  ngOnInit(): void {
+    // Preload if not in cache
+    if (!this.parentRequestsState.getSnapshot()) {
+      this.parentRequestsService
+        .getParentRequests()
+        .pipe(take(1))
+        .subscribe((data) => this.parentRequestsState.set(data));
+    }
+
+    // Subscribe safely to avoid null errors
+    this.parentRequestsState
+      .get$()
+      .pipe(
+        filter((data): data is any[] => data !== null), // ignore nulls
+      )
+      .subscribe((data) => {
+        // dynamic check: show flag if ANY request has status 'Pending'
+        this.showFlag = data.some((request) => request.status === 'Pending');
+      });
+  }
+}
