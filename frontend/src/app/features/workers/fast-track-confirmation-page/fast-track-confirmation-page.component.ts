cddf55e4eca4d8073dc18fa0755fea8f2a0dd801
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2026-04-07 16:27:09 +0100

    Add new component for fast track confiramtion page

--- frontend/src/app/features/workers/fast-track-confirmation-page/fast-track-confirmation-page.component.ts
+++ frontend/src/app/features/workers/fast-track-confirmation-page/fast-track-confirmation-page.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { BackLinkService } from '@core/services/backLink.service';
 
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { UntypedFormArray, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Establishment } from '@core/model/establishment.model';
+import { WorkersGroupedByJobRoleResponse } from '@core/model/worker.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { WorkerService } from '@core/services/worker.service';
+import { AlertService } from '@core/services/alert.service';
+import { EstablishmentService } from '@core/services/establishment.service';
 
 @Component({
   selector: 'app-fast-track-confirmation-page',
@@ -9,12 +15,37 @@ import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
   standalone: false,
 })
 export class FastTrackConfirmationPageComponent implements OnInit {
+  @ViewChild('formEl') formEl: ElementRef;
   public form: UntypedFormGroup;
+  public workplace: Establishment;
+  public workersByJobRole: WorkersGroupedByJobRoleResponse;
+  public totalCount: number;
+  public filteredGroups: any;
+  public jobRoleIndex: number;
+  public jobRoleChanged: any;
+  public submitted: boolean;
 
-  constructor(private backLinkService: BackLinkService, private formBuilder: UntypedFormBuilder) {}
+  constructor(
+    private backLinkService: BackLinkService,
+    private formBuilder: UntypedFormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private workerService: WorkerService,
+    private alertService: AlertService,
+    private establishmentService: EstablishmentService,
+  ) {}
 
   ngOnInit(): void {
     this.setBackLink();
+    this.workplace = this.route.snapshot.data.establishment;
+    this.workersByJobRole = this.workerService.getWorkersGroupedByJobRole();
+    this.jobRoleIndex = this.route.snapshot.queryParams['index'];
+    this.jobRoleChanged = this.workersByJobRole?.groups[this.jobRoleIndex];
+    this.filteredGroups = this.workersByJobRole?.groups
+      .map((group, index) => ({ ...group, originalIndex: index }))
+      .filter((group) => group.annualHourlyPay?.rate != null);
+
+    this.totalCount = this.filteredGroups?.reduce((sum, group) => sum + group.count, 0);
 
     this.setupForm();
   }
@@ -26,6 +57,48 @@ export class FastTrackConfirmationPageComponent implements OnInit {
   private setupForm(): void {
     this.form = this.formBuilder.group({
       workers: this.formBuilder.array([]),
+    });
+
+    const workersFormArray = this.form.get('workers') as UntypedFormArray;
+
+    this.workersByJobRole.groups.forEach((group) => {
+      workersFormArray.push(
+        this.formBuilder.group({
+          workerId: group.jobId,
+          value: group.annualHourlyPay?.value || null,
+          rate: group.annualHourlyPay?.rate || null,
+        }),
+      );
+    });
+  }
+
+  public onSubmit(): void {
+    this.submitted = true;
+
+    this.establishmentService
+      .updateWorkers(this.workplace.uid, this.workerPayByJobRole())
+      .subscribe(() => this.onSubmitSuccess());
+  }
+
+  private workerPayByJobRole() {
+    const payload = this.workersByJobRole?.groups
+      .filter((group) => group.annualHourlyPay?.rate != null)
+      .flatMap((group) =>
+        group.workers.map((worker) => ({
+          uid: worker.uid,
+          annualHourlyPay: group.annualHourlyPay,
+        })),
+      );
+
+    return payload;
+  }
+
+  private onSubmitSuccess(): void {
+    this.router.navigate(['/workplace', this.workplace.uid, 'update-pay-multiple-staff']).then(() => {
+      this.alertService.addAlert({
+        type: 'success',
+        message: 'Pay updated in ' + this.totalCount + ' staff record' + (this.totalCount === 1 ? '' : 's') + ' ',
+      });
     });
   }
 }
