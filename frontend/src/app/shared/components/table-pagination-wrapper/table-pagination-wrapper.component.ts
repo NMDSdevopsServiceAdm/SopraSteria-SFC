d1d0862f9b9fca4a5985a5a84f4f8af276567532
Joe Fong <joe.fong@madetech.com>
2026-04-09 09:31:18 +0100

    undo the changes to app-table-pagination-wrapper to remove unneccessary bulk

--- frontend/src/app/shared/components/table-pagination-wrapper/table-pagination-wrapper.component.ts
+++ frontend/src/app/shared/components/table-pagination-wrapper/table-pagination-wrapper.component.ts
@@ -1,14 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output, OnDestroy } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { EstablishmentService } from '@core/services/establishment.service';
-import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-table-pagination-wrapper',
-  templateUrl: './table-pagination-wrapper.component.html',
-  standalone: false,
+    selector: 'app-table-pagination-wrapper',
+    templateUrl: './table-pagination-wrapper.component.html',
+    standalone: false
 })
-export class TablePaginationWrapperComponent implements OnInit, OnDestroy {
+export class TablePaginationWrapperComponent implements OnInit {
   @Input() maintainedPageIndex: number;
   @Input() totalCount: number;
   @Input() count: number;
@@ -19,10 +17,6 @@ export class TablePaginationWrapperComponent implements OnInit, OnDestroy {
   @Input() label = 'Search';
   @Input() accessibleLabel: string;
   @Input() setQueryInParams: boolean = false;
-  @Input() wdfView: boolean = false;
-  @Input() showNewPill: boolean = false;
-  @Input() showUpdatePayForMultipleStaffLink: boolean = false;
-  @Input() workplaceUid: string;
   @Output() fetchData = new EventEmitter<{
     index: number;
     itemsPerPage: number;
@@ -34,33 +28,14 @@ export class TablePaginationWrapperComponent implements OnInit, OnDestroy {
   private fragment: string;
   private tab: string;
   public sortBySelected: string;
-  public showUpdatePayForMultipleStaffLinkOnTopOfSearchBar: boolean = false;
-  public showUpdatePayForMultipleStaffLinkWhenNoSearchBar: boolean = false;
-  public updatePayForMultipleStaffLinkText = 'Update pay for multiple staff';
-  private subscriptions: Subscription = new Subscription();
 
-  constructor(
-    private router: Router,
-    private establishmentService: EstablishmentService,
-  ) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.sortBySelected = Object.keys(this.sortByParamMap).find((key) => this.sortByParamMap[key] === this.sortByValue);
     if (this.maintainedPageIndex && this.maintainedPageIndex !== this.currentPageIndex) {
       this.currentPageIndex = this.maintainedPageIndex;
     }
-    this.setShowUpdatePayForMultipleStaffLinkVariables();
-  }
-
-  public setShowUpdatePayForMultipleStaffLinkVariables(): void {
-    this.showUpdatePayForMultipleStaffLinkOnTopOfSearchBar =
-      this.showUpdatePayForMultipleStaffLink && this.totalCount > this.itemsPerPage && !this.wdfView;
-
-    this.showUpdatePayForMultipleStaffLinkWhenNoSearchBar =
-      this.showUpdatePayForMultipleStaffLink &&
-      this.totalCount > 1 &&
-      this.totalCount <= this.itemsPerPage &&
-      !this.wdfView;
   }
 
   private checkForFragment(): void {
@@ -113,26 +88,5 @@ export class TablePaginationWrapperComponent implements OnInit, OnDestroy {
       sortByValue: this.sortByValue,
     };
     this.fetchData.emit(properties);
-  }
-
-  private setUpdatePayForMultiStaffViewed(): void {
-    const data = {
-      property: 'updatePayForMultiStaffViewed',
-      value: true,
-    };
-    this.subscriptions.add(
-      this.establishmentService.updateSingleEstablishmentField(this.workplaceUid, data).subscribe(),
-    );
-  }
-
-  public handleOnClick() {
-    if (this.showNewPill) {
-      this.setUpdatePayForMultiStaffViewed();
-    }
-    this.router.navigate(['workplace', this.workplaceUid, 'staff-record', 'update-pay-for-multiple-staff']);
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
   }
 }
