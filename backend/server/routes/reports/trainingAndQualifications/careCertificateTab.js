1d4dad2bf3a38a813cb29010228ace7126f57037
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2026-05-15 13:25:58 +0100

    Add title and foot note

--- backend/server/routes/reports/trainingAndQualifications/careCertificateTab.js
+++ backend/server/routes/reports/trainingAndQualifications/careCertificateTab.js
@@ -9,20 +9,54 @@ const {
   fitColumnsToSize,
   alignColumnToLeft,
   addBlankRowIfTableEmpty,
+  newBackgroundColours,
+  addText,
+  setColourForRange,
+  tableHeaderCellStyle,
+  applyStyleToRange,
 } = require('../../../utils/excelUtils');
 const models = require('../../../models');
+
+const colCache = require('exceljs/lib/utils/col-cache');
+const lodash = require('lodash');
+
+const columnsToDisplay = [
+  { columnName: 'Name or ID number', field: 'mandatory' },
+  { columnName: 'Main job role', field: 'total' },
+  { columnName: 'Care certificate', field: 'expired' },
+  { columnName: 'L2 Adult Social Care Certificate', field: 'expiringSoon' },
+];
 
 const generateCareCertificateTab = async (workbook, establishmentId, isParent = false) => {
   const rawEstablishments = await models.establishment.getWorkersWithCareCertificateStatus(establishmentId, isParent);
 
   const establishments = convertWorkersWithCareCertificateStatus(rawEstablishments);
+
   const careCertificateTab = workbook.addWorksheet('Care Certificate', { views: [{ showGridLines: false }] });
-  addContentToCareCertificateTab(careCertificateTab, establishments, isParent);
+
+  addTitle(careCertificateTab);
+  addTopTableHeader(careCertificateTab, columnsToDisplay);
+
+  // addContentToCareCertificateTab(careCertificateTab, establishments, isParent);
+
+  addFootNote(careCertificateTab);
 };
 
+const addTitle = (careCertificateTab) => {
+  addText(careCertificateTab, 'B1:Z1', 'Care Certificates', { size: 24, bold: true });
+  careCertificateTab.getCell('B1').alignment = { vertical: 'middle' };
+  setColourForRange(careCertificateTab, 'A1:Z1', { backgroundColour: newBackgroundColours.lightGrey });
+};
+
+const addTopTableHeader = (tab, columnsToDisplay) => {
+  const lastColumnLetter = colCache.n2l(1 + columnsToDisplay.length);
+
+  const topHeaderRange = `B3:${lastColumnLetter}3`;
+  applyStyleToRange(tab, topHeaderRange, tableHeaderCellStyle);
+};
 const addContentToCareCertificateTab = (careCertificateTab, establishments, isParent) => {
-  addHeading(careCertificateTab, 'B2', 'D2', 'Care Certificate');
-  addLine(careCertificateTab, 'A4', isParent ? 'E4' : 'D4');
+  // addHeading(careCertificateTab, 'B2', 'D2', 'Care Certificate');
+  // addLine(careCertificateTab, 'A4', isParent ? 'E4' : 'D4');
   alignColumnToLeft(careCertificateTab, 2);
   if (isParent) alignColumnToLeft(careCertificateTab, 3);
 
@@ -41,12 +75,6 @@ const createCareCertificateTable = (careCertificateTab, isParent) => {
     textColours.white,
     isParent ? ['B', 'C', 'D', 'E'] : ['B', 'C', 'D'],
   );
-
-  const columns = [
-    { name: 'Worker ID', filterButton: true },
-    { name: 'Job role', filterButton: true },
-    { name: 'Status', filterButton: true },
-  ];
 
   isParent && columns.unshift({ name: 'Workplace', filterButton: true });
 
@@ -71,6 +99,17 @@ const addRowsToCareCertificateTable = (careCertificateTable, establishments, isP
 
   addBlankRowIfTableEmpty(careCertificateTable, isParent ? 4 : 3);
   careCertificateTable.commit();
+};
+
+const addFootNote = (tab) => {
+  const footNoteText = ['Note, the data displayed in this table has been generated from staff records.'];
+
+  tab.addRow([]);
+
+  footNoteText.forEach((text) => {
+    const newRow = tab.lastRow.number + 1;
+    addText(tab, `B${newRow}`, text);
+  });
 };
 
 module.exports.generateCareCertificateTab = generateCareCertificateTab;
