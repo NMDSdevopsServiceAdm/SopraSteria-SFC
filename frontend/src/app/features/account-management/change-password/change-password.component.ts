dcccf99c2b691066713e56273172e4c1170bd3ce
Joe Fong <joe.fong@madetech.com>
2026-06-03 16:50:10 +0100

    add missing unit test for change password component, add logout and navigation on password change

--- frontend/src/app/features/account-management/change-password/change-password.component.ts
+++ frontend/src/app/features/account-management/change-password/change-password.component.ts
@@ -1,30 +1,39 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { JourneyType } from '@core/breadcrumb/breadcrumb.model';
 import { UserDetails } from '@core/model/userDetails.model';
+import { AuthService } from '@core/services/auth.service';
 import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { UserService } from '@core/services/user.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-change-password',
-    templateUrl: './change-password.component.html',
-    standalone: false
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  standalone: false,
 })
 export class ChangePasswordComponent implements OnInit, OnDestroy {
   public submitted: boolean;
   public userDetails: UserDetails;
   private subscriptions: Subscription = new Subscription();
 
-  constructor(private breadcrumbService: BreadcrumbService, private userService: UserService) {}
+  constructor(
+    private breadcrumbService: BreadcrumbService,
+    private userService: UserService,
+    private router: Router,
+    private authService: AuthService,
+  ) {}
 
   ngOnInit() {
     this.breadcrumbService.show(JourneyType.ACCOUNT);
     this.submitted = false;
-    this.subscriptions.add(this.userService.loggedInUser$.subscribe(user => (this.userDetails = user)));
+    this.subscriptions.add(this.userService.loggedInUser$.subscribe((user) => (this.userDetails = user)));
   }
 
   public onResetPasswordSuccess(): void {
     this.submitted = true;
+    this.authService.logoutWithoutRouting();
+    this.router.navigate(['/password-saved']);
   }
 
   ngOnDestroy() {
