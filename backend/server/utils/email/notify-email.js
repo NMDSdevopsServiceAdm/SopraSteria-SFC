a6b29d1d023ac6144f4a420a52037bb7910af09e
Joe Fong <joe.fong@madetech.com>
2026-06-10 10:44:31 +0100

    add missing unit tests for gov notify send email methods

--- backend/server/utils/email/notify-email.js
+++ backend/server/utils/email/notify-email.js
@@ -1,10 +1,44 @@
 const config = require('../../config/config');
 const { v4: uuidv4 } = require('uuid');
-uuidv4();
 
 const GovNotifyClient = require('notifications-node-client').NotifyClient;
 
 const REPLY_TO_ID = config.get('notify.replyTo');
+const DEFAULT_DUMMY_ID = '80d54020-c420-46f1-866d-b8cc3196809d';
+
+const EmailTypes = {
+  passwordReset: { type: 'passwordReset', templateIdKey: 'notify.templates.resetPassword', slug: 'password-reset' },
+  addUser: { type: 'addUser', templateIdKey: 'notify.templates.addUser', slug: 'add-user' },
+};
+
+// exports.sendEmailToUser = async (emailType, emailAddress, personalisation) => {
+//   if (!emailType) {
+//     console.error('INFO - sendEmailToUser - email type is missing');
+//     return;
+//   }
+
+//   const templateId = config.get(emailType.templateIdKey);
+//   const apiKey = config.get('notify.key');
+//   const reference = config.get('env') + `-${emailType.slug}-` + uuidv4();
+
+//   if (apiKey === 'unknown' || REPLY_TO_ID === DEFAULT_DUMMY_ID || templateId === DEFAULT_DUMMY_ID) {
+//     console.log(`INFO - ${emailType.type} - gov.uk notify is not configured`);
+//     return;
+//   }
+
+//   try {
+//     const notifyClient = new GovNotifyClient(config.get('notify.key'));
+//     await notifyClient.sendEmail(templateId, emailAddress, {
+//       personalisation,
+//       reference,
+//       emailReplyToId: REPLY_TO_ID,
+//     });
+
+//     console.log(`${emailType.type} - Successfully sent (requested) email`);
+//   } catch (err) {
+//     console.error(`${emailType.type} - FAILED to send (request) email:`, err);
+//   }
+// };
 
 exports.sendPasswordReset = async (emailAddress, name, resetUuid) => {
   if (
@@ -30,7 +64,7 @@ exports.sendPasswordReset = async (emailAddress, name, resetUuid) => {
 
     console.log('sendPasswordReset - Successfully sent (requested) email');
   } catch (err) {
-    console.error('sendPasswordReset - FAILED to send (request) email');
+    console.error('sendPasswordReset - FAILED to send (request) email', err);
   }
 };
 
@@ -58,6 +92,6 @@ exports.sendAddUser = async (emailAddress, name, addUserUuid) => {
 
     console.log('sendAddUser - Successfully sent (requested) email');
   } catch (err) {
-    console.error('sendAddUser - FAILED to send (requested) email to');
+    console.error('sendAddUser - FAILED to send (requested) email', err);
   }
 };
