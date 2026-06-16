ca20677cd5c9d4f0888f83cdc06073e3adf849ea
Joe Fong <joe.fong@madetech.com>
2026-06-16 11:33:46 +0100

    add a unit test to update banner

--- frontend/src/app/shared/components/update-banner/update-banner.component.spec.ts
+++ frontend/src/app/shared/components/update-banner/update-banner.component.spec.ts
@@ -1,5 +1,3 @@
-import lodash from 'lodash';
-
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
@@ -8,6 +6,7 @@ import { provideRouter, Router, RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
 import { render } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
+import lodash from 'lodash';
 
 import { UpdateBannerComponent } from './update-banner.component';
 
