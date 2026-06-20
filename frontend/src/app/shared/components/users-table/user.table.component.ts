f1844d5cb41e86036573affae5be9293853dc814
Praveena-Prasannan <Praveena.Prasannan@skillsforcare.org.uk>
2026-06-20 20:05:51 +0100

    Refactor user last login display logic

--- frontend/src/app/shared/components/users-table/user.table.component.ts
+++ frontend/src/app/shared/components/users-table/user.table.component.ts
@@ -1,17 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Establishment } from '@core/model/establishment.model';
 import { UserDetails, UserPermissionsType, UserStatus } from '@core/model/userDetails.model';
+import { UserService } from '@core/services/user.service';
 
 @Component({
-    selector: 'app-user-table',
-    templateUrl: './user-table.component.html',
-    standalone: false
+  selector: 'app-user-table',
+  templateUrl: './user-table.component.html',
+  standalone: false,
 })
-export class UserTableComponent {
+export class UserTableComponent implements OnInit {
   @Input() users: UserDetails[] = [];
   @Input() canViewUser = true;
   @Input() workplace: Establishment;
   @Input() userPermissionsTypes: UserPermissionsType[];
+
+  public lastLoggedInFromLogin: string;
+  public loggedUserUid: string;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.loggedInUser$.subscribe((user) => {
+      this.lastLoggedInFromLogin = user?.lastLoggedInFromLogin;
+      this.loggedUserUid = user?.uid;
+    });
+  }
+
+  private isLoggedUser(user: UserDetails): boolean {
+    return this.loggedUserUid === user.uid;
+  }
+
+  /**
+   * Use locally stored last login for logged-in user,
+   * otherwise use API value.
+   */
+  public getUserLastLogin(user: UserDetails): string {
+    return this.isLoggedUser(user) ? this.lastLoggedInFromLogin : user.lastLoggedIn;
+  }
 
   public isPending(user: UserDetails): boolean {
     return user.status === UserStatus.Pending;
