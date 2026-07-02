89695ff148a6abbdbb027082c271729e8c4ad2ef
Praveena-Prasannan <Praveena.Prasannan@skillsforcare.org.uk>
2026-07-02 15:21:08 +0100

    fix: update user details on submit

--- frontend/src/app/features/account-management/change-user-research/change-user-research.component.ts
+++ frontend/src/app/features/account-management/change-user-research/change-user-research.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { UntypedFormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Establishment } from '@core/model/establishment.model';
+import { UserDetails } from '@core/model/userDetails.model';
 import { BackLinkService } from '@core/services/backLink.service';
+import { BreadcrumbService } from '@core/services/breadcrumb.service';
 import { CreateAccountService } from '@core/services/create-account/create-account.service';
+import { EstablishmentService } from '@core/services/establishment.service';
 import { UserService } from '@core/services/user.service';
 import { UserResearchInviteDirective } from '@shared/directives/user/user-research-invite.directive';
 
@@ -13,40 +17,53 @@ import { UserResearchInviteDirective } from '@shared/directives/user/user-resear
 })
 export class ChangeUserResearchComponent extends UserResearchInviteDirective {
   public showProgressBar = false;
+  private primaryWorkplace: Establishment;
+  public user: UserDetails;
 
   constructor(
+    private establishmentService: EstablishmentService,
     protected backLinkService: BackLinkService,
     protected formBuilder: UntypedFormBuilder,
     protected router: Router,
     protected route: ActivatedRoute,
     protected userService: UserService,
     private createAccountService: CreateAccountService,
+    protected breadcrumbService: BreadcrumbService,
   ) {
-    super(backLinkService, formBuilder, router, route, userService);
+    super(backLinkService, formBuilder, router, route, userService, breadcrumbService);
   }
 
   init(): void {
     this.isExistingUser = true;
-    this.userService.loggedInUser$.subscribe((user) => {
-      this.userDetails = user;
-    });
     this.insideFlow = false;
     this.showProgressBar = false;
 
     this.confirmPagePath = '/account-management';
+    this.primaryWorkplace = this.establishmentService.primaryWorkplace;
   }
 
   protected loadUserResearchInviteResponse(): void {
-    this.userResearchInviteResponse = this.createAccountService.userResearchInviteResponse$.value;
+    this.userResearchInviteResponse = this.userDetails?.userResearchInviteResponse ?? null;
   }
 
   public onSubmit(): void {
     const responseValue = this.form.value.inviteResponse;
-
+    this.createAccountService.userResearchInviteResponse$.next(responseValue);
     if (responseValue !== null) {
-      this.createAccountService.userResearchInviteResponse$.next(responseValue);
-    }
+      const updatedUser: UserDetails = {
+        ...this.userDetails,
+        userResearchInviteResponse: responseValue,
+      };
 
+      this.userService
+        .updateUserDetails(this.primaryWorkplace.uid, this.userDetails.uid, updatedUser)
+        .subscribe((data) => {
+          this.userService.loggedInUser = {
+            ...this.userDetails,
+            ...data,
+          };
+        });
+    }
     this.router.navigate(['/account-management']);
   }
 }
