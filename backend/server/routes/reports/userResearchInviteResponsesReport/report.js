14b6a049ff760fb14cc7c4532f8d9fe22709faa7
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2026-07-09 15:32:55 +0100

    remove the link to a new report

--- backend/server/routes/reports/userResearchInviteResponsesReport/report.js
+++ backend/server/routes/reports/userResearchInviteResponsesReport/report.js
@@ -4,6 +4,7 @@ const moment = require('moment');
 const express = require('express');
 const router = express.Router();
 const { UserResearchInviteResponsesDataService } = require('./data');
+const { UserResearchInviteResponseChangesDataService } = require('./userResearchResponseChangesData');
 
 const printRow = (worksheet, result) => {
   worksheet.addRow({
@@ -15,18 +16,22 @@ const printRow = (worksheet, result) => {
     totalStaff: result?.establishment?.dataValues?.NumberOfStaffValue ?? '',
     userResearchInviteResponse: result?.UserResearchInviteResponseValue ?? '',
     createdDate: result?.created ?? '',
-    updatedDate: ''
+    updatedDate: result?.updated ?? '',
   });
 };
 
 const generateUserResearchInviteResponsesReport = async (_req, res) => {
   const reportData = await UserResearchInviteResponsesDataService.getReportData();
+  const changesData = await UserResearchInviteResponseChangesDataService.getReportData();
   let workbook = new excelJS.Workbook();
 
   workbook.creator = 'Skills-For-Care';
   workbook.properties.date1904 = true;
 
   const worksheet = workbook.addWorksheet('User Research Invite Responses');
+
+  const changesWorksheet = workbook.addWorksheet('Response Changes');
+
   worksheet.columns = [
     { header: 'Workplace ID', key: 'workplaceID' },
     { header: 'Name', key: 'name' },
@@ -39,6 +44,39 @@ const generateUserResearchInviteResponsesReport = async (_req, res) => {
     { header: 'Updated Date', key: 'updatedDate' },
   ];
 
+  changesWorksheet.columns = [
+    { header: 'Workplace ID', key: 'workplaceID' },
+    { header: 'Name', key: 'name' },
+    { header: 'Email Address', key: 'emailAddress' },
+    { header: 'Job Role', key: 'jobRole' },
+    { header: 'Main Service', key: 'mainService' },
+    { header: 'Total Staff', key: 'totalStaff' },
+    { header: 'previous response', key: 'currentValue' },
+    { header: 'latest response', key: 'newValue' },
+    { header: 'change date (latest response)', key: 'changeDate' },
+    { header: 'change date (user details)', key: 'updatedDate' },
+  ];
+
+  changesData.forEach((user) => {
+    const result = user.dataValues;
+
+    const latestChangedEvent = result.auditEvents?.sort((a, b) => new Date(b.when) - new Date(a.when))[0];
+
+    changesWorksheet.addRow({
+      workplaceID: result?.establishment?.dataValues?.nmdsId ?? '',
+      name: result?.FullNameValue ?? '',
+      emailAddress: result?.EmailValue ?? '',
+      jobRole: result?.JobTitleValue ?? '',
+      mainService: result?.establishment?.dataValues?.mainService?.dataValues?.name ?? '',
+      totalStaff: result?.establishment?.dataValues?.NumberOfStaffValue ?? '',
+
+      currentValue: latestChangedEvent?.event?.current ?? '',
+      newValue: latestChangedEvent?.event?.new ?? '',
+      changeDate: latestChangedEvent?.when ?? '',
+      updatedDate: result?.updated ?? '',
+    });
+  });
+
   const headerRow = worksheet.getRow(1);
   headerRow.font = { bold: true, name: 'Calibri' };
 
@@ -47,6 +85,7 @@ const generateUserResearchInviteResponsesReport = async (_req, res) => {
   });
 
   excelUtils.fitColumnsToSize(worksheet);
+  excelUtils.fitColumnsToSize(changesWorksheet);
 
   res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
   res.setHeader(
