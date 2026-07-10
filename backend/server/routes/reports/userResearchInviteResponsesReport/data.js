943e8fcbeb2f342a83330d09f73f89470ffe3a21
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2026-07-10 17:41:45 +0100

    remove the new tab and add the changes to the existing report

--- backend/server/routes/reports/userResearchInviteResponsesReport/data.js
+++ backend/server/routes/reports/userResearchInviteResponsesReport/data.js
@@ -4,35 +4,45 @@ const moment = require('moment');
 
 class UserResearchInviteResponsesDataService {
   static async getReportData() {
-     return await models.user.findAll({
-       attributes: [
-         'FullNameValue',
-         'EmailValue',
-         'JobTitleValue',
-         'UserResearchInviteResponseValue',
-         'created',
-         'updated',
-       ],
-       where: {
-         UserResearchInviteResponseValue: { [Op.or]: ['Yes', 'No'] },
-         Archived: false,
-         created: { [Op.gt]: moment().subtract(6, 'months').startOf('day').toDate() },
-       },
-       include: [
-         {
-           model: models.establishment,
-           as: 'establishment',
-           attributes: ['nmdsId', 'NumberOfStaffValue'],
-           include: [
-             {
-               model: models.services,
-               as: 'mainService',
-               attributes: ['name'],
-             },
-           ],
-         },
-       ],
-     });
+    return await models.user.findAll({
+      attributes: [
+        'FullNameValue',
+        'EmailValue',
+        'JobTitleValue',
+        'UserResearchInviteResponseValue',
+        'created',
+        'updated',
+      ],
+      where: {
+        UserResearchInviteResponseValue: { [Op.or]: ['Yes', 'No'] },
+        Archived: false,
+        created: { [Op.gt]: moment().subtract(6, 'months').startOf('day').toDate() },
+      },
+      include: [
+        {
+          model: models.establishment,
+          as: 'establishment',
+          attributes: ['nmdsId', 'NumberOfStaffValue'],
+          include: [
+            {
+              model: models.services,
+              as: 'mainService',
+              attributes: ['name'],
+            },
+          ],
+        },
+        {
+          model: models.userAudit,
+          as: 'auditEvents',
+          attributes: ['when', 'type', 'property', 'event'],
+          where: {
+            property: 'UserResearchInviteResponse',
+            type: 'changed',
+          },
+          required: false,
+        },
+      ],
+    });
   }
 }
 
--- backend/server/routes/reports/userResearchInviteResponsesReport/report.js
+++ backend/server/routes/reports/userResearchInviteResponsesReport/report.js
@@ -4,9 +4,11 @@ const moment = require('moment');
 const express = require('express');
 const router = express.Router();
 const { UserResearchInviteResponsesDataService } = require('./data');
-const { UserResearchInviteResponseChangesDataService } = require('./userResearchResponseChangesData');
 
 const printRow = (worksheet, result) => {
+  const latestChangedEvent = result.auditEvents?.sort((a, b) => new Date(b.when) - new Date(a.when))[0];
+  const previousResponse = latestChangedEvent?.event?.current;
+
   worksheet.addRow({
     workplaceID: result?.establishment?.dataValues?.nmdsId ?? '',
     name: result?.FullNameValue ?? '',
@@ -17,20 +19,22 @@ const printRow = (worksheet, result) => {
     userResearchInviteResponse: result?.UserResearchInviteResponseValue ?? '',
     createdDate: result?.created ?? '',
     updatedDate: result?.updated ?? '',
+    previousResponse: typeof previousResponse === 'string' ? previousResponse : '',
+
+    latestResponse: typeof previousResponse === 'string' ? latestChangedEvent?.event?.new : '',
+
+    changeDate: typeof previousResponse === 'string' ? latestChangedEvent?.when : '',
   });
 };
 
 const generateUserResearchInviteResponsesReport = async (_req, res) => {
   const reportData = await UserResearchInviteResponsesDataService.getReportData();
-  const changesData = await UserResearchInviteResponseChangesDataService.getReportData();
   let workbook = new excelJS.Workbook();
 
   workbook.creator = 'Skills-For-Care';
   workbook.properties.date1904 = true;
 
   const worksheet = workbook.addWorksheet('User Research Invite Responses');
-
-  const changesWorksheet = workbook.addWorksheet('Response Changes');
 
   worksheet.columns = [
     { header: 'Workplace ID', key: 'workplaceID' },
@@ -41,41 +45,11 @@ const generateUserResearchInviteResponsesReport = async (_req, res) => {
     { header: 'Total Staff', key: 'totalStaff' },
     { header: 'User Research Invite Response', key: 'userResearchInviteResponse' },
     { header: 'User Creation Date', key: 'createdDate' },
-    { header: 'Updated Date', key: 'updatedDate' },
+    { header: 'Change Date (user details)', key: 'updatedDate' },
+    { header: 'Previous Response', key: 'previousResponse' },
+    { header: 'Latest Response', key: 'latestResponse' },
+    { header: 'Change Date (latest response)', key: 'changeDate' },
   ];
-
-  changesWorksheet.columns = [
-    { header: 'Workplace ID', key: 'workplaceID' },
-    { header: 'Name', key: 'name' },
-    { header: 'Email Address', key: 'emailAddress' },
-    { header: 'Job Role', key: 'jobRole' },
-    { header: 'Main Service', key: 'mainService' },
-    { header: 'Total Staff', key: 'totalStaff' },
-    { header: 'previous response', key: 'currentValue' },
-    { header: 'latest response', key: 'newValue' },
-    { header: 'change date (latest response)', key: 'changeDate' },
-    { header: 'change date (user details)', key: 'updatedDate' },
-  ];
-
-  changesData.forEach((user) => {
-    const result = user.dataValues;
-
-    const latestChangedEvent = result.auditEvents?.sort((a, b) => new Date(b.when) - new Date(a.when))[0];
-
-    changesWorksheet.addRow({
-      workplaceID: result?.establishment?.dataValues?.nmdsId ?? '',
-      name: result?.FullNameValue ?? '',
-      emailAddress: result?.EmailValue ?? '',
-      jobRole: result?.JobTitleValue ?? '',
-      mainService: result?.establishment?.dataValues?.mainService?.dataValues?.name ?? '',
-      totalStaff: result?.establishment?.dataValues?.NumberOfStaffValue ?? '',
-
-      currentValue: latestChangedEvent?.event?.current ?? '',
-      newValue: latestChangedEvent?.event?.new ?? '',
-      changeDate: latestChangedEvent?.when ?? '',
-      updatedDate: result?.updated ?? '',
-    });
-  });
 
   const headerRow = worksheet.getRow(1);
   headerRow.font = { bold: true, name: 'Calibri' };
@@ -85,7 +59,6 @@ const generateUserResearchInviteResponsesReport = async (_req, res) => {
   });
 
   excelUtils.fitColumnsToSize(worksheet);
-  excelUtils.fitColumnsToSize(changesWorksheet);
 
   res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
   res.setHeader(
