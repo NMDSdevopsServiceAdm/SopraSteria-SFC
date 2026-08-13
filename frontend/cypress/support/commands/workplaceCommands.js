07bfb6806c93d38515a9920cea8d3894c67c8323
Joe Fong <joe.fong@madetech.com>
2026-08-13 13:55:55 +0100

    adding e2e test for slv miniflow

--- frontend/cypress/support/commands/workplaceCommands.js
+++ frontend/cypress/support/commands/workplaceCommands.js
@@ -106,6 +106,17 @@ Cypress.Commands.add('setWorkplaceCWPAwarenessQuestionViewed', (establishmentID)
   cy.task('dbQuery', { queryString, parameters });
 });
 
+Cypress.Commands.add('fillWorkplaceCWPAnswers', (establishmentID) => {
+  const queryString = `UPDATE cqc."Establishment"
+      SET "CareWorkforcePathwayWorkplaceAwarenessFK" = 1,
+          "CareWorkforcePathwayUseValue" = 'Yes',
+          "CWPAwarenessQuestionViewed" = true
+      WHERE "EstablishmentID" = $1;`;
+  const parameters = [establishmentID];
+
+  cy.task('dbQuery', { queryString, parameters });
+});
+
 Cypress.Commands.add('resetWorkplaceDHAAnswers', (establishmentID) => {
   const queryStrings = [
     `UPDATE cqc."Establishment"
@@ -149,6 +160,28 @@ Cypress.Commands.add('setWorkplaceDHAAnswers', (establishmentID, answers = {}) =
   const dbQueries = queryStrings.map((queryString) => ({ queryString, parameters }));
 
   cy.task('multipleDbQueries', dbQueries);
+});
+
+Cypress.Commands.add('fillWorkplaceDHAAnswers', (establishmentID) => {
+  const queryString = `UPDATE cqc."Establishment"
+      SET "StaffDoDelegatedHealthcareActivitiesValue" = 'Yes',
+          "StaffWhatKindDelegatedHealthcareActivitiesValue" = 'Yes'
+      WHERE "EstablishmentID" = $1;`;
+  const parameters = [establishmentID];
+
+  cy.task('dbQuery', { queryString, parameters });
+});
+
+Cypress.Commands.add('updateStaffTotalNumberToMatchActualWorkers', (establishmentID) => {
+  const queryString = `UPDATE cqc."Establishment" e
+      SET "NumberOfStaffValue" =
+        (SELECT COUNT(*) FROM cqc."Worker" w
+         WHERE "EstablishmentFK" = $1 AND "Archived" <> true)
+      WHERE "EstablishmentID" = $1
+      RETURNING *;`;
+  const parameters = [establishmentID];
+
+  cy.task('dbQuery', { queryString, parameters });
 });
 
 Cypress.Commands.add('setWorkplaceMainService', (establishmentID, mainServiceId) => {
