d7b6114e81a0f51ee0f3f636246114fe2286bb93
Joe Fong <joe.fong@madetech.com>
2026-08-13 15:17:08 +0100

    add unit test for resetVacanciesAndTurnoverService

--- frontend/src/app/core/guards/reset-vacancies-and-turnover-service/reset-vacancies-and-turnover-service.guard.ts
+++ frontend/src/app/core/guards/reset-vacancies-and-turnover-service/reset-vacancies-and-turnover-service.guard.ts
@@ -1,8 +1,13 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, CanDeactivateFn } from '@angular/router';
 import { VacanciesAndTurnoverService } from '@core/services/vacancies-and-turnover.service';
 
-export const resetVacanciesAndTurnoverService: CanActivateFn = (_route, _state) => {
+export const resetVacanciesAndTurnoverService: CanDeactivateFn<any> = (
+  _component,
+  _route,
+  _currentState,
+  _nextState,
+) => {
   const vacanciesAndTurnoverService = inject(VacanciesAndTurnoverService);
   vacanciesAndTurnoverService.clearAllSelectedJobRoles();
   vacanciesAndTurnoverService.resetVisitedAndSubmittedPages();
