eb47f8bac574440c0afdbd31df89956f135496b8
Joe Fong <joe.fong@madetech.com>
2026-08-13 14:18:55 +0100

    update e2e test

--- frontend/cypress/e2e/slvMiniFlow/slvMiniFlowForParentViewSub.spec.cy.js
+++ frontend/cypress/e2e/slvMiniFlow/slvMiniFlowForParentViewSub.spec.cy.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
-import { StandAloneEstablishment } from '../../support/mockEstablishmentData';
+import { ParentEstablishment, SubEstablishmentNotDataOwner } from '../../support/mockEstablishmentData';
 import { userPassword } from '../../support/configData';
 import { runTestsForSLVMiniFlow } from './runTestsForSlvMiniFlow.cy';
 
 describe('Starters Leavers Vacancies mini flow from home summary flag', { tags: '@workplace' }, () => {
   beforeEach(() => {
-    cy.loginAsUser(StandAloneEstablishment.editUserLoginName, userPassword);
+    cy.loginAsUser(ParentEstablishment.editUserLoginName, userPassword);
 
     cy.url().should('contain', 'dashboard');
   });
 
-  runTestsForSLVMiniFlow(StandAloneEstablishment);
+  const subEstablishment = SubEstablishmentNotDataOwner;
+  runTestsForSLVMiniFlow(subEstablishment);
 });
