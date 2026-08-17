efcdc49659e63037c783e90e62f539990bee77e3
Joe Fong <joe.fong@madetech.com>
2026-08-17 10:25:19 +0100

    amend SummaryRecordChangeComponent to allow using different link text

--- frontend/src/app/shared/components/summary-record-change/summary-record-change.component.spec.ts
+++ frontend/src/app/shared/components/summary-record-change/summary-record-change.component.spec.ts
@@ -7,8 +7,12 @@ import { render } from '@testing-library/angular';
 import { SummaryRecordChangeComponent } from './summary-record-change.component';
 
 describe('SummaryRecordChangeComponent', () => {
-  async function setup(explanationText = '', link = [], hasData = false) {
-    const component = await render(SummaryRecordChangeComponent, {
+  async function setup(overrides: any = {}) {
+    const explanationText = overrides?.explanationText ?? ' test';
+    const link = [];
+    const hasData = overrides?.hasData ?? false;
+    const overrideLabelText = overrides?.overrideLabelText ?? null;
+    const setupTools = await render(SummaryRecordChangeComponent, {
       imports: [SharedModule, RouterModule],
       declarations: [],
       providers: [provideRouter([]), provideHttpClient(), provideHttpClientTesting()],
@@ -16,10 +20,14 @@ describe('SummaryRecordChangeComponent', () => {
         explanationText,
         link,
         hasData,
+        overrideLabelText,
       },
     });
 
+    const component = setupTools.fixture.componentInstance;
+
     return {
+      ...setupTools,
       component,
     };
   }
@@ -30,17 +38,27 @@ describe('SummaryRecordChangeComponent', () => {
   });
 
   it('should render Change when it has data', async () => {
-    const { component } = await setup(' test', [], true);
-    expect(component.queryByText).toBeTruthy();
+    const { queryByText } = await setup({ hasData: true });
+    expect(queryByText('Change')).toBeTruthy();
   });
 
   it('should render Add information when it doesnt has data', async () => {
-    const { component } = await setup(' test', [], false);
-    expect(component.queryByText('Add')).toBeTruthy();
+    const { queryByText } = await setup({ hasData: false });
+    expect(queryByText('Add')).toBeTruthy();
   });
 
   it('should render the screen reader text', async () => {
-    const { component } = await setup(' test', [], false);
-    expect(component.queryByText('test')).toBeTruthy();
+    const { queryByText } = await setup({ hasData: false });
+    expect(queryByText('test')).toBeTruthy();
+  });
+
+  it('should render the given label text when overrideLabelText is provided', async () => {
+    const { queryByText } = await setup({ overrideLabelText: 'Review and confirm', hasData: true });
+    expect(queryByText('Review and confirm')).toBeTruthy();
+  });
+
+  it('should render the given label text when overrideLabelText is provided', async () => {
+    const { queryByText } = await setup({ overrideLabelText: 'Review and confirm', hasData: false });
+    expect(queryByText('Review and confirm')).toBeTruthy();
   });
 });
