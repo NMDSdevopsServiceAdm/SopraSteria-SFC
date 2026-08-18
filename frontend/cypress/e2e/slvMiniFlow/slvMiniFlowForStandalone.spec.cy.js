0e28745b008ed591dbc15ef328ffa42ad80971a0
Joe Fong <joe.fong@madetech.com>
2026-08-18 14:49:05 +0100

    remove unused import

--- frontend/cypress/e2e/slvMiniFlow/slvMiniFlowForStandalone.spec.cy.js
+++ frontend/cypress/e2e/slvMiniFlow/slvMiniFlowForStandalone.spec.cy.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
-import { onWorkplacePage } from '../../support/page_objects/onWorkplacePage';
 import { StandAloneEstablishment } from '../../support/mockEstablishmentData';
 import { userPassword } from '../../support/configData';
-import { onHomePage } from '../../support/page_objects/onHomePage';
 import { runTestsForSLVMiniFlow } from './runTestsForSlvMiniFlow.cy';
 
 describe('Starters Leavers Vacancies mini flow from home summary flag', { tags: '@workplace' }, () => {
