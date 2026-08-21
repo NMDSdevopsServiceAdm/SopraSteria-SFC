4199839d9fdc77a67cf71504c768257902e92c7b
Joe Fong <joe.fong@madetech.com>
2026-08-21 11:50:39 +0100

    mark a flaky e2e test as skipped (to be investigated seperately)

--- frontend/cypress/e2e/others/pageAfterLogin.spec.cy.js
+++ frontend/cypress/e2e/others/pageAfterLogin.spec.cy.js
@@ -98,7 +98,7 @@ describe('page after login', { tags: '@others' }, () => {
     cy.url().should('eq', Cypress.config().baseUrl + '/dashboard');
   });
 
-  it('should go to the update your vacancies and turnover data page', () => {
+  it.skip('should go to the update your vacancies and turnover data page', () => {
     setUpUserAttributes('LastViewedVacanciesAndTurnoverMessage', null);
     cy.url().should('eq', Cypress.config().baseUrl + '/update-your-vacancies-and-turnover-data');
     cy.get('h1').should('contain', 'Your Workplace vacancies and turnover information');
