74fc6af114ce3f508ca0dae939cb2ab1374405bf
Joe Fong <joe.fong@madetech.com>
2026-08-25 11:12:36 +0100

    (WIP) implementing nurse form interactions

--- frontend/src/app/features/workers/review-and-confirm-nurse-field-of-practice/review-and-confirm-nurse-field-of-practice.component.ts
+++ frontend/src/app/features/workers/review-and-confirm-nurse-field-of-practice/review-and-confirm-nurse-field-of-practice.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
-import { UntypedFormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NurseFieldOfPractice, RegisteredNurse } from '@core/model/nurse-field-of-practice.model';
 import { BackLinkService } from '@core/services/backLink.service';
 import { WorkerService } from '@core/services/worker.service';
+import { AccordionToggleButtonComponent } from './accordion-toggle-button/accordion-toggle-button.component';
+import { NurseRow } from './nurse-row.component';
 
 @Component({
   selector: 'app-review-and-confirm-nurse-field-of-practice',
-  imports: [],
+  imports: [ReactiveFormsModule, NurseRow],
   templateUrl: './review-and-confirm-nurse-field-of-practice.component.html',
   styleUrl: './review-and-confirm-nurse-field-of-practice.component.scss',
 })
 export class ReviewAndConfirmNurseFieldOfPracticeComponent implements OnInit {
-  public registeredNurses: Partial<RegisteredNurse>[];
+  public registeredNurses: RegisteredNurse[];
+  public allNurseFieldsOfPractice: NurseFieldOfPractice[];
+  public form: UntypedFormGroup;
 
   constructor(
     protected formBuilder: UntypedFormBuilder,
@@ -23,6 +27,18 @@ export class ReviewAndConfirmNurseFieldOfPracticeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.backLinkService.showBackLink();
     this.registeredNurses = this.route.snapshot.data?.registeredNurses;
+    this.allNurseFieldsOfPractice = this.route.snapshot.data.allNurseFieldsOfPractice;
+    this.setupForm();
+  }
+
+  setupForm(): void {
+    const allWorkerFormControls = Object.fromEntries(
+      this.registeredNurses.map((worker) => {
+        return [worker.uid, this.formBuilder.array(this.allNurseFieldsOfPractice.map(() => null))];
+      }),
+    );
+    this.form = this.formBuilder.group(allWorkerFormControls);
   }
 }
