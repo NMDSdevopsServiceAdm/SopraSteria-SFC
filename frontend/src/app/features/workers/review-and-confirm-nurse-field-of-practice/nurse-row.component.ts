76b3bb25a891c4132740fd6d6a67e0a583e8fabf
Joe Fong <joe.fong@madetech.com>
2026-08-25 11:33:44 +0100

    temp store curr changes

--- frontend/src/app/features/workers/review-and-confirm-nurse-field-of-practice/nurse-row.component.ts
+++ frontend/src/app/features/workers/review-and-confirm-nurse-field-of-practice/nurse-row.component.ts
@@ -22,25 +22,24 @@ const ButtonTextChange = {
   styleUrl: './review-and-confirm-nurse-field-of-practice.component.scss',
 })
 export class NurseRow implements OnInit {
-  public worker = input.required<RegisteredNurse>();
-  public workerIndex = input.required<number>();
+  public readonly worker = input.required<RegisteredNurse>();
+  public readonly workerIndex = input.required<number>();
 
-  public allNurseFieldsOfPractice: NurseFieldOfPractice[] = this.route.snapshot.data.allNurseFieldsOfPractice;
+  public readonly allNurseFieldsOfPractice: NurseFieldOfPractice[] = this.route.snapshot.data.allNurseFieldsOfPractice;
   public form: FormGroup = this.formBuilder.group({
     nurseFieldOfPractice: this.formBuilder.array(this.allNurseFieldsOfPractice.map(() => null)),
   });
-  private formValue = toSignal(this.form.get('nurseFieldOfPractice')!.valueChanges);
+  private formValue = toSignal<Array<boolean>>(this.form.get('nurseFieldOfPractice')!.valueChanges);
 
-  public isExpanded = signal(false);
+  public isExpanded = false;
 
   public currentChoices = computed(() => {
-    const formValue: Array<boolean> = this.formValue();
-    return this.allNurseFieldsOfPractice.filter((_field, index) => formValue[index]);
+    const formValue = this.formValue();
+    return this.allNurseFieldsOfPractice.filter((_field, index) => formValue?.[index]);
   });
   public buttonText = computed(() => {
-    const formValue: Array<boolean> = this.formValue();
-    const atLeastOneTick = formValue?.some((x) => x);
-    return atLeastOneTick ? ButtonTextChange : ButtonTextAdd;
+    const atLeastOneChosen = this.currentChoices()?.length > 0;
+    return atLeastOneChosen ? ButtonTextChange : ButtonTextAdd;
   });
 
   constructor(
@@ -54,12 +53,19 @@ export class NurseRow implements OnInit {
 
   public prefill() {
     const previousAnswer = this.worker().nurseFieldOfPractice;
-    const previousAnswerIds = new Set(previousAnswer.map((field) => field.id));
-    const choices = this.allNurseFieldsOfPractice.map((field) => previousAnswerIds.has(field.id));
-    this.form.patchValue({ nurseFieldOfPractice: choices });
+    this.form.patchValue({ nurseFieldOfPractice: this.answerToFormValues(previousAnswer) });
+  }
+
+  public answerToFormValues(chosenFields: Array<NurseFieldOfPractice>): Array<boolean> {
+    const chosenFieldIds = new Set(chosenFields.map((field) => field.id));
+    return this.allNurseFieldsOfPractice.map((field) => chosenFieldIds.has(field.id));
+  }
+
+  public formValuesToAnswer(formValues: Array<boolean>): Array<NurseFieldOfPractice> {
+    return this.allNurseFieldsOfPractice.filter((_field, index) => formValues[index]);
   }
 
   public handleToggle(state: boolean): void {
-    this.isExpanded.set(state);
+    this.isExpanded = state;
   }
 }
