dc0aa58fe436149827582fd0d98115321229ef2e
Joe Fong <joe.fong@madetech.com>
2026-09-21 12:14:44 +0100

    try setup serverless deploy for sendEmails lambda

--- lambdas/sendingEmails/sendEmails/serverless.js
+++ lambdas/sendingEmails/sendEmails/serverless.js
@@ -10,8 +10,8 @@ module.exports = {
     runtime: 'nodejs22.x',
     lambdaHashingVersion: '20201221',
     stage: process.env.STAGE ?? 'dev',
-    region: process.env.LAMBDA_REGION ?? 'eu-west-2',
-    ...(process.env.LAMBDA_ROLE_ARN && { iam: { role: process.env.LAMBDA_ROLE_ARN } }),
+    region: process.env.SEND_EMAILS_LAMBDA_REGION ?? 'eu-west-2',
+    ...(process.env.SEND_EMAILS_LAMBDA_ROLE_ARN && { iam: { role: process.env.SEND_EMAILS_LAMBDA_ROLE_ARN } }),
   },
 
   functions: {
